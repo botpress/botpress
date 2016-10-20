20aca661e656fd677fbdf5a678004a3e930aced6
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-19 22:39:30 -0400

    finished client-side auth utils

---
+++ web/master/jsx/components/Authentication/auth.js
@@ -0,0 +1,44 @@
+import EventEmitter2 from 'eventemitter2'
+import axios from 'axios'
+
+const storageKey = 'skin/token'
+
+export const authEvents = (new EventEmitter2())
+
+export const getToken = () => {
+  const tokenStr = localStorage.getItem(storageKey)
+
+  if(!!tokenStr) {
+    const token = JSON.parse(tokenStr)
+    axios.defaults.headers.common['authorization'] = token.token
+    return token
+  }
+
+  return false
+}
+
+export const setToken = (token) => {
+  localStorage.setItem(storageKey, JSON.stringify({
+    token, time: new Date()
+  }))
+
+  axios.defaults.headers.common['authorization'] = token
+  authEvents.emit('new_token')
+}
+
+export const logout = () => {
+  localStorage.removeItem(storageKey)
+  authEvents.emit('logout')
+}
+
+export const login = (user, password) => {
+  return axios.post('/api/login', { user, password })
+  .then((result) => {
+    if(result.data.success) {
+      setToken(result.data.token)
+      authEvents.emit('login')
+    } else {
+      throw new Error(result.data.reason)
+    }
+  })
+}
