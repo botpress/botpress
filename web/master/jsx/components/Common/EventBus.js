fed7fab0601fab19180ea720395e64f1d9a4276d
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-19 22:41:23 -0400

    refactored the EventBus to support socket authentication with new tokens

--- web/master/jsx/components/Common/EventBus.js
+++ web/master/jsx/components/Common/EventBus.js
@@ -1,5 +1,7 @@
 import EventEmitter2 from 'eventemitter2'
 
+import { getToken, authEvents } from '../Authentication/auth'
+
 class EventBus extends EventEmitter2 {
 
   constructor() {
@@ -7,32 +9,48 @@ class EventBus extends EventEmitter2 {
       wildcard: true,
       maxListeners: 100
     })
+
+    this.dispatchSocketEvent = this.dispatchSocketEvent.bind(this)
+    this.setup = this.setup.bind(this)
+
+    this.onAny(this.dispatchClientEvent)
+
+    authEvents.on('new_token', this.setup)
   }
 
-  start() {
-    if(this.started) {
+  dispatchSocketEvent(event) {
+    this.emit(event.name, event.data, 'server')
+  }
+
+  dispatchClientEvent(name, data, from) {
+    if(from === 'server') {
+      // we sent this event ourselves
       return
-    } else {
-      this.started = true
+    }
+
+    if(this.socket) {
+      this.socket.emit('event', { name, data: data })
+    }
+  }
+
+  setup() {
+    let query = ''
+    if(window.AUTH_ENABLED) {
+      const token = getToken()
+      if(!!token) {
+        query = 'token=' + token.token
+      }
+    }
+
+    if(this.socket) {
+      this.socket.off('event', this.dispatchEvent)
+      this.socket.disconnect()
     }
 
     // TODO Change this to support prod
-    const socket = this.socket = io.connect('http://localhost:3000', { query: 'token=' + 'TOKEN' })
-    socket.on('event', (event) => {
-      this.emit(event.name, event.data, 'server')
-    })
-    this.onAny(function(event, data, from) {
-      if(from === 'server') {
-        // we sent this event ourselves
-        return
-      }
-      socket.emit('event', {
-        name: event,
-        data: data
-      })
-    })
+    const socket = this.socket = io.connect('http://localhost:3000', { query })
+    socket.on('event', this.dispatchSocketEvent)
   }
-
 }
 
 EventBus.default = new EventBus()
