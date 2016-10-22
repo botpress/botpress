8c667d244842bc4d3051cd478d679b8079e0733f
Dany Fortin-Simard <dany.fortin-simard@hotmail.com>
2016-10-22 18:55:09 -0400

    search implementation

--- src/cli/search.js
+++ src/cli/search.js
@@ -1,4 +1,38 @@
-module.exports = function(search, options) {
-  // TODO Implement this
-  console.log('search triggered');
+import prompt from 'prompt'
+import chalk from 'chalk'
+
+module.exports = function(value, options) {
+
+  var getKeywords = function(value, cb){
+
+    if(typeof(value) === 'string'){
+      cb(null, value)
+
+    } else {
+      var schema = {
+        properties: {
+          keyword: {
+            description: chalk.white('keyword:'),
+            pattern: /^[a-zA-Z0-9][a-zA-Z0-9-_\.]+$/,
+            message: 'keyword must be letters, digits, dashes, underscores and dots.',
+            required: true
+          }
+        }
+      }
+
+      prompt.message = ''
+      prompt.delimiter = ''
+      prompt.start();
+
+      prompt.get(schema, function (err, result) {
+          cb(err, result.keyword)
+      })
+    }
+  }
+
+  getKeywords(value, function(err, data){
+     if (err) return console.error(err);
+     console.log("You are actualy looking for: "+ data);
+  })
+
 }
