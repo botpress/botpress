6132c80b724a94b662820b372eb273b9bd19b847
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-21 22:24:16 -0400

    added template files

---
+++ src/cli/templates/botfile.js
@@ -0,0 +1,19 @@
+module.exports = {
+  dataDir: "./data",
+  disableFileLogs: false,
+  notification: {
+    file: 'notifications.json',
+    maxLength: 50
+  },
+  log: {
+    file: 'bot.log',
+    maxSize: 1e6 // 1mb
+  },
+  login: {
+    enabled: true,
+    tokenExpiry: "6 hours",
+    password: "password",
+    maxAttempts: 3,
+    resetAfter: 5 * 60 * 10000 // 5 minutes
+  }
+}
