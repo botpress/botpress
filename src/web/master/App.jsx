406046c5ccb21c0ae03d091a5fcb8dd4bd7b27ae
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-24 13:57:41 -0400

    removed unused files

--- src/web/master/App.jsx
+++ src/web/master/App.jsx
@@ -6,7 +6,5 @@ require("bootstrap/dist/css/bootstrap.css")
 const style = require('./App.scss')
 
 ReactDOM.render(<div>
-  <button type="button" className="btn btn-default" aria-label="Left Align">
-    <span className="glyphicon glyphicon-align-left" aria-hidden="true"></span>
-  </button>
+  <h1>HELLO </h1>
 </div>, document.getElementById('app'))
