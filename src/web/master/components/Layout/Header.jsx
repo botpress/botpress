244bd0764b6c8a5171c06cd7b774d2d468cfc652
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-26 15:32:12 -0400

    removed unused buttons

--- src/web/master/components/Layout/Header.jsx
+++ src/web/master/components/Layout/Header.jsx
@@ -24,17 +24,6 @@ class Header extends React.Component {
   render() {
     return <Navbar inverse className={style.navbar}>
       <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider/>
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
         <Nav pullRight>
           <li role="presentation">
             <Link to="/logs">
