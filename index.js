c1ed0e06b667b829aa209e0782be97fad477226b
Sylvain Perron <slvn.perron+github@gmail.com>
2016-10-27 13:26:09 -0400

    couple of minor fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var fs = require('fs')
 var path = require('path')
 
-if(fs.existsSync(path.join(__dirname, 'src'))) {
-  module.exports = require(path.join(__dirname, 'src/skin'))
+if(fs.existsSync(path.join(__dirname, 'src/skin.js'))) {
+  module.exports = require(path.join(__dirname, 'src/skin.js'))
 } else {
-  module.exports = require(path.join(__dirname, 'lib/skin'))
+  module.exports = require(path.join(__dirname, 'lib/skin.js'))
 }
