3fac72c98da58cb9e0ef24e8773689306cbd564f
Sylvain Perron <slvn.perron+github@gmail.com>
2017-02-11 13:57:11 -0500

    Added database helpers for dates and table creation

--- src/database_helpers.js
+++ src/database_helpers.js
@@ -11,6 +11,77 @@ const isLite = knex => {
 
 module.exports = knex => {
 
+  const dateParse = exp => {
+    return isLite(knex)
+    ? knex.raw(`strftime('%Y-%m-%dT%H:%M:%fZ', ${exp})`)
+    : knex.raw(exp)
+  }
+
+  const dateFormat = date => {
+    const iso = moment(date).toDate().toISOString()
+    return dateParse(`'${iso}'`)
+  }
+
+  const columnOrDateFormat = colOrDate => {
+    const lite = isLite(knex)
+
+    if (colOrDate.sql) {
+      return colOrDate.sql
+    }
+
+    if (typeof colOrDate === 'string') {
+      return lite ? dateParse(colOrDate) : `"${colOrDate}"`
+    }
+
+    return dateFormat(colOrDate)
+  }
+
+  return {
+    isLite: () => isLite(knex),
+
+    // knex's createTableIfNotExists doesn't work with postgres
+    // https://github.com/tgriesser/knex/issues/1303
+    createTableIfNotExists: (tableName, cb) => {
+      return knex.schema.hasTable(tableName)
+      .then(exists => {
+        if (exists) return
+        return knex.schema.createTableIfNotExists(tableName, cb)
+      })
+    },
+
+    date: {
+      format: dateFormat,
+
+      now: () => isLite(knex) ? knex.raw("strftime('%Y-%m-%dT%H:%M:%fZ', 'now')") : knex.raw('now()'),
+
+      isBefore: (d1, d2) => {
+        d1 = columnOrDateFormat(d1)
+        d2 = columnOrDateFormat(d2)
+
+        return knex.raw(d1 + ' < ' + d2)
+      },
+
+      isAfter: (d1, d2) => {
+        d1 = columnOrDateFormat(d1)
+        d2 = columnOrDateFormat(d2)
+
+        return knex.raw(d1 + ' > ' + d2)
+      },
+
+      isBetween: (d1, d2, d3) => {
+        d1 = columnOrDateFormat(d1)
+        d2 = columnOrDateFormat(d2)
+
+        return knex.raw(`${d1} BETWEEN ${d2} AND ${d3}`)
+      },
+
+      isSameDay: (d1, d2) => {
+        d1 = columnOrDateFormat(d1)
+        d2 = columnOrDateFormat(d2)
+
+        return knex.raw(`date(${d1}) = date(${d2})`)
+      }
+    }
 
   }
 }
