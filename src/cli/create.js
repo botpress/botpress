05b3fa6758a9b6326d161f37e45ed5b261ad8a5e
Sylvain Perron <slvn.perron+github@gmail.com>
2017-02-26 15:26:30 -0500

    Fix #41

--- src/cli/create.js
+++ src/cli/create.js
@@ -1,3 +1,4 @@
+import { spawn } from 'child_process'
 import prompt from 'prompt'
 import chalk from 'chalk'
 import path from 'path'
@@ -9,8 +10,11 @@ import stats from '../stats'
 const MODULE_NAME_CONVENTION_BEGINS = 'botpress-'
 const MODULE_NAME_REGEX = new RegExp(/^botpress-.*/g)
 
-const introductionText = 'you are now creating a new module for your bot.'
-const nextStepText = 'your module is now setup and ready to be develop.'
+const introductionText = "This program will bootstrap a new Botpress module"
+const doneText = "You're all set! The module is boostrapped and ready to be developed."
+const documentation = "Tip: when coding your bot, use the command `npm run watch` to recompile" +
+  " your module automatically. Also, we strongly recommend that you install your module using " +
+  "`npm link ../path/to/botpress-module` so that your bot always points to the most recent version."
 
 const getTemplate = (template) => {
   const templatePath = path.join(__dirname, 'templates/create' , template)
@@ -25,14 +29,22 @@ const generateTemplate = (directory, filename, variables = {}) => {
   fs.writeFileSync(destination, compiled)
 }
 
-const prefixDirectoryNameWithBotpress = (directory) => {
-  util.print('warn','the name of your module needs to begin by "botpress-"')
-  util.print('warn','we renamed your module to '+ chalk.bold(MODULE_NAME_CONVENTION_BEGINS + directory))
-  return MODULE_NAME_CONVENTION_BEGINS + directory
+const prefixModuleNameWithBotpress = name => {
+
+  if (!MODULE_NAME_REGEX.test(name)) {
+    util.print('warn','the name of your module needs to begin by "botpress-"')
+    util.print('warn','we renamed your module to '+ chalk.bold(MODULE_NAME_CONVENTION_BEGINS + name))
+    name = MODULE_NAME_CONVENTION_BEGINS + name
+  }
+
+  return name
 }
 
 
 module.exports = function() {
+  const moduleDirectory = path.resolve('.')
+  const dirname = path.basename(moduleDirectory)
+
   stats({}).track('cli', 'modules', 'create')
 
   util.print(introductionText)
@@ -40,11 +52,12 @@ module.exports = function() {
   var schema = {
     properties: {
       name: {
-        description: chalk.white('name:'),
+        description: chalk.white('module name:'),
         pattern: /^[a-z0-9][a-z0-9-_\.]+$/,
         message: 'name must be only lowercase letters, ' +
           'digits, dashes, underscores and dots.',
-        required: true
+        required: true,
+        default: dirname
       },
       description: {
         required: false,
@@ -68,20 +81,17 @@ module.exports = function() {
   prompt.start()
 
   prompt.get(schema, function (err, result) {
-    var moduleDirectory = result.name
-
-    if (!MODULE_NAME_REGEX.test(moduleDirectory)) {
-      result.name = moduleDirectory = prefixDirectoryNameWithBotpress(moduleDirectory)
+    result.name = prefixModuleNameWithBotpress(result.name)
+    
+    
+    if (dirname !== result.name) {
+      util.print('warn', `We usually recommend that the name of the module directory (${dirname}) be the same as the module name (${result.name})`)
     }
 
     if (fs.existsSync(path.join(moduleDirectory, 'package.json'))) {
-      util.print('error','directory name already exists in the current folder.')
+      util.print('error', "Expected module directory to be empty / uninitialized")
       process.exit(1)
     } else {
-      if (!fs.existsSync(moduleDirectory)) {
-        fs.mkdirSync(moduleDirectory)
-      }
-
       generateTemplate(moduleDirectory, 'package.json', result)
       generateTemplate(moduleDirectory, 'LICENSE')
       generateTemplate(moduleDirectory, 'webpack.js')
@@ -90,12 +100,32 @@ module.exports = function() {
 
       fs.mkdirSync(moduleDirectory + '/src')
       generateTemplate(moduleDirectory, 'src/index.js')
-      
+
       fs.mkdirSync(moduleDirectory + '/src/views')
       generateTemplate(moduleDirectory, 'src/views/index.jsx')
       generateTemplate(moduleDirectory, 'src/views/style.scss')
 
-      util.print('success',nextStepText)
+      util.print('Installing dependencies, please wait...')
+
+      const install = spawn(util.npmCmd, ['install'])
+
+      install.stdout.on('data', (data) => {
+        process.stdout.write(data.toString())
+      })
+
+      install.stderr.on('data', (data) => {
+        process.stdout.write(data.toString())
+      })
+
+      install.on('close', (code) => {
+        if (code > 0) {
+          util.print('error', 'An error occured during the dependencies installation of your module')
+        } else {
+          util.print('success', 'Module dependencies installed')
+          util.print(doneText)
+          util.print(documentation)
+        }
+      })
     }
   })
 }
