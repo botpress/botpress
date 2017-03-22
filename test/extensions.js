fef8f3fc60cff34fd62b118fc90c777cbb03dfa8
Dany Fortin-Simard <dany.fortin-simard@hotmail.com>
2017-03-22 15:56:42 -0400

    Add some test for require extensions

--- test/extensions.js
+++ test/extensions.js
@@ -20,12 +20,14 @@ describe('Extensions Loader', () => {
   }
 
   before(function() {
+
     const extensionPath = (edition, file) => path.resolve(__dirname, '../extensions', edition, file)
-    const fileContent = edition => 'module.exports = () => ' + edition
+    const fileContent = edition => `module.exports = () => '${edition}'`
 
     writeFile(extensionPath('lite', 'b.js'), fileContent('lite'))
     writeFile(extensionPath('pro', 'a.js'), fileContent('pro'))
     writeFile(extensionPath('ultimate', 'c.js'), fileContent('ultimate'))
+    
   })
 
   after(function() {
@@ -35,13 +37,33 @@ describe('Extensions Loader', () => {
   it('Require works for edition', function() {
 
     process.env.BOTPRESS_EDITION = 'ultimate'
-    expect(requireExtension('c.js')).to.equal('ultimate')
+    expect(requireExtension('c.js')()).to.equal('ultimate')
 
     process.env.BOTPRESS_EDITION = 'pro'
-    expect(requireExtension('a.js')).to.equal('pro')
+    expect(requireExtension('a.js')()).to.equal('pro')
 
     process.env.BOTPRESS_EDITION = 'lite'
-    expect(requireExtension('a.js')).to.equal('lite')
+    expect(requireExtension('b.js')()).to.equal('lite')
+  })
+
+  it('Require works for downgrade (Ultimate -> Pro)', function() {
+    process.env.BOTPRESS_EDITION = 'ultimate'
+    expect(requireExtension('a.js')()).to.equal('pro')
+  })
+
+  it('Require works for downgrade (Ultimate -> Lite)', function() {
+    process.env.BOTPRESS_EDITION = 'ultimate'
+    expect(requireExtension('b.js')()).to.equal('lite')
+  })
+
+  it('Require works for downgrade (Pro -> Lite)', function() {
+    process.env.BOTPRESS_EDITION = 'pro'
+    expect(requireExtension('b.js')()).to.equal('lite')
+  })
+
+  it('File require does not exist in lite', function() {
+    process.env.BOTPRESS_EDITION = 'ultimate'
+    expect(() => requireExtension('d.js')).to.throw()
   })
 
 })
