9b4ee2bd2623529269ec93671d33dc1bd730978d
Sylvain Perron <slvn.perron+github@gmail.com>
2017-03-28 17:25:44 -0400

    LoginInitialization

--- src/web/views/Login/index.jsx
+++ src/web/views/Login/index.jsx
@@ -22,6 +22,10 @@ export default class LoginPage extends Component {
     }
   }
 
+  componentDidMount() {
+    Decorators.LoginInitialization && Decorators.LoginInitialization(this)
+  }
+
   renderGlobalStyle() {
     return <style>
       {
