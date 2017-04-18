9eba1d82210c79357d6f3a95928cc70f31f4a865
Sylvain Perron <slvn.perron+github@gmail.com>
2017-04-18 03:17:37 -0400

    Additional login page styling

--- src/web/views/Login/index.jsx
+++ src/web/views/Login/index.jsx
@@ -28,13 +28,6 @@ export default class LoginPage extends Component {
     Decorators.LoginInitialization && Decorators.LoginInitialization(this)
   }
 
-  renderGlobalStyle() {
-    return <style>
-      {
-        "\ body{\ background-color: #e4eaec;\ }\ "
-      }</style>
-  }
-
   handlePasswordChange(event) {
     this.setState({ password: event.target.value })
   }
@@ -65,44 +58,61 @@ export default class LoginPage extends Component {
     </div>
   }
 
+  componentDidMount() {
+    document.body.className=classnames('bp-body', 'bp-body-login', styles['body'])
+  }
+
+  componentWillUnmount() {
+    document.body.className="bp-body"
+  }
+
   render() {
     const hasChangedPassword = !!this.props.location.query.reset
     const hasAnError = !!this.state.error
 
     const tallPanelStyle = hasChangedPassword || hasAnError
 
-    const panelStyle = classnames('panel', 'bp-login', styles['panel-center'], {
+    const panelStyle = classnames('panel')
+
+    const blockStyle = classnames('bp-login', styles['panel-center'], {
       [styles['panel-center-tall']]: tallPanelStyle
     })
+
+    const logoStyle = classnames('bp-logo', styles['logo'])
 
     const headerStyle = classnames('panel-heading', 'text-center', styles.header, 'bp-header')
     const errorStyle = classnames(styles.error)
     const successStyle = classnames(styles.success)
 
-    return <div>{this.renderGlobalStyle()}
+    return <div>
       <div className="block-center mt-xl wd-xl">
-        <div className={panelStyle}>
-          <div className={headerStyle}>
-            <h4>Login</h4>
+        <div className={blockStyle}>
+          <div className={logoStyle}>
+            <img src="/img/logo_grey.png"></img>
           </div>
-          <div className="panel-body">
-            {this.renderLoading()}
-            {this.state.error && <p className={errorStyle}>{this.state.error}</p>}
-            {hasChangedPassword && <p className={successStyle}>Password changed successfully</p>}
-            <form onSubmit={this.handleSubmit.bind(this)}>
-              <FormGroup>
-                <ControlLabel>User</ControlLabel>
-                <Decorators.User value={this.state.user} onChange={::this.handleUserChange}/>
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>Password</ControlLabel>
-                <FormControl type="password" placeholder="" value={this.state.password} onChange={::this.handlePasswordChange}/>
-              </FormGroup>
-              <Button className="pull-right" type="submit">Login</Button>
-              <Decorators.ForgotPassword />
-            </form>
-          </div>
-        </div>
+          <div className={panelStyle}>
+            <div className={headerStyle}>
+              <h4>Login</h4>
+            </div>
+            <div className="panel-body">
+              {this.renderLoading()}
+              {this.state.error && <p className={errorStyle}>{this.state.error}</p>}
+              {hasChangedPassword && <p className={successStyle}>Password changed successfully</p>}
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <FormGroup>
+                  <ControlLabel>User</ControlLabel>
+                  <Decorators.User value={this.state.user} onChange={::this.handleUserChange}/>
+                </FormGroup>
+                <FormGroup>
+                  <ControlLabel>Password</ControlLabel>
+                  <FormControl type="password" placeholder="" value={this.state.password} onChange={::this.handlePasswordChange}/>
+                </FormGroup>
+                <Button className="pull-right" type="submit">Login</Button>
+                <Decorators.ForgotPassword />
+              </form>
+            </div> { /* End Panel Body */ }
+          </div> { /* End Panel */ }
+        </div> { /* End Block */ }
       </div>
     </div>
   }
