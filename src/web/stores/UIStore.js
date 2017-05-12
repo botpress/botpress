c34f4b5be778357eac20be23f5b2783751163ce9
Dany Fortin-Simard <dany@botpress.io>
2017-05-12 15:18:27 -0400

    Add view mode to  UI store

--- src/web/stores/UIStore.js
+++ src/web/stores/UIStore.js
@@ -1,19 +1,25 @@
 import { Store, toImmutable } from 'nuclear-js'
 
 import actionTypes from '~/actions/actionTypes'
-const { TOGGLE_LICENSE_MODAL, TOGGLE_ABOUT_MODAL } = actionTypes
+const { 
+  TOGGLE_LICENSE_MODAL,
+  TOGGLE_ABOUT_MODAL,
+  VIEW_MODE_CHANGED
+} = actionTypes
 
 export default Store({
   getInitialState() {
     return toImmutable({
       licenseModalOpened: false,
-      aboutModalOpened: false
+      aboutModalOpened: false,
+      viewMode: -1
     })
   },
 
   initialize() {
     this.on(TOGGLE_LICENSE_MODAL, toggleLicenseModal)
     this.on(TOGGLE_ABOUT_MODAL, toggleAboutModal)
+    this.on(VIEW_MODE_CHANGED, viewModeChanged)
   }
 })
 
@@ -23,4 +29,8 @@ function toggleLicenseModal(state) {
 
 function toggleAboutModal(state) {
   return state.set('aboutModalOpened', !state.get('aboutModalOpened'))
+}
+
+function viewModeChanged(state, { mode }) {
+  return state.set('viewMode', mode.toString())
 }
