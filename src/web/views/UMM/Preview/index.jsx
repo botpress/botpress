00af67e57f8db3206066049e2a4b44ae78e731e5
Dany Fortin-Simard <dany@botpress.io>
2017-05-13 10:20:12 -0400

    Initial structure fo the files

---
+++ src/web/views/UMM/Preview/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import classnames from 'classnames'
+
+import { Grid, Row, Col, Panel } from 'react-bootstrap'
+
+const style = require('./style.scss')
+
+export default class PreviewList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return <h1>Preview</h1>
+  }
+}
--- src/web/views/UMM/index.js
+++ src/web/views/UMM/index.js
@@ -5,6 +5,10 @@ import classnames from 'classnames'
 import ContentWrapper from '~/components/Layout/ContentWrapper'
 import PageHeader from '~/components/Layout/PageHeader'
 
+import List from './List'
+import Code from './Code'
+import Preview from './Preview'
+
 import { Grid, Row, Col, Panel } from 'react-bootstrap'
 
 const style = require('./style.scss')
@@ -12,9 +16,70 @@ const style = require('./style.scss')
 export default class UMMView extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.fetchBlocks()
+
+    this.setState({
+      loading: false
+    })
+  }
+
+  fetchBlocks() {
+    this.state = {
+      blocks: {}
+    }
+  }
+
+  renderLeftSection() {
+    const classNames = classnames({
+      [style.left]: true,
+      'bp-umm-left': true
+    })
+
+    return <Row className={classNames}>
+        <Col md={12}>
+          <List />
+        </Col>
+      </Row>
+  }
+
+  renderCenterSection() {
+    const classNames = classnames({
+      [style.center]: true,
+      'bp-umm-center': true
+    })
+
+    return <Row className={classNames}>
+        <Col md={12}>
+          <Code />
+        </Col>
+      </Row>
+  }
+
+  renderRightSection() {
+    const classNames = classnames({
+      [style.right]: true,
+      'bp-umm-right': true
+    })
+
+    return <Row className={classNames}>
+        <Col md={12}>
+          <Preview />
+        </Col>
+      </Row>
   }
 
   render() {
+    if (this.state.loading) {
+      return null
+    }
+
     const classNames = classnames({
       [style.umm]: true,
       'bp-umm': true
@@ -25,8 +90,14 @@ export default class UMMView extends Component {
         <PageHeader><span>Universal Message Markdown</span></PageHeader>
         <Grid fluid>
           <Row className={classNames}>
-            <Col>
-              <h1>UMM</h1>
+            <Col md={2}>
+              {this.renderLeftSection()}
+            </Col>
+            <Col md={5}>
+              {this.renderCenterSection()}
+            </Col>
+            <Col md={5}>
+              {this.renderRightSection()}
             </Col>
           </Row>
         </Grid>
