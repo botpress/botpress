54045a46a82857fb2dd223ce611b24b55287cfa4
Sylvain Perron <slvn.perron+github@gmail.com>
2017-06-02 16:47:53 -0400

    Added a few plugins

--- book.js
+++ book.js
@@ -3,7 +3,7 @@ var pkg = require('./package.json')
 module.exports = {
   root: "./docs",
   title: "Botpress Official Documentation",
-  plugins: ["noembed", "sitemap"],
+  plugins: ["noembed", "sitemap", "expandable-chapters", "alerts"],
   variables: {
     version: pkg.version
   },
