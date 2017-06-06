4af10144c61c71b3292194ea71dcc23d3db90563
Dany Fortin-Simard <dany@botpress.io>
2017-06-06 14:48:40 -0400

    Partial work toward align code and views

--- src/web/views/UMM/Code/index.jsx
+++ src/web/views/UMM/Code/index.jsx
@@ -10,42 +10,49 @@ require('codemirror/lib/codemirror.css')
 require('codemirror/theme/zenburn.css')
 require('codemirror/mode/yaml/yaml')
 
-const LAST_LINE_REGEX = /^.+:/
+const LAST_LINE_REGEX = /^(.+):/
 
 export default class CodeView extends Component {
   constructor(props) {
     super(props)
+
+    this.codeMirror = null
   }
 
   componentDidMount() {
-    this.getBlockHeights()
-    .then(::this.getBlockLines)
-    .then(::this.getCorrespondingDiv)
-    .then(::this.setHeightLastLines)
+    this.refreshAdjustments()
   }
 
-  getBlockHeights() {
+  refreshAdjustments() {
+    this.getBlockDivs()
+    .then(::this.getLastLines)
+    .then(::this.getLineDivs)
+    .then(::this.setHeights)
+  }
+
+  getBlockDivs() {
     return new Promise((resolve, reject) => {
       const blocks = document.getElementsByClassName('bp-umm-block')
-      const blockHeights = []
-      _.forEach(blocks, (block) => {
-        blockHeights.push(block.clientHeight)
-      })
 
       this.setState({
-        blockHeights: blockHeights
+        blockDivs: blocks
       })
 
       return resolve()
     })
   }
 
-  getBlockLines() {
+  getLastLines() {
     return new Promise((resolve, reject) => {
       const lines = []
       _.forEach(this.props.code.split('\n'), (line, i) => {
         if (LAST_LINE_REGEX.test(line)) {
-          lines.push(i)
+          const contentNext = line.match(LAST_LINE_REGEX)[1]
+
+          lines.push({ 
+            index: i,
+            contentNext: contentNext 
+          })
         }
       })
 
@@ -57,34 +64,84 @@ export default class CodeView extends Component {
     })
   }
 
-  getCorrespondingDiv() {
-    const lineDivs = document.getElementsByClassName('CodeMirror-line')
+  getLineDivs() {
+    const lines = document.getElementsByClassName('CodeMirror-line')
+    let beginIndex = 0
 
     return new Promise((resolve, reject) => {
-      const lastLineDivs = []
+      const lineDivs = []
 
-      _.forEach(this.state.lastLines, (i) => {
-        if (i !== 0) {
-          lastLineDivs.push(lineDivs[i - 1])
+      _.forEach(this.state.lastLines, (last) => {
+        if (last.index !== 0) {
+          const index = last.index - 1
+
+          lineDivs.push({
+            lastLine: lines[index],
+            numberOfRows: index - beginIndex + 1
+          })
+
+          beginIndex = index + 1
         }
       })
 
       this.setState({
-        lastLineDivs: lastLineDivs
+        lineDivs: lineDivs
       })
 
       return resolve()
     })
   }
 
-  setHeightLastLines() {
+  setHeight(line, block, i, rowHeight) {
+    const linesHeight = rowHeight * line.numberOfRows
+    const blockHeight = block.clientHeight
+
+    let numberOfRowToAdd = Math.floor((blockHeight - linesHeight) / rowHeight) + 1
+
+    if (linesHeight <= blockHeight) {
+
+      let toAdd = ""
+      
+      for (let count = 0; count < numberOfRowToAdd; count++) {
+        toAdd += "\n"
+      }
+
+      const content = this.state.lastLines[i + 1].contentNext
+      const code = this.props.code.replace(content, toAdd + content)
+      this.props.update(code)
+    }
+
+    numberOfRowToAdd = numberOfRowToAdd > 0 ? numberOfRowToAdd : 0
+    const marginBottom = ((line.numberOfRows + numberOfRowToAdd) * rowHeight) - (blockHeight)
+    
+    block.setAttribute('style', 'margin-bottom: ' + marginBottom + 'px;')
+  }
+
+  setHeights() {
+    const row = document.getElementsByClassName('CodeMirror-line')[0]
+    let rowHeight = 0
+    
+    if (row) {
+      rowHeight = row.clientHeight
+    }
+    
     return new Promise((resolve, reject) => {
-      _.forEach(this.state.lastLineDivs, (line) => {
-        line.setAttribute('style', 'background-color:red; border: 1px solid blue;')
+      _.forEach(this.state.lineDivs, (line, i) => {
+
+        const block = this.state.blockDivs[i]
+        
+        if (!this.props.error && block) {
+          this.setHeight(line, block, i, rowHeight)
+        }
       })
 
       return resolve()
     })
+  }
+
+  handleCodeChanged(event) {
+    this.props.update(event)
+    this.refreshAdjustments()
   }
 
   renderEditor() {
@@ -97,15 +154,17 @@ export default class CodeView extends Component {
       theme: 'zenburn',
       mode: 'yaml',
       lineNumbers: true,
+      lineWrapping: true,
       indentUnit: 1,
       tabSize: 1,
+      showInvisibles: true,
       scrollbarStyle: null
     }
 
     return <CodeMirror 
       className={classNames}
       value={this.props.code}
-      onChange={this.props.update}
+      onChange={::this.handleCodeChanged}
       options={options} />
   }
 
