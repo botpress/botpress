060570e8ad0a53ecd34d90967216db26b89667cb
Dany Fortin-Simard <dany@botpress.io>
2017-06-07 06:53:49 -0400

    Use infected views from modules

--- src/web/views/UMM/Preview/Block/index.jsx
+++ src/web/views/UMM/Preview/Block/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import classnames from 'classnames'
 
 import Outgoing from './Outgoing'
+import InjectedModuleView from '~/components/PluginInjectionSite/module'
 
 const style = require('./style.scss')
 
@@ -10,8 +11,23 @@ export default class Block extends Component {
     super(props)
   }
 
-  renderOutgoing(data, key) {
-    return <Outgoing data={data} key={key} />
+  renderOutgoing({ platform, type, text, raw }, key) {
+    if (platform === 'facebook') {
+      platform = 'messenger'
+    }
+
+    const moduleName = 'botpress-' + platform
+
+    return (
+      <InjectedModuleView
+        key={key}
+        moduleName={moduleName}
+        viewName={'UMMOutgoing'}
+        type={type}
+        text={text}
+        raw={raw}
+        onNotFound={null} />
+    ) 
   }
 
   render() {
