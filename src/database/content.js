ef9db57aceff14d91a489a1e9bd9f381f3dba636
Sylvain Perron <slvn.perron+github@gmail.com>
2017-09-18 12:41:20 -0400

    Content Editor – Changed from varchar(255) to text

--- src/database/content.js
+++ src/database/content.js
@@ -7,11 +7,11 @@ import helpers from './helpers'
 module.exports = knex => {
   return helpers(knex).createTableIfNotExists('content_items', function (table) {
     table.string('id').primary()
-    table.string('data')
-    table.string('formData')
-    table.string('metadata')
+    table.text('data')
+    table.text('formData')
+    table.text('metadata')
     table.string('categoryId')
-    table.string('previewText')
+    table.text('previewText')
     table.string('created_by')
     table.timestamp('created_on')
   })
