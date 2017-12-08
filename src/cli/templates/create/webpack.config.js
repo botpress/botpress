4a32e57b6fe85ddcf78a604013b2a279d8c83cad
Hugo Josefson <hugo@josefson.org>
2017-12-09 00:36:34 +0100

    Use minimum-node-version instead of cleanVersion()

--- src/cli/templates/create/webpack.config.js
+++ src/cli/templates/create/webpack.config.js
@@ -1,145 +1,149 @@
 const path = require('path')
+const minimumNodeVersion = require('minimum-node-version').default
 const nodeExternals = require('webpack-node-externals')
 const pkg = require('./package.json')
 
-const cleanVersion = semver => semver.replace(/[>=]/g, '')
-
-const commonConfig = {
-  context: path.resolve(__dirname, 'src'),
-  devtool: 'source-map',
-  stats: 'minimal'
-}
-
-const nodeConfig = Object.assign({}, commonConfig, {
-  entry: ['./index.js'],
-  output: {
-    path: path.resolve(__dirname, 'bin'),
-    filename: 'node.bundle.js',
-    libraryTarget: 'commonjs2',
-    library: ['botpress', pkg.name]
-  },
-  resolve: {
-    extensions: ['.js'],
-    modules: [path.resolve(__dirname, 'node_modules'), 'node_modules']
-  },
-  externals: [nodeExternals(), 'botpress'],
-  target: 'node',
-  node: {
-    __filename: false,
-    __dirname: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            sourceMaps: true,
-            presets: [
-              [
-                'env',
-                {
-                  targets: {
-                    node: cleanVersion(pkg.engines.node)
-                  }
-                }
-              ]
-            ],
-            plugins: ['transform-object-rest-spread', 'add-module-exports']
-          }
-        }
-      }
-    ]
+const configsPromise = minimumNodeVersion().then(nodeVersion => {
+  const commonConfig = {
+    context: path.resolve(__dirname, 'src'),
+    devtool: 'source-map',
+    stats: 'minimal'
   }
-})
 
-const webConfig = Object.assign({}, commonConfig, {
-  entry: ['./views/index.jsx'],
-  output: {
-    path: path.resolve(__dirname, 'bin'),
-    publicPath: '/js/modules/',
-    filename: 'web.bundle.js',
-    libraryTarget: 'assign',
-    library: ['botpress', pkg.name]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'react',
-              [
-                'env',
-                {
-                  targets: {
-                    browsers: ['last 2 versions', 'ie >= 10']
-                  }
-                }
-              ]
-            ],
-            plugins: ['transform-object-rest-spread']
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
+  const nodeConfig = Object.assign({}, commonConfig, {
+    entry: ['./index.js'],
+    output: {
+      path: path.resolve(__dirname, 'bin'),
+      filename: 'node.bundle.js',
+      libraryTarget: 'commonjs2',
+      library: ['botpress', pkg.name]
+    },
+    resolve: {
+      extensions: ['.js'],
+      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules']
+    },
+    externals: [nodeExternals(), 'botpress'],
+    target: 'node',
+    node: {
+      __filename: false,
+      __dirname: false
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: pkg.name + '__[name]__[local]___[hash:base64:5]'
+              sourceMaps: true,
+              presets: [
+                [
+                  'env',
+                  {
+                    targets: {
+                      node: nodeVersion
+                    }
+                  }
+                ]
+              ],
+              plugins: ['transform-object-rest-spread', 'add-module-exports']
             }
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '../fonts/[name].[ext]'
           }
         }
-      }
-    ]
-  }
+      ]
+    }
+  })
+
+  const webConfig = Object.assign({}, commonConfig, {
+    entry: ['./views/index.jsx'],
+    output: {
+      path: path.resolve(__dirname, 'bin'),
+      publicPath: '/js/modules/',
+      filename: 'web.bundle.js',
+      libraryTarget: 'assign',
+      library: ['botpress', pkg.name]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                'react',
+                [
+                  'env',
+                  {
+                    targets: {
+                      browsers: ['last 2 versions', 'ie >= 10']
+                    }
+                  }
+                ]
+              ],
+
+              plugins: ['transform-object-rest-spread']
+            }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: pkg.name + '__[name]__[local]___[hash:base64:5]'
+              }
+            },
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '../fonts/[name].[ext]'
+            }
+          }
+        }
+      ]
+    }
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  const liteConfig = Object.assign({}, webConfig, {
+    // Add your lite views here, see example below
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      // example: './views/example.lite.jsx'
+    },
+    output: {
+      path: path.resolve(__dirname, 'bin/lite'),
+      publicPath: '/js/lite-modules/',
+      filename: '[name].bundle.js',
+      libraryTarget: 'assign',
+      library: ['botpress', pkg.name]
+    }
+  })
+
+  return [nodeConfig, webConfig /*, liteConfig*/]
 })
 
-// eslint-disable-next-line no-unused-vars
-const liteConfig = Object.assign({}, webConfig, {
-  // Add your lite views here, see example below
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    // example: './views/example.lite.jsx'
-  },
-  output: {
-    path: path.resolve(__dirname, 'bin/lite'),
-    publicPath: '/js/lite-modules/',
-    filename: '[name].bundle.js',
-    libraryTarget: 'assign',
-    library: ['botpress', pkg.name]
-  }
-})
-
-module.exports = [nodeConfig, webConfig /*, liteConfig*/]
+module.exports = configsPromise
