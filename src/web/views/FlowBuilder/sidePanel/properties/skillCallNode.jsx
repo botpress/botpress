b427748ada061960c6b62c1dbec7a3318882581b
Sylvain Perron <slvn.perron+github@gmail.com>
2017-12-08 17:07:59 -0500

    Side panel of Skills

--- src/web/views/FlowBuilder/sidePanel/properties/skillCallNode.jsx
+++ src/web/views/FlowBuilder/sidePanel/properties/skillCallNode.jsx
@@ -8,7 +8,7 @@ import TransitionSection from './TransitionSection'
 
 const style = require('../style.scss')
 
-export default class StandardNodePropertiesPanel extends Component {
+export default class SkillCallNodePropertiesPanel extends Component {
   renameNode(text) {
     if (text.length > 0 && text !== this.props.node.name) {
       this.props.updateNode({ name: text })
@@ -37,17 +37,6 @@ export default class StandardNodePropertiesPanel extends Component {
           className={style.name}
           onChanged={::this.renameNode}
           transform={this.transformText}
-        />
-        <ActionSection
-          items={node['onEnter']}
-          header="On Enter"
-          onItemsUpdated={items => this.props.updateNode({ onEnter: items })}
-        />
-        <ActionSection
-          items={node['onReceive']}
-          header="On Receive"
-          waitable={true}
-          onItemsUpdated={items => this.props.updateNode({ onReceive: items })}
         />
         <TransitionSection
           items={node['next']}
