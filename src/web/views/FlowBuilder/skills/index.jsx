b6015c9927b313397e7f11f99b92c0f471750849
Sylvain Perron <slvn.perron+github@gmail.com>
2017-12-10 16:15:37 -0500

    Injecting module's UI in skill builder

--- src/web/views/FlowBuilder/skills/index.jsx
+++ src/web/views/FlowBuilder/skills/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Modal, Button, Alert } from 'react-bootstrap'
 import classnames from 'classnames'
-
 import find from 'lodash/find'
 
+import InjectedModuleView from '~/components/PluginInjectionSite/module'
+
 export default class SkillsBuilder extends React.Component {
+  renderModuleNotFound() {
+    return 'Error'
+  }
+
   render() {
     const show = this.props.opened
     const onCancel = () => this.props.cancelNewSkill()
@@ -25,7 +30,9 @@ export default class SkillsBuilder extends React.Component {
         <Modal.Header closeButton>
           <Modal.Title>Insert a new skill | {skill && skill.name}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Hello, there we should inject the component's view</Modal.Body>
+        <Modal.Body>
+          <InjectedModuleView moduleName={skill && skill.id} onNotFound={this.renderModuleNotFound} />
+        </Modal.Body>
         <Modal.Footer>
           <Button onClick={onCancel}>Cancel</Button>
           <Button onClick={onSubmit} bsStyle="primary">
