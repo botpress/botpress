8c0b5a5d629e1e86309a24fc4fa05a3edb3c0072
Eugene Mirotin <emirotin@gmail.com>
2017-12-13 01:30:07 +0300

    cli login / logout

---
+++ src/cli/auth.js
@@ -0,0 +1,127 @@
+import fs from 'fs'
+import path from 'path'
+import Promise from 'bluebird'
+import prompt from 'prompt'
+import chalk from 'chalk'
+import validUrl from 'valid-url'
+import axios from 'axios'
+import Confirm from 'prompt-confirm'
+
+import util from '../util'
+
+const AUTH_FILE = '.auth.json'
+
+// TODO: this part replicates `start.js`, refactor
+const getDataDir = () => {
+  const projectPath = path.resolve('.')
+
+  const botfile = path.join(projectPath, 'botfile.js')
+  if (!fs.existsSync(botfile)) {
+    util.print('error', `(fatal) No ${chalk.bold('botfile.js')} file found at: ` + botfile)
+    process.exit(1)
+  }
+
+  // eslint-disable-next-line no-eval
+  const bf = eval('require')(botfile)
+  return util.getDataLocation(bf.dataDir, projectPath)
+}
+
+const getAuthFile = () => path.join(getDataDir(), AUTH_FILE)
+
+const readAuth = () => {
+  const authFile = getAuthFile()
+  try {
+    const json = fs.readFileSync(authFile, 'utf-8')
+    return JSON.parse(json)
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.warn(`Error reading ${authFile}:`, err.message)
+    }
+  }
+  return {}
+}
+
+const writeAuth = auth => {
+  const authFile = getAuthFile()
+  fs.writeFileSync(authFile, JSON.stringify(auth, null, 2))
+}
+
+const AUTH_DISABLED = '[AUTH DISABLED]'
+
+const doLogin = async botUrl => {
+  const res = await axios.get(`${botUrl}/api/auth/enabled`)
+  if (res.data === false) {
+    return AUTH_DISABLED
+  }
+
+  const schema = {
+    properties: {
+      user: {
+        description: chalk.white('User:'),
+        required: true
+      },
+      password: {
+        description: chalk.white('Password:'),
+        hidden: true,
+        required: true
+      }
+    }
+  }
+
+  prompt.message = ''
+  prompt.delimiter = ''
+  prompt.start()
+
+  const { user, password } = await Promise.fromCallback(cb => prompt.get(schema, cb))
+  const result = await axios.post(`${botUrl}/api/login`, { user, password })
+
+  if (result.data.success) {
+    return result.data.token
+  }
+  throw new Error(result.data.reason)
+}
+
+exports.login = async botUrl => {
+  botUrl = botUrl.replace(/\/+$/, '')
+
+  if (!validUrl.isUri(botUrl)) {
+    console.error(`Doesn't look like valid URL: ${botUrl}`)
+    return
+  }
+
+  try {
+    const token = await doLogin(botUrl)
+    const auth = readAuth()
+    auth[botUrl] = token
+    writeAuth(auth)
+    console.log(`Logged in successfully. Auth token saved in ${AUTH_FILE}`)
+  } catch (err) {
+    console.error('Error:', err.message || 'Unknown')
+  }
+}
+
+exports.logout = botUrl => {
+  if (!botUrl) {
+    new Confirm("You're about to delete all saved auth tokens in the current folder. Are you sure?")
+      .run()
+      .then(answer => {
+        if (!answer) {
+          return
+        }
+        writeAuth({})
+      })
+
+    return
+  }
+
+  botUrl = botUrl.replace(/\/+$/, '')
+  const auth = readAuth()
+  if (!auth[botUrl]) {
+    console.warn(`No saved token for ${botUrl}, nothing to do.`)
+    return
+  }
+
+  delete auth[botUrl]
+  writeAuth(auth)
+  console.log('Logged out successfully.')
+}
--- src/server/anonymous.js
+++ src/server/anonymous.js
@@ -1,6 +1,11 @@
 import ExtraApiProviders from '+/api'
 
 module.exports = (bp, app) => {
+  app.get('/api/auth/enabled', async (req, res) => {
+    bp.stats.track('api', 'auth', 'enabled')
+    res.json(!!bp.botfile.login.enabled)
+  })
+
   app.post('/api/login', async (req, res) => {
     bp.stats.track('api', 'auth', 'login')
     const result = await bp.security.login(req.body.user, req.body.password, req.ip)
