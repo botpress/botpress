1bca21421f5688e3cc78fd72585ebf398c7e0e6d
Josh Salwen <jnsalwen@yahoo.com>
2017-12-18 12:17:44 -0800

    update build script.js * use cross-os instead of run-script-os * use `async`/`await` instead of sync module

--- build.js
+++ build.js
@@ -1,67 +1,89 @@
-var mkdirp = require('mkdirp')
-var ncp = require('ncp').ncp
-var remove = require('remove')
-var Sync = require('sync')
-var webpackJs = require('./webpack.js')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const ncp = require('ncp').ncp
+const rimraf = require('rimraf')
+const webpackJs = require('./webpack.js')
 
 var EMAIL_TPL = './extensions/enterprise/pro/emails/templates'
 var BOTPRESS_EDITION = process.env.BOTPRESS_EDITION
 
-function cleanOldBuild() {
-  console.log('Cleaning old build')
-  try {
-    remove.removeSync('./lib')
-  } catch (err) {
-    // ignore directory missing error
-    if (err.errno !== -4058) {
-      // rethrow the error
-      throw err
-    }
-  }
-}
-
-function copyEmailTemplates() {
-  if (BOTPRESS_EDITION === 'pro' || BOTPRESS_EDITION === 'ultimate') {
-    console.log('Copying email templates')
-    // call: mkdir -p lib/emails/templates
-    mkdirp.sync('./lib/emails/templates')
-    // call: cp -a
-    Sync(
-      function() {
-        ncp.sync(null, EMAIL_TPL, './lib/emails/templates')
-      },
-      function(err) {
-        if (err) {
-          console.error(err)
-        }
-      }
-    )
-  }
-}
-
-function bundleApp(cb) {
-  console.log('Bundling app...')
-  //node webpack.js --compile
-  webpackJs.run(copyTemplates)
-}
-
-function copyTemplates() {
-  console.log('Copying templates')
-  // call: mkdir -p lib/cli/templates
-  mkdirp.sync('./lib/cli/templates')
-  // call: cp -a
-  Sync(
-    function() {
-      ncp.sync(null, './src/cli/templates', './lib/cli/templates')
-    },
-    function(err) {
+const cleanOldBuild = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Cleaning old build')
+    let dataPath = path.normalize(__dirname + '/lib/')
+    // call: rm -rf lib/
+    rimraf(dataPath, err => {
       if (err) {
-        console.error(err)
+        reject(err)
+        return
       }
-    }
-  )
+      resolve()
+    })
+  })
 }
 
-cleanOldBuild()
-copyEmailTemplates()
-bundleApp()
+const copyEmailTemplates = () => {
+  return new Promise((resolve, reject) => {
+    if (BOTPRESS_EDITION === 'pro' || BOTPRESS_EDITION === 'ultimate') {
+      console.log('Copying email templates')
+      // call: mkdir -p lib/emails/templates
+      mkdirp('./lib/emails/templates', err => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        // call: cp -a
+        ncp(EMAIL_TPL, './lib/emails/templates', err => {
+          if (err) {
+            reject(err)
+            return
+          }
+          resolve()
+        })
+      })
+    } else {
+      resolve()
+    }
+  })
+}
+
+const bundleApp = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Bundling app...')
+    //node webpack.js --compile
+    webpackJs.run(err => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      console.log('Copying templates')
+      // call: mkdir -p lib/cli/templates
+      mkdirp('./lib/cli/templates', err => {
+        if (err) {
+          reject(err)
+          return
+        } else {
+          // call: cp -a
+          ncp('./src/cli/templates', './lib/cli/templates', err => {
+            if (err) {
+              reject(err)
+              return
+            }
+            resolve()
+          })
+        }
+      })
+    })
+  })
+}
+
+const execute = async () => {
+  await cleanOldBuild()
+  await copyEmailTemplates()
+  await bundleApp()
+  process.exit(0)
+}
+
+execute()
