2b69613a5c3f4fb9b40cbffb94a48c7fce0fb97e
Eugene Mirotin <emirotin@gmail.com>
2018-01-22 23:43:03 +0300

    extra sample bot data

---
+++ dev-bot/forms/abstract.form.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+
+module.exports = {
+  id: 'abstract',
+  title: 'Some Content',
+  ummBloc: '#some-content',
+
+  jsonSchema: {
+    title: 'Bag of Strings',
+    description: 'Some random strings',
+    type: 'string'
+  }
+}
