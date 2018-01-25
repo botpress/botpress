fd5ed1f7eea31a9410941a58635964580662fd7d
Eugene Mirotin <emirotin@gmail.com>
2018-01-25 14:29:20 +0300

    extract selectors to a separate module

--- src/web/actions/index.js
+++ src/web/actions/index.js
@@ -1,7 +1,7 @@
 import { createAction } from 'redux-actions'
 import axios from 'axios'
 import _ from 'lodash'
-import { getCurrentFlow } from '~/reducers'
+import { getCurrentFlow } from '~/reducers/selectors'
 
 // Flows
 export const requestFlows = createAction('FLOWS/REQUEST')
