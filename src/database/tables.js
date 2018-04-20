7b4eb0dd755d1a4653cf347db441b5bc27e4259b
Sylvain Perron <slvn.perron+github@gmail.com>
2018-04-20 18:18:39 -0400

    chore(refactoring): removed 'database' extension

--- src/database/tables.js
+++ src/database/tables.js
@@ -1,9 +1,7 @@
-import { table_factories } from '+/database'
-
 module.exports = [
   require('./users.js'),
   require('./tags.js'),
   require('./notifications.js'),
   require('./sessions.js'),
   require('./ghost.js')
-].concat(table_factories)
+]
