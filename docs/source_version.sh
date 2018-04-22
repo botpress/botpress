62c4ca04e2736500126ab7946adcbe00d7ae8d41
Sylvain Perron <slvn.perron+github@gmail.com>
2018-04-22 12:10:48 -0400

    fix: fixed source_version.sh

--- docs/source_version.sh
+++ docs/source_version.sh
@@ -3,5 +3,5 @@ echo $(cat ../package.json \
 | head -1 \
 | awk -F: '{ print $2 }' \
 | sed 's/[",]//g' \
-| tr -d '[[:space:]]')
-| sed -E "s/^(.+)\.(.+)\.(.+)$/\1.\2/" # takes a complex version (1.2.3) and turns it into (1.2)
+| tr -d '[[:space:]]' \
+| sed -E "s/^(.+)\.(.+)\.(.+)$/\1.\2/") # takes a complex version (1.2.3) and turns it into (1.2)
