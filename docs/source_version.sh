2df5d36d23969869cff55669701f02c8e8141a91
Sylvain Perron <slvn.perron+github@gmail.com>
2018-04-21 22:23:42 -0400

    chore(docs): build script with automatic version detection

--- docs/source_version.sh
+++ docs/source_version.sh
@@ -1,8 +1,6 @@
-PACKAGE_VERSION=$(cat ../package.json \
+echo $(cat ../package.json \
 | grep version \
 | head -1 \
 | awk -F: '{ print $2 }' \
 | sed 's/[",]//g' \
 | tr -d '[[:space:]]')
-
-echo "baseurl: \"$PACKAGE_VERSION\"" > _config.prod.yml
