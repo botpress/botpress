e299cf53092c6bbe4c553affe397a57c9520cf73
Eugene Mirotin <emirotin@gmail.com>
2018-05-14 11:06:57 +0500

    feat(flows): hide the Skills list from the sidebar

--- packages/core/botpress/src/web/views/FlowBuilder/sidePanel/index.jsx
+++ packages/core/botpress/src/web/views/FlowBuilder/sidePanel/index.jsx
@@ -1,44 +1,28 @@
 import React, { Component } from 'react'
 
-import { Tabs, Tab } from 'react-bootstrap'
-import _ from 'lodash'
+import reject from 'lodash/reject'
 
 import FlowsList from './flows/list'
 
 const style = require('./style.scss')
 
 export default class SidePanel extends Component {
-  state = {}
-
   goToFlow = flow => this.props.history.push(`/flows/${flow.replace(/\.flow\.json/, '')}`)
 
   render() {
-    const [skills, nonSkills] = _.partition(this.props.flows, x => x.name.startsWith('skills/'))
+    const normalFlows = reject(this.props.flows, x => x.name.startsWith('skills/'))
 
     return (
-      <Tabs animation={false} className={style.panel} id="flow-panel-tabs-top">
-        <Tab eventKey={1} title="Flows">
-          <FlowsList
-            flows={nonSkills}
-            dirtyFlows={this.props.dirtyFlows}
-            goToFlow={this.goToFlow}
-            deleteFlow={this.props.deleteFlow}
-            duplicateFlow={this.props.duplicateFlow}
-            currentFlow={this.props.currentFlow}
-          />
-        </Tab>
-        <Tab eventKey={2} title="Skills">
-          <FlowsList
-            stripPrefix="skills/"
-            flows={skills}
-            dirtyFlows={this.props.dirtyFlows}
-            goToFlow={this.goToFlow}
-            deleteFlow={this.props.deleteFlow}
-            duplicateFlow={this.props.duplicateFlow}
-            currentFlow={this.props.currentFlow}
-          />
-        </Tab>
-      </Tabs>
+      <div className={style.panel}>
+        <FlowsList
+          flows={normalFlows}
+          dirtyFlows={this.props.dirtyFlows}
+          goToFlow={this.goToFlow}
+          deleteFlow={this.props.deleteFlow}
+          duplicateFlow={this.props.duplicateFlow}
+          currentFlow={this.props.currentFlow}
+        />
+      </div>
     )
   }
 }
