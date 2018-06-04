9469877c0dec6b19f36c1978e98c6fee25298f30
Eugene Mirotin <emirotin@gmail.com>
2018-06-04 15:02:30 +0500

    feat(roles): rename resources to follow dot-separated scheme

--- packages/core/botpress/src/server/secured.js
+++ packages/core/botpress/src/server/secured.js
@@ -14,7 +14,7 @@ module.exports = (bp, app) => {
     res.send('pong')
   })
 
-  app.secure('read', 'modules/list').get('/api/modules', (req, res) => {
+  app.secure('read', 'modules.list').get('/api/modules', (req, res) => {
     const modules = _.map(bp._loadedModules, module => {
       return {
         name: util.getModuleShortname(module.name),
@@ -31,11 +31,11 @@ module.exports = (bp, app) => {
     res.send(modules)
   })
 
-  app.secure('read', 'middleware/list').get('/api/middlewares', (req, res) => {
+  app.secure('read', 'middleware.list').get('/api/middlewares', (req, res) => {
     res.send(bp.middlewares.list())
   })
 
-  app.secure('write', 'middleware/customizations').post('/api/middlewares/customizations', (req, res) => {
+  app.secure('write', 'middleware.customizations').post('/api/middlewares/customizations', (req, res) => {
     bp.stats.track('api', 'middlewares', 'customizations')
     const { middlewares } = req.body
     bp.middlewares.setCustomizations(middlewares)
@@ -43,7 +43,7 @@ module.exports = (bp, app) => {
     res.send(bp.middlewares.list())
   })
 
-  app.secure('write', 'middleware/customizations').delete('/api/middlewares/customizations', (req, res) => {
+  app.secure('write', 'middleware.customizations').delete('/api/middlewares/customizations', (req, res) => {
     bp.stats.track('api', 'middlewares', 'customizations')
     bp.middlewares.resetCustomizations()
     bp.middlewares.load()
@@ -63,31 +63,38 @@ module.exports = (bp, app) => {
     res.send(req.user)
   })
 
-  app.secure('read', 'bot/information').get('/api/bot/information', (req, res) => {
+  app.secure('read', 'bot.information').get('/api/bot/information', (req, res) => {
     res.send(bp.about.getBotInformation())
   })
 
-  app.secure('read', 'modules/list/community').get('/api/module/all', (req, res) => {
+  app.secure('read', 'modules.list.community').get('/api/module/all', (req, res) => {
     bp.modules.listAllCommunityModules().then(modules => res.send(modules))
   })
 
-  app.secure('read', 'modules/list/community').get('/api/module/hero', (req, res) => {
+  app.secure('read', 'modules.list.community').get('/api/module/hero', (req, res) => {
     bp.modules.getRandomCommunityHero().then(hero => res.send(hero))
+  })
+
+  app.secure('read', 'modules.list.community').get('/api/bot/contributor', (req, res) => {
+    res.send(bp.bot.getContributor())
   })
 
   app.get('/api/bot/production', (req, res) => {
     res.send(!util.isDeveloping)
   })
 
-  app.secure('read', 'modules/list/community').get('/api/bot/contributor', (req, res) => {
-    res.send(bp.bot.getContributor())
-  })
-
   app.get('/api/license', async (req, res) => {
     res.send(await bp.licensing.getLicensing())
   })
 
-  app.secure('write', 'bot/information/license').post('/api/license', (req, res) => {
+  app.delete('/api/guided-tour', (req, res) => {
+    fs.unlink(path.join(bp.projectLocation, '.welcome'), () => {
+      bp.isFirstRun = false
+      res.sendStatus(200)
+    })
+  })
+
+  app.secure('write', 'bot.information.license').post('/api/license', (req, res) => {
     bp.stats.track('api', 'license', 'change')
     bp.licensing
       .changeLicense(req.body.license)
@@ -101,14 +108,7 @@ module.exports = (bp, app) => {
       )
   })
 
-  app.delete('/api/guided-tour', (req, res) => {
-    fs.unlink(path.join(bp.projectLocation, '.welcome'), () => {
-      bp.isFirstRun = false
-      res.sendStatus(200)
-    })
-  })
-
-  app.secure('read', 'bot/logs').get('/api/logs', (req, res) => {
+  app.secure('read', 'bot.logs').get('/api/logs', (req, res) => {
     const options = {
       from: new Date() - 7 * 24 * 60 * 60 * 1000,
       until: new Date(),
@@ -126,11 +126,11 @@ module.exports = (bp, app) => {
     })
   })
 
-  app.secure('read', 'bot/logs').get('/api/logs/key', (req, res) => {
+  app.secure('read', 'bot.logs').get('/api/logs/key', (req, res) => {
     res.send({ secret: logsSecret })
   })
 
-  app.secure('read', 'bot/logs/archive').get('/api/logs/archive/:key', (req, res) => {
+  app.secure('read', 'bot.logs.archive').get('/api/logs/archive/:key', (req, res) => {
     bp.stats.track('api', 'logs', 'archive')
     if (req.params.key !== logsSecret) {
       return res.sendStatus(403)
@@ -142,11 +142,11 @@ module.exports = (bp, app) => {
     })
   })
 
-  app.secure('read', 'bot/content').get('/api/content/categories', async (req, res) => {
+  app.secure('read', 'bot.content').get('/api/content/categories', async (req, res) => {
     res.send(await bp.contentManager.listAvailableCategories())
   })
 
-  app.secure('read', 'bot/content').get('/api/content/items', async (req, res) => {
+  app.secure('read', 'bot.content').get('/api/content/items', async (req, res) => {
     const from = req.query.from || 0
     const count = req.query.count || 50
     let { searchTerm, categoryId, orderBy } = req.query
@@ -158,7 +158,7 @@ module.exports = (bp, app) => {
     res.send(await bp.contentManager.listCategoryItems(categoryId, { from, count, searchTerm, orderBy }))
   })
 
-  app.secure('read', 'bot/content').get('/api/content/items/count', async (req, res) => {
+  app.secure('read', 'bot.content').get('/api/content/items/count', async (req, res) => {
     let { categoryId } = req.query
     if (categoryId === 'all') {
       categoryId = null
@@ -166,15 +166,15 @@ module.exports = (bp, app) => {
     res.send({ count: await bp.contentManager.categoryItemsCount(categoryId) })
   })
 
-  app.secure('read', 'bot/content').get('/api/content/items/:id', async (req, res) => {
+  app.secure('read', 'bot.content').get('/api/content/items/:id', async (req, res) => {
     res.send(await bp.contentManager.getItem(req.params.id))
   })
 
-  app.secure('read', 'bot/content').get('/api/content/items-batched/:ids', async (req, res) => {
+  app.secure('read', 'bot.content').get('/api/content/items-batched/:ids', async (req, res) => {
     res.send(await bp.contentManager.getItems(req.params.ids))
   })
 
-  app.secure('write', 'bot/content').post('/api/content/categories/:id/items', async (req, res) => {
+  app.secure('write', 'bot.content').post('/api/content/categories/:id/items', async (req, res) => {
     res.send(
       await bp.contentManager.createOrUpdateCategoryItem({
         formData: req.body.formData,
@@ -183,7 +183,7 @@ module.exports = (bp, app) => {
     )
   })
 
-  app.secure('write', 'bot/content').post('/api/content/categories/:id/items/:itemId', async (req, res) => {
+  app.secure('write', 'bot.content').post('/api/content/categories/:id/items/:itemId', async (req, res) => {
     await bp.contentManager.createOrUpdateCategoryItem({
       itemId: req.params.itemId,
       formData: req.body.formData,
@@ -192,20 +192,20 @@ module.exports = (bp, app) => {
     res.sendStatus(200)
   })
 
-  app.secure('write', 'bot/content').post('/api/content/categories/all/bulk_delete', async (req, res) => {
+  app.secure('write', 'bot.content').post('/api/content/categories/all/bulk_delete', async (req, res) => {
     await bp.contentManager.deleteCategoryItems(req.body)
     res.sendStatus(200)
   })
 
-  app.secure('read', 'bot/ghost_content').get('/api/ghost_content/status', async (req, res) => {
+  app.secure('read', 'bot.ghost_content').get('/api/ghost_content/status', async (req, res) => {
     res.send(await bp.ghostManager.getPending())
   })
 
-  app.secure('read', 'bot/ghost_content').get('/api/ghost_content/export', async (req, res) => {
+  app.secure('read', 'bot.ghost_content').get('/api/ghost_content/export', async (req, res) => {
     res.send(await bp.ghostManager.getPendingWithContent({ stringifyBinary: true }))
   })
 
-  app.secure('write', 'bot/ghost_content').delete('/api/ghost_content/:folder', async (req, res) => {
+  app.secure('write', 'bot.ghost_content').delete('/api/ghost_content/:folder', async (req, res) => {
     res.send(await bp.ghostManager.revertAllPendingChangesForFile(req.params.folder, req.query.file))
   })
 
@@ -215,28 +215,28 @@ module.exports = (bp, app) => {
     }
   })
 
-  app.secure('write', 'bot/media').post('/api/media', mediaUploadMulter.single('file'), async (req, res) => {
+  app.secure('write', 'bot.media').post('/api/media', mediaUploadMulter.single('file'), async (req, res) => {
     const filename = await bp.mediaManager.saveFile(req.file.originalname, req.file.buffer)
     const url = `/media/${filename}`
     return res.json({ url })
   })
 
-  app.secure('read', 'bot/flows').get('/api/flows/all', async (req, res) => {
+  app.secure('read', 'bot.flows').get('/api/flows/all', async (req, res) => {
     const flows = await bp.dialogEngine.getFlows()
     res.send(flows)
   })
 
-  app.secure('read', 'bot/flows').get('/api/flows/available_actions', async (req, res) => {
+  app.secure('read', 'bot.flows').get('/api/flows/available_actions', async (req, res) => {
     const actions = bp.dialogEngine.getAvailableActions()
     res.send(actions)
   })
 
-  app.secure('write', 'bot/flows').post('/api/flows/save', async (req, res) => {
+  app.secure('write', 'bot.flows').post('/api/flows/save', async (req, res) => {
     await bp.dialogEngine.flowProvider.saveFlows(req.body)
     res.sendStatus(200)
   })
 
-  app.secure('read', 'bot/skills').post('/api/skills/:skillId/generate', async (req, res) => {
+  app.secure('read', 'bot.skills').post('/api/skills/:skillId/generate', async (req, res) => {
     res.send(await bp.skills.generateFlow(req.params.skillId, req.body))
   })
 }
