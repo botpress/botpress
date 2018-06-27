bfeb19ab8afb2e47143378475e310050bec310af
Eugene Mirotin <emirotin@gmail.com>
2018-06-27 14:58:09 +0300

    chore(perf): unmock getOrCreateUser

--- packages/channels/botpress-channel-web/src/users.js
+++ packages/channels/botpress-channel-web/src/users.js
@@ -1,12 +1,9 @@
 import sillyname from 'sillyname'
 
-module.exports = async (bp, config) => {
+module.exports = async bp => {
   const knex = await bp.db.get()
 
   async function getOrCreateUser(userId, throwIfNotFound = false) {
-    // TODO: perf!!!
-    return
-
     const realUserId = userId.startsWith('webchat:') ? userId.substr(8) : userId
 
     const user = await knex('users')
@@ -17,32 +14,29 @@ module.exports = async (bp, config) => {
       .then()
       .get(0)
 
-    if (!user) {
-      if (throwIfNotFound) {
-        throw new Error(`User ${realUserId} not found`)
-      }
-
-      await createNewUser(realUserId)
-      return getOrCreateUser(realUserId, true)
+    if (user) {
+      return user
     }
 
-    return user
+    if (throwIfNotFound) {
+      throw new Error(`User ${realUserId} not found`)
+    }
+
+    await createNewUser(realUserId)
+    return getOrCreateUser(realUserId, true)
   }
 
   function createNewUser(userId) {
     const [first_name, last_name] = sillyname().split(' ')
-    const user = {
-      first_name: first_name,
-      last_name: last_name,
+
+    return bp.db.saveUser({
+      first_name,
+      last_name,
       profile_pic: null,
       id: userId,
       platform: 'webchat'
-    }
-
-    return bp.db.saveUser(user)
+    })
   }
-
-  async function patchUserInfo(userId, fields) {}
 
   return { getOrCreateUser }
 }
