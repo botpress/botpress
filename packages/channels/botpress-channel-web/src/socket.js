7437e3c7ec8dae7125633ed215cbc7dbd8330cd6
Eugene Mirotin <emirotin@gmail.com>
2018-07-13 14:45:44 +0500

    chore(channel-web): dry the code

--- packages/channels/botpress-channel-web/src/socket.js
+++ packages/channels/botpress-channel-web/src/socket.js
@@ -3,6 +3,7 @@ import Promise from 'bluebird'
 
 import users from './users'
 import db from './db'
+import { sanitizeUserId } from './util'
 
 const outgoingTypes = ['text', 'login_prompt', 'file', 'carousel', 'custom']
 
@@ -45,7 +46,7 @@ module.exports = async (bp, config) => {
         originatesFromUserMessage: false
       }))
 
-    const socketId = user.userId.replace(/webchat:/gi, '')
+    const socketId = sanitizeUserId(user.userId)
 
     if (typing) {
       bp.events.emit('guest.webchat.typing', {
