37279bd1c8352c6ae8b2762dc974dfbcfc24d576
Sylvain Perron <slvn.perron+github@gmail.com>
2018-07-23 12:15:02 -0400

    fix: serving bundled static UI

--- modules/ui/static/components/Util/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ProgressBar } from 'react-bootstrap'
-
-const Loading = () => <ProgressBar active color="info" now={100} />
-
-export default Loading
--- modules/ui/static/util/Emoji.js
+++
@@ -1,8 +0,0 @@
-import ReactEmoji from 'react-emoji'
-
-module.exports = (text, size = 20) => {
-  return ReactEmoji.emojify(text, {
-    emojiType: 'emojione',
-    attributes: { width: size, height: size }
-  })
-}
