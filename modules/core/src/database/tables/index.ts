996fba3c50b075094e508e059fe282d18b067509
Renaud Lainé <rnd.laine+github@gmail.com>
2018-07-26 14:46:38 -0400

    Change some file names to better reflect their class name

--- modules/core/src/database/tables/index.ts
+++ modules/core/src/database/tables/index.ts
@@ -1,9 +1,9 @@
 import { ExtendedKnex, Table } from '../interfaces'
 
-import BotsTable from './bots'
-import MetadataTable from './metadata'
-import MigrationsTable from './migrations'
-import ModulesTable from './modules'
+import BotsTable from './bots-table'
+import MigrationsTable from './migrations-table'
+import ModulesTable from './modules-table'
+import MetadataTable from './server-metadata-table'
 
 const tables: (typeof Table)[] = [BotsTable, MigrationsTable, MetadataTable, ModulesTable]
 
