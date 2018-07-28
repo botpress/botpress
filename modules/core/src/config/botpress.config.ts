44fd56f09909bcbe27eb3f81ba506a28dc246247
Sylvain Perron <slvn.perron+github@gmail.com>
2018-07-28 13:56:51 -0400

    HTTP config in botpress config file

--- modules/core/src/config/botpress.config.ts
+++ modules/core/src/config/botpress.config.ts
@@ -1,5 +1,11 @@
 import { DatabaseConfig } from './database.config'
 
 export type BotpressConfig = {
+  httpServer: {
+    /** BP_HTTP_HOST */
+    host: string | null
+    port: number
+    backlog: number
+  }
   database: DatabaseConfig
 }
