b03dcc5dd0ca65303ccb9657e076ab0f2a27b3ae
Sylvain Perron <slvn.perron+github@gmail.com>
2018-07-30 16:08:20 -0400

    fixed pkg script

--- modules/core/scripts/package-binaries.sh
+++ modules/core/scripts/package-binaries.sh
@@ -1,5 +1,7 @@
 if [ -d config/ ]; then
-  rm -rf bin/config/ && cp -r config/ bin/config/
+  rm -rf bin/config/
+  mkdir -p bin/config
+  cp -r config/ bin/config/
   echo "Copied configuration files"
   echo "Packaging binaries..."
   # TODO: Add targets linux-x86,linux-x64,win-x86,win-x64
