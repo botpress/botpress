55d6b000f8ec46eeeec688a928b879358ff279a7
Sylvain Perron <slvn.perron+github@gmail.com>
2018-08-06 10:37:37 -0400

    fix bad merge

--- modules/core/src/services/cms/ghost-cms-service.ts
+++ modules/core/src/services/cms/ghost-cms-service.ts
@@ -15,7 +15,7 @@ const CONTENT_ELEMENTS_TABLE = 'content_elements'
 const LOCATION = 'content-types'
 
 @injectable()
-export class GhostCMSService implements CMSService {
+export class GhostCMSService implements CMSService, IDisposeOnExit {
   private contentTypes: ContentType[] = []
   private filesById = {}
   private sandbox: SafeCodeSandbox
@@ -114,8 +114,8 @@ export class GhostCMSService implements CMSService {
     }
   }
 
-  private async loadContentTypeFromFile(sandbox: SafeCodeSandbox, fileName: string): Promise<void> {
-    const contentType = <ContentType>await sandbox.run(fileName)
+  private async loadContentTypeFromFile(fileName: string): Promise<void> {
+    const contentType = <ContentType>await this.sandbox.run(fileName)
 
     if (!contentType || !contentType.id) {
       throw new Error('Invalid content type ' + fileName)
