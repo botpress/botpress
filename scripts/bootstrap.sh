a434d1d87a80a64463279f946e5431558a14a263
Sylvain Perron <slvn.perron+github@gmail.com>
2018-08-21 17:06:42 -0400

    added clean script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,15 +1,11 @@
-#find ./packages -name package.json | grep -v -E "node_modules|dist" | xargs -n1 dirname | xargs -n1 npm install
-
 npm install
+
 originalPath=$PWD
+
 find ./packages -name package.json | grep -v -E "node_modules|dist" | 
 while read filename
 do
-  directory=$(dirname "$filename")
-  echo $directory
-  cd $directory
+  cd $(dirname "$filename")
   npm install
-  echo $PWD
   cd $originalPath
-  echo $PWD
 done
