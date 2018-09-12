1b61c1afbffe293493d6633a308a0c6afb020ac0
Renaud Lainé <rnd.laine+github@gmail.com>
2018-09-12 16:36:25 -0400

    Add notification service

--- packages/core/src/repositories/notifications.ts
+++ packages/core/src/repositories/notifications.ts
@@ -3,29 +3,31 @@ import { inject, injectable } from 'inversify'
 import Database from '../database'
 import { TYPES } from '../misc/types'
 
-export class ModuleNotification {
-  constructor(public id: string, public icon: string, public name: string) {}
-}
-
 export class Notification {
   constructor(
+    public botId: string,
     public message: string,
     public level: string,
-    public module: ModuleNotification,
+    public moduleId: string,
+    public moduleIcon: string,
+    public moduleName: string,
     public redirectUrl: string
   ) {}
 
   public created_on?: string
+  public modified_on?: string
   public read = false
   public archived = false
 }
 
+type DefaultGetOptions = { archived?: boolean; read?: boolean }
+
 export interface NotificationsRepository {
-  getBydId(id: string)
-  insert(notification: Notification)
-  update(notification: Notification)
-  deleteById(id: string)
-  getAllUnarchived()
+  getBydId(id: string): Promise<Notification>
+  getAll(options?: DefaultGetOptions): Promise<Notification[]>
+  insert(notification: Notification): Promise<Notification>
+  update(notification: Notification): Promise<void>
+  deleteById(id: string): Promise<void>
 }
 
 @injectable()
@@ -42,13 +44,19 @@ export class KnexNotificationsRepository implements NotificationsRepository {
       .then()) as Notification
   }
 
-  async getAllUnarchived(): Promise<Notification[]> {
-    return (await this.database
-      .knex(this.TABLE_NAME)
-      .select('*')
-      .where('archived', false)
-      .limit(250)
-      .then()) as Notification[]
+  async getAll(options?: DefaultGetOptions): Promise<Notification[]> {
+    let query = this.database.knex(this.TABLE_NAME).select('*')
+    // Knex cannot chain "where" clause like "orderBy". They must be chained with "andWhere" or "orWhere"
+    if (options && options.archived && options.read) {
+      query = query.where('archived', options.archived).andWhere('read', options.read)
+    } else if (options && options.archived) {
+      query = query.where('archived', options.archived)
+    } else if (options && options.read) {
+      query = query.where('read', options.read)
+    }
+    query.limit(250)
+
+    return (await query.then()) as Notification[]
   }
 
   async insert(notification: Notification): Promise<Notification> {
---
+++ packages/core/src/services/notification/service.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'inversify'
+
+import { TYPES } from '../../misc/types'
+import { Notification, NotificationsRepository } from '../../repositories'
+
+@injectable()
+export class NotificationsService {
+  constructor(@inject(TYPES.NotificationsRepository) private notificationsRepository: NotificationsRepository) {}
+
+  async archive(notification: Notification): Promise<void> {
+    notification.archived = true
+    await this.notificationsRepository.update(notification)
+  }
+
+  async archiveAll() {
+    const notifications = await this.notificationsRepository.getAll({ archived: false })
+    await Promise.mapSeries(notifications, notification => this.archive(notification))
+  }
+
+  async create(notification: Notification): Promise<Notification> {
+    return this.notificationsRepository.insert(notification)
+  }
+
+  async getInbox(): Promise<Notification[]> {
+    return this.notificationsRepository.getAll({ archived: false, read: false })
+  }
+
+  async markAsRead(notification: Notification): Promise<void> {
+    notification.read = true
+    await this.notificationsRepository.update(notification)
+  }
+
+  async markAllAsRead(): Promise<void> {
+    const notifications = await this.notificationsRepository.getAll({ archived: false, read: false })
+    await Promise.mapSeries(notifications, u => this.markAsRead(u))
+  }
+
+  async getArchived(): Promise<Notification[]> {
+    return this.notificationsRepository.getAll({ archived: true })
+  }
+}
