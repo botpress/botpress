eb2c6da15dd9b6eddefbb165606eca5b57e6b1c2
Sylvain Perron <slvn.perron+github@gmail.com>
2018-09-25 12:20:22 -0400

    Updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,50 +3,58 @@ const ts = require('gulp-typescript')
 const rimraf = require('gulp-rimraf')
 const run = require('gulp-run')
 
-gulp.task('clean-all', () => {
+const tsProject = ts.createProject('./src/tsconfig.json')
+
+const wipe = () => {
   return gulp.src(['./node_modules', './out']).pipe(rimraf())
-})
+}
 
-gulp.task('clean', () => {
+const clean = () => {
   return gulp.src('./out').pipe(rimraf())
-})
+}
 
-gulp.task('build-ts', () => {
-  const tsProject = ts.createProject('./src/tsconfig.json')
-  return gulp
-    .src('./src/**/*.ts')
+const buildTs = () => {
+  return tsProject
+    .src()
     .pipe(tsProject())
     .pipe(gulp.dest('./out'))
-})
+}
 
-gulp.task('watch-ts', ['build-ts'], () => {
-  return gulp.watch('./src/**/*.ts', ['build-ts'])
-})
+const watch = () => {
+  return gulp.watch('./src/**/*.ts', buildTs)
+}
 
-gulp.task('build-static', ['build-ts'], () => {
+const createDirectories = () => {
+  return gulp
+    .src('*.*', { read: false })
+    .pipe(gulp.dest('./out/data'))
+    .pipe(gulp.dest('./out/data/storage'))
+}
+
+const copyStatic = () => {
   return gulp.src('./src/bp/vanilla/**/*').pipe(gulp.dest('./out/bp/data'))
-})
+}
 
-const botpressJsonSchemas =
-  './node_modules/.bin/typescript-json-schema --required "src/bp/core/config/*.ts" BotpressConfig --out "out/botpress.config.schema.json" --ignoreErrors'
-const botJsonSchemas =
-  './node_modules/.bin/typescript-json-schema --required "src/bp/core/config/*.ts" BotConfig --out "out/bot.config.schema.json" --ignoreErrors'
-gulp.task('build-schemas', ['build-ts'], () => {
+const buildSchemas = () => {
+  const botpressJsonSchemas =
+    './node_modules/.bin/typescript-json-schema --required "src/bp/core/config/*.ts" BotpressConfig --out "out/botpress.config.schema.json" --ignoreErrors'
+  const botJsonSchemas =
+    './node_modules/.bin/typescript-json-schema --required "src/bp/core/config/*.ts" BotConfig --out "out/bot.config.schema.json" --ignoreErrors'
   return gulp
     .src('.')
     .pipe(run(botpressJsonSchemas))
     .pipe(run(botJsonSchemas))
-})
+}
 
-gulp.task('test', ['build-ts'], () => {
+const runTests = () => {
   return gulp.src('.').pipe(run('./node_modules/.bin/jest --detectOpenHandles -c ./jest.config.js'))
-})
+}
 
 process.on('uncaughtException', err => {
   console.error('An error coccured in your gulpfile: ', err)
   process.exit(1)
 })
 
-gulp.task('default', ['clean', 'build-ts', 'build-schemas', 'build-static'])
-
-gulp.task('dev', ['clean', 'build-schemas', 'build-static', 'watch-ts'])
+gulp.task('test', gulp.series([buildTs, runTests]))
+gulp.task('default', gulp.series([clean, buildTs, buildSchemas, createDirectories, copyStatic]))
+gulp.task('dev', gulp.series([(clean, buildSchemas, createDirectories, copyStatic, watch)]))
