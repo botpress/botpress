9749e3c13a53cd0dc2d79a7fe0a5e5c05cc2e13a
Yann Allard <yann.allard1@gmail.com>
2018-10-08 17:39:18 -0400

    nlu and qna cleanup

--- modules/nlu/src/backend/api.ts
+++ modules/nlu/src/backend/api.ts
@@ -1,32 +1,33 @@
 import { SDK } from '.'
+import ScopedNlu from './scopednlu'
 
-export default async (bp: SDK) => {
+export default async (bp: SDK, botScopedNlu: Map<string, ScopedNlu>) => {
   const router = bp.http.createRouterForBot('nlu')
 
+  router.get('/intents', async (req, res) => {
+    res.send(await botScopedNlu.get(req.params.botId).storage.getIntents())
+  })
+
+  router.get('/intents/:intent', async (req, res) => {
+    res.send(await botScopedNlu.get(req.params.botId).storage.getIntent(req.params.intent))
+  })
+
   router.delete('/intents/:intent', async (req, res) => {
-    await bp.nlu.forBot(req.params.botId).storage.deleteIntent(req.params.intent)
+    await botScopedNlu.get(req.params.botId).storage.deleteIntent(req.params.intent)
     res.sendStatus(200)
   })
 
   router.post('/intents/:intent', async (req, res) => {
-    await bp.nlu.forBot(req.params.botId).storage.saveIntent(req.params.intent, req.body)
+    await botScopedNlu.get(req.params.botId).storage.saveIntent(req.params.intent, req.body)
     res.sendStatus(200)
   })
 
-  router.get('/intents', async (req, res) => {
-    res.send(await bp.nlu.forBot(req.params.botId).storage.getIntents())
-  })
-
-  router.get('/intents/:intent', async (req, res) => {
-    res.send(await bp.nlu.forBot(req.params.botId).storage.getIntent(req.params.intent))
-  })
-
   router.get('/entities', async (req, res) => {
-    res.send((await bp.nlu.forBot(req.params.botId).provider.getAvailableEntities()).map(x => x.name))
+    res.send((await botScopedNlu.get(req.params.botId).provider.getAvailableEntities()).map(x => x.name))
   })
 
   router.get('/sync/check', async (req, res) => {
-    res.send(await bp.nlu.forBot(req.params.botId).provider.checkSyncNeeded())
+    res.send(await botScopedNlu.get(req.params.botId).provider.checkSyncNeeded())
   })
 
   router.get('/sync', async (req, res) => {
@@ -35,7 +36,7 @@ export default async (bp: SDK) => {
         bp.RealTimePayload.forAdmins('toast.nlu-sync', { text: 'NLU Sync In Progress', type: 'info', time: 120000 })
       )
 
-      await bp.nlu.forBot(req.params.botId).provider.sync()
+      await botScopedNlu.get(req.params.botId).provider.sync()
       bp.realtime.sendPayload(
         bp.RealTimePayload.forAdmins('toast.nlu-sync', { text: 'NLU Sync Success', type: 'success' })
       )
