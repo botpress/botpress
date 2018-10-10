543500e68ec724f271d00e5ac4dbbf475e691196
Sylvain Perron <slvn.perron+github@gmail.com>
2018-10-10 09:41:08 -0400

    buildspec

--- build/codebuild-extras.sh
+++ build/codebuild-extras.sh
@@ -4,6 +4,8 @@ export CI=true
 export CODEBUILD=true
 
 export CODEBUILD_GIT_BRANCH=`git symbolic-ref HEAD --short 2>/dev/null`
+echo "BRANCH $CODEBUILD_GIT_BRANCH"
+
 if [ "$CODEBUILD_GIT_BRANCH" == "" ] ; then
   CODEBUILD_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
   export CODEBUILD_GIT_BRANCH=${CODEBUILD_GIT_BRANCH#remotes/origin/}
