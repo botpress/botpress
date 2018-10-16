101ab0ee815fd9a32b8f4c52e4f20bfd3a5f19fa
Sylvain Perron <slvn.perron+github@gmail.com>
2018-10-16 16:23:27 -0400

    feat(xx): ghost content export

--- packages/core/botpress/src/web/views/GhostContent/util.js
+++ packages/core/botpress/src/web/views/GhostContent/util.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
-
+import get from 'lodash/get'
 import groupBy from 'lodash/groupBy'
 import mapValues from 'lodash/mapValues'
+
+import { downloadBlob } from '~/util'
 
 const transformData = data => mapValues(data, entries => groupBy(entries, 'file'))
 
@@ -17,3 +19,18 @@ export const getHost = () => {
 
 export const revertPendingFileChanges = (folder, file) =>
   axios.delete(`/api/ghost_content/${folder}`, { params: { file } }).then()
+
+export const revertPendingFileChangesXX = data => {
+  const reqData = { filePath: data.path, revision: data.revision }
+  return axios.post('/api/versioning/revert', reqData).then()
+}
+
+export const exportArchiveXX = async () => {
+  const res = await axios.get('/api/versioning/export', { responseType: 'blob' })
+  let name = get(res, 'headers.content-disposition', 'archive.tgz')
+  if (name.includes('filename=')) {
+    name = name.substr(name.indexOf('filename=') + 'filename='.length)
+  }
+
+  downloadBlob(name, res.data)
+}
