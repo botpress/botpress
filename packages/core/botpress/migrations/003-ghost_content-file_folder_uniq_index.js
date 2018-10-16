64ec3a37b5b2517725343364fccbcfe7357163bb
Oleksiy Pletnov <k4merton@gmail.com>
2018-10-16 15:04:48 +0300

    fix(core): remove ghost file uniq index migration (resolve #1017)

--- packages/core/botpress/migrations/003-ghost_content-file_folder_uniq_index.js
+++
@@ -1,9 +0,0 @@
-exports.up = knex =>
-  knex.schema.alterTable('ghost_content', t => {
-    t.unique(['folder', 'file'])
-  })
-
-exports.down = knex =>
-  knex.schema.alterTable('ghost_content', t => {
-    t.dropUnique(['folder', 'file'])
-  })
