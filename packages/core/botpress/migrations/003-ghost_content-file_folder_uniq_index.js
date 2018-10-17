aef783bf26c03a79c85917d52707d3504f4eb962
Alex Pletnov <k4merton@gmail.com>
2018-10-18 01:26:56 +0300

    fix(core): fix ghost-content uniq index migration for sqlite (#1033)

--- packages/core/botpress/migrations/003-ghost_content-file_folder_uniq_index.js
+++ packages/core/botpress/migrations/003-ghost_content-file_folder_uniq_index.js
@@ -1,7 +1,14 @@
-exports.up = knex =>
+exports.up = knex => {
+  if (knex.client.config.client === 'sqlite3') {
+    return
+  }
   knex.raw(`
     ALTER TABLE ghost_content DROP CONSTRAINT IF EXISTS ghost_content_folder_file_unique;
     ALTER TABLE ghost_content ADD CONSTRAINT ghost_content_folder_file_unique UNIQUE (folder, file);
   `)
+}
 
-exports.down = knex => knex.raw('ALTER TABLE ghost_content DROP CONSTRAINT IF EXISTS ghost_content_folder_file_unique;')
+exports.down = knex =>
+  knex.schema.alterTable('ghost_content', t => {
+    t.dropUnique(['folder', 'file'])
+  })
