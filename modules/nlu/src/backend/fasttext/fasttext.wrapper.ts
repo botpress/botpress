ac1090fa362a7004d79aa91cd111be39fdf26253
Sylvain Perron <slvn.perron+github@gmail.com>
2018-10-18 12:54:37 -0400

    fixed NLU

--- modules/nlu/src/backend/fasttext/fasttext.wrapper.ts
+++ modules/nlu/src/backend/fasttext/fasttext.wrapper.ts
@@ -1,4 +1,4 @@
-import { execFileSync } from 'child_process'
+import { execFile, execFileSync } from 'child_process'
 import { join } from 'path'
 
 let bin = 'ft_lnx'
@@ -12,7 +12,14 @@ const BINPATH = join(__dirname, bin)
 
 export default class FTWrapper {
   binpath = BINPATH
-  static supervised(trainingSetPath: string, modelOutPath: string, learningRate: number = 0.7, epoch: number = 1) {
+  static supervised(
+    trainingSetPath: string,
+    modelOutPath: string,
+    learningRate: number = 1,
+    epoch: number = 500,
+    bucket: number = 10000,
+    dim: number = 10
+  ) {
     execFileSync(
       BINPATH,
       [
@@ -24,13 +31,35 @@ export default class FTWrapper {
         '-lr',
         learningRate.toString(),
         '-epoch',
-        epoch.toString()
+        epoch.toString(),
+        '-loss',
+        'hs',
+        '-dim',
+        dim.toString(),
+        '-bucket',
+        bucket.toString()
       ],
       { stdio: 'ignore' }
     )
   }
 
-  static predictProb(modelPath: string, inputFilePath: string, numClass = 1): string {
-    return execFileSync(BINPATH, ['predict-prob', modelPath, inputFilePath, numClass.toString()]).toString()
+  static async predictProb(modelPath: string, inputText: string, numClass = 5): Promise<string> {
+    const args = ['predict-prob', modelPath, '-', numClass.toString()]
+    const result = execFile(BINPATH, args, {
+      encoding: 'utf8',
+      stdio: ['pipe', 'ignore', 'ignore']
+    })
+
+    let out: string = ''
+    result.stdin.end(inputText)
+    result.stdout.on('data', chunk => {
+      out += chunk
+    })
+
+    return Promise.fromCallback<string>(cb => {
+      result.stdout.on('close', () => {
+        cb(undefined, out)
+      })
+    })
   }
 }
