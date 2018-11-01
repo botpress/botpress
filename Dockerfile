32e85ac7e93a57fb47df79a432900352a92b0702
Yann Allard <yann.allard1@gmail.com>
2018-10-31 22:00:36 -0400

    updating codebuild for docs

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM ubuntu:18.04
 ADD . /botpress
-WORKDIR /botpress
-RUN chmod +x bp
 WORKDIR /botpress/modules
 RUN for file in `ls -1 *.tgz`; do mkdir ${file%.*} |tar -zxf $file -C ${file%.*}; done
 RUN rm -rf *.tgz
 WORKDIR /botpress
+RUN chmod +x bp
 EXPOSE 3001
 CMD ["./bp"]
--- build/source_version.sh
+++ build/source_version.sh
@@ -3,6 +3,5 @@ echo $(cat package.json \
 | head -1 \
 | awk -F: '{ print $2 }' \
 | sed 's/[",]//g' \
-| sed 's/.,_//g' \
-| sed 's/\./_/g' \
-| tr -d '[[:space:]]')
+| tr -d '[[:space:]]' \
+| sed -E "s/^(.+)\.(.+)\.(.+)$/\1.\2/") # takes a complex version (1.2.3) and turns it into (1.2)
