bab0ffc3a08f72d1f19b3421f55fb8cf8428bd95
Renaud Lainé <rnd.laine+github@gmail.com>
2018-11-19 09:34:02 -0500

    add mac address to machineId

--- src/bp/common/stats.ts
+++ src/bp/common/stats.ts
@@ -6,8 +6,10 @@ export const gaId = 'UA-90044826-3'
 
 export const machineUUID = async () => {
   return machineId().catch(() => {
+    // In case MachineId fails
     const hash = crypto.createHash('sha256')
-    hash.update(os.arch() + os.hostname() + os.platform() + os.type())
+    const network = os.networkInterfaces()
+    hash.update(os.arch() + os.hostname() + os.platform() + os.type() + network['mac'])
     return hash.digest('hex')
   })
 }
