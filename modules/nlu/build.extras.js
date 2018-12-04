83eb9e859546f6218d8a8bb74d13f716fb7640c0
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2018-12-04 16:45:52 -0500

    included word embbedding clutering in crf slot tagger

--- modules/nlu/build.extras.js
+++ modules/nlu/build.extras.js
@@ -1,3 +1,3 @@
 module.exports = {
-  copyFiles: ['src/backend/tools/bin/ft_*', 'src/backend/fasttext/*.dll', 'src/backend/tools/pretrained/*']
+  copyFiles: ['src/backend/tools/bin/ft_*', 'src/backend/fasttext/*.dll', 'src/backend/tools/pretrained/*', 'src/backend/tools/bin/crfsuite_*']
 }
--- modules/nlu/src/backend/pipelines/entities/crf_extractor.ts
+++ modules/nlu/src/backend/pipelines/entities/crf_extractor.ts
@@ -1,27 +1,37 @@
+import { resolve } from 'bluebird'
 import fs from 'fs'
 import _ from 'lodash'
+import kmeans from 'ml-kmeans'
 import os from 'os'
 import tmp from 'tmp'
 
 import { Tagger, Trainer } from '../../tools/crfsuite'
 import FastText from '../../tools/fastText'
+import { SlotExtractor } from '../../typings'
 
 import { Token, tokenize } from './tagger'
 
-const FT_DIMENTIONS = 10
+const FT_DIMENTIONS = 15
+const K = 15
+const CRF_PARAMAS = {
+  c1: '0.0001',
+  c2: '0.01',
+  max_iterations: '500',
+  'feature.possible_transitions': '1',
+  'feature.possible_states': '1',
+}
 
-type Features = {
-  b_embeddings: number[] // TODO GMM class instead .. [FT_DIMENTIONS]-dimentional fastText vector
+type Features = Partial<{
+  b_cluster: number
   b_upper: boolean
   b_title: boolean
   b_low: string
 
-  a_embeddings: number[] // [FT_DIMENTIONS]-dimentional fastText vector
+  a_cluster: number
   a_upper: boolean
   a_low: string
   a_title: boolean
 
-  w_embeddings: number[]
   w_upper: boolean
   w_low: string
   w_title: boolean
@@ -29,36 +39,49 @@ type Features = {
   w_bias: number
   w_eos: boolean
   w_bos: boolean
-}
+}>
 
-export default class CRFExtractor {
+export default class CRFExtractor implements SlotExtractor {
   private _isTrained: boolean = false
   private _ftModelFn = ''
   private _crfModelFn = ''
   private _ft!: FastText
   private _tagger!: typeof Tagger
+  private _kmeansModel
 
-  static samplesFromFile(filePath: string): Token[][] {
-    const text = fs.readFileSync(filePath, 'utf8')
-    const lines = text
-      .split(os.EOL)
-      .map(x => x.trim())
-      .filter(x => x.length)
-    return lines.map(l => tokenize(l))
-  }
-
-  async train(samples: Token[][]) {
+  async train(inputs: string[]) {
     this._isTrained = false
+    const samples = inputs.map(s => tokenize(s))
     await this._trainFastText(samples)
+    await this._trainKmeans(samples)
     await this._trainCrf(samples)
 
-    // Create the tagger
     this._tagger = Tagger()
     await this._tagger.open(this._crfModelFn)
     this._isTrained = true
   }
 
-  async predict(sample: Token[]): Promise<string[]> {
+  async extract(text: string): Promise<any> {
+    const tokens = tokenize(text, false)
+    const tags = await this.tag(tokens)
+
+    return _.zip(tokens, tags)
+      .filter(tokTag => tokTag[1] != 'o')
+      .reduce((slots: any, [token, tag]) => {
+        if (token && tag) {
+          const slotName = tag.slice(2)
+          // we could remove the I check if the extractor is not accurate enough
+          if (tag[0] === 'I' && slots[slotName]) {
+            slots[slotName] + ` ${token.value}`
+          } else {
+            slots[slotName] = token.value
+          }
+          return slots
+        }
+      }, {})
+  }
+
+  async tag(sample: Token[]): Promise<string[]> {
     if (!this._isTrained) {
       throw new Error('Model not trained, please call train() before')
     }
@@ -67,25 +90,33 @@ export default class CRFExtractor {
       const x = await this._word2feat(sample, i)
       seq.push(this._feat2vec(x))
     }
+    // Here, it would be awesome if we could get some probabilities for each tag
+    // CRFSuite offers that option by passing the -i, --marginal option
+    // From the docs: Output the marginal probabilities of labels. When this function is enabled, each predicted label is followed by ":x.xxxx", where "x.xxxx" presents the probability of the label. This function is disabled by default.
+
     return this._tagger.tag(seq)
+  }
+
+  private async _trainKmeans(samples: Token[][]): Promise<any> {
+    const data = await Promise.mapSeries(_.flatten(samples), async t => await this._ft.wordVectors(t.value))
+    try {
+      this._kmeansModel = kmeans(data, K)
+      return resolve()
+    } catch (error) {
+      console.error('error tra ining Kmeans', error)
+      throw error
+    }
   }
 
   private async _trainCrf(samples: Token[][]) {
     this._crfModelFn = tmp.fileSync({ postfix: '.bin' }).name
     const trainer = Trainer()
-    trainer.set_params({
-      c1: '0.0001',
-      c2: '0.01',
-      max_iterations: '500',
-      'feature.possible_transitions': '1',
-      'feature.possible_states': '1'
-    })
+    trainer.set_params(CRF_PARAMAS)
 
     for (const sample of samples) {
       const entries: string[][] = []
       const labels: string[] = []
       for (let i = 0; i < sample.length; i++) {
-        // TODO Add sliding window here
         const x = await this._word2feat(sample, i)
         labels.push(sample[i].type)
         entries.push(this._feat2vec(x))
@@ -93,55 +124,34 @@ export default class CRFExtractor {
       trainer.append(entries, labels)
     }
 
-    await trainer.train(this._crfModelFn)
+    trainer.train(this._crfModelFn, '-q')
   }
 
   private async _trainFastText(samples: Token[][]) {
     this._ftModelFn = tmp.fileSync({ postfix: '.bin' }).name
-
     const ftTrainFn = tmp.fileSync({ postfix: '.txt' }).name
     this._ft = new FastText(this._ftModelFn)
 
-    const lines: string[] = []
-    for (const sample of samples) {
-      const line = sample.map(x => (x.type === 'o' ? x.value : x.type)).join(' ')
-      lines.push(line)
-    }
-    const ftTrainContent = lines.join('\n')
+    const trainContent = samples.reduce((current, next) => {
+      return `${current}${next.map(s => s.type == 'o' ? s.value : s.type.slice(2, s.type.length)).join(' ')}${os.EOL}`
+    }, '')
 
-    fs.writeFileSync(ftTrainFn, ftTrainContent, 'utf8')
+    fs.writeFileSync(ftTrainFn, trainContent, 'utf8')
     this._ft.train(ftTrainFn, {
       method: 'skipgram',
       minCount: 2,
       bucket: 25000,
-      dim: 3,
-      learningRate: 0.1,
+      dim: FT_DIMENTIONS,
+      learningRate: 0.5,
       wordGram: 3,
       maxn: 6,
       minn: 2,
-      epoch: 500
+      epoch: 50
     })
   }
 
   private async _word2feat(tokens: Token[], idx: number): Promise<Features> {
-    const feat: Features = {
-      a_embeddings: new Array(FT_DIMENTIONS).fill(0),
-      a_low: '',
-      a_title: false,
-      a_upper: false,
-      b_embeddings: new Array(FT_DIMENTIONS).fill(0),
-      b_low: '',
-      b_title: false,
-      b_upper: false,
-      w_bias: 1.0,
-      w_bos: false,
-      w_embeddings: new Array(FT_DIMENTIONS).fill(0),
-      w_eos: false,
-      w_digit: false,
-      w_low: '',
-      w_title: false,
-      w_upper: false
-    }
+    const feat: Features = {}
 
     if (idx === 0) {
       feat.w_bos = true
@@ -150,7 +160,7 @@ export default class CRFExtractor {
       feat.b_low = t.toLowerCase()
       feat.b_upper = t === t.toUpperCase()
       feat.b_title = t.length >= 2 && t[0].toUpperCase() === t[0] && t[1].toLowerCase() === t[1]
-      feat.b_embeddings = await this._ft.wordVectors(t)
+      feat.b_cluster = await this._getWordCluster(t)
     }
 
     if (idx === tokens.length - 1) {
@@ -160,17 +170,21 @@ export default class CRFExtractor {
       feat.a_low = t.toLowerCase()
       feat.a_upper = t === t.toUpperCase()
       feat.a_title = t.length >= 2 && t[0].toUpperCase() === t[0] && t[1].toLowerCase() === t[1]
-      feat.a_embeddings = await this._ft.wordVectors(t)
+      feat.a_cluster = await this._getWordCluster(t)
     }
 
     const t = tokens[idx].value
     feat.w_digit = /^\d+$/i.test(t)
     feat.w_low = t.toLowerCase()
-    feat.w_embeddings = await this._ft.wordVectors(t)
     feat.w_upper = t === t.toUpperCase()
     feat.w_title = t.length >= 2 && t[0].toUpperCase() === t[0] && t[1].toLowerCase() === t[1]
 
     return feat
+  }
+
+  private async _getWordCluster(word: string): Promise<number> {
+    const vector = await this._ft.wordVectors(word)
+    return this._kmeansModel.nearest([vector])[0]
   }
 
   private _feat2vec(f: Features): string[] {
@@ -182,7 +196,7 @@ export default class CRFExtractor {
       ret.push('w[-1]=' + f.b_low)
       f.b_title && ret.push('w[-1]title')
       f.b_upper && ret.push('w[-1]upper')
-      // TODO Add word embedding cluster here
+      ret.push(`w[-1]cluster=${f.b_cluster!.toString()}`)
     }
 
     if (f.w_eos) {
@@ -191,14 +205,13 @@ export default class CRFExtractor {
       ret.push('w[1]=' + f.a_low)
       f.a_title && ret.push('w[1]title')
       f.a_upper && ret.push('w[1]upper')
-      // TODO Add word embedding cluster here
+      ret.push(`w[1]cluster=${f.a_cluster!.toString()}`)
     }
 
     ret.push('w[0]biais=' + f.w_bias)
     f.w_digit && ret.push('w[0]digit')
     f.w_title && ret.push('w[0]title')
     f.w_upper && ret.push('w[0]upper')
-    // TODO Add word embedding cluster here
 
     return ret
   }
