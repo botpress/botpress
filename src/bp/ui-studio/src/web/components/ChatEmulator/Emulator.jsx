655550ea4ac1fc1261193e3dfd53c142623066a7
Sylvain Perron <slvn.perron+github@gmail.com>
2018-12-16 18:58:29 -0500

    added message inspection

--- src/bp/ui-studio/src/web/components/ChatEmulator/Emulator.jsx
+++ src/bp/ui-studio/src/web/components/ChatEmulator/Emulator.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 import Promise from 'bluebird'
+import SplitPane from 'react-split-pane'
+import JSONTree from 'react-json-tree'
 
 import { Glyphicon } from 'react-bootstrap'
 import classnames from 'classnames'
 
+import inspectorTheme from './inspectorTheme'
 import Message from './Message'
 
 import style from './Emulator.styl'
@@ -13,6 +16,7 @@ export default class EmulatorChat extends React.Component {
   constructor(props) {
     super(props)
     this.textInputRef = React.createRef()
+    this.endOfMessagesRef = React.createRef()
   }
 
   state = {
@@ -51,6 +55,8 @@ export default class EmulatorChat extends React.Component {
       sending: false,
       selectedIndex: this.state.messages.length
     })
+
+    this.endOfMessagesRef.current.scrollIntoView(false)
   }
 
   handleKeyPress = e => {
@@ -63,24 +69,53 @@ export default class EmulatorChat extends React.Component {
 
   handleMsgChange = e => !this.state.sending && this.setState({ textInputValue: e.target.value })
 
+  get inspectorData() {
+    return this.state.selectedIndex >= 0 ? this.state.messages[this.state.selectedIndex].result : {}
+  }
+
+  inspectorShouldExpand(key, data, level) {
+    return level <= 1
+  }
+
   render() {
     return (
       <div className={style.container}>
-        <div className={style.history}>
-          {this.state.messages.map((msg, idx) => (
-            <Message
-              tabIndex={this.state.messages.length - idx + 1}
-              key={`msg-idx`}
-              onFocus={() => this.setState({ selectedIndex: idx })}
-              selected={this.state.selectedIndex === idx}
-              message={msg}
-            />
-          ))}
-
-          {/* This is used to loop using Tab, we're going back to text input */}
-          <div tabIndex={this.state.messages.length + 1} onFocus={() => this.textInputRef.current.focus()} />
+        <div className={style.panes}>
+          <SplitPane
+            split="horizontal"
+            minSize={50}
+            defaultSize={'75%'}
+            pane2Style={{ 'overflow-y': 'auto', backgroundColor: '#272822' }}
+          >
+            <div className={style.history}>
+              {this.state.messages.map((msg, idx) => (
+                <Message
+                  tabIndex={this.state.messages.length - idx + 1}
+                  key={`msg-idx`}
+                  onFocus={() => this.setState({ selectedIndex: idx })}
+                  selected={this.state.selectedIndex === idx}
+                  message={msg}
+                />
+              ))}
+              {/* This is used to loop using Tab, we're going back to text input */}
+              {/* Also used to scroll to the end of the messages */}
+              <div
+                ref={this.endOfMessagesRef}
+                tabIndex={this.state.messages.length + 1}
+                onFocus={() => this.textInputRef.current.focus()}
+              />
+            </div>
+            <div className={style.inspector}>
+              <JSONTree
+                data={this.inspectorData}
+                theme={inspectorTheme}
+                invertTheme={false}
+                hideRoot={true}
+                shouldExpandNode={this.inspectorShouldExpand}
+              />
+            </div>
+          </SplitPane>
         </div>
-        <div className={style.inspector}>Inspector here</div>
         <textarea
           tabIndex={1}
           ref={this.textInputRef}
