728bd8162cc1ed08bb79c516f72d0cec35c5dcbb
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2018-12-18 11:43:22 -0500

    fix slots tag slot uses all entities

--- modules/nlu/src/backend/storage.ts
+++ modules/nlu/src/backend/storage.ts
@@ -36,16 +36,12 @@ export default class Storage {
 
   async saveIntent(intent: string, content: sdk.NLU.IntentDefinition) {
     intent = sanitizeFilenameNoExt(intent)
-    const sysEntities = this.getSystemEntities()
+    const entities = await this.getAvailableEntities()
 
     if (content.slots) {
       await Promise.map(content.slots, async slot => {
-        if (!sysEntities.find(e => e.name === slot.entity)) {
-          try {
-            await this.ghost.readFileAsString(this.entitiesDir, `${slot.entity}.json`)
-          } catch (err) {
-            throw Error(`"${slot.entity}" is neither a system entity nor a custom entity`)
-          }
+        if (!entities.find(e => e.name === slot.entity)) {
+          throw Error(`"${slot.entity}" is neither a system entity nor a custom entity`)
         }
       })
     }
