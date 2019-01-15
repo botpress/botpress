ee561e431ee260618674c809a5c8009aa98c3552
Sylvain Perron <slvn.perron+github@gmail.com>
2019-01-14 20:54:59 -0500

    fix ghost UI incorrect cmd

--- src/bp/ui-admin/src/Pages/Versioning.js
+++ src/bp/ui-admin/src/Pages/Versioning.js
@@ -20,11 +20,11 @@ class Versioning extends Component {
   }
 
   setPullCommand = () => {
-    let bpcli = navigator.appVersion.indexOf("Win") !== -1 ? "bp.exe" : "bp"
+    let bpcli = navigator.appVersion.indexOf('Win') !== -1 ? 'bp.exe' : './bp'
 
     const { token } = pullToken()
     const host = window.location.origin
-    const command = `${bpcli} pull --host ${host} --target { YOUR_DATA_TARGET_DIR } --auth ${token}`
+    const command = `${bpcli} pull --host ${host} --auth ${token} --targetDir data`
     this.setState({ command })
   }
 
@@ -34,35 +34,30 @@ class Versioning extends Component {
 
   renderMainContent = () => (
     <div>
-      {
-        this.props.loading && <div>loading</div>
-      }
-      {
-        !this.props.loading && _.isEmpty(this.props.pendingChanges) && <div>you're all set</div>
-      }
-      {
-        !this.props.loading && !_.isEmpty(this.props.pendingChanges) &&
-        <div>
-          <p>Some changes has been made since this server was deployed. Run the botpress pull command to sync server data locally</p>
-          <code>
-            {this.state.command}
-          </code>
-          <CopyToClipboard text={this.state.command} onCopy={this.setCopied}>
-            <Button color="link" size="sm" className="license-infos__icon">
-              <svg href="#" id="TooltipCopy" height="15" viewBox="0 0 16 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M3.996 0H16v16h-4.004v4H0V4h3.996V0zM6 2v12h8V2H6zM2 6v12h8v-1.997H3.998V6H2z"
-                  fill="#4A4A4A"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </Button>
-          </CopyToClipboard>
-          {
-            this.state.copied && <span>&nbsp;copied</span>
-          }
-        </div>
-      }
+      {this.props.loading && <div>loading</div>}
+      {!this.props.loading && _.isEmpty(this.props.pendingChanges) && <div>you're all set</div>}
+      {!this.props.loading &&
+        !_.isEmpty(this.props.pendingChanges) && (
+          <div>
+            <p>
+              Some changes has been made since this server was deployed. Run the botpress pull command to sync server
+              data locally
+            </p>
+            <code>{this.state.command}</code>
+            <CopyToClipboard text={this.state.command} onCopy={this.setCopied}>
+              <Button color="link" size="sm" className="license-infos__icon">
+                <svg href="#" id="TooltipCopy" height="15" viewBox="0 0 16 20" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M3.996 0H16v16h-4.004v4H0V4h3.996V0zM6 2v12h8V2H6zM2 6v12h8v-1.997H3.998V6H2z"
+                    fill="#4A4A4A"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </Button>
+            </CopyToClipboard>
+            {this.state.copied && <span>&nbsp;copied</span>}
+          </div>
+        )}
     </div>
   )
 
@@ -78,8 +73,11 @@ class Versioning extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   ...state.versioning
 })
 
-export default connect(mapStateToProps, { fetchPendingChanges })(Versioning)
+export default connect(
+  mapStateToProps,
+  { fetchPendingChanges }
+)(Versioning)
