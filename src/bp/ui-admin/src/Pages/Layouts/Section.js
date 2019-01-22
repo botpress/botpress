06cfbcf0074a74a54327eba00c67f32f72e4a4f1
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-01-22 11:07:15 -0500

    removed sidenav

--- src/bp/ui-admin/src/Pages/Layouts/Section.js
+++ src/bp/ui-admin/src/Pages/Layouts/Section.js
@@ -1,7 +1,6 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 import { MdInfoOutline } from 'react-icons/lib/md'
-import Menu from '../Components/Menu'
 
 import { Row, Col, UncontrolledTooltip } from 'reactstrap'
 
@@ -17,29 +16,24 @@ class Section extends Component {
     ) : null
 
     return (
-      <Fragment>
-        <aside className="bp-main-content-sidebar">
-          <Menu activePage={this.props.activePage} currentTeam={this.props.currentTeam} />
-        </aside>
-        <div className="bp-main-content-main">
-          <Row>
-            <Col xs={12} md={10}>
-              <h2 className="bp-main-content__title">
-                {this.props.title}
-                {help}
-              </h2>
-            </Col>
-            <Col xs={12} md={2}>
-              {this.props.sideMenu}
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={10}>
-              {this.props.mainContent}
-            </Col>
-          </Row>
-        </div>
-      </Fragment>
+      <div className="bp-main-content-main">
+        <Row>
+          <Col xs={12} md={10}>
+            <h2 className="bp-main-content__title">
+              {this.props.title}
+              {help}
+            </h2>
+          </Col>
+          <Col xs={12} md={2}>
+            {this.props.sideMenu}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={10}>
+            {this.props.mainContent}
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
