5aee366eb72b08cbbab85b27bbd6bf66f9cf27a5
Yann Allard <yann.allard1@gmail.com>
2019-02-22 10:59:43 -0500

    added back bot config for schema building

---
+++ src/bp/core/config/bot.config.ts
@@ -0,0 +1,36 @@
+/** ONLY FOR SCHEMA BUILDING - ALSO EDIT IN BOTPRESS.D.TS */
+
+export type BotConfig = {
+  $schema?: string
+  id: string
+  name: string
+  description?: string
+  category?: string
+  details: BotDetails
+  author?: string
+  disabled?: boolean
+  private?: boolean
+  version: string
+  imports: {
+    /** Defines the list of content types supported by the bot */
+    contentTypes: string[]
+  }
+  dialog?: DialogConfig
+  logs?: LogsConfig
+}
+
+export interface BotDetails {
+  website?: string
+  phoneNumber?: string
+  termsConditions?: string
+  emailAddress?: string
+}
+
+export interface LogsConfig {
+  expiration: string
+}
+
+export interface DialogConfig {
+  timeoutInterval: string
+  sessionTimeoutInterval: string
+}
