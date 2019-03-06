2c53ab57dda1df8915186907cc86bf707fc6e34a
Yann Allard <yann.allard1@gmail.com>
2019-03-06 15:22:15 -0500

    updated views for telegram

--- modules/channel-telegram/src/views/full/index.jsx
+++ modules/channel-telegram/src/views/full/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class TemplateModule extends React.Component {
+export default class FullView extends React.Component {
   render() {
     return null
   }
