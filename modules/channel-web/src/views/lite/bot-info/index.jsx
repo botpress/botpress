23aa009456efcc47a3bc95a007df8c47afac5258
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-03-07 16:35:53 -0500

    actual bot info page implementation

--- modules/channel-web/src/views/lite/bot-info/index.jsx
+++ modules/channel-web/src/views/lite/bot-info/index.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+// import classnames from 'classnames'
 
-export default ({ bot, onDismiss }) => (
-  <div>
-    this is the bot info page props
-    <button onClick={onDismiss} />
-    <span>{bot.some}</span>
+import style from './style.scss'
+import PhoneIcon from './PhoneIcon'
+import WebsiteIcon from './WebsiteIcon'
+import EmailIcon from './EmailIcon'
+import ConvoIcon from './ConvoIcon'
+
+const CoverPicture = ({ botInfo }) => (
+  <img
+    src={
+      (botInfo.details && botInfo.details.coverPictureUrl) || `https://via.placeholder.com/400x175?text=${botInfo.name}`
+    }
+  />
+)
+
+const Avatar = ({ botInfo, webchatConfig }) => (
+  <img
+    src={
+      (botInfo.details && botInfo.details.avatarUrl) ||
+      (webchatConfig && webchatConfig.botAvatarUrl) ||
+      `https://via.placeholder.com/64x64?text=${botInfo.name[0]}`
+    }
+  />
+)
+
+export default ({ botInfo, onDismiss, dismissLabel, webchatConfig }) => (
+  <div className={'bp-bot-info-container ' + style['bot-info-container']}>
+    <CoverPicture botInfo={botInfo} />
+    <div className={'bp-bot-info-summmary ' + style.summary}>
+      <Avatar botInfo={botInfo} />
+      <h3>{botInfo.name}</h3>
+
+      {/* truncate after x number of lines ? */}
+      <p>{botInfo.description}</p>
+    </div>
+    {botInfo.details && (
+      <Fragment>
+        <div className={'bp-bot-info-links ' + style.links}>
+          {botInfo.details.phoneNumber && (
+            <div className={'bp-bot-info-icon-link ' + style['icon-link']}>
+              <i>
+                <PhoneIcon />
+              </i>
+              <a target="_blank" href={`tel:${botInfo.details.phoneNumber}`}>
+                {botInfo.details.phoneNumber}
+              </a>
+            </div>
+          )}
+          {botInfo.details.website && (
+            <div className={'bp-bot-info-icon-link ' + style['icon-link']}>
+              <i>
+                <WebsiteIcon />
+              </i>
+              <a target="_blank" href={botInfo.details.website}>
+                {botInfo.details.website}
+              </a>
+            </div>
+          )}
+          {botInfo.details.emailAddress && (
+            <div className={'bp-bot-info-icon-link ' + style['icon-link']}>
+              <i>
+                <EmailIcon />
+              </i>
+              <a target="_blank" href={`mailto:${botInfo.details.emailAddress}`}>
+                {botInfo.details.emailAddress}
+              </a>
+            </div>
+          )}
+        </div>
+        {botInfo.details.termsConditions && (
+          <div className={'bp-bot-info-terms ' + style.terms}>
+            <a target="_blank" href={botInfo.details.termsConditions}>
+              View Terms of Service
+            </a>
+          </div>
+        )}
+        {botInfo.details.privacyPolicy && (
+          <div className={'bp-bot-info-terms ' + style.terms}>
+            <a target="_blank" href={botInfo.details.privacyPolicy}>
+              View Privacy Policy
+            </a>
+          </div>
+        )}
+      </Fragment>
+    )}
+    <button className={'bp-bot-info-start-convo-button ' + style.startBtn} onClick={onDismiss}>
+      {dismissLabel || 'Start Conversation'}
+    </button>
   </div>
 )
