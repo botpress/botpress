4f31fbc658702f7def45b67d15c2c326a48ff03a
Sylvain Perron <slvn.perron+github@gmail.com>
2019-03-15 09:03:10 -0400

    fix(nlu): only copy vector files

--- modules/nlu/src/backend/models.ts
+++ modules/nlu/src/backend/models.ts
@@ -3,16 +3,19 @@ import fs from 'fs'
 import _ from 'lodash'
 import path from 'path'
 
+const debug = DEBUG('nlu').sub('pretrained')
+
 export default async (bp: typeof sdk) => {
   const models = await bp.ghost.forGlobal().directoryListing('./models', '*.vec')
   const pretrainedPath = path.join(__dirname, 'pretrained')
-  const files = fs.readdirSync(pretrainedPath)
+  const files = fs.readdirSync(pretrainedPath).filter(x => x.toLowerCase().endsWith('.vec'))
   const globalGhost = bp.ghost.forGlobal()
   const missing = _.without(files, ...models)
 
+  debug('start copying missing pretrained models', missing)
   for (const model of missing) {
     const data = fs.readFileSync(path.join(pretrainedPath, model))
     await globalGhost.upsertFile('./models', model, data)
-    bp.logger.debug(`Copied pretrained model "${model}"`)
   }
+  debug('end copy missing pretrained models')
 }
