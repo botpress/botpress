b45eb5487db38550d1371c3b85a9a93d8a076785
Yann Allard <yann.allard1@gmail.com>
2019-03-21 18:17:45 -0400

    fix(admin): added schema for profile update

--- src/bp/core/routers/auth.ts
+++ src/bp/core/routers/auth.ts
@@ -5,11 +5,12 @@ import { AuthStrategies } from 'core/services/auth-strategies'
 import AuthService, { TOKEN_AUDIENCE } from 'core/services/auth/auth-service'
 import { WorkspaceService } from 'core/services/workspace-service'
 import { Request, RequestHandler, Router } from 'express'
+import Joi from 'joi'
 import _ from 'lodash'
 
 import { CustomRouter } from './customRouter'
 import { NotFoundError } from './errors'
-import { checkTokenHeader, success as sendSuccess } from './util'
+import { checkTokenHeader, success as sendSuccess, validateBodySchema } from './util'
 
 export class AuthRouter extends CustomRouter {
   private checkTokenHeader!: RequestHandler
@@ -87,6 +88,22 @@ export class AuthRouter extends CustomRouter {
   }
 
   updateProfile = async (req, res) => {
+    validateBodySchema(
+      req,
+      Joi.object().keys({
+        firstname: Joi.string()
+          .min(0)
+          .max(35)
+          .trim()
+          .required(),
+        lastname: Joi.string()
+          .min(0)
+          .max(35)
+          .trim()
+          .required()
+      })
+    )
+
     await this.workspaceService.updateUser(req.tokenUser.email, {
       firstname: req.body.firstname,
       lastname: req.body.lastname
