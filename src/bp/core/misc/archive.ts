b340000d3907c5e45054fe666aaecd188b29ed60
Yann Allard <yann.allard1@gmail.com>
2019-03-28 11:34:24 -0400

    minor refactor

--- src/bp/core/misc/archive.ts
+++ src/bp/core/misc/archive.ts
@@ -7,22 +7,26 @@ import { VError } from 'verror'
 import { forceForwardSlashes } from './utils'
 
 export const extractArchive = async (archive: Buffer, destination: string): Promise<string[]> => {
-  if (!fse.existsSync(destination)) {
-    fse.mkdirSync(destination)
+  try {
+    if (!fse.existsSync(destination)) {
+      fse.mkdirSync(destination)
+    }
+    const buffStream = new stream.PassThrough()
+    const tarWriteStream = tar.x({ sync: true, strict: true, cwd: destination })
+
+    buffStream.end(archive)
+    buffStream.pipe(tarWriteStream)
+
+    await new Promise((resolve, reject) => {
+      tarWriteStream.on('end', resolve)
+      tarWriteStream.on('error', reject)
+    })
+
+    const files = await Promise.fromCallback<string[]>(cb => glob('**/*.*', { cwd: destination }, cb))
+    return files.map(filePath => forceForwardSlashes(filePath))
+  } catch (err) {
+    throw new VError(err, `[Archive] Error extracting archive to "${destination}"`)
   }
-  const buffStream = new stream.PassThrough()
-  const tarWriteStream = tar.x({ sync: true, strict: true, cwd: destination })
-
-  buffStream.end(archive)
-  buffStream.pipe(tarWriteStream)
-
-  await new Promise((resolve, reject) => {
-    tarWriteStream.on('end', resolve)
-    tarWriteStream.on('error', reject)
-  })
-
-  const files = await Promise.fromCallback<string[]>(cb => glob('**/*.*', { cwd: destination }, cb))
-  return files.map(filePath => forceForwardSlashes(filePath))
 }
 
 export const createArchive = async (fileName: string, folder: string, files: string[]): Promise<string> => {
