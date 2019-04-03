1931f555496aa0a346cafcedbc879bae98f31243
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-04-02 21:11:49 -0400

    added some icons to improve readability

--- src/bp/ui-admin/src/Pages/Workspace/Bots/Bot.jsx
+++ src/bp/ui-admin/src/Pages/Workspace/Bots/Bot.jsx
@@ -2,9 +2,12 @@ import React, { Fragment } from 'react'
 import { Badge, UncontrolledButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap'
 import { AccessControl } from '../../../App/AccessControl'
 import { Link } from 'react-router-dom'
+import { IoIosChatbubble } from 'react-icons/lib/io'
+import { MdModeEdit, MdArchive, MdDelete, MdSkipNext } from 'react-icons/lib/md'
+import { FaCog } from 'react-icons/lib/fa'
 
-export default ({ bot, requestStageChange, deleteBot, exportBot, permissions, allowPromotion }) => (
-  <div className="bp_table-row" key={bot.id}>
+export default ({ bot, requestStageChange, deleteBot, exportBot, permissions, allowStageChange }) => (
+  <div className="pipeline_bot" key={bot.id}>
     <div className="actions">
       <UncontrolledButtonDropdown>
         <DropdownToggle caret size="sm" outline color="primary">
@@ -12,15 +15,31 @@ export default ({ bot, requestStageChange, deleteBot, exportBot, permissions, al
         </DropdownToggle>
         <DropdownMenu>
           <DropdownItem tag="a" target="_blank" href={`${window.location.origin}/s/${bot.id}`}>
-            Open chat
+            <IoIosChatbubble /> &nbsp;Open chat
           </DropdownItem>
-          <DropdownItem tag="a" href={`/studio/${bot.id}`}>
-            Edit
+          <DropdownItem tag={Link} to={`/studio/${bot.id}`}>
+            <MdModeEdit />
+            &nbsp;Edit in studio
+          </DropdownItem>
+          <DropdownItem tag="a" href={`/admin/bot/${bot.id}/details`}>
+            <FaCog />
+            &nbsp;Configs
           </DropdownItem>
           <AccessControl permissions={permissions} resource="admin.bots.*" operation="write">
-            <DropdownItem onClick={exportBot}>Export</DropdownItem>
-            {allowPromotion && <DropdownItem onClick={requestStageChange}>Promote to next stage</DropdownItem>}
-            <DropdownItem onClick={deleteBot}>Delete</DropdownItem>
+            <DropdownItem onClick={exportBot}>
+              <MdArchive />
+              &nbsp;Export
+            </DropdownItem>
+            {allowStageChange && (
+              <DropdownItem onClick={requestStageChange}>
+                <MdSkipNext />
+                &nbsp;Promote to next stage
+              </DropdownItem>
+            )}
+            <DropdownItem onClick={deleteBot}>
+              <MdDelete />
+              &nbsp;Delete
+            </DropdownItem>
           </AccessControl>
         </DropdownMenu>
       </UncontrolledButtonDropdown>
