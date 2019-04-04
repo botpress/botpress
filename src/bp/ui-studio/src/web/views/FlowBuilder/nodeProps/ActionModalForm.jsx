d9a89e114f4b338d3dd79519c6aa40d9e353acd7
Oleksiy Pletnov <k4merton@gmail.com>
2019-04-05 02:14:26 +0300

    fix(studio): fix exception on adding actions

--- src/bp/ui-studio/src/web/views/FlowBuilder/nodeProps/ActionModalForm.jsx
+++ src/bp/ui-studio/src/web/views/FlowBuilder/nodeProps/ActionModalForm.jsx
@@ -114,7 +114,7 @@ export default class ActionModalForm extends Component {
               const fn = avActions.find(fn => fn.name === (val && val.value))
               const paramsDefinition = _.get(fn, 'metadata.params') || []
               this.setState({
-                functionInputValue: val,
+                functionInputValue: val && val.value,
                 paramsDef: paramsDefinition,
                 actionMetadata: fn.metadata || {}
               })
