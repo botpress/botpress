1aba78e2b8825a38e301176fef9221d88a633b65
Renaud Lainé <rnd.laine+github@gmail.com>
2019-04-09 14:38:22 -0400

    fix any entity slot filling

--- modules/basic-skills/src/actions/slot_fill.js
+++ modules/basic-skills/src/actions/slot_fill.js
@@ -2,22 +2,27 @@
  * Extract entities that match a slot
  * @hidden true
  * @param slotName The name of the slot to extract. (e.g. destination_from)
- * @param entity The entity of the slot. (e.g. City)
+ * @param entityName The entity of the slot. (e.g. City)
  */
-const slotFill = async (slotName, entity) => {
-  if (Array.isArray(event.nlu.entities)) {
-    for (const e of event.nlu.entities) {
-      if (e.name === entity || entity === 'any') {
-        if (!session.extractedSlots[slotName]) {
-          session.extractedSlots[slotName] = {
-            value: e.data.value,
-            timestamp: Date.now()
-          }
-
-          session.extractedSlots.notFound = 0
-        }
+const slotFill = async (slotName, entityName) => {
+  if (event.nlu.entities && event.nlu.entities.length) {
+    for (const entity of event.nlu.entities) {
+      if (entity.name === entityName) {
+        setSlot(slotName, entity.data.value)
       }
     }
+  } else if (entityName === 'any') {
+    setSlot(slotName, event.payload.text)
+  }
+}
+
+const setSlot = (slotName, value) => {
+  if (!session.extractedSlots[slotName]) {
+    session.extractedSlots[slotName] = {
+      value,
+      timestamp: Date.now()
+    }
+    session.extractedSlots.notFound = 0
   }
 }
 
