de6281e8bb70920015cb23b2dadf15c33710228c
Renaud Lainé <rnd.laine+github@gmail.com>
2019-04-10 11:11:00 -0400

    slot skill form clean up

--- modules/basic-skills/src/views/full/slot.jsx
+++ modules/basic-skills/src/views/full/slot.jsx
@@ -29,30 +29,40 @@ export class Slot extends React.Component {
     this.fetchIntents().then(() => this.setStateFromProps())
   }
 
-  componentDidUpdate() {
+  getSelectedIntent() {
     const intentName = this.state.selectedIntentOption && this.state.selectedIntentOption.value
-    const intent = intentName && this.state.intents.find(x => x.name === intentName)
+    return intentName && this.state.intents.find(x => x.name === intentName)
+  }
+
+  getSelectedSlot(intent) {
     const slotName = this.state.selectedSlotOption && this.state.selectedSlotOption.value
-    const slot = intent && intent.slots.find(x => x.name === slotName)
-    const entity = slot && slot.entity
+    return intent && intent.slots.find(x => x.name === slotName)
+  }
 
-    const data = {
-      validationRegex: this.state.validationRegex,
-      retryAttempts: this.state.maxRetryAttempts,
-      contentElement: this.state.contentElement,
-      notFoundElement: this.state.notFoundElement,
-      slotName,
-      intent: intentName,
-      entity,
-      validationAction: this.state.validationAction
-    }
-
-    if (
+  isFormValid() {
+    return (
       this.state.selectedIntentOption &&
       this.state.selectedSlotOption &&
       this.state.contentElement &&
       this.state.notFoundElement
-    ) {
+    )
+  }
+
+  componentDidUpdate() {
+    if (this.isFormValid()) {
+      const intent = this.getSelectedIntent()
+      const slot = this.getSelectedSlot(intent)
+
+      const data = {
+        retryAttempts: this.state.maxRetryAttempts,
+        contentElement: this.state.contentElement,
+        notFoundElement: this.state.notFoundElement,
+        validationAction: this.state.validationAction,
+        intent: intent.name,
+        slotName: slot.name,
+        entity: slot.entity
+      }
+
       this.props.onDataChanged && this.props.onDataChanged(data)
       this.props.onValidChanged && this.props.onValidChanged(true)
     }
@@ -150,101 +160,99 @@ export class Slot extends React.Component {
       })
 
     return (
-      <React.Fragment>
-        <div className={style.modalContent}>
-          {this.state.error && <Alert bsStyle="danger">{this.state.error}</Alert>}
-          <Row>
-            <Col md={6}>
-              <Label>Choose an intent</Label>
-              <Select
-                id="intent"
-                name="intent"
-                isSearchable={true}
-                className={style.intentSelect}
-                onChange={this.handleIntentChange}
-                value={this.state.selectedIntentOption}
-                options={this.state.intents.map(intent => {
-                  return { value: intent.name, label: intent.name }
-                })}
+      <div className={style.modalContent}>
+        {this.state.error && <Alert bsStyle="danger">{this.state.error}</Alert>}
+        <Row>
+          <Col md={6}>
+            <Label>Choose an intent</Label>
+            <Select
+              id="intent"
+              name="intent"
+              isSearchable={true}
+              className={style.intentSelect}
+              onChange={this.handleIntentChange}
+              value={this.state.selectedIntentOption}
+              options={this.state.intents.map(intent => {
+                return { value: intent.name, label: intent.name }
+              })}
+            />
+          </Col>
+          <Col md={6}>
+            <Label for="slot">Choose a slot to fill</Label>
+            <Select
+              id="slot"
+              name="slot"
+              className={style.slotSelect}
+              isSearchable={true}
+              onChange={this.handleSlotChange}
+              value={this.state.selectedSlotOption}
+              options={slotOptions}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Label for="contentPicker">Bot will ask</Label>
+            <BotpressTooltip message="The bot should ask a question specific about the slot to fill. (e.g. What is your email?)" />
+            <ContentPickerWidget
+              style={{ zIndex: 0 }}
+              name="contentPicker"
+              id="contentPicker"
+              itemId={this.state.contentElement}
+              onChange={this.handleContentChange}
+              placeholder="Pick content"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md="9">
+            <Label>Invalid Input Message</Label>
+            <BotpressTooltip message="This message will appear to the user when the information he has given is invalid. (e.g. Your email is invalid)" />
+            <ContentPickerWidget
+              style={{ zIndex: 0 }}
+              id="notFoundElement"
+              itemId={this.state.notFoundElement}
+              onChange={this.handleNotFoundChange}
+              placeholder="Pick content to display when the slot is not found"
+            />
+          </Col>
+          <Col md="3">
+            <Label for="retryAttempts">Max retry attempts</Label>
+            <BotpressTooltip message="This is the maximum number of times the bot will try to extract the slot. When the limit is reached, the bot will execute the 'On not found' transition." />
+            <Input
+              id="retryAttempts"
+              name="retryAttempts"
+              type="number"
+              min="0"
+              max={MAX_RETRIES}
+              value={this.state.maxRetryAttempts}
+              onChange={this.handleMaxRetryAttemptsChange}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Input
+              type="checkbox"
+              id="validationCheck"
+              name="validationCheck"
+              onChange={this.toggleAddValidation}
+              checked={this.state.addValidation}
+            />
+            &nbsp;
+            <Label for="validationCheck">Custom Input Validation</Label>
+            <BotpressTooltip message="You can add custom validation for your slot with an action. It should assign a boolean value to the temp.valid variable." />
+            {this.state.addValidation && (
+              <SelectActionDropdown
+                className={style.actionSelect}
+                value={this.state.validationAction && this.state.validationAction.value}
+                options={this.state.actions}
+                onChange={this.handleActionChange}
               />
-            </Col>
-            <Col md={6}>
-              <Label for="slot">Choose a slot to fill</Label>
-              <Select
-                id="slot"
-                name="slot"
-                className={style.slotSelect}
-                isSearchable={true}
-                onChange={this.handleSlotChange}
-                value={this.state.selectedSlotOption}
-                options={slotOptions}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12}>
-              <Label for="contentPicker">Bot will ask</Label>
-              <BotpressTooltip message="The bot should ask a question specific about the slot to fill. (e.g. What is your email?)" />
-              <ContentPickerWidget
-                style={{ zIndex: 0 }}
-                name="contentPicker"
-                id="contentPicker"
-                itemId={this.state.contentElement}
-                onChange={this.handleContentChange}
-                placeholder="Pick content"
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md="9">
-              <Label>Invalid Input Message</Label>
-              <BotpressTooltip message="This message will appear to the user when the information he has given is invalid. (e.g. Your email is invalid)"/>
-              <ContentPickerWidget
-                style={{ zIndex: 0 }}
-                id="notFoundElement"
-                itemId={this.state.notFoundElement}
-                onChange={this.handleNotFoundChange}
-                placeholder="Pick content to display when the slot is not found"
-              />
-            </Col>
-            <Col md="3">
-              <Label for="retryAttempts">Max retry attempts</Label>
-              <BotpressTooltip message="This is the maximum number of times the bot will try to extract the slot. When the limit is reached, the bot will execute the 'On not found' transition."/>
-              <Input
-                id="retryAttempts"
-                name="retryAttempts"
-                type="number"
-                min="0"
-                max={MAX_RETRIES}
-                value={this.state.maxRetryAttempts}
-                onChange={this.handleMaxRetryAttemptsChange}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12}>
-              <Input
-                type="checkbox"
-                id="validationCheck"
-                name="validationCheck"
-                onChange={this.toggleAddValidation}
-                checked={this.state.addValidation}
-              />
-              &nbsp;
-              <Label for="validationCheck">Custom Input Validation</Label>
-              <BotpressTooltip message="You can add custom validation for your slot with an action. It should assign a boolean value to the temp.valid variable."/>
-              {this.state.addValidation && (
-                <SelectActionDropdown
-                  className={style.actionSelect}
-                  value={this.state.validationAction && this.state.validationAction.value}
-                  options={this.state.actions}
-                  onChange={this.handleActionChange}
-                />
-              )}
-            </Col>
-          </Row>
-        </div>
-      </React.Fragment>
+            )}
+          </Col>
+        </Row>
+      </div>
     )
   }
 }
