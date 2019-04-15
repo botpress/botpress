80b7c010ff88bff8b737f9f021eea2378d037b4b
allardy <yann.allard1@gmail.com>
2019-04-15 14:49:06 -0400

    fix(cms): added missing properties to simplify multilang

--- src/bp/core/api.ts
+++ src/bp/core/api.ts
@@ -174,12 +174,8 @@ const ghost = (ghostService: GhostService): typeof sdk.ghost => {
 
 const cms = (cmsService: CMSService, mediaService: MediaService): typeof sdk.cms => {
   return {
-    getContentElement(botId: string, id: string): Promise<any> {
-      return cmsService.getContentElement(botId, id)
-    },
-    getContentElements(botId: string, ids: string[]): Promise<any[]> {
-      return cmsService.getContentElements(botId, ids)
-    },
+    getContentElement: cmsService.getContentElement.bind(cmsService),
+    getContentElements: cmsService.getContentElements.bind(cmsService),
     listContentElements: cmsService.listContentElements.bind(cmsService),
     deleteContentElements: cmsService.deleteContentElements.bind(cmsService),
     getAllContentTypes(botId?: string): Promise<any[]> {
@@ -188,14 +184,7 @@ const cms = (cmsService: CMSService, mediaService: MediaService): typeof sdk.cms
     renderElement(contentId: string, args: any, eventDestination: sdk.IO.EventDestination): Promise<any> {
       return cmsService.renderElement(contentId, args, eventDestination)
     },
-    createOrUpdateContentElement(
-      botId: string,
-      contentTypeId: string,
-      formData: string,
-      contentElementId?: string
-    ): Promise<string> {
-      return cmsService.createOrUpdateContentElement(botId, contentTypeId, formData, contentElementId)
-    },
+    createOrUpdateContentElement: cmsService.createOrUpdateContentElement.bind(cmsService),
     async saveFile(botId: string, fileName: string, content: Buffer): Promise<string> {
       return mediaService.saveFile(botId, fileName, content)
     },
