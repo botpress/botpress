ac8835462ec5e6b39994bbdacec128290e45899a
Renaud Lainé <rnd.laine+github@gmail.com>
2019-04-15 21:50:03 -0400

    refactor callApi.js

--- modules/basic-skills/src/actions/call_api.js
+++ modules/basic-skills/src/actions/call_api.js
@@ -1,25 +1,19 @@
 const axios = require('axios')
-
 /**
  * @hidden true
  */
+
 const callApi = async (url, method, body, memory, variable) => {
-  const client = axios.create({
-    baseURL: url
-  })
-
-  let response = undefined
-  if (method === 'get') {
-    response = await client.get('/').then(res => res.data)
-  } else if (method === 'post') {
-    response = await client.post('/', body).then(res => res.data)
-  } else if (method === 'put') {
-    response = await client.put('/', body).then(res => res.data)
-  } else if (method === 'delete') {
-    response = await client.delete('/').then(res => res.data)
+  try {
+    const { data } = await axios({
+      method,
+      url,
+      data: body
+    })
+    event.state[memory][variable] = data
+  } catch (error) {
+    bp.logger.warning(`Error: ${error.response.status || ''} while calling resource "${url}"`)
   }
-
-  event.state[memory][variable] = response
 }
 
 return callApi(args.url, args.method, args.body, args.memory, args.variable)
