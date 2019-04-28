856bb256655289d608e113b714d55a2acc4fcdb9
Sylvain Perron <slvn.perron+github@gmail.com>
2019-04-28 09:01:34 -0700

    fix(nlu): tokenizer should not lower case

--- modules/nlu/src/backend/pipelines/language/tokenizers.ts
+++ modules/nlu/src/backend/pipelines/language/tokenizers.ts
@@ -1,8 +1,4 @@
-const WhiteSpaceTokenizer = (txt: string) =>
-  txt
-    .split(/[^a-zA-Z0-9_]/i)
-    .map(x => x.toLowerCase().trim())
-    .filter(x => x.trim().length)
+const WhiteSpaceTokenizer = (txt: string) => txt.split(/[^a-zA-Z0-9_]/i).filter(x => x.trim().length)
 
 export const tokenize = async (text: string, lang: string): Promise<string[]> => {
   // TODO Add ML-Based Tokenizer here
