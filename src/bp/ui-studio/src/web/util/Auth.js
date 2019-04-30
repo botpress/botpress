109f5f37d10954ecd99f32f5e3624899f85db5c3
allardy <yann.allard1@gmail.com>
2019-04-29 20:09:35 -0400

    fix(channel-web): added cookie fallback

--- src/bp/ui-studio/src/web/util/Auth.js
+++ src/bp/ui-studio/src/web/util/Auth.js
@@ -1,13 +1,38 @@
 import EventEmitter2 from 'eventemitter2'
 import axios from 'axios'
 import nanoid from 'nanoid'
+import Cookie from 'js-cookie'
 
 const storageKey = 'bp/token'
+
+const storage = {
+  set: (key, value) => {
+    try {
+      localStorage ? localStorage.setItem(key, value) : Cookie.set(key, value)
+    } catch (err) {
+      console.log('Error while getting data from storage.', err.message)
+    }
+  },
+  get: key => {
+    try {
+      return localStorage ? localStorage.getItem(key) : Cookie.get(key)
+    } catch (err) {
+      console.log('Error while getting data from storage.', err.message)
+    }
+  },
+  del: key => {
+    try {
+      localStorage ? localStorage.removeItem(key) : Cookie.remove(key)
+    } catch (err) {
+      console.log('Error while deleting data from storage.', err.message)
+    }
+  }
+}
 
 export const authEvents = new EventEmitter2()
 
 export const getToken = () => {
-  const tokenStr = localStorage.getItem(storageKey)
+  const tokenStr = storage.get(storageKey)
 
   if (tokenStr) {
     return JSON.parse(tokenStr)
@@ -17,7 +42,7 @@ export const getToken = () => {
 }
 
 export const setToken = token => {
-  localStorage.setItem(
+  storage.set(
     storageKey,
     JSON.stringify({
       token,
@@ -30,7 +55,7 @@ export const setToken = token => {
 }
 
 export const logout = () => {
-  localStorage.removeItem(storageKey)
+  storage.del(storageKey)
   authEvents.emit('logout')
 }
 
@@ -48,16 +73,15 @@ export const login = (email, password) => {
 }
 
 export const setVisitorId = userId => {
-  localStorage.setItem('bp/socket/user', userId)
+  storage.set('bp/socket/user', userId)
   window.__BP_VISITOR_ID = userId
 }
 
 export const getUniqueVisitorId = () => {
-  let userId = localStorage.getItem('bp/socket/user')
-
+  let userId = storage.get('bp/socket/user')
   if (!userId) {
     userId = nanoid()
-    localStorage.setItem('bp/socket/user', userId)
+    storage.set('bp/socket/user', userId)
   }
 
   window.__BP_VISITOR_ID = userId
