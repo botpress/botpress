dd94689f893168501f5e5fb3411fd3a4833021eb
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-05-06 16:51:47 -0400

    removed dead code

--- modules/testing/src/views/full/FailureReport.jsx
+++ modules/testing/src/views/full/FailureReport.jsx
@@ -6,51 +6,6 @@ import style from './style.scss'
 import Interaction from './Interaction'
 
 class FailureReport extends React.Component {
-  renderMessage(response, source) {
-    const { contentElements } = this.props
-
-    if (typeof response === 'string') {
-      const element = contentElements.find(el => el.id === response)
-      response = (element && element.preview) || response
-    }
-
-    if (typeof response === 'object') {
-      const InjectedModuleView = this.props.bp.getModuleInjector()
-      return (
-        <div className="bpw-from-bot" key={response}>
-          <InjectedModuleView
-            moduleName={'channel-web'}
-            componentName={'Message'}
-            extraProps={{ payload: response || 'error previewing' }}
-            onNotFound={undefined}
-          />
-          <small>(source: {source})</small>
-        </div>
-      )
-    } else {
-      return (
-        <div className="bpw-from-bot" key={response}>
-          <div className="bpw-chat-bubble">{response}</div>
-        </div>
-      )
-    }
-  }
-
-  renderInteraction(interaction) {
-    const { userMessage, botReplies } = interaction
-    return (
-      <div style={{ margin: '0 15px 0 15px' }}>
-        <div style={{ width: '100%', marginBottom: 3 }} align="right">
-          <div className="bpw-from-user" style={{ width: 'auto' }}>
-            <div className="bpw-chat-bubble">{userMessage}</div>
-          </div>
-        </div>
-
-        {botReplies.map(({ botResponse, replySource }) => this.renderMessage(botResponse, replySource))}
-      </div>
-    )
-  }
-
   render() {
     return (
       <Grid fluid={true}>
