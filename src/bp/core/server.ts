36458fded646951d23c5119666ecb0ce3627a166
Sylvain Perron <slvn.perron+github@gmail.com>
2019-05-06 21:55:12 -0400

    added white-label licensing guard

--- src/bp/core/server.ts
+++ src/bp/core/server.ts
@@ -203,7 +203,7 @@ export default class HTTPServer {
       res.send(await this.monitoringService.getStatus())
     })
 
-    this.app.use('/assets', express.static(this.resolveAsset('')))
+    this.app.use('/assets', this.guardWhiteLabel(), express.static(this.resolveAsset('')))
     this.app.use(rewrite('/:app/:botId/*env.js', '/api/v1/bots/:botId/:app/js/env.js'))
 
     this.app.use(`${BASE_API_PATH}/auth`, this.authRouter.router)
@@ -254,6 +254,15 @@ export default class HTTPServer {
     })
 
     return this.app
+  }
+
+  private guardWhiteLabel() {
+    return (req, res, next) => {
+      if (path.normalize(req.path) === '/custom-theme.css' && (!process.IS_PRO_ENABLED || !process.IS_LICENSED)) {
+        return res.sendStatus(404)
+      }
+      next()
+    }
   }
 
   setupStaticRoutes(app) {
