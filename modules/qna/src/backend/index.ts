0b3912b4a97cda0114d37294b197fcb4c0af2e24
David V. Antonio <david.vitora@gmail.com>
2019-05-09 09:18:17 -0300

    Performance improvement and will not delete qna

--- modules/qna/src/backend/index.ts
+++ modules/qna/src/backend/index.ts
@@ -55,19 +55,19 @@ const onFlowChanged = async (bp: typeof sdk, botId: string, newFlow: sdk.Flow) =
 }
 
 const onElementChanged = async (bp, botId, action, element) => {
-  const qnaStorage = await botScopedStorage.get(botId)
-  const questions = await qnaStorage.getQuestions({ question: '', categories: [] }, { limit: 0, offset: 0 })
   // Update all questions that have this contentElement as answer
   if (action === 'delete') {
-    const affectedItems = questions.items.filter(item =>
-      item.data.answers.filter(answer => answer.contentId === element.id)
+    const qnaStorage = await botScopedStorage.get(botId)
+    const questions = await qnaStorage.getQuestions({ question: '', categories: [] }, { limit: 0, offset: 0 })
+    const affectedItems = questions.items.filter(
+      item => item.data.answers.filter(answer => answer.contentId === element.id).length
     )
     for (const item of affectedItems) {
       const newAnswers = item.data.answers.filter(answer => answer.contentId !== element.id)
       if (newAnswers.length) {
         await qnaStorage.update({ ...item.data, answers: newAnswers }, item.id)
       } else {
-        await qnaStorage.delete(item.id)
+        await qnaStorage.update({ ...item.data, answers: ['output responses deleted'], enabled: false }, item.id)
       }
     }
   }
