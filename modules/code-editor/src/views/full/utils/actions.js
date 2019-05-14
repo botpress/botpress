547c96595aeef890dccd6ac233ef0f87f523833d
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-05-14 16:18:20 -0400

    fix function instrumentation

--- modules/code-editor/src/views/full/utils/actions.js
+++ modules/code-editor/src/views/full/utils/actions.js
@@ -1,21 +1,16 @@
-const action = {
-  startBlock: `async function action(bp: typeof sdk, event: sdk.IO.IncomingEvent, args: any, { user, temp, session } = event.state) {
-/** Your action code starts below */
-
-`,
-  endBlock: `
-/** Your action code ends here */ 
-}
-`
-}
+const ACTION_SIGNATURE =
+  'async function action(bp: typeof sdk, event: sdk.IO.IncomingEvent, args: any, { user, temp, session } = event.state)'
+const ACTION_START_COMMENT = `/** Your action code starts below */`
+const ACTION_END_COMMENT = '/** Your action code ends here */'
 
 const wrapper = {
-  add: content => `${action.startBlock}${content}${action.endBlock}`,
-  remove: content =>
-    content
-      .replace(action.startBlock.trim(), '')
-      .replace(action.endBlock.trim(), '')
-      .trim()
+  add: content => `${ACTION_SIGNATURE}{\n${ACTION_START_COMMENT}\n\n${content}\n${ACTION_END_COMMENT}\n}`,
+  remove: content => {
+    const contentStart = content.indexOf(ACTION_START_COMMENT) + ACTION_START_COMMENT.length
+    const contentEnd = content.indexOf(ACTION_END_COMMENT)
+
+    return content.substring(contentStart, contentEnd).trim()
+  }
 }
 
 export { wrapper }
