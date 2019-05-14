4d9d143fcf6456c8b996e5b1197df9a66c73aa9b
Francois <francois_levasseur@hotmail.com>
2019-05-14 16:52:11 -0400

    added json inspector, data janitor and othe reformat

--- modules/history/src/backend/index.ts
+++ modules/history/src/backend/index.ts
@@ -1,27 +1,46 @@
 import 'bluebird-global'
 import * as sdk from 'botpress/sdk'
 
+import { Config } from '../config'
+
 const onServerStarted = async (bp: typeof sdk) => {
   bp.database.createTableIfNotExists('msg_history', table => {
     table.increments('id').primary()
+    table.date('created_on')
+    table.string('thread_id')
     table.string('msg_content')
   })
 }
 
+const cleanDatabase = async (db, limitDate: Date) => {
+  console.log(`clean database called with limit date = '${limitDate.toDateString()}' (${limitDate.getTime()})`)
+  await db
+    .table('msg_history')
+    .where('created_on', '<', limitDate.getTime())
+    .del()
+}
+
 const onServerReady = async (bp: typeof sdk) => {
   const router = bp.http.createRouterForBot('history')
+  const globalConfig = (await bp.config.getModuleConfig('history')) as Config
 
-  router.get('/conversations', async (req, res) => {
-    const conversations = await bp.database
-      .select('*')
+  router.get('/conversations/:from/:to', async (req, res) => {
+    const from = req.params.from
+    const to = req.params.to
+
+    const limitDate = new Date(Date.now())
+    limitDate.setDate(limitDate.getDate() - globalConfig.DatabaseEntryDaysToLive)
+
+    cleanDatabase(bp.database, limitDate)
+
+    const uniqueConversations = await bp.database
+      .select()
+      .distinct('thread_id')
+      .where('created_on', '>=', from)
+      .where('created_on', '<=', to)
+      .whereNotNull('thread_id')
       .table('msg_history')
-      .map(x => JSON.parse(x.msg_content))
-      .filter(msg => msg.botId === req.params.botId)
-      .map(msg => msg.threadId)
-
-    const uniqueConversations = conversations.filter((value, index) => {
-      return conversations.indexOf(value) === index
-    })
+      .map(x => x.thread_id)
 
     res.send(uniqueConversations)
   })
@@ -30,10 +49,10 @@ const onServerReady = async (bp: typeof sdk) => {
     const convId = req.params.convId
 
     const messages = await bp.database
-      .select('*')
+      .select('msg_content')
+      .where('thread_id', convId)
       .table('msg_history')
       .map(x => JSON.parse(x.msg_content))
-      .filter(msg => msg.threadId === convId)
 
     res.send(messages)
   })
@@ -55,7 +74,7 @@ const entryPoint: sdk.ModuleEntryPoint = {
     name: 'history',
     fullName: 'History',
     homepage: 'https://botpress.io',
-    menuIcon: 'timeline'
+    menuIcon: 'history'
   }
 }
 
--- modules/history/src/hooks/before_incoming_middleware/appendIngoingMsg.js
+++
@@ -1,7 +0,0 @@
-const appendIngoingMsg = async () => {
-  if (event.type === 'text' || event.type === 'quick_reply') {
-    await bp.database('msg_history').insert({ msg_content: JSON.stringify(event) })
-  }
-}
-
-return appendIngoingMsg()
