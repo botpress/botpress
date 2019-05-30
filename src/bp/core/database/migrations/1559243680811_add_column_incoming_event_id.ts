bcae740186316ef957271f30e82cc19e2dfcfaba
Renaud Lainé <rnd.laine+github@gmail.com>
2019-05-30 15:28:47 -0400

    chore: add incoming event id column

---
+++ src/bp/core/database/migrations/1559243680811_add_column_incoming_event_id.ts
@@ -0,0 +1,16 @@
+/**
+ * Use as a template for migrations.
+ */
+
+import Knex from 'knex'
+
+export const up = async (knex: Knex): Promise<void> => {
+  return knex.schema.alterTable('web_messages', table => {
+    table.string('incomingEventId')
+  })
+}
+export const down = async (knex: Knex): Promise<void> => {
+  return knex.schema.alterTable('web_messages', table => {
+    table.dropColumn('incomingEventId')
+  })
+}
