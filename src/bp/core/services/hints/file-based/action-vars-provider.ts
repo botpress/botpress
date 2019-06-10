86d174398b17dd8ecd97e6897d5c6bb4022f4d27
Sylvain Perron <slvn.perron+github@gmail.com>
2019-06-10 10:13:45 -0400

    crawl only user-defined actions

--- src/bp/core/services/hints/file-based/action-vars-provider.ts
+++ src/bp/core/services/hints/file-based/action-vars-provider.ts
@@ -5,22 +5,33 @@ const REGEXES = [
   /(temp|session|user|bot).([A-Z0-9_]+?) =/gi //
 ]
 
+// We ignore automatically generated files from modules
+// So we just index files created by the users
+const IGNORE_CONTENT_PATTERN = /\/\/CHECKSUM:/i
+
 export default class ActionVariablesProvider implements FileBasedHintProvider {
   readonly filePattern = ['bots/*/actions/**/*.js', 'global/actions/**/*.js']
   readonly readFile = true
 
   indexFile(filePath: string, content: string): Hint[] {
+    if (IGNORE_CONTENT_PATTERN.test(content)) {
+      return []
+    }
+
     const hints: Hint[] = []
 
     for (const regex of REGEXES) {
       let matches: RegExpExecArray | null
       do {
         matches = regex.exec(content)
-        if (matches) {
+        if (matches && matches[2].length >= 3) {
           hints.push({
-            category: 'ACTION',
-            description: 'Variable action, found in ' + filePath,
-            name: `event.state.${matches[1]}.${matches[2]}`,
+            category: 'VARIABLES',
+            description: '',
+            source: 'Extracted from action files',
+            location: 'File: ' + filePath,
+            parentObject: 'event.state',
+            name: `${matches[1]}.${matches[2]}`,
             partial: false,
             scope: 'inputs'
           })
