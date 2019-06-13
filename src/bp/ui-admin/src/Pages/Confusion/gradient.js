159d909755e6286f141118ae33268ddbba600c9c
Maxime Laboissonniere <maximelaboissonniere@gmail.com>
2019-06-12 21:27:23 -0400

    refactored gradient

--- src/bp/ui-admin/src/Pages/Confusion/gradient.js
+++ src/bp/ui-admin/src/Pages/Confusion/gradient.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 const gradient = {
   0: [255, 0, 0],
   0.25: [255, 128, 0],
@@ -6,30 +8,34 @@ const gradient = {
   1: [144, 238, 144]
 }
 
-export function getColorByPercent(percent) {
-  const low = Math.max(0, Math.floor(percent / 0.25) * 0.25)
-  const high = Math.min(1, Math.ceil(percent / 0.25) * 0.25)
-  const ratio = Math.max(0, Math.min(1, (high - percent) / 0.25))
-  return pickHex(gradient[low], gradient[high], ratio)
-}
+const componentToHex = c => (c.length == 1 ? '0' + c : c)
+
+const curriedMultiplier = _.curry(_.multiply)
+
+const rgbToHex = colors =>
+  `#${colors
+    .map(color => color.toString(16))
+    .map(componentToHex)
+    .join('')}`
 
 function pickHex(color1, color2, weight) {
   const p = weight
   const w = p * 2 - 1
+
   const w1 = (w / 1 + 1) / 2
   const w2 = 1 - w1
-  return rgbToHex(
-    Math.round(color1[0] * w1 + color2[0] * w2),
-    Math.round(color1[1] * w1 + color2[1] * w2),
-    Math.round(color1[2] * w1 + color2[2] * w2)
+
+  const colors = _.zipWith(color1.map(curriedMultiplier(w1)), color2.map(curriedMultiplier(w2)), (a, b) =>
+    Math.round(a + b)
   )
+
+  return rgbToHex(colors)
 }
 
-function componentToHex(c) {
-  const hex = c.toString(16)
-  return hex.length == 1 ? '0' + hex : hex
-}
+export function getColorByPercent(percent) {
+  const low = Math.max(0, Math.floor(percent / 0.25) * 0.25)
+  const high = Math.min(1, Math.ceil(percent / 0.25) * 0.25)
+  const ratio = Math.max(0, Math.min(1, (high - percent) / 0.25))
 
-function rgbToHex(r, g, b) {
-  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
+  return pickHex(gradient[low], gradient[high], ratio)
 }
