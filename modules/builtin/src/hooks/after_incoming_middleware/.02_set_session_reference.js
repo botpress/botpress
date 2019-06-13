b04c7937ef3cde166b59873752c0a7386fcba70e
Renaud Lainé <rnd.laine+github@gmail.com>
2019-06-13 14:34:26 -0400

    fix set session reference missing payload

--- modules/builtin/src/hooks/after_incoming_middleware/.02_set_session_reference.js
+++ modules/builtin/src/hooks/after_incoming_middleware/.02_set_session_reference.js
@@ -1,9 +1,10 @@
 async function hook() {
-  if (event.type === 'session_reference' && event.signature && event.text) {
-    const verifySignature = await bp.security.getMessageSignature(event.text)
-    if (event.signature === verifySignature) {
+  if (event.type === 'session_reference' && event.payload.signature && event.payload.text) {
+    const verifySignature = await bp.security.getMessageSignature(event.payload.text)
+
+    if (event.payload.signature === verifySignature) {
       Object.assign(event.state.session, {
-        reference: event.text
+        reference: event.payload.text
       })
       event.setFlag(bp.IO.WellKnownFlags.SKIP_DIALOG_ENGINE, true)
       event.setFlag(bp.IO.WellKnownFlags.FORCE_PERSIST_STATE, true)
