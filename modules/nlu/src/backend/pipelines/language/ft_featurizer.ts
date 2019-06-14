370c7b66c9533e95799a4792eb6875a8ab4b1f2e
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-06-14 17:03:42 -0400

    get rid of numjs

--- modules/nlu/src/backend/pipelines/language/ft_featurizer.ts
+++ modules/nlu/src/backend/pipelines/language/ft_featurizer.ts
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import * as numjs from 'numjs'
 
 import { LanguageProvider } from '../../typings'
 
@@ -10,39 +9,50 @@ const debug = DEBUG('nlu')
 type Token = string
 type Document = Token[]
 
-export default class FTWordVecFeaturizer {
-  public static async getFeatures(
-    lang: string,
-    doc: Document,
-    docTfidf: _.Dictionary<number>,
-    langProvider: LanguageProvider
-  ): Promise<number[]> {
-    const defaultWordWeight = docTfidf['__avg__'] || 1
-    const vecs = await langProvider.vectorize(doc, lang)
+export const getSentenceFeatures = async (
+  lang: string,
+  doc: Document,
+  docTfidf: _.Dictionary<number>,
+  langProvider: LanguageProvider
+): Promise<number[]> => {
+  const vecs = await langProvider.vectorize(doc, lang)
 
-    debug(`get for '${lang}'`, { doc, gotten: vecs.map(x => x.length) })
+  debug(`get for '${lang}'`, { doc, gotten: vecs.map(x => x.length) })
 
-    if (!vecs.length) {
-      throw new Error(`Could not get sentence vectors (empty result)`)
-    }
-
-    // Compute sentence vector
-    // See https://github.com/facebookresearch/fastText/blob/26bcbfc6b288396bd189691768b8c29086c0dab7/src/fasttext.cc#L486s
-    const sentenceVec = numjs.zeros(vecs[0].length)
-    let totalWeight = 0
-    vecs.forEach((arr, i) => {
-      let sum = 0
-      arr.forEach(x => (sum += x * x))
-      const norm = Math.sqrt(sum)
-      if (norm > 0) {
-        const weight = docTfidf[doc[i]] || defaultWordWeight
-        totalWeight += weight
-        const arr2 = numjs.array(arr).divide(norm / weight)
-        sentenceVec.add(arr2, false)
-      }
-    })
-
-    sentenceVec.divide(totalWeight, false)
-    return sentenceVec.tolist() as number[]
+  if (!vecs.length) {
+    throw new Error(`Could not get sentence vectors (empty result)`)
   }
+
+  return computeSentenceEmbedding(vecs, doc, docTfidf)
+}
+
+// Taken from https://github.com/facebookresearch/fastText/blob/26bcbfc6b288396bd189691768b8c29086c0dab7/src/fasttext.cc#L486s
+export function computeSentenceEmbedding(wordVectors: number[][], doc: Document, docTfidf: Dic<number>): number[] {
+  const defaultWordWeight = docTfidf['__avg__'] || 1
+  let totalWeight = 0
+  let sentenceVec = new Array(wordVectors[0].length).fill(0)
+
+  wordVectors.forEach((vec, i) => {
+    const norm = computeNorm(vec)
+    if (norm > 0) {
+      const weight = docTfidf[doc[i]] || defaultWordWeight
+      totalWeight += weight
+      const weightedVec = scalarDivide(vec, norm / weight)
+      sentenceVec = vecAdd(sentenceVec, weightedVec)
+    }
+  })
+
+  return scalarDivide(sentenceVec, totalWeight)
+}
+
+function computeNorm(vec: number[]): number {
+  return Math.sqrt(vec.reduce((acc, next) => acc + Math.pow(next, 2), 0))
+}
+
+function vecAdd(vec1: number[], vec2: number[]): number[] {
+  return _.zip(vec1, vec2).map(([x, y]) => x + y)
+}
+
+function scalarDivide(vec: number[], divider: number): number[] {
+  return vec.map(x => x / divider)
 }
