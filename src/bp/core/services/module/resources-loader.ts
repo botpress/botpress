75fc46e2e7aed2f2d1b91f818598df3728cc4cd7
Sylvain Perron <slvn.perron+github@gmail.com>
2019-06-16 15:54:46 -0400

    chore(core): changed assets path to ./data/assets

--- src/bp/core/services/module/resources-loader.ts
+++ src/bp/core/services/module/resources-loader.ts
@@ -64,7 +64,7 @@ export class ModuleResourceLoader {
       },
       {
         src: `${this.modulePath}/assets`,
-        dest: `/assets/modules/${this.moduleName}`,
+        dest: `/data/assets/modules/${this.moduleName}`,
         ignoreChecksum: true
       },
       {
