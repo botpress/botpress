ea08f91764b13e6378c8ab3a1f91589121cce02e
Renaud Lainé <rnd.laine+github@gmail.com>
2019-06-19 16:51:33 -0400

    add example of form with contextual help

--- examples/proactive/README.md
+++ examples/proactive/README.md
@@ -10,4 +10,4 @@ You can learn more about proactive messages [here](https://botpress.io/docs/tuto
 1. [Enable](https://botpress.io/docs/main/module/#enabling-or-disabling-modules) the module
 1. Build the module: `cd modules/proactive && yarn && yarn build`
 1. Start Botpress: `cd ../.. && yarn start`
-1. Go to this url
+1. Go to http://localhost:3000/assets/modules/proactive-module/
--- examples/proactive/src/backend/index.ts
+++ examples/proactive/src/backend/index.ts
@@ -1,11 +1,16 @@
 import * as sdk from 'botpress/sdk'
 
+const botTemplates: sdk.BotTemplate[] = [
+  { id: 'proactive-bot', name: 'Proactive Bot', desc: `An bot used to show proactive examples` }
+]
+
 const onServerStarted = async (bp: typeof sdk) => {}
 const onServerReady = async (bp: typeof sdk) => {}
 
 const entryPoint: sdk.ModuleEntryPoint = {
   onServerStarted,
   onServerReady,
+  botTemplates,
   definition: {
     name: 'proactive-module',
     menuIcon: 'none',
