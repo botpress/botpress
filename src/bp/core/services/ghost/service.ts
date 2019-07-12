e9d2e51679225506b6ab88ebd9b3d8f3bf530e32
Sylvain Perron <slvn.perron+github@gmail.com>
2019-07-12 11:40:40 -0400

    fix(ghost): allow files up to 100mb

--- src/bp/core/services/ghost/service.ts
+++ src/bp/core/services/ghost/service.ts
@@ -19,7 +19,7 @@ import { PendingRevisions, ServerWidePendingRevisions, StorageDriver } from '.'
 import DBStorageDriver from './db-driver'
 import DiskStorageDriver from './disk-driver'
 
-const MAX_GHOST_FILE_SIZE = asBytes('20mb')
+const MAX_GHOST_FILE_SIZE = asBytes('100mb')
 
 @injectable()
 export class GhostService {
@@ -75,7 +75,7 @@ export class GhostService {
     process.BOTPRESS_EVENTS.on('after_bot_unmount', args => {
       if (args.botId === botId) {
         scopedGhost.events.removeAllListeners()
-      }
+    }
     })
 
     this._scopedGhosts.set(botId, scopedGhost)
@@ -182,7 +182,7 @@ export class ScopedGhostService {
     const fileName = this._normalizeFileName(rootFolder, file)
 
     if (content.length > MAX_GHOST_FILE_SIZE) {
-      throw new Error(`The size of the file ${fileName} is over the 20mb limit`)
+      throw new Error(`The size of the file ${fileName} is over the 100mb limit`)
     }
 
     await this.primaryDriver.upsertFile(fileName, content, recordRevision)
