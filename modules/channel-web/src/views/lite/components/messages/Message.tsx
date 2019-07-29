c55d2d296afd59b075c97e6e205685adf5566c2e
allardy <yann.allard1@gmail.com>
2019-07-29 15:41:50 -0400

    fix(webchat): fix for some messages which crashed the whole chat

--- modules/channel-web/src/views/lite/components/messages/Message.tsx
+++ modules/channel-web/src/views/lite/components/messages/Message.tsx
@@ -11,6 +11,14 @@ import * as Keyboard from '../Keyboard'
 import { Carousel, FileMessage, LoginPrompt, Text } from './renderer'
 
 class Message extends Component<MessageProps> {
+  state = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
   render_text(textMessage?: string) {
     const { text, markdown } = this.props.payload
 
@@ -110,6 +118,10 @@ class Message extends Component<MessageProps> {
   }
 
   render() {
+    if (this.state.hasError) {
+      return '* Cannot display message *'
+    }
+
     const type = this.props.type || (this.props.payload && this.props.payload.type)
     const wrappedType = this.props.payload && this.props.payload.wrapped && this.props.payload.wrapped.type
     const renderer = (this['render_' + type] || this.render_unsupported).bind(this)
@@ -117,7 +129,7 @@ class Message extends Component<MessageProps> {
 
     const rendered = renderer()
     if (rendered === null) {
-      return undefined
+      return null
     }
 
     const additionalStyle = (this.props.payload && this.props.payload['web-style']) || {}
