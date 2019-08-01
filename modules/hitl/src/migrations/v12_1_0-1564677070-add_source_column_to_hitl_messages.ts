6033aac9bcb92fb8849a9949f289f7c2c262baef
Renaud Lainé <rnd.laine+github@gmail.com>
2019-08-01 15:15:15 -0400

    catch migration error

--- modules/hitl/src/migrations/v12_1_0-1564677070-add_source_column_to_hitl_messages.ts
+++ modules/hitl/src/migrations/v12_1_0-1564677070-add_source_column_to_hitl_messages.ts
@@ -6,8 +6,10 @@ const migration: sdk.ModuleMigration = {
     description: 'Add source column to hitl_messages',
     type: 'database'
   },
-  up: async ({ bp, database, inversify }: MigrationOpts): Promise<sdk.MigrationResult> => {
-    await bp.database.schema.alterTable('hitl_messages', table => table.string('source'))
+  up: async ({ bp }: MigrationOpts): Promise<sdk.MigrationResult> => {
+    await bp.database.schema
+      .alterTable('hitl_messages', table => table.string('source'))
+      .catch(err => ({ success: false, message: err.message }))
     return { success: true, message: 'Source column created.' }
   }
 }
