2bd971473fc83b18c8717d6d1daf1580a50ab1bf
allardy <yann.allard1@gmail.com>
2019-08-14 22:08:39 -0400

    fix(admin): ui tweaks for bot creation

---
+++ src/bp/ui-admin/src/Pages/Workspace/Bots/ImportBotModal.tsx
@@ -0,0 +1,140 @@
+import { Button, Classes, Dialog, FileInput, FormGroup, InputGroup, Intent } from '@blueprintjs/core'
+import _ from 'lodash'
+import React, { Component } from 'react'
+
+import api from '../../../api'
+
+import { sanitizeBotId } from './CreateBotModal'
+
+interface Props {
+  onCreateBotSuccess: () => void
+  toggle: () => void
+  isOpen: boolean
+}
+
+const defaultState = {
+  botId: '',
+  error: null,
+  file: null,
+  fileValue: null,
+  idTaken: false,
+  isProcessing: false
+}
+
+class ImportBotModal extends Component<Props> {
+  private _form: HTMLFormElement | null = null
+
+  state = { ...defaultState }
+
+  importBot = async e => {
+    e.preventDefault()
+    if (this.isButtonDisabled) {
+      return
+    }
+    this.setState({ isProcessing: true })
+
+    try {
+      // @ts-ignore
+      await api.getSecured({ timeout: 30000 }).post(`/admin/bots/${this.state.botId}/import`, this.state.fileValue, {
+        headers: { 'Content-Type': 'application/tar+gzip' }
+      })
+
+      this.props.onCreateBotSuccess()
+      this.toggleDialog()
+    } catch (error) {
+      this.setState({ error: error.message, isProcessing: false })
+    }
+  }
+
+  checkIdAvailability = _.debounce(async () => {
+    if (!this.state.botId) {
+      return this.setState({ idTaken: false })
+    }
+
+    try {
+      const { data: idTaken } = await api.getSecured().get(`/admin/bots/${this.state.botId}/exists`)
+      this.setState({ idTaken })
+    } catch (error) {
+      this.setState({ error: error.message })
+    }
+  }, 500)
+
+  handleBotIdChanged = e => this.setState({ botId: sanitizeBotId(e.target.value) }, this.checkIdAvailability)
+
+  handleFileChanged = e => {
+    const fr = new FileReader()
+    fr.readAsArrayBuffer(e.target.files[0])
+    fr.onload = loadedEvent => {
+      this.setState({ fileValue: _.get(loadedEvent, 'target.result') })
+    }
+    this.setState({ file: e.target.value })
+  }
+
+  toggleDialog = () => {
+    this.setState({ ...defaultState })
+    this.props.toggle()
+  }
+
+  get isButtonDisabled() {
+    const { isProcessing, botId, fileValue, idTaken } = this.state
+    return isProcessing || !botId || !fileValue || idTaken || !this._form || !this._form.checkValidity()
+  }
+
+  render() {
+    return (
+      <Dialog
+        isOpen={this.props.isOpen}
+        icon="import"
+        onClose={this.toggleDialog}
+        transitionDuration={0}
+        title="Import bot from archive"
+      >
+        <form ref={form => (this._form = form)}>
+          <div className={Classes.DIALOG_BODY}>
+            <FormGroup
+              label={<span>Bot ID {this.state.idTaken && <span className="text-danger">Already in use</span>}</span>}
+              labelFor="text-input"
+              labelInfo="*"
+              helperText="This ID cannot be changed, so choose wisely. It will be displayed in the URL and your visitors can see it.
+              Special characters are not allowed. Minimum length: 4"
+            >
+              <InputGroup
+                id="text-input"
+                tabIndex={1}
+                placeholder="The ID of your bot"
+                intent={Intent.PRIMARY}
+                minLength={3}
+                value={this.state.botId}
+                onChange={this.handleBotIdChanged}
+                autoFocus={true}
+              />
+            </FormGroup>
+
+            <FormGroup
+              label="Bot Archive"
+              labelInfo="*"
+              labelFor="archive"
+              helperText="File must be compressed using tar-gzip (.tgz)"
+            >
+              <FileInput tabIndex={2} text={this.state.file || 'Choose file...'} onChange={this.handleFileChanged} />
+            </FormGroup>
+          </div>
+          <div className={Classes.DIALOG_FOOTER}>
+            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+              <Button
+                tabIndex={3}
+                type="submit"
+                text={this.state.isProcessing ? 'Please wait...' : 'Import Bot'}
+                onClick={this.importBot}
+                disabled={this.isButtonDisabled}
+                intent={Intent.PRIMARY}
+              />
+            </div>
+          </div>
+        </form>
+      </Dialog>
+    )
+  }
+}
+
+export default ImportBotModal
