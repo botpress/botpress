8d0838d053f04921cd2fc810efc53f4b9a933231
allardy <yann.allard1@gmail.com>
2019-08-27 12:31:31 -0400

    added missing slash

--- src/bp/ui-studio/src/web/util/EventBus.ts
+++ src/bp/ui-studio/src/web/util/EventBus.ts
@@ -60,10 +60,10 @@ class EventBus extends EventEmitter2 {
     const socketUrl = window['BP_SOCKET_URL'] || window.location.origin
     const transports = ['websocket', 'polling']
 
-    this.adminSocket = io(socketUrl + 'admin', { query, transports, path: `${window['ROOT_PATH']}/socket.io` })
+    this.adminSocket = io(socketUrl + '/admin', { query, transports, path: `${window['ROOT_PATH']}/socket.io` })
     this.adminSocket.on('event', this.dispatchSocketEvent)
 
-    this.guestSocket = io(socketUrl + 'guest', { query, transports, path: `${window['ROOT_PATH']}/socket.io` })
+    this.guestSocket = io(socketUrl + '/guest', { query, transports, path: `${window['ROOT_PATH']}/socket.io` })
     this.guestSocket.on('event', this.dispatchSocketEvent)
   }
 }
