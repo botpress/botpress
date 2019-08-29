f775f61b78a85bc987f5e7f6da7972a9bf478c25
allardy <yann.allard1@gmail.com>
2019-08-29 11:13:55 -0400

    updated ui

--- modules/extensions/src/views/lite/components/debugger/views/Error.tsx
+++ modules/extensions/src/views/lite/components/debugger/views/Error.tsx
@@ -1,17 +1,22 @@
-import { Colors, H5, Pre } from '@blueprintjs/core'
+import { Button, Collapse, Colors, H5, H6, Pre } from '@blueprintjs/core'
 import * as sdk from 'botpress/sdk'
-import React, { FC } from 'react'
+import _ from 'lodash'
+import React, { FC, useState } from 'react'
 import JSONTree from 'react-json-tree'
 
 import inspectorTheme from '../inspectorTheme'
 import style from '../style.scss'
 
 export const Error: FC<{ error: sdk.IO.EventError }> = ({ error }) => {
+  const [showStack, setShowStack] = useState(false)
+  const message = _.first(error.stacktrace && error.stacktrace.split('\n'))
+
   return (
     <div>
       {error.type === 'action-execution' && (
         <div className={style.subSection}>
           <H5 color={Colors.DARK_GRAY5}>Error executing action "{error.actionName}"</H5>
+          <p style={{ margin: 15 }}>{message}</p>
           <H5 color={Colors.DARK_GRAY5}>Arguments</H5>
           <Pre className={style.inspectorContainer}>
             <div className={style.inspector}>
@@ -29,8 +34,17 @@ export const Error: FC<{ error: sdk.IO.EventError }> = ({ error }) => {
       <br />
       {error.stacktrace && (
         <div>
-          <H5 color={Colors.DARK_GRAY5}>Stack trace</H5>
-          <Pre className={style.stacktrace}>{error.stacktrace}</Pre>
+          <H5 color={Colors.DARK_GRAY5}>
+            <Button
+              onClick={() => setShowStack(!showStack)}
+              text="Stack trace"
+              rightIcon={showStack ? 'chevron-down' : 'chevron-up'}
+              minimal={true}
+            />
+          </H5>
+          <Collapse isOpen={showStack}>
+            <Pre className={style.stacktrace}>{error.stacktrace}</Pre>
+          </Collapse>
         </div>
       )}
     </div>
