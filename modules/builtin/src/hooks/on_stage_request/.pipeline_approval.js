a46c91910b9d841536ae58cf26ce6b946ef89d64
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-09-23 15:52:33 -0400

    pipeline_approval.js

---
+++ modules/builtin/src/hooks/on_stage_request/.pipeline_approval.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/**
+ * Gives a short demonstration of a pipeline approval process.
+ *
+ * @param bp The botpress SDK
+ * @param bot The complete configuration of the bot
+ * @param users The list of users of that workspace (email, role)
+ * @param pipeline The list of configured stages
+ * @param hookResult The result of the hook which contains actions
+ */
+const stageChangeRequest = async () => {
+  const request_user = users.find(u => u.email == bot.pipeline_status.stage_request.requested_by)
+
+  // By default, we want to keep the bot in the current stage
+  hookResult.actions = []
+
+  const stageRequest = bot.pipeline_status.stage_request
+  stageRequest.approvers = stageRequest.approvers || []
+
+  const requestUserEmail = request_user.email
+  // If the current user is an approver, mark his approval
+  if (stageRequest.approvers.map(x => x.email).includes(requestUserEmail)) {
+    stageRequest.approvers.find(x => x.email === requestUserEmail).approved = true
+  }
+
+  // The status will be displayed in the bots list in the Workspace
+  stageRequest.status = `Approvals: ${stageRequest.approvers.filter(x => x.approved === true).length}/${stageRequest.approvers.length}`
+
+  bp.config.mergeBotConfig(bot.id, { pipeline_status: bot.pipeline_status })
+
+  // If all approvers have approved, move the bot to the next stage
+  if (stageRequest.approvers.filter(x => x.approved === false).length === 0) {
+    const currentStage = pipeline.find(x => x.id === bot.pipeline_status.current_stage.id)
+    hookResult.actions = [currentStage.action]
+  }
+
+  return
+}
+
+const _getApprovers = () => {
+  // Either hardcode approvers like this, or call your own service to retrieve approvers
+  return [
+    {
+      email: 'alice@acme.com',
+      approved: false
+    },
+    {
+      email: 'bob@acme.com',
+      approved: false
+    },
+    {
+      email: 'security@acme.com',
+      approved: false
+    }
+  ]
+}
+
+return stageChangeRequest()
