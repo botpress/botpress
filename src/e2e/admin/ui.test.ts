e293f8203ae5da52629ef308eb5e015e9c072047
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-10-07 14:18:18 -0400

    Cleanup

--- src/e2e/admin/ui.test.ts
+++ src/e2e/admin/ui.test.ts
@@ -1,6 +1,6 @@
 import { bpConfig } from '../../../jest-puppeteer.config'
 import { clickOn, expectMatch, fillField } from '../expectPuppeteer'
-import { expectAdminApiCallSuccess, expectCallSuccess, getTime } from '../utils'
+import { expectAdminApiCallSuccess, expectCallSuccess } from '../utils'
 
 describe('Admin - UI', () => {
   it('Load server license page', async () => {
@@ -33,42 +33,27 @@ describe('Admin - UI', () => {
   })
 
   it('Change user profile', async () => {
-    console.log(`${getTime()} Change user profile: clicking btn-menu`)
     await clickOn('#btn-menu')
-    console.log(`${getTime()} Change user profile: clicking btn-profile`)
     await clickOn('#btn-profile')
-    console.log(`${getTime()} Change user profile: filling input-first-name`)
     await fillField('#input-firstname', 'Bob')
-    console.log(`${getTime()} Change user profile: filling input-last-name`)
     await fillField('#input-lastname', 'Lalancette')
-    console.log(`${getTime()} Change user profile: Awaiting promise`)
     await Promise.all([
       expectCallSuccess(`${bpConfig.host}/api/v1/auth/me/profile`, 'POST'),
       await clickOn('#btn-submit')
     ])
-    console.log(`${getTime()} Change user profile: Clicking on cross`)
     await clickOn(".recipe-toaster svg[data-icon='cross']")
-    console.log(`${getTime()} Change user profile: Clicking on btn-menu`)
     await page.waitForFunction(() => {
       return document.querySelector('.bp3-overlay').childElementCount === 0
     })
-    await clickOn('#btn-menu')
-    console.log(`${getTime()} Change user profile: Expecting signed in`)
     await expectMatch('Signed in as Bob Lalancette')
   })
 
   it('Update password', async () => {
-    console.log(`${getTime()} Update password: clicking on btn-menu`)
     await clickOn('#btn-menu')
-    console.log(`${getTime()} Update password: clicking on btn-changepass`)
     await clickOn('#btn-changepass')
-    console.log(`${getTime()} Update password: filling input-password`)
     await fillField('#input-password', bpConfig.password)
-    console.log(`${getTime()} Update password: filling input-newpassword`)
     await fillField('#input-newPassword', bpConfig.password)
-    console.log(`${getTime()} Update password: filling input-confirmpassword`)
     await fillField('#input-confirmPassword', bpConfig.password)
-    console.log(`${getTime()} Update password: awaiting promise`)
     await Promise.all([
       expectCallSuccess(`${bpConfig.host}/api/v1/auth/login/basic/default`, 'POST'),
       clickOn('#btn-submit')
