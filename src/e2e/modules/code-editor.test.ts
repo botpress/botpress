480acb1df9bf2b192e46c74a191af7703d5d2169
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-10-09 09:49:10 -0400

    Remove logging

--- src/e2e/modules/code-editor.test.ts
+++ src/e2e/modules/code-editor.test.ts
@@ -5,8 +5,7 @@ import {
   expectBotApiCallSuccess,
   gotoStudio,
   triggerKeyboardShortcut,
-  waitForBotApiResponse,
-  getTime
+  waitForBotApiResponse
 } from '../utils'
 
 const waitForFilesToLoad = async () =>
@@ -25,23 +24,15 @@ describe('Module - Code Editor', () => {
   })
 
   it('Create new action', async () => {
-    console.log(`${getTime()} Create new action: answering dialog`)
     autoAnswerDialog('hello')
-    console.log(`${getTime()} Create new action: btn-add-action`)
     await clickOn('#btn-add-action')
-    console.log(`${getTime()} Create new action: btn-add-action-bot`)
     await clickOn('#btn-add-action-bot')
 
-    console.log(`${getTime()} Create new action: focus monaco-editor`)
     await page.focus('#monaco-editor')
-    console.log(`${getTime()} Create new action: click`)
     await page.mouse.click(469, 297)
-    console.log(`${getTime()} Create new action: wait for 500`)
     await page.waitFor(500) // Required so the editor is correctly focused at the right place
-    console.log(`${getTime()} Create new action: type hi`)
     await page.keyboard.type(`const lol = 'hi' //`)
 
-    console.log(`${getTime()} Create new action: keys`)
     await Promise.all([
       expectBotApiCallSuccess('mod/code-editor/save', 'POST'),
       expectBotApiCallSuccess('mod/code-editor/files', 'GET'),
