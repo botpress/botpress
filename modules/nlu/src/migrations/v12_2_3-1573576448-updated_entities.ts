aa084fbb1c59349bb314fc2492e6ba9953761105
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2019-11-12 16:10:49 -0500

    fixed migration description

--- modules/nlu/src/migrations/v12_2_3-1573576448-updated_entities.ts
+++ modules/nlu/src/migrations/v12_2_3-1573576448-updated_entities.ts
@@ -2,9 +2,9 @@ import * as sdk from 'botpress/sdk'
 
 const migration: sdk.ModuleMigration = {
   info: {
-    description: '',
-    target: 'core',
-    type: 'config'
+    description: 'Added missing fields in custom entities',
+    target: 'bot',
+    type: 'content'
   },
   up: async ({ bp, metadata }: sdk.ModuleMigrationOpts): Promise<sdk.MigrationResult> => {
     const migrateBotEntities = async (botId: string) => {
@@ -31,13 +31,12 @@ const migration: sdk.ModuleMigration = {
 
         await bpfs.upsertFile('./entities', fileName, JSON.stringify(entityDef, undefined, 2))
       }
-
-      if (metadata.botId) {
-        await migrateBotEntities(botId)
-      } else {
-        const bots = await bp.bots.getAllBots()
-        await Promise.map(bots.keys(), botId => migrateBotEntities(botId))
-      }
+    }
+    if (metadata.botId) {
+      await migrateBotEntities(metadata.botId)
+    } else {
+      const bots = await bp.bots.getAllBots()
+      await Promise.map(bots.keys(), botId => migrateBotEntities(botId))
     }
 
     return { success: true, message: 'Entities updated successfully' }
