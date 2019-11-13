ac753797a922e1867e82cf18ab0fb31a854879dd
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-13 11:39:41 -0500

    Functional component

--- docs/guide/website/pages/en/error.js
+++ docs/guide/website/pages/en/error.js
@@ -1,16 +1,14 @@
 const React = require('react')
 
-class Error extends React.Component {
-  render() {
-    return (
-      <div className="error">
-        <div className="container">
-          <div className="robot">🤖</div>
-          <div className="message">Oops! Page not found.</div>
-        </div>
+const Error = () => {
+  return (
+    <div className="error">
+      <div className="container">
+        <div className="robot">🤖</div>
+        <div className="message">Oops! Page not found.</div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 module.exports = Error
