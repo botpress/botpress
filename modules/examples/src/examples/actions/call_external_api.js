43d2589d81ece3e7915aee82e6559d269678e26f
allardy <yann.allard1@gmail.com>
2019-11-19 09:09:36 -0500

    small ajustment to example

--- modules/examples/src/examples/actions/call_external_api.js
+++ modules/examples/src/examples/actions/call_external_api.js
@@ -9,10 +9,27 @@ const callApi = async () => {
   // Call the Github API and fetch the latest releases
   const { data } = await axios.get('https://api.github.com/repos/botpress/botpress/releases')
 
-  // Display the latest version in logs
-  bp.logger.info(`Latest Botpress version is ${data[0].name}`)
+  // The first element returned is the most recent release
+  const mostRecentRelease = data[0]
 
-  // You can save the complete response in the session, then use it later
+  const latestVersion = mostRecentRelease.name
+  const releaseNotesUrl = mostRecentRelease.html_url
+
+  // Display the latest version in logs
+  bp.logger.info(`Latest Botpress version is ${latestVersion}`)
+
+  // Prepare the message
+  const message = {
+    type: 'text',
+    text: `Latest version: **${latestVersion}**\n\n[View release notes](${releaseNotesUrl})`,
+    // Markdown enables rich content, for example links or bold text. Otherwise, content will be displayed as-is
+    markdown: true
+  }
+
+  // Send the message to the user (note the array, since you can send multiple payloads in the same reply)
+  await bp.events.replyToEvent(event, [message])
+
+  // You could also save the complete response in the session, then use it later
   session.response = data
 }
 
