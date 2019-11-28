4790629bd713bd1dfe964e294484e8ad98dc92a1
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-28 16:20:34 -0500

    message endpoint

--- modules/uipath/src/backend/index.ts
+++ modules/uipath/src/backend/index.ts
@@ -1,6 +1,30 @@
 import * as sdk from 'botpress/sdk'
 
-const onServerStarted = async (bp: typeof sdk) => {}
+const onServerStarted = async (bp: typeof sdk) => {
+  const router = bp.http.createRouterForBot('uipath', {
+    checkAuthentication: false
+  })
+
+  router.post('/message', async (req, res) => {
+    try {
+      const body = req.body
+
+      await bp.events.replyToEvent(
+        {
+          channel: body.channel,
+          target: body.target,
+          botId: body.botId,
+          threadId: body.threadId
+        },
+        [body.message]
+      )
+
+      res.sendStatus(200)
+    } catch (err) {
+      res.status(400).send(err)
+    }
+  })
+}
 const onServerReady = async (bp: typeof sdk) => {}
 
 const entryPoint: sdk.ModuleEntryPoint = {
