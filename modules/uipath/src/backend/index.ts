136db827e7e1744ddbdb96a234465180cd064340
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-28 15:16:55 -0500

    Copied basic files

---
+++ modules/uipath/src/backend/index.ts
@@ -0,0 +1,19 @@
+import * as sdk from 'botpress/sdk'
+
+const onServerStarted = async (bp: typeof sdk) => {}
+const onServerReady = async (bp: typeof sdk) => {}
+
+const entryPoint: sdk.ModuleEntryPoint = {
+  onServerStarted,
+  onServerReady,
+  definition: {
+    name: 'basic-module',
+    menuIcon: 'none',
+    menuText: 'BasicExample',
+    noInterface: false,
+    fullName: 'BasicExample',
+    homepage: 'https://botpress.io'
+  }
+}
+
+export default entryPoint
