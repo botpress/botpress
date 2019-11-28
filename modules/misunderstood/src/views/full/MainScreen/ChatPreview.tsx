2a0fd954c28b3eac105178fecbedbc88553b75cd
Anton <demogoran@gmail.com>
2019-11-28 15:30:36 +0300

    fix for event.preview moved to backend; fix for skill choice results

--- modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
+++ modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
@@ -1,28 +1,31 @@
-import classnames from 'classnames'
-import React from 'react'
+import classnames from "classnames";
+import React from "react";
+import pick from "lodash/pick";
 
-import { ContextMessage } from '../../../types'
+import { ContextMessage } from "../../../types";
 
-import style from './style.scss'
+import style from "./style.scss";
 
 const ChatPreview = ({ messages }: { messages: ContextMessage[] }) => (
   <div className={style.chatPreview}>
-    {messages
-      .filter(message => message.preview)
-      .map((message, i) => (
-        <div
-          key={i}
-          className={classnames(style.chatPreviewMessage, {
-            [style.chatPreviewMessage_Incoming]: message.direction === 'incoming',
-            [style.chatPreviewMessage_Outgoing]: message.direction === 'outgoing',
-            [style.chatPreviewMessage_Current]: message.isCurrent
-          })}
-        >
-          <div className={style.chatPreviewAvatar}>{message.direction === 'incoming' ? 'U' : 'B'}</div>
-          <div className={style.chatPreviewText}>{(message.preview || '').replace(/<[^>]*>?/gm, '')}</div>
+    {messages.map((message, i) => (
+      <div
+        key={i}
+        className={classnames(style.chatPreviewMessage, {
+          [style.chatPreviewMessage_Incoming]: message.direction === "incoming",
+          [style.chatPreviewMessage_Outgoing]: message.direction === "outgoing",
+          [style.chatPreviewMessage_Current]: message.isCurrent
+        })}
+      >
+        <div className={style.chatPreviewAvatar}>
+          {message.direction === "incoming" ? "U" : "B"}
         </div>
-      ))}
+        <div className={style.chatPreviewText}>
+          {message.preview || message.payloadMessage || "Event(custom)"}
+        </div>
+      </div>
+    ))}
   </div>
-)
+);
 
-export default ChatPreview
+export default ChatPreview;
