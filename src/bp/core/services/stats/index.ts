0a4681c0edc67638b184981618232542266e411f
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-12-02 15:15:38 -0500

    Locking mechanism

--- src/bp/core/services/stats/index.ts
+++ src/bp/core/services/stats/index.ts
@@ -1,16 +1,28 @@
-import { injectable } from 'inversify'
+import { TYPES } from 'core/types'
+import { inject, injectable } from 'inversify'
 
+import { JobService } from '../job-service'
+
+const LOCK_RESOURCE = 'botpress:statsService'
 @injectable()
 export class StatsService {
-  constructor() {}
+  constructor(@inject(TYPES.JobService) private jobService: JobService) {}
 
   public start() {
     console.log('Stats Service Started!')
 
-    setInterval(this.run, 1000)
+    setInterval(this.run.bind(this), 5000)
   }
 
-  private run() {
-    console.log('running')
+  private async run() {
+    const lock = await this.jobService.acquireLock(LOCK_RESOURCE, 20000)
+    if (lock) {
+      this.sendStats()
+      await lock.unlock()
+    }
+  }
+
+  private sendStats() {
+    console.log('Sending stats')
   }
 }
