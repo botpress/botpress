be0679f0e48cdcfa5e9ddb0ec1a2f5b632320da3
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-12-02 15:21:16 -0500

    Get bot count

--- src/bp/core/services/stats/index.ts
+++ src/bp/core/services/stats/index.ts
@@ -1,28 +1,38 @@
 import { TYPES } from 'core/types'
 import { inject, injectable } from 'inversify'
 
+import { BotService } from '../bot-service'
 import { JobService } from '../job-service'
 
 const LOCK_RESOURCE = 'botpress:statsService'
 @injectable()
 export class StatsService {
-  constructor(@inject(TYPES.JobService) private jobService: JobService) {}
+  constructor(
+    @inject(TYPES.JobService) private jobService: JobService,
+    @inject(TYPES.BotService) private botService: BotService
+  ) {}
 
   public start() {
-    console.log('Stats Service Started!')
-
     setInterval(this.run.bind(this), 5000)
   }
 
   private async run() {
     const lock = await this.jobService.acquireLock(LOCK_RESOURCE, 20000)
     if (lock) {
-      this.sendStats()
+      await this.sendStats()
       await lock.unlock()
     }
   }
 
-  private sendStats() {
+  private async sendStats() {
     console.log('Sending stats')
+    const stats = await this.getStats()
+    console.log('Stats:', stats)
+  }
+
+  private async getStats() {
+    return {
+      botCount: (await this.botService.getBotsIds()).length
+    }
   }
 }
