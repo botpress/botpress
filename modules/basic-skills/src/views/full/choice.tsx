e69a911b511234ef383c7dcf91fb1257bd0a9404
Ahmed Ashour <asashour@yahoo.com>
2019-12-04 14:18:58 +0100

    fix(skill-choice): consider only selected options

--- modules/basic-skills/src/views/full/choice.tsx
+++ modules/basic-skills/src/views/full/choice.tsx
@@ -142,13 +142,12 @@ export class Choice extends React.Component<SkillProps<ChoiceData> & { bp: any }
   onContentChanged = (element, force = false) => {
     if (element && (force || element.id !== this.state.contentId)) {
       this.choices = _.get(element, 'formData.choices$' + this.props.contentLang) || []
-      const initialKeywords = element.id === this.state.contentId ? this.state.keywords : {}
       const keywords = this.choices.reduce((acc, v) => {
         if (!acc[v.value]) {
           acc[v.value] = _.uniq([v.value, v.title])
         }
         return acc
-      }, initialKeywords)
+      }, {})
       this.setState({ contentId: element.id, keywords: keywords })
     }
   }
@@ -182,8 +181,8 @@ export class Choice extends React.Component<SkillProps<ChoiceData> & { bp: any }
       this.choices && this.choices.length ? (
         this.renderMatchingSection()
       ) : (
-        <Callout intent={Intent.DANGER}>No choices available. Pick a content element that contains choices.</Callout>
-      )
+          <Callout intent={Intent.DANGER}>No choices available. Pick a content element that contains choices.</Callout>
+        )
 
     const contentPickerProps: any = {}
     const contentType = this.getContentType()
