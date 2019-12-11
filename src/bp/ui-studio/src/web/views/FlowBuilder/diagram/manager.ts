a1c16e616b8d74156cfd26fc4f8fc84c19c8a9a7
allardy <yann.allard1@gmail.com>
2019-12-11 12:07:37 -0500

    fix(flow): fix part 2 for the choice skill transitions

--- src/bp/ui-studio/src/web/views/FlowBuilder/diagram/manager.ts
+++ src/bp/ui-studio/src/web/views/FlowBuilder/diagram/manager.ts
@@ -118,6 +118,9 @@ export class DiagramManager {
         } else if (model.lastModified !== node.lastModified) {
           // Node has been modified
           this._syncNode(node, model, snapshot())
+          // TODO: Implement this correctly.
+          // Fixes an issue where links are at position 0,0 after adding a transition)
+          this.diagramEngine['flowBuilder'].checkForLinksUpdate()
         } else {
           // @ts-ignore
           model.setData({
@@ -368,6 +371,8 @@ export class DiagramManager {
         const link = new DefaultLinkModel()
         link.setSourcePort(sourcePort)
         link.setTargetPort(targetPort)
+
+        // console.log('existing', existingLink, node.id, targetNode.id, 'PORT', sourcePort, targetPort)
 
         if (existingLink) {
           link.setPoints(
