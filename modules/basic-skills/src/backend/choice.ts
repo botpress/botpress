0d6e92a803d870ae9b7c94354ae1c4a6e2553920
Oleksiy Pletnov <k4merton@gmail.com>
2020-01-18 18:42:33 +0200

    fix(studio): keep randomId on choice edit (resolve #2813)

--- modules/basic-skills/src/backend/choice.ts
+++ modules/basic-skills/src/backend/choice.ts
@@ -1,6 +1,5 @@
 import * as sdk from 'botpress/sdk'
 import _ from 'lodash'
-import nanoid from 'nanoid/generate'
 import yn from 'yn'
 
 const setup = async bp => {
@@ -34,7 +33,7 @@ const setup = async bp => {
 }
 
 const generateFlow = async (data: any, metadata: sdk.FlowGeneratorMetadata): Promise<sdk.FlowGenerationResult> => {
-  const randomId = nanoid('abcdefghijklmnopqrstuvwxyz0123456789', 10)
+  const { randomId } = data
   const hardRetryLimit = 10
   const nbMaxRetries = Math.min(Number(data.config.nbMaxRetries), hardRetryLimit)
   const repeatQuestion = yn(data.config.repeatChoicesOnInvalid)
