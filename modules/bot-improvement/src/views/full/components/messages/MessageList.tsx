9825f5dc8e125f17aaacb833ccf531c40d1f15f2
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2020-01-27 16:11:46 -0500

    Scroll flagged message into view

--- modules/bot-improvement/src/views/full/components/messages/MessageList.tsx
+++ modules/bot-improvement/src/views/full/components/messages/MessageList.tsx
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 import { FlaggedMessageGroup } from '../../../../backend/typings'
 import style from '../../style.scss'
@@ -7,10 +7,26 @@ import style from '../../style.scss'
 import Message from './Message'
 
 export const MessageList: FC<{ messageGroups: FlaggedMessageGroup[] }> = props => {
+  const { messageGroups } = props
+  const itemRefs = {}
+
+  useEffect(() => {
+    if (_.isEmpty(messageGroups)) {
+      return
+    }
+    const flaggedMessageGroup = messageGroups.find(g => g.flagged)
+    const ref = itemRefs[flaggedMessageGroup.incoming.id]
+    ref.scrollIntoView()
+  })
+
   return (
     <div>
-      {props.messageGroups.map((group, groupIdx) => (
-        <div key={groupIdx} className={group.flagged && style['messageGroup-flagged']}>
+      {messageGroups.map((group, groupIdx) => (
+        <div
+          ref={el => (itemRefs[group.incoming.id] = el)}
+          key={groupIdx}
+          className={group.flagged && style['messageGroup-flagged']}
+        >
           <Message message={group.incoming} />
           {group.replies.map((message, messageIdx) => (
             <Message key={`${groupIdx}${messageIdx}`} message={message} />
