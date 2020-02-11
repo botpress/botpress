4f1d132e7618c8412fe07813c6b346310c920df7
samuelmasse <samuelmasse4@gmail.com>
2020-02-11 13:39:04 -0500

    config images

--- src/bp/ui-studio/src/web/views/Config/index.tsx
+++ src/bp/ui-studio/src/web/views/Config/index.tsx
@@ -1,5 +1,7 @@
 import { Button, FileInput, FormGroup, InputGroup, Menu, MenuItem } from '@blueprintjs/core'
 import { ItemRenderer, Select } from '@blueprintjs/select'
+import { stat } from 'fs'
+import _ from 'lodash'
 import React, { Component } from 'react'
 
 import api from '../../../../../ui-admin/src/api'
@@ -11,18 +13,21 @@ const statusList = [
 ]
 
 class ConfigView extends Component {
+  /*
   initialFormState = {
     name: '',
     description: '',
     privacyPolicy: '',
     avatarUrl: '',
     coverPictureUrl: '',
-    category: undefined,
+    // category: undefined,
     website: '',
     phoneNumber: '',
     termsConditions: '',
     emailAddress: '',
-    status: ''
+    status: '',
+    selectedLanguages: [],
+    selectedDefaultLang: ''
   }
 
   state = {
@@ -36,32 +41,29 @@ class ConfigView extends Component {
   async componentDidMount() {
     const bots = await this.fetchBots()
     const languages = await this.fetchLanguages()
-    const bot = bots.find(x => x.id === this.state.botId)
 
+    const bot = bots.find(x => x.id === this.state.botId)
     const status = bot.disabled ? 'disabled' : bot.private ? 'private' : 'public'
 
-    const details = bot.details
-    this.setState({
+    this.initialFormState = {
       name: bot.name || '',
       description: bot.description || '',
-      website: details.website || '',
-      phoneNumber: details.phoneNumber || '',
-      termsConditions: details.termsConditions || '',
-      privacyPolicy: details.privacyPolicy || '',
-      emailAddress: details.emailAddress || '',
-      status: statusList.find(x => x.value === status),
-      languages
-      // status: statusList.find(x => x.value === status),
-      // category: this.state.categories.find(x => x.value === this.bot.category),
-      // avatarUrl: details.avatarUrl || '',
-      // coverPictureUrl: details.coverPictureUrl || ''
-      // selectedLanguages: this.bot.languages || [],
-      // selectedDefaultLang: this.bot.defaultLanguage
-    })
-  }
+      website: bot.details.website || '',
+      phoneNumber: bot.details.phoneNumber || '',
+      termsConditions: bot.details.termsConditions || '',
+      privacyPolicy: bot.details.privacyPolicy || '',
+      emailAddress: bot.details.emailAddress || '',
+      status,
+      selectedLanguages: bot.languages || [],
+      selectedDefaultLang: bot.defaultLanguage,
+      avatarUrl: bot.details.avatarUrl || '',
+      coverPictureUrl: bot.details.coverPictureUrl || ''
+    }
 
-  handleInputChanged = event => {
-    this.setState({ [event.target.name]: event.target.value })
+    this.setState({
+      ...this.initialFormState,
+      languages
+    })
   }
 
   async fetchBots() {
@@ -71,7 +73,11 @@ class ConfigView extends Component {
 
   async fetchLanguages() {
     const { data } = await api.getSecured().get('/admin/languages/available')
-    return data.languages
+    const languages = _.sortBy(data.languages, 'name').map(lang => ({
+      label: lang.name,
+      value: lang.code
+    }))
+    return languages
   }
 
   saveChanges = async () => {
@@ -79,8 +85,8 @@ class ConfigView extends Component {
       name: this.state.name,
       description: this.state.description,
       // category: category && category.value,
-      // defaultLanguage: selectedDefaultLang && selectedDefaultLang.value,
-      // languages: selectedLanguages && selectedLanguages.map(x => x.value),
+      defaultLanguage: this.state.selectedDefaultLang,
+      languages: this.state.selectedLanguages,
       details: {
         website: this.state.website,
         phoneNumber: this.state.phoneNumber,
@@ -89,13 +95,10 @@ class ConfigView extends Component {
         avatarUrl: this.state.avatarUrl,
         coverPictureUrl: this.state.coverPictureUrl,
         privacyPolicy: this.state.privacyPolicy
-      }
+      },
+      disabled: this.state.status === 'disabled',
+      private: this.state.status === 'private'
     }
-
-    bot.disabled = this.state.status === 'disabled' // && bot.defaultLanguage === this.bot.defaultLanguage // force enable if language changed
-    bot.private = this.state.status === 'private'
-    console.log(`disabled: ${bot.disabled}`)
-    console.log(`private: ${bot.private}`)
 
     await api
       .getSecured()
@@ -103,16 +106,58 @@ class ConfigView extends Component {
       .catch(err => this.setState({ error: err, isSaving: false }))
   }
 
+  handleInputChanged = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleCommunityLanguageChanged = lang => {
+    this.setState({ selectedDefaultLang: lang.target.value, selectedLanguages: [lang.target.value] })
+  }
+
+  handleImageFileChanged = async event => {
+    const targetProp = event.target.name
+    if (!event.target.files) {
+      return
+    }
+
+    if (!event.target.files[0].type.includes('image/')) {
+      this.setState({
+        error: `${targetProp} requires an image file`
+      })
+      return
+    }
+
+    const data = new FormData()
+    data.append('file', event.target.files[0])
+
+    if (this.state.error) {
+      this.setState({ error: null })
+    }
+
+    await api
+      .getSecured()
+      .post(`/bots/${this.state.botId}/media`, data, { headers: { 'Content-Type': 'multipart/form-data' } })
+      .then(response => {
+        this.setState({ [targetProp]: response.data.url })
+      })
+      .catch(err => {
+        this.setState({ error: err })
+      })
+  }
+
   render() {
     const container = {
       maxWidth: '1000px',
       margin: '30px auto'
     }
+    const block = {
+      display: 'block'
+    }
     const languages = []
     for (const lang of this.state.languages) {
       languages.push(
-        <option key={lang.code} value={lang.code}>
-          {lang.name}
+        <option key={lang.value} value={lang.value}>
+          {lang.label}
         </option>
       )
     }
@@ -124,6 +169,9 @@ class ConfigView extends Component {
         </option>
       )
     }
+
+    console.log(this.state.avatarUrl)
+
     return (
       <div style={container}>
         <form>
@@ -146,8 +194,15 @@ class ConfigView extends Component {
             />
           </FormGroup>
           <FormGroup label="Language" labelFor="language">
-            <div id="language" className="bp3-select">
-              <select>{languages}</select>
+            <div className="bp3-select">
+              <select
+                id="language"
+                name="language"
+                value={this.state.selectedLanguages}
+                onChange={this.handleCommunityLanguageChanged}
+              >
+                {languages}
+              </select>
             </div>
           </FormGroup>
           <FormGroup label="Website" labelFor="website">
@@ -186,20 +241,49 @@ class ConfigView extends Component {
             />
           </FormGroup>
           <FormGroup label="Bot Avatar" labelFor="avatar">
-            <FileInput id="avatar" text="Choose file" />
+            <FileInput
+              text="Choose file"
+              inputProps={{
+                id: 'avatarUrl',
+                name: 'avatarUrl',
+                accept: 'image/*',
+                onChange: this.handleImageFileChanged
+              }}
+            />
+            {this.state.avatarUrl !== this.initialFormState.avatarUrl && (
+              <p className="configUploadSuccess">
+                The bot avatar has been uploaded successfully. You need to save the form in order for the changes to
+                take effect.
+              </p>
+            )}
+            {this.state.avatarUrl && <img height={75} style={block} alt="avatar" src={this.state.avatarUrl} />}
           </FormGroup>
           <FormGroup label="Cover Picture" labelFor="cover">
-            <FileInput id="cover" text="Choose file" />
+            <FileInput
+              text="Choose file"
+              inputProps={{
+                id: 'coverPictureUrl',
+                name: 'coverPictureUrl',
+                accept: 'image/*',
+                onChange: this.handleImageFileChanged
+              }}
+            />
+            {this.state.coverPictureUrl !== this.initialFormState.coverPictureUrl && (
+              <p className="configUploadSuccess">
+                The cover picture has been uploaded successfully. You need to save the form in order for the changes to
+                take effect.
+              </p>
+            )}
+            {this.state.coverPictureUrl && (
+              <img height={200} style={block} className="coverImg" alt="cover" src={this.state.coverPictureUrl} />
+            )}
           </FormGroup>
           <Button text="Save changes" intent="primary" icon="floppy-disk" onClick={this.saveChanges} />
         </form>
       </div>
     )
   }
-
-  itemRenderer(item) {
-    return <MenuItem key={item} text={item} />
-  }
+  */
 }
 
 export default ConfigView
