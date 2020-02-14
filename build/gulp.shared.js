d86e6c365d2b86ab0cdc86b0cd325d9036643f28
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-02-14 11:54:56 -0800

    Foxed shared build

--- build/gulp.shared.js
+++ build/gulp.shared.js
@@ -1,4 +1,16 @@
+const gulp = require('gulp')
 const exec = require('child_process').exec
+const rimraf = require('gulp-rimraf')
+
+const build = () => {
+  gulp.task('build:shared', gulp.series([buildShared, clean]))
+
+  return gulp.series(['build:shared'])
+}
+
+const clean = () => {
+  return gulp.src('./out/bp/ui-shared/dist', { allowEmpty: true }).pipe(rimraf())
+}
 
 const watch = cb => {
   const shared = exec('yarn && yarn start', { cwd: 'src/bp/ui-shared' }, err => cb(err))
@@ -6,7 +18,7 @@ const watch = cb => {
   shared.stderr.pipe(process.stderr)
 }
 
-const build = cb => {
+const buildShared = cb => {
   const shared = exec('yarn && yarn build', { cwd: 'src/bp/ui-shared' }, err => cb(err))
   shared.stdout.pipe(process.stdout)
   shared.stderr.pipe(process.stderr)
