66b11849bb51489c529b853c325356dfcee94357
allardy <yann.allard1@gmail.com>
2020-02-25 15:16:50 -0500

    fix global typings

--- modules/code-editor/build.extras.js
+++ modules/code-editor/build.extras.js
@@ -1,7 +1,7 @@
 module.exports = {
   copyFiles: [
     'src/botpress.d.ts',
-    'src/typings/node.d.ts',
+    'src/typings/node.d.txt',
     'src/typings/es6include.txt',
     'src/typings/bot.config.schema.json',
     'src/typings/botpress.config.schema.json'
