c1a32311ec0f7f7338ced4907d4fcb385f723aa1
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2020-02-26 15:53:57 -0500

    Fix ts-ignores

--- src/bp/ui-studio/src/web/views/OneFlow/index.tsx
+++ src/bp/ui-studio/src/web/views/OneFlow/index.tsx
@@ -220,6 +220,8 @@ class FlowBuilder extends Component<Props, State> {
 
     return (
       <Container keyHandlers={keyHandlers} sidePanelWidth={320}>
+        {/*
+  // @ts-ignore */}
         <SidePanel
           readOnly={this.state.readOnly}
           mutexInfo={this.state.mutexInfo}
@@ -236,6 +238,7 @@ class FlowBuilder extends Component<Props, State> {
             flowPreview={this.state.flowPreview}
             showSearch={this.state.showSearch}
             hideSearch={this.hideSearch}
+            // @ts-ignore
             ref={el => {
               if (!!el) {
                 // @ts-ignore
