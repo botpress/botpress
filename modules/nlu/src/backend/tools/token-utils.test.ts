333a8f93248c63a075374078bb0c70e65ea68526
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2020-02-28 14:49:27 -0500

    remove dead code tests

--- modules/nlu/src/backend/tools/token-utils.test.ts
+++ modules/nlu/src/backend/tools/token-utils.test.ts
@@ -1,9 +1,7 @@
 import { LATIN_CHARSET } from './chars'
 import {
   isWord,
-  makeTokens,
   mergeSimilarCharsetTokens,
-  mergeSpecialCharactersTokens,
   processUtteranceTokens,
   restoreOriginalUtteranceCasing,
   SPACE
@@ -15,85 +13,6 @@ test('isWord', () => {
   expect(isWord('!')).toBeFalsy()
   expect(isWord('^jo!')).toBeFalsy()
   expect(isWord('?¿')).toBeFalsy()
-})
-
-// We might want to get rid of this once engine2 is done
-describe('Tokens generation', () => {
-  test('Make token objects from strings with sanitized text', async () => {
-    // arrange
-    const text = 'Never gonna let you Down'
-    //            012345678901234567890123
-    const stringTokens = ['never', 'gonna', 'let', 'you', 'down']
-
-    // act
-    const tokens = makeTokens(stringTokens, text)
-
-    // assert
-    expect(tokens).toHaveLength(5)
-
-    const expectedStarts = [0, 6, 12, 16, 20]
-    const actualStarts = tokens.map(t => t.start)
-    expect(actualStarts).toEqual(expectedStarts)
-
-    const expectedEnds = [5, 11, 15, 19, 24]
-    const actualEnds = tokens.map(t => t.end)
-    expect(actualEnds).toEqual(expectedEnds)
-  })
-
-  test('Make token objects from strings with sentencepiece like tokens', async () => {
-    // arrange
-    const text = 'Never gonna let you Down'
-    //            012345678901234567890123
-    const stringTokens = [`${SPACE}never`, `${SPACE}gon`, 'na', `${SPACE}let`, `${SPACE}you`, `${SPACE}down`] // \u2581 is ▁
-
-    // act
-    const tokens = makeTokens(stringTokens, text)
-
-    // assert
-    expect(tokens).toHaveLength(6)
-
-    const expectedStarts = [0, 6, 9, 12, 16, 20]
-    const actualStarts = tokens.map(t => t.start)
-    expect(actualStarts).toEqual(expectedStarts)
-
-    const expectedEnds = [5, 9, 11, 15, 19, 24]
-    const actualEnds = tokens.map(t => t.end)
-    expect(actualEnds).toEqual(expectedEnds)
-  })
-})
-
-// TODO remove this once engine 2 is done
-describe('Token Merging', () => {
-  test('Merge special Characters with numbers should merge all consecutive numbers', async () => {
-    // arrange
-    const text = '1234vanillaIce#4321'
-    const stringTokens = [SPACE + '1', '23', '4', 'vanilla', 'ice', '#', '43', '2', '1']
-    const tokens = makeTokens(stringTokens, text)
-
-    const numbers = '0123456789'.split('')
-
-    // act
-    const actualTokens = mergeSpecialCharactersTokens(tokens, numbers)
-
-    // assert
-    const expectedTokens = [SPACE + '1234vanillaice', '#', '4321'] // merging latin-based tokens only, merge only numbers
-    expect(actualTokens.map(t => t.value)).toEqual(expectedTokens)
-  })
-
-  test('Merge special Characters with pipes should merge all consecutive pipes', async () => {
-    // arrange
-    const text = '|||yes|||yes|||yes|||'
-    const base = ['yes', '|', '|', '|']
-    const stringTokens = [SPACE + '|', '|', '|', ...base, ...base, ...base]
-    const tokens = makeTokens(stringTokens, text)
-
-    // act
-    const actualTokens = mergeSpecialCharactersTokens(tokens, ['|'])
-
-    // assert
-    const expectedTokens = [SPACE + '|||', 'yes', '|||', 'yes', '|||', 'yes', '|||']
-    expect(actualTokens.map(t => t.value)).toEqual(expectedTokens)
-  })
 })
 
 describe('Raw token processing', () => {
