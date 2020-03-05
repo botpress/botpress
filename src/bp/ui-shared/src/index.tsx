8f253de686c77155ebe0fdcf9f7106ba5a4fd0f7
allardy <yann.allard1@gmail.com>
2020-03-05 16:50:33 -0500

    chore(dev): move dropdown component to ui-shared

--- src/bp/ui-shared/src/index.tsx
+++ src/bp/ui-shared/src/index.tsx
@@ -1,7 +1,9 @@
 import TooltipStyle from './style/tooltip.scss'
 import confirmDialog from './ConfirmDialog'
+import Dropdown from './Dropdown'
 import TreeView from './TreeView'
 
 exports.confirmDialog = confirmDialog
 exports.style = { TooltipStyle }
 exports.TreeView = TreeView
+exports.Dropdown = Dropdown
