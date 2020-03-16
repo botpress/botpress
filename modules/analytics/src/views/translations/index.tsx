ad5ee4c0fbc304e79d0f3f9fa55785504587ab07
Samuel Massé <samuelmasse4@gmail.com>
2020-03-16 18:02:51 -0400

    module translation (started)

---
+++ modules/analytics/src/views/translations/index.tsx
@@ -0,0 +1,17 @@
+import { langExtend, langInit } from 'botpress/shared'
+
+import ar from './ar.json'
+import en from './en.json'
+import es from './es.json'
+import fr from './fr.json'
+import pt from './pt.json'
+import ru from './ru.json'
+import uk from './uk.json'
+const translations = { en, fr, pt, es, ar, ru, uk }
+
+const initializeTranslations = () => {
+  langExtend(translations)
+  langInit()
+}
+
+export { initializeTranslations }
