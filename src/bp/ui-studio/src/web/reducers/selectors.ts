c556ad50d8ac2dcd93a858f577a36a3686190bea
allardy <yann.allard1@gmail.com>
2020-03-16 14:04:48 -0400

    only trigger nodes

--- src/bp/ui-studio/src/web/reducers/selectors.ts
+++ src/bp/ui-studio/src/web/reducers/selectors.ts
@@ -14,7 +14,7 @@ export const getAllFlows = createSelector([_getFlowsByName], flowsByName => {
 export const getFlowNamesList = createSelector([getAllFlows], flows => {
   const normalFlows = _.reject(flows, x => x.name && x.name.startsWith('skills/'))
   return normalFlows.map(x => {
-    const withTriggers = x.nodes.filter(x => x.triggers !== undefined || x.type === 'trigger')
+    const withTriggers = x.nodes.filter(x => x.type === 'trigger')
     return { name: x.name, label: x.label, triggerCount: withTriggers.length }
   })
 })
