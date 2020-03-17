b94ded95bc67a3a8502c1af3272a1ce60c84684d
allardy <yann.allard1@gmail.com>
2020-03-16 20:18:01 -0400

    chore(ndu): cleanup

--- modules/ndu/src/backend/ndu-engine.ts
+++ modules/ndu/src/backend/ndu-engine.ts
@@ -4,7 +4,6 @@ import * as sdk from 'botpress/sdk'
 import _ from 'lodash'
 
 import { BASE_DATA } from './base-data'
-import Database from './db'
 import { Features } from './typings'
 
 const debug = DEBUG('ndu').sub('processing')
@@ -71,7 +70,7 @@ export class UnderstandingEngine {
   trainer: sdk.MLToolkit.SVM.Trainer
   predictor: sdk.MLToolkit.SVM.Predictor
 
-  constructor(private bp: typeof sdk, private db: Database) {
+  constructor(private bp: typeof sdk) {
     this.trainer = new this.bp.MLToolkit.SVM.Trainer()
   }
 
@@ -124,35 +123,6 @@ export class UnderstandingEngine {
     }
   }
 
-  preprocessEvent(event: sdk.IO.IncomingEvent) {
-    const currentFlow = event.state?.context?.currentFlow ?? 'n/a'
-    const currentTopic = event.state?.session?.nduContext?.last_topic ?? 'n/a'
-    const currentNode = event.state?.context?.currentNode ?? 'n/a'
-    const isInMiddleOfFlow = currentFlow !== 'n/a'
-
-    return {
-      currentFlow,
-      currentNode,
-      currentTopic,
-      isInMiddleOfFlow
-    }
-  }
-
-  // getPredictionScores(predictions: sdk.NLU.Predictions) {
-  //   const results = _.flatMap(predictions, ({ confidence, intents }, key) =>
-  //     intents.map(intent => ({
-  //       topicName: key,
-  //       topicConfidence: confidence,
-  //       topicIntentName: intent.label,
-  //       topicIntentConfidence: intent.confidence,
-  //       topicIntentConfidenceAdjusted: confidence * intent.confidence,
-  //       topicIntentType: intent.label.startsWith('__qna__') ? 'QNA' : 'NLU'
-  //     }))
-  //   )
-
-  //   return _.orderBy(results, 'topicIntentConfidenceAdj', 'desc')
-  // }
-
   async processEvent(event: sdk.IO.IncomingEvent) {
     Object.assign(event, {
       ndu: {
@@ -165,31 +135,12 @@ export class UnderstandingEngine {
       return
     }
 
-    const { currentFlow, currentTopic, currentNode, isInMiddleOfFlow } = this.preprocessEvent(event)
+    const currentFlow = event.state?.context?.currentFlow ?? 'n/a'
+    const currentTopic = event.state?.session?.nduContext?.last_topic ?? 'n/a'
+    const currentNode = event.state?.context?.currentNode ?? 'n/a'
+    const isInMiddleOfFlow = currentFlow !== 'n/a'
+
     debug('Processing %o', { currentFlow, currentNode, isInMiddleOfFlow })
-
-    const actions = []
-
-    // // Need some magic to get the best topic (maybe it's the second one with better scoring intents? )
-    // const bestTopic = _.findKey(event.nlu.predictions, x => x.confidence > this.MIN_CONFIDENCE)
-
-    // // No active goal or contexts, so we'll add the top as context
-    // if (!(currentFlow && isInMiddleOfFlow) && !activeContexts.length && event.nlu.predictions) {
-    //   if (bestTopic) {
-    //     event.state.session.nluContexts = [...(event.state.session.nluContexts || []), { context: bestTopic, ttl: 3 }]
-    //     debug(`No active goal or context. Activate topic with highest confidence: ${bestTopic} `)
-    //   } else {
-    //     debug(`No active goal or context. Top context prediction too low `)
-    //   }
-    // }
-
-    // const predictionScores = this.getPredictionScores(event.nlu.predictions)
-
-    // const bestIntents = event.nlu.predictions?.[bestTopic]?.intents?.map(x => ({
-    //   name: x.label,
-    //   confidence: x.confidence,
-    //   context: bestTopic
-    // }))
 
     // // Overwrite the NLU detected intents
     // event.nlu.intent = bestIntents?.[0]
@@ -370,64 +321,6 @@ export class UnderstandingEngine {
 
     // TODO: NDU what to do if no action elected
     // TODO: NDU what to do if confused action
-
-    // return
-
-    // const possibleGoals = this._getGoalsWithCompletedTriggers(event)
-    // possibleGoals.length && debug(`Possible goals: %o`, possibleGoals)
-
-    // // If it's a QNA, we query the module to get the actions to execute
-    // if (event.nlu.intent?.name?.startsWith('__qna__')) {
-    //   debug(`Sending knowledge for %o`, event.nlu.intent)
-    // const qnaActions = await this.queryQna(event.nlu.intent.name, event)
-
-    // if (isInMiddleOfFlow && qnaActions.find(x => x.action === 'redirect')) {
-    //   qnaActions = qnaActions.filter(x => x.action === 'redirect')
-    //   // actions.push({ action: 'continue' })
-    // }
-
-    // actions.push(...qnaActions)
-    // }
-
-    // // When not actively in a flow and a trigger is active, redirect the user to a goal
-    // if (possibleGoals.length && !isInMiddleOfFlow) {
-    //   // Need some magic to pick the best goal based on parameters
-    //   const goal = possibleGoals[0]
-
-    //   debug(`Not currently in a flow, redirecting to goal %o `, goal)
-    //   const [topic] = goal.split('/')
-
-    //   event.state.session.nluContexts = [
-    //     { context: 'global', ttl: 2 },
-    //     { context: topic, ttl: 2 }
-    //   ]
-
-    //   actions.push({ action: 'startGoal', data: { goal } })
-    //   actions.push({ action: 'continue' })
-    // }
-
-    // // Nothing will be done, continue with the normal flow
-    // if (!actions.length) {
-    //   actions.push({ action: 'continue' })
-    // }
-
-    // event.ndu.actions = actions
-
-    // await this.db.insertData({
-    //   incomingEventId: event.id,
-    //   currentTopicName: currentTopic,
-    //   currentTopicGoal: currentGoal,
-    //   currentTopicLastActionName: undefined,
-    //   currentTopicLastActionSince: undefined,
-    //   nduDecisionActions: event.ndu.actions,
-    //   nduDecisionConfidence: event.nlu.intent?.confidence,
-    //   ..._.take(predictionScores, nbRankingIntent).reduce((acc, pred, idx) => {
-    //     return {
-    //       ...acc,
-    //       ..._.mapKeys(pred, (_val, key) => `ranking${idx + 1}${key}`)
-    //     }
-    //   }, {})
-    // })
   }
 
   private _getGoalsWithCompletedTriggers(event: sdk.IO.IncomingEvent) {
