3609590fdeacb0d420c75fe26292e87165275738
Yann Allard <42552874+allardy@users.noreply.github.com>
2020-03-19 15:29:23 -0400

    fix(qna): content language (#3113)

--- src/bp/ui-studio/src/web/views/OneFlow/sidePanel/TopicQnAEditor/EditTopicQnAModal.tsx
+++ src/bp/ui-studio/src/web/views/OneFlow/sidePanel/TopicQnAEditor/EditTopicQnAModal.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react'
 import InjectedModuleView from '~/components/PluginInjectionSite/module'
 import { BaseDialog, DialogBody } from '~/components/Shared/Interface'
+import withLanguage from '~/components/Util/withLanguage'
 
 interface Props {
   selectedTopic: string
+  contentLang: string
   isOpen: boolean
   toggle: () => void
 }
@@ -22,7 +24,7 @@ const EditTopicQnAModal: FC<Props> = props => {
         <InjectedModuleView
           moduleName="qna"
           componentName="LiteEditor"
-          contentLang="en"
+          contentLang={props.contentLang}
           extraProps={{ topicName: props.selectedTopic }}
         />
       </DialogBody>
@@ -30,4 +32,4 @@ const EditTopicQnAModal: FC<Props> = props => {
   )
 }
 
-export default EditTopicQnAModal
+export default withLanguage(EditTopicQnAModal)
