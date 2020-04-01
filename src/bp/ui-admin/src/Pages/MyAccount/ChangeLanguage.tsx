ca892e0364c148d2613b6a17e524c95b738e4a08
Samuel Massé <samuelmasse4@gmail.com>
2020-04-01 14:15:01 -0400

    fix

--- src/bp/ui-admin/src/Pages/MyAccount/ChangeLanguage.tsx
+++ src/bp/ui-admin/src/Pages/MyAccount/ChangeLanguage.tsx
@@ -1,34 +1,26 @@
-import { Button, Classes, Dialog, FormGroup, Intent } from '@blueprintjs/core'
-import { lang } from 'botpress/shared'
+import { Button, FormGroup, Intent } from '@blueprintjs/core'
+import { BaseDialog, DialogBody, DialogFooter, Dropdown, lang, Option } from 'botpress/shared'
 import React, { FC, useState } from 'react'
-import Select from 'react-select'
-import { toastFailure, toastSuccess } from '~/utils/toaster'
 
 interface Props {
   isOpen: boolean
   toggle: () => void
 }
 
-interface SelectItem {
-  value: string
-  label: string
-}
-
 const ChangeLanguage: FC<Props> = props => {
-  const languages: SelectItem[] = [
+  const languages: Option[] = [
     { value: 'en', label: 'English' },
     { value: 'fr', label: 'Français' }
   ]
 
-  const [language, setLanguage] = useState<SelectItem>(getCurrentLanguage())
+  const [language, setLanguage] = useState<Option>(getCurrentLanguage())
 
-  const submit = async event => {
-    event.preventDefault()
+  function submit() {
     localStorage.setItem('uiLanguage', language.value)
     window.location.reload()
   }
 
-  function getCurrentLanguage(): SelectItem {
+  function getCurrentLanguage(): Option {
     const locale = lang.locale()
     const option = languages.find(x => x.value === locale)
     if (option === undefined) {
@@ -42,28 +34,24 @@ const ChangeLanguage: FC<Props> = props => {
   }
 
   return (
-    <Dialog
+    <BaseDialog
       title={lang.tr('admin.changeLanguage')}
       icon="translate"
       isOpen={props.isOpen}
       onClose={props.toggle}
       transitionDuration={0}
       canOutsideClickClose={false}
+      onSubmit={submit}
     >
-      <form onSubmit={submit}>
-        <div className={Classes.DIALOG_BODY}>
-          <FormGroup label={lang.tr('admin.uiLanguage')} helperText={lang.tr('admin.uiLanguageHelper')}>
-            <Select options={languages} value={language} onChange={languageChanged} />
-          </FormGroup>
-        </div>
-
-        <div className={Classes.DIALOG_FOOTER}>
-          <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-            <Button id="btn-submit" type="submit" text={lang.tr('save')} tabIndex={4} intent={Intent.PRIMARY} />
-          </div>
-        </div>
-      </form>
-    </Dialog>
+      <DialogBody>
+        <FormGroup label={lang.tr('admin.uiLanguage')} helperText={lang.tr('admin.uiLanguageHelper')}>
+          <Dropdown items={languages} defaultItem={language} onChange={languageChanged} />
+        </FormGroup>
+      </DialogBody>
+      <DialogFooter>
+        <Button id="btn-submit" type="submit" text={lang.tr('save')} tabIndex={4} intent={Intent.PRIMARY} />
+      </DialogFooter>
+    </BaseDialog>
   )
 }
 
