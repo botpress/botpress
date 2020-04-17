02bd5c283315564f8986a17c3cc6aee88a04f0a3
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-04-17 08:17:31 -0700

    Add missing shortcut

--- src/bp/ui-shared/src/MainContainer/index.tsx
+++ src/bp/ui-shared/src/MainContainer/index.tsx
@@ -1,13 +1,19 @@
 import cx from 'classnames'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { HotKeys } from 'react-hotkeys'
 
 import style from './style.scss'
 import { MainContainerProps } from './typings'
 
 const MainContainer: FC<MainContainerProps> = props => {
+  const [showSidePanel, setShowSidePanel] = useState(true)
+  const toggleSidePanel = () => {
+    setShowSidePanel(!showSidePanel)
+  }
+
   const keyHandlers = {
-    ...(props.keyHandlers || {})
+    ...(props.keyHandlers || {}),
+    'toggle-sidepanel': toggleSidePanel
   }
 
   const children = React.Children.toArray(props.children)
@@ -15,7 +21,7 @@ const MainContainer: FC<MainContainerProps> = props => {
   return (
     <HotKeys handlers={keyHandlers} keyMap={props.keyMap || {}} className={style.fullsize} focused>
       <div className={style.container}>
-        <div className={style.leftSideBar}>{children[0]}</div>
+        {showSidePanel && <div className={style.leftSideBar}>{children[0]}</div>}
         <div className={cx(style.fullsize, style.mainContent, { [style.yOverflowScroll]: props.yOverflowScroll })}>
           {children.slice(1)}
         </div>
