8c31407e0757b8051412bd7969ca0f85e11b657e
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-04-21 14:25:09 -0700

    Fix read more translation + slight refactor (#3247)

--- modules/channel-web/src/views/lite/components/messages/renderer/Text.tsx
+++ modules/channel-web/src/views/lite/components/messages/renderer/Text.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import Linkify from 'react-linkify'
 
 import { Renderer } from '../../../typings'
@@ -9,11 +9,26 @@ import { renderUnsafeHTML } from '../../../utils'
  * @param {boolean} markdown Enable markdown parsing for the given text
  * @param {string} text The text to display
  * @param {boolean} escapeHTML Prevent unsafe HTML rendering when markdown is enabled
+ * @param {number} maxLength Enables show more button when text overflows limit
  */
 export const Text = (props: Renderer.Text) => {
-  let message = <p>{props.text}</p>
-  if (props.markdown) {
-    const html = renderUnsafeHTML(props.text, props.escapeHTML)
+  const [showMore, setShowMore] = useState(false)
+  const { maxLength, markdown, escapeHTML, intl } = props
+  let text = props.text
+  let hasShowMore
+
+  if (intl && maxLength && text.length > maxLength) {
+    hasShowMore = true
+    if (!showMore) {
+      const newMessage = text.substring(0, maxLength)
+
+      text = `${newMessage}${newMessage.substring(-1) !== '.' && '...'}`
+    }
+  }
+
+  let message = <p>{text}</p>
+  if (markdown) {
+    const html = renderUnsafeHTML(text, escapeHTML)
 
     message = <div dangerouslySetInnerHTML={{ __html: html }} />
   }
@@ -21,6 +36,23 @@ export const Text = (props: Renderer.Text) => {
   return (
     <Linkify properties={{ target: '_blank' }}>
       <div>{message}</div>
+
+      {hasShowMore && (
+        <button type="button" onClick={e => setShowMore(!showMore)} className="bpw-message-read-more">
+          {showMore &&
+            intl.formatMessage({
+              id: 'messages.showLess',
+              defaultMessage: 'Show Less'
+            })}
+          {!showMore &&
+            intl.formatMessage({
+              id: 'messages.readMore',
+              defaultMessage: 'Read More'
+            })}
+        </button>
+      )}
     </Linkify>
   )
 }
+
+export default Text
