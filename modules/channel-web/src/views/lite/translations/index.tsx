f13be0c25ffd9605150a19e2e1fd3d624f6d2ae9
Christian König <christian.koenig@etecture.de>
2020-04-22 14:09:40 +0200

    fix(translations): correct json import

--- modules/channel-web/src/views/lite/translations/index.tsx
+++ modules/channel-web/src/views/lite/translations/index.tsx
@@ -1,21 +1,21 @@
 import { addLocaleData } from 'react-intl'
 import localeAr from 'react-intl/locale-data/ar'
+import localeDe from 'react-intl/locale-data/de'
 import localeEn from 'react-intl/locale-data/en'
 import localeEs from 'react-intl/locale-data/es'
 import localeFr from 'react-intl/locale-data/fr'
 import localePt from 'react-intl/locale-data/pt'
 import localeRu from 'react-intl/locale-data/ru'
 import localeUk from 'react-intl/locale-data/uk'
-import localeDe from 'react-intl/locale-data/de'
 
 import ar from './ar.json'
+import de from './de.json'
 import en from './en.json'
 import es from './es.json'
 import fr from './fr.json'
 import pt from './pt.json'
 import ru from './ru.json'
 import uk from './uk.json'
-import de from './en.json'
 
 const defaultLocale = 'en'
 const translations = { en, fr, pt, es, ar, ru, uk, de }
@@ -30,7 +30,7 @@ const getUserLocale = (manualLocale?: 'browser' | string) => {
 }
 
 const initializeLocale = () => {
-  addLocaleData([...localeEn, ...localeFr, ...localePt, ...localeEs, ...localeAr, ...localeRu, ...localeUk])
+  addLocaleData([...localeEn, ...localeFr, ...localePt, ...localeEs, ...localeAr, ...localeRu, ...localeUk, ...localeDe])
 }
 
 export { initializeLocale, translations, defaultLocale, getUserLocale }
