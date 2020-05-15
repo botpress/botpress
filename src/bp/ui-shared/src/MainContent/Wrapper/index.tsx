6f6e08677f03f0cef1626e9712fc0f4fdda4257e
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-05-14 20:45:08 -0700

    feat(shared): right sidebar portal that pushes content (#3331)

--- src/bp/ui-shared/src/MainContent/Wrapper/index.tsx
+++ src/bp/ui-shared/src/MainContent/Wrapper/index.tsx
@@ -4,9 +4,11 @@ import style from './style.scss'
 import { WrapperProps } from './typings'
 
 const Wrapper: FC<WrapperProps> = props => {
+  const { childRef, children } = props
   return (
-    <div ref={ref => props.childRef?.(ref)} className={style.wrapper}>
-      {...props.children}
+    <div id="main-content-wrapper" ref={ref => childRef?.(ref)} className={style.wrapper}>
+      {...children}
+      <div className={style.rightSidebar} id="sidebar-portal"></div>
     </div>
   )
 }
