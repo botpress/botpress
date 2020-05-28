9414eff7dfd085557202159406c691b2560447cb
jijeshp <jijeshp920@gmail.com>
2020-05-29 00:35:05 +0400

    minor changes

--- modules/analytics/src/views/full/index.tsx
+++ modules/analytics/src/views/full/index.tsx
@@ -162,8 +162,11 @@ const Analytics: FC<any> = ({ bp }) => {
     // tslint:disable-next-line: no-floating-promises
     fetchAnalytics(state.selectedChannel, state.dateRange).then(metrics => {
       dispatch({ type: 'receivedMetrics', data: { dateRange: state.dateRange, metrics } })
-      const channels = _.uniq(_.map(metrics, 'channel'))
-      setChannels(prevState => _.uniq([...prevState, ...channels]))
+      const newChannels = metrics.map(x => {
+        return { value: x.channel, label: capitalize(x.channel) }
+      })
+      setChannels(_.uniq([...channels, ...newChannels]))
+
     })
 
     /* Get the previous range data so we can compare them and see what changed */
