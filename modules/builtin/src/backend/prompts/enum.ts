83cc6f77a5b57f320b93be6a7f5df9550d69cd71
Samuel Massé <samuelmasse4@gmail.com>
2020-06-17 19:10:17 -0400

    custom prompt enum

--- modules/builtin/src/backend/prompts/enum.ts
+++ modules/builtin/src/backend/prompts/enum.ts
@@ -1,10 +1,14 @@
+import axios from 'axios'
 import { IO, Prompt, PromptConfig } from 'botpress/sdk'
+import * as sdk from 'botpress/sdk'
 
 class PromptEnum implements Prompt {
   private _entity: string
+  private _question: string
 
-  constructor({ entity }) {
+  constructor({ entity, question }) {
     this._entity = entity
+    this._question = question
   }
 
   extraction(event: IO.IncomingEvent): { value: string; confidence: number } | undefined {
@@ -24,6 +28,28 @@ class PromptEnum implements Prompt {
 
     return { valid: true }
   }
+
+  customPrompt = async (event: IO.OutgoingEvent, incomingEvent, bp: typeof sdk) => {
+    if (event.channel === 'web') {
+      const payloads = await bp.cms.renderElement('builtin_text', { type: 'text', text: this._question }, event)
+      const withoutTyping = payloads.filter((x: any) => x.type !== 'typing')
+
+      const res = await axios.get(`mod/nlu/entities/${this._entity}`, await bp.http.getAxiosConfigForBot(event.botId))
+      const entity = res.data
+      const replies = entity.occurrences.map(x => {
+        return { label: x.name, payload: x.name }
+      })
+
+      event.type = 'custom'
+      event.payload = {
+        type: 'custom',
+        module: 'channel-web',
+        component: 'QuickReplies',
+        quick_replies: replies,
+        wrapped: withoutTyping[0]
+      }
+    }
+  }
 }
 
 const config: PromptConfig = {
@@ -31,7 +57,8 @@ const config: PromptConfig = {
   label: 'Enum',
   valueType: 'string',
   params: {
-    entity: { label: 'Entity', type: 'string' }
+    entity: { label: 'Entity', type: 'string' },
+    question: { label: 'Question', type: 'string' }
   }
 }
 
