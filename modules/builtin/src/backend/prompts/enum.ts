dd3f6ccd4773ae241d8b57e3c560dd1958fafca9
Samuel Massé <samuelmasse4@gmail.com>
2020-06-18 12:39:37 -0400

    enum dropdown and choice

--- modules/builtin/src/backend/prompts/enum.ts
+++ modules/builtin/src/backend/prompts/enum.ts
@@ -5,10 +5,12 @@ import * as sdk from 'botpress/sdk'
 class PromptEnum implements Prompt {
   private _entity: string
   private _question: string
+  private _useDropdown: boolean
 
-  constructor({ entity, question }) {
+  constructor({ entity, question, useDropdown }) {
     this._entity = entity
     this._question = question
+    this._useDropdown = useDropdown
   }
 
   extraction(event: IO.IncomingEvent): { value: string; confidence: number } | undefined {
@@ -30,23 +32,35 @@ class PromptEnum implements Prompt {
   }
 
   customPrompt = async (event: IO.OutgoingEvent, incomingEvent, bp: typeof sdk) => {
-    if (event.channel === 'web') {
-      const payloads = await bp.cms.renderElement('builtin_text', { type: 'text', text: this._question }, event)
-      const withoutTyping = payloads.filter((x: any) => x.type !== 'typing')
+    if (this._useDropdown && event.channel === 'web') {
+      const {
+        data: { occurrences }
+      } = await axios.get(`mod/nlu/entities/${this._entity}`, await bp.http.getAxiosConfigForBot(event.botId))
 
-      const res = await axios.get(`mod/nlu/entities/${this._entity}`, await bp.http.getAxiosConfigForBot(event.botId))
-      const entity = res.data
-      const replies = entity.occurrences.map(x => {
+      const replies = occurrences.map(x => {
         return { label: x.name, payload: x.name }
       })
 
       event.type = 'custom'
-      event.payload = {
-        type: 'custom',
-        module: 'channel-web',
-        component: 'QuickReplies',
-        quick_replies: replies,
-        wrapped: withoutTyping[0]
+      if (replies.length <= 3) {
+        const payloads = await bp.cms.renderElement('builtin_text', { type: 'text', text: this._question }, event)
+        const withoutTyping = payloads.filter((x: any) => x.type !== 'typing')
+
+        event.payload = {
+          type: 'custom',
+          module: 'channel-web',
+          component: 'QuickReplies',
+          quick_replies: replies,
+          wrapped: withoutTyping[0]
+        }
+      } else {
+        event.payload = {
+          type: 'custom',
+          module: 'extensions',
+          message: this._question,
+          component: 'Dropdown',
+          options: replies
+        }
       }
     }
   }
@@ -58,7 +72,8 @@ const config: PromptConfig = {
   valueType: 'string',
   params: {
     entity: { label: 'Entity', type: 'string' },
-    question: { label: 'Question', type: 'string' }
+    question: { label: 'Question', type: 'string' },
+    useDropdown: { label: 'Use dropdown', type: 'boolean' }
   }
 }
 
