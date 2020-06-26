112b77cca55134c7df85326e29f3df4584ac5b0a
Francois <francois_levasseur@hotmail.com>
2020-06-26 10:43:07 -0400

    feat(nlu): train now button becomes retrain all when auto train is on

--- modules/nlu/src/views/full/common/TrainingControl.tsx
+++ modules/nlu/src/views/full/common/TrainingControl.tsx
@@ -2,14 +2,16 @@ import { NLUApi } from 'api'
 import React, { FC } from 'react'
 
 import style from './style.scss'
-import AutoTrainToggle from './AutoTrainToggle'
+import AutoTrainToggle, { AutoTrainObserver } from './AutoTrainToggle'
 import TrainNow from './TrainNow'
+
+const autoTrainObserver: AutoTrainObserver = { listeners: [] }
 
 const TrainingControl: FC<{ api: NLUApi; eventBus: any }> = ({ api, eventBus }) => {
   return (
     <div className={style.trainingControl}>
-      <AutoTrainToggle api={api} />
-      <TrainNow api={api} eventBus={eventBus} />
+      <AutoTrainToggle api={api} observer={autoTrainObserver} />
+      <TrainNow api={api} eventBus={eventBus} observer={autoTrainObserver} />
     </div>
   )
 }
