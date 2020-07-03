67e46ca8ecbd0cba3c4d1dd982c436ddf673dd70
Olivier Lachance <olivier.lachance89@gmail.com>
2020-07-03 13:06:46 -0400

    another batch of fixes & changes

--- src/bp/ui-admin/src/telemetry/client.ts
+++ src/bp/ui-admin/src/telemetry/client.ts
@@ -7,15 +7,13 @@ import uuid from 'uuid'
 
 import store from '../store'
 
-import { EventData, EventPackageInfoType, StoreInfoType, TelemetryPackage } from './telemetry_type'
+import { EventPackageInfoType, StoreInfoType, TelemetryPackage } from './telemetry_type'
 
 const telemetryPackageVersion = '1.0.0'
 const dataClusterVersion = '1.0.0'
 
-export const endpoint = 'https://telemetry.botpress.dev'
-
-export const corsConfig = {
-  baseURL: endpoint,
+export const axiosConfig = {
+  baseURL: 'https://telemetry.botpress.dev',
   headers: {
     withCredentials: false
   }
@@ -25,9 +23,9 @@ const storeInfos: StoreInfoType = {}
 
 const eventPackageInfo: EventPackageInfoType = {}
 
-const switchLock = (lockKey: string) => {
-  if (_.has(eventPackageInfo, lockKey)) {
-    eventPackageInfo[lockKey].locked = !eventPackageInfo[lockKey].locked
+const toggleLock = (eventName: string) => {
+  if (_.has(eventPackageInfo, eventName)) {
+    eventPackageInfo[eventName].locked = !eventPackageInfo[eventName].locked
   }
 }
 
@@ -44,32 +42,18 @@ const getEventTimeout = (event: string) => {
 }
 
 const addEventTimeout = (event: string, timeout: number) => {
-  setTimeout(() => switchLock(event), timeout)
+  setTimeout(() => toggleLock(event), timeout)
   window.localStorage.setItem(event, (timeout + moment().valueOf()).toString())
 }
 
-const checkStoreInfoReceived = () => {
-  let check = true
-  for (const infoName in storeInfos) {
-    check = check && getStoreInfo(infoName) !== '' && getStoreInfo(infoName) !== undefined
-  }
-  return check
-}
+const checkStoreInfoReceived = () => !Object.keys(storeInfos).find(info => _.isEmpty(storeInfos[info]))
 
-export const addStoreInfo = (name: string, pathInStore: string) => {
-  storeInfos[name] = {
-    storedInfo: '',
-    loadInfo: function() {
-      storeInfos[name].storedInfo = _.get(store.getState(), pathInStore)
-    }
-  }
-}
-
-export const addStoreInfoFormatted = (name: string, pathInStore: string, formatter: Function) => {
+export const addStoreInfo = (name: string, pathInStore: string, formatter?: Function) => {
   storeInfos[name] = {
     storedInfo: '',
     loadInfo: () => {
-      storeInfos[name].storedInfo = formatter(_.get(store.getState(), pathInStore))
+      const value = _.get(store.getState(), pathInStore)
+      storeInfos[name].storedInfo = formatter ? formatter(value) : value
     }
   }
 }
@@ -85,17 +69,17 @@ export const getStoreInfo = (name: string) => {
 export const addTelemetryEvent = (name: string, timeout: string, getPackage: Function) => {
   eventPackageInfo[name] = {
     locked: getEventTimeout(name) >= 0,
-    timeout: timeout,
-    getPackage: getPackage
+    timeout,
+    getPackage
   }
 }
 
 const checkTelemetry = async () => {
-  for (const event in eventPackageInfo) {
-    if (!eventPackageInfo[event].locked) {
-      switchLock(event)
+  for (const eventName in eventPackageInfo) {
+    if (!eventPackageInfo[eventName].locked) {
+      toggleLock(eventName)
 
-      await sendTelemetry(getTelemetryPackage(event, eventPackageInfo[event].getPackage()), event)
+      await sendTelemetry(getTelemetryPackage(eventName, eventPackageInfo[eventName].getPackage()), eventName)
     }
   }
 }
@@ -105,7 +89,7 @@ export const startTelemetry = () => {
 
   addStoreInfo('bp_release', 'version.currentVersion')
 
-  addStoreInfoFormatted('bp_license', 'license.licensing.isPro', (info: any) => {
+  addStoreInfo('bp_license', 'license.licensing.isPro', (info: any) => {
     return !!info ? 'pro' : 'community'
   })
 
@@ -121,7 +105,7 @@ export const startTelemetry = () => {
 
   for (const event in eventPackageInfo) {
     if (getEventTimeout(event) >= 0) {
-      setTimeout(() => switchLock(event), getEventTimeout(event))
+      setTimeout(() => toggleLock(event), getEventTimeout(event))
     }
   }
 
@@ -129,21 +113,18 @@ export const startTelemetry = () => {
     for (const infoName in storeInfos) {
       storeInfos[infoName].loadInfo()
     }
+  })
 
+  setInterval(() => {
     if (checkStoreInfoReceived()) {
       checkTelemetry().catch(err => {
-        console.log(err)
+        console.error(err)
       })
     }
-  })
+  }, ms('30s'))
 }
 
 const getTelemetryPackage = (event_type: string, data: object): TelemetryPackage => {
-  const baseCluster: EventData = {
-    schema: dataClusterVersion
-  }
-  const event_data = _.assign(baseCluster, data)
-
   return {
     schema: telemetryPackageVersion,
     uuid: uuid.v4(),
@@ -152,11 +133,14 @@ const getTelemetryPackage = (event_type: string, data: object): TelemetryPackage
     bp_license: getStoreInfo('bp_license'),
     event_type: event_type,
     source: 'client',
-    event_data: event_data
+    event_data: {
+      schema: dataClusterVersion,
+      ...data
+    }
   }
 }
 
 const sendTelemetry = async (data: TelemetryPackage, event: string) => {
-  await axios.post('/', data, corsConfig)
+  await axios.post('/', data, axiosConfig)
   addEventTimeout(event, ms(eventPackageInfo[event].timeout))
 }
