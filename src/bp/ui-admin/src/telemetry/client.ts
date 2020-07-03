39cd136d2b415dae8d7a9ab8b8bb5b927f35d260
Olivier Lachance <olivier.lachance89@gmail.com>
2020-07-03 11:44:55 -0400

    Revert "another one fix"
    This reverts commit 2d44407fe88d28ba1a5b55dbc2c4cfcac2483fd7.

--- src/bp/ui-admin/src/telemetry/client.ts
+++ src/bp/ui-admin/src/telemetry/client.ts
@@ -131,7 +131,6 @@ export const startTelemetry = () => {
     }
 
     if (checkStoreInfoReceived()) {
-      unsubscribe()
       checkTelemetry().catch(err => {
         console.log(err)
       })
