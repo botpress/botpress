dbd5b9789080b331ed4f4105599425244ef2bc81
François Levasseur <francois_levasseur@hotmail.com>
2020-07-06 16:09:28 -0400

    feat(nlu): tokenize using vocab when s-piece is struggling

---
+++ modules/nlu/src/backend/tools/vocab-tokenizer.test.ts
@@ -0,0 +1,59 @@
+import vocabTokenizer from './vocab-tokenizer'
+
+test('vocab tokenizer should split', () => {
+  // arange
+  const vocab = ['shes', 'a', 'witch', 'burn', 'her']
+  const sentencepieceToken = 'shesa'
+  const fedVocabTokenizer = vocabTokenizer(vocab)
+
+  // act
+  const actual = fedVocabTokenizer(sentencepieceToken)
+
+  // assert
+  expect(actual).toHaveLength(2)
+  expect(actual[0]).toBe('shes')
+  expect(actual[1]).toBe('a')
+})
+
+test('vocab tokenizer should split with the correct token', () => {
+  // arange
+  const vocab = ['she', 'shes', 'is', 's', 'a', 'sa', 'witch', 'burn', 'her']
+  const sentencepieceToken = 'shesa'
+  const fedVocabTokenizer = vocabTokenizer(vocab)
+
+  // act
+  const actual = fedVocabTokenizer(sentencepieceToken)
+
+  // assert
+  expect(actual).toHaveLength(2)
+  expect(actual[0]).toBe('shes')
+  expect(actual[1]).toBe('a')
+})
+
+test('vocab tokenizer should not try more than combinations of length 2', () => {
+  // arange
+  const vocab = ['she', 'shes', 'is', 's', 'a', 'sa', 'witch', 'burn', 'her']
+  const sentencepieceToken = 'shesawitch'
+  const fedVocabTokenizer = vocabTokenizer(vocab)
+
+  // act
+  const actual = fedVocabTokenizer(sentencepieceToken)
+
+  // assert
+  expect(actual).toHaveLength(1)
+  expect(actual[0]).toBe('shesawitch')
+})
+
+test('vocab tokenizer should not split', () => {
+  // arange
+  const vocab = ['she', 'shes', 'is', 's', 'a', 'witch', 'burn', 'her']
+  const sentencepieceToken = 'arthur'
+  const fedVocabTokenizer = vocabTokenizer(vocab)
+
+  // act
+  const actual = fedVocabTokenizer(sentencepieceToken)
+
+  // assert
+  expect(actual).toHaveLength(1)
+  expect(actual[0]).toBe('arthur')
+})
