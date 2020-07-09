6cee485d957b2e798e01e2260b05ece94061e6f2
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-07-09 11:53:51 -0700

    Add tests

--- src/bp/ui-shared/src/FormFields/SuperInput/SuperInput.test.ts
+++ src/bp/ui-shared/src/FormFields/SuperInput/SuperInput.test.ts
@@ -1,9 +1,50 @@
-// $variable
-// $variable $variable2 {{event}} {{event2}}
-// {{event}}
-// $variable $variable2{{event}} {{event2}} $variable
-// $variable $variable2 {{event}}{{event2}} $variable
-// $variable $variable2 {{event}} {{event2}}$variable
-// $variable$variable2 {{event}} {{event2}} $variable
+import { convertToString, convertToTags } from './utils'
 
-export {}
+const convertTests = [
+  {
+    input: '$variable',
+    output: '[[{"value": "variable", "prefix": "$"}]]'
+  },
+  {
+    input: '{{event}}',
+    output: '[[{"value": "event", "prefix": "{{"}]]'
+  },
+  {
+    input: '$variable $variable2 {{event}} {{event2}}',
+    output: '[[{"value": "variable", "prefix": "$"}]] [[{"value": "variable2", "prefix": "$"}]] [[{"value": "event", "prefix": "{{"}]] [[{"value": "event2", "prefix": "{{"}]]'
+  },
+  {
+    input: '$variable $variable2{{event}} {{event2}} $variable',
+    output: '[[{"value": "variable", "prefix": "$"}]] [[{"value": "variable2", "prefix": "$"}]][[{"value": "event", "prefix": "{{"}]] [[{"value": "event2", "prefix": "{{"}]] [[{"value": "variable", "prefix": "$"}]]'
+  },
+  {
+    input: '$variable $variable2 {{event}}{{event2}} $variable',
+    output: '[[{"value": "variable", "prefix": "$"}]] [[{"value": "variable2", "prefix": "$"}]] [[{"value": "event", "prefix": "{{"}]][[{"value": "event2", "prefix": "{{"}]] [[{"value": "variable", "prefix": "$"}]]'
+  },
+  {
+    input: '$variable $variable2 {{event}} {{event2}}$variable',
+    output: '[[{"value": "variable", "prefix": "$"}]] [[{"value": "variable2", "prefix": "$"}]] [[{"value": "event", "prefix": "{{"}]] [[{"value": "event2", "prefix": "{{"}]][[{"value": "variable", "prefix": "$"}]]'
+  },
+  {
+    input: '$variable$variable2 {{event}} {{event2}} $variable',
+    output: '[[{"value": "variable", "prefix": "$"}]][[{"value": "variable2", "prefix": "$"}]] [[{"value": "event", "prefix": "{{"}]] [[{"value": "event2", "prefix": "{{"}]] [[{"value": "variable", "prefix": "$"}]]'
+  },
+  {
+    input: '$variable$variable2{{event}}{{event2}}$variable',
+    output: '[[{"value": "variable", "prefix": "$"}]][[{"value": "variable2", "prefix": "$"}]][[{"value": "event", "prefix": "{{"}]][[{"value": "event2", "prefix": "{{"}]][[{"value": "variable", "prefix": "$"}]]'
+  }
+]
+
+describe('Utility methods for super input', () => {
+  convertTests.forEach(({ input, output }) => {
+    test(`convertToTags test cases: ${input}`, () => {
+      expect(convertToTags(input)).toEqual(output)
+    })
+  })
+
+  convertTests.forEach(({ input, output }) => {
+    test(`convertToString test cases: ${output}`, () => {
+      expect(convertToString(output)).toEqual(input)
+    })
+  })
+})
