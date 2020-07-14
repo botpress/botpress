8b6b311880e27a3be66398dff8d66e1bdd4a686c
Said MARAR <mararsaid@gmail.com>
2020-07-14 03:06:56 +0200

    remove extra space Prettier

--- src/bp/core/services/workspace-service.ts
+++ src/bp/core/services/workspace-service.ts
@@ -73,7 +73,7 @@ export class WorkspaceService {
       throw new Error(`Specified workspace "${workspaceId}" doesn't exist`)
     }
 
-    if (!workspace.bots.includes(botId))  {
+    if (!workspace.bots.includes(botId)) {
       workspace.bots.push(botId)
     }
     return this.save(workspaces)
