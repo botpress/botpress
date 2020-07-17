4046a9fa7ed76cf766d5c51c1f1b5f290cba28cd
Francois <francois_levasseur@hotmail.com>
2020-07-17 09:15:35 -0400

    refac of check config

--- src/bp/ml/svm-js/core/config.ts
+++ src/bp/ml/svm-js/core/config.ts
@@ -7,83 +7,31 @@ import kernelTypes from './kernel-types'
 
 export function checkConfig(config: SvmConfig) {
   assert(config.kFold > 0, 'k-fold must be >= 1')
-  // parameter C used for C-SVC, epsilon-SVR, and nu-SVR
-  if (_.isString(config.svm_type)) {
-    config.svm_type = svmTypes[config.svm_type]
-  }
-  if (
-    config.svm_type === svmTypes.C_SVC ||
-    config.svm_type === svmTypes.EPSILON_SVR ||
-    config.svm_type === svmTypes.NU_SVR
-  ) {
-    if (_.isNumber(config.C)) {
-      config.C = [config.C as number]
-    }
-    assert(config.C && (config.C as number[]).length > 0, 'Require at least one value for C parameter')
-  } else {
+
+  if (![svmTypes.C_SVC, svmTypes.EPSILON_SVR, svmTypes.NU_SVR].includes(config.svm_type)) {
     config.C = []
   }
 
-  if (_.isString(config.kernel_type)) {
-    config.kernel_type = kernelTypes[config.kernel_type]
-  }
-  // parameter gamma used for POLY, RBF, and SIGMOID kernels
-  if (
-    config.kernel_type === kernelTypes.POLY ||
-    config.kernel_type === kernelTypes.RBF ||
-    config.kernel_type === kernelTypes.SIGMOID
-  ) {
-    if (_.isNumber(config.gamma)) {
-      config.gamma = [config.gamma as number]
-    }
-    assert(config.gamma && (config.gamma as number[]).length > 0, 'Require at least one value for gamma parameter')
-  } else {
+  if (![kernelTypes.POLY, kernelTypes.RBF, kernelTypes.SIGMOID].includes(config.kernel_type)) {
     config.gamma = []
   }
 
-  // parameter epsilon used for epsilon-SVR only
-  if (config.svm_type === svmTypes.EPSILON_SVR) {
-    if (_.isNumber(config.p)) {
-      config.p = [config.p as number]
-    }
-    assert(config.p && (config.p as number[]).length > 0, 'Require at least one value for epsilon parameter')
-  } else {
+  if (config.svm_type !== svmTypes.EPSILON_SVR) {
     config.p = []
   }
 
-  // parameter nu used for nu-SVC, one-class SVM, and nu-SVR
-  if (
-    config.svm_type === svmTypes.NU_SVC ||
-    config.svm_type === svmTypes.ONE_CLASS ||
-    config.svm_type === svmTypes.NU_SVR
-  ) {
-    if (_.isNumber(config.nu)) {
-      config.nu = [config.nu as number]
-    }
-    assert(config.nu && (config.nu as number[]).length > 0, 'Require at least one value for nu parameter')
-  } else {
+  if (![svmTypes.NU_SVC, svmTypes.ONE_CLASS, svmTypes.NU_SVR].includes(config.svm_type)) {
     config.nu = []
   }
 
-  // parameter degree used only for POLY kernel
-  if (config.kernel_type === kernelTypes.POLY) {
-    if (_.isNumber(config.degree)) {
-      config.degree = [config.degree as number]
-    }
-    assert(config.degree && (config.degree as number[]).length > 0, 'Require at least one value for degree parameter')
-  } else {
+  if (config.kernel_type !== kernelTypes.POLY) {
     config.degree = []
   }
 
-  // parameter r used for POLY kernel
-  if (config.kernel_type === kernelTypes.POLY || config.kernel_type === kernelTypes.SIGMOID) {
-    if (_.isNumber(config.coef0)) {
-      config.coef0 = [config.coef0 as number]
-    }
-    assert(config.coef0 && (config.coef0 as number[]).length > 0, 'Require at least one value for r parameter')
-  } else {
+  if (![kernelTypes.POLY, kernelTypes.SIGMOID].includes(config.kernel_type)) {
     config.coef0 = []
   }
+
   return config
 }
 
@@ -93,29 +41,20 @@ const defaultConf: SvmConfig = {
   nr_weight: 0,
   weight_label: [],
   weight: [],
-
-  degree: [2], // for POLY kernel
-  gamma: [0.001, 0.01, 0.5], // for POLY, RBF and SIGMOID kernels
-  coef0: [0.125, 0.5], // for POLY and SIGMOID kernels (coef0)
-
-  // SVM specific parameters
-  C: [1, 2], // cost for C_SVC, EPSILON_SVR and NU_SVR
-  nu: [0.01, 0.125, 0.5, 1], // for NU_SVC, ONE_CLASS and NU_SVR
-  p: [0.01, 0.125, 0.5, 1], // for EPSILON-SVR
-
-  // training options
-  kFold: 4, // k parameter for k-fold cross validation
-
-  normalize: true, // whether to use mean normalization during data pre-processing
-
-  reduce: false, // whether to use PCA to reduce dataset dimension during data pre-processing
-  // (see http://en.wikipedia.org/wiki/Principal_component_analysis)
-  retainedVariance: 0.99, // Define the acceptable impact on data integrity (if PCA activated)
-
-  eps: 1e-3, // stopping criteria
-  cache_size: 200, // cache size in MB
-  shrinking: true, // whether to use the shrinking heuristics
-  probability: false // whether to train a SVC or SVR model for probability estimates
+  degree: [2],
+  gamma: [0.001, 0.01, 0.5],
+  coef0: [0.125, 0.5],
+  C: [1, 2],
+  nu: [0.01, 0.125, 0.5, 1],
+  p: [0.01, 0.125, 0.5, 1],
+  kFold: 4,
+  normalize: true,
+  reduce: false,
+  retainedVariance: 0.99,
+  eps: 1e-3,
+  cache_size: 200,
+  shrinking: true,
+  probability: false
 }
 
 export function configMapper(config: SvmConfig): SvmParameters {
