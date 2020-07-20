6314b735548eac1602bdc1108fb8dce70e5c63e2
Francois <francois_levasseur@hotmail.com>
2020-07-20 17:53:03 -0400

    chore(mltoolkit): crf ml worker is handled at the same place as svm

--- src/bp/sdk/botpress.d.ts
+++ src/bp/sdk/botpress.d.ts
@@ -411,7 +411,7 @@ declare module 'botpress/sdk' {
     }
 
     export namespace CRF {
-      export interface Tagger {
+      export class Tagger {
         tag(xseq: Array<string[]>): { probability: number; result: string[] }
         open(model_filename: string): boolean
         marginal(xseq: Array<string[]>): { [label: string]: number }[]
@@ -430,12 +430,9 @@ declare module 'botpress/sdk' {
         labels: string[]
       }
 
-      export interface Trainer {
-        train(elements: DataPoint[], options: TrainerOptions): Promise<string>
+      export class Trainer {
+        train(elements: DataPoint[], options: TrainerOptions, debugCallback?: (msg: string) => void): Promise<string>
       }
-
-      export const createTrainer: () => Trainer
-      export const createTagger: () => Tagger
     }
 
     export namespace SentencePiece {
