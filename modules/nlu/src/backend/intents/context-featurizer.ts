351099baf3633c718106f2129dacc20085652b44
Francois <francois_levasseur@hotmail.com>
2020-07-22 16:51:11 -0400

    fix(nlu): make sure all word emebddings have same length

--- modules/nlu/src/backend/intents/context-featurizer.ts
+++ modules/nlu/src/backend/intents/context-featurizer.ts
@@ -12,9 +12,10 @@ function shouldConsiderToken(token: UtteranceToken): boolean {
 }
 
 export function getCtxFeatures(utt: Utterance, customEntities: string[]): number[] {
+  const entitiesOH = getEntitiesEncoding(utt, customEntities)
   const toks = utt.tokens.filter(shouldConsiderToken)
   if (_.isEmpty(toks)) {
-    return zeroes(utt.tokens[0].vector.length)
+    return zeroes(utt.tokens[0].vector.length + entitiesOH.length)
   }
 
   const totalWeight = toks.reduce((sum, t) => sum + Math.min(1, t.tfidf), 0) || 1
@@ -24,7 +25,6 @@ export function getCtxFeatures(utt: Utterance, customEntities: string[]): number
     return vectorAdd(sum, weightedVec)
   }, zeroes(utt.tokens[0].vector.length))
 
-  const entitiesOH = getEntitiesEncoding(utt, customEntities)
   const sentenceEmbedding = scalarDivide(weightedSum, totalWeight)
   return [...sentenceEmbedding, ...entitiesOH]
 }
