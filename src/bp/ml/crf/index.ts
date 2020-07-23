80f003a4be15a9b49e678a1458a67f24f1bbfaa3
François Levasseur <francois_levasseur@hotmail.com>
2020-07-23 17:32:39 -0400

    chore(mltoolkit): crf ml worker is handled at the same place as svm (#3631)

---
+++ src/bp/ml/crf/index.ts
@@ -0,0 +1,51 @@
+import tmp from 'tmp'
+import { MLToolkit } from 'botpress/sdk'
+
+import crfsuite, { Trainer as AddonTrainer, Tagger as AddonTagger } from './addon'
+
+export class Trainer implements MLToolkit.CRF.Trainer {
+  private trainer: AddonTrainer
+
+  constructor() {
+    this.trainer = new crfsuite.Trainer({ debug: true })
+  }
+
+  async train(
+    elements: MLToolkit.CRF.DataPoint[],
+    options: MLToolkit.CRF.TrainerOptions,
+    debugCallback?: (msg: string) => void
+  ): Promise<string> {
+    this.trainer.set_params(options)
+
+    for (const { features, labels } of elements) {
+      this.trainer.append(features, labels)
+    }
+
+    const crfModelFilename = tmp.fileSync({ postfix: '.bin' }).name
+
+    const emptyCallback = () => {}
+    await this.trainer.train_async(crfModelFilename, debugCallback ?? emptyCallback)
+
+    return crfModelFilename
+  }
+}
+
+export class Tagger implements MLToolkit.CRF.Tagger {
+  private tagger: AddonTagger
+
+  constructor() {
+    this.tagger = new crfsuite.Tagger()
+  }
+
+  tag(xseq: string[][]): { probability: number; result: string[] } {
+    return this.tagger.tag(xseq)
+  }
+
+  open(model_filename: string): boolean {
+    return this.tagger.open(model_filename)
+  }
+
+  marginal(xseq: string[][]): { [label: string]: number }[] {
+    return this.tagger.marginal(xseq)
+  }
+}
