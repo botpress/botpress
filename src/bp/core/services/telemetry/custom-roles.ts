89b9d06c903d2dfb9888bc9cdec08f26c2cc6750
chsir17 <sirois.christophe@gmail.com>
2020-07-29 15:29:38 -0400

    completed getRoles

--- src/bp/core/services/telemetry/custom-roles.ts
+++ src/bp/core/services/telemetry/custom-roles.ts
@@ -1,7 +1,5 @@
-import { parseActionInstruction } from 'common/action'
-import { BUILTIN_MODULES } from 'common/defaults'
 import LicensingService from 'common/licensing-service'
-import { buildSchema, TelemetryEvent } from 'common/telemetry'
+import { buildSchema } from 'common/telemetry'
 import Database from 'core/database'
 import { calculateHash } from 'core/misc/utils'
 import { TelemetryRepository } from 'core/repositories/telemetry'
@@ -10,12 +8,11 @@ import { inject, injectable } from 'inversify'
 import _ from 'lodash'
 
 import { GhostService } from '..'
-import { BotService } from '../bot-service'
-import { FlowService } from '../dialog/flow/service'
 import { JobService } from '../job-service'
 
 import { TelemetryStats } from './telemetry-stats'
 
+const DEFAULT_ROLES = ['dev', 'admin', 'editor']
 @injectable()
 export class RolesStats extends TelemetryStats {
   protected url: string
@@ -34,17 +31,30 @@ export class RolesStats extends TelemetryStats {
   }
 
   protected async getStats() {
-    console.log('got stats')
     return {
       ...buildSchema(await this.getServerStats(), 'server'),
       event_type: 'builtin_actions',
-      event_data: { schema: '1.0.0', roles: await this.getRolesConfigs() }
+      event_data: { schema: '1.0.0', roles: await this.getRoles() }
     }
   }
 
-  private async getRolesConfigs() {
-    const workspaces = await this.ghostService.global().readFileAsObject('/', 'workspaces.json')
-    console.log()
-    return {}
+  private async getRoles() {
+    const workspaces = await this.getWorkspaceConfig()
+
+    return workspaces.reduce((acc, workspace) => {
+      const roles = workspace.roles.map(role => ({
+        id: _.includes(DEFAULT_ROLES, role.id) ? role.id : calculateHash(role.id),
+        rules: role.rules
+      }))
+      return [...acc, { id: calculateHash(workspace.id), roles }]
+    }, [])
+  }
+
+  private async getWorkspaceConfig() {
+    try {
+      return (await this.ghostService.global().readFileAsObject('/', 'workspaces.json')) as any[]
+    } catch (error) {
+      return []
+    }
   }
 }
