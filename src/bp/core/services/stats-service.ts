28ae3c6f8fabd9a54de75dc30127bd28b6f05d49
chsir17 <sirois.christophe@gmail.com>
2020-07-31 10:03:11 -0400

    changesl lock name

--- src/bp/core/services/stats-service.ts
+++ src/bp/core/services/stats-service.ts
@@ -7,6 +7,7 @@ import { JobService } from './job-service'
 import { ActionsStats } from './telemetry/actions'
 import { RolesStats } from './telemetry/custom-roles'
 import { LegacyStats } from './telemetry/legacy-stats'
+import { SDKStats } from './telemetry/sdk-methods'
 
 const DB_REFRESH_LOCK = 'botpress:telemetryDB'
 const DB_REFRESH_INTERVAL = ms('15 minute')
@@ -18,13 +19,15 @@ export class StatsService {
     @inject(TYPES.TelemetryRepository) private telemetryRepo: TelemetryRepository,
     @inject(TYPES.ActionStats) private actionStats: ActionsStats,
     @inject(TYPES.LegacyStats) private legacyStats: LegacyStats,
-    @inject(TYPES.RolesStats) private rolesStats: RolesStats
+    @inject(TYPES.RolesStats) private rolesStats: RolesStats,
+    @inject(TYPES.SDKStats) private sdkStats: SDKStats
   ) {}
 
   public async start() {
     await this.actionStats.start()
     await this.legacyStats.start()
     await this.rolesStats.start()
+    await this.sdkStats.start()
 
     await this.refreshDB(DB_REFRESH_INTERVAL)
     setInterval(this.refreshDB.bind(this, DB_REFRESH_INTERVAL), DB_REFRESH_INTERVAL)
--- src/bp/core/services/telemetry/sdk-methods.ts
+++ src/bp/core/services/telemetry/sdk-methods.ts
@@ -5,13 +5,16 @@ import { calculateHash } from 'core/misc/utils'
 import { TelemetryRepository } from 'core/repositories/telemetry'
 import { TYPES } from 'core/types'
 import { inject, injectable } from 'inversify'
-import _ from 'lodash'
+import _, { toSafeInteger } from 'lodash'
 
 import { GhostService } from '..'
 import { BotService } from '../bot-service'
 import { JobService } from '../job-service'
 
 import { TelemetryStats } from './telemetry-stats'
+
+const { Parser } = require('typescript')
+
 @injectable()
 export class SDKStats extends TelemetryStats {
   protected url: string
@@ -27,10 +30,11 @@ export class SDKStats extends TelemetryStats {
   ) {
     super(ghostService, database, licenseService, jobService, telemetryRepo)
     this.url = process.TELEMETRY_URL
-    this.lock = 'botpress:telemetry-custom-roles'
+    this.lock = 'botpress:telemetry-SDK'
   }
 
   protected async getStats() {
+    console.log('test')
     return {
       ...buildSchema(await this.getServerStats(), 'server'),
       event_type: 'custom_roles',
@@ -42,7 +46,12 @@ export class SDKStats extends TelemetryStats {
     const bots = await this.botService.getBotsIds()
 
     for (const botId of bots) {
-      console.log(await this.ghostService.forBot(botId).directoryListing('/', '.js'))
+      const actions = await this.ghostService.forBot(botId).directoryListing('/actions', '*.js')
+
+      for (const action of actions) {
+        const ast = Parser.parse(await this.ghostService.forBot(botId).readFileAsString('/actions', action))
+        console.log(ast)
+      }
     }
   }
 }
