c7f542b5123dec3cc44b664b9cbb2e58d48e3812
allardy <yann.allard1@gmail.com>
2020-08-04 14:04:25 -0400

    fix(ndu): load and persist model

--- modules/ndu/src/backend/index.ts
+++ modules/ndu/src/backend/index.ts
@@ -8,11 +8,13 @@ import api from './api'
 import { dialogConditions } from './conditions'
 import { registerMiddleware } from './middleware'
 import { UnderstandingEngine } from './ndu-engine'
+import { getModel } from './training/trainer'
 import { MountedBots } from './typings'
 
 export const bots: MountedBots = {}
 
 export let conditions: sdk.Condition[] = []
+export let broadcastLoadModel: Function
 
 const onServerStarted = async (bp: typeof sdk) => {
   await registerMiddleware(bp, bots)
@@ -22,13 +24,28 @@ const onServerReady = async (bp: typeof sdk) => {
   // Must be in onServerReady so all modules have registered their conditions
   conditions = bp.dialog.getConditions()
   await api(bp)
+
+  const loadModel = async (botId: string, hash: string) => {
+    if (!bots[botId]) {
+      bp.logger.warn(`Can't load model for unmounted bot ${botId}`)
+      return
+    }
+
+    const model = await getModel(bp.ghost.forBot(botId), hash)
+    if (model) {
+      await bots[botId].loadModel(model)
+    }
+  }
+
+  broadcastLoadModel = await bp.distributed.broadcast(loadModel)
 }
 
 const onBotMount = async (bp: typeof sdk, botId: string) => {
   const botConfig = await bp.bots.getBotById(botId)
+
   if (botConfig.oneflow) {
     const config = (await bp.config.getModuleConfigForBot('ndu', botId)) as Config
-    bots[botId] = new UnderstandingEngine(bp, conditions, config)
+    bots[botId] = new UnderstandingEngine(bp, botId, conditions, config)
   }
 }
 
@@ -36,11 +53,11 @@ const onBotUnmount = async (bp: typeof sdk, botId: string) => {
   delete bots[botId]
 }
 
-const botTemplates: sdk.BotTemplate[] = [{ id: 'oneflow', name: 'Test bot', desc: `Test bot` }]
+const botTemplates: sdk.BotTemplate[] = [{ id: 'oneflow', name: 'Test bot', desc: 'Test bot' }]
 
 const onFlowChanged = async (bp: typeof sdk, botId: string, flow: sdk.Flow) => {
   if (bots[botId]) {
-    await bots[botId].invalidateWorkflows(botId)
+    await bots[botId].invalidateWorkflows()
   }
 }
 
