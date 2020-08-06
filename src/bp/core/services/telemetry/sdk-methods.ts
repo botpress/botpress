3f73acf1ad4ef3c55a5df3040029dc6b29d01c72
chsir17 <sirois.christophe@gmail.com>
2020-08-06 15:17:14 -0400

    refactor and added more metadata to sdk functions

--- src/bp/core/services/telemetry/sdk-methods.ts
+++ src/bp/core/services/telemetry/sdk-methods.ts
@@ -37,45 +37,84 @@ export class SDKStats extends TelemetryStats {
   }
 
   protected async getStats() {
-    return {
+    const temp = {
       ...buildSchema(await this.getServerStats(), 'server'),
       event_type: 'custom_roles',
       event_data: { schema: '1.0.0', SDKMethods: await this.SDKMethodsUsage() }
     }
+    return temp
   }
 
   private async SDKMethodsUsage() {
     const bots = await this.botService.getBotsIds()
-    const methods: any[] = []
 
-    for (const botId of bots) {
-      const actions = await this.ghostService.forBot(botId).directoryListing('/actions', '*.js')
+    const globalActionsNames = await this.ghostService
+      .global()
+      .directoryListing('/', 'actions/*.js')
+      .map(path => path.split('/').pop() || '')
 
-      for (const action of actions) {
-        const stringAction = await this.ghostService.forBot(botId).readFileAsString('/actions', action)
+    const globalActions = await this.buildUsages(globalActionsNames)
 
-        const ast = parse(stringAction, { allowReturnOutsideFunction: true })
+    const perBotActions = await Promise.map(bots, async botId => {
+      const botActionsNames = await this.ghostService.forBot(botId).directoryListing('/actions', '*.js')
+      const botActions = await this.buildUsages(botActionsNames, botId)
+      return botActions
+    })
 
-        const that = this
-        traverse(ast, {
-          enter(path) {
-            if (path.node.type == 'CallExpression') {
-              const callee = that.findCallee(path.node.callee)
-              methods.push(callee)
-            }
-          }
-        })
-      }
-    }
-    const sdkMethods = methods.filter(method => method.split('.')[0] === 'bp')
-    return _.countBy(sdkMethods)
+    return {}
   }
 
-  private findCallee(node) {
-    if (node.type === 'MemberExpression') {
-      return `${this.findCallee(node.object)}.${node.property.name}`
-    } else {
-      return node.name // breaks recursion
+  private async buildUsages(globalActionsNames: string[], botId?: string) {
+    const parseConfigs = { allowReturnOutsideFunction: true }
+
+    return await Promise.map(globalActionsNames, async fileName => {
+      const file = await this.readFileAsString('/actions', fileName, botId)
+      const functions = this.extractFunctions(parse(file, parseConfigs))
+      const usage = { fileName, usages: this.parseFunctions(functions) }
+      return botId ? { ...usage, botId } : usage
+    })
+  }
+
+  private parseFunctions(functions) {
+    const sdkFunctions = _.countBy(functions.filter(method => method.split('.')[0] === 'bp'))
+    return _.map(sdkFunctions, (count, fn) => {
+      const [, moduleName, functionName] = fn.split('.')
+      return { moduleName, functionName, count }
+    })
+  }
+
+  private extractFunctions(ast) {
+    const methods: any[] = []
+
+    const findCallee = function(node) {
+      if (node.type === 'MemberExpression') {
+        return `${findCallee(node.object)}.${node.property.name}`
+      } else {
+        return node.name // breaks recursion
+      }
+    }
+
+    const enter = function(path) {
+      if (path.node.type === 'CallExpression') {
+        const callee = findCallee(path.node.callee)
+        methods.push(callee)
+      }
+    }
+
+    traverse(ast, { enter })
+
+    return methods
+  }
+
+  private async readFileAsString(rootFolder: string, fileName: string, botId?: string) {
+    try {
+      if (botId) {
+        return await this.ghostService.forBot(botId).readFileAsString(rootFolder, fileName)
+      } else {
+        return await this.ghostService.global().readFileAsString(rootFolder, fileName)
+      }
+    } catch (error) {
+      return ''
     }
   }
 }
