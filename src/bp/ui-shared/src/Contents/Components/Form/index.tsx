9846101bb6492ffe64277203ecb37ff3da52f712
Maxime Cloutier <maxime.cloutier@botpress.com>
2020-08-12 10:56:43 -0700

    fix(studio): tooltips persistance (#3724)

--- src/bp/ui-shared/src/Contents/Components/Form/index.tsx
+++ src/bp/ui-shared/src/Contents/Components/Form/index.tsx
@@ -1,9 +1,9 @@
-import { Button, Checkbox, Position, Tooltip } from '@blueprintjs/core'
-import { FormMoreInfo } from 'botpress/sdk'
+import { Button, Checkbox } from '@blueprintjs/core'
 import cx from 'classnames'
 import _ from 'lodash'
 import React, { FC, Fragment, useEffect, useReducer, useRef, useState } from 'react'
 
+import ToolTip from '../../../../../ui-shared-lite/ToolTip'
 import { lang } from '../../../translations'
 import SuperInput from '../../../FormFields/SuperInput'
 import superInputStyle from '../../../FormFields/SuperInput/style.scss'
@@ -85,16 +85,15 @@ const Form: FC<FormProps> = ({
     }
 
     return field.superInput && !['text', 'text_array'].includes(field.type) ? (
-      <Tooltip
+      <ToolTip
         content={lang(
           isSuperInput(field, parent) ? 'superInput.convertToRegularInput' : 'superInput.convertToSmartInput'
         )}
-        position={Position.TOP}
       >
         <Button className={style.superInputBtn} small minimal onClick={() => toggleSuperInput(field, parent)}>
           {lang(field.label)}
         </Button>
-      </Tooltip>
+      </ToolTip>
     ) : (
       lang(field.label)
     )
