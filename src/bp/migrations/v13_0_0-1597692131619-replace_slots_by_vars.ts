d7a500f499abe45a99a99748afdc7546da503a40
Francois <francois_levasseur@hotmail.com>
2020-08-17 17:58:51 -0400

    fix: append examples for all slots

--- src/bp/migrations/v13_0_0-1597692131619-replace_slots_by_vars.ts
+++ src/bp/migrations/v13_0_0-1597692131619-replace_slots_by_vars.ts
@@ -26,8 +26,19 @@ class ComplexEntityCreator {
 
   constructor(private entities: sdk.NLU.EntityDefinition[]) {}
 
-  buildNewComplex(entities: string[]) {
-    entities = entities.filter(e => e !== 'any')
+  buildAny(name: string) {
+    this.newComplexs[name] = {
+      id: name,
+      name,
+      type: 'complex',
+      list_entities: [],
+      pattern_entities: [],
+      examples: []
+    }
+  }
+
+  buildNewComplex(allEntities: string[]) {
+    const entities = allEntities.filter(e => e !== 'any')
 
     const name = this.makeName(entities)
     if (!!this.newComplexs[name]) {
@@ -57,14 +68,17 @@ class ComplexEntityCreator {
   }
 
   appendExample(entity: string, example: string) {
-    this.newComplexs[entity]?.examples?.push(example)
+    const examples = this.newComplexs[entity]?.examples
+    if (examples && !examples.includes(example)) {
+      examples.push(example)
+    }
   }
 
   getNewComplexs(): sdk.NLU.EntityDefinition[] {
     return Object.values(this.newComplexs)
   }
 
-  private makeName(entities: string[]): string {
+  makeName(entities: string[]): string {
     return _(entities)
       .filter(e => e.length)
       .sortBy()
@@ -91,30 +105,40 @@ const migration: Migration = {
         const rawContent = await scopedGhost.readFileAsString(INTENT_DIR, intentFile)
         const intent: OldIntent = JSON.parse(rawContent)
         for (const slot of intent.slots) {
-          if (slot.entities.length > 1) {
+          if (slot.entities.length === 1 && slot.entities[0] === 'any') {
+            const entityName = slot.name
+            complexCreator.buildAny(entityName) // build entity from slot name
+            slot.entity = entityName
+          } else if (slot.entities.length > 1) {
             const newName = complexCreator.buildNewComplex(slot.entities)
-            slot.entities = [newName]
+            slot.entity = newName
+          } else {
+            slot.entity = slot.entities[0]
           }
-
-          slot.entity = slot.entities[0]
-          delete slot.entities
         }
 
-        const findEntity = function(slotName: string): string | undefined {
-          return intent.slots.find(s => s.name === slotName)?.entity
+        const findEntities = function(slotName: string): string[] | undefined {
+          return intent.slots.find(s => s.name === slotName)?.entities
         }
 
         const ALL_SLOTS_REGEX = /\[(.+?)\]\(([\w_\. :-]+)\)/gi // taken from 'nlu-core/utterance/utterance-parser.ts'
         for (const lang of Object.keys(intent.utterances)) {
           intent.utterances[lang] = intent.utterances[lang].map((utt: string) =>
             utt.replace(ALL_SLOTS_REGEX, function(_wholeMatch, slotExample: string, slotName: string) {
-              const slotEntity = findEntity(slotName)
-              if (slotEntity === 'any') {
-                complexCreator.appendExample(slotEntity, slotExample)
+              const slotEntities = findEntities(slotName)
+              if (slotEntities) {
+                const isOnlyAny = slotEntities.length === 1 && slotEntities[0] === 'any'
+                const newComplex = isOnlyAny ? slotName : complexCreator.makeName(slotEntities.filter(e => e !== 'any'))
+                complexCreator.appendExample(newComplex, slotExample)
               }
+
               return `$${slotName}`
             })
           )
+        }
+
+        for (const slot of intent.slots) {
+          delete slot.entities
         }
 
         for (const newComplex of complexCreator.getNewComplexs()) {
