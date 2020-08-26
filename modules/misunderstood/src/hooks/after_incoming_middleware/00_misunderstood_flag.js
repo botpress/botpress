56e2bbf75718e7e63ec7c20cb9b904866f2c4441
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2020-08-26 16:01:06 -0400

    fix(misunderstood) Ignore Choice skill keywords

--- modules/misunderstood/src/hooks/after_incoming_middleware/00_misunderstood_flag.js
+++ modules/misunderstood/src/hooks/after_incoming_middleware/00_misunderstood_flag.js
@@ -1,7 +1,27 @@
 const axios = require('axios')
 
 const flag = async () => {
-  if (event.type === 'text' && event.nlu.intent.name === 'none') {
+  const matchesQuickReply = async () => {
+    const { currentFlow } = event.state.context
+    if (currentFlow.startsWith('skills/choice')) {
+      const flow = await bp.ghost.forBot(event.botId).readFileAsObject('flows', currentFlow)
+
+      for (const kwList of Object.values(flow.skillData.keywords)) {
+        for (const kw of kwList) {
+          if (kw.toLowerCase() === event.preview.toLowerCase()) {
+            bp.logger.info(
+              `Misunderstood: event matches Choice Skill quick reply (preview: ${event.preview}, keyword: ${kw})`
+            )
+            return true
+          }
+        }
+      }
+    }
+
+    return false
+  }
+
+  if (event.type === 'text' && event.nlu.intent.name === 'none' && !(await matchesQuickReply())) {
     const data = {
       eventId: event.id,
       botId: event.botId,
