ede2217e6fea319dc9841cc3a9bd08d8219e47bc
Francois <francois_levasseur@hotmail.com>
2020-09-03 17:18:31 -0400

    added comment

--- src/bp/nlu-server/typings.ts
+++ src/bp/nlu-server/typings.ts
@@ -3,7 +3,7 @@ import * as sdk from 'botpress/sdk'
 export interface TrainInput {
   language: string
   topics: {
-    [topic: string]: sdk.NLU.IntentDefinition[]
+    [topic: string]: sdk.NLU.IntentDefinition[] // TODO: change this for new API spec
   }
   entities: sdk.NLU.EntityDefinition[]
   password: string
