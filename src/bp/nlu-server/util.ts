3b29d7303e64dfe7fd733805aa2697a4ebae845f
Francois <francois_levasseur@hotmail.com>
2020-09-08 10:11:50 -0400

    rm unused functions from utils

--- src/bp/nlu-server/util.ts
+++ src/bp/nlu-server/util.ts
@@ -1,20 +1,8 @@
-import { Request } from 'express'
 import _ from 'lodash'
 
 import { UnauthorizedError } from '../core/routers/errors'
 
 const debugAuth = DEBUG('api:auth')
-
-export const isAdminToken = (req, adminToken: string) => {
-  if (!adminToken || !adminToken.length) {
-    return true
-  }
-  if (!req.headers.authorization) {
-    return false
-  }
-  const [, token] = req.headers.authorization.split(' ')
-  return token === adminToken
-}
 
 export const authMiddleware = (secureToken: string, secondToken?: string) => (req, _res, next) => {
   if (!secureToken || !secureToken.length) {
@@ -45,14 +33,6 @@ export const authMiddleware = (secureToken: string, secondToken?: string) => (re
   next()
 }
 
-export const disabledReadonlyMiddleware = (readonly: boolean) => (_req, _res, next) => {
-  if (readonly) {
-    return next(new UnauthorizedError('API server is running in read-only mode'))
-  }
-
-  next()
-}
-
 export const handleUnexpectedError = (err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const errorCode = err.errorCode || 'BP_000'
@@ -72,8 +52,4 @@ export const handleErrorLogging = (err, req, res, next) => {
   }
 
   next(err)
-}
-
-export type RequestWithLang = Request & {
-  language?: string
 }
