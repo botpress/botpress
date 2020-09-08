97f315d07ea4514d48b42069ff74191192f09147
allardy <yann.allard1@gmail.com>
2020-09-08 13:29:36 -0400

    fix label

--- src/bp/ui-studio/src/web/views/OneFlow/diagram/nodes/ExecuteContents/index.tsx
+++ src/bp/ui-studio/src/web/views/OneFlow/diagram/nodes/ExecuteContents/index.tsx
@@ -10,16 +10,23 @@ interface Props {
   editNodeItem: (node: BlockModel, index: number) => void
 }
 
-const ExecuteContents: FC<Props> = ({ node, editNodeItem }) => (
-  <div className={style.contentsWrapper}>
-    <div className={style.contentWrapper}>
-      <div className={style.content} onClick={() => editNodeItem(node, 0)}>
-        {node.execute.actionName === '__newAction'
-          ? lang.tr('module.ndu.conditions.customCode')
-          : node.execute.actionName}
+const ExecuteContents: FC<Props> = ({ node, editNodeItem }) => {
+  let actionName = lang.tr('studio.flow.node.noActionSelected')
+
+  const nodeActionName = node.execute?.actionName
+  if (nodeActionName) {
+    actionName = nodeActionName === '__newAction' ? lang.tr('module.ndu.conditions.customCode') : nodeActionName
+  }
+
+  return (
+    <div className={style.contentsWrapper}>
+      <div className={style.contentWrapper}>
+        <div className={style.content} onClick={() => editNodeItem(node, 0)}>
+          {actionName}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ExecuteContents
