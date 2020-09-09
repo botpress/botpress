2bd3ea7295f0733a1dae27c260dd2b0c0dc73207
allardy <yann.allard1@gmail.com>
2020-09-09 12:39:09 -0400

    fix(builtin): disable old templates

--- modules/builtin/src/backend/index.ts
+++ modules/builtin/src/backend/index.ts
@@ -33,7 +33,8 @@ const onServerStarted = async (bp: typeof sdk) => {
 const entryPoint: sdk.ModuleEntryPoint = {
   onServerStarted,
   variables: [BoxedDate, BoxedBoolean, BoxedNumber, BoxedString, BoxedEnum, BoxedPattern, BoxedComplex],
-  botTemplates,
+  // TODO: remove templates
+  // botTemplates,
   translations: { en, fr },
   prompts,
   definition: {
