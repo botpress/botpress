741504a81fdc8d28c3eeafc6699a68e243ce4f1f
François Levasseur <francois_levasseur@hotmail.com>
2020-09-09 12:32:51 -0400

    fix(nlu): log error stack trace and botId in NLU (#3921)

---
+++ src/bp/ml/error-utils.ts
@@ -0,0 +1,26 @@
+import _ from 'lodash'
+
+export type ErrorMessage = { message: string; stack?: string }
+
+export function serializeError(err: any): ErrorMessage {
+  if (err instanceof Error) {
+    const { message, stack } = err
+    return { message, stack }
+  }
+
+  if (_.isString(err)) {
+    return { message: err }
+  }
+
+  if (_.isObject(err)) {
+    return { message: JSON.stringify(err, null, 2) }
+  }
+
+  return { message: '' }
+}
+
+export function deserializeError(err: ErrorMessage): Error {
+  const newErr = new Error(err.message)
+  newErr.stack = err.stack
+  return newErr
+}
