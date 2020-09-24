4a4737c24f0851bcf4103f9203b435ae5e51afe8
Maxime Cloutier <maxi.cloutier@gmail.com>
2020-09-24 10:32:14 -0400

    Handle paste in qna

--- src/bp/ui-shared/src/Textarea/index.tsx
+++ src/bp/ui-shared/src/Textarea/index.tsx
@@ -15,6 +15,7 @@ const Textarea: FC<TextareaProps> = ({
   className,
   value,
   placeholder,
+  onPaste,
   onBlur,
   onKeyDown
 }) => {
@@ -62,6 +63,7 @@ const Textarea: FC<TextareaProps> = ({
   return (
     <Fragment>
       <textarea
+        onPaste={onPaste}
         style={{ height: height.current + 'px' }}
         ref={inputRef}
         className={cx(style.textarea, className)}
