e3d54929709542c8d2be06c835b69b875e006268
Philippe Dionne <dionne.phil@gmail.com>
2020-10-05 15:32:25 -0400

    Create types

---
+++ modules/hitl2/src/types.ts
@@ -0,0 +1,65 @@
+interface UserProfile {
+  email: string
+  isSuperAdmin?: boolean
+  strategyType: string
+  strategy: string
+  firstname?: string
+  lastname?: string
+  picture_url?: string
+  fullName: string
+  permissions?: []
+}
+
+export type AgentType = {
+  id: string
+  workspace: string
+  role: string
+  online: boolean
+} & Partial<UserProfile>
+
+export type UserType = {
+  id: string
+  variables?: { name: string; value: any }[]
+} & Partial<UserProfile>
+
+export interface EscalationType {
+  id: string
+  agentId?: string
+  status: 'pending' | 'assigned' | 'resolved'
+  userConversationId: string
+  agentConversationId: string
+  userConversation: EventType
+  agentConversation: EventType
+  comments: CommentType[]
+  assignedAt?: Date
+  resolvedAt?: Date
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface EventType {
+  id: string
+  direction: string
+  botId: string
+  channel: string
+  success: boolean
+  createdOn: Date
+  threadId: string
+  event: string
+}
+
+export interface CommentType {
+  id: string
+  userId: string
+  escalationId: string
+  content: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface SocketMessageType {
+  payload: {}
+  resource: string
+  type: string
+  id: string
+}
