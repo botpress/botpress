feef9399557b01acee3f3abb718a0e136bf7d025
Philippe Dionne <dionne.phil@gmail.com>
2020-10-05 15:48:40 -0400

    Create EscalationItem component

---
+++ modules/hitl2/src/views/full/Components/EscalationBadge.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react'
+
+import { AgentType } from './../../../types'
+
+import { Tag } from '@blueprintjs/core'
+import { lang } from 'botpress/shared'
+
+interface Props {
+  status: string
+  assignedToAgent: Partial<AgentType>
+  currentAgent?: AgentType
+}
+
+const EscalationBadge: FC<Props> = props => {
+  switch (props.status) {
+    case 'assigned':
+      if (props.assignedToAgent.id == props.currentAgent.id) {
+        return <Tag intent="primary">{lang.tr('module.hitl2.escalation.assignment.me')}</Tag>
+      } else {
+        return (
+          <Tag intent="none">
+            {lang.tr('module.hitl2.escalation.assignment.other', { name: props.assignedToAgent.fullName })}
+          </Tag>
+        )
+      }
+    case 'resolved':
+      return <Tag intent="success">{lang.tr(`module.hitl2.escalation.status.${props.status}`)}</Tag>
+    default:
+      return <Tag>{lang.tr(`module.hitl2.escalation.status.${props.status}`)}</Tag>
+  }
+}
+
+export default EscalationBadge
---
+++ modules/hitl2/src/views/full/Components/EscalationItem.tsx
@@ -0,0 +1,90 @@
+import React, { FC, useContext } from 'react'
+import _ from 'lodash'
+import moment from 'moment'
+
+import { ApiType } from '../Api'
+import { EscalationType } from '../../../types'
+
+import { Context } from '../Store'
+
+import { Grid, Row, Col } from 'react-flexbox-grid'
+import { Icon, Button } from '@blueprintjs/core'
+import { toast, lang } from 'botpress/shared'
+import EscalationBadge from './EscalationBadge'
+
+type Props = {
+  api: ApiType
+} & EscalationType
+
+const EscalationItem: FC<Props> = props => {
+  const { api } = props
+
+  const { state, dispatch } = useContext(Context)
+
+  async function handleSelect(id: string) {
+    dispatch({ type: 'setCurrentEscalation', payload: id })
+  }
+
+  async function handleAssign() {
+    try {
+      const escalation = await api.assignEscalation(props.id)
+      api.updateCurrentAgentOnline({ online: true })
+      toast.success(lang.tr('module.hitl2.escalation.assign', { id: escalation.id }))
+    } catch (error) {
+      if (_.inRange(error.response.status, 400, 499)) {
+        toast.failure(error.response.data.errors[0].detail)
+      } else {
+        dispatch({ type: 'setError', payload: error })
+      }
+    }
+  }
+
+  async function handleResolve() {
+    try {
+      const escalation = await api.resolveEscalation(props.id)
+      api.updateCurrentAgentOnline({ online: true })
+      toast.success(lang.tr('module.hitl2.escalation.resolve', { id: escalation.id }))
+    } catch (error) {
+      if (_.inRange(error.response.status, 400, 499)) {
+        toast.failure(error.response.data.errors[0].detail)
+      } else {
+        dispatch({ type: 'setError', payload: error })
+      }
+    }
+  }
+
+  function formatDate(date: Date) {
+    return moment(date).fromNow()
+  }
+
+  return (
+    <Grid>
+      <Row between="xs">
+        <Col>{state.currentEscalation?.id == props.id ? <Icon icon="dot" intent="primary"></Icon> : null}</Col>
+        <Col>
+          <p>Id: {props.id}</p>
+          <p className="bp3-text-small bp3-text-muted">
+            {lang.tr('module.hitl2.escalation.created', { date: formatDate(props.createdAt) })}
+          </p>
+          <p>From: {props.userConversation.channel}</p>
+        </Col>
+        <Col>
+          <EscalationBadge
+            status={props.status}
+            assignedToAgent={state.agents[props.agentId]}
+            currentAgent={state.currentAgent}
+          ></EscalationBadge>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button onClick={() => handleSelect(props.id)}>Select</Button>
+          <Button onClick={handleAssign}>Assign to me</Button>
+          <Button onClick={handleResolve}>Resolve</Button>
+        </Col>
+      </Row>
+    </Grid>
+  )
+}
+
+export default EscalationItem
