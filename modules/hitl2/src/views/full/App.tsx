f90d339babd2c29e9d8b33eb23e48e263488a4c0
Philippe Dionne <dionne.phil@gmail.com>
2020-10-05 15:42:01 -0400

    Fetch initial app state

--- modules/hitl2/src/views/full/App.tsx
+++ modules/hitl2/src/views/full/App.tsx
@@ -12,6 +12,52 @@ const App = ({ bp }) => {
   const api = Api(bp)
 
   const { state, dispatch } = useContext(Context)
+
+  const [escalationsLoading, setEscalationsLoading] = useState(true)
+
+  async function getCurrentAgent() {
+    try {
+      const agent = await api.getCurrentAgent()
+      dispatch({ type: 'setCurrentAgent', payload: agent })
+    } catch (error) {
+      dispatch({ type: 'setError', payload: error })
+    }
+  }
+
+  async function getAgents() {
+    try {
+      const agents = await api.getAgents()
+      dispatch({ type: 'setAgents', payload: agents })
+    } catch (error) {
+      dispatch({ type: 'setError', payload: error })
+    }
+  }
+
+  async function getEscalations() {
+    try {
+      const escalations = await api.getEscalations()
+      dispatch({ type: 'setEscalations', payload: escalations })
+    } catch (error) {
+      dispatch({ type: 'setError', payload: error })
+    }
+  }
+
+  useEffect(() => {
+    Promise.all([getCurrentAgent(), getAgents(), getEscalations()]).then(() => {
+      setEscalationsLoading(false)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (state.error) {
+      if (state.error.response) {
+        toast.failure(`Error: ${state.error.response.data.message}`)
+      } else {
+        toast.failure(`Error: ${state.error}`)
+      }
+    }
+  }, [state.error])
+
   return (
   )
 }
