191c43cfb81a731d99a3877c6b0b4df38cd5f00a
Philippe Dionne <dionne.phil@gmail.com>
2020-10-13 18:19:23 -0400

    Improve comment display

--- modules/hitl2/src/views/full/Components/Comment.tsx
+++ modules/hitl2/src/views/full/Components/Comment.tsx
@@ -1,8 +1,27 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
+import moment from 'moment'
+
+import { Context } from '../Store'
+
 import { CommentType } from '../../../types'
 
 const Comment: FC<CommentType> = props => {
-  return <div>{props.content}</div>
+  const { state } = useContext(Context)
+
+  function formatDate(str) {
+    return moment(str).format('DD/MM/YYYY')
+  }
+
+  const agent = state.agents[props.agentId]
+
+  return (
+    <div>
+      <p>{props.content}</p>
+      <p className="bp3-text-small bp3-text-muted">
+        {formatDate(props.createdAt)} <span>⋅</span> {agent.fullName || agent.id}
+      </p>
+    </div>
+  )
 }
 
 export default Comment
