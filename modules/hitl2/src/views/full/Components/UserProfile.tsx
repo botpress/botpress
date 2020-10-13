5419d69473403369bd6fd76ed141bf321def8ba8
Philippe Dionne <dionne.phil@gmail.com>
2020-10-13 11:28:48 -0400

    Display user variables in table

--- modules/hitl2/src/views/full/Components/UserProfile.tsx
+++ modules/hitl2/src/views/full/Components/UserProfile.tsx
@@ -1,15 +1,20 @@
 import _ from 'lodash'
-import React, { FC, useEffect, useState } from 'react'
-import { Divider } from '@blueprintjs/core'
+import React, { FC, useState } from 'react'
 import hash from 'object-hash'
 import Haikunator from 'haikunator'
+
 import { EventType } from '../../../types'
+
+import { HTMLTable } from '@blueprintjs/core'
+import { EmptyState, lang } from 'botpress/shared'
+import Collapsible from '../../../../../../src/bp/ui-shared-lite/Collapsible'
 
 interface Props {
   conversation: EventType
 }
 
 const UserProfile: FC<Props> = props => {
+  const [expanded, setExpanded] = useState(true)
   const [userNames, setUserNames] = useState({})
   const [haiku, sethaiku] = useState(() => {
     return new Haikunator({ defaults: { tokenLength: 0 } })
@@ -17,7 +22,9 @@ const UserProfile: FC<Props> = props => {
 
   const user = _.get(JSON.parse(props.conversation.event), 'state.user', {})
 
-  function defaultUserName() {
+  const variables = _.omit(user, 'language', 'timezone', 'fullname', 'emil')
+
+  function defaultUserName(): string {
     const key = hash(_.pick(props.conversation, ['channel', 'threadId']))
 
     if (!userNames[key]) {
@@ -37,19 +44,32 @@ const UserProfile: FC<Props> = props => {
         <li>language: {user.language}</li>
       </ul>
 
-      <Divider></Divider>
-
-      {user.variables ? (
-        <ul>
-          {user.variables.map((variable, index) => {
-            return (
-              <li key={index}>
-                {variable.name} : {variable.value}
-              </li>
-            )
-          })}
-        </ul>
-      ) : null}
+      <Collapsible
+        opened={expanded}
+        toggleExpand={() => setExpanded(!expanded)}
+        name={lang.tr('module.hitl2.user.variables.heading')}
+      >
+        {_.isEmpty(variables) ? (
+          <EmptyState text={lang.tr('module.hitl2.user.variables.empty')}></EmptyState>
+        ) : (
+          <HTMLTable condensed={true} width="100%">
+            <thead>
+              <tr>
+                <th>Variable</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(variables).map((entry, index) => (
+                <tr key={index}>
+                  <td>{entry[0]}</td>
+                  <td>{entry[1]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </HTMLTable>
+        )}
+      </Collapsible>
     </div>
   )
 }
