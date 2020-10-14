352dac46b6f838c4ea300deb3db6f30b0c9cf953
Philippe Dionne <dionne.phil@gmail.com>
2020-10-14 16:09:30 -0400

    Enforce sidebar escalation items order & limit

--- modules/hitl2/src/views/full/sidebar/components/EscalationList.tsx
+++ modules/hitl2/src/views/full/sidebar/components/EscalationList.tsx
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import { EscalationType } from '../../../../types'
 import { EscalationsMapType } from '../../sidebar/Store'
@@ -12,26 +12,37 @@ import EscalationItem from './EscalationItem'
 interface Props {
   escalations: EscalationsMapType
   loading: boolean
+  itemCount?: number
 }
 
 const EscalationList: FC<Props> = props => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    setItems(_.slice(_.orderBy(props.escalations, 'createdAt', 'desc'), 0, props.itemCount))
+  }, [props.escalations])
+
   return (
     <div>
       {props.loading && <Spinner></Spinner>}
 
-      {!props.loading && _.isEmpty(props.escalations) && (
+      {!props.loading && _.isEmpty(items) && (
         <EmptyState icon={<CasesIcon />} text={lang.tr('module.hitl2.sidebar.escalations.empty')}></EmptyState>
       )}
 
-      {!_.isEmpty(props.escalations) && (
+      {!_.isEmpty(items) && (
         <ul>
-          {_.values(props.escalations).map((escalation: EscalationType) => (
+          {_.values(items).map((escalation: EscalationType) => (
             <EscalationItem key={escalation.id} {...escalation}></EscalationItem>
           ))}
         </ul>
       )}
     </div>
   )
+}
+
+EscalationList.defaultProps = {
+  itemCount: 5
 }
 
 export default EscalationList
