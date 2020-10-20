92aa35c7b97d9774a6f653859c31b657640508b2
Francois <francois_levasseur@hotmail.com>
2020-10-20 13:41:32 -0400

    fix: unit test expect new-yorkers to not match new york with loose entity

--- modules/nlu/src/backend/entities/list-extractor.test.ts
+++ modules/nlu/src/backend/entities/list-extractor.test.ts
@@ -37,7 +37,7 @@ const list_entities: ListEntityModel[] = [
   },
   {
     entityName: 'airport',
-    fuzzyTolerance: FuzzyTolerance.Loose,
+    fuzzyTolerance: FuzzyTolerance.Medium,
     id: 'custom.list.city',
     languageCode: 'en',
     mappingsTokens: {
