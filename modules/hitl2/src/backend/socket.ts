907e141f5e8a5f65b4f09f8ebcd3e717ad361caf
Philippe Dionne <dionne.phil@gmail.com>
2020-10-20 14:15:01 -0400

    send -> sendPayload

--- modules/hitl2/src/backend/socket.ts
+++ modules/hitl2/src/backend/socket.ts
@@ -3,7 +3,7 @@ import { SocketMessageType } from '../types'
 
 export default (bp: typeof sdk) => {
   return {
-    send: (message: SocketMessageType) => {
+    sendPayload: (message: SocketMessageType) => {
       bp.realtime.sendPayload(new bp.RealTimePayload('hitl2', message))
     }
   }
