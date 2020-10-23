6374721e1663feb3614f3ad792f865c2048567fb
Francois <francois_levasseur@hotmail.com>
2020-10-23 15:39:45 -0400

    fix(studio): put back previous library

--- src/bp/ui-studio/src/web/views/OneFlow/sidePanel/Library/index.tsx
+++ src/bp/ui-studio/src/web/views/OneFlow/sidePanel/Library/index.tsx
@@ -1,186 +1,94 @@
-import { Button } from '@blueprintjs/core'
-import axios from 'axios'
-import { NLU } from 'botpress/sdk'
-import cx from 'classnames'
-import React, { FC, Fragment, useEffect, useState } from 'react'
+import { Menu, MenuItem } from '@blueprintjs/core'
+import { lang, TreeView } from 'botpress/shared'
+import { LibraryElement } from 'common/typings'
+import React, { FC, useCallback, useState } from 'react'
 import { connect } from 'react-redux'
-import { createFlow, updateFlow } from '~/actions'
-import { getAllFlows, RootReducer } from '~/reducers'
+import { removeElementFromLibrary } from '~/actions'
+import MarkdownRenderer from '~/components/Shared/MarkdownRenderer'
+import { RootReducer } from '~/reducers'
 
-import style from './style.scss'
-import EntityModal from './Modal'
-import EntityNameModal from './NameModal'
+import style from '../style.scss'
+
+import Editor from './LiteEditor'
 
 type StateProps = ReturnType<typeof mapStateToProps>
 type DispatchProps = typeof mapDispatchToProps
+
 interface OwnProps {
-  goToFlow: (flow: any) => void
+  filter: string
+  refreshLibrary: () => void
 }
 
 type Props = OwnProps & StateProps & DispatchProps
-interface TableItem {
-  label: string
-  click: () => void
-  edit: () => void
-  delete: () => void
+
+const nodeRenderer = ({ contentId, type, preview }: LibraryElement) => {
+  return {
+    label: (
+      <div
+        className={style.grabbable}
+        draggable={true}
+        onDragStart={event => {
+          event.dataTransfer.setData('diagram-node', JSON.stringify({ contentId, type: 'node', id: type }))
+        }}
+      >
+        <MarkdownRenderer content={preview} noLink={true} size="sm"></MarkdownRenderer>
+      </div>
+    )
+  }
 }
 
 const Library: FC<Props> = props => {
-  const [currentEntity, setCurrentEntity] = useState<NLU.EntityDefinition>()
-  const [currentNamingEntity, setCurrentNamingEntity] = useState<NLU.EntityDefinition>()
-  const [forceUpdate, setForceUpdate] = useState(false)
-  const [entities, setEntities] = useState<NLU.EntityDefinition[]>([])
+  const [isEditOpen, setEditOpen] = useState(false)
+  const [editItem, setEditItem] = useState('')
 
-  useEffect(() => {
-    async function fetchEntities() {
-      const res = await axios.get(`${window.BOT_API_PATH}/nlu/entities`)
-      setEntities(res.data)
+  const getContextMenu = (element: LibraryElement) => {
+    return (
+      <Menu>
+        <MenuItem
+          id="btn-remove"
+          icon="remove"
+          text={lang.tr('studio.flow.removeFromLibrary')}
+          onClick={() => {
+            props.removeElementFromLibrary(element.contentId)
+            props.refreshLibrary()
+          }}
+        />
+      </Menu>
+    )
+  }
+
+  const onDoubleClick = (element: LibraryElement, elementType) => {
+    if (elementType === 'document') {
+      setEditItem(element.contentId)
+      setEditOpen(true)
     }
-    // tslint:disable-next-line: no-floating-promises
-    fetchEntities()
-  }, [forceUpdate])
-
-  const createEntity = async (type: string) => {
-    const originalName = `${type}-entity`
-    let name
-    let index = 0
-    do {
-      name = `${originalName}${index ? `-${index}` : ''}`
-      index++
-    } while (entities.find(x => x.name === name))
-
-    const entity = {
-      id: name,
-      name,
-      type,
-      occurrences: []
-    }
-
-    await axios.post(`${window.BOT_API_PATH}/nlu/entities`, entity)
-    setForceUpdate(!forceUpdate)
   }
 
-  const deleteEntity = async (entity: NLU.EntityDefinition) => {
-    await axios.post(`${window.BOT_API_PATH}/nlu/entities/${entity.id}/delete`)
-    setForceUpdate(!forceUpdate)
-  }
-
-  const updateEntity = async (targetEntityId: string, entity: NLU.EntityDefinition) => {
-    await axios.post(`${window.BOT_API_PATH}/nlu/entities/${targetEntityId}`, entity)
-    const idx = entities.findIndex(ent => ent.name === entity.name)
-    setEntities([...entities.slice(0, idx), entity, ...entities.slice(idx + 1)])
-  }
-
-  const renderTable = (title: string, items: TableItem[]) => {
-    return (
-      <table
-        className={cx(
-          style.table,
-          'bp3-html-table bp3-html-table-striped bp3-html-table-bordered .bp3-html-table-condensed'
-        )}
-      >
-        <thead>
-          <tr>
-            <th>{title}</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>{renderTableRows(items)}</tbody>
-      </table>
-    )
-  }
-  const renderTableRows = (items: TableItem[]) => {
-    return (
-      <Fragment>
-        {items &&
-          items?.map((item, i) => (
-            <tr key={i}>
-              <td>
-                <Button text={item.label} onClick={item.click} />
-              </td>
-              <td>
-                <Button icon="edit" onClick={item.edit} />
-              </td>
-              <td>
-                <Button icon="delete" onClick={item.delete} />
-              </td>
-            </tr>
-          ))}
-      </Fragment>
-    )
-  }
-
-  const renderVariableTypes = () => {
-    return (
-      <section>
-        {renderTable(
-          'Variable Types',
-          entities
-            .filter(x => x.type !== 'system')
-            .map<TableItem>(x => ({
-              label: x.name,
-              click: () => {
-                setCurrentEntity(x)
-              },
-              edit: () => {
-                setCurrentNamingEntity(x)
-              },
-              delete: async () => {
-                await deleteEntity(x)
-              }
-            }))
-        )}
-        <p>
-          <Button text="Add Enumeration" onClick={() => createEntity('list')} />
-        </p>
-        <p>
-          <Button text="Add Pattern" onClick={() => createEntity('pattern')} />
-        </p>
-      </section>
-    )
-  }
-
-  const renderModal = () => {
-    return (
-      <EntityModal
-        entity={currentEntity}
-        entities={entities}
-        updateEntity={updateEntity}
-        isOpen={currentEntity !== undefined}
-        onClose={() => setCurrentEntity(undefined)}
-      />
-    )
-  }
-  const renderNameModal = () => {
-    return (
-      <EntityNameModal
-        entity={currentNamingEntity}
-        isOpen={currentNamingEntity !== undefined}
-        onClose={() => {
-          setForceUpdate(!forceUpdate)
-          setCurrentNamingEntity(undefined)
-        }}
-      />
-    )
-  }
+  const filters = { text: props.filter, field: 'preview' }
+  const toggle = () => setEditOpen(false)
 
   return (
-    <div className={style.library}>
-      {renderVariableTypes()}
-      {renderModal()}
-      {renderNameModal()}
+    <div>
+      <TreeView<LibraryElement>
+        elements={props.library}
+        nodeRenderer={nodeRenderer}
+        onContextMenu={getContextMenu}
+        onDoubleClick={onDoubleClick}
+        filterText={props.filter}
+        filterProps="preview"
+      />
+      <Editor itemId={editItem} isOpen={isEditOpen} toggle={toggle}></Editor>
     </div>
   )
 }
 
 const mapStateToProps = (state: RootReducer) => ({
-  flows: getAllFlows(state)
+  conditions: state.ndu.conditions,
+  library: state.content.library
 })
 
 const mapDispatchToProps = {
-  createFlow,
-  updateFlow
+  removeElementFromLibrary
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Library)
--- src/bp/ui-studio/src/web/views/OneFlow/sidePanel/index.tsx
+++ src/bp/ui-studio/src/web/views/OneFlow/sidePanel/index.tsx
@@ -164,7 +164,10 @@ const SidePanelContent: FC<Props> = props => {
             <NavbarGroup>
               <Tabs onChange={onTabChanged}>
                 <Tab id="topics" title={lang.tr('topics')} />
-                <Tab id="library" title={lang.tr('library')} />
+                {/*
+                  TODO add Library once we have something display
+                  <Tab id="library" title={lang.tr('library')} />
+                */}
               </Tabs>
             </NavbarGroup>
             {props.permissions.includes('create') && currentTab === 'topics' && (
@@ -195,7 +198,7 @@ const SidePanelContent: FC<Props> = props => {
             />
           )}
 
-          {currentTab === 'library' && <Library goToFlow={goToFlow} />}
+          {currentTab === 'library' && <Library filter={libraryFilter} />}
         </React.Fragment>
       )}
 
