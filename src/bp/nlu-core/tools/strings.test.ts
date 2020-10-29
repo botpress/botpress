7322f8d3956a65ccb3c97fa44b7f155feffdb5af
Francois <francois_levasseur@hotmail.com>
2020-10-28 20:40:30 -0400

    chore(nlu): rm leven & jaro from toolkit + use damerau-leven in entities

--- src/bp/nlu-core/tools/strings.test.ts
+++ src/bp/nlu-core/tools/strings.test.ts
@@ -37,23 +37,6 @@ describe('String utils', () => {
     expect(stringUtils.countSpecial('MyNaµis åntH0n¥')).toEqual(3)
   })
 
-  test('levenshtein', () => {
-    expect(stringUtils.levenshtein('testing', 'tesing')).toEqual(1) // 1 x suppresion
-    expect(stringUtils.levenshtein('testting', 'testing')).toEqual(1) // 1 x addition
-    expect(stringUtils.levenshtein('tasting', 'testing')).toEqual(1) // 1 x substitution
-    expect(stringUtils.levenshtein('teing', 'testing')).toEqual(2) // 2 x suppression
-    expect(stringUtils.levenshtein('tesstting', 'testing')).toEqual(2) // 2 x addition
-    expect(stringUtils.levenshtein('teasing', 'testing')).toEqual(2) // 1 x suppression + 1 x addition
-    expect(stringUtils.levenshtein('teasing', 'testing')).toEqual(2) // 1 x suppression + 1 x addition
-    expect(stringUtils.levenshtein('tastting', 'testing')).toEqual(2) // 1 x substitution + 1 x addition
-    expect(stringUtils.levenshtein('tetsng', 'testing')).toEqual(2) // 1 x suppression + 1 x substitution
-    expect(stringUtils.levenshtein('tetsing', 'testing')).toEqual(2) // letterSwap (1 sup + 1 add)
-    expect(stringUtils.levenshtein('tetsig', 'testing')).toEqual(3) // 1 x suppression + 1 x letterSwap (1 sup + 1 add)
-    expect(stringUtils.levenshtein('tetsinng', 'testing')).toEqual(3) // 1 x letterSwap (1 sup + 1 add) + 1 x addition
-    expect(stringUtils.levenshtein('tetsinng', 'testing')).toEqual(3) // 1 x letterSwap (1 sup + 1 add) + 1 x addition
-    expect(stringUtils.levenshtein('tetsong', 'testing')).toEqual(3) // 1 x letterSwap (1 sup + 1 add) + 1 x substitution
-  })
-
   test('darmeau levenshtein', () => {
     expect(stringUtils.damerauLevenshtein('testing', 'tesing')).toEqual(1) // 1 x suppresion
     expect(stringUtils.damerauLevenshtein('testting', 'testing')).toEqual(1) // 1 x addition
