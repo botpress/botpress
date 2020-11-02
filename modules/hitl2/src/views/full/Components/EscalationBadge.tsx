fe89e706f9d55e627cddce833dbfef6ceec41b31
Philippe Dionne <dionne.phil@gmail.com>
2020-11-02 13:04:51 -0500

    Remove custom tags

--- modules/hitl2/src/views/full/Components/EscalationBadge.tsx
+++ modules/hitl2/src/views/full/Components/EscalationBadge.tsx
@@ -1,9 +1,6 @@
 import { Tag, Intent } from '@blueprintjs/core'
 import { lang } from 'botpress/shared'
-import cx from 'classnames'
 import React, { FC } from 'react'
-
-import sharedStyle from '../../../../../../src/bp/ui-shared-lite/style.scss'
 
 import { AgentType } from './../../../types'
 
@@ -19,24 +16,14 @@ const EscalationBadge: FC<Props> = props => {
       return <Tag intent={Intent.SUCCESS}>{lang.tr(`module.hitl2.escalation.status.${props.status}`)}</Tag>
     case 'assigned':
       if (props.assignedToAgent?.id === props.currentAgent?.id) {
-        return (
-          <div className={cx(sharedStyle.badge, sharedStyle.ocean)}>
-            {lang.tr('module.hitl2.escalation.assignment.me')}
-          </div>
-        )
+        return <Tag intent={Intent.PRIMARY}>{lang.tr('module.hitl2.escalation.assignment.me')}</Tag>
       } else {
         return (
-          <div className={cx(sharedStyle.badge, sharedStyle.white)}>
-            {lang.tr('module.hitl2.escalation.assignment.other', { name: props.assignedToAgent?.fullName })}
-          </div>
+          <Tag>{lang.tr('module.hitl2.escalation.assignment.other', { name: props.assignedToAgent?.fullName })}</Tag>
         )
       }
     default:
-      return (
-        <div className={cx(sharedStyle.badge, sharedStyle.warning)}>
-          {lang.tr(`module.hitl2.escalation.status.${props.status}`)}
-        </div>
-      )
+      return <Tag intent={Intent.WARNING}>{lang.tr(`module.hitl2.escalation.status.${props.status}`)}</Tag>
   }
 }
 
