e105b53de4679f125c6084c2d95fd86a98dd2f0b
Said MARAR <mararsaid@gmail.com>
2020-11-10 21:39:49 +0100

    fix(studio): enhance unclosed brackets, respect order and exact length

--- src/bp/ui-studio/src/web/components/Util/form.util.ts
+++ src/bp/ui-studio/src/web/components/Util/form.util.ts
@@ -1,2 +1,14 @@
-export const isMissingCurlyBraceClosure = (text: string): boolean =>
-  (text?.match(/{{/g) || []).length !== (text?.match(/}}/g) || []).length
+export const isMissingCurlyBraceClosure = (text: string): boolean => {
+  const brackets = text?.match(/{{{?|}}}?/g)
+
+  return brackets && !(brackets.length % 2 === 0 &&
+    new Array(brackets.length / 2)
+      .fill([])
+      .map( () => brackets.splice(0, 2))
+      .map( bracket =>
+        bracket[0].match(/{{{?/g) &&
+        bracket[1].match(/}}}?/g) &&
+        bracket[0].length === bracket[1].length
+      )
+      .reduce((a, c) => c && a))
+}
