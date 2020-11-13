4f3a290a48d3091e809c0e6ae26e17ae99694409
Jesse H. Blanchet <jhblanchet@gmail.com>
2020-11-13 09:46:15 -0500

    fix(misunderstood) refactor

--- modules/misunderstood/src/hooks/after_incoming_middleware/00_misunderstood_flag.js
+++ modules/misunderstood/src/hooks/after_incoming_middleware/00_misunderstood_flag.js
@@ -1,41 +1,51 @@
 const axios = require('axios')
 const _ = require('lodash')
 
+const { currentFlow, currentNode } = event.state.context
+
+const matchesQuickReply = async (flow) => {
+  if (currentFlow && currentFlow.startsWith('skills/choice')) {
+    const preview = event.preview.toLowerCase()
+    const kw = _.chain(flow.skillData.keywords)
+      .values()
+      .flatten()
+      .find(kw => kw.toLowerCase() === preview.toLowerCase())
+      .value()
+
+    if (kw) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const inWaitNode = async (flow) => {
+  if (currentFlow && currentNode) {
+    const node = _.find(flow.nodes, node => node.name === currentNode)
+    
+    if (node && node.onReceive != null) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const isMisunderstood = async () => {
+  if (event.type === 'text' && event.nlu.intent.name === 'none'){
+    const flow = currentFlow && await bp.ghost.forBot(event.botId).readFileAsObject('flows', currentFlow)
+
+    if (await matchesQuickReply(flow) || await inWaitNode(flow)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
 const flag = async () => {
-  const matchesQuickReply = async () => {
-    const { currentFlow } = event.state.context
-    if (currentFlow && currentFlow.startsWith('skills/choice')) {
-      const flow = await bp.ghost.forBot(event.botId).readFileAsObject('flows', currentFlow)
-      const preview = event.preview.toLowerCase()
-      const kw = _.chain(flow.skillData.keywords)
-        .values()
-        .flatten()
-        .find(kw => kw.toLowerCase() === preview.toLowerCase())
-        .value()
-
-      if (kw) {
-        return true
-      }
-    }
-
-    return false
-  }
-  
-  const inWaitNode = async () => {
-    const { currentFlow, currentNode } = event.state.context
-    if (currentFlow && currentNode) {
-      const flow = await bp.ghost.forBot(event.botId).readFileAsObject('flows', currentFlow)
-      const node = _.find(flow.nodes, node => node.name === currentNode)
-      
-      if (node && node.onReceive != null) {
-        return true
-      }
-    }
-
-    return false
-  }
-
-  if (event.type === 'text' && event.nlu.intent.name === 'none' && !(await matchesQuickReply()) && !(await inWaitNode())) {
+  if (await isMisunderstood()){
     const data = {
       eventId: event.id,
       botId: event.botId,
