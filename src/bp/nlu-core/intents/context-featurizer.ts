7ae181b2c35350001500ea31e2b135b3166a847c
Francois <francois_levasseur@hotmail.com>
2020-11-24 15:13:19 -0500

    added comments

--- src/bp/nlu-core/intents/context-featurizer.ts
+++ src/bp/nlu-core/intents/context-featurizer.ts
@@ -19,7 +19,11 @@ export function getCtxFeatures(utt: Utterance, customEntities: string[]): number
     return zeroes(utt.tokens[0].vector.length + entitiesOH.length)
   }
 
-  // TODO: should maybe use a topic computed tfidf (tfid is computed on intents)
+  /**
+   * TODO:
+   * - should maybe use a topic computed tfidf (tfid is computed on intents)
+   * - why do we even filter out patterns and systems ?
+   */
   const sentenceEmbedding = utt.sentenceEmbedding({ keepToken: shouldConsiderToken })
   return [...sentenceEmbedding, ...entitiesOH]
 }
