169ed7e191b3d53f4d23db94776b4a6e30a81297
Philippe Dionne <dionne.phil@gmail.com>
2020-11-25 14:19:40 -0500

    Refactor HandoffBadge component

--- modules/hitlnext/src/views/full/app/components/HandoffBadge.tsx
+++ modules/hitlnext/src/views/full/app/components/HandoffBadge.tsx
@@ -2,23 +2,14 @@ import { Intent, Tag } from '@blueprintjs/core'
 import { lang } from 'botpress/shared'
 import React, { FC } from 'react'
 
-import { IAgent } from '../../../../types'
-
 interface Props {
   status: string
 }
 
-const HandoffBadge: FC<Props> = props => {
-  switch (props.status) {
-    case 'resolved':
-      return <Tag intent={Intent.NONE}>{lang.tr(`module.hitlnext.handoff.status.${props.status}`)}</Tag>
-    case 'assigned':
-      return <Tag intent={Intent.PRIMARY}>{lang.tr(`module.hitlnext.handoff.status.${props.status}`)}</Tag>
-    case 'pending':
-      return <Tag intent={Intent.WARNING}>{lang.tr(`module.hitlnext.handoff.status.${props.status}`)}</Tag>
-    default:
-      return <Tag>{lang.tr(`module.hitlnext.handoff.status.${props.status}`)}</Tag>
-  }
+const statusIntents = { resolved: Intent.NONE, assigned: Intent.PRIMARY, pending: Intent.WARNING }
+
+const HandoffBadge: FC<Props> = ({ status }) => {
+  return <Tag intent={statusIntents[status]}>{lang.tr(`module.hitlnext.handoff.status.${status}`)}</Tag>
 }
 
 export default HandoffBadge
