072a83b6bfe61fcccd32b3c0d4c5a62cb359d6e4
Roberto Escalante H <robertoescalante91@gmail.com>
2020-12-02 13:17:32 -0600

    added methods to deconstructor at the top of the method

--- src/bp/ui-studio/src/web/views/FlowBuilder/inspector/index.tsx
+++ src/bp/ui-studio/src/web/views/FlowBuilder/inspector/index.tsx
@@ -69,6 +69,8 @@ class Inspector extends Component<Props> {
       currentFlow,
       currentFlowNode,
       onDeleteSelectedElements,
+      copyFlowNodeElement,
+      pasteFlowNodeElement,
       readOnly,
       refreshFlowsLinks,
       requestEditSkill,
@@ -128,8 +130,8 @@ class Inspector extends Component<Props> {
           node={currentFlowNode}
           updateNode={updateNodeAndRefresh}
           updateFlow={updateFlow}
-          copyFlowNodeElement={this.props.copyFlowNodeElement}
-          pasteFlowNodeElement={this.props.pasteFlowNodeElement}
+          copyFlowNodeElement={copyFlowNodeElement}
+          pasteFlowNodeElement={pasteFlowNodeElement}
           transitionOnly={nodeType === 'router'}
           buffer={buffer}
         />
