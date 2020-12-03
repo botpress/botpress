79847a5f2bc8210193271fde0da687071752cd6c
franklevasseur <francois_levasseur@hotmail.com>
2020-12-03 15:22:25 -0500

    refactor seed making algorithm

--- modules/nlu/src/backend/seed-service.ts
+++ modules/nlu/src/backend/seed-service.ts
@@ -4,6 +4,11 @@ import _ from 'lodash'
 
 const MAX_SEED = 10000
 
+const BYTES_PER_NUMBER = 8 // 64 bits floats === 8 bytes
+const NIBBLES_PER_NUMBER = BYTES_PER_NUMBER * 2 // 0xff takes 2 nibbles
+
+const BASE = 16
+
 const _hashToNumber = (text: string): number => {
   const stringHash = crypto
     .createHash('md5')
@@ -11,18 +16,11 @@ const _hashToNumber = (text: string): number => {
     .digest('hex') // md5 produces 128 bits (16 bytes) hex encoded hash
 
   // we take half the hash so that it is the size of a number
-  const bytesPerNumber = 8 // 64 bits floats === 8 bytes
-  const nibblesPerNumber = bytesPerNumber * 2 // 0xff takes 2 nibbles
-  const truncated = stringHash.slice(nibblesPerNumber)
-
-  const base = 16
-  const rawSeed = parseInt(truncated, base)
-  return Math.abs(rawSeed) % MAX_SEED
+  const truncated = stringHash.slice(NIBBLES_PER_NUMBER)
+  return parseInt(truncated, BASE)
 }
 
 export const getSeed = (bot: sdk.BotConfig): number => {
-  if (_.isNumber(bot.nluSeed)) {
-    return Math.abs(bot.nluSeed) % MAX_SEED
-  }
-  return _hashToNumber(bot.id)
+  const rawSeed = _.isNumber(bot.nluSeed) ? bot.nluSeed : _hashToNumber(bot.id)
+  return Math.abs(rawSeed) % MAX_SEED
 }
