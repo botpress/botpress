9d0151b280649cdfad365c47b4d1692cc7d29e42
allardy <yann.allard1@gmail.com>
2020-12-03 11:37:18 -0500

    mini fix

--- modules/hitlnext/src/views/full/shared/helper.ts
+++ modules/hitlnext/src/views/full/shared/helper.ts
@@ -1,7 +1,7 @@
 import { IAgent } from '../../../types'
 
 export function agentName(agent: IAgent) {
-  const { firstname, lastname } = agent.attributes
+  const { firstname, lastname } = agent.attributes || {}
 
   if (firstname || lastname) {
     return [firstname, lastname].filter(Boolean).join(' ')
