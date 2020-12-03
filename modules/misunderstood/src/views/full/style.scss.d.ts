fe9e4dbd8f869f1203553b48a06a477021e886fe
David Vitor Antonio <david.vitora@gmail.com>
2020-12-03 03:41:06 -0300

    feat(misunderstood): add date filter

--- modules/misunderstood/src/views/full/style.scss.d.ts
+++ modules/misunderstood/src/views/full/style.scss.d.ts
@@ -4,6 +4,7 @@ interface CssExports {
   'applyAllButton': string;
   'contentFixed': string;
   'contentStretch': string;
+  'filterItem': string;
   'headerTabs': string;
   'mainView': string;
   'mainViewTable': string;
