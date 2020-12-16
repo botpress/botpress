08a98c128df918d95c654e719506ae0dc278d5ea
François Levasseur <francois_levasseur@hotmail.com>
2020-12-16 10:26:03 -0500

    put back api return types

--- modules/nlu/src/api.ts
+++ modules/nlu/src/api.ts
@@ -5,26 +5,29 @@ import * as sdk from 'botpress/sdk'
 export type NLUApi = ReturnType<typeof makeApi>
 
 export const makeApi = (bp: { axios: AxiosInstance }) => ({
-  fetchContexts: () => bp.axios.get('/nlu/contexts').then(res => res.data),
-  fetchIntentsWithQNAs: () => bp.axios.get('/nlu/intents').then(res => res.data),
-  fetchIntents: async () => {
+  fetchContexts: (): Promise<string[]> => bp.axios.get('/nlu/contexts').then(res => res.data),
+  fetchIntentsWithQNAs: (): Promise<NLU.IntentDefinition[]> => bp.axios.get('/nlu/intents').then(res => res.data),
+  fetchIntents: async (): Promise<NLU.IntentDefinition[]> => {
     const { data } = await bp.axios.get('/nlu/intents')
     return data.filter(x => !x.name.startsWith('__qna__'))
   },
-  fetchIntent: (intentName: string) => bp.axios.get(`/nlu/intents/${intentName}`).then(res => res.data),
+  fetchIntent: (intentName: string): Promise<NLU.IntentDefinition> =>
+    bp.axios.get(`/nlu/intents/${intentName}`).then(res => res.data),
   createIntent: (intent: Partial<NLU.IntentDefinition>) => bp.axios.post('/nlu/intents', intent),
-  updateIntent: (targetIntent: string, intent: Partial<NLU.IntentDefinition>) =>
+  updateIntent: (targetIntent: string, intent: Partial<NLU.IntentDefinition>): Promise<void> =>
     bp.axios.post(`/nlu/intents/${targetIntent}`, intent),
-  deleteIntent: (name: string) => bp.axios.post(`/nlu/intents/${name}/delete`),
-  syncIntentTopics: (intentNames?: string[]) => bp.axios.post('/nlu/sync/intents/topics', { intentNames }),
-  fetchEntities: () => bp.axios.get('/nlu/entities').then(res => res.data),
-  fetchEntity: (entityName: string) => bp.axios.get(`/nlu/entities/${entityName}`).then(res => res.data),
-  createEntity: (entity: NLU.EntityDefinition) => bp.axios.post('/nlu/entities/', entity),
-  updateEntity: (targetEntityId: string, entity: NLU.EntityDefinition) =>
+  deleteIntent: (name: string): Promise<void> => bp.axios.post(`/nlu/intents/${name}/delete`),
+  syncIntentTopics: (intentNames?: string[]): Promise<void> =>
+    bp.axios.post('/nlu/sync/intents/topics', { intentNames }),
+  fetchEntities: (): Promise<NLU.EntityDefinition[]> => bp.axios.get('/nlu/entities').then(res => res.data),
+  fetchEntity: (entityName: string): Promise<NLU.EntityDefinition> =>
+    bp.axios.get(`/nlu/entities/${entityName}`).then(res => res.data),
+  createEntity: (entity: NLU.EntityDefinition): Promise<void> => bp.axios.post('/nlu/entities/', entity),
+  updateEntity: (targetEntityId: string, entity: NLU.EntityDefinition): Promise<void> =>
     bp.axios.post(`/nlu/entities/${targetEntityId}`, entity),
-  deleteEntity: (entityId: string) => bp.axios.post(`/nlu/entities/${entityId}/delete`),
-  train: () => bp.axios.post('/mod/nlu/train'),
-  cancelTraining: () => bp.axios.post('/mod/nlu/train/delete')
+  deleteEntity: (entityId: string): Promise<void> => bp.axios.post(`/nlu/entities/${entityId}/delete`),
+  train: (): Promise<void> => bp.axios.post('/mod/nlu/train'),
+  cancelTraining: (): Promise<void> => bp.axios.post('/mod/nlu/train/delete')
 })
 
 export const createApi = async (bp: typeof sdk, botId: string) => {
