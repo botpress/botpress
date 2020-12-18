5641c1e17fb7b6b52f68f57a83d33d3e9bcdc8ae
Philippe Dionne <dionne.phil@gmail.com>
2020-12-18 10:35:54 -0500

    Add migration description

--- modules/hitlnext/src/migrations/v12_16_0-1608057053-add_tags_to_handoffs.ts
+++ modules/hitlnext/src/migrations/v12_16_0-1608057053-add_tags_to_handoffs.ts
@@ -1,8 +1,9 @@
 import * as sdk from 'botpress/sdk'
+import { MODULE_NAME } from '../constants'
 
 const migration: sdk.ModuleMigration = {
   info: {
-    description: '',
+    description: `Adds support for tags in ${MODULE_NAME} module`,
     target: 'core',
     type: 'database'
   },
@@ -17,7 +18,7 @@ const migration: sdk.ModuleMigration = {
 
     return {
       success: true,
-      message: exists ? 'tags column already exists, skipping...' : 'tags column created.'
+      message: exists ? 'Tags column already exists, skipping...' : 'Tags column created.'
     }
   }
 }
