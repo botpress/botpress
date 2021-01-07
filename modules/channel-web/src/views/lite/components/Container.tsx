98d910b89aec21e2f0c37076031ec2cb06620678
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-01-07 10:19:15 -0500

    render empty div when webchat is loading

--- modules/channel-web/src/views/lite/components/Container.tsx
+++ modules/channel-web/src/views/lite/components/Container.tsx
@@ -17,7 +17,7 @@ import OverridableComponent from './OverridableComponent'
 class Container extends React.Component<ContainerProps> {
   renderBody() {
     if (!this.props.isInitialized) {
-      return null
+      return (<div className="bpw-msg-list-container" />)
     }
 
     if (this.props.isConversationsDisplayed) {
