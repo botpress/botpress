a1c07455f4308c426f0963efd4a5e8dc299485a0
franklevasseur <francois_levasseur@hotmail.com>
2021-01-13 17:16:39 -0500

    chore(stan): make sure probabilities sum to one when using oos field

--- src/bp/nlu-server/remove-none.test.ts
+++ src/bp/nlu-server/remove-none.test.ts
@@ -1,5 +1,3 @@
-import { NLU } from 'botpress/sdk'
-
 import { BpPredictOutput } from './api-mapper'
 import removeNoneIntent from './remove-none'
 
@@ -12,7 +10,7 @@ test('remove none intent', () => {
         confidence: 0.5,
         oos: 0.666,
         intents: [
-          { label: 'A', confidence: 0.5, extractor: 'classifier', slots: {} },
+          { label: 'A', confidence: 0.58, extractor: 'classifier', slots: {} },
           { label: 'none', confidence: 0.42, extractor: 'classifier', slots: {} }
         ]
       },
@@ -20,7 +18,7 @@ test('remove none intent', () => {
         confidence: 0.5,
         oos: 0.123,
         intents: [
-          { label: 'B', confidence: 0.5, extractor: 'classifier', slots: {} },
+          { label: 'B', confidence: 0.01, extractor: 'classifier', slots: {} },
           { label: 'none', confidence: 0.99, extractor: 'classifier', slots: {} }
         ]
       }
@@ -34,7 +32,8 @@ test('remove none intent', () => {
   const withoutNone = removeNoneIntent(nlu)
 
   // assert
-  expect(withoutNone.contexts.global.oos).toBe(0.666)
+  const expectedGlobalOOS = 0.666 / (0.666 + 0.58) // 0.53
+  expect(withoutNone.contexts.global.oos).toBe(expectedGlobalOOS)
   expect(withoutNone.contexts.someTopic.oos).toBe(0.99)
   expect(withoutNone.contexts.global.intents.some(i => i.label === 'none')).toBe(false)
   expect(withoutNone.contexts.someTopic.intents.some(i => i.label === 'none')).toBe(false)
