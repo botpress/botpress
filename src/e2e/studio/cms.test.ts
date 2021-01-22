729e062a2cdeadc04c7cc8bdc251eaf4989effb8
Francois-Xavier P. Darveau <frxpoulin@gmail.com>
2021-01-21 22:31:26 -0500

    added minimal testing

--- src/e2e/studio/cms.test.ts
+++ src/e2e/studio/cms.test.ts
@@ -1,5 +1,7 @@
-import { clickOn, fillField } from '../expectPuppeteer'
-import { CONFIRM_DIALOG, expectBotApiCallSuccess, gotoStudio, waitForBotApiResponse } from '../utils'
+import path from 'path'
+
+import { clickOn, uploadFile } from '../expectPuppeteer'
+import { expectBotApiCallSuccess, gotoStudio } from '../utils'
 
 const getElementCount = async (): Promise<number> => {
   return (await page.$$('.icon-edit')).length
@@ -27,9 +29,23 @@ describe('Studio - CMS', () => {
     await expect(after).toBeLessThan(before)
   })
 
+  it('Create an image element', async () => {
+    const before = await getElementCount()
+    await page.waitForSelector('#btn-filter-builtin_image')
+    await page.hover('#btn-filter-builtin_image')
+    await clickOn('#btn-list-create-builtin_image')
+    await uploadFile('input[type="file"]', path.join(__dirname, '../assets/alien.png'))
+    await expectBotApiCallSuccess('media', 'POST')
+    await clickOn('.style__textarea___2P8hT')
+    await page.keyboard.type('We are not the same I am a martian')
+    await clickOn('button[type="submit"]')
+    await expectBotApiCallSuccess('content/builtin_image/elements', 'POST')
+    const after = await getElementCount()
+    expect(after).toBeGreaterThan(before)
+    //
+  })
+
   // it('Create text element', async () => {
-  //   const before = await getElementCount()
-  //   await clickOn('#btn-list-create-builtin_text')
 
   //   await page.keyboard.press('Tab')
   //   await page.keyboard.type('hey!')
