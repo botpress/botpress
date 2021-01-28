ae4d7ded176592a61aad4da314d9ea6aa17b7fc0
vizitiuRoman <romanv@readcast.net>
2021-01-28 12:05:19 +0200

    Add new router for notifications

--- modules/api-client/src/backend/api.ts
+++ modules/api-client/src/backend/api.ts
@@ -5,48 +5,75 @@ import Database from './db';
 
 export default (bp: typeof sdk, db: Database) => {
   const asyncMiddleware = asyncMw(bp.logger);
-  const router = bp.http.createRouterForBot('users');
 
-  router.get(
-    '/',
-    asyncMiddleware(async (req, res) => {
-      try {
-        const users = [{
-          username: 'Roman',
-          lastname: 'Vizitiu'
-        }];
-        res.send(users);
-      } catch (err) {
-        throw new StandardError('Cannot get users', err);
-      }
-    })
-  );
+  {
+    const router = bp.http.createRouterForBot('users');
 
-  router.get(
-    '/:login',
-    asyncMiddleware(async (req, res) => {
-      try {
-        const user = await db.getUserByLogin(req.params.login);
-        if (!user.length) {
-          throw new StandardError('Not found user by login');
+    router.get(
+      '/',
+      asyncMiddleware(async (req, res) => {
+        try {
+          const users = [{
+            username: 'Roman',
+            lastname: 'Vizitiu'
+          }];
+          res.send(users);
+        } catch (err) {
+          throw new StandardError('Cannot get users', err);
         }
-        res.send(user[0])
-      } catch (err) {
-        throw new StandardError('Cannot auth user', err);
-      }
-    })
-  );
+      })
+    );
 
-  router.post(
-    '/auth',
-    asyncMiddleware(async (req, res) => {
-      try {
-        const createdUser = await db.upsert(req.body);
-        res.send(createdUser[0])
-      } catch (err) {
-        throw new StandardError('Cannot auth user', err);
-      }
-    })
-  );
+    router.get(
+      '/:login',
+      asyncMiddleware(async (req, res) => {
+        try {
+          const user = await db.getUserByLogin(req.params.login);
+          if (!user.length) {
+            throw new StandardError('Not found user by login');
+          }
+          res.send(user[0]);
+        } catch (err) {
+          throw new StandardError('Cannot auth user', err);
+        }
+      })
+    );
+
+    router.post(
+      '/auth',
+      asyncMiddleware(async (req, res) => {
+        try {
+          const createdUser = await db.upsert(req.body);
+          res.send(createdUser[0]);
+        } catch (err) {
+          throw new StandardError('Cannot auth user', err);
+        }
+      })
+    );
+  }
+
+  {
+    const router = bp.http.createRouterForBot('notifications', {
+      checkAuthentication: false
+    });
+
+    router.post(
+      '/',
+      asyncMiddleware(async (req, res) => {
+        try {
+          const event = {
+            channel: 'web', target: req.body.target, botId: 'ecbv2'
+          }
+          const payloads = await bp.cms.renderElement(
+            'builtin_text', { text: req.body.message.body.en }, event
+          );
+          await bp.events.replyToEvent(event, payloads);
+          return res.status(200).send([]);
+        } catch (err) {
+          throw new StandardError('Cannot get users', err);
+        }
+      })
+    );
+  }
 
 }
--- modules/api-client/src/types.ts
+++ modules/api-client/src/types.ts
@@ -2,5 +2,7 @@ import { ReqUserData } from '@rdcdev/dbank-client';
 
 export interface User {
   login: string;
+  userId: string;
+  channel: string;
   req_user_data: ReqUserData
 }
