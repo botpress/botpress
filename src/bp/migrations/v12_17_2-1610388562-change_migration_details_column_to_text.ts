50745916c8cccee5b65fceb72f5d20edddf4b1fd
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-02-12 13:07:01 -0500

    duplicated the migration + added explanation

--- src/bp/migrations/v12_17_2-1610388562-change_migration_details_column_to_text.ts
+++ src/bp/migrations/v12_17_2-1610388562-change_migration_details_column_to_text.ts
@@ -1,6 +1,3 @@
-// Since the table creation did not reflect this migration from 12.16.2 to 12.17.2,
-// we had to duplicate it so no version would not be affected.
-// See: https://github.com/botpress/botpress/pull/4513
 import * as sdk from 'botpress/sdk'
 import { Migration } from 'core/services/migration'
 
