4d48f309d0ad58b3ae5facdec9eb30b7b8c0764f
Samuel Massé <samuelmasse4@gmail.com>
2021-02-12 18:00:37 -0500

    fast migration

--- modules/channel-web/src/migrations/v12_18_0-1613001263-messaging.ts
+++ modules/channel-web/src/migrations/v12_18_0-1613001263-messaging.ts
@@ -9,75 +9,51 @@ const migration: sdk.ModuleMigration = {
     if (await bp.database.schema.hasTable('web_conversations')) {
       console.log('Migrating conversations...')
 
-      const batchSize = 1000
-      const conversationsCount = <number>(await bp.database('web_conversations').count())[0].count
-      let maxId = 0
+      await bp.database.raw(`
+        INSERT INTO "conversations" (
+          "id",
+          "userId",
+          "botId",
+          "createdOn")
+        SELECT "web_conversations"."id",
+          "web_conversations"."userId",
+          "web_conversations"."botId",
+          "web_conversations"."created_on"
+        FROM "web_conversations"`)
 
-      for (let i = 0; i < conversationsCount; i += batchSize) {
-        const conversations = await bp
-          .database('web_conversations')
-          .select('*')
-          .offset(i)
-          .limit(batchSize)
-
-        conversations.forEach(conversation => {
-          conversation.createdOn = conversation.created_on
-
-          delete conversation.title
-          delete conversation.description
-          delete conversation.logo_url
-          delete conversation.created_on
-          delete conversation.last_heard_on
-          delete conversation.user_last_seen_on
-          delete conversation.bot_last_seen_on
-
-          maxId = Math.max(maxId, conversation.id)
-        })
-
-        await bp.database('conversations').insert(conversations)
-      }
-
-      if (bp.database.isLite) {
-        await bp.database.raw(`ALTER TABLE conversations AUTO_INCREMENT = ${maxId + 100}`)
-      } else {
-        await bp.database.raw(`ALTER SEQUENCE conversations_id_seq RESTART WITH ${maxId + 100}`)
+      if (!bp.database.isLite) {
+        const maxId = <number>Object.values((await bp.database('conversations').max('id'))[0])[0]
+        await bp.database.raw(`ALTER SEQUENCE conversations_id_seq RESTART WITH ${maxId + 1000}`)
       }
     }
 
     if (await bp.database.schema.hasTable('web_messages')) {
       console.log('Migrating messages...')
 
-      const batchSize = 1000
-      const messagesCount = <number>(await bp.database('web_messages').count())[0].count
+      await bp.database.raw(`
+        INSERT INTO "messages" ("conversationId",
+          "eventId",
+          "incomingEventId",
+          "from",
+          "sentOn",
+          "payload")
+        SELECT "web_messages"."conversationId",
+          "web_messages"."eventId",
+          "web_messages"."incomingEventId",
+          "web_messages"."userId",
+          "web_messages"."sent_on",
+          "web_messages"."payload"
+        FROM "web_messages"`)
 
-      for (let i = 0; i < messagesCount; i += batchSize) {
-        const messages = await bp
-          .database('web_messages')
-          .select('*')
-          .offset(i)
-          .limit(batchSize)
+      await bp
+        .database('messages')
+        .update({ from: 'user' })
+        .whereNotNull('from')
 
-        messages.forEach(message => {
-          message.sentOn = message.sent_on
-          message.from = message.userId ? 'user' : 'bot'
-          message.payload = {
-            ...(message.payload || {}),
-            web: { userName: message.full_name, avatarUrl: message.avatar_url }
-          }
-
-          delete message.id
-          delete message.userId
-          delete message.message_type
-          delete message.message_text
-          delete message.message_raw
-          delete message.message_data
-          delete message.full_name
-          delete message.avatar_url
-          delete message.sent_on
-        })
-
-        await bp.database('messages').insert(messages)
-      }
+      await bp
+        .database('messages')
+        .update({ from: 'bot' })
+        .whereNull('from')
     }
 
     return { success: true, message: 'Tables migrated successfully' }
