73f6edeb2eabb5b913bec11bab82526001345776
Samuel Massé <samuelmasse4@gmail.com>
2021-02-12 19:56:42 -0500

    tests

---
+++ src/bp/core/services/functions.test.ts
@@ -0,0 +1,43 @@
+import 'bluebird-global'
+import 'jest-extended'
+import 'reflect-metadata'
+
+import { CustomFunctionService } from './functions'
+
+describe('Functions', () => {
+  const functions = new CustomFunctionService()
+
+  test('Function call', () => {
+    functions.forBot('brobot').register('broacts', {
+      patsback: () => 'pats bro',
+      saybro: () => 'bro'
+    })
+
+    const broacts = functions.forBot('brobot').get('broacts')
+    expect(broacts.patsback()).toEqual('pats bro')
+    expect(broacts.saybro()).toEqual('bro')
+  })
+
+  test('Global function call', () => {
+    functions.global().register('api', {
+      doThing: (number: number) => number + 2
+    })
+
+    const api = functions.global().get('api')
+    expect(api.doThing(4)).toEqual(6)
+  })
+
+  test('Remove functions for bot', () => {
+    functions.forBot('bakugan').register('bakuganAPI', {
+      goBankrupt: () => 'yes'
+    })
+
+    let bakuganAPI = functions.forBot('bakugan').get('bakuganAPI')
+    expect(bakuganAPI.goBankrupt).not.toEqual(undefined)
+
+    functions.removeFunctionsForBot('bakugan')
+
+    bakuganAPI = functions.forBot('bakugan').get('bakuganAPI')
+    expect(bakuganAPI).toEqual(undefined)
+  })
+})
