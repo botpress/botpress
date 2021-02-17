af8ee2b0c47e1c7379958185ceb839386f774afe
Samuel Massé <samuelmasse4@gmail.com>
2021-02-16 21:05:37 -0500

    working

--- modules/channel-telegram/src/backend/client.ts
+++ modules/channel-telegram/src/backend/client.ts
@@ -10,24 +10,29 @@ const outgoingTypes = ['text', 'typing', 'image', 'login_prompt', 'carousel']
 
 export const sendEvent = async (bp: typeof sdk, botId: string, ctx: ContextMessageUpdate, args: { type: string }) => {
   // NOTE: getUpdate and setWebhook dot not return the same context mapping
-  const threadId = _.get(ctx, 'chat.id') || _.get(ctx, 'message.chat.id')
-  const target = _.get(ctx, 'from.id') || _.get(ctx, 'message.from.id')
+  const chatId = `${ctx.chat?.id || ctx.message?.chat.id}`
+  const userId = `${ctx.from?.id || ctx.message?.from.id}`
 
-  const payload = _.get(ctx, 'message') || _.get(ctx, 'callback_query')
-  const preview = _.get(ctx, 'message.text') || _.get(ctx, 'callback_query.data')
+  const payload = ctx.message || ctx.callbackQuery
+  const preview = ctx.message.text || ctx.callbackQuery.data
 
-  await bp.events.sendEvent(
-    bp.IO.Event({
-      botId,
-      payload,
-      preview,
-      channel: 'telegram',
-      direction: 'incoming',
-      threadId: threadId && threadId.toString(),
-      target: target && target.toString(),
-      ...args
-    })
-  )
+  let convoId: number
+  if (chatId) {
+    convoId = +(await bp.kvs.forBot(botId).get(`convmap/telegram_${chatId}`))
+
+    if (!convoId) {
+      const conversation = await bp.messaging.createConversation({ botId, userId })
+      convoId = conversation.id
+
+      await bp.kvs.forBot(botId).set(`convmap/telegram_${chatId}`, conversation.id)
+      await bp.kvs.forBot(botId).set(`rconvmap/telegram_${conversation.id}`, chatId)
+    }
+  } else {
+    const conversation = await bp.messaging.getOrCreateRecentConversation({ botId, userId })
+    convoId = conversation.id
+  }
+
+  await bp.messaging.sendIncoming(convoId, { ...args, ...payload }, { channel: 'telegram', preview })
 }
 
 export const registerMiddleware = (bp: typeof sdk, outgoingHandler) => {
@@ -55,7 +60,7 @@ export async function setupBot(bp: typeof sdk, botId: string, clients: Clients) 
 export async function setupMiddleware(bp: typeof sdk, clients: Clients) {
   registerMiddleware(bp, outgoingHandler)
 
-  async function outgoingHandler(event: sdk.IO.Event, next: sdk.IO.MiddlewareNextCallback) {
+  async function outgoingHandler(event: sdk.IO.OutgoingEvent, next: sdk.IO.MiddlewareNextCallback) {
     if (event.channel !== 'telegram') {
       return next()
     }
@@ -66,7 +71,7 @@ export async function setupMiddleware(bp: typeof sdk, clients: Clients) {
     }
 
     const messageType = event.type === 'default' ? 'text' : event.type
-    const chatId = event.threadId || event.target
+    const chatId = (await bp.kvs.forBot(event.botId).get(`rconvmap/telegram_${event.threadId}`)) || event.target
 
     if (!_.includes(outgoingTypes, messageType)) {
       return next(new Error(`Unsupported event type: ${event.type}`))
@@ -84,6 +89,8 @@ export async function setupMiddleware(bp: typeof sdk, clients: Clients) {
       // TODO We don't support sending files, location requests (and probably more) yet
       throw new Error(`Message type "${messageType}" not implemented yet`)
     }
+
+    await bp.messaging.createMessage(+event.threadId, event.id, event.incomingEventId, 'bot', event.payload)
 
     next(undefined, false)
   }
