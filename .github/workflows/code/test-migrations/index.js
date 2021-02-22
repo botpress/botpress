6776c60edcc59c11db02383d175ac965804c4990
allardy <yann.allard1@gmail.com>
2021-02-22 16:22:26 -0500

    cleanup

--- .github/workflows/code/test-migrations/index.js
+++ .github/workflows/code/test-migrations/index.js
@@ -1,6 +1,6 @@
 require('bluebird-global')
 const exec = require('child_process').exec
-const archive = require('../../../../out/bp/core/misc/archive')
+const archive = require('./out/bp/core/misc/archive')
 const fs = require('fs')
 const rimraf = require('rimraf')
 const path = require('path')
@@ -23,22 +23,22 @@ const start = async () => {
   const dir = await Promise.fromCallback(cb => s3.listObjectsV2({ Bucket }, cb))
   for (const file of dir.Contents) {
     const [botName, version] = file.Key.split('_')
-    const cleanVersion = version.replace(/.tgz|.zip/, '')
+    const archiveVersion = version.replace(/.tgz|.zip/, '')
 
     const buffer = await Promise.fromCallback(cb => s3.getObject({ Bucket, Key: file.Key }, cb))
     await prepareDataFolder(buffer.Body)
 
-    await testMigration(botName, cleanVersion, targetVersion)
-    await testMigration(botName, targetVersion, cleanVersion, true)
+    await testMigration(botName, archiveVersion, targetVersion, { isDown: false })
+    await testMigration(botName, targetVersion, archiveVersion, { isDown: true })
   }
 }
 
 const prepareDataFolder = async buffer => {
-  await Promise.fromCallback(cb => rimraf(`./out/bp/data`, cb))
-  await archive.extractArchive(buffer, `./out/bp`)
+  await Promise.fromCallback(cb => rimraf('./out/bp/data', cb))
+  await archive.extractArchive(buffer, './out/bp')
 }
 
-const testMigration = async (botName, startVersion, targetVersion, isDown) => {
+const testMigration = async (botName, startVersion, targetVersion, { isDown }) => {
   let stdoutBuffer = ''
   await Promise.fromCallback(cb => {
     const ctx = exec(`yarn start migrate ${isDown ? 'down' : 'up'} --target ${targetVersion}`, { cwd: './' }, err =>
@@ -49,21 +49,21 @@ const testMigration = async (botName, startVersion, targetVersion, isDown) => {
 
   const success = stdoutBuffer.match(/Migration(s?) completed successfully/)
 
+  if (!success) {
+    core.setFailed(`Migration failed. Please fix it`)
+    console.log(stdoutBuffer)
+  }
+
   console.log(
     `${success ? chalk.green(`[SUCCESS]`) : chalk.red(`[FAILURE]`)} Migration ${
       isDown ? 'DOWN' : 'UP'
     } of ${botName} (${startVersion} -> ${targetVersion})`
   )
-
-  if (!success) {
-    core.setFailed(`Migration failed. Please fix it`)
-    console.log(stdoutBuffer)
-  }
 }
 
 const getMostRecentVersion = () => {
-  const coreMigrations = getMigrations(`./out/bp`)
-  const modules = fs.readdirSync(`./modules`)
+  const coreMigrations = getMigrations('./out/bp')
+  const modules = fs.readdirSync('./modules')
 
   const moduleMigrations = _.flatMap(modules, module => getMigrations(`./modules/${module}/dist`))
   const versions = [...coreMigrations, ...moduleMigrations].map(x => x.version).sort(semver.compare)
