25eb6c78ca0bd6dc451e2f1dfb672bfd3713189b
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-02-24 11:54:20 -0500

    chore(core): improve cache-invalidator

--- src/bp/core/services/ghost/cache-invalidators.ts
+++ src/bp/core/services/ghost/cache-invalidators.ts
@@ -8,72 +8,52 @@ import { forceForwardSlashes } from '../../misc/utils'
 import { TYPES } from '../../types'
 
 export namespace CacheInvalidators {
-  enum ChangeEventAction {
-    CREATED = 0,
-    DELETED = 1,
-    MODIFIED = 2,
-    RENAMED = 3
-  }
-
-  /**
-   * See https://github.com/Axosoft/nsfw/tree/master/docs
-   */
-  interface ChangeEventType {
-    /** the type of event that occurred */
-    action: ChangeEventAction
-    /** the location the event took place */
-    directory: string
-    /** the name of the file that was changed (Not available for rename events) */
-    file: string
-    /** the name of the file before a rename (Only available for rename events) */
-    oldFile: string
-    /** the name of the file after a rename (Only available for rename events) */
-    newFile: string
-  }
-
   @injectable()
   export class FileChangedInvalidator {
+    private watcher?: chokidar.FSWatcher
+    private cache?: ObjectCache
+
     constructor(
       @inject(TYPES.Logger)
       @tagged('name', 'CacheInvalidator')
       private logger: Logger
     ) {}
-    watcher!: {
-      start: Function
-      stop: Function
-    }
-    cache?: ObjectCache
 
-    install(objectCache: ObjectCache) {
+    public install(objectCache: ObjectCache) {
+      if (!objectCache) {
+        return
+      }
+
       this.cache = objectCache
 
+      // TODO: move this into a constant/utils file
       const foldersToWatch = [
         path.join(process.PROJECT_LOCATION, 'data', 'bots'),
         path.join(process.PROJECT_LOCATION, 'data', 'global')
       ]
 
-      const watcher = chokidar.watch(foldersToWatch, {
+      this.watcher = chokidar.watch(foldersToWatch, {
         ignoreInitial: true,
         ignorePermissionErrors: true
       })
 
-      watcher.on('add', this.handle)
-      watcher.on('change', this.handle)
-      watcher.on('unlink', this.handle)
-      watcher.on('error', err => this.logger.attachError(err).error('Watcher error'))
+      this.watcher.on('add', this._handle)
+      this.watcher.on('change', this._handle)
+      this.watcher.on('unlink', this._handle)
+      this.watcher.on('error', err => this.logger.attachError(err).error('Watcher error'))
     }
 
-    async stop() {
-      await this.watcher.stop()
+    private _relativePath = (file: string): string => {
+      return forceForwardSlashes(path.relative(process.PROJECT_LOCATION, path.dirname(file)))
     }
 
-    handle = async file => {
+    private _handle = async (file: string) => {
       if (!this.cache) {
         return
       }
 
-      const relativePath = forceForwardSlashes(path.relative(process.PROJECT_LOCATION, path.dirname(file)))
-      this.cache.events.emit('invalidation', relativePath)
+      const relativePath = this._relativePath(file)
+
       await this.cache.invalidateStartingWith(relativePath)
     }
   }
