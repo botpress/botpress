87124e718d8f789e51dfc2d4ba73342b11331f8e
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-02-24 15:13:24 -0500

    PR comments + ut improvements

--- src/bp/common/object-cache.ts
+++ src/bp/common/object-cache.ts
@@ -2,9 +2,9 @@ import { EventEmitter } from 'events'
 
 export interface ObjectCache {
   readonly events: EventEmitter
-  get<T>(key: string): Promise<T>
-  set<T>(key: string, obj: T): Promise<void>
-  has(key: string): Promise<boolean>
+  get<T>(key: string): T
+  set<T>(key: string, obj: T): void
+  has(key: string): boolean
   invalidate(key: string): Promise<void>
   invalidateStartingWith(prefix: string): Promise<void>
   sync(message: string): Promise<void>
--- src/bp/core/services/ghost/memory-cache.test.ts
+++ src/bp/core/services/ghost/memory-cache.test.ts
@@ -5,7 +5,7 @@ import 'jest-extended'
 import LRU from 'lru-cache'
 import sizeof from 'object-sizeof'
 
-import { createSpyObject, MockObject } from '../../misc/utils'
+import { createSpyObject, MockObject, asBytes } from '../../misc/utils'
 
 import { ObjectCache } from 'common/object-cache'
 import MemoryObjectCache, { EVENTS } from './memory-cache'
@@ -17,10 +17,10 @@ const ANOTHER_KEY = 'another key'
 const A_VALUE = 'a value'
 const AN_OBJECT_VALUE = { key: 'value' }
 const HUGE_OBJECT_VALUE = Array.from({ length: 40 }, () => Math.floor(Math.random() * 40).toString())
-const MAX_CACHE_SIZE = 24 // MAX_CACHE_SIZE must be less than the size of HUGE_OBJECT_VALUE
+const MAX_CACHE_SIZE = 50 // MAX_CACHE_SIZE must be less than the size of HUGE_OBJECT_VALUE
 const SYNC_MESSAGE = 'a message'
 
-const OLD_BP_MAX_MEMORY_CACHE_SIZE = process.core_env.BP_MAX_MEMORY_CACHE_SIZE
+const OLD_BP_MAX_MEMORY_CACHE_SIZE = process.env.BP_MAX_MEMORY_CACHE_SIZE
 
 describe('Memory Cache', () => {
   let cache: ObjectCache
@@ -29,8 +29,7 @@ describe('Memory Cache', () => {
   let emptyCache: () => void
 
   beforeEach(() => {
-    // @ts-ignore
-    process.core_env.BP_MAX_MEMORY_CACHE_SIZE = `${MAX_CACHE_SIZE}b`
+    process.env.BP_MAX_MEMORY_CACHE_SIZE = `${MAX_CACHE_SIZE}b`
 
     cacheInvalidator = createSpyObject<CacheInvalidators.FileChangedInvalidator>()
     cache = new MemoryObjectCache(cacheInvalidator.T)
@@ -50,8 +49,7 @@ describe('Memory Cache', () => {
   })
 
   afterAll(() => {
-    // @ts-ignore
-    process.core_env.BP_MAX_MEMORY_CACHE_SIZE = OLD_BP_MAX_MEMORY_CACHE_SIZE
+    process.env.BP_MAX_MEMORY_CACHE_SIZE = OLD_BP_MAX_MEMORY_CACHE_SIZE
   })
 
   describe('Initialize', () => {
@@ -60,6 +58,18 @@ describe('Memory Cache', () => {
 
       expect(lru).not.toBeUndefined()
       expect(lru.max).toEqual(MAX_CACHE_SIZE)
+      expect(lru.lengthCalculator).toEqual(sizeof)
+      expect(lru.length).toEqual(0)
+      expect(lru.itemCount).toEqual(0)
+    })
+
+    it('LRU cache is initialized with default max size', () => {
+      delete process.env.BP_MAX_MEMORY_CACHE_SIZE
+      cache = new MemoryObjectCache(cacheInvalidator.T)
+      const lru = cache['cache'] as LRU<string, any>
+
+      expect(lru).not.toBeUndefined()
+      expect(lru.max).toEqual(asBytes(MemoryObjectCache.DEFAULT_MAX_MEMORY_CACHE_SIZE))
       expect(lru.lengthCalculator).toEqual(sizeof)
       expect(lru.length).toEqual(0)
       expect(lru.itemCount).toEqual(0)
@@ -81,90 +91,90 @@ describe('Memory Cache', () => {
   })
 
   describe('Set', () => {
-    beforeEach(async () => {
+    beforeEach(() => {
       emptyCache()
     })
 
-    it('Caches any type of values', async () => {
+    it('Caches any type of values', () => {
       const values = [[], null, undefined, '', 0, 0.0, true, {}, () => {}]
 
       for (const value of values) {
-        await cache.set(A_KEY, value)
+        cache.set(A_KEY, value)
 
-        expect(await cache.get(A_KEY)).toEqual(value)
-        expect(typeof (await cache.get(A_KEY))).toEqual(typeof value)
+        expect(cache.get(A_KEY)).toEqual(value)
+        expect(typeof cache.get(A_KEY)).toEqual(typeof value)
       }
     })
 
-    it('Does not cache a value which size is bigger then the size of the cache', async () => {
+    it('Does not cache a value which size is bigger then the size of the cache', () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
       expect(sizeof(HUGE_OBJECT_VALUE) > MAX_CACHE_SIZE).toBeTrue()
 
-      await cache.set(A_KEY, HUGE_OBJECT_VALUE)
+      cache.set(A_KEY, HUGE_OBJECT_VALUE)
 
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
       expect(callback).not.toHaveBeenCalled()
     })
 
-    it('Removes a key that is being overridden if the value size is bigger then the size of the cache', async () => {
+    it('Removes a key that is being overridden if the value size is bigger then the size of the cache', () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
       expect(sizeof(HUGE_OBJECT_VALUE) > MAX_CACHE_SIZE).toBeTrue()
 
-      await cache.set(A_KEY, A_VALUE)
-      await cache.set(A_KEY, HUGE_OBJECT_VALUE)
+      cache.set(A_KEY, A_VALUE)
+      cache.set(A_KEY, HUGE_OBJECT_VALUE)
 
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
       expect(callback).not.toHaveBeenCalled()
     })
 
-    it('Overrides the value when a new one is provided', async () => {
-      await cache.set(A_KEY, A_VALUE)
-      expect(await cache.get(A_KEY)).toEqual(A_VALUE)
+    it('Overrides the value when a new one is provided', () => {
+      cache.set(A_KEY, A_VALUE)
+      expect(cache.get(A_KEY)).toEqual(A_VALUE)
 
-      await cache.set(A_KEY, AN_OBJECT_VALUE)
-      expect(await cache.get(A_KEY)).toEqual(AN_OBJECT_VALUE)
+      cache.set(A_KEY, AN_OBJECT_VALUE)
+      expect(cache.get(A_KEY)).toEqual(AN_OBJECT_VALUE)
     })
 
-    it(`Emits an ${EVENTS.invalidation} event when a value is overridden`, async () => {
+    it(`Emits an ${EVENTS.invalidation} event when a value is overridden`, () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
-      await cache.set(A_KEY, A_VALUE)
-      expect(await cache.get(A_KEY)).toEqual(A_VALUE)
+      cache.set(A_KEY, A_VALUE)
+      expect(cache.get(A_KEY)).toEqual(A_VALUE)
 
-      await cache.set(A_KEY, AN_OBJECT_VALUE)
-      expect(await cache.get(A_KEY)).toEqual(AN_OBJECT_VALUE)
+      cache.set(A_KEY, AN_OBJECT_VALUE)
+      expect(cache.get(A_KEY)).toEqual(AN_OBJECT_VALUE)
 
       expect(callback).toHaveBeenCalledTimes(1)
       expect(callback).toHaveBeenCalledWith(A_KEY)
     })
 
-    it(`Does not emit an ${EVENTS.invalidation} event when no value is overridden`, async () => {
+    it(`Does not emit an ${EVENTS.invalidation} event when no value is overridden`, () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
-      await cache.set(A_KEY, A_VALUE)
-      expect(await cache.get(A_KEY)).toEqual(A_VALUE)
+      cache.set(A_KEY, A_VALUE)
+      expect(cache.get(A_KEY)).toEqual(A_VALUE)
 
       expect(callback).not.toHaveBeenCalled()
     })
 
-    it(`Does not emit an ${EVENTS.invalidation} event if the key was not inserted into the cache`, async () => {
+    it(`Does not emit an ${EVENTS.invalidation} event if the key was not inserted into the cache`, () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
       expect(sizeof(HUGE_OBJECT_VALUE) > MAX_CACHE_SIZE).toBeTrue()
 
-      await cache.set(A_KEY, A_VALUE)
-      await cache.set(A_KEY, HUGE_OBJECT_VALUE)
+      cache.set(A_KEY, A_VALUE)
+      cache.set(A_KEY, HUGE_OBJECT_VALUE)
 
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
       expect(callback).not.toHaveBeenCalled()
     })
@@ -186,27 +196,27 @@ describe('Memory Cache', () => {
     })
 
     it('Removes the proper value from the cache', async () => {
-      await cache.set(A_KEY, A_VALUE)
-      expect(await cache.has(A_KEY)).toBeTrue()
+      cache.set(A_KEY, A_VALUE)
+      expect(cache.has(A_KEY)).toBeTrue()
 
-      await cache.set(ANOTHER_KEY, AN_OBJECT_VALUE)
-      expect(await cache.has(ANOTHER_KEY)).toBeTrue()
+      cache.set(ANOTHER_KEY, AN_OBJECT_VALUE)
+      expect(cache.has(ANOTHER_KEY)).toBeTrue()
 
       await cache.invalidate(A_KEY)
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
-      expect(await cache.has(ANOTHER_KEY)).toBeTrue()
+      expect(cache.has(ANOTHER_KEY)).toBeTrue()
     })
 
     it(`Emits an ${EVENTS.invalidation} event when a value is removed`, async () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
-      await cache.set(A_KEY, A_VALUE)
-      expect(await cache.has(A_KEY)).toBeTrue()
+      cache.set(A_KEY, A_VALUE)
+      expect(cache.has(A_KEY)).toBeTrue()
 
       await cache.invalidate(A_KEY)
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
       expect(callback).toHaveBeenCalledTimes(1)
       expect(callback).toHaveBeenCalledWith(A_KEY)
@@ -216,61 +226,61 @@ describe('Memory Cache', () => {
       const callback = jest.fn()
       cache.events.on(EVENTS.invalidation, key => callback(key))
 
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
       await cache.invalidate(A_KEY)
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
 
       expect(callback).not.toHaveBeenCalled()
     })
   })
 
   describe('Get', () => {
-    it('Returns undefined when the cache is empty', async () => {
+    it('Returns undefined when the cache is empty', () => {
       assertCacheIsEmpty()
 
-      expect(await cache.get(A_KEY)).toBeUndefined()
+      expect(cache.get(A_KEY)).toBeUndefined()
     })
 
-    it('Returns undefined if the key does not exist', async () => {
-      await cache.set(A_KEY, A_VALUE)
+    it('Returns undefined if the key does not exist', () => {
+      cache.set(A_KEY, A_VALUE)
 
-      expect(await cache.has(AN_INVALID_KEY)).toBeFalse()
-      expect(await cache.has(A_KEY)).toBeTrue()
+      expect(cache.has(AN_INVALID_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeTrue()
 
-      expect(await cache.get(AN_INVALID_KEY)).toBeUndefined()
-      expect(await cache.get(A_KEY)).toEqual(A_VALUE)
+      expect(cache.get(AN_INVALID_KEY)).toBeUndefined()
+      expect(cache.get(A_KEY)).toEqual(A_VALUE)
     })
 
-    it('Returns the object value from the cache', async () => {
-      await cache.set(A_KEY, AN_OBJECT_VALUE)
+    it('Returns the object value from the cache', () => {
+      cache.set(A_KEY, AN_OBJECT_VALUE)
 
-      expect(typeof (await cache.get<typeof AN_OBJECT_VALUE>(A_KEY))).toBe(typeof AN_OBJECT_VALUE)
-      expect(await cache.get<typeof AN_OBJECT_VALUE>(A_KEY)).toEqual(AN_OBJECT_VALUE)
+      expect(typeof cache.get<typeof AN_OBJECT_VALUE>(A_KEY)).toBe(typeof AN_OBJECT_VALUE)
+      expect(cache.get<typeof AN_OBJECT_VALUE>(A_KEY)).toEqual(AN_OBJECT_VALUE)
     })
   })
 
   describe('Has', () => {
-    beforeEach(async () => {
+    beforeEach(() => {
       emptyCache()
     })
 
-    it('Returns false when the cache is empty', async () => {
+    it('Returns false when the cache is empty', () => {
       assertCacheIsEmpty()
 
-      expect(await cache.has(A_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeFalse()
     })
 
-    it('Returns false if the key does not exist', async () => {
-      await cache.set(A_KEY, A_VALUE)
+    it('Returns false if the key does not exist', () => {
+      cache.set(A_KEY, A_VALUE)
 
-      expect(await cache.has(AN_INVALID_KEY)).toBeFalse()
-      expect(await cache.has(A_KEY)).toBeTrue()
+      expect(cache.has(AN_INVALID_KEY)).toBeFalse()
+      expect(cache.has(A_KEY)).toBeTrue()
     })
 
-    it('Returns true when the key exist', async () => {
-      await cache.set(A_KEY, AN_OBJECT_VALUE)
+    it('Returns true when the key exist', () => {
+      cache.set(A_KEY, AN_OBJECT_VALUE)
 
-      expect(await cache.has(A_KEY)).toBeTrue()
+      expect(cache.has(A_KEY)).toBeTrue()
     })
   })
 
@@ -287,7 +297,7 @@ describe('Memory Cache', () => {
   })
 
   describe('InvalidateStartingWith', () => {
-    beforeEach(async () => {
+    beforeEach(() => {
       emptyCache()
     })
 
@@ -308,21 +318,26 @@ describe('Memory Cache', () => {
       const stringKey = `string::${A_KEY}`
       const keys = [bufferKey, objectKey, stringKey]
 
+      expect(sizeof(A_VALUE) * keys.length <= MAX_CACHE_SIZE).toBeTrue()
+
       for (const key of keys) {
-        await cache.set(key, A_VALUE)
-        expect(await cache.has(key)).toBeTrue()
+        cache.set(key, A_VALUE)
       }
 
-      await cache.set(ANOTHER_KEY, AN_OBJECT_VALUE)
-      expect(await cache.has(ANOTHER_KEY)).toBeTrue()
+      for (const key of keys) {
+        expect(cache.has(key)).toBeTrue()
+      }
+
+      cache.set(ANOTHER_KEY, AN_OBJECT_VALUE)
+      expect(cache.has(ANOTHER_KEY)).toBeTrue()
 
       await cache.invalidateStartingWith(A_KEY)
 
       for (const key of keys) {
-        expect(await cache.has(key)).toBeFalse()
+        expect(cache.has(key)).toBeFalse()
       }
 
-      expect(await cache.has(ANOTHER_KEY)).toBeTrue()
+      expect(cache.has(ANOTHER_KEY)).toBeTrue()
     })
   })
 })
