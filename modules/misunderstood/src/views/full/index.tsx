d1afab36acf422294d9e18fc5e20d8e031d7233d
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-03-08 16:37:30 -0500

    Reset on date range change

--- modules/misunderstood/src/views/full/index.tsx
+++ modules/misunderstood/src/views/full/index.tsx
@@ -254,7 +254,8 @@ export default class MisunderstoodMainView extends React.Component<Props, State>
       selectedEventIndex: 0,
       selectedEvent: firstEvent,
       eventNotFound: !firstEvent,
-      eventCounts
+      eventCounts,
+      checkedEventIds: []
     })
   }
 
