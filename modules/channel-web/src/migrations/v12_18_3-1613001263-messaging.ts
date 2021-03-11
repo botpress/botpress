7aae34b919092682ee6f9583c2fe0aa148bc5c4d
Samuel Massé <samuelmasse4@gmail.com>
2021-03-10 19:23:04 -0500

    fix

--- modules/channel-web/src/migrations/v12_18_3-1613001263-messaging.ts
+++ modules/channel-web/src/migrations/v12_18_3-1613001263-messaging.ts
@@ -6,67 +6,72 @@ const migration: sdk.ModuleMigration = {
     type: 'database'
   },
   up: async ({ bp }: sdk.ModuleMigrationOpts): Promise<sdk.MigrationResult> => {
-    await bp.database.schema.createTable('temp_new_convo_ids', table => {
-      table
-        .integer('oldId')
-        .primary()
-        .references('id')
-        .inTable('web_conversations')
-      table.uuid('newId')
-    })
+    if (bp.database.isLite) {
+      // TODO do migration for sqlite
+      throw 'migration for sqlite not yet implemented!'
+    } else {
+      await bp.database.schema.createTable('temp_new_convo_ids', table => {
+        table
+          .integer('oldId')
+          .primary()
+          .references('id')
+          .inTable('web_conversations')
+        table.uuid('newId')
+      })
 
-    await bp.database.raw(`
-    INSERT INTO "temp_new_convo_ids" (
-      "oldId",
-      "newId")
-    SELECT
-      "web_conversations"."id",
-      gen_random_uuid()
-    FROM "web_conversations"`)
+      await bp.database.raw(`
+      INSERT INTO "temp_new_convo_ids" (
+        "oldId",
+        "newId")
+      SELECT
+        "web_conversations"."id",
+        gen_random_uuid()
+      FROM "web_conversations"`)
 
-    await bp.database.raw(`
-    INSERT INTO "conversations" (
-      "id",
-      "userId",
-      "botId",
-      "createdOn")
-    SELECT "temp_new_convo_ids"."newId",
-      "web_conversations"."userId",
-      "web_conversations"."botId",
-      "web_conversations"."created_on"
-    FROM "web_conversations"
-    INNER JOIN "temp_new_convo_ids" ON "web_conversations"."id" = "temp_new_convo_ids"."oldId"`)
+      await bp.database.raw(`
+      INSERT INTO "conversations" (
+        "id",
+        "userId",
+        "botId",
+        "createdOn")
+      SELECT "temp_new_convo_ids"."newId",
+        "web_conversations"."userId",
+        "web_conversations"."botId",
+        "web_conversations"."created_on"
+      FROM "web_conversations"
+      INNER JOIN "temp_new_convo_ids" ON "web_conversations"."id" = "temp_new_convo_ids"."oldId"`)
 
-    await bp.database.raw(`
-    INSERT INTO "messages" (
-      "id",
-      "conversationId",
-      "eventId",
-      "incomingEventId",
-      "from",
-      "sentOn",
-      "payload")
-    SELECT gen_random_uuid(),
-      "temp_new_convo_ids"."newId",
-      "web_messages"."eventId",
-      "web_messages"."incomingEventId",
-      "web_messages"."userId",
-      "web_messages"."sent_on",
-      "web_messages"."payload"
-    FROM "web_messages"
-    INNER JOIN "temp_new_convo_ids" ON "web_messages"."conversationId" = "temp_new_convo_ids"."oldId"`)
+      await bp.database.raw(`
+      INSERT INTO "messages" (
+        "id",
+        "conversationId",
+        "eventId",
+        "incomingEventId",
+        "from",
+        "sentOn",
+        "payload")
+      SELECT gen_random_uuid(),
+        "temp_new_convo_ids"."newId",
+        "web_messages"."eventId",
+        "web_messages"."incomingEventId",
+        "web_messages"."userId",
+        "web_messages"."sent_on",
+        "web_messages"."payload"
+      FROM "web_messages"
+      INNER JOIN "temp_new_convo_ids" ON "web_messages"."conversationId" = "temp_new_convo_ids"."oldId"`)
 
-    await bp
-      .database('messages')
-      .update({ from: 'user' })
-      .whereNotNull('from')
+      await bp
+        .database('messages')
+        .update({ from: 'user' })
+        .whereNotNull('from')
 
-    await bp
-      .database('messages')
-      .update({ from: 'bot' })
-      .whereNull('from')
+      await bp
+        .database('messages')
+        .update({ from: 'bot' })
+        .whereNull('from')
 
-    bp.database.schema.dropTable('temp_new_convo_ids')
+      await bp.database.schema.dropTable('temp_new_convo_ids')
+    }
 
     return { success: true, message: 'Tables migrated successfully' }
   }
