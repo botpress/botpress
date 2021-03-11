efcea3f07ccccc17d696f57a48023dc21ab796fe
Pierre Snell <pierre.snell@botpress.com>
2021-03-11 16:22:44 -0500

    fixed sly comment

--- modules/nlu/src/views/full/intents/slots/EntitySelector.tsx
+++ modules/nlu/src/views/full/intents/slots/EntitySelector.tsx
@@ -4,6 +4,7 @@ import { lang } from 'botpress/shared'
 import React, { FC, useEffect, useState } from 'react'
 import { NLUApi } from '../../../../api'
 import style from '../style.scss'
+import { entityNameInput } from './style.scss'
 
 const SYSTEM_ENTITIES = [
   'amountOfMoney',
@@ -55,11 +56,20 @@ export const EntitySelector: FC<Props> = props => {
     }
   }
 
+  const getEntityName = (entity: EntityOption): string => {
+    let entityName = `${entity.type}.${entity.name}`
+    const translatedName = lang.tr('module.nlu.slots.names.' + entity.name)
+
+    if (SYSTEM_ENTITIES.includes(entity.name) && translatedName !== entity.name) {
+      entityName += ` (${translatedName})`
+    }
+
+    return entityName
+  }
+
   const entityItemRenderer: ItemRenderer<EntityOption> = (entity: EntityOption, { handleClick, modifiers }) => (
     <MenuItem
-      text={`${entity.type}.${
-        SYSTEM_ENTITIES.includes(entity.name) ? lang.tr('module.nlu.slots.names.' + entity.name) : entity.name
-      }`}
+      text={getEntityName(entity)}
       key={`${entity.name}${entity.type}`}
       onClick={handleClick}
       active={modifiers.active}
