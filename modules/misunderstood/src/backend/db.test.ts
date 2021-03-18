4a7799794291daec1eba13d8569d4d88f37276a2
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-03-18 14:22:43 -0400

    Empty DB test

--- modules/misunderstood/src/backend/db.test.ts
+++ modules/misunderstood/src/backend/db.test.ts
@@ -1,5 +1,30 @@
-describe('Module - Misunderstood - DB', () => {
-  it('Empty test', async () => {
-    console.log('hello')
+import Db from './db'
+// import * as sdk from 'botpress/sdk'
+import {
+  DbFlaggedEvent,
+  FilteringOptions,
+  FlaggedEvent,
+  FLAGGED_MESSAGE_STATUS,
+  FLAGGED_MESSAGE_STATUSES,
+  FLAG_REASON,
+  ResolutionData,
+  RESOLUTION_TYPE
+} from '../types'
+import 'reflect-metadata'
+import Database from '../../../../src/bp/core/database'
+
+import { PersistedConsoleLogger } from '../../../../src/bp/core/logger'
+import { createSpyObject, MockObject } from '../../../../src/bp/core/misc/utils'
+import { createDatabaseSuite } from '../../../../src/bp/core/database/index.tests'
+
+createDatabaseSuite('Misunderstood', (database: Database) => {
+  const logger: MockObject<PersistedConsoleLogger> = createSpyObject<PersistedConsoleLogger>()
+
+  describe('Get', () => {
+    it("Returns undefined if key doesn't exist", async () => {
+      const db = new Db({ database: database.knex })
+      await db.initialize()
+      await db.listEvents('', '', FLAGGED_MESSAGE_STATUS.new)
+    })
   })
 })
