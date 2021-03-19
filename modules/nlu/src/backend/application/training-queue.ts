87cc6be33e39c057122f3db189171115a5f9c4e4
Charles Catta <charles.catta@botpress.com>
2021-03-19 10:18:17 -0400

    Remove more awaits for returns

--- modules/nlu/src/backend/application/training-queue.ts
+++ modules/nlu/src/backend/application/training-queue.ts
@@ -49,7 +49,7 @@ export class TrainingQueue {
   }
 
   public async needsTraining(trainId: TrainingId): Promise<void> {
-    await this._trainingRepo.inTransaction(async ctx => {
+    return this._trainingRepo.inTransaction(async ctx => {
       const currentTraining = await ctx.get(trainId)
       if (currentTraining?.status === 'training') {
         return // do not notify socket if currently training
@@ -61,7 +61,7 @@ export class TrainingQueue {
   }
 
   public queueTraining = async (trainId: TrainingId): Promise<void> => {
-    await this._trainingRepo.inTransaction(async ctx => {
+    return this._trainingRepo.inTransaction(async ctx => {
       const currentTraining = await ctx.get(trainId)
       if (!currentTraining) {
         return
@@ -84,11 +84,6 @@ export class TrainingQueue {
       await ctx.set(trainId, newState)
       await this._notify(trainId, newState)
     }, 'queueTraining')
-    if (this._paused) {
-      return
-    }
-
-    return this.runTask()
   }
 
   public pause() {
