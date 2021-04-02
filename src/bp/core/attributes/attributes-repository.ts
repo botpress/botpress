566a898670cb898d9e590952049c1f48c618e3c8
Samuel Massé <samuelmasse4@gmail.com>
2021-04-01 21:06:16 -0400

    local cache

--- src/bp/core/attributes/attributes-repository.ts
+++ src/bp/core/attributes/attributes-repository.ts
@@ -2,15 +2,30 @@ import * as sdk from 'botpress/sdk'
 import { TYPES } from 'core/app/types'
 import Database from 'core/database'
 import { inject, injectable } from 'inversify'
+import LRUCache from 'lru-cache'
+import ms from 'ms'
 
 @injectable()
 export class AttributesRepository {
   private readonly TABLE_NAME = 'attributes'
+  private cache: LRUCache<sdk.uuid, string>
 
-  constructor(@inject(TYPES.Database) private database: Database) {}
+  constructor(@inject(TYPES.Database) private database: Database) {
+    this.cache = new LRUCache({ max: 50000, maxAge: ms('20m') })
+  }
 
   async set(entityId: sdk.uuid, attribute: string, value: string): Promise<void> {
-    await this.query().insert({ entityId, attribute, value })
+    const current = await this.get(entityId, attribute)
+
+    if (!current) {
+      await this.query().insert({ entityId, attribute, value })
+    } else if (value !== current) {
+      await this.query()
+        .where({ entityId, attribute })
+        .update({ value })
+
+      // TODO invalidate cache
+    }
   }
 
   async remove(entityId: sdk.uuid, attribute: string): Promise<boolean> {
@@ -18,12 +33,25 @@ export class AttributesRepository {
       .where({ entityId, attribute })
       .del()
 
+    // TODO invalidate cache
+
     return deletedRows > 0
   }
 
   async get(entityId: sdk.uuid, attribute: string): Promise<string> {
+    const cached = this.cache.get(entityId)
+    if (cached) {
+      return cached
+    }
+
     const entry = await this.query().where({ entityId, attribute })
-    return entry[0]?.value
+    const value = entry[0]?.value
+
+    if (value) {
+      this.cache.set(entityId, value)
+    }
+
+    return value
   }
 
   private query() {
