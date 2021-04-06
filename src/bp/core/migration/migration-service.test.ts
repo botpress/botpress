3b585361a4c709c0bda712d8f65dd517cf2a2a66
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-04-06 13:06:17 -0400

    pr comments

--- src/bp/core/migration/migration-service.test.ts
+++ src/bp/core/migration/migration-service.test.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata'
 import 'bluebird-global'
 
+jest.mock('fs-extra')
+
 import { GhostService } from 'core/bpfs'
 import { ConfigProvider } from 'core/config'
-import Database from 'core/database'
+import Database from 'core/database/index'
 import { PersistedConsoleLogger } from 'core/logger'
 import { createSpyObject, MockObject } from 'core/misc/utils'
 import yn from 'yn'
+import fse from 'fs-extra'
 import { MigrationService } from './migration-service'
 
 // TODO: Improve these unit tests and add more of them
@@ -54,13 +57,40 @@ describe('Migration Service', () => {
   })
 
   describe('getAllMigrations', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+    })
+
     it('Returns all the migrations currently available', async () => {
       const original = process.LOADED_MODULES
+
+      const spy = jest.spyOn(fse, 'existsSync').mockImplementation(() => {
+        return true
+      })
 
       try {
         process.LOADED_MODULES = {}
         // Makes sure that the specified paths in getAllMigrations exist.
         migration.getAllMigrations()
+        expect(spy).toHaveBeenCalledTimes(1)
+      } catch (err) {
+        fail(err)
+      } finally {
+        process.LOADED_MODULES = original
+      }
+    })
+
+    it('Throw an error if the core migrations folder does not exists', async () => {
+      const original = process.LOADED_MODULES
+
+      const spy = jest.spyOn(fse, 'existsSync').mockImplementation(() => {
+        return false
+      })
+
+      try {
+        process.LOADED_MODULES = {}
+        expect(() => migration.getAllMigrations()).toThrow()
+        expect(spy).toHaveBeenCalledTimes(1)
       } catch (err) {
         fail(err)
       } finally {
