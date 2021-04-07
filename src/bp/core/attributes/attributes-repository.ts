7ede310003e2353dca5dffd457a11f1ed161c950
Samuel Massé <samuelmasse4@gmail.com>
2021-04-07 15:22:47 -0400

    scoped repo

--- src/bp/core/attributes/attributes-repository.ts
+++ src/bp/core/attributes/attributes-repository.ts
@@ -8,66 +8,96 @@ import ms from 'ms'
 
 @injectable()
 export class AttributesRepository {
-  private readonly TABLE_NAME = 'attributes'
-  private cache: LRUCache<sdk.uuid, string>
-  private invalidateAttributeCache: (entityId: sdk.uuid, attribute: string, value: string | undefined) => void = this
-    ._localInvalidateAttributeCache
+  private repos: { [botId: string]: ScopedAttributesRepository } = {}
+  private invalidateAttributesCache: (attribute: string, entity: string, value: string | undefined) => void = this
+    ._localInvalidateAttributesCache
 
   constructor(
     @inject(TYPES.Database) private database: Database,
     @inject(TYPES.JobService) private jobService: JobService
-  ) {
-    this.cache = new LRUCache({ max: 50000, maxAge: ms('20m') })
-  }
+  ) {}
 
   @postConstruct()
   async init() {
-    this.invalidateAttributeCache = <any>(
-      await this.jobService.broadcast<void>(this._localInvalidateAttributeCache.bind(this))
+    this.invalidateAttributesCache = <any>(
+      await this.jobService.broadcast<void>(this._localInvalidateAttributesCache.bind(this))
     )
   }
 
-  private _localInvalidateAttributeCache(entityId: sdk.uuid, attribute: string, value: string | undefined) {
-    const cached = this.cache.get(entityId)
+  forAttribute(attribute: string) {
+    let repo = this.repos[attribute]
+    if (!repo) {
+      repo = new ScopedAttributesRepository(attribute, this.database, (entity, value) =>
+        this.invalidateAttributesCache(attribute, entity, value)
+      )
+      this.repos[attribute] = repo
+    }
+    return repo
+  }
+
+  private _localInvalidateAttributesCache(attribute: string, entity: sdk.uuid, value: string | undefined) {
+    this.forAttribute(attribute).localInvalidateCache(entity, value)
+  }
+}
+
+export class ScopedAttributesRepository {
+  private readonly TABLE_NAME = 'attributes'
+  private cache: LRUCache<sdk.uuid, string>
+
+  constructor(
+    private attribute: string,
+    private database: Database,
+    private invalidateCache: (entity: sdk.uuid, value: string | undefined) => void
+  ) {
+    this.cache = new LRUCache({ max: 2000, maxAge: ms('5m') })
+  }
+
+  public localInvalidateCache(entity: sdk.uuid, value: string | undefined) {
+    const cached = this.cache.get(entity)
     if (cached) {
+      if (value) {
+        this.cache.set(entity, value)
+      } else {
+        this.cache.del(entity)
+      }
     }
   }
 
-  async set(entityId: sdk.uuid, attribute: string, value: string): Promise<void> {
-    const current = await this.get(entityId, attribute)
+  async set(entity: sdk.uuid, value: string): Promise<void> {
+    const current = await this.get(entity)
 
     if (!current) {
-      await this.query().insert({ entityId, attribute, value })
+      await this.query().insert({ entity, attribute: this.attribute, value })
     } else if (value !== current) {
       await this.query()
-        .where({ entityId, attribute })
+        .where({ entity, attribute: this.attribute })
         .update({ value })
 
-      this.invalidateAttributeCache(entityId, attribute, value)
+      this.invalidateCache(entity, value)
     }
   }
 
-  async remove(entityId: sdk.uuid, attribute: string): Promise<boolean> {
+  async remove(entity: sdk.uuid): Promise<boolean> {
     const deletedRows = await this.query()
-      .where({ entityId, attribute })
+      .where({ entity, attribute: this.attribute })
       .del()
 
-    this.invalidateAttributeCache(entityId, attribute, undefined)
+    this.invalidateCache(entity, undefined)
 
     return deletedRows > 0
   }
 
-  async get(entityId: sdk.uuid, attribute: string): Promise<string> {
-    const cached = this.cache.get(entityId)
+  async get(entity: sdk.uuid): Promise<string> {
+    const cached = this.cache.get(entity)
     if (cached) {
       return cached
     }
 
-    const entry = await this.query().where({ entityId, attribute })
+    const entry = await this.query().where({ entity, attribute: this.attribute })
     const value = entry[0]?.value
 
     if (value) {
-      this.cache.set(entityId, value)
+      this.cache.set(entity, value)
     }
 
     return value
