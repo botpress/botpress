337e523fa866b13b72dfd7d56de6c8dc46f65d4a
Samuel Massé <samuelmasse4@gmail.com>
2021-04-19 18:08:05 -0400

    single choice content type

--- modules/builtin/src/content-types/single_choice.js
+++ modules/builtin/src/content-types/single_choice.js
@@ -65,47 +65,15 @@ function renderMessenger(data) {
   ]
 }
 
-function renderSlack(data) {
-  const events = []
-
-  if (data.typing) {
-    events.push({
-      type: 'typing',
-      value: data.typing
-    })
-  }
-
-  return [
-    ...events,
-    {
-      text: data.text,
-      quick_replies: {
-        type: 'actions',
-        elements: data.choices.map((q, idx) => ({
-          type: 'button',
-          action_id: 'replace_buttons' + idx,
-          text: {
-            type: 'plain_text',
-            text: q.title
-          },
-          value: q.value.toUpperCase()
-        }))
-      }
-    }
-  ]
-}
-
 function renderElement(data, channel) {
   // These channels now use channel renderers
-  if ([].includes(channel)) {
+  if (['slack'].includes(channel)) {
     // TODO : automate this from the schema
     return { text: data.text, choices: data.choices }
   }
 
   if (channel === 'messenger') {
     return renderMessenger(data)
-  } else if (channel === 'slack') {
-    return renderSlack(data)
   } else {
     return render(data)
   }
