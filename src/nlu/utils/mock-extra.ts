83e509322239ade22cfe6a28dc9097bac0396151
François Levasseur <francois_levasseur@hotmail.com>
2021-04-21 15:58:26 -0400

    chore(nlu): rm jest from nlu dependencies

---
+++ src/nlu/utils/mock-extra.ts
@@ -0,0 +1,15 @@
+export type MockObject<T> = { T: T } & { readonly [key in keyof T]: jest.Mock }
+export function createSpyObject<T>(): MockObject<T> {
+  const obj = {}
+  const handler: ProxyHandler<object> = {
+    get(obj, prop) {
+      if (prop === 'T') {
+        return proxy
+      }
+
+      return prop in obj ? obj[prop] : (obj[prop] = jest.fn())
+    }
+  }
+  const proxy = new Proxy(obj, handler)
+  return proxy as MockObject<T>
+}
