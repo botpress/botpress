102a24d6025dbd9d0229d0bdf36448bc0c634ea9
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-04-21 19:20:07 -0400

    pr comments

--- modules/channel-vonage/src/backend/client.ts
+++ modules/channel-vonage/src/backend/client.ts
@@ -198,10 +198,7 @@ export class VonageClient {
 
     if (payload.type === 'typing') {
       // Note: Vonage Messages API does not support typing indicators for privacy reasons
-      // Sandbox is limited to one call per second: https://developer.nexmo.com/messages/concepts/messages-api-sandbox#rate-limit
-      if (this.config.useTestingApi) {
-        await Promise.delay(1000)
-      }
+      await Promise.delay(1000)
     } else if (payload.quick_replies) {
       await this.sendQuickReply(event, payload.quick_replies)
     } else if (payload.options) {
