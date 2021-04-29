c226b54232d30a9b49ee01c60eb34bb2e81d1b1b
Samuel Massé <samuelmasse4@gmail.com>
2021-04-29 13:50:49 -0400

    cleanup renderers

--- modules/builtin/src/content-types/image.js
+++ modules/builtin/src/content-types/image.js
@@ -117,8 +117,7 @@ function renderTeams(data) {
 function renderElement(data, channel) {
   // These channels now use channel renderers
   if ([].includes(channel)) {
-    // TODO : automate this from the schema
-    return { type: 'image', image: utils.formatURL(data.BOT_URL, data.image), title: data.title }
+    return { type: 'image', ...data.payload }
   }
 
   if (channel === 'messenger') {
