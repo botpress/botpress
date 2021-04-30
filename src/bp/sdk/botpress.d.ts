e7e3aabf641d74838a8dedcce0549027f4f931b8
Samuel Massé <samuelmasse4@gmail.com>
2021-04-30 14:01:39 -0400

    action.action strongly typed

--- src/bp/sdk/botpress.d.ts
+++ src/bp/sdk/botpress.d.ts
@@ -1720,22 +1720,19 @@ declare module 'botpress/sdk' {
   }
 
   export interface ActionButton {
-    action: string
+    action: 'Say something' | 'Open URL' | 'Postback'
     title: string
   }
 
   export interface ActionSaySomething extends ActionButton {
-    action: 'Say something'
     text: string | MultiLangText
   }
 
   export interface ActionOpenURL extends ActionButton {
-    action: 'Open URL'
     url: string
   }
 
   export interface ActionPostback extends ActionButton {
-    action: 'Postback'
     payload: string
   }
 
