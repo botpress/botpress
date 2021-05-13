bcd49e3761f23f6a848dd3aea7c6f0d72a829c1b
François-Xavier P. Darveau <frxpoulin@gmail.com>
2021-05-13 19:22:14 -0400

    pr comments

--- modules/hitlnext/src/views/full/app/components/Message.tsx
+++ modules/hitlnext/src/views/full/app/components/Message.tsx
@@ -5,18 +5,27 @@ import React, { FC } from 'react'
 
 import style from '../../style.scss'
 
-// To support complex content types, export message from webchat in ui-shared lite and show it here
+const renderPayload = (event: IO.Event) => {
+  const { payload } = event
+  try {
+    return (
+      <Collapsible name={`type: ${payload.type}`}>
+        <div>{JSON.stringify(payload, null, 2)}</div>
+      </Collapsible>
+    )
+  } catch (error) {
+    return null
+  }
+}
+
+//TODO: To support complex content types, export message from webchat in ui-shared lite and show it here
 export const Message: FC<IO.StoredEvent> = props => {
-  const { preview, payload } = props.event
+  const { preview } = props.event
   return (
     <div className={cx(style.messageContainer, props.direction === 'incoming' ? style.user : style.bot)}>
       <div className={cx(style.message)}>
         {preview && <span>{preview}</span>}
-        {!preview && (
-          <Collapsible name={`type: ${payload.type}`}>
-            <div>{JSON.stringify(payload, null, 2)}</div>
-          </Collapsible>
-        )}
+        {!preview && renderPayload(props.event)}
       </div>
     </div>
   )
