377df13a3a666fa930037e86eaad6052734f0311
François-Xavier P. Darveau <frxpoulin@gmail.com>
2021-05-13 15:26:15 -0400

    removed dead code

---
+++ modules/examples/src/views/lite/index.jsx
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import React from 'react'
+
+export class LiteView extends React.Component {
+  render() {
+    return null
+  }
+}
+
+export { TheFormCustomComponent } from './TheFormCustomComponent'
