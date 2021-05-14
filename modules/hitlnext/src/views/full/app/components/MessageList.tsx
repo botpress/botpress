8c11fc0745af45b4eeeb18b6334594f68bc13d71
Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
2021-05-13 21:12:24 -0400

    feat(hitlnext): display custom component in live chat (#5001)
    * use native web components to display events
    
    * changed the play sound logic on webchat
    
    * removed history message components out of lite
    
    * removed dead code
    
    * displaypayload json in offline convo
    
    * move comments form inside collapsible
    
    * remove test code
    
    * pr comments
    
    * remove nlu-installer dist files

--- modules/hitlnext/src/views/full/app/components/MessageList.tsx
+++ modules/hitlnext/src/views/full/app/components/MessageList.tsx
@@ -3,18 +3,15 @@ import cx from 'classnames'
 import sortby from 'lodash/sortBy'
 import React, { FC } from 'react'
 
+import style from '../../style.scss'
 import { Message } from './Message'
-import style from './style.scss'
 
 interface Props {
   events: sdk.IO.StoredEvent[]
 }
 
 // This does not support message groups
-// Either export message group from webchat in ui-shared-lite and show it here
-// Or show a "readonly webchat"
 const MessageList: FC<Props> = props => {
-  // TODO some smart grouping do display date
   return (
     <div className={cx(style.messageList)}>
       {sortby(props.events, 'id').map(ev => (
