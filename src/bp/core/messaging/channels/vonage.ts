a43708604bc9783cfdc343ef6353e3b09e4ea22a
Samuel Massé <samuelmasse4@gmail.com>
2021-06-15 15:00:11 -0400

    refact 2

--- src/bp/core/messaging/channels/vonage.ts
+++ src/bp/core/messaging/channels/vonage.ts
@@ -1,8 +1,3 @@
-import { HTTPServer } from 'core/app/server'
-import { GhostService } from 'core/bpfs'
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import { MessagingClient } from '../messaging-client'
-import { MessagingService } from '../messaging-service'
 import { Channel } from './base'
 
 export class ChannelVonage extends Channel {
