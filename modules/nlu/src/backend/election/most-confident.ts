681b9a94c5a71b541e087e60eb02c3a9c18ddff0
Francois-Xavier P. Darveau <955524+EFF@users.noreply.github.com>
2021-06-16 14:24:42 -0400

    Update most-confident.ts

--- modules/nlu/src/backend/election/most-confident.ts
+++ modules/nlu/src/backend/election/most-confident.ts
@@ -2,17 +2,17 @@ import { IO, NLU } from 'botpress/sdk'
 import _ from 'lodash'
 
 export const getMostConfidentContext = (
-  nlu: IO.EventUnderstanding
+  nluEvent: IO.EventUnderstanding
 ): (NLU.ContextPrediction & { name: string }) | undefined => {
-  if (!nlu.predictions) {
+  if (!nluEvent.predictions) {
     return undefined
   }
 
-  const allContexts = Object.keys(nlu.predictions)
-  const { includedContexts } = nlu
+  const allContexts = Object.keys(nluEvent.predictions)
+  const { includedContexts } = nluEvent
   const availableContexts = _.intersection(includedContexts, allContexts).length ? includedContexts : allContexts
 
-  return _(nlu.predictions)
+  return _(nluEvent.predictions)
     .pickBy((_p, ctx) => availableContexts.includes(ctx))
     .entries()
     .map(([name, ctx]) => ({ ...ctx, name }))
