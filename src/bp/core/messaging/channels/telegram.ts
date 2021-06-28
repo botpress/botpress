30f016e712984efd057467fd82311c9df7df5420
Samuel Massé <samuelmasse4@gmail.com>
2021-06-28 16:24:07 -0400

    fix telegram

--- src/bp/core/messaging/channels/telegram.ts
+++ src/bp/core/messaging/channels/telegram.ts
@@ -5,6 +5,19 @@ export class ChannelTelegram extends Channel {
     return 'telegram'
   }
 
+  async loadConfigForBot(botId: string) {
+    const baseConfig = await super.loadConfigForBot(botId)
+
+    if (baseConfig) {
+      return {
+        ...baseConfig,
+        webhookUrl: `${process.EXTERNAL_URL}/api/v1/bots/${botId}/mod/channel-telegram/webhook`
+      }
+    } else {
+      return undefined
+    }
+  }
+
   setupProxies() {
     this.setupProxy('/webhook')
   }
