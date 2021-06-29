63266084c6e36468895efdb8f94b1c8f66b149be
allardy <yann.allard1@gmail.com>
2021-06-29 10:05:45 -0400

    add out

---
+++ packages/native-extensions/out/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.empty = void 0;
+// Just so the package can be imported and be detected correctly by typescript
+exports.empty = {};
