583b883b8eced5fdb745b08c34baee1752a2cde3
sadda11asm <saddam.asmatullayev@nu.edu.kz>
2021-07-07 00:28:39 +0600

    feat: add env variable for license key in e2e

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -19,6 +19,7 @@ module.exports = {
     password: '123456',
     botId: 'test-bot',
     host: 'http://localhost:3000',
+    licenseKey: process.env.BP_LICENSE_KEY, // add license key to env variables
     apiHost: 'http://localhost:3000', // When testing the Admin UI on port 3001, set this to the correct port
     recreateBot: true // When testing something specific, set to false to keep the existing bot
   }
--- src/e2e/admin/auth.test.ts
+++ src/e2e/admin/auth.test.ts
@@ -5,9 +5,18 @@ import { clickOn, fillField } from '../expectPuppeteer'
 import { getResponse, gotoAndExpect } from '../utils'
 
 describe('Auth UI', () => {
+  config.default.pro.licenseKey = bpConfig.licenseKey
   const content = JSON.stringify(config.default)
   let jwt: string
   let default_content: string
+
+  const sleep = (milliseconds) => {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
 
   const data = content => {
     return {
@@ -39,11 +48,12 @@ describe('Auth UI', () => {
     if (page.url().includes('/register')) {
       await fillField('#confirmPassword', bpConfig.password)
       await clickOn('#btn-register')
+      const response = await getResponse(`${bpConfig.apiHost}/api/v2/admin/auth/register/basic/default`, 'POST')
+      jwt = (await response.json()).payload.jwt
     } else {
       await clickOn('#btn-signin')
       const response = await getResponse(`${bpConfig.apiHost}/api/v2/admin/auth/login/basic/default`, 'POST')
       jwt = (await response.json()).payload.jwt
-      console.log('JWT', jwt)
     }
   })
 
@@ -63,10 +73,15 @@ describe('Auth UI', () => {
       },
       headers
     )
+    await page.goto('https://google.com', {waitUntil: 'domcontentloaded'}) // to avoid frontend errors during server reboot
     expect(resp1.status).toEqual(200)
     default_content = resp1.data.fileContent
     const resp2 = await axios.post(`${bpConfig.apiHost}/api/v1/bots/___/mod/code-editor/save`, data(content), headers)
     expect(resp2.status).toEqual(200)
+    const resp3 = await axios.post(`${bpConfig.apiHost}/api/v2/admin/management/rebootServer`, { data: {} }, headers)
+    expect(resp3.status).toEqual(200) // reboot is required after adding auth strategies
+    sleep(8000) // wait while server is rebooting
+    await page.goto(`${bpConfig.host}/admin/workspace/default/bots`)
   })
 
   it('Log out', async () => {
@@ -75,16 +90,13 @@ describe('Auth UI', () => {
     await getResponse('/api/v2/admin/auth/logout', 'POST')
   })
 
-  it('Open workspaces page', async () => {
-    await gotoAndExpect(`${bpConfig.host}/admin/login/`)
-  })
-
-  it('Load Login page', async () => {
+  it('Open login page', async () => {
+    await page.goto(`${bpConfig.host}/admin/login/`, {waitUntil: 'domcontentloaded'});
     expect(page.url().includes('login')).toBeTruthy()
   })
 
-  it('Preview non-hidden login strategies', async () => {
-    expect((await page.$('#btn-default')) !== null).toBeTruthy()
+  it('Preview non-hidden auth strategies', async () => {
+    expect((await page.$('#btn-signin')) == null).toBeTruthy()
     expect((await page.$('#btn-botpress')) !== null).toBeTruthy()
     expect((await page.$('#btn-botpress2')) == null).toBeTruthy()
   })
