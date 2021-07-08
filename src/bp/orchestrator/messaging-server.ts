27e2097a05b40337c750ea0536ee51b4d8555d42
Samuel Massé <samuelmasse4@gmail.com>
2021-07-08 18:17:53 -0400

    redis_scope

--- src/bp/orchestrator/messaging-server.ts
+++ src/bp/orchestrator/messaging-server.ts
@@ -52,6 +52,7 @@ export const startMessagingServer = async (opts: Partial<MessagingServerOptions>
     DATABASE_POOL: process.env.DATABASE_POOL,
     CLUSTER_ENABLED: process.core_env.CLUSTER_ENABLED?.toString(),
     REDIS_URL: process.core_env.REDIS_URL,
+    REDIS_SCOPE: process.core_env.BP_REDIS_SCOPE,
     SKIP_LOAD_ENV: 'true',
     SKIP_LOAD_CONFIG: 'true'
   }
