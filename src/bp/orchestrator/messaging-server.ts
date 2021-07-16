5320c5c9bdad1c4d7d079c583d3804f09de4b3ab
Samuel Massé <samuelmasse4@gmail.com>
2021-07-15 21:35:37 -0400

    pass spinned env var

--- src/bp/orchestrator/messaging-server.ts
+++ src/bp/orchestrator/messaging-server.ts
@@ -59,7 +59,8 @@ export const startMessagingServer = async (opts: Partial<MessagingServerOptions>
     REDIS_OPTIONS: process.env.REDIS_OPTIONS,
     LOGGING_ENABLED: 'false',
     SKIP_LOAD_ENV: 'true',
-    SKIP_LOAD_CONFIG: 'true'
+    SKIP_LOAD_CONFIG: 'true',
+    SPINNED: 'true'
   }
 
   if (!process.core_env.DEV_MESSAGING_PATH) {
