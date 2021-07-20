2d838c8876e2e9debd05a2da06698f47beafdd35
Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>
2021-07-20 13:12:26 -0400

    fix(google-speech): move closest number utils into google-speech code (#5199)

--- modules/google-speech/src/backend/utils.ts
+++ modules/google-speech/src/backend/utils.ts
@@ -19,3 +19,16 @@ export const timeoutFn = async <R>(promise: Promise<R>, timeout?: string): Promi
     return promise
   }
 }
+
+export const closest = (arr: number[], value: number) => {
+  return arr.reduce((prev, curr) => {
+    const diffPref = Math.abs(prev - value)
+    const diffCurr = Math.abs(curr - value)
+
+    if (diffPref === diffCurr) {
+      return prev > curr ? prev : curr
+    } else {
+      return diffCurr < diffPref ? curr : prev
+    }
+  })
+}
