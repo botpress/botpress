9687511b3ba309cb5e366ce5fddebc9a64faa4ca
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-08-03 11:41:20 -0400

    chore(*): remove modules

--- modules/.nlu-testing/src/shared/utils.ts
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-
-import { Test, TestResult } from './typings'
-
-export function computeSummary(tests: Test[], testResults: _.Dictionary<TestResult>): number {
-  const passedCount = Object.values(testResults).filter(res => res.success).length
-  return _.round((passedCount / tests.length) * 100, 1)
-}
