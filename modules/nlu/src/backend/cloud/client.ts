f5b8a5e811c8f101afc4af3170fea8b15f821339
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2021-08-05 15:29:46 -0400

    fix(nlu): added module moment & fixed cloud client

--- modules/nlu/src/backend/cloud/client.ts
+++ modules/nlu/src/backend/cloud/client.ts
@@ -1,7 +1,10 @@
-import { NLUClient } from '@botpress/nlu-client/src/client'
+import { Client } from '@botpress/nlu-client'
+import { AxiosInstance } from 'axios'
 import { createOauthClient, OauthClientProps } from './oauth'
 
-export class NLUCloudClient extends NLUClient {
+export class NLUCloudClient extends Client {
+  private _client: AxiosInstance
+
   constructor(options: OauthClientProps) {
     super(options.endpoint, undefined)
     this._client = createOauthClient(options)
