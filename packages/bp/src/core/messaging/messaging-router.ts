17c9cc122e348ff7489c5e8fcf357f597169ece4
Samuel Massé <samuelmasse4@gmail.com>
2021-08-09 18:04:02 -0400

    fix

--- packages/bp/src/core/messaging/messaging-router.ts
+++ packages/bp/src/core/messaging/messaging-router.ts
@@ -23,7 +23,7 @@ export class MessagingRouter extends CustomRouter {
           return next?.(new UnauthorizedError('Password is missing or invalid'))
         } else if (
           this.messaging.isExternal &&
-          req.headers['x-webhook-token'] !== this.messaging.getWebhookToken(req?.body.client?.id)
+          req.headers['x-webhook-token'] !== this.messaging.getWebhookToken(req?.body?.client?.id)
         ) {
           return next?.(new UnauthorizedError('Invalid webhook token'))
         }
