f21f033c40007dc01e6bc54aa3b846dcb0642c62
Roberto Escalante H <robertoescalante91@gmail.com>
2021-08-13 17:51:48 -0600

    render options refactor

--- modules/hitlnext/src/views/full/app/components/HandoffListHeader.tsx
+++ modules/hitlnext/src/views/full/app/components/HandoffListHeader.tsx
@@ -32,6 +32,72 @@ const HandoffListHeader: FC<Props> = ({
   setSortOption,
   disabled
 }) => {
+  const defaultFilterOptions: ToolbarButtonProps['optionsItems'] = [
+    {
+      content: (
+        <Checkbox
+          checked={filterOptions.unassigned}
+          label={lang.tr('module.hitlnext.filter.unassigned')}
+          onChange={() => setFilterOptions({ ...filterOptions, unassigned: !filterOptions.unassigned })}
+        />
+      )
+    },
+    {
+      content: (
+        <Checkbox
+          checked={filterOptions.assignedMe}
+          label={lang.tr('module.hitlnext.filter.assignedMe')}
+          onChange={() => setFilterOptions({ ...filterOptions, assignedMe: !filterOptions.assignedMe })}
+        />
+      )
+    },
+    {
+      content: (
+        <Checkbox
+          checked={filterOptions.assignedOther}
+          label={lang.tr('module.hitlnext.filter.assignedOther')}
+          onChange={() => setFilterOptions({ ...filterOptions, assignedOther: !filterOptions.assignedOther })}
+        />
+      )
+    },
+    {
+      content: (
+        <Checkbox
+          checked={filterOptions.resolved}
+          label={lang.tr('module.hitlnext.filter.resolved')}
+          onChange={() => setFilterOptions({ ...filterOptions, resolved: !filterOptions.resolved })}
+        />
+      )
+    }
+  ]
+
+  const renderOptions = (tags: string[]): ToolbarButtonProps['optionsItems'] => {
+    if (tags && tags.length) {
+      const tagOptions: ToolbarButtonProps['optionsItems'] = tags.map(tag => {
+        return {
+          content: (
+            <Checkbox
+              checked={filterOptions.tags.includes(tag)}
+              label={tag}
+              onChange={() =>
+                setFilterOptions({
+                  ...filterOptions,
+                  tags:
+                    filterOptions.tags && filterOptions.tags.includes(tag)
+                      ? _.pull(filterOptions.tags, tag)
+                      : [...filterOptions.tags, tag]
+                })
+              }
+            />
+          )
+        }
+      })
+      return defaultFilterOptions.concat(tagOptions)
+    } else {
+      return defaultFilterOptions
+    }
+  }
+
   const buttons: ToolbarButtonProps[] = [
     {
       icon: 'sort',
@@ -56,105 +122,17 @@ const HandoffListHeader: FC<Props> = ({
     },
     {
       icon: 'filter',
-      optionsItems: [
-        {
-          content: (
-            <Checkbox
-              checked={filterOptions.unassigned}
-              label={lang.tr('module.hitlnext.filter.unassigned')}
-              onChange={() => setFilterOptions({ ...filterOptions, unassigned: !filterOptions.unassigned })}
-            />
-          )
-        },
-        {
-          content: (
-            <Checkbox
-              checked={filterOptions.assignedMe}
-              label={lang.tr('module.hitlnext.filter.assignedMe')}
-              onChange={() => setFilterOptions({ ...filterOptions, assignedMe: !filterOptions.assignedMe })}
-            />
-          )
-        },
-        {
-          content: (
-            <Checkbox
-              checked={filterOptions.assignedOther}
-              label={lang.tr('module.hitlnext.filter.assignedOther')}
-              onChange={() => setFilterOptions({ ...filterOptions, assignedOther: !filterOptions.assignedOther })}
-            />
-          )
-        },
-        {
-          content: (
-            <Checkbox
-              checked={filterOptions.resolved}
-              label={lang.tr('module.hitlnext.filter.resolved')}
-              onChange={() => setFilterOptions({ ...filterOptions, resolved: !filterOptions.resolved })}
-            />
-          )
-        }
-      ],
+      optionsItems: renderOptions(tags),
       tooltip: lang.tr('module.hitlnext.filterBy'),
       disabled
     }
   ]
 
-  const renderButtons = (tags: string[]): ToolbarButtonProps[] => {
-    const tagElements: ToolbarButtonProps[] = tags.map(tag => {
-      return {
-        content: (
-          <Checkbox
-            checked={filterOptions.tags.includes(tag)}
-            label={tag}
-            onChange={() =>
-              setFilterOptions({
-                ...filterOptions,
-                tags:
-                  filterOptions.tags && filterOptions.tags.includes(tag)
-                    ? _.pull(filterOptions.tags, tag)
-                    : [...filterOptions.tags, tag]
-              })
-            }
-          />
-        )
-      }
-    })
-    return [
-      {
-        icon: 'sort',
-        optionsItems: [
-          {
-            label: lang.tr('module.humanhelpnext.sort.mostRecentlyCreated'),
-            selected: sortOption === 'mostRecent',
-            action: () => {
-              setSortOption('mostRecent')
-            }
-          },
-          {
-            label: lang.tr('module.hitlnext.sort.leastRecentlyCreated'),
-            selected: sortOption === 'leastRecent',
-            action: () => {
-              setSortOption('leastRecent')
-            }
-          }
-        ],
-        tooltip: lang.tr('module.hitlnext.sortBy'),
-        disabled
-      },
-      {
-        icon: 'filter',
-        optionsItems: buttons[1].optionsItems.concat(tagElements),
-        tooltip: lang.tr('module.hitlnext.filterBy'),
-        disabled
-      }
-    ]
-  }
-
   return (
     <MainLayout.Toolbar
       className={style.hitlToolBar}
       tabs={[{ id: 'handoffs', title: lang.tr('module.hitlnext.sidebar.tab') }]}
-      buttons={tags && tags.length > 0 ? renderButtons(tags) : buttons}
+      buttons={buttons}
     ></MainLayout.Toolbar>
   )
 }
