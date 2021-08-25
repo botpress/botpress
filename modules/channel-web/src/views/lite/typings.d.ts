4bf3d0447f84822aad16361694284a5f3d0950cd
Samuel Massé <59894025+samuelmasse@users.noreply.github.com>
2021-08-25 16:06:57 -0400

    Update modules/channel-web/src/views/lite/typings.d.ts
    Co-authored-by: Laurent Leclerc Poulin <laurentlp@users.noreply.github.com>

--- modules/channel-web/src/views/lite/typings.d.ts
+++ modules/channel-web/src/views/lite/typings.d.ts
@@ -31,7 +31,7 @@ export namespace Renderer {
     store?: RootStore
     bp?: StudioConnector
     fromLabel?: string
-    messageId?: string
+    messageId?: uuid
     /** When true, the message isn't wrapped by its bubble */
     noBubble?: boolean
     keyboard?: any
