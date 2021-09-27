ec588df7b07a8c1419fe2d82074b7627ec9275bf
thomas caissie <tomkcey@gmail.com>
2021-09-27 12:33:53 -0400

    chore(clean): clean further

--- modules/nlu/src/backend/election/natural-election.test.ts
+++ modules/nlu/src/backend/election/natural-election.test.ts
@@ -1,34 +1,34 @@
 import { extractElectedIntentSlot } from './natural-election'
-import { mockEventUnderstanding } from '../../test/mocks'
+import { fakeEventUnderstanding } from '../../test/types'
 
 describe('naturalElection', () => {
   describe('extractElectedIntentSlot', () => {
     it("should return input directly if the 'predictions' property is nil", () => {
-      const input = mockEventUnderstanding({ predictions: undefined })
+      const input = fakeEventUnderstanding({ predictions: undefined })
       const result = extractElectedIntentSlot(input)
       expect(result).toStrictEqual(input)
     })
 
     it("should return input directly if the 'intent' property is nil", () => {
-      const input = mockEventUnderstanding({ predictions: {}, intent: undefined })
+      const input = fakeEventUnderstanding({ predictions: {}, intent: undefined })
       const result = extractElectedIntentSlot(input)
       expect(result).toStrictEqual(input)
     })
 
     it("should return input directly if the 'context' property of the 'predictions' property is nil", () => {
-      const input = mockEventUnderstanding({ predictions: { context: undefined }, intent: {} })
+      const input = fakeEventUnderstanding({ predictions: { context: undefined }, intent: {} })
       const result = extractElectedIntentSlot(input)
       expect(result).toStrictEqual(input)
     })
 
     it("should return input directly if the 'intents' property of the prediction 'context' property is nil", () => {
-      const input = mockEventUnderstanding({ predictions: { context: { intents: undefined } }, intent: {} })
+      const input = fakeEventUnderstanding({ predictions: { context: { intents: undefined } }, intent: {} })
       const result = extractElectedIntentSlot(input)
       expect(result).toStrictEqual(input)
     })
 
     it("should return input with an empty 'slots' object if no labels within the 'intents' array match the 'intent' name", () => {
-      const input = mockEventUnderstanding({
+      const input = fakeEventUnderstanding({
         predictions: { context: { intents: [{ label: 'name', confidence: 1, extractor: 'extractor', slots: [] }] } },
         intent: { name: 'label' }
       })
@@ -37,7 +37,7 @@ describe('naturalElection', () => {
     })
 
     it("should return input with the 'slots' object filled with that of the elected intent if a label within the 'intents' array matches the 'intent' name", () => {
-      const input = mockEventUnderstanding({
+      const input = fakeEventUnderstanding({
         predictions: { context: { intents: [{ label: 'name', confidence: 1, extractor: 'extractor', slots: [] }] } },
         intent: { name: 'name' }
       })
--- modules/nlu/src/test/types.ts
+++ modules/nlu/src/test/types.ts
@@ -1,7 +1,12 @@
 import { IO } from 'botpress/sdk'
 
+/**
+ * @description extending IO.EventUnderstanding as is is necessary in this context (nlu/backend/election) as the Typescript compiler doesn't allow me to import botpress/sdk in a test file
+ * @todo fix Typescript configuration file to allow botpres/sdk import in test files
+ */
 export type EventUnderstanding = IO.EventUnderstanding
-export function mockEventUnderstanding(x?: Partial<EventUnderstanding>): EventUnderstanding {
+
+export function fakeEventUnderstanding(x?: Partial<EventUnderstanding>): EventUnderstanding {
   return {
     detectedLanguage: 'en',
     errored: false,
