d0c11cbeef8ce2dac7b1c914feba693b337fb4d5
Erik Derohanian <erik@erik-j.de>
2021-09-30 16:29:33 -0400

    chore(core): add tsx to prettier github check
    Also ran prettier on tsx files, several weren't formatted

--- modules/hitlnext/src/views/full/app/components/ConversationHistory.tsx
+++ modules/hitlnext/src/views/full/app/components/ConversationHistory.tsx
@@ -22,13 +22,16 @@ const ConversationHistory: FC<Props> = ({ api, bp, conversationId }) => {
   const [loading, setLoading] = useState(true)
   const [events, setEvents] = useState<IO.StoredEvent[]>([])
 
-  const handleMessage = useCallback((message: ISocketMessage) => {
-    if (message.resource === 'event' && message.type === 'create') {
-      if (message.payload.threadId === conversationId) {
-        setEvents(evts => [...evts, message.payload])
+  const handleMessage = useCallback(
+    (message: ISocketMessage) => {
+      if (message.resource === 'event' && message.type === 'create') {
+        if (message.payload.threadId === conversationId) {
+          setEvents(evts => [...evts, message.payload])
+        }
       }
-    }
-  }, [conversationId])
+    },
+    [conversationId]
+  )
 
   useEffect(() => {
     bp.events.on(`${WEBSOCKET_TOPIC}:${window.BOT_ID}`, handleMessage)
