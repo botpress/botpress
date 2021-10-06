59c2c6fd1f90c0264fd315a0f84136eb6f95ffdc
thomas caissie <tomkcey@gmail.com>
2021-10-06 08:51:57 -0400

    chore(examples): update Composer example

--- examples/custom-component/src/views/lite/components/Composer.jsx
+++ examples/custom-component/src/views/lite/components/Composer.jsx
@@ -1,26 +1,14 @@
-// This component is an example on how to replace the composer input of the web chat (text input)
-export class Composer extends React.Component {
-  render() {
-    /**
-     * We are re-using styling and the original input component. You can use part of existing props or replace all of them.
-     * Check out the code of 'channel-web' on the link below to see how the this.props methods are handled.
-     * https://github.com/botpress/botpress/blob/master/modules/channel-web/src/views/web/side/index.jsx
-     */
-    const { style, Input } = this.props.original
+import React from 'react'
 
-    return (
-      <div className={style.composer}>
-        <div className={style['flex-column']}>
-          <Input
-            placeholder={'Reply to some bot name (or this.props.name)'}
-            send={this.props.onTextSend}
-            change={this.props.onTextChanged}
-            text={this.props.text}
-            recallHistory={this.props.recallHistory}
-            config={this.props.config}
-          />
-        </div>
-      </div>
-    )
-  }
+// This component is an example on how to replace the composer input of the web chat (text input)
+
+export const Composer = props => {
+  const {} = this.props.store.composer
+
+  return (
+    <div>
+      <input type="text" placeholder="custom composer..."></input>
+      <button type="button"></button>
+    </div>
+  )
 }
