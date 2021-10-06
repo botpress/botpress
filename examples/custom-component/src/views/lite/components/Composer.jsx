0efe5ede5ee5c4cc1b3e3addbbce49d17fc2bc76
thomas caissie <tomkcey@gmail.com>
2021-10-06 09:20:31 -0400

    chore(example): update Composer example

--- examples/custom-component/src/views/lite/components/Composer.jsx
+++ examples/custom-component/src/views/lite/components/Composer.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-
 // This component is an example on how to replace the composer input of the web chat (text input)
+import React, { useState } from 'react'
+import style from './style.scss'
+// You can use Sass (https://sass-lang.com/documentation/syntax) to style your custom composer
 
 export const Composer = props => {
-  const {} = this.props.store.composer
+  const [input, setInput] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const handleChange = e => {
+    e.preventDefault()
+    setInput(e.target.value)
+    if (input === 0) {
+      setIsDisabled(true)
+    } else {
+      setIsDisabled(false)
+    }
+  }
+
+  const handleOnClick = async e => {
+    e.preventDefault()
+    if (isDisabled) {
+      return
+    }
+    props.store.composer.updateMessage(input.trim())
+    await props.store.sendMessage()
+    setInput('')
+    setIsDisabled(true)
+  }
 
   return (
-    <div>
-      <input type="text" placeholder="custom composer..."></input>
-      <button type="button"></button>
+    <div className={style.wrapper}>
+      <input
+        className={style.input}
+        type="text"
+        placeholder="custom composer input..."
+        onChange={handleChange}
+        value={input}
+      />
+      <button className={style.btn} type="button" onClick={handleOnClick}>
+        send
+      </button>
     </div>
   )
 }
