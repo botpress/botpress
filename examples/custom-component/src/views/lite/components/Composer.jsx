ad5b0e050af39d845e5028b1b9d956897f396351
thomas caissie <tomkcey@gmail.com>
2021-10-06 12:50:15 -0400

    chore(example): add more comments and add qol in example html

--- examples/custom-component/src/views/lite/components/Composer.jsx
+++ examples/custom-component/src/views/lite/components/Composer.jsx
@@ -1,4 +1,5 @@
 // This component is an example on how to replace the composer input of the web chat (text input)
+
 import React, { useState } from 'react'
 import style from './style.scss'
 // You can use Sass (https://sass-lang.com/documentation/syntax) to style your custom composer
@@ -7,24 +8,46 @@ export const Composer = props => {
   const [input, setInput] = useState('')
   const [isDisabled, setIsDisabled] = useState(true)
 
+  /**
+   * In this onChange example we're setting the value of the component input state as well as switching the toggle for enabling message sending on or off
+   * It's a simple function, but yours could potentially be more complex.
+   * For example, you could add API calls to make varifications or fetch additional data.
+   */
   const handleChange = e => {
+    // Prevent the default behaviour of the input HTML component `onchange` event
     e.preventDefault()
+    // Proceed to set our component `input` state with the value received from the HTML input element
     setInput(e.target.value)
-    if (input === 0) {
+    // Calculate whether the component input state value is now an empty string (has length of 0)
+    if (input.length === 0) {
+      // If it's an empty string, we disable sending that value as a chat message
       setIsDisabled(true)
     } else {
+      // If it's not, we enable sending that value as a chat message
       setIsDisabled(false)
     }
   }
 
+  /**
+   * In this onClick example we're sending the saved input state to the chat if sending is enabled.
+   * It's a simple function, but yours could potentially be more complex.
+   * For example, you could add API calls to make varifications or fetch additional data.
+   */
   const handleOnClick = async e => {
+    // Prevent the default behaviour of the input HTML component `onchange` event
     e.preventDefault()
+    // Check if the component message sending toggle evaluates to disabled
     if (isDisabled) {
+      // if it does, then simply return right away, cancelling further processing
       return
     }
+    // Use the ComposerStore instance passed to this component through the props to update the message list
     props.store.composer.updateMessage(input.trim())
+    // Then send the message
     await props.store.sendMessage()
+    // Then set the component input value to an empty string
     setInput('')
+    // Then disable sending messages, since the value of the component input is an empty string
     setIsDisabled(true)
   }
 
