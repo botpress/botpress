070e8e767e62a885965a62bd16d48fd600fb670e
Samuel Massé <samuelmasse4@gmail.com>
2021-10-12 15:08:57 -0400

    fix

--- packages/bp/src/core/messaging/messaging-router.ts
+++ packages/bp/src/core/messaging/messaging-router.ts
@@ -20,17 +20,17 @@ export class MessagingRouter extends CustomRouter {
   public setupRoutes(): void {
     this.router.post(
       '/receive',
-      this.validateRequest,
+      this.validateRequest.bind(this),
       this.asyncMiddleware(async (req, res) => {
         const event: MessagingEvent = req.body
 
         if (event.type === 'message.new') {
-          const { error } = MessageNewEventSchema.validate(req.body)
+          const { error } = MessageNewEventSchema.validate(event.data)
           if (error) {
             return res.status(400).send(error.message)
           }
 
-          await this.messaging.receive(req.body as MessageNewEventData)
+          await this.messaging.receive(event.data as MessageNewEventData)
         } else if (event.type === 'user.new') {
           this.messaging.incrementNewUsersCount()
         }
@@ -42,16 +42,20 @@ export class MessagingRouter extends CustomRouter {
     this.legacy.setup()
   }
 
-  private validateRequest(req: Request, res: Response, next: NextFunction | undefined) {
+  private validateRequest(req: Request, res: Response, next: NextFunction) {
     if (this.messaging.isExternal) {
-      if (req.headers.password !== process.INTERNAL_PASSWORD) {
-        return next?.(new UnauthorizedError('Password is missing or invalid'))
-      }
-    } else {
       const token = req.headers['x-webhook-token']
 
-      if (!token || token !== this.messaging.getWebhookToken(req.body?.clientId)) {
-        return next?.(new UnauthorizedError('Invalid webhook token'))
+      if (!token || token !== this.messaging.getWebhookToken(req.body?.data?.clientId)) {
+        return next(new UnauthorizedError('Invalid webhook token'))
+      } else {
+        return next(undefined)
+      }
+    } else {
+      if (req.headers.password !== process.INTERNAL_PASSWORD) {
+        return next(new UnauthorizedError('Password is missing or invalid'))
+      } else {
+        return next(undefined)
       }
     }
   }
@@ -70,23 +74,20 @@ interface MessagingEvent {
   data: any
 }
 
-const MessageNewEventSchema = joi.object({
-  type: joi.string().required(),
-  data: joi
-    .object({
-      clientId: joi.string().required(),
-      userId: joi.string().required(),
-      conversationId: joi.string().required(),
-      channel: joi.string().required(),
-      message: joi
-        .object({
-          id: joi.string().required(),
-          conversationId: joi.string().required(),
-          authorId: joi.string().required(),
-          sentOn: joi.date().required(),
-          payload: joi.object().required()
-        })
-        .required()
-    })
-    .required()
-})
+const MessageNewEventSchema = joi
+  .object({
+    clientId: joi.string().required(),
+    userId: joi.string().required(),
+    conversationId: joi.string().required(),
+    channel: joi.string().required(),
+    message: joi
+      .object({
+        id: joi.string().required(),
+        conversationId: joi.string().required(),
+        authorId: joi.string().required(),
+        sentOn: joi.date().required(),
+        payload: joi.object().required()
+      })
+      .required()
+  })
+  .required()
