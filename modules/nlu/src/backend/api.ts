e961e30069dba00a012058fb414d4968b32793c9
franklevasseur <francois_levasseur@hotmail.com>
2021-11-01 10:20:29 -0400

    fix(nlu): nlu module returns 404 when nlu server is unreachable

--- modules/nlu/src/backend/api.ts
+++ modules/nlu/src/backend/api.ts
@@ -74,7 +74,10 @@ export const registerRouter = async (bp: typeof sdk, app: NLUApplication) => {
   router.get('/health', async (req, res) => {
     // When the health is bad, we'll refresh the status in case it has changed (eg: user added languages)
     const health = await app.getHealth()
-    res.send(health)
+    if (!health) {
+      return res.status(404).send('NLU Server is unreachable')
+    }
+    return res.send(health)
   })
 
   router.get('/training/:language', async (req, res) => {
