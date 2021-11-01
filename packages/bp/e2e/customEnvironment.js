3b42faa246211576331c20fb29cc9d6bec977485
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-11-01 17:47:41 -0400

    chore(e2e): fix saving screenshot when some test fail

--- packages/bp/e2e/customEnvironment.js
+++ packages/bp/e2e/customEnvironment.js
@@ -1,11 +1,14 @@
 const PuppeteerEnvironment = require('jest-environment-puppeteer')
 
 class CustomEnvironment extends PuppeteerEnvironment {
-  async handleTestEvent(event, state) {
-    if (event.name == 'test_fn_failure') {
-      const testName = state.currentlyRunningTest.name
+  savePath = path.join(__dirname, '../../../', 'build/tests/e2e/screenshots')
+
+  async handleTestEvent(event, _state) {
+    if (event.name === 'test_fn_failure') {
+      const filename = path.format({ dir: this.savePath, name: event.test.name, ext: '.png' })
+
       // Take a screenshot at the point of failure
-      await this.global.page.screenshot({ path: `build/tests/e2e/screenshots/${testName}.png` })
+      await this.global.page.screenshot({ path: filename })
     }
   }
 }
