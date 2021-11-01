deda21bc1d10e5e603956f20fe46346140f226c9
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-11-01 19:31:43 -0400

    test

--- packages/bp/e2e/customEnvironment.js
+++ packages/bp/e2e/customEnvironment.js
@@ -4,9 +4,9 @@ const path = require('path')
 class CustomEnvironment extends PuppeteerEnvironment {
   savePath = path.join(__dirname, '../../../', 'build/tests/e2e/screenshots')
 
-  async handleTestEvent(event, _state) {
+  async handleTestEvent(event, state) {
     if (event.name === 'test_fn_failure') {
-      const filename = path.format({ dir: this.savePath, name: event.test.name, ext: '.png' })
+      const filename = path.format({ dir: this.savePath, name: state.currentlyRunningTest.name, ext: '.png' })
 
       // Take a screenshot at the point of failure
       await this.global.page.screenshot({ path: filename })
