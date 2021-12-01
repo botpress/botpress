17b52e4d7a3fb53136baac6b3984352bec64fe37
allardy <yann.allard1@gmail.com>
2021-12-01 02:28:32 -0500

    fix build warning

--- build/module-builder/src/webpack.ts
+++ build/module-builder/src/webpack.ts
@@ -63,7 +63,7 @@ export function config(projectPath) {
               presets: [['@babel/preset-env'], '@babel/preset-typescript', '@babel/preset-react'],
               plugins: [
                 ['@babel/plugin-proposal-decorators', { legacy: true }],
-                ['@babel/plugin-proposal-class-properties', { loose: true }],
+                ['@babel/plugin-proposal-class-properties'],
                 '@babel/plugin-syntax-function-bind',
                 '@babel/plugin-proposal-function-bind'
               ]
