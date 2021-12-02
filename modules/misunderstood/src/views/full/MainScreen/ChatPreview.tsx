a7e65148e12c7b7d58ffdab4435de5155aaa4d6d
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-12-02 18:25:13 -0500

    fix card display and add support for session_reset

--- modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
+++ modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
@@ -12,6 +12,8 @@ const fixPayload = (payload: any): ContextMessage['payload'] => {
   // We convert them into text elements since we only want to display the text value
   if (payload.type === 'single-choice' || payload.type === 'quick_reply') {
     payload = { ...payload, type: 'text' }
+  } else if (payload.type === 'dropdown') {
+    payload = { ...payload, type: 'text', text: payload.message }
   }
 
   return payload
@@ -19,21 +21,28 @@ const fixPayload = (payload: any): ContextMessage['payload'] => {
 
 const ChatPreview = ({ messages }: { messages: ContextMessage[] }) => (
   <div className={style.chatPreview}>
-    {messages.map((message, i) => (
-      <div
-        key={i}
-        className={classnames(style.chatPreviewMessage, {
-          [style.chatPreviewMessage_Incoming]: message.direction === 'incoming',
-          [style.chatPreviewMessage_Outgoing]: message.direction === 'outgoing',
-          [style.chatPreviewMessage_Current]: message.isCurrent
-        })}
-      >
-        <div className={style.chatPreviewAvatar}>{message.direction === 'incoming' ? 'U' : 'B'}</div>
-        <div className={style.chatPreviewText}>
-          <ReactMessageRenderer content={fixPayload(message.payload)} config={defaultMessageConfig} />
+    {messages.map((message, i) => {
+      // TODO: Add translation and move this logic into the @botpress/messaging-components lib
+      if (message.type === 'session_reset') {
+        return <div className={style.chatPreviewMessage_System}>Reset the conversation</div>
+      }
+
+      return (
+        <div
+          key={i}
+          className={classnames(style.chatPreviewMessage, {
+            [style.chatPreviewMessage_Incoming]: message.direction === 'incoming',
+            [style.chatPreviewMessage_Outgoing]: message.direction === 'outgoing',
+            [style.chatPreviewMessage_Current]: message.isCurrent
+          })}
+        >
+          <div className={style.chatPreviewAvatar}>{message.direction === 'incoming' ? 'U' : 'B'}</div>
+          <div className={style.chatPreviewText}>
+            <ReactMessageRenderer content={fixPayload(message.payload)} config={defaultMessageConfig} />
+          </div>
         </div>
-      </div>
-    ))}
+      )
+    })}
   </div>
 )
 
