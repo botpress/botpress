0e8323ae990fd0714de7ce6f1dd042f2ab71ed36
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2021-12-06 17:09:09 -0500

    removed some temp hack + refactoring

--- modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
+++ modules/misunderstood/src/views/full/MainScreen/ChatPreview.tsx
@@ -6,19 +6,6 @@ import { ContextMessage } from '../../../types'
 
 import style from './style.scss'
 
-// TODO: Fix this either when we create the event or in the components lib
-const fixPayload = (payload: any): ContextMessage['payload'] => {
-  // Channel-web sends single-choice events and messaging replies with quick_reply
-  // We convert them into text elements since we only want to display the text value
-  if (payload.type === 'single-choice' || payload.type === 'quick_reply') {
-    payload = { ...payload, type: 'text' }
-  } else if (payload.type === 'dropdown') {
-    payload = { ...payload, type: 'text', text: payload.message }
-  }
-
-  return payload
-}
-
 const ChatPreview = ({ messages }: { messages: ContextMessage[] }) => (
   <div className={style.chatPreview}>
     {messages.map((message, i) => {
@@ -26,23 +13,29 @@ const ChatPreview = ({ messages }: { messages: ContextMessage[] }) => (
       if (message.type === 'session_reset') {
         return <div className={style.chatPreviewMessage_System}>Reset the conversation</div>
       }
+      const isUserMessage = message.direction === 'incoming'
+      const isBotMessage = message.direction === 'outgoing'
+      const isCardMessage = message.payload.type === 'card'
 
       return (
         <div
           key={i}
           className={classnames(style.chatPreviewMessage, {
-            [style.chatPreviewMessage_Incoming]: message.direction === 'incoming',
-            [style.chatPreviewMessage_Outgoing]: message.direction === 'outgoing',
+            [style.chatPreviewMessage_Incoming]: isUserMessage,
+            [style.chatPreviewMessage_Outgoing]: isBotMessage,
             [style.chatPreviewMessage_Current]: message.isCurrent
           })}
         >
-          <div className={style.chatPreviewAvatar}>{message.direction === 'incoming' ? 'U' : 'B'}</div>
+          <div className={style.chatPreviewAvatar}>{isUserMessage ? 'U' : 'B'}</div>
           <div
             className={classnames(style.chatPreviewText, {
-              [style.card]: message.payload.type === 'card'
+              [style.card]: isCardMessage
             })}
           >
-            <ReactMessageRenderer content={fixPayload(message.payload)} config={defaultMessageConfig} />
+            <ReactMessageRenderer
+              content={message.payload}
+              config={{ ...defaultMessageConfig, isBotMessage, isLastGroup: false, isLastOfGroup: false }}
+            />
           </div>
         </div>
       )
