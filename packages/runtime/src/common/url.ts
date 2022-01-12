51ced0113669d891c3a1d359546c7bd0300f2797
Yann Allard <yann.allard1@gmail.com>
2022-01-12 11:15:44 -0500

    pr comment

--- packages/runtime/src/common/url.ts
+++ packages/runtime/src/common/url.ts
@@ -4,17 +4,29 @@ export const isBpUrl = (str: string): boolean => {
   return re.test(str)
 }
 
-export const formatUrl = (baseUrl: string, url: string): string => {
+interface MediaUrlElements {
+  botId?: string
+  mediaId?: string
+  isBotpressUrl: boolean
+}
+
+const parseBotpressMediaUrl = (url: string): MediaUrlElements => {
   const bpUrlRegex = /^\/api\/.*\/bots\/(.*)\/media\/(.*)/g
   const parts = bpUrlRegex.exec(url)
 
-  if(!parts){
+  if (parts) {
+    return { botId: parts[1], mediaId: parts[2], isBotpressUrl: true }
+  }
+
+  return { isBotpressUrl: false }
+}
+
+export const formatUrl = (baseUrl: string, url: string): string => {
+  const item = parseBotpressMediaUrl(url)
+
+  if (!item.isBotpressUrl) {
     return url
   }
 
-  if(!process.env.MEDIA_URL){
-    return `${baseUrl}${url}`
-  }
-
-  return `${process.env.MEDIA_URL}/${parts[1]}/${parts[2]}`
+  return process.env.MEDIA_URL ? `${process.env.MEDIA_URL}/${item.botId}/${item.mediaId}` : `${baseUrl}${url}`
 }
