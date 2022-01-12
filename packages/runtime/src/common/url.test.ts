f8501401d3e338a6a6ef4de332a394da498d5436
Yann Allard <yann.allard1@gmail.com>
2022-01-12 11:27:58 -0500

    typo

--- packages/runtime/src/common/url.test.ts
+++ packages/runtime/src/common/url.test.ts
@@ -1,10 +1,10 @@
 import { formatUrl, parseBotpressMediaUrl } from './url'
 
-describe('URL Fromatting', () => {
+describe('URL Formatting', () => {
   const EXTERNAL_URL = 'http://localhost:3000'
   const MEDIA_URL = 'http://media.service'
 
-  it('Validate url parsing', () => {
+  it('Validate URL parsing', () => {
     const result = parseBotpressMediaUrl('/api/v1/bots/welcome-bot/media/abc123.png')
     expect(result).toEqual({
       isBotpressUrl: true,
@@ -13,17 +13,17 @@ describe('URL Fromatting', () => {
     })
   })
 
-  it('Basic url', () => {
+  it('Basic URL', () => {
     const result = formatUrl(EXTERNAL_URL, '/api/v1/bots/welcome-bot/media/abc123.png')
     expect(result).toBe(`${EXTERNAL_URL}/api/v1/bots/welcome-bot/media/abc123.png`)
   })
 
-  it('Not a botpress URL', () => {
+  it('Not a Botpress URL', () => {
     const result = formatUrl(EXTERNAL_URL, 'http://google.com/images/abc123.png')
     expect(result).toBe('http://google.com/images/abc123.png')
   })
 
-  it('With a custom media url', () => {
+  it('With a custom media URL', () => {
     const result = formatUrl(EXTERNAL_URL, '/api/v1/bots/welcome-bot/media/abc123.png', MEDIA_URL)
     expect(result).toBe(`${MEDIA_URL}/welcome-bot/abc123.png`)
   })
