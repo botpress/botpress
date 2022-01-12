f875802238f8f73905ac90d2a0ec16a2d76ca8f9
Yann Allard <yann.allard1@gmail.com>
2022-01-12 10:26:14 -0500

    feat(runtime): add media url support

--- packages/runtime/src/common/url.ts
+++ packages/runtime/src/common/url.ts
@@ -4,11 +4,17 @@ export const isBpUrl = (str: string): boolean => {
   return re.test(str)
 }
 
-// Duplicate of modules/builtin/src/content-types/_utils.js
 export const formatUrl = (baseUrl: string, url: string): string => {
-  if (isBpUrl(url)) {
-    return `${baseUrl}${url}`
-  } else {
+  const bpUrlRegex = /^\/api\/.*\/bots\/(.*)\/media\/(.*)/g
+  const parts = bpUrlRegex.exec(url)
+
+  if(!parts){
     return url
   }
+
+  if(!process.env.MEDIA_URL){
+    return `${baseUrl}${url}`
+  }
+
+  return `${process.env.MEDIA_URL}/${parts[1]}/${parts[2]}`
 }
