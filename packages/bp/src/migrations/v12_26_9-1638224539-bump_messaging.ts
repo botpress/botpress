811ad5efdff02ffb385a4ba290e96369b7960cb1
Samuel Massé <samuelmasse4@gmail.com>
2022-01-13 14:03:33 -0500

    bump versions

--- packages/bp/src/migrations/v12_26_9-1638224539-bump_messaging.ts
+++ packages/bp/src/migrations/v12_26_9-1638224539-bump_messaging.ts
@@ -4,13 +4,13 @@ import { runMessagingMigration } from 'orchestrator/messaging-server'
 
 const migration: Migration = {
   info: {
-    description: 'Migrates the messaging database from 0.1.18 to 0.1.20',
+    description: 'Migrates the messaging database from 0.1.18 to 0.1.21',
     target: 'core',
     type: 'database'
   },
 
   up: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
-    return runMessagingMigration('up', '0.1.20', metadata.isDryRun)
+    return runMessagingMigration('up', '0.1.21', metadata.isDryRun)
   },
 
   down: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
