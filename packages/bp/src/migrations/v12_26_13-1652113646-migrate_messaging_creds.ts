92b547962ff681421be5d75238d6204daa171199
Samuel Massé <samuelmasse4@gmail.com>
2022-03-25 14:14:31 -0400

    better up migration

--- packages/bp/src/migrations/v12_26_13-1652113646-migrate_messaging_creds.ts
+++ packages/bp/src/migrations/v12_26_13-1652113646-migrate_messaging_creds.ts
@@ -10,15 +10,45 @@ const migration: Migration = {
 
   up: async ({ bp, metadata, configProvider }: MigrationOpts): Promise<sdk.MigrationResult> => {
     const migration = async (botId: string, botConfig: any) => {
-      await bp.database('srv_channels').insert({
-        botId,
-        clientId: botConfig.messaging.id,
-        clientToken: botConfig.messaging.token,
-        config: bp.database.json.set({})
-      })
+      const move = async () => {
+        if (!botConfig.messaging) {
+          return bp.logger.warn(`[${botId}] No messaging config to migrate`)
+        } else if (!botConfig.messaging.id?.length) {
+          return bp.logger.warn(`[${botId}] No messaging clientId to migrate`)
+        } else if (!botConfig.messaging.token?.length) {
+          return bp.logger.warn(`[${botId}] No messaging clientToken to migrate`)
+        }
 
-      delete botConfig.messaging.id
-      delete botConfig.messsaging.token
+        await bp
+          .database('srv_channels')
+          .where({ clientId: botConfig.messaging.id })
+          .del()
+        await bp
+          .database('srv_channels')
+          .where({ botId })
+          .del()
+
+        await bp.database('srv_channels').insert({
+          botId,
+          clientId: botConfig.messaging.id,
+          clientToken: botConfig.messaging.token,
+          config: bp.database.json.set({})
+        })
+      }
+
+      try {
+        await move()
+      } catch (e) {
+        bp.logger
+          .attachError(e)
+          .error(`[${botId}] Failed to move messaging credentials to srv_channels. New credentials willl be generated`)
+      }
+
+      if (botConfig.messaging) {
+        delete botConfig.messaging.id
+        delete botConfig.messaging.token
+      }
+
       await configProvider.setBotConfig(botId, botConfig)
     }
 
