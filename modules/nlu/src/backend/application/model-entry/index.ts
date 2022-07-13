c4ab4467077fb1722e0506355542a7c5547839cc
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-07-13 11:41:54 -0400

    remove nlu module

--- modules/nlu/src/backend/application/model-entry/index.ts
+++
@@ -1,4 +0,0 @@
-export { ModelEntryService } from './model-entry-service'
-export { TrainingEntryService } from './training-entry-service'
-export { ModelEntryRepository } from './model-entry-repo'
-export * from './typings'
--- modules/nlu/src/backend/application/model-entry/typings.ts
+++
@@ -1,9 +0,0 @@
-export interface ModelKey {
-  botId: string
-  language: string
-}
-
-export interface ModelEntry extends ModelKey {
-  modelId: string
-  definitionHash: string
-}
