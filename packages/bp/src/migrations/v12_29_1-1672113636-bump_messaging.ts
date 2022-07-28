9f1118374673e06b0a97e43197599578673340e4
Laurent Leclerc-Poulin <laurentleclercpoulin@gmail.com>
2022-07-28 10:28:27 -0400

    add migration for messaging

---
+++ packages/bp/src/migrations/v12_29_1-1672113636-bump_messaging.ts
@@ -0,0 +1,21 @@
+import * as sdk from 'botpress/sdk'
+import { Migration, MigrationOpts } from 'core/migration'
+import { runMessagingMigration } from 'orchestrator/messaging-server'
+
+const migration: Migration = {
+  info: {
+    description: 'Migrates the messaging database from 1.2.3 to 1.2.4',
+    target: 'core',
+    type: 'database'
+  },
+
+  up: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
+    return runMessagingMigration('up', '1.2.4', metadata.isDryRun)
+  },
+
+  down: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
+    return runMessagingMigration('down', '1.2.3', metadata.isDryRun)
+  }
+}
+
+export default migration
