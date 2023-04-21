f0c04274492ea4903250ef9fcfe5143b87fdef11
David Vitor Antonio <david.vitora@gmail.com>
2023-04-21 11:50:10 -0300

    chore(core): messaging migration

--- packages/bp/src/migrations/v12_30_8-1682086166-bump_messaging.ts
+++ packages/bp/src/migrations/v12_30_8-1682086166-bump_messaging.ts
@@ -4,13 +4,13 @@ import { runMessagingMigration } from 'orchestrator/messaging-server'
 
 const migration: Migration = {
   info: {
-    description: 'Migrates the messaging database from 1.2.7 to 1.2.10',
+    description: 'Migrates the messaging database from 1.2.7 to 1.2.9',
     target: 'core',
     type: 'database'
   },
 
   up: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
-    return runMessagingMigration('up', '1.2.10', metadata.isDryRun)
+    return runMessagingMigration('up', '1.2.9', metadata.isDryRun)
   },
 
   down: async ({ metadata }: MigrationOpts): Promise<sdk.MigrationResult> => {
