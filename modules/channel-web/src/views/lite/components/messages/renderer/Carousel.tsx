a02b7827df8d9eec59e65b4e0c1e0e193e73024d
David Vitor Antonio <david.vitora@gmail.com>
2023-05-01 15:00:30 -0300

    fix(channel-web): render carousel right away (#12597)

--- modules/channel-web/src/views/lite/components/messages/renderer/Carousel.tsx
+++ modules/channel-web/src/views/lite/components/messages/renderer/Carousel.tsx
@@ -27,8 +27,13 @@ export class Carousel extends React.Component<ICarouselProps, ICarouselState> {
   }
 
   componentDidMount() {
-    // Delay this to avoid incorrect values when closing-opening the webchat
-    setTimeout(() => this.setState({ adjustedWidth: this.ref.offsetWidth - window.innerWidth }), 300)
+    this.setState({ adjustedWidth: this.ref.offsetWidth - window.innerWidth })
+    // Reset this to avoid incorrect values when closing-opening the webchat
+    setTimeout(() => {
+      this.setState({
+        adjustedWidth: this.ref.offsetWidth - window.innerWidth
+      })
+    }, 300)
   }
 
   renderCarousel() {
@@ -51,10 +56,10 @@ export class Carousel extends React.Component<ICarouselProps, ICarouselState> {
       arrows: elements.length > 1
     }
 
-    const settings = Object.assign({}, defaultSettings, carousel.settings)
+    const settings = Object.assign({}, defaultSettings, carousel?.settings)
 
     return (
-      <Slider {...settings}>
+      <Slider key={this.state.adjustedWidth} {...settings}>
         {elements.map((el, idx) => (
           <Card
             element={el}
