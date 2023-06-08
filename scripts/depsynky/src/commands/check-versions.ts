cd2aab74a39441c4b5cc4afe96b4335858d30729
Francois Levasseur <francois.levasseur@botpress.com>
2023-06-08 18:16:20 -0400

    forbid references to local package in public packages

--- scripts/depsynky/src/commands/check-versions.ts
+++ scripts/depsynky/src/commands/check-versions.ts
@@ -11,45 +11,38 @@ export type CheckVersionsOpts = {
 
 const LOCAL_VERSION = 'workspace:*'
 
-type CheckReport = { name: string; currentVersion: string; targetVersion: string }
-const check = (current: Record<string, string>, target: Record<string, string>): CheckReport | undefined => {
-  for (const [name, version] of utils.objects.entries(target)) {
-    const currentVersion = current[name]
-    if (currentVersion && currentVersion !== LOCAL_VERSION && currentVersion !== version) {
-      return {
-        name,
-        currentVersion,
-        targetVersion: version,
+const checker =
+  (pkg: utils.pkgjson.PackageJson) => (current: Record<string, string>, target: Record<string, string>) => {
+    for (const [name, version] of utils.objects.entries(target)) {
+      const currentVersion = current[name]
+      if (!currentVersion) {
+        continue
+      }
+      const isLocal = currentVersion === LOCAL_VERSION
+      const isPublic = !pkg.private
+      if (isPublic && isLocal) {
+        throw new errors.DepSynkyError(
+          `Package ${pkg.name} is public and cannot depend on local package ${name}. To keep reference on local package, make ${pkg.name} private.`
+        )
+      }
+      if (!isLocal && currentVersion !== version) {
+        throw new errors.DepSynkyError(
+          `Dependency ${name} is out of sync in ${pkg.name}: ${currentVersion} != ${version}`
+        )
       }
     }
   }
-
-  return
-}
 
 export const checkVersions = (argv: YargsConfig<typeof config.checkSchema>, opts: Partial<CheckVersionsOpts> = {}) => {
   const allPackages = utils.pnpm.searchWorkspaces(argv.rootDir)
   const targetVersions = opts.targetVersions ?? utils.pnpm.versions(allPackages)
 
-  for (const {
-    path: pkgPath,
-    content: { name: pkgName },
-  } of allPackages) {
+  for (const { path: pkgPath, content } of allPackages) {
     const { dependencies, devDependencies } = utils.pkgjson.read(pkgPath)
 
-    const depReport = dependencies && check(dependencies, targetVersions)
-    if (depReport) {
-      throw new errors.DepSynkyError(
-        `Dependency ${depReport.name} is out of sync in ${pkgName}: ${depReport.currentVersion} != ${depReport.targetVersion}`
-      )
-    }
-
-    const devDepReport = devDependencies && check(devDependencies, targetVersions)
-    if (devDepReport) {
-      throw new errors.DepSynkyError(
-        `Dev dependency ${devDepReport.name} is out of sync in ${pkgName}: ${devDepReport.currentVersion} != ${devDepReport.targetVersion}`
-      )
-    }
+    const check = checker(content)
+    dependencies && check(dependencies, targetVersions)
+    devDependencies && check(devDependencies, targetVersions)
   }
 
   logger.info('All versions are in sync')
--- scripts/depsynky/src/commands/sync-versions.ts
+++ scripts/depsynky/src/commands/sync-versions.ts
@@ -8,23 +8,38 @@ export type SyncVersionsOpts = {
 }
 
 const LOCAL_VERSION = 'workspace:*'
-const update = (current: Record<string, string>, target: Record<string, string>) => {
-  for (const [name, version] of utils.objects.entries(target)) {
-    const currentVersion = current[name]
-    if (currentVersion && currentVersion !== LOCAL_VERSION) {
-      current[name] = version
+const updater =
+  (pkg: utils.pkgjson.PackageJson) => (current: Record<string, string>, target: Record<string, string>) => {
+    for (const [name, version] of utils.objects.entries(target)) {
+      const currentVersion = current[name]
+      if (!currentVersion) {
+        continue
+      }
+      const isLocal = currentVersion === LOCAL_VERSION
+      const isPublic = !pkg.private
+      if (!isLocal) {
+        current[name] = version
+        continue
+      }
+      if (isPublic && isLocal) {
+        utils.logging.logger.warn(
+          `Package ${pkg.name} is public and cannot depend on local package ${name}. To keep reference on local package, make ${pkg.name} private.`
+        )
+        current[name] = version
+        continue
+      }
     }
+    return current
   }
-  return current
-}
 
 export const syncVersions = (argv: YargsConfig<typeof config.syncSchema>, opts: Partial<SyncVersionsOpts> = {}) => {
   const allPackages = searchWorkspaces(argv.rootDir)
   const targetVersions = opts.targetVersions ?? utils.pnpm.versions(allPackages)
 
-  for (const { path: pkgPath } of allPackages) {
+  for (const { path: pkgPath, content } of allPackages) {
     const { dependencies, devDependencies } = utils.pkgjson.read(pkgPath)
 
+    const update = updater(content)
     const updatedDeps = dependencies && update(dependencies, targetVersions)
     const updatedDevDeps = devDependencies && update(devDependencies, targetVersions)
 
