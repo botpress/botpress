387824afa88f71fc3d88ec9ec8f8ca7ece799f09
Sylvain Perron <sylvain.perron@botpress.com>
2023-06-28 19:09:24 -0400

    ..

--- integrations/linear/src/setup.ts
+++ integrations/linear/src/setup.ts
@@ -1,16 +1,15 @@
 import { CreateConversationFunction, RegisterFunction, UnregisterFunction } from './misc/types'
 // import { getLinearClient } from './misc/utils'
 
-export const register: RegisterFunction = async ({ ctx, client }) => {
+export const register: RegisterFunction = async () => {
   // const linearClient = await getLinearClient(client, ctx.integrationId)
   // const user = await linearClient.userSettings.then((settings) => settings.user)
-
-  await client.setState({
-    type: 'integration',
-    name: 'configuration',
-    id: ctx.integrationId,
-    payload: { botUserId: undefined }, // TODO: fixme for non-oauth
-  })
+  // await client.setState({
+  //   type: 'integration',
+  //   name: 'configuration',
+  //   id: ctx.integrationId,
+  //   payload: { botUserId: undefined }, // TODO: fixme for non-oauth
+  // })
 }
 
 export const unregister: UnregisterFunction = async () => {
