63211dca606ae0c23a8950bfe64c373b1a28d3a0
Sylvain Perron <sylvain.perron@botpress.com>
2023-06-28 18:59:14 -0400

    fix register

--- integrations/linear/src/setup.ts
+++ integrations/linear/src/setup.ts
@@ -1,15 +1,15 @@
 import { CreateConversationFunction, RegisterFunction, UnregisterFunction } from './misc/types'
-import { getLinearClient } from './misc/utils'
+// import { getLinearClient } from './misc/utils'
 
 export const register: RegisterFunction = async ({ ctx, client }) => {
-  const linearClient = await getLinearClient(client, ctx.integrationId)
-  const user = await linearClient.userSettings.then((settings) => settings.user)
+  // const linearClient = await getLinearClient(client, ctx.integrationId)
+  // const user = await linearClient.userSettings.then((settings) => settings.user)
 
   await client.setState({
     type: 'integration',
     name: 'configuration',
     id: ctx.integrationId,
-    payload: { botUserId: user?.id },
+    payload: { botUserId: undefined }, // TODO: fixme for non-oauth
   })
 }
 
