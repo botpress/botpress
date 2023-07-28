f6e6c572a912f72c2b31e297c664d78c1e1bd8fc
François Levasseur <francois_levasseur@hotmail.com>
2023-07-27 20:40:24 -0400

    fix(cli): handle http status 400 as integration with version not found (#12744)

--- packages/cli/src/api-client.ts
+++ packages/cli/src/api-client.ts
@@ -54,16 +54,16 @@ export class ApiClient {
 
   public async findPrivateIntegration(ref: IntegrationRef): Promise<Integration | undefined> {
     if (ref.type === 'id') {
-      return this.validateStatus(() => this.client.getIntegration(ref).then((r) => r.integration), [404, 500]) // TODO: fix that
+      return this.validateStatus(() => this.client.getIntegration(ref).then((r) => r.integration), 404)
     }
-    return this.validateStatus(() => this.client.getIntegrationByName(ref).then((r) => r.integration), [404, 500])
+    return this.validateStatus(() => this.client.getIntegrationByName(ref).then((r) => r.integration), 404)
   }
 
   public async findPublicIntegration(ref: IntegrationRef): Promise<Integration | undefined> {
     if (ref.type === 'id') {
-      return this.validateStatus(() => this.client.getPublicIntegrationById(ref).then((r) => r.integration), [404, 500])
+      return this.validateStatus(() => this.client.getPublicIntegrationById(ref).then((r) => r.integration), 404)
     }
-    return this.validateStatus(() => this.client.getPublicIntegration(ref).then((r) => r.integration), [404, 500])
+    return this.validateStatus(() => this.client.getPublicIntegration(ref).then((r) => r.integration), 404)
   }
 
   public async testLogin(): Promise<void> {
