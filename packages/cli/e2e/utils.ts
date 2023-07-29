1685ef7d06dd7e2b895a94e052f891bf294668f7
Francois Levasseur <francois.levasseur@botpress.com>
2023-07-29 17:10:30 -0400

    chore(cli): e2e tests install local sdk and client

--- packages/cli/e2e/utils.ts
+++ packages/cli/e2e/utils.ts
@@ -1,5 +1,18 @@
 import childprocess from 'child_process'
+import fs from 'fs'
+import _ from 'lodash'
+import pathlib from 'path'
 import tmp from 'tmp'
+
+type PackageJson = {
+  name: string
+  version?: string
+  description?: string
+  scripts?: Record<string, string>
+  dependencies?: Record<string, string>
+  devDependencies?: Record<string, string>
+  peerDependencies?: Record<string, string>
+}
 
 export const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms))
 
@@ -33,6 +46,24 @@ export const npmInstall = async ({ workDir }: { workDir: string }) => {
     stdio: 'inherit',
   })
   return { exitCode: status ?? 0 }
+}
+
+export const fixBotpressDependencies = async ({
+  workDir,
+  target,
+}: {
+  workDir: string
+  target: Record<string, string | undefined>
+}) => {
+  const packageJsonPath = pathlib.join(workDir, 'package.json')
+  const originalPackageJson: PackageJson = require(packageJsonPath)
+
+  const newPackageJson = {
+    ...originalPackageJson,
+    dependencies: _.mapValues(originalPackageJson.dependencies ?? {}, (version, name) => target[name] ?? version),
+  }
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson, null, 2))
 }
 
 export const handleExitCode = ({ exitCode }: { exitCode: number }) => {
