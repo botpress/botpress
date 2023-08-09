0a8742f2f2a6c314e3aed0f622672cfe98c91e3d
Bassam Tantawi <bassam.tantawi@botpress.com>
2023-08-09 13:22:27 -0400

    lint check

--- integrations/shopify/src/setup/unregister.ts
+++ integrations/shopify/src/setup/unregister.ts
@@ -1,5 +1,5 @@
-import type { IntegrationContext, IntegrationProps } from '@botpress/sdk'
 import { Client } from '@botpress/client'
+import type { IntegrationContext, IntegrationProps } from '@botpress/sdk'
 import axios from 'axios'
 import { SHOPIFY_API_VERSION } from '../const'
 import type * as botpress from '.botpress'
@@ -28,7 +28,7 @@ export const unregister: UnregisterFunction = async ({
     await deleteWebhook(ctx, webhook.webhookId, logger)
   }
 
-  async function deleteWebhook(ctx: IntegrationContext<Configuration>, webhookId: string, logger: IntegrationLogger) {
+  async function deleteWebhook(ctx, webhookId, logger) {
     try {
       const axiosConfig = {
         baseURL: `https://${ctx.configuration.shopName}.myshopify.com`,
