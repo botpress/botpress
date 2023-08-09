497a236ab1ff36b1f9c263799c5469039c539432
Roberto Escalante H <30730628+RobertoE91@users.noreply.github.com>
2023-08-09 08:49:02 -0600

    feat(integrations): asana (#12759)

---
+++ integrations/asana/src/actions/create-task.ts
@@ -0,0 +1,28 @@
+import { createTaskInputSchema } from '../misc/custom-schemas'
+import type { Implementation } from '../misc/types'
+
+import { getClient } from '../utils'
+
+export const createTask: Implementation['actions']['createTask'] = async ({ ctx, input, logger }) => {
+  const validatedInput = createTaskInputSchema.parse(input)
+  const asanaClient = getClient(ctx.configuration)
+  const task = {
+    workspace: ctx.configuration.workspaceGid,
+    name: validatedInput.name,
+    notes: validatedInput.notes || undefined,
+    assignee: validatedInput.assignee || undefined,
+    projects: validatedInput.projects?.split(',').map((project) => project.trim()) || undefined,
+    parent: validatedInput.parent || undefined,
+    due_on: validatedInput.due_on || validatedInput.start_on || undefined,
+    start_on: validatedInput.start_on || undefined,
+  }
+  let response
+  try {
+    response = await asanaClient.createTask(task)
+    logger.forBot().info(`Successful - Create Task - ${response.permalink_url}`)
+  } catch (error) {
+    logger.forBot().debug(`'Create Task' exception ${JSON.stringify(error)}`)
+    response = {}
+  }
+  return { permalink_url: response.permalink_url || '' }
+}
---
+++ integrations/asana/src/actions/find-user.ts
@@ -0,0 +1,18 @@
+import { findUserInputSchema } from '../misc/custom-schemas'
+import type { Implementation } from '../misc/types'
+
+import { getClient } from '../utils'
+
+export const findUser: Implementation['actions']['findUser'] = async ({ ctx, input, logger }) => {
+  const validatedInput = findUserInputSchema.parse(input)
+  const asanaClient = getClient(ctx.configuration)
+  let response
+  try {
+    response = await asanaClient.findUser(validatedInput.userEmail)
+    logger.forBot().info(`Successful - Find User - ${response.name}`)
+  } catch (error) {
+    logger.forBot().debug(`'Find User' exception ${JSON.stringify(error)}`)
+    response = {}
+  }
+  return response
+}
