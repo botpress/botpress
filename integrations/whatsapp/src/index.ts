a09b8c90d595c0696345b1397654e63d7d6f4397
Abraham Lopez <abraham.lopez@botpress.com>
2023-08-16 13:30:13 -0600

    fix(whatsapp): prompts with more than 3 options should show as a dropdown

--- integrations/whatsapp/src/index.ts
+++ integrations/whatsapp/src/index.ts
@@ -87,7 +87,20 @@ const integration = new Integration({
           })
         },
         choice: async ({ payload, logger, ...props }) => {
-          await outgoing.sendMany({ ...props, logger, generator: choice.generateOutgoingMessages({ payload, logger }) })
+          if (payload.options.length <= choice.INTERACTIVE_MAX_BUTTONS_COUNT) {
+            await outgoing.sendMany({
+              ...props,
+              logger,
+              generator: choice.generateOutgoingMessages({ payload, logger }),
+            })
+          } else {
+            // If choice options exceeds the maximum number of buttons allowed by Whatsapp we use a dropdown instead to avoid buttons being split into multiple groups with a repeated message.
+            await outgoing.sendMany({
+              ...props,
+              logger,
+              generator: dropdown.generateOutgoingMessages({ payload, logger }),
+            })
+          }
         },
       },
     },
