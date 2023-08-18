06f910e47ddeb81c6bc6a4cdce78116c9230ee96
Francois Levasseur <francois.levasseur@botpress.com>
2023-08-18 12:38:19 -0400

    1692376699011

--- .github/scripts/ls-sentry-integrations.sh
+++ .github/scripts/ls-sentry-integrations.sh
@@ -5,7 +5,7 @@ filter=""
 for sentry_integration in "${sentry_integrations[@]}";
 do 
   actual_integration=$(echo $sentry_integration | sed 's/\.sentryclirc//g')
-  filter="-F $actual_integration $filter"
+  filter="-F ./$actual_integration $filter"
 done
 
 echo $filter
