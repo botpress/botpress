5e87e29b66dec89895cd689fac3630c86c6c8323
allardy <yann.allard1@gmail.com>
2023-08-18 12:42:01 -0400

    add to impl

--- integrations/zendesk/src/actions/index.ts
+++ integrations/zendesk/src/actions/index.ts
@@ -2,10 +2,12 @@ import { closeTicket } from './close-ticket'
 import { createTicket } from './create-ticket'
 import { findCustomer } from './find-customer'
 import { getTicket } from './get-ticket'
+import { listAgents } from './list-agents'
 
 export default {
   getTicket,
   findCustomer,
   createTicket,
   closeTicket,
+  listAgents,
 }
