b7777f52253bdc5f5f7464e28d26243b3c80bf7d
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2023-08-22 17:28:51 -0400

    feat(integrations/trello): added trello integration (#12784)

---
+++ integrations/trello/src/setup/register.ts
@@ -0,0 +1,8 @@
+import type { RegisterFunction } from '../misc/types'
+
+export const register: RegisterFunction = async () => {
+  /**
+   * This is called when a bot installs the integration.
+   * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
+   */
+}
