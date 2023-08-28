2d48003a6a2865c5791354196a857d1d69513fe3
allardy <yann.allard1@gmail.com>
2023-08-28 13:23:08 -0400

    add author id

--- integrations/zendesk/src/actions/close-ticket.ts
+++ integrations/zendesk/src/actions/close-ticket.ts
@@ -3,8 +3,11 @@ import { getZendeskClient } from '../client'
 import { IntegrationProps } from '.botpress'
 
 export const closeTicket: IntegrationProps['actions']['closeTicket'] = async ({ ctx, input }) => {
+  const originalTicket = await getZendeskClient(ctx.configuration).getTicket(input.ticketId)
+
   const ticket = await getZendeskClient(ctx.configuration).updateTicket(input.ticketId, {
     comment: input.comment,
+    authorId: originalTicket.requester_id,
     status: 'closed',
   })
 
