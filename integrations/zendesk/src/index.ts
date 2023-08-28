50099ede63f36d13e5cb8f79ba59e78b69744929
Francois Levasseur <francois.levasseur@botpress.com>
2023-08-28 14:07:18 -0400

    fix checks

--- integrations/zendesk/src/index.ts
+++ integrations/zendesk/src/index.ts
@@ -3,15 +3,9 @@ import actions from './actions'
 import channels from './channels'
 import { handler } from './handler'
 import { register, unregister } from './setup'
-import { Integration, secrets } from '.botpress'
+import * as bp from '.botpress'
 
-sentryHelpers.init({
-  dsn: secrets.SENTRY_DSN,
-  environment: secrets.SENTRY_ENVIRONMENT,
-  release: secrets.SENTRY_RELEASE,
-})
-
-const integration = new Integration({
+const integration = new bp.Integration({
   register,
   unregister,
   actions,
@@ -19,4 +13,8 @@ const integration = new Integration({
   handler,
 })
 
-export default sentryHelpers.wrapIntegration(integration)
+export default sentryHelpers.wrapIntegration(integration, {
+  dsn: bp.secrets.SENTRY_DSN,
+  environment: bp.secrets.SENTRY_ENVIRONMENT,
+  release: bp.secrets.SENTRY_RELEASE,
+})
