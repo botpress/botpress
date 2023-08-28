4fc671c9a81b16d0e60367437afe092f96261f69
allardy <yann.allard1@gmail.com>
2023-08-28 15:02:30 -0400

    console

--- integrations/zendesk/src/actions/close-ticket.ts
+++ integrations/zendesk/src/actions/close-ticket.ts
@@ -5,7 +5,14 @@ import { IntegrationProps } from '.botpress'
 export const closeTicket: IntegrationProps['actions']['closeTicket'] = async ({ ctx, input }) => {
   const originalTicket = await getZendeskClient(ctx.configuration).getTicket(input.ticketId)
 
+  console.info(originalTicket)
   const ticket = await getZendeskClient(ctx.configuration).updateTicket(input.ticketId, {
+    comment: input.comment,
+    authorId: originalTicket.requester_id,
+    status: 'closed',
+  })
+
+  console.log('update with', {
     comment: input.comment,
     authorId: originalTicket.requester_id,
     status: 'closed',
