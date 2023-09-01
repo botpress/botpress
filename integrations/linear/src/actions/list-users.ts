7126f7192a626e1701ec63807381faa2fcb6c6ab
allardy <yann.allard1@gmail.com>
2023-09-01 01:50:45 -0400

    remove console

--- integrations/linear/src/actions/list-users.ts
+++ integrations/linear/src/actions/list-users.ts
@@ -10,11 +10,6 @@ export const listUsers: IntegrationProps['actions']['listUsers'] = async ({
 }) => {
   const linearClient = await getLinearClient(client, ctx.integrationId)
 
-  console.log('query', {
-    orderBy: LinearDocument.PaginationOrderBy.UpdatedAt,
-    first: count || 10,
-    after: startCursor,
-  })
   const query = await linearClient.users({
     orderBy: LinearDocument.PaginationOrderBy.UpdatedAt,
     first: count || 10,
@@ -26,7 +21,6 @@ export const listUsers: IntegrationProps['actions']['listUsers'] = async ({
     lastSeen: user.lastSeen ? user.lastSeen.toISOString() : undefined,
     updatedAt: user.updatedAt.toISOString(),
   }))
-  console.log(JSON.stringify({ users, nd: query.nodes }, null, 2))
 
   return {
     users,
