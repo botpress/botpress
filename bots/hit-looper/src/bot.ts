e577c86d4846040120c4fa997ca8fb1aa97f5e00
Francois Levasseur <francois.levasseur@botpress.com>
2023-10-19 18:16:47 -0400

    minor fix

--- bots/hit-looper/src/bot.ts
+++ bots/hit-looper/src/bot.ts
@@ -3,18 +3,10 @@ import * as botpress from '.botpress'
 
 const telegram = new botpress.telegram.Telegram({
   enabled: true,
-  config: {
-    botToken: '6402478878:AAE-zzePKjgIl23G4VoP_S1StPaf4JoBzHU',
-  },
 })
 
 const zendesk = new botpress.zendesk.Zendesk({
   enabled: true,
-  config: {
-    apiToken: '2ie7LaF0vpHCClxVdlpIsAXvWTk7RQT7dG4OiJlq',
-    email: 'francois_levasseur@hotmail.com',
-    organizationSubdomain: 'botpress6674',
-  },
 })
 
 export const bot = new botpress.Bot({
