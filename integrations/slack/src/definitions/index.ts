92fcf8487828a0d30273d635bacf68b9767800b7
michaelmass <10071388+michaelmass@users.noreply.github.com>
2023-11-01 08:38:27 -0400

    update

--- integrations/slack/src/definitions/index.ts
+++ integrations/slack/src/definitions/index.ts
@@ -10,8 +10,8 @@ export const configuration = {
     linkTemplateScript: 'linkTemplate.vrl',
   },
   schema: z.object({
-    botToken: z.string().optional(),
-    signingSecret: z.string().optional(),
+    botToken: z.string().optional(), // TODO revert once the multiple configuration is available
+    signingSecret: z.string().optional(), // TODO revert once the multiple configuration is available
   }),
 } satisfies IntegrationDefinitionProps['configuration']
 
