8a07e38603c1a2f729ff1e5b893a76fef5ff8de3
Francois Levasseur <francois.levasseur@botpress.com>
2023-11-08 20:25:43 -0500

    chore(gsheets): remove data validation action implementation

--- integrations/gsheets/src/actions/get-values.ts
+++ integrations/gsheets/src/actions/get-values.ts
@@ -1,14 +1,12 @@
-import { getValuesInputSchema } from 'src/misc/custom-schemas'
 import { getClient } from '../client'
 import type { Implementation } from '../misc/types'
 
 export const getValues: Implementation['actions']['getValues'] = async ({ ctx, input, logger }) => {
-  const validatedInput = getValuesInputSchema.parse(input)
   const GoogleSheetsClient = getClient(ctx.configuration)
   let response
 
   try {
-    response = await GoogleSheetsClient.getValues(validatedInput.range)
+    response = await GoogleSheetsClient.getValues(input.range)
     logger.forBot().info(`Successful - Get Values - ${response?.range}`)
   } catch (error) {
     response = {}
