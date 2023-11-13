55baf8363c339da8e40bd4f2b57f0bce46056564
Francois Levasseur <francois.levasseur@botpress.com>
2023-11-13 10:09:43 -0500

    feat(gsheets): check credentials when registering bot

--- integrations/gsheets/src/index.ts
+++ integrations/gsheets/src/index.ts
@@ -1,8 +1,20 @@
 import actions from './actions'
 import * as bp from '.botpress'
+import { getClient } from './client'
 
 export default new bp.Integration({
-  register: async () => {},
+  register: async (props) => {
+    props.logger.forBot().info('Registering Gsheets integration')
+    try {
+      const gsheetsClient = getClient(props.ctx.configuration)
+      const { properties, sheets } = await gsheetsClient.getSpreadsheet('')
+      const summary = JSON.stringify({ properties, sheets }).substring(0, 200)
+      props.logger.forBot().info(`Successfully connected to Gsheets: ${summary}`)
+    } catch (thrown) {
+      props.logger.forBot().error(`Failed to connect to Gsheets: ${thrown}`)
+      throw thrown
+    }
+  },
   unregister: async () => {},
   actions,
   channels: {},
