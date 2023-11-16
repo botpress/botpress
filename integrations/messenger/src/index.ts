c7b29a01705a57a1c2906b3d4095fedb3122677d
Charlie Henin <charlie.henin@gmail.com>
2023-11-16 15:40:54 -0500

    feat(messenger): add logs to bot (#12886)

--- integrations/messenger/src/index.ts
+++ integrations/messenger/src/index.ts
@@ -19,42 +19,75 @@ const integration = new bp.Integration({
     channel: {
       messages: {
         text: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendText(recipientId, payload.text)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending text message from bot to Messenger:', payload.text)
+            return messenger.sendText(recipientId, payload.text)
+          }),
         image: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendImage(recipientId, payload.imageUrl)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending image message from bot to Messenger:', payload.imageUrl)
+            return messenger.sendImage(recipientId, payload.imageUrl)
+          }),
         markdown: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendText(recipientId, payload.markdown)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending markdown message from bot to Messenger:', payload.markdown)
+            return messenger.sendText(recipientId, payload.markdown)
+          }),
         audio: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendAudio(recipientId, payload.audioUrl)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending audio message from bot to Messenger:', payload.audioUrl)
+            return messenger.sendAudio(recipientId, payload.audioUrl)
+          }),
         video: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendVideo(recipientId, payload.videoUrl)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending video message from bot to Messenger:', payload.videoUrl)
+            return messenger.sendVideo(recipientId, payload.videoUrl)
+          }),
         file: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) => messenger.sendFile(recipientId, payload.fileUrl)),
+          sendMessage(props, async (messenger, recipientId) => {
+            props.logger.forBot().debug('Sending file message from bot to Messenger:', payload.fileUrl)
+            return messenger.sendFile(recipientId, payload.fileUrl)
+          }),
         location: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) =>
-            messenger.sendText(recipientId, formatGoogleMapLink(payload))
-          ),
+          sendMessage(props, async (messenger, recipientId) => {
+            const googleMapLink = formatGoogleMapLink(payload)
+
+            props.logger.forBot().debug('Sending location message from bot to Messenger:', googleMapLink)
+            return messenger.sendText(recipientId, googleMapLink)
+          }),
         carousel: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) =>
-            messenger.sendMessage(recipientId, getCarouselMessage(payload))
-          ),
+          sendMessage(props, async (messenger, recipientId) => {
+            const carouselMessage = getCarouselMessage(payload)
+
+            props.logger.forBot().debug('Sending carousel message from bot to Messenger:', carouselMessage)
+            return messenger.sendMessage(recipientId, getCarouselMessage(payload))
+          }),
         card: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) =>
-            messenger.sendMessage(recipientId, getCarouselMessage({ items: [payload] }))
-          ),
+          sendMessage(props, async (messenger, recipientId) => {
+            const cardMessage = getCarouselMessage({ items: [payload] })
+
+            props.logger.forBot().debug('Sending card message from bot to Messenger:', cardMessage)
+            return messenger.sendMessage(recipientId, cardMessage)
+          }),
         dropdown: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) =>
-            messenger.sendMessage(recipientId, getChoiceMessage(payload))
-          ),
+          sendMessage(props, async (messenger, recipientId) => {
+            const choiceMessage = getChoiceMessage(payload)
+
+            props.logger.forBot().debug('Sending dropdown message from bot to Messenger:', choiceMessage)
+            return messenger.sendMessage(recipientId, choiceMessage)
+          }),
         choice: async ({ payload, ...props }) =>
-          sendMessage(props, async (messenger, recipientId) =>
-            messenger.sendMessage(recipientId, getChoiceMessage(payload))
-          ),
+          sendMessage(props, async (messenger, recipientId) => {
+            const choiceMessage = getChoiceMessage(payload)
+
+            props.logger.forBot().debug('Sending choice message from bot to Messenger:', choiceMessage)
+            return messenger.sendMessage(recipientId, getChoiceMessage(payload))
+          }),
       },
     },
   },
   handler: async ({ req, client, ctx, logger }) => {
-    console.info('Handler received request')
+    logger.forBot().debug('Handler received request from Messenger with payload:', req.body)
 
     if (req.query) {
       const query = queryString.parse(req.query)
@@ -63,34 +96,50 @@ const integration = new bp.Integration({
       const token = query['hub.verify_token']
       const challenge = query['hub.challenge']
 
-      if (mode === 'subscribe' && token === ctx.configuration.verifyToken) {
-        if (!challenge) {
+      if (mode === 'subscribe') {
+        if (token === ctx.configuration.verifyToken) {
+          if (!challenge) {
+            logger.forBot().warn('Returning HTTP 400 as no challenge parameter was received in query string of request')
+            return {
+              status: 400,
+            }
+          }
+
           return {
-            status: 400,
+            body: typeof challenge === 'string' ? challenge : '',
+          }
+        } else {
+          logger
+            .forBot()
+            .warn("Returning HTTP 403 as the Messenger token doesn't match the one in the bot configuration")
+          return {
+            status: 403,
           }
         }
-
-        return {
-          body: typeof challenge === 'string' ? challenge : '',
-        }
       } else {
+        logger.forBot().warn(`Returning HTTP 400 as the '${mode}' mode received in the query string isn't supported`)
         return {
-          status: 403,
+          status: 400,
         }
       }
     }
 
     if (!req.body) {
-      console.warn('Handler received an empty body')
+      logger.forBot().warn('Handler received an empty body, so the message was ignored')
       return
     }
 
-    const data = JSON.parse(req.body) as MessengerPayload
+    try {
+      const data = JSON.parse(req.body) as MessengerPayload
 
-    for (const { messaging } of data.entry) {
-      for (const message of messaging) {
-        await handleMessage(message, { client, ctx, logger })
+      for (const { messaging } of data.entry) {
+        for (const message of messaging) {
+          await handleMessage(message, { client, ctx, logger })
+        }
       }
+    } catch (e: any) {
+      logger.forBot().error('Error while handling request:', e)
+      logger.forBot().debug('Request body received:', req.body)
     }
 
     return
@@ -278,6 +327,7 @@ async function handleMessage(
   }: { client: bp.Client; ctx: IntegrationContext<bp.configuration.Configuration>; logger: IntegrationLogger }
 ) {
   if (message?.message?.text) {
+    logger.forBot().debug('Received text message from Messenger:', message.message.text)
     const { conversation } = await client.getOrCreateConversation({
       channel: 'channel',
       tags: {
