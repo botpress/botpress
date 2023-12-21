8d145b55baca7f809ae3ed71873654674b427c33
Francois Levasseur <francois.levasseur@botpress.com>
2023-12-21 17:18:41 -0500

    feat(sdk): some more typings for integration specific client

--- packages/sdk/src/type-utils.ts
+++ packages/sdk/src/type-utils.ts
@@ -12,3 +12,63 @@ export type Join<S extends (string | number | symbol)[]> = S extends [infer H, .
   : ''
 
 export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
+
+export type ValueOf<T> = T[keyof T]
+
+export type Inspect<T> = { [K in keyof T]: T[K] }
+
+export type KeyBy<T, Key extends keyof T> = {
+  [TInstance in Cast<T[Key], string>]: Extract<T, { [K in Key]: TInstance }>
+}
+
+// tests
+
+type IsEqual<T, U> = T extends U ? (U extends T ? true : false) : false
+type Expect<T extends true> = T
+
+type _test_keyby = Expect<
+  IsEqual<
+    KeyBy<
+      | {
+          name: 'circle'
+        }
+      | {
+          name: 'square'
+        }
+      | {
+          name: 'rectangle'
+        },
+      'name'
+    >,
+    {
+      circle: {
+        name: 'circle'
+      }
+      square: {
+        name: 'square'
+      }
+      rectangle: {
+        name: 'rectangle'
+      }
+    }
+  >
+>
+
+type _test_join = Expect<IsEqual<Join<['a', 'b', 'c']>, 'abc'>>
+
+type _test_union_to_intersection = Expect<
+  IsEqual<
+    UnionToIntersection<
+      | {
+          name: string
+        }
+      | {
+          age: number
+        }
+    >,
+    {
+      name: string
+      age: number
+    }
+  >
+>
