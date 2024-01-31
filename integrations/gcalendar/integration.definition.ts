76b2d892594d91adc84d360fca88a4ef0d9a3ae7
Charles Catta <charles.catta@botpress.com>
2024-01-31 13:32:09 -0500

    move definitions to integration.definition file

--- integrations/gcalendar/integration.definition.ts
+++ integrations/gcalendar/integration.definition.ts
@@ -1,7 +1,17 @@
 import { IntegrationDefinition } from '@botpress/sdk'
-
+import { z } from 'zod'
 import { INTEGRATION_NAME } from './src/const'
-import { configuration, actions } from './src/definitions'
+import {
+  listEventsInputSchema,
+  listEventsOutputSchema,
+  createEventInputSchema,
+  createEventOutputSchema,
+  updateEventInputSchema,
+  updateEventOutputSchema,
+  deleteEventInputSchema,
+  deleteEventOutputSchema,
+} from './src/misc/custom-schemas'
+import { updateEventUi, deleteEventUi, createEventUi } from './src/misc/custom-uis'
 
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
@@ -10,6 +20,60 @@ export default new IntegrationDefinition({
   title: 'Google Calendar',
   readme: 'hub.md',
   icon: 'icon.svg',
-  configuration,
-  actions,
+  configuration: {
+    schema: z.object({
+      calendarId: z
+        .string()
+        .describe('The ID of the Google Calendar to interact with. You can find it in your Google Calendar settings.'),
+      privateKey: z
+        .string()
+        .describe('The private key from the Google service account. You can get it from the downloaded JSON file.'),
+      clientEmail: z
+        .string()
+        .email()
+        .describe('The client email from the Google service account. You can get it from the downloaded JSON file.'),
+    }),
+  },
+  actions: {
+    listEvents: {
+      title: 'List Events',
+      input: {
+        schema: listEventsInputSchema,
+        ui: {},
+      },
+      output: {
+        schema: listEventsOutputSchema,
+      },
+    },
+    createEvent: {
+      title: 'Create Event',
+      input: {
+        schema: createEventInputSchema,
+        ui: createEventUi,
+      },
+      output: {
+        schema: createEventOutputSchema,
+      },
+    },
+    updateEvent: {
+      title: 'Update Event',
+      input: {
+        schema: updateEventInputSchema,
+        ui: updateEventUi,
+      },
+      output: {
+        schema: updateEventOutputSchema,
+      },
+    },
+    deleteEvent: {
+      title: 'Delete Event',
+      input: {
+        schema: deleteEventInputSchema,
+        ui: deleteEventUi,
+      },
+      output: {
+        schema: deleteEventOutputSchema,
+      },
+    },
+  },
 })
