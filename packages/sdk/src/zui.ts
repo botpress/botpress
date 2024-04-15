cd9925fc4e1fc59edbaf870f99d7c4d1303b5484
Charles Catta <charles.catta@botpress.com>
2024-04-15 12:09:25 -0400

    bump zui

--- packages/sdk/src/zui.ts
+++ packages/sdk/src/zui.ts
@@ -6,140 +6,135 @@ const commonInputParams = z.object({
   horizontal: z.boolean().optional(),
 })
 
-export const studioComponentDefinitions = [
-  {
-    type: 'string',
-    id: 'textInput',
-    schema: commonInputParams.extend({
-      multiLine: z.boolean().optional(),
-      growVertically: z.boolean().optional(),
-      suggestions: z.array(z.string()).optional(),
-    }),
+export const studioComponentDefinitions = {
+  string: {
+    textInput: {
+      id: 'textInput',
+      params: commonInputParams.extend({
+        multiLine: z.boolean().optional(),
+        growVertically: z.boolean().optional(),
+        suggestions: z.array(z.string()).optional(),
+      }),
+    },
+    dropdown: {
+      id: 'dropdown',
+      params: commonInputParams.extend({
+        filterable: z.boolean().optional(),
+      }),
+    },
+    radiogroup: {
+      id: 'radiogroup',
+      params: commonInputParams.extend({}),
+    },
+    datepicker: {
+      id: 'datepicker',
+      params: commonInputParams.extend({
+        dateFormat: z.string().optional(),
+        minDate: z.string().optional(),
+        maxDate: z.string().optional(),
+        defaultTimezone: z.string().optional(),
+        disableTimezoneSelection: z.boolean().optional(),
+        highlightCurrentDay: z.boolean().optional(),
+        showShortcutButtons: z.boolean().optional(),
+        showOutsideDaysOfMonth: z.boolean().optional(),
+        firstDayOfWeek: z.number().optional(),
+        canChangeMonth: z.boolean().optional(),
+        showWeekNumbers: z.boolean().optional(),
+      }),
+    },
+    timepicker: {
+      id: 'timepicker',
+      params: commonInputParams.extend({
+        useAMPM: z.boolean().optional(),
+        timeFormat: z.string().optional(),
+        minTime: z.string().optional(),
+        maxTime: z.string().optional(),
+        showArrowButtons: z.boolean().optional(),
+        precision: z.enum(['minute', 'second', 'millisecond']).optional(),
+      }),
+    },
+    variablepicker: {
+      id: 'variablepicker',
+      params: z.object({
+        type: z.enum([
+          'any',
+          'string',
+          'number',
+          'boolean',
+          'object',
+          'pattern',
+          'date',
+          'array',
+          'target',
+          'time',
+          'enum',
+        ]),
+        horizontal: z.boolean().optional(),
+      }),
+    },
+    richTextEditor: {
+      id: 'richTextEditor',
+      params: z.object({
+        allowDynamicVariable: z.boolean().optional(),
+        resizable: z.boolean().optional(),
+      }),
+    },
+    JSONInput: {
+      id: 'JSONInput',
+      params: commonInputParams.extend({
+        showPreview: z.boolean().optional(),
+        showValidationError: z.boolean().optional(),
+      }),
+    },
+    fileInput: {
+      id: 'fileInput',
+      params: commonInputParams.extend({
+        fileTypes: z.array(z.enum(['image', 'audio', 'video'])).optional(),
+        showUploadedFiles: z.boolean().optional(),
+      }),
+    },
   },
-  {
-    type: 'string',
-    id: 'dropdown',
-    schema: commonInputParams.extend({
-      filterable: z.boolean().optional(),
-    }),
+  number: {
+    numberInput: {
+      id: 'numberInput',
+      params: commonInputParams.extend({
+        allowNumericCharactersOnly: z.boolean().optional(),
+        stepSize: z.number().optional(),
+      }),
+    },
+    slider: {
+      id: 'slider',
+      params: z.object({
+        horizontal: z.boolean().optional(),
+        stepSize: z.number().optional(),
+      }),
+    },
   },
-  {
-    type: 'string',
-    id: 'radiogroup',
-    schema: commonInputParams.extend({}),
+  boolean: {
+    switch: {
+      id: 'switch',
+      params: commonInputParams,
+    },
   },
-  {
-    type: 'string',
-    id: 'datepicker',
-    schema: commonInputParams.extend({
-      dateFormat: z.string().optional(),
-      minDate: z.string().optional(),
-      maxDate: z.string().optional(),
-      defaultTimezone: z.string().optional(),
-      disableTimezoneSelection: z.boolean().optional(),
-      highlightCurrentDay: z.boolean().optional(),
-      showShortcutButtons: z.boolean().optional(),
-      showOutsideDaysOfMonth: z.boolean().optional(),
-      firstDayOfWeek: z.number().optional(),
-      canChangeMonth: z.boolean().optional(),
-      showWeekNumbers: z.boolean().optional(),
-    }),
+  array: {
+    optionList: {
+      id: 'optionList',
+      params: commonInputParams,
+    },
+    stringList: {
+      id: 'stringList',
+      params: commonInputParams,
+    },
   },
-  {
-    type: 'string',
-    id: 'timepicker',
-    schema: commonInputParams.extend({
-      useAMPM: z.boolean().optional(),
-      timeFormat: z.string().optional(),
-      minTime: z.string().optional(),
-      maxTime: z.string().optional(),
-      showArrowButtons: z.boolean().optional(),
-      precision: z.enum(['minute', 'second', 'millisecond']).optional(),
-    }),
+  object: {
+    collapsible: {
+      id: 'collapsible',
+      params: z.object({
+        defaultOpen: z.boolean().optional(),
+      }),
+    },
   },
-  {
-    type: 'string',
-    id: 'variablepicker',
-    schema: z.object({
-      type: z.enum([
-        'any',
-        'string',
-        'number',
-        'boolean',
-        'object',
-        'pattern',
-        'date',
-        'array',
-        'target',
-        'time',
-        'enum',
-      ]),
-      horizontal: z.boolean().optional(),
-    }),
-  },
-  {
-    type: 'string',
-    id: 'richTextEditor',
-    schema: z.object({
-      allowDynamicVariable: z.boolean().optional(),
-      resizable: z.boolean().optional(),
-    }),
-  },
-  {
-    type: 'string',
-    id: 'JSONInput',
-    schema: commonInputParams.extend({
-      showPreview: z.boolean().optional(),
-      showValidationError: z.boolean().optional(),
-    }),
-  },
-  {
-    type: 'string',
-    id: 'fileInput',
-    schema: commonInputParams.extend({
-      fileTypes: z.array(z.enum(['image', 'audio', 'video'])).optional(),
-      showUploadedFiles: z.boolean().optional(),
-    }),
-  },
-  {
-    type: 'number',
-    id: 'numberInput',
-    schema: commonInputParams.extend({
-      allowNumericCharactersOnly: z.boolean().optional(),
-      stepSize: z.number().optional(),
-    }),
-  },
-  {
-    type: 'number',
-    id: 'slider',
-    schema: z.object({
-      horizontal: z.boolean().optional(),
-      stepSize: z.number().optional(),
-    }),
-  },
-  {
-    type: 'boolean',
-    id: 'switch',
-    schema: commonInputParams,
-  },
-  {
-    type: 'array',
-    id: 'optionList',
-    schema: commonInputParams,
-  },
-  {
-    type: 'array',
-    id: 'stringList',
-    schema: commonInputParams,
-  },
-  {
-    type: 'object',
-    id: 'collapsible',
-    schema: z.object({
-      defaultOpen: z.boolean().optional(),
-    }),
-  },
-] satisfies UIComponentDefinitions
+} as const satisfies UIComponentDefinitions
 
 declare module '@bpinternal/zui' {
   type ComponentDefinitions = typeof studioComponentDefinitions
