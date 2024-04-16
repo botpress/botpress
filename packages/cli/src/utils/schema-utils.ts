f646cf0f8b21bb66f37b0e80c3f63dc6d60726f7
Francois Levasseur <francois.levasseur@botpress.com>
2024-04-16 15:38:26 -0400

    update

--- packages/cli/src/utils/schema-utils.ts
+++ packages/cli/src/utils/schema-utils.ts
@@ -1,5 +1,9 @@
 import { z, transforms } from '@botpress/sdk'
 
+type ZuiToJsonSchema = typeof transforms.zuiToJsonSchema
+type JsonSchema = ReturnType<ZuiToJsonSchema>
+type ObjectJsonSchema = Extract<JsonSchema, { type: 'object' }>
+
 type SchemaOptions = {
   title?: string
   examples?: any[]
@@ -10,6 +14,29 @@ type SchemaDefinition = {
   ui?: Record<string, SchemaOptions | undefined>
 }
 
+const isObjectSchema = (schema: JsonSchema): schema is ObjectJsonSchema => schema.type === 'object'
+
 export function mapZodToJsonSchema(definition: SchemaDefinition): ReturnType<typeof transforms.zuiToJsonSchema> {
-  return transforms.zuiToJsonSchema(definition.schema, { target: 'jsonSchema7' })
+  const schema = transforms.zuiToJsonSchema(definition.schema, { target: 'jsonSchema7' })
+  if (!isObjectSchema(schema) || !definition.ui) {
+    return schema
+  }
+
+  for (const [key, value] of Object.entries(definition.ui ?? {})) {
+    const property = schema.properties?.[key]
+
+    if (!property) {
+      continue
+    }
+
+    if (!!value?.title) {
+      ;(property as any).title = value.title
+    }
+
+    if (!!value?.examples) {
+      ;(property as any).examples = value.examples
+    }
+  }
+
+  return schema
 }
