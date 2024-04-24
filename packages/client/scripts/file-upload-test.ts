903305083f576ba5177df47488a543c1062683c7
Abraham Lopez <abraham.lopez@botpress.com>
2024-04-24 17:15:02 -0400

    update

--- packages/client/scripts/file-upload-test.ts
+++ packages/client/scripts/file-upload-test.ts
@@ -2,7 +2,7 @@
  * This script is meant to be used as a manual test for the custom file creation/upload method added in the client.
  *
  * To test, follow these steps:
- *   1. Run Tilt on the Skynet repository using the `files-api` mode. Make sure `USAGE_BASE_URL` is commented out in the `files-api` Tilt resource otherwise you'll need to create a workspace and a bot and change the bot ID below.
+ *   1. Run Tilt on the Skynet repository using the `full` mode. If you want to use the leaner `files-api` mode then you'll need to comment out the `USAGE_BASE_URL` environment variable passed to the `files-api` Tilt resource.
  *   2. Run `pnpm build` in the `packages/client` directory.
  *   3. Then run `npx ts-node scripts/file-upload-test.ts` to run this script.
  */
