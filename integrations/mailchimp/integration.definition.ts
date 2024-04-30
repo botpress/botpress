56386c1bc18c538eac9684d6fae7dd597018b684
Charles Catta <charles.catta@botpress.com>
2024-04-30 10:12:10 -0400

    bump

--- integrations/mailchimp/integration.definition.ts
+++ integrations/mailchimp/integration.definition.ts
@@ -17,7 +17,7 @@ const INTEGRATION_NAME = 'mailchimp'
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
   title: 'Mailchimp',
-  version: '0.3.1',
+  version: '0.3.2',
   readme: 'hub.md',
   icon: 'icon.svg',
   description:
