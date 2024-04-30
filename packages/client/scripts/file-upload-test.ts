fe8cc9be0a2ccc9bc9be7888c429f767d1b712e4
Abraham Lopez <abraham.lopez@botpress.com>
2024-04-30 18:33:06 -0400

    use new getFile endpoint

--- packages/client/scripts/file-upload-test.ts
+++ packages/client/scripts/file-upload-test.ts
@@ -1,10 +1,12 @@
+import { GetFileResponse } from '../dist/gen'
+
 /**
  * This script is meant to be used as a manual test for the custom file creation/upload method added in the client.
  *
  * To test, follow these steps:
- *   1. Run Tilt on the Skynet repository using the `full` mode. If you want to use the leaner `files-api` mode then you'll need to comment out the `USAGE_BASE_URL` environment variable passed to the `files-api` Tilt resource.
+ *   1. Run Tilt on the Skynet repository using the `full` mode. If you want to use the leaner `files-api` mode then you'll need to comment out the `USAGE_BASE_URL` environment variable passed to the `files-api` Tilt resource so that we can use the predefined bot ID below.
  *   2. Run `pnpm build` in the `packages/client` directory.
- *   3. Then run `npx ts-node scripts/file-upload-test.ts` to run this script.
+ *   3. Then run `pnpm test:manual` to run this script.
  */
 const { Client } = require('../dist/index.cjs')
 
@@ -15,10 +17,16 @@ const client = new Client({
 })
 
 void (async function () {
-  const { file } = await client.createAndUploadFile({
-    name: 'test.txt',
-    data: Buffer.from('aaa'),
-  })
-
-  console.info('FILE UPLOADED SUCCESSFULLY:', file)
+  await client
+    .createAndUploadFile({
+      name: 'test.txt',
+      data: Buffer.from('aaa'),
+    })
+    .then(({ file }: GetFileResponse) => {
+      console.debug('file:', file, '\n')
+      console.info('\x1b[32m%s\x1b[0m', 'File was created and uploaded successfully.')
+    })
+    .catch((err: any) => {
+      console.error('\x1b[31m%s\x1b[0m', `Failed to create and upload file: ${err.stack ?? err.message}`)
+    })
 })()
