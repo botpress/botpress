f50d45a188506909cea0a10984b7afebdaa290d2
Abraham Lopez <abraham.lopez@botpress.com>
2024-05-01 12:19:11 -0400

    update test comment

--- packages/client/tests/manual/file-upload.test.ts
+++ packages/client/tests/manual/file-upload.test.ts
@@ -5,7 +5,7 @@ import { GetFileResponse } from '../../dist/gen'
  * This a test to be run manually to test the custom file creation/upload method added in the client.
  *
  * Follow these steps to run it:
- *   1. Run Tilt on the Skynet repository using the `files-api` mode and make sure a `DEV_BYPASS_USAGE_CHECKS: 'true'` env var is passed to the `files-api` Tilt resource so that we can use the predefined bot ID below.
+ *   1. Run Tilt on the Skynet repository using the `files-api` mode and make sure a "DEV_BYPASS_USAGE_CHECKS" env var with a string value of "true" is passed to the `files-api` Tilt resource so that we can use the predefined bot ID below.
  *   2. Run `pnpm build` in the `packages/client` directory.
  *   3. Then run `pnpm test:manual` to run this script.
  */
