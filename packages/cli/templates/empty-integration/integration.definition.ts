444f7ce821007a156a9ed0fd9fcffaaa9259e19d
Francois Levasseur <francois.levasseur@botpress.com>
2024-05-15 16:02:07 -0400

    add readme and icons for each integration template

--- packages/cli/templates/empty-integration/integration.definition.ts
+++ packages/cli/templates/empty-integration/integration.definition.ts
@@ -4,4 +4,6 @@ import { integrationName } from './package.json'
 export default new IntegrationDefinition({
   name: integrationName,
   version: '0.0.1',
+  readme: 'hub.md',
+  icon: 'icon.svg',
 })
--- packages/cli/templates/webhook-message/integration.definition.ts
+++ packages/cli/templates/webhook-message/integration.definition.ts
@@ -4,6 +4,8 @@ import { integrationName } from './package.json'
 export default new IntegrationDefinition({
   name: integrationName,
   version: '0.0.1',
+  readme: 'hub.md',
+  icon: 'icon.svg',
   configuration: {
     schema: z.object({
       webhookUrl: z.string().describe('The url to post the bot answers to.'),
