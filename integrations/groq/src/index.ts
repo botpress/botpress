001da8128e95fb77e135648946f6be6048b831c2
Abraham Lopez <abraham.lopez@botpress.com>
2024-06-13 17:11:42 -0400

    refactor

--- integrations/groq/src/index.ts
+++ integrations/groq/src/index.ts
@@ -2,16 +2,16 @@ import OpenAI from 'openai'
 import { GenerateContentInput, openai } from '@botpress/common'
 import * as bp from '.botpress'
 
+const groqClient = new OpenAI({
+  baseURL: 'https://api.groq.com/openai/v1',
+  apiKey: bp.secrets.GROQ_API_KEY,
+})
+
 export default new bp.Integration({
   register: async () => {},
   unregister: async () => {},
   actions: {
     generateContent: async ({ input, logger }) => {
-      const groqClient = new OpenAI({
-        baseURL: 'https://api.groq.com/openai/v1',
-        apiKey: bp.secrets.GROQ_API_KEY,
-      })
-
       return await openai.generateContent(<GenerateContentInput>input, groqClient, logger, { provider: 'groq' })
     },
   },
--- integrations/openai/src/index.ts
+++ integrations/openai/src/index.ts
@@ -2,15 +2,15 @@ import { GenerateContentInput, openai } from '@botpress/common'
 import OpenAI from 'openai'
 import * as bp from '.botpress'
 
+const openAIClient = new OpenAI({
+  apiKey: bp.secrets.OPENAI_API_KEY,
+})
+
 export default new bp.Integration({
   register: async () => {},
   unregister: async () => {},
   actions: {
     generateContent: async ({ input, logger }) => {
-      const openAIClient = new OpenAI({
-        apiKey: bp.secrets.OPENAI_API_KEY,
-      })
-
       return await openai.generateContent(<GenerateContentInput>input, openAIClient, logger, { provider: 'openai' })
     },
   },
