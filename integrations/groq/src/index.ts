32c4548090031e51515e711aa7f1a53ee5234577
Abraham Lopez <abraham.lopez@botpress.com>
2024-06-13 17:10:30 -0400

    update

--- integrations/groq/src/index.ts
+++ integrations/groq/src/index.ts
@@ -7,12 +7,12 @@ export default new bp.Integration({
   unregister: async () => {},
   actions: {
     generateContent: async ({ input, logger }) => {
-      const groq = new OpenAI({
+      const groqClient = new OpenAI({
         baseURL: 'https://api.groq.com/openai/v1',
         apiKey: bp.secrets.GROQ_API_KEY,
       })
 
-      return await openai.generateContent(<GenerateContentInput>input, groq, logger, { provider: 'groq' })
+      return await openai.generateContent(<GenerateContentInput>input, groqClient, logger, { provider: 'groq' })
     },
   },
   channels: {},
