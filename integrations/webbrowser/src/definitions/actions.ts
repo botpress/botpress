55f9b48de99d9b7e6f4fc5d3c67fce8648e74eee
allardy <yann.allard1@gmail.com>
2024-06-14 14:48:51 -0400

    add title / desc

--- integrations/webbrowser/src/definitions/actions.ts
+++ integrations/webbrowser/src/definitions/actions.ts
@@ -1,23 +1,27 @@
 import { z } from '@botpress/sdk'
 
 const captureScreenshot = {
+  title: 'Capture Screenshot',
+  description: 'Capture a screenshot of the specified page.',
   input: {
     schema: z.object({
-      url: z.string()
-    })
+      url: z.string(),
+    }),
   },
   output: {
     schema: z.object({
-      imageUrl: z.string()
-    })
-  }
+      imageUrl: z.string(),
+    }),
+  },
 }
 
 const browsePage = {
+  title: 'Browse Page',
+  description: 'Extract metadata and content of the specified page as markdown.',
   input: {
     schema: z.object({
-      url: z.string()
-    })
+      url: z.string(),
+    }),
   },
   output: {
     schema: z.object({
@@ -27,13 +31,13 @@ const browsePage = {
         description: z.string().nullable().optional(),
         favicon: z.string().nullable().optional(),
         author: z.string().nullable().optional(),
-        datePublished: z.string().nullable().optional()
-      })
-    })
-  }
+        datePublished: z.string().nullable().optional(),
+      }),
+    }),
+  },
 }
 
 export const actionDefinitions = {
   captureScreenshot,
-  browsePage
+  browsePage,
 }
