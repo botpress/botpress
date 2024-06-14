d702dc81c9c05bd3ef0852e84940bbb346d9a53b
Abraham Lopez <abraham.lopez@botpress.com>
2024-06-14 15:35:55 -0400

    PR comments

--- integrations/openai/integration.definition.ts
+++ integrations/openai/integration.definition.ts
@@ -2,7 +2,7 @@ import { schemas } from '@botpress/common'
 import { IntegrationDefinition } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
-  name: 'groq',
+  name: 'openai',
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
