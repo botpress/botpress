73709cdb1cae2877c232fbfa73b187e1c0044a84
Abraham Lopez <10536776+AbrahamLopez10@users.noreply.github.com>
2024-06-18 12:53:13 -0400

    feat(integrations): return usage cost of OpenAI and Groq integrations in action output (#13150)

--- integrations/groq/src/index.ts
+++ integrations/groq/src/index.ts
@@ -14,6 +14,13 @@ export default new bp.Integration({
     generateContent: async ({ input, logger }) => {
       return await llm.openai.generateContent(<llm.schemas.GenerateContentInput>input, groqClient, logger, {
         provider: 'groq',
+        modelCosts: {
+          // Source: https://wow.groq.com/
+          'llama3-8b-8192': { inputCostPer1MTokens: 0.05, outputCostPer1MTokens: 0.08 },
+          'llama3-70b-8192': { inputCostPer1MTokens: 0.59, outputCostPer1MTokens: 0.79 },
+          'mixtral-8x7b-32768': { inputCostPer1MTokens: 0.24, outputCostPer1MTokens: 0.24 },
+          'gemma-7b-it': { inputCostPer1MTokens: 0.07, outputCostPer1MTokens: 0.07 },
+        },
       })
     },
   },
--- integrations/openai/src/index.ts
+++ integrations/openai/src/index.ts
@@ -13,6 +13,13 @@ export default new bp.Integration({
     generateContent: async ({ input, logger }) => {
       return await llm.openai.generateContent(<llm.schemas.GenerateContentInput>input, openAIClient, logger, {
         provider: 'openai',
+        modelCosts: {
+          // Source: https://openai.com/api/pricing/
+          // Only full model names should be supported here, as the short model names can be pointed to a newer model with different pricing by OpenAI at any time.
+          'gpt-4o-2024-05-13': { inputCostPer1MTokens: 5, outputCostPer1MTokens: 15 },
+          'gpt-3.5-turbo-0125': { inputCostPer1MTokens: 0.5, outputCostPer1MTokens: 1.5 },
+          'gpt-4-turbo-2024-04-09': { inputCostPer1MTokens: 10, outputCostPer1MTokens: 30 },
+        },
       })
     },
   },
