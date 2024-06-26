5e5b1df2dfc036b056e9cc9da81a1696ff18c53f
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2024-06-26 15:36:44 -0400

    test

--- integrations/website-indexer/src/scraper.ts
+++ integrations/website-indexer/src/scraper.ts
@@ -1,91 +1,12 @@
-import { ZodError, z } from '@botpress/sdk'
+import { z } from '@botpress/sdk'
 import axios, { AxiosInstance } from 'axios'
 import axiosRetry from 'axios-retry'
-import _ from 'lodash'
 import { v5 } from 'uuid'
+import { getErrorMessage } from './errors'
+import { urlSchema } from './urlSchema'
 import * as bp from '.botpress'
 
 type IntegrationLogger = bp.Logger
-
-const joinPath = (path: (string | number)[]): string => {
-  if (path.length === 1) {
-    return path[0]!.toString()
-  }
-
-  return path.reduce<string>((acc, item) => {
-    if (typeof item === 'number') {
-      return `${acc}[${item}]`
-    }
-
-    return `${acc}${acc.length === 0 ? '' : '.'}${item}`
-  }, '')
-}
-
-const printZodErrors = (errors: z.ZodIssue[]) =>
-  `Validation Error: ${errors
-    .map((x) => `${x.path.length > 0 ? `'${joinPath(x.path)}': ` : ''} ${x.message}`)
-    .join('\n')}`
-
-export const getErrorMessage = (err: unknown): string => {
-  if (axios.isAxiosError(err)) {
-    const data = err.response?.data
-    const statusCode = err.response?.status
-
-    if (!data) {
-      return `${err.message} (Status Code: ${statusCode})`
-    }
-
-    if (_.isArray(data.errors)) {
-      return `${printZodErrors(data.errors)} (Status Code: ${statusCode})`
-    }
-
-    return `${data.message || data.error?.message || data.error || err.message} (Status Code: ${statusCode})`
-  } else if (err instanceof ZodError) {
-    return printZodErrors(err.errors)
-  } else if (err instanceof Error) {
-    return err.message || 'Unexpected error'
-  } else if (err && typeof err === 'object' && 'message' in err && typeof err.message === 'string') {
-    return err.message
-  } else if (typeof err === 'string') {
-    return err
-  }
-
-  return 'Unexpected error'
-}
-
-export const urlSchema = z.string().transform((url, ctx) => {
-  url = url.trim()
-  if (!url.includes('://')) {
-    url = `https://${url}`
-  }
-  try {
-    const x = new URL(url)
-    if (x.protocol !== 'http:' && x.protocol !== 'https:') {
-      ctx.addIssue({
-        // code: z.ZodIssueCode.custom,
-        code: 'custom',
-        message: 'Invalid protocol, only URLs starting with HTTP and HTTPS are supported',
-      })
-      return z.NEVER
-    }
-
-    if (!/.\.[a-zA-Z]{2,}$/.test(x.hostname)) {
-      ctx.addIssue({
-        code: 'custom',
-        message: 'Invalid TLD',
-      })
-      return z.NEVER
-    }
-    const pathName = x.pathname.endsWith('/') ? x.pathname.slice(0, -1) : x.pathname
-    return `${x.origin}${pathName}`.toLowerCase()
-  } catch (e) {
-    ctx.addIssue({
-      code: 'custom',
-      message: 'Invalid URL: ' + getErrorMessage(e),
-    })
-    return z.NEVER
-  }
-})
 
 const fetchPageHtmlSchema = z.object({
   url: urlSchema,
---
+++ integrations/website-indexer/src/test.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+import { fetchUrls } from './urlFetcher'
+
+async function main() {
+  const rootUrls = [
+    'https://www.nytimes.com',
+    'https://www.botpress.com',
+    'https://sallysbakingaddiction.com',
+    'https://www.switchbacktravel.com',
+    'https://www.tankarium.com',
+    'https://www.loveandlemons.com',
+    'https://www.wired.com',
+    'https://pagesix.com',
+    'https://www.gizmodo.com',
+    'https://www.itsalovelylife.com',
+    'https://www.smartpassiveincome.com',
+    'https://www.theblondesalad.com',
+  ]
+
+  const remaining = new Set(rootUrls)
+
+  await Promise.all(
+    rootUrls.map(async (url) => {
+      const urls = await fetchUrls(url)
+      console.log(`Fetched ${urls.length} urls from ${url}`)
+      remaining.delete(url)
+      console.log(`Remaining: [${Array.from(remaining).join(', ')}]`)
+    })
+  )
+}
+
+void main()
