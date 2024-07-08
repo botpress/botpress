f2ebf8ff6e1a06bce478bb88e2aef0d7dcc48c67
Sylvain Perron <1315508+slvnperron@users.noreply.github.com>
2024-07-08 17:26:02 -0400

    chore(integrations): deploy charts and browser integrations (#13175) (#13176)

--- integrations/charts/integration.definition.ts
+++ integrations/charts/integration.definition.ts
@@ -4,7 +4,7 @@ import { actionDefinitions } from 'src/definitions/actions'
 export default new IntegrationDefinition({
   name: 'charts',
   description: 'Easily generate a variety of charts, including line, bar, pie, and scatter plots, etc.',
-  version: '0.0.1',
+  version: '0.2.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   actions: actionDefinitions,
