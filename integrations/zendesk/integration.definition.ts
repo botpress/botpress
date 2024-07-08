7cbcaebb99e5255c0fe9a2ab95c622eb8a983ad3
Francois Levasseur <francois.levasseur@botpress.com>
2024-07-08 15:33:32 -0400

    feat(zendesk): implement the HITL interface

--- integrations/zendesk/integration.definition.ts
+++ integrations/zendesk/integration.definition.ts
@@ -1,11 +1,11 @@
-import { IntegrationDefinition } from '@botpress/sdk'
+import { IntegrationDefinition, interfaces } from '@botpress/sdk'
 import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 import { actions, events, configuration, channels, states, user } from './src/definitions'
 
 export default new IntegrationDefinition({
   name: 'zendesk',
   title: 'Zendesk',
-  version: '0.4.2',
+  version: '0.5.0',
   icon: 'icon.svg',
   description:
     'Optimize your support workflow using Zendesk. Seamlessly integrate your chatbot into Zendesk chat, enabling you to trigger workflows from ticket updates. Easily manage tickets, access conversations, and engage with customers',
@@ -17,4 +17,4 @@ export default new IntegrationDefinition({
   actions,
   events,
   secrets: sentryHelpers.COMMON_SECRET_NAMES,
-})
+}).extend(interfaces.hitl, () => ({}))
--- integrations/zendesk/src/actions/index.ts
+++ integrations/zendesk/src/actions/index.ts
@@ -1,9 +1,11 @@
 import { closeTicket } from './close-ticket'
 import { createTicket } from './create-ticket'
+import { createUser } from './create-user'
 import { findCustomer } from './find-customer'
 import { getTicket } from './get-ticket'
 import { getTicketConversation } from './get-ticket-conversation'
 import { listAgents } from './list-agents'
+import { openTicket } from './open-ticket'
 import { setConversationRequester } from './set-conversation-requester'
 import * as bp from '.botpress'
 
@@ -11,8 +13,10 @@ export default {
   getTicket,
   findCustomer,
   createTicket,
+  createUser,
   closeTicket,
   listAgents,
   getTicketConversation,
   setConversationRequester,
+  openTicket,
 } satisfies bp.IntegrationProps['actions']
