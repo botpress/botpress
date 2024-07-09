49d84876444dd003bc0aad3019d8a4763f7e7274
Francois Levasseur <francois.levasseur@botpress.com>
2024-07-09 14:43:03 -0400

    chore: refactor hit-looper bot for new hitl flow

--- bots/hit-looper/src/bot.ts
+++ bots/hit-looper/src/bot.ts
@@ -1,12 +1,25 @@
 import { z } from '@botpress/sdk'
 import * as botpress from '.botpress'
 
+const BOT_TOKEN = '6800892956:AAGjc0-oZZq2BTy9FpRq1E-9JMfRiwNcu4E'
+const API_TOKEN = 'w7SufH95dbomRFe1e3tNoS0mOFcumNfFwwaIrjBJ'
+const EMAIL = 'francois.levasseur@botpress.com'
+const ORGANIZATION_SUBDOMAIN = 'botpress3163'
+
 const telegram = new botpress.telegram.Telegram({
   enabled: true,
+  config: {
+    botToken: BOT_TOKEN,
+  },
 })
 
 const zendesk = new botpress.zendesk.Zendesk({
   enabled: true,
+  config: {
+    apiToken: API_TOKEN,
+    email: EMAIL,
+    organizationSubdomain: ORGANIZATION_SUBDOMAIN,
+  },
 })
 
 export const bot = new botpress.Bot({
@@ -27,6 +40,18 @@ export const bot = new botpress.Bot({
   },
   events: {},
   recurringEvents: {},
+  user: {
+    tags: {
+      downstream: {
+        title: 'Downstream User ID',
+        description: 'ID of the downstream user binded to the upstream one',
+      },
+      upstream: {
+        title: 'Upstream User ID',
+        description: 'ID of the upstream user binded to the downstream one',
+      },
+    },
+  },
   conversation: {
     tags: {
       downstream: {
