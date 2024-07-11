4a7371e4a4875625280ebe54ce0ee49edee03f8d
Francois Levasseur <francois.levasseur@botpress.com>
2024-07-11 12:00:05 -0400

    feat(sdk): expose inner client in sdk clients

--- packages/sdk/src/bot/client/index.ts
+++ packages/sdk/src/bot/client/index.ts
@@ -6,71 +6,71 @@ import * as routes from './routes'
  * Just like the regular botpress client, but typed with the bot's properties.
  */
 export class BotSpecificClient<TBot extends BaseBot> {
-  public constructor(private readonly client: Client) {}
+  public constructor(public readonly inner: Client) {}
 
-  public getConversation: routes.GetConversation<TBot> = (x) => this.client.getConversation(x)
-  public listConversations: routes.ListConversations<TBot> = (x) => this.client.listConversations(x)
-  public updateConversation: routes.UpdateConversation<TBot> = (x) => this.client.updateConversation(x)
-  public deleteConversation: routes.DeleteConversation<TBot> = (x) => this.client.deleteConversation(x)
+  public getConversation: routes.GetConversation<TBot> = (x) => this.inner.getConversation(x)
+  public listConversations: routes.ListConversations<TBot> = (x) => this.inner.listConversations(x)
+  public updateConversation: routes.UpdateConversation<TBot> = (x) => this.inner.updateConversation(x)
+  public deleteConversation: routes.DeleteConversation<TBot> = (x) => this.inner.deleteConversation(x)
 
-  public listParticipants: routes.ListParticipants<TBot> = (x) => this.client.listParticipants(x)
-  public addParticipant: routes.AddParticipant<TBot> = (x) => this.client.addParticipant(x)
-  public getParticipant: routes.GetParticipant<TBot> = (x) => this.client.getParticipant(x)
-  public removeParticipant: routes.RemoveParticipant<TBot> = (x) => this.client.removeParticipant(x)
+  public listParticipants: routes.ListParticipants<TBot> = (x) => this.inner.listParticipants(x)
+  public addParticipant: routes.AddParticipant<TBot> = (x) => this.inner.addParticipant(x)
+  public getParticipant: routes.GetParticipant<TBot> = (x) => this.inner.getParticipant(x)
+  public removeParticipant: routes.RemoveParticipant<TBot> = (x) => this.inner.removeParticipant(x)
 
-  public getEvent: routes.GetEvent<TBot> = ((x) => this.client.getEvent(x)) as routes.GetEvent<TBot>
-  public listEvents: routes.ListEvents<TBot> = ((x) => this.client.listEvents(x)) as routes.ListEvents<TBot>
+  public getEvent: routes.GetEvent<TBot> = ((x) => this.inner.getEvent(x)) as routes.GetEvent<TBot>
+  public listEvents: routes.ListEvents<TBot> = ((x) => this.inner.listEvents(x)) as routes.ListEvents<TBot>
 
-  public createMessage: routes.CreateMessage<TBot> = ((x) => this.client.createMessage(x)) as routes.CreateMessage<TBot>
+  public createMessage: routes.CreateMessage<TBot> = ((x) => this.inner.createMessage(x)) as routes.CreateMessage<TBot>
   public getOrCreateMessage: routes.GetOrCreateMessage<TBot> = ((x) =>
-    this.client.getOrCreateMessage(x)) as routes.GetOrCreateMessage<TBot>
-  public getMessage: routes.GetMessage<TBot> = ((x) => this.client.getMessage(x)) as routes.GetMessage<TBot>
-  public updateMessage: routes.UpdateMessage<TBot> = ((x) => this.client.updateMessage(x)) as routes.UpdateMessage<TBot>
-  public listMessages: routes.ListMessages<TBot> = ((x) => this.client.listMessages(x)) as routes.ListMessages<TBot>
-  public deleteMessage: routes.DeleteMessage<TBot> = ((x) => this.client.deleteMessage(x)) as routes.DeleteMessage<TBot>
+    this.inner.getOrCreateMessage(x)) as routes.GetOrCreateMessage<TBot>
+  public getMessage: routes.GetMessage<TBot> = ((x) => this.inner.getMessage(x)) as routes.GetMessage<TBot>
+  public updateMessage: routes.UpdateMessage<TBot> = ((x) => this.inner.updateMessage(x)) as routes.UpdateMessage<TBot>
+  public listMessages: routes.ListMessages<TBot> = ((x) => this.inner.listMessages(x)) as routes.ListMessages<TBot>
+  public deleteMessage: routes.DeleteMessage<TBot> = ((x) => this.inner.deleteMessage(x)) as routes.DeleteMessage<TBot>
 
-  public getUser: routes.GetUser<TBot> = (x) => this.client.getUser(x)
-  public listUsers: routes.ListUsers<TBot> = (x) => this.client.listUsers(x)
-  public updateUser: routes.UpdateUser<TBot> = (x) => this.client.updateUser(x)
-  public deleteUser: routes.DeleteUser<TBot> = (x) => this.client.deleteUser(x)
+  public getUser: routes.GetUser<TBot> = (x) => this.inner.getUser(x)
+  public listUsers: routes.ListUsers<TBot> = (x) => this.inner.listUsers(x)
+  public updateUser: routes.UpdateUser<TBot> = (x) => this.inner.updateUser(x)
+  public deleteUser: routes.DeleteUser<TBot> = (x) => this.inner.deleteUser(x)
 
   public getState: routes.GetState<TBot> = ((x) =>
-    this.client.getState(x).then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.GetState<TBot>
+    this.inner.getState(x).then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.GetState<TBot>
   public setState: routes.SetState<TBot> = ((x) =>
-    this.client.setState(x).then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.SetState<TBot>
+    this.inner.setState(x).then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.SetState<TBot>
   public getOrSetState: routes.GetOrSetState<TBot> = ((x) =>
-    this.client
+    this.inner
       .getOrSetState(x)
       .then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.GetOrSetState<TBot>
   public patchState: routes.PatchState<TBot> = ((x) =>
-    this.client
+    this.inner
       .patchState(x)
       .then((y) => ({ state: { ...y.state, payload: y.state.payload } }))) as routes.PatchState<TBot>
 
-  public callAction: routes.CallAction<TBot> = (x) => this.client.callAction(x)
+  public callAction: routes.CallAction<TBot> = (x) => this.inner.callAction(x)
 
-  public uploadFile: routes.UploadFile<TBot> = (x) => this.client.uploadFile(x)
-  public upsertFile: routes.UpsertFile<TBot> = (x) => this.client.upsertFile(x)
-  public deleteFile: routes.DeleteFile<TBot> = (x) => this.client.deleteFile(x)
-  public listFiles: routes.ListFiles<TBot> = (x) => this.client.listFiles(x)
-  public getFile: routes.GetFile<TBot> = (x) => this.client.getFile(x)
-  public updateFileMetadata: routes.UpdateFileMetadata<TBot> = (x) => this.client.updateFileMetadata(x)
-  public searchFiles: routes.SearchFiles<TBot> = (x) => this.client.searchFiles(x)
+  public uploadFile: routes.UploadFile<TBot> = (x) => this.inner.uploadFile(x)
+  public upsertFile: routes.UpsertFile<TBot> = (x) => this.inner.upsertFile(x)
+  public deleteFile: routes.DeleteFile<TBot> = (x) => this.inner.deleteFile(x)
+  public listFiles: routes.ListFiles<TBot> = (x) => this.inner.listFiles(x)
+  public getFile: routes.GetFile<TBot> = (x) => this.inner.getFile(x)
+  public updateFileMetadata: routes.UpdateFileMetadata<TBot> = (x) => this.inner.updateFileMetadata(x)
+  public searchFiles: routes.SearchFiles<TBot> = (x) => this.inner.searchFiles(x)
 
   /**
    * @deprecated Use `callAction` to delegate the conversation creation to an integration.
    */
-  public createConversation: routes.CreateConversation<TBot> = (x) => this.client.createConversation(x)
+  public createConversation: routes.CreateConversation<TBot> = (x) => this.inner.createConversation(x)
   /**
    * @deprecated Use `callAction` to delegate the conversation creation to an integration.
    */
-  public getOrCreateConversation: routes.GetOrCreateConversation<TBot> = (x) => this.client.getOrCreateConversation(x)
+  public getOrCreateConversation: routes.GetOrCreateConversation<TBot> = (x) => this.inner.getOrCreateConversation(x)
   /**
    * @deprecated Use `callAction` to delegate the user creation to an integration.
    */
-  public createUser: routes.CreateUser<TBot> = (x) => this.client.createUser(x)
+  public createUser: routes.CreateUser<TBot> = (x) => this.inner.createUser(x)
   /**
    * @deprecated Use `callAction` to delegate the user creation to an integration.
    */
-  public getOrCreateUser: routes.GetOrCreateUser<TBot> = (x) => this.client.getOrCreateUser(x)
+  public getOrCreateUser: routes.GetOrCreateUser<TBot> = (x) => this.inner.getOrCreateUser(x)
 }
--- packages/sdk/src/integration/client/index.ts
+++ packages/sdk/src/integration/client/index.ts
@@ -6,72 +6,72 @@ import * as routes from './routes'
  * Just like the regular botpress client, but typed with the integration's properties.
  */
 export class IntegrationSpecificClient<TIntegration extends BaseIntegration> {
-  public constructor(private readonly client: Client) {}
+  public constructor(public readonly inner: Client) {}
 
   public createConversation: routes.CreateConversation<TIntegration> = ((x) =>
-    this.client.createConversation(x)) as routes.CreateConversation<TIntegration>
+    this.inner.createConversation(x)) as routes.CreateConversation<TIntegration>
   public getConversation: routes.GetConversation<TIntegration> = ((x) =>
-    this.client.getConversation(x)) as routes.GetConversation<TIntegration>
+    this.inner.getConversation(x)) as routes.GetConversation<TIntegration>
   public listConversations: routes.ListConversations<TIntegration> = ((x) =>
-    this.client.listConversations(x)) as routes.ListConversations<TIntegration>
+    this.inner.listConversations(x)) as routes.ListConversations<TIntegration>
   public getOrCreateConversation: routes.GetOrCreateConversation<TIntegration> = ((x) =>
-    this.client.getOrCreateConversation(x)) as routes.GetOrCreateConversation<TIntegration>
+    this.inner.getOrCreateConversation(x)) as routes.GetOrCreateConversation<TIntegration>
   public updateConversation: routes.UpdateConversation<TIntegration> = ((x) =>
-    this.client.updateConversation(x)) as routes.UpdateConversation<TIntegration>
+    this.inner.updateConversation(x)) as routes.UpdateConversation<TIntegration>
   public deleteConversation: routes.DeleteConversation<TIntegration> = ((x) =>
-    this.client.deleteConversation(x)) as routes.DeleteConversation<TIntegration>
+    this.inner.deleteConversation(x)) as routes.DeleteConversation<TIntegration>
 
   public listParticipants: routes.ListParticipants<TIntegration> = ((x) =>
-    this.client.listParticipants(x)) as routes.ListParticipants<TIntegration>
+    this.inner.listParticipants(x)) as routes.ListParticipants<TIntegration>
   public addParticipant: routes.AddParticipant<TIntegration> = ((x) =>
-    this.client.addParticipant(x)) as routes.AddParticipant<TIntegration>
+    this.inner.addParticipant(x)) as routes.AddParticipant<TIntegration>
   public getParticipant: routes.GetParticipant<TIntegration> = ((x) =>
-    this.client.getParticipant(x)) as routes.GetParticipant<TIntegration>
+    this.inner.getParticipant(x)) as routes.GetParticipant<TIntegration>
   public removeParticipant: routes.RemoveParticipant<TIntegration> = ((x) =>
-    this.client.removeParticipant(x)) as routes.RemoveParticipant<TIntegration>
+    this.inner.removeParticipant(x)) as routes.RemoveParticipant<TIntegration>
 
   public createEvent: routes.CreateEvent<TIntegration> = ((x) =>
-    this.client.createEvent(x)) as routes.CreateEvent<TIntegration>
-  public getEvent: routes.GetEvent<TIntegration> = ((x) => this.client.getEvent(x)) as routes.GetEvent<TIntegration>
+    this.inner.createEvent(x)) as routes.CreateEvent<TIntegration>
+  public getEvent: routes.GetEvent<TIntegration> = ((x) => this.inner.getEvent(x)) as routes.GetEvent<TIntegration>
   public listEvents: routes.ListEvents<TIntegration> = ((x) =>
-    this.client.listEvents(x)) as routes.ListEvents<TIntegration>
+    this.inner.listEvents(x)) as routes.ListEvents<TIntegration>
 
   public createMessage: routes.CreateMessage<TIntegration> = ((x) =>
-    this.client.createMessage(x)) as routes.CreateMessage<TIntegration>
+    this.inner.createMessage(x)) as routes.CreateMessage<TIntegration>
   public getOrCreateMessage: routes.GetOrCreateMessage<TIntegration> = ((x) =>
-    this.client.getOrCreateMessage(x)) as routes.GetOrCreateMessage<TIntegration>
+    this.inner.getOrCreateMessage(x)) as routes.GetOrCreateMessage<TIntegration>
   public getMessage: routes.GetMessage<TIntegration> = ((x) =>
-    this.client.getMessage(x)) as routes.GetMessage<TIntegration>
+    this.inner.getMessage(x)) as routes.GetMessage<TIntegration>
   public updateMessage: routes.UpdateMessage<TIntegration> = ((x) =>
-    this.client.updateMessage(x)) as routes.UpdateMessage<TIntegration>
+    this.inner.updateMessage(x)) as routes.UpdateMessage<TIntegration>
   public listMessages: routes.ListMessages<TIntegration> = ((x) =>
-    this.client.listMessages(x)) as routes.ListMessages<TIntegration>
+    this.inner.listMessages(x)) as routes.ListMessages<TIntegration>
   public deleteMessage: routes.DeleteMessage<TIntegration> = ((x) =>
-    this.client.deleteMessage(x)) as routes.DeleteMessage<TIntegration>
+    this.inner.deleteMessage(x)) as routes.DeleteMessage<TIntegration>
 
   public createUser: routes.CreateUser<TIntegration> = ((x) =>
-    this.client.createUser(x)) as routes.CreateUser<TIntegration>
-  public getUser: routes.GetUser<TIntegration> = ((x) => this.client.getUser(x)) as routes.GetUser<TIntegration>
-  public listUsers: routes.ListUsers<TIntegration> = (x) => this.client.listUsers(x)
+    this.inner.createUser(x)) as routes.CreateUser<TIntegration>
+  public getUser: routes.GetUser<TIntegration> = ((x) => this.inner.getUser(x)) as routes.GetUser<TIntegration>
+  public listUsers: routes.ListUsers<TIntegration> = (x) => this.inner.listUsers(x)
   public getOrCreateUser: routes.GetOrCreateUser<TIntegration> = ((x) =>
-    this.client.getOrCreateUser(x)) as routes.GetOrCreateUser<TIntegration>
+    this.inner.getOrCreateUser(x)) as routes.GetOrCreateUser<TIntegration>
   public updateUser: routes.UpdateUser<TIntegration> = ((x) =>
-    this.client.updateUser(x)) as routes.UpdateUser<TIntegration>
-  public deleteUser: routes.DeleteUser<TIntegration> = (x) => this.client.deleteUser(x)
+    this.inner.updateUser(x)) as routes.UpdateUser<TIntegration>
+  public deleteUser: routes.DeleteUser<TIntegration> = (x) => this.inner.deleteUser(x)
 
-  public getState: routes.GetState<TIntegration> = ((x) => this.client.getState(x)) as routes.GetState<TIntegration>
-  public setState: routes.SetState<TIntegration> = ((x) => this.client.setState(x)) as routes.SetState<TIntegration>
+  public getState: routes.GetState<TIntegration> = ((x) => this.inner.getState(x)) as routes.GetState<TIntegration>
+  public setState: routes.SetState<TIntegration> = ((x) => this.inner.setState(x)) as routes.SetState<TIntegration>
   public getOrSetState: routes.GetOrSetState<TIntegration> = ((x) =>
-    this.client.getOrSetState(x)) as routes.GetOrSetState<TIntegration>
+    this.inner.getOrSetState(x)) as routes.GetOrSetState<TIntegration>
   public patchState: routes.PatchState<TIntegration> = ((x) =>
-    this.client.patchState(x)) as routes.PatchState<TIntegration>
+    this.inner.patchState(x)) as routes.PatchState<TIntegration>
 
-  public configureIntegration: routes.ConfigureIntegration<TIntegration> = (x) => this.client.configureIntegration(x)
+  public configureIntegration: routes.ConfigureIntegration<TIntegration> = (x) => this.inner.configureIntegration(x)
 
-  public uploadFile: routes.UploadFile<TIntegration> = (x) => this.client.uploadFile(x)
-  public upsertFile: routes.UpsertFile<TIntegration> = (x) => this.client.upsertFile(x)
-  public deleteFile: routes.DeleteFile<TIntegration> = (x) => this.client.deleteFile(x)
-  public listFiles: routes.ListFiles<TIntegration> = (x) => this.client.listFiles(x)
-  public getFile: routes.GetFile<TIntegration> = (x) => this.client.getFile(x)
-  public updateFileMetadata: routes.UpdateFileMetadata<TIntegration> = (x) => this.client.updateFileMetadata(x)
+  public uploadFile: routes.UploadFile<TIntegration> = (x) => this.inner.uploadFile(x)
+  public upsertFile: routes.UpsertFile<TIntegration> = (x) => this.inner.upsertFile(x)
+  public deleteFile: routes.DeleteFile<TIntegration> = (x) => this.inner.deleteFile(x)
+  public listFiles: routes.ListFiles<TIntegration> = (x) => this.inner.listFiles(x)
+  public getFile: routes.GetFile<TIntegration> = (x) => this.inner.getFile(x)
+  public updateFileMetadata: routes.UpdateFileMetadata<TIntegration> = (x) => this.inner.updateFileMetadata(x)
 }
