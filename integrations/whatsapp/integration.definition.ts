ac16765700216db2e5f15b08535095c600f37947
David Vitor Antonio <david.vitora@gmail.com>
2024-07-29 18:04:25 -0300

    support new studio configuration panel

--- integrations/whatsapp/integration.definition.ts
+++ integrations/whatsapp/integration.definition.ts
@@ -51,14 +51,22 @@ export default new IntegrationDefinition({
     },
     schema: z.object({
       useManualConfiguration: z.boolean().optional().describe('Skip oAuth and supply details from a Meta App'),
-      verifyToken: z.string().min(1).optional().describe('Token used for verification when subscribing to webhooks'),
+      verifyToken: z.string().optional().describe('Token used for verification when subscribing to webhooks'),
       accessToken: z
         .string()
-        .min(1)
         .optional()
         .describe('Access Token from a System Account that has permission to the Meta app'),
-      clientSecret: z.string().min(1).optional().describe('Meta app secret used for webhook signature check'),
-      phoneNumberId: z.string().min(1).optional().describe('Default Phone used for starting conversations'),
+      clientSecret: z.string().optional().describe('Meta app secret used for webhook signature check'),
+      phoneNumberId: z.string().optional().describe('Default Phone used for starting conversations'),
+    }).hidden(formData => {
+      const showConfig = !formData?.useManualConfiguration
+
+      return {
+        verifyToken: showConfig,
+        accessToken: showConfig,
+        clientSecret: showConfig,
+        phoneNumberId: showConfig,
+      }
     }),
   },
   identifier: {
