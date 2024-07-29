71e9a75d1e3b26f1441b8e219fa29f6690eea7ad
David Vitor Antonio <david.vitora@gmail.com>
2024-07-29 18:05:31 -0300

    prettier

--- integrations/whatsapp/integration.definition.ts
+++ integrations/whatsapp/integration.definition.ts
@@ -49,25 +49,27 @@ export default new IntegrationDefinition({
         title: 'Use Manual Configuration',
       },
     },
-    schema: z.object({
-      useManualConfiguration: z.boolean().optional().describe('Skip oAuth and supply details from a Meta App'),
-      verifyToken: z.string().optional().describe('Token used for verification when subscribing to webhooks'),
-      accessToken: z
-        .string()
-        .optional()
-        .describe('Access Token from a System Account that has permission to the Meta app'),
-      clientSecret: z.string().optional().describe('Meta app secret used for webhook signature check'),
-      phoneNumberId: z.string().optional().describe('Default Phone used for starting conversations'),
-    }).hidden(formData => {
-      const showConfig = !formData?.useManualConfiguration
+    schema: z
+      .object({
+        useManualConfiguration: z.boolean().optional().describe('Skip oAuth and supply details from a Meta App'),
+        verifyToken: z.string().optional().describe('Token used for verification when subscribing to webhooks'),
+        accessToken: z
+          .string()
+          .optional()
+          .describe('Access Token from a System Account that has permission to the Meta app'),
+        clientSecret: z.string().optional().describe('Meta app secret used for webhook signature check'),
+        phoneNumberId: z.string().optional().describe('Default Phone used for starting conversations'),
+      })
+      .hidden((formData) => {
+        const showConfig = !formData?.useManualConfiguration
 
-      return {
-        verifyToken: showConfig,
-        accessToken: showConfig,
-        clientSecret: showConfig,
-        phoneNumberId: showConfig,
-      }
-    }),
+        return {
+          verifyToken: showConfig,
+          accessToken: showConfig,
+          clientSecret: showConfig,
+          phoneNumberId: showConfig,
+        }
+      }),
   },
   identifier: {
     extractScript: 'extract.vrl',
