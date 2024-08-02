1a8d85a17ae2981d805930d28108cc06cf02d587
Francois Levasseur <francois.levasseur@botpress.com>
2024-08-02 12:08:25 -0400

    chore: move back hitl interface in public repo

--- packages/sdk/src/interfaces/index.ts
+++ packages/sdk/src/interfaces/index.ts
@@ -1,5 +1,7 @@
-export * from './llm'
-export * from './text-to-image'
-export * from './speech-to-text'
 export * as schemas from './schemas'
+
+export * from './hitl'
+export * from './llm'
+export * from './speech-to-text'
 export * from './sync'
+export * from './text-to-image'
