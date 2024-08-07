426ce30ce060c8dfcd9b0b817a6b1d8831b9113b
ermek-botpress <ermek.barmashev@botpress.com>
2024-08-07 14:43:35 -0400

    feat(zendesk): sync zendesk kb to bp kb
    wip
    
    wip
    
    wip
    
    wip

---
+++ integrations/zendesk/src/events/article-unpublished.ts
@@ -0,0 +1,40 @@
+import { Client, Logger, Context } from '.botpress'
+import { ZendeskEvent } from 'src/webhookEvents'
+
+export const articleUnpublished = async ({
+  event,
+  client,
+  logger,
+  ctx,
+}: {
+  event: ZendeskEvent
+  client: Client
+  logger: Logger
+  ctx: Context
+}) => {
+  console.log(event, 'evemttt')
+  if (ctx.configuration.syncKnowledgeBaseWithBot) {
+    const existingFiles = await client.listFiles({
+      tags: {
+        zendeskId: `${event.detail.id}`,
+      },
+    })
+
+    const existingFile = existingFiles.files[0]
+
+    if (!existingFile) {
+      logger.forBot().error(`Article not found in the BP KB`)
+      return
+    }
+
+    await client.deleteFile({ id: existingFile.id })
+    logger.forBot().info(`Successfully deleted unpublished article "${existingFile.tags?.title}"`)
+  }
+
+  await client.createEvent({
+    type: 'articleUnpublished',
+    payload: {
+      articleId: event.detail.id,
+    },
+  })
+}
