f7a4e53fd5ee4e7bf9f96a70978aaf0178137eee
Pascal Breton <pascal.breton@botpress.com>
2024-09-06 18:11:43 -0400

    add more entities

--- integrations/trello/definitions/entities.ts
+++ integrations/trello/definitions/entities.ts
@@ -1,10 +1,33 @@
 import { IntegrationDefinitionProps } from '@botpress/sdk'
+import { BoardSchema } from 'src/schemas/entities/board'
 import { CardSchema } from 'src/schemas/entities/card'
+import { ListSchema } from 'src/schemas/entities/list'
+import { MemberSchema } from 'src/schemas/entities/member'
 
 export const entities: IntegrationDefinitionProps['entities'] = {
   card: {
     title: 'Card',
     description: 'A card in a Trello list',
     schema: CardSchema,
+  },
+  list: {
+    title: 'List',
+    description: 'A list in a Trello board',
+    schema: ListSchema,
+  },
+  board: {
+    title: 'Board',
+    description: 'A Trello board',
+    schema: BoardSchema,
+  },
+  boardMember: {
+    title: 'Board Member',
+    description: 'A member of a Trello board',
+    schema: MemberSchema,
+  },
+  cardMember: {
+    title: 'Card Member',
+    description: 'A member assigned to a Trello card',
+    schema: MemberSchema,
   },
 } as const
