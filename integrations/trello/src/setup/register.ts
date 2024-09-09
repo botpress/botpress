1bc740da619532f1fef5baf89ede6f5b9f4ca2e8
ermek-botpress <ermek.barmashev@botpress.com>
2024-09-09 16:40:10 -0400

    fix(templates): change comments to indicate that register is run on config save (#13273)

--- integrations/trello/src/setup/register.ts
+++ integrations/trello/src/setup/register.ts
@@ -2,7 +2,7 @@ import type { RegisterFunction } from '../misc/types'
 
 export const register: RegisterFunction = async () => {
   /**
-   * This is called when a bot installs the integration.
+   * This is called when an integration configuration is saved.
    * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
    */
 }
