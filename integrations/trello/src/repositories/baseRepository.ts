0349e2d16adf82fb479d27b483c7e1dca5ce44db
Pascal Breton <pascal.breton@botpress.com>
2024-09-10 11:58:22 -0400

    rename all files to camelCase

--- integrations/trello/src/repositories/baseRepository.ts
+++ integrations/trello/src/repositories/baseRepository.ts
@@ -2,7 +2,7 @@ import { TrelloClient } from 'trello.js'
 import { TrelloOperationError } from '../errors/trelloOperationError'
 
 export abstract class BaseRepository {
-  public constructor(protected readonly trelloClient: TrelloClient) {}
+  public constructor(protected readonly trelloClient: TrelloClient) { }
 
   protected handleError(operation: string, error: unknown): never {
     throw new TrelloOperationError(`Error during ${operation}: ${error}`, error)
