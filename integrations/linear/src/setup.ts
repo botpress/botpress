eb3dc411b99532759949f569cf15219b760e490a
Pascal Breton <pascal.breton@botpress.com>
2024-10-02 15:50:21 -0400

    remove the deprecated createConversation function

--- integrations/linear/src/setup.ts
+++ integrations/linear/src/setup.ts
@@ -8,6 +8,3 @@ export const unregister: bp.IntegrationProps['unregister'] = async () => {
   // nothing to unregister
 }
 
-export const createConversation: bp.IntegrationProps['createConversation'] = async ({ client, channel, tags }) => {
-  await client.createConversation({ channel, tags })
-}
