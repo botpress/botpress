500fed994cd4223bfc5d430fbfefd3f6afda2ca9
Pascal Breton <pascal.breton@botpress.com>
2024-10-04 15:47:40 -0400

    chore(integrations/slack): bump patch version

--- integrations/slack/integration.definition.ts
+++ integrations/slack/integration.definition.ts
@@ -28,7 +28,7 @@ export default new IntegrationDefinition({
   name: 'slack',
   title: 'Slack',
   description: 'This integration allows your bot to interact with Slack.',
-  version: '1.0.1',
+  version: '1.0.2',
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration: {
