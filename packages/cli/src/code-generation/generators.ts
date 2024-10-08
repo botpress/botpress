947b31018b9047cf1ed01b296179dad13e3d5a8b
Francois Levasseur <francois.levasseur@botpress.com>
2024-10-08 17:14:08 -0400

    update

--- packages/cli/src/code-generation/generators.ts
+++ packages/cli/src/code-generation/generators.ts
@@ -1,30 +1,18 @@
 import * as sdk from '@botpress/sdk'
 import { JSONSchema7 } from 'json-schema'
-import { compile } from 'json-schema-to-typescript'
 import * as prettier from 'prettier'
 import * as utils from '../utils'
 import * as consts from './consts'
 
-export type GeneratorType = 'zui' | 'jsonSchemaToTypescript'
-
-export const zuiSchemaToTypeScriptType = async (
-  zuiSchema: sdk.z.AnyZodObject,
-  name: string,
-  type: GeneratorType = 'jsonSchemaToTypescript'
-): Promise<string> => {
-  if (type === 'zui') {
-    let code = zuiSchema.toTypescript()
-    code = `export type ${name} = ${code}`
-    code = prettier.format(code, { parser: 'typescript' })
-    return [
-      //
-      consts.GENERATED_HEADER,
-      code,
-    ].join('\n')
-  }
-  const jsonSchema = await utils.schema.mapZodToJsonSchema({ schema: zuiSchema })
-  const code = await compile(jsonSchema, name, { unknownAny: false })
-  return code
+export const zuiSchemaToTypeScriptType = async (zuiSchema: sdk.z.AnyZodObject, name: string): Promise<string> => {
+  let code = zuiSchema.toTypescript()
+  code = `export type ${name} = ${code}`
+  code = prettier.format(code, { parser: 'typescript' })
+  return [
+    //
+    consts.GENERATED_HEADER,
+    code,
+  ].join('\n')
 }
 
 export const jsonSchemaToTypescriptZuiSchema = async (
