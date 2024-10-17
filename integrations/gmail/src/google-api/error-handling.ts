430e8bb7badf138a6447f44995aaa41135e07a50
Pascal Breton <pascal.breton@botpress.com>
2024-10-17 09:25:22 -0400

    feat(packages/common): add a default error redactor

--- integrations/gmail/src/google-api/error-handling.ts
+++ integrations/gmail/src/google-api/error-handling.ts
@@ -1,11 +1,9 @@
-import { createAsyncFnWrapperWithErrorRedaction } from '@botpress/common'
-import * as sdk from '@botpress/sdk'
+import { createAsyncFnWrapperWithErrorRedaction, defaultErrorRedactor } from '@botpress/common'
 
-export const wrapWithTryCatch = createAsyncFnWrapperWithErrorRedaction((error: Error, customMessage: string) => {
-  console.log(customMessage, error)
-
-  // since emails can be quite sensitive, by default we will not expose the
-  // original error message to the user, instead replacing it with a generic
-  // message if we're not given a RuntimeError
-  return error instanceof sdk.RuntimeError ? error : new sdk.RuntimeError(customMessage)
-})
+/*
+  Since emails can be quite sensitive, by default we will not expose the
+  original error message to the user, instead replacing it with a generic
+  message if we're not given a RuntimeError. This is done by the default
+  error redactor function.
+*/
+export const wrapWithTryCatch = createAsyncFnWrapperWithErrorRedaction(defaultErrorRedactor)
