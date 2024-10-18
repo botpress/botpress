48685f438691930d95554bffcc579643928b621b
Pascal Breton <pascal.breton@botpress.com>
2024-10-18 08:53:21 -0400

    chore(integrations/gmail): bump patch version

--- integrations/gmail/integration.definition.ts
+++ integrations/gmail/integration.definition.ts
@@ -3,7 +3,7 @@ import { configuration, identifier, channels, user, states, actions, events, sec
 
 export default new sdk.IntegrationDefinition({
   name: 'gmail',
-  version: '0.4.6',
+  version: '0.4.7',
   title: 'Gmail',
   description: 'This integration allows your bot to interact with Gmail.',
   icon: 'icon.svg',
