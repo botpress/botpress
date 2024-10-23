d1492c8e0d1617941e405b26df02473c0982ea5b
Sébastien Poitras <sebastienpoitras@Mac.localdomain>
2024-10-23 10:31:41 -0400

    Initial commit

---
+++ integrations/googledrive/integration.definition.ts
@@ -0,0 +1,9 @@
+import { IntegrationDefinition} from '@botpress/sdk'
+import { integrationName } from './package.json'
+
+export default new IntegrationDefinition({
+  name: integrationName,
+  version: '0.0.1',
+  readme: 'hub.md',
+  icon: 'icon.svg',
+})
