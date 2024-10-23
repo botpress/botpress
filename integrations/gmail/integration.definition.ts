56fd2bdd38e6e899dd5cb3957b923593811b0f84
Pascal Breton <pascal.breton@botpress.com>
2024-10-23 13:39:14 -0400

    chore(integrations/gmail): bump minor version

--- integrations/gmail/integration.definition.ts
+++ integrations/gmail/integration.definition.ts
@@ -13,7 +13,7 @@ import {
 
 export default new sdk.IntegrationDefinition({
   name: 'gmail',
-  version: '0.5.3',
+  version: '0.6.0',
   title: 'Gmail',
   description: "Send, receive, and manage emails directly within your bot's workflow.",
   icon: 'icon.svg',
