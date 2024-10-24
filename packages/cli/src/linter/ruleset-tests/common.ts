4d02e3050427b44f88e00f226b12846368c6f544
Pascal Breton <pascal.breton@botpress.com>
2024-10-24 12:33:03 -0400

    chore(packages/cli): abstract describeRule with a factory

---
+++ packages/cli/src/linter/ruleset-tests/common.ts
@@ -0,0 +1,32 @@
+import { describe } from 'vitest'
+import { Document, type ISpectralDiagnostic, Spectral, type RuleDefinition } from '@stoplight/spectral-core'
+import { Json as JsonParser } from '@stoplight/spectral-parsers'
+
+export type RecursivePartial<T> = {
+  [P in Extract<keyof T, string>]?: T[P] extends (infer U)[]
+    ? RecursivePartial<U>[]
+    : T[P] extends object | undefined
+    ? RecursivePartial<T[P]>
+    : T[P]
+}
+
+type Ruleset = {
+  rules: Record<string, Readonly<RuleDefinition>>
+}
+
+export const createDescribeRule =
+  <TDefinition>() =>
+  <TRuleset extends Ruleset>(ruleset: TRuleset) =>
+  <TPartialDefinition extends RecursivePartial<TDefinition>>(
+    ruleName: Extract<keyof (typeof ruleset)['rules'], string>,
+    fn: (lint: (definition: TPartialDefinition) => Promise<ISpectralDiagnostic[]>) => void
+  ) =>
+    describe.concurrent(ruleName, () => {
+      const spectral = new Spectral()
+      spectral.setRuleset({ ...ruleset, rules: { [ruleName]: ruleset.rules[ruleName]! } })
+
+      const lintFn = (definition: TPartialDefinition) =>
+        spectral.run(new Document(JSON.stringify(definition), JsonParser))
+
+      fn(lintFn)
+    })
