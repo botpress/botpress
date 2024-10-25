64b5781198dc6aac637f7254c5cff59f7b438150
Francois Levasseur <francois.levasseur@botpress.com>
2024-10-25 14:38:49 -0400

    update

---
+++ bots/sinlin/integrations.ts
@@ -0,0 +1,30 @@
+import * as childprocess from 'child_process'
+import * as path from 'path'
+
+type RunCommandOptions = {
+  workDir: string
+}
+
+type RunCommandOutput = {
+  exitCode: number
+}
+const runCommand = (cmd: string, { workDir }: RunCommandOptions): RunCommandOutput => {
+  const [program, ...args] = cmd.split(' ')
+  if (!program) {
+    throw new Error('Cannot run empty command')
+  }
+  const { error, status } = childprocess.spawnSync(program, args, {
+    cwd: workDir,
+    stdio: 'inherit',
+  })
+  if (error) {
+    throw error
+  }
+  return { exitCode: status ?? 0 }
+}
+
+const integrationNames: string[] = ['linear', 'telegram']
+for (const integrationName of integrationNames) {
+  const integrationPath = path.resolve(path.join('..', '..', 'integrations', integrationName))
+  runCommand(`bp add ${integrationPath} -y`, { workDir: __dirname })
+}
