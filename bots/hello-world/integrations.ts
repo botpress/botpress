27ddabd240960ff59e975cdc5a32c42c064b1751
Francois Levasseur <francois.levasseur@botpress.com>
2024-10-25 16:28:28 -0400

    update

--- bots/hello-world/integrations.ts
+++ bots/hello-world/integrations.ts
@@ -2,7 +2,7 @@ import * as common from '@botpress/common'
 import * as path from 'path'
 
 const { runCommand } = common.cmd
-const integrationNames: string[] = ['whatsapp']
+const integrationNames: string[] = ['whatsapp', 'webhook']
 for (const integrationName of integrationNames) {
   const integrationPath = path.resolve(path.join('..', '..', 'integrations', integrationName))
   runCommand(`pnpm exec bp add ${integrationPath} -y`, { workDir: __dirname })
