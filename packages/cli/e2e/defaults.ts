00d56c6633e8969e6cc8dde6e25db5570854404b
François Levasseur <francois_levasseur@hotmail.com>
2024-10-26 15:47:29 -0400

    feat(cli): configurable outdir and entrypoint both become constant (#13385)

--- packages/cli/e2e/defaults.ts
+++ packages/cli/e2e/defaults.ts
@@ -1,13 +1,9 @@
-import * as consts from '../src/consts'
-
 const noBuild = false
 const secrets = [] satisfies string[]
 const sourceMap = false
 const verbose = false
 const confirm = true
 const json = false
-const entryPoint = consts.defaultEntrypoint
-const outDir = consts.defaultOutputFolder
 const allowDeprecated = false
 const isPublic = false
 const minify = true
@@ -19,8 +15,6 @@ export default {
   verbose,
   confirm,
   json,
-  entryPoint,
-  outDir,
   allowDeprecated,
   public: isPublic,
 }
--- packages/cli/src/consts.ts
+++ packages/cli/src/consts.ts
@@ -5,11 +5,8 @@ import { CLI_ROOT_DIR } from './root'
 // configurable
 
 export const defaultBotpressHome = pathlib.join(os.homedir(), '.botpress')
-
-export const defaultOutputFolder = '.botpress'
 export const defaultWorkDir = process.cwd()
 export const defaultInstallPath = process.cwd()
-export const defaultEntrypoint = pathlib.join('src', 'index.ts')
 export const defaultBotpressApiUrl = 'https://api.botpress.cloud'
 export const defaultBotpressAppUrl = 'https://app.botpress.cloud'
 export const defaultTunnelUrl = 'https://tunnel.botpress.cloud'
@@ -17,12 +14,13 @@ export const defaultTunnelUrl = 'https://tunnel.botpress.cloud'
 // not configurable
 
 export const cliRootDir = CLI_ROOT_DIR
-
 export const emptyBotDirName = 'empty-bot'
 export const emptyIntegrationDirName = 'empty-integration'
 export const helloWorldIntegrationDirName = 'hello-world'
 export const webhookMessageIntegrationDirName = 'webhook-message'
 export const installDirName = 'bp_modules'
+export const outDirName = '.botpress'
+export const distDirName = 'dist'
 
 export const fromCliRootDir = {
   emptyBotTemplate: pathlib.join('templates', emptyBotDirName),
@@ -35,16 +33,23 @@ export const fromHomeDir = {
   globalCacheFile: 'global.cache.json',
 }
 
+export const fromOutDir = {
+  distDir: distDirName,
+  outFile: pathlib.join(distDirName, 'index.js'),
+  implementationDir: 'implementation',
+  secretsDir: 'secrets',
+  projectCacheFile: 'project.cache.json',
+}
+
 export const fromWorkDir = {
   integrationDefinition: 'integration.definition.ts',
   interfaceDefinition: 'interface.definition.ts',
   botDefinition: 'bot.definition.ts',
-}
-
-export const fromOutDir = {
-  distDir: 'dist',
-  outFile: pathlib.join('dist', 'index.js'),
-  implementationDir: 'implementation',
-  secretsDir: 'secrets',
-  projectCacheFile: 'project.cache.json',
+  entryPoint: pathlib.join('src', 'index.ts'),
+  outDir: outDirName,
+  distDir: pathlib.join(outDirName, fromOutDir.distDir),
+  outFile: pathlib.join(outDirName, fromOutDir.outFile),
+  implementationDir: pathlib.join(outDirName, fromOutDir.implementationDir),
+  secretsDir: pathlib.join(outDirName, fromOutDir.secretsDir),
+  projectCacheFile: pathlib.join(outDirName, fromOutDir.projectCacheFile),
 }
