3cc6090dbb98bdda51c33fa0ff26842511b77ae7
Sébastien Poitras <sebastien.poitras@botpress.com>
2024-10-31 10:39:05 -0400

    Refactor

--- integrations/googledrive/src/types.d.ts
+++ integrations/googledrive/src/types.d.ts
@@ -4,7 +4,11 @@ export type GoogleDriveClient = drive_v3.Drive
 export type UnvalidatedGoogleDriveFile = drive_v3.Schema$File
 export type UnvalidatedGoogleDriveDrive = drive_v3.Schema$Drive
 
-export type GoogleDriveFile = UnvalidatedGoogleDriveFile & { id: string }
-export type GoogleDriveDrive = UnvalidatedGoogleDriveDrive & { id: string }
+export type GoogleDriveFile = UnvalidatedGoogleDriveFile & {
+    id: string // Non null
+}
+export type GoogleDriveDrive = UnvalidatedGoogleDriveDrive & {
+    id: string // Non null
+}
 
 export type GoogleOAuth2Client = InstanceType<(typeof google.auth)['OAuth2']>
