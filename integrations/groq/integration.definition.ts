5c4b62cb3cd1da096f5fa4180b41a6e92a622d6e
Abraham Lopez <10536776+AbrahamLopez10@users.noreply.github.com>
2024-11-01 14:42:25 -0400

    chore(integrations/groq): bump to major version to satisfy integration schema validator (#13404)

--- integrations/groq/integration.definition.ts
+++ integrations/groq/integration.definition.ts
@@ -6,12 +6,9 @@ import stt from './bp_modules/speech-to-text'
 export default new IntegrationDefinition({
   name: 'groq',
   title: 'Groq',
-  version: '6.4.0',
+  version: '7.0.0',
   readme: 'hub.md',
   icon: 'icon.svg',
-  configuration: {
-    schema: z.any() as unknown as z.AnyZodObject, // TODO: remove this and bump a major
-  },
   entities: {
     modelRef: {
       schema: z.object({
