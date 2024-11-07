4502ccbd56c27f4be3c35e829cff839474c75065
François Levasseur <francois_levasseur@hotmail.com>
2024-11-07 10:44:59 -0500

    chore: rm webchat integration (#13411)

--- integrations/webchat/src/definitions/schemas.ts
+++
@@ -1,8 +0,0 @@
-import { z } from '@botpress/sdk'
-
-export const TriggerSchema = z.object({
-  origin: z.literal('website').describe('The origin of the event trigger'),
-  userId: z.string().uuid().describe('The webchat userId that triggered the event'),
-  conversationId: z.string().uuid().describe('The webchat conversationId that triggered the event'),
-  payload: z.record(z.any()).describe('The payload to send with the event'),
-})
