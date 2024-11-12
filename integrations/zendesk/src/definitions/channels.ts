f2f80bacc4f14235712e62c7affdf791893beded
Francois Levasseur <francois.levasseur@botpress.com>
2024-11-12 13:47:58 -0500

    fix(zendesk): message history can contain markdown

--- integrations/zendesk/src/definitions/channels.ts
+++ integrations/zendesk/src/definitions/channels.ts
@@ -3,7 +3,7 @@ import { IntegrationDefinitionProps } from '@botpress/sdk'
 export const channels = {
   hitl: {
     title: 'Zendesk Ticket',
-    messages: {}, // defined by the integration
+    messages: {}, // defined by the interface
     conversation: {
       tags: {
         id: {
