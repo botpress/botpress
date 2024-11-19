7ae161ff097ee3dbae67b499769f12237c43e237
François Levasseur <francois_levasseur@hotmail.com>
2024-11-19 14:13:21 -0500

    fix(sdk): make default types resilient to deep partial inputs (#13446)

--- packages/sdk/src/bot/types/generic.ts
+++ packages/sdk/src/bot/types/generic.ts
@@ -1,20 +1,27 @@
-import { BaseIntegration } from '../../integration/types/generic'
+import { BaseIntegration, DefaultIntegration, InputBaseIntegration } from '../../integration/types/generic'
 import * as utils from '../../utils/type-utils'
 
 export * from '../../integration/types/generic'
+
+export type BaseAction = {
+  input: any
+  output: any
+}
 
 export type BaseBot = {
   integrations: Record<string, BaseIntegration>
   events: Record<string, any>
   states: Record<string, any>
-  actions: Record<string, Record<'input' | 'output', any>>
+  actions: Record<string, BaseAction>
 }
 
-export type DefaultBot<B extends Partial<BaseBot>> = {
-  integrations: utils.Default<B['integrations'], BaseBot['integrations']>
+export type DefaultBot<B extends utils.DeepPartial<BaseBot>> = {
   events: utils.Default<B['events'], BaseBot['events']>
   states: utils.Default<B['states'], BaseBot['states']>
   actions: utils.Default<B['actions'], BaseBot['actions']>
+  integrations: undefined extends B['integrations']
+    ? BaseBot['integrations']
+    : {
+        [K in keyof B['integrations']]: DefaultIntegration<utils.Cast<B['integrations'][K], InputBaseIntegration>>
+      }
 }
-
-type _MakeBot_creates_a_TBot = utils.AssertExtends<DefaultBot<{}>, BaseBot>
--- packages/sdk/src/integration/types/generic.ts
+++ packages/sdk/src/integration/types/generic.ts
@@ -1,17 +1,26 @@
 import * as utils from '../../utils/type-utils'
 
+export type BaseMessage = {
+  tags: Record<string, any>
+}
+
+export type BaseConversation = {
+  tags: Record<string, any>
+}
+
 export type BaseChannel = {
   messages: Record<string, any>
-  message: {
-    tags: Record<string, any>
-  }
-  conversation: {
-    tags: Record<string, any>
-    creation: {
-      enabled: boolean
-      requiredTags: string[]
-    }
-  }
+  message: BaseMessage
+  conversation: BaseConversation
+}
+
+export type BaseUser = {
+  tags: Record<string, any>
+}
+
+export type BaseAction = {
+  input: any
+  output: any
 }
 
 export type BaseIntegration = {
@@ -19,38 +28,35 @@ export type BaseIntegration = {
   version: string
   configuration: any
   configurations: Record<string, any>
-  actions: Record<string, Record<'input' | 'output', any>>
+  actions: Record<string, BaseAction>
   channels: Record<string, BaseChannel>
   events: Record<string, any>
   states: Record<string, any>
-  user: {
-    tags: Record<string, any>
-    creation: {
-      enabled: boolean
-      requiredTags: string[]
-    }
-  }
+  user: BaseUser
   entities: Record<string, any>
 }
 
-export type DefaultChannel<C extends Partial<BaseIntegration['channels'][string]>> = {
-  messages: utils.Default<C['messages'], BaseIntegration['channels'][string]['messages']>
-  message: utils.Default<C['message'], BaseIntegration['channels'][string]['message']>
-  conversation: utils.Default<C['conversation'], BaseIntegration['channels'][string]['conversation']>
+export type InputBaseChannel = utils.DeepPartial<BaseChannel>
+export type DefaultChannel<C extends InputBaseChannel> = {
+  messages: utils.Default<C['messages'], BaseChannel['messages']>
+  message: utils.Default<C['message'], BaseChannel['message']>
+  conversation: utils.Default<C['conversation'], BaseChannel['conversation']>
 }
 
-export type DefaultIntegration<I extends Partial<BaseIntegration>> = {
+export type InputBaseIntegration = utils.DeepPartial<BaseIntegration>
+export type DefaultIntegration<I extends InputBaseIntegration> = {
   name: utils.Default<I['name'], BaseIntegration['name']>
   version: utils.Default<I['version'], BaseIntegration['version']>
   configuration: utils.Default<I['configuration'], BaseIntegration['configuration']>
   configurations: utils.Default<I['configurations'], BaseIntegration['configurations']>
   actions: utils.Default<I['actions'], BaseIntegration['actions']>
-  channels: utils.Default<I['channels'], BaseIntegration['channels']>
   events: utils.Default<I['events'], BaseIntegration['events']>
   states: utils.Default<I['states'], BaseIntegration['states']>
   user: utils.Default<I['user'], BaseIntegration['user']>
   entities: utils.Default<I['entities'], BaseIntegration['entities']>
+  channels: undefined extends I['channels']
+    ? BaseIntegration['channels']
+    : {
+        [K in keyof I['channels']]: DefaultChannel<utils.Cast<I['channels'][K], InputBaseChannel>>
+      }
 }
-
-type _MakeChannel_creates_a_TChannel = utils.AssertExtends<DefaultChannel<{}>, BaseChannel>
-type _MakeIntegration_creates_a_TIntegration = utils.AssertExtends<DefaultIntegration<{}>, BaseIntegration>
