73c968d9f116f2c525f9e19dc74c0379c70b5282
Francois Levasseur <francois.levasseur@botpress.com>
2024-11-25 15:52:55 -0500

    update

--- plugins/personality/src/index.ts
+++ plugins/personality/src/index.ts
@@ -1,39 +1,40 @@
+import * as gen from './generate-content'
+import * as rewrite from './rewrite-prompt'
 import * as bp from '.botpress'
 
 const plugin = new bp.Plugin({
   actions: {},
 })
 
-type LLMInput = bp.interfaces.llm.actions.generateContent.input.Input
-type LLMOutput = bp.interfaces.llm.actions.generateContent.output.Output
-
-type GenerateContentProps = {
-  client: bp.Client
-  input: LLMInput
-}
-// eslint-disable-next-line unused-imports/no-unused-vars
-const generateContent = async (props: GenerateContentProps): Promise<LLMOutput> => {
-  const { name: integrationName } = plugin.config.interfaces.llm
-
-  const { client, input } = props
-  const response = await client.callAction({
-    type: `${integrationName}:generateContent`,
-    input,
-  })
-  return response.output
-}
-
-plugin.hook.beforeOutgoingMessage('*', async ({ data: message }) => {
+plugin.hook.beforeOutgoingMessage('*', async ({ data: message, client }) => {
   if (message.type !== 'text') {
     return { data: message }
   }
 
+  const messageText = message.payload.text as string
+
   const { model, personality } = plugin.config.configuration
-  model
-  personality
 
-  // TODO: implement
+  const prompt = rewrite.prompt({
+    model,
+    personality,
+    payload: messageText,
+  })
+  const output = await gen.generateContent({
+    integrationName: plugin.config.interfaces.llm.name,
+    client,
+    input: prompt,
+  })
 
+  const { success, json } = gen.parseLLMOutput(output)
+
+  const parseResult = rewrite.responseSchema.safeParse(json)
+  if (!success || !parseResult.success) {
+    console.error('Failed to rewrite message')
+    return { data: message }
+  }
+
+  message.payload.text = parseResult.data.payload
   return { data: message }
 })
 
---
+++ plugins/personality/src/rewrite-prompt.ts
@@ -0,0 +1,46 @@
+import { z } from '@botpress/sdk'
+import dedent from 'dedent'
+import { LLMInput } from './generate-content'
+
+export const responseSchema = z.object({
+  payload: z.string(),
+})
+
+type PromptArgs = {
+  model: string
+  personality: string
+  payload: string
+}
+export const prompt = (args: PromptArgs): LLMInput => ({
+  model: { id: args.model },
+  responseFormat: 'json_object',
+  temperature: 0,
+  messages: [
+    {
+      role: 'system' as 'user', // TODO: fix llm interface to allow for system
+      content: dedent`
+      Please rewrite the below JSON messages in your own voice and personality.
+      Do NOT CHANGE the nature and meaning of any of the messages.
+      Do not add additional greetings, introduction or personality traits in the below messages.
+      Just change the writing style.
+      Preserve the numbering, ordering, meaning, casing and spacing.
+      Only rewrite the main message text, keep buttons and choices as-is.
+      
+      Your personality is as follows:
+      ---
+      ${args.personality}
+      ---
+      `,
+    },
+    {
+      role: 'user',
+      content: dedent`
+      \`\`\`json
+      { "payload": ${args.payload} }
+      \`\`\`
+
+      type OutputFormat = ${responseSchema.toTypescript()}
+      `,
+    },
+  ],
+})
