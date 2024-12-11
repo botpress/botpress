00f3205998c2dfb8e5dbb83eab3812a316d5a4cf
Abraham Lopez <abraham.lopez@botpress.com>
2024-12-11 18:26:37 -0500

    fix

--- integrations/groq/src/index.ts
+++ integrations/groq/src/index.ts
@@ -18,12 +18,12 @@ const languageModels: Record<ModelId, llm.ModelDetails> = {
       'The Meta Llama 3.3 multilingual large language model (LLM) is a pretrained and instruction tuned generative model in 70B (text in/text out). The Llama 3.3 instruction tuned text only model is optimized for multilingual dialogue use cases and outperforms many of the available open source and closed chat models on common industry benchmarks.',
     tags: ['recommended', 'general-purpose', 'coding'],
     input: {
-      costPer1MTokens: 0.04,
+      costPer1MTokens: 0.59,
       maxTokens: 128_000,
     },
     output: {
-      costPer1MTokens: 0.04,
-      maxTokens: 8192,
+      costPer1MTokens: 0.79,
+      maxTokens: 32_768,
     },
   },
   'llama-3.2-1b-preview': {
