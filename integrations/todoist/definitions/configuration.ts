5b0f7d2c2d8a555ed8b5998b5f4ca3c700eeb3f2
Pascal Breton <pascal.breton@botpress.com>
2025-01-07 08:56:42 -0500

    chore(integrations/todoist)!: refactor PAT authentication
    BREAKING CHANGE: The default config mode no longer contains the
                     PAT. Users must choose the new "Manual
                     configuration" option to keep using PATs.

--- integrations/todoist/definitions/configuration.ts
+++ integrations/todoist/definitions/configuration.ts
@@ -1,15 +1,22 @@
 import sdk, { z } from '@botpress/sdk'
 
 export const configuration = {
-  schema: z.object({
-    apiToken: z.string().optional().title('API Token').describe('The API token to authenticate with the Todoist API'),
-  }),
+  schema: z.object({}),
   identifier: {
     linkTemplateScript: 'linkTemplate.vrl',
+    required: true,
   },
 } as const satisfies sdk.IntegrationDefinitionProps['configuration']
 
-export const configurations = undefined satisfies sdk.IntegrationDefinitionProps['configurations']
+export const configurations = {
+  apiToken: {
+    title: 'Manual configuration',
+    description: 'Configure manually by supplying an API token',
+    schema: z.object({
+      apiToken: z.string().title('API Token').describe('The API token to authenticate with the Todoist API'),
+    }),
+  },
+} satisfies sdk.IntegrationDefinitionProps['configurations']
 
 export const identifier = {
   extractScript: 'extract.vrl',
