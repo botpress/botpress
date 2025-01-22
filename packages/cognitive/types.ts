9c6a449d441fa543e9d9b1c3a05d31090d6adae4
Francois Levasseur <francois.levasseur@botpress.com>
2025-01-22 14:54:37 -0500

    chore(cognitive): standard code generation scripts

--- packages/cognitive/types.ts
+++ packages/cognitive/types.ts
@@ -1,5 +1,6 @@
 import { llm } from '@botpress/common'
-import { writeFileSync } from 'node:fs'
+import * as fslib from 'fs/promises'
+import pathlib from 'path'
 
 const GenerateContentInput = llm.schemas.GenerateContentInputBaseSchema.toTypescript()
 const GenerateContentOutput = llm.schemas.GenerateContentOutputSchema.toTypescript()
@@ -11,4 +12,20 @@ export type GenerateContentOutput = ${GenerateContentOutput};
 export type Model = ${Model};
 `
 
-writeFileSync('./src/gen.ts', types)
+const main = async (argv: string[]) => {
+  const outDir = argv[0]
+  if (!outDir) {
+    throw new Error('Missing output directory')
+  }
+  await fslib.mkdir(outDir, { recursive: true })
+  await fslib.writeFile(pathlib.join(outDir, 'index.ts'), types)
+}
+
+void main(process.argv.slice(2))
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
