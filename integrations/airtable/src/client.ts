dff91bdee2984ad71516d6b1430841239545b8c4
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-01-22 14:54:12 -0500

    feat(airtable): added the action listRecords

--- integrations/airtable/src/client.ts
+++ integrations/airtable/src/client.ts
@@ -1,6 +1,7 @@
-import Airtable from 'airtable'
+import Airtable, { type FieldSet, type Records } from 'airtable'
 import axios, { AxiosInstance } from 'axios'
 import { TableFields } from './misc/types'
+import { stringify } from 'querystring'
 
 export class AirtableApi {
   private _base: Airtable.Base
@@ -11,12 +12,36 @@ export class AirtableApi {
     this._baseId = baseId
     this._base = new Airtable({ apiKey, endpointUrl }).base(baseId)
     this._axiosClient = axios.create({
-      baseURL: endpointUrl || 'https://api.airtable.com/v0/',
+      baseURL: 'https://api.airtable.com/v0/',
       headers: {
         Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json',
       },
     })
+  }
+
+  public async listRecords({
+    tableIdOrName,
+    filterByFormula,
+    offset,
+  }: {
+    tableIdOrName: string
+    filterByFormula?: string
+    offset?: string
+  }): Promise<{ records: Records<FieldSet>; offset?: string }> {
+    const response = await this._axiosClient.get(
+      `${this._baseId}/${tableIdOrName}?${stringify({ offset, filterByFormula })}`
+    )
+
+    const records = response.data?.records
+
+    if (!records) {
+      return { records: [], offset: undefined }
+    }
+
+    return {
+      records,
+      offset: response.data?.offset,
+    }
   }
 
   public async getTableRecords(tableIdOrName: string) {
