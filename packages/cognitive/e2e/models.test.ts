5cc229d263ed29be573bb466a533fe7548a9d55f
François Levasseur <francois_levasseur@hotmail.com>
2025-01-23 15:03:16 -0500

    chore: get-or-create the bot used for cognitive tests (#13581)

--- packages/cognitive/e2e/models.test.ts
+++ packages/cognitive/e2e/models.test.ts
@@ -108,7 +108,7 @@ describe('Remote Model Provider', () => {
     expect(preferences).toEqual(null)
   })
 
-  // TODO: fix this test
+  // TODO: Fix this failing test
   test.skip('save file preferences', async () => {
     const client = getTestClient()
     const provider = new RemoteModelProvider(client)
