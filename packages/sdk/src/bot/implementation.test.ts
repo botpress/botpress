b267f90d650ef0791dafec0c245d2deaae4f1627
Francois Levasseur <francois.levasseur@botpress.com>
2025-02-07 15:10:58 -0500

    update

--- packages/sdk/src/bot/implementation.test.ts
+++ packages/sdk/src/bot/implementation.test.ts
@@ -6,21 +6,21 @@ const createBot = () => new BotImplementation({ actions: {}, plugins: {} })
 test('getting text message handlers also returns global handlers', () => {
   const bot = createBot()
 
-  bot.on.message('text', async () => {})
-  bot.on.message('*', async () => {})
+  bot.on.message('text', async function foo() {})
+  bot.on.message('*', async function bar() {})
 
   const textHandlers = bot.messageHandlers['text']
-  expect(textHandlers?.length).toEqual(2)
+  expect(textHandlers?.map((h) => h.name)).toEqual(['foo', 'bar'])
 })
 
 test('getting global message handlers only returns global handlers once', () => {
   const bot = createBot()
 
-  bot.on.message('text', async () => {})
-  bot.on.message('*', async () => {})
+  bot.on.message('text', async function foo() {})
+  bot.on.message('*', async function bar() {})
 
   const commonHandlers = bot.messageHandlers['*']
-  expect(commonHandlers?.length).toEqual(1)
+  expect(commonHandlers?.map((h) => h.name)).toEqual(['bar'])
 })
 
 test('getting foo event handlers also returns global handlers', () => {
@@ -30,17 +30,17 @@ test('getting foo event handlers also returns global handlers', () => {
   bot.on.event('*', async function bar() {})
 
   const fooHandlers = bot.eventHandlers['foo']
-  expect(fooHandlers?.length).toEqual(2)
+  expect(fooHandlers?.map((h) => h.name)).toEqual(['foo', 'bar'])
 })
 
 test('getting global event handlers only returns global handlers once', () => {
   const bot = createBot()
 
-  bot.on.event('foo', async () => {})
-  bot.on.event('*', async () => {})
+  bot.on.event('foo', async function foo() {})
+  bot.on.event('*', async function bar() {})
 
   const commonHandlers = bot.eventHandlers['*']
-  expect(commonHandlers?.length).toEqual(1)
+  expect(commonHandlers?.map((h) => h.name)).toEqual(['bar'])
 })
 
 test('getting foo stateExpired handlers also returns global handlers', () => {
@@ -50,17 +50,17 @@ test('getting foo stateExpired handlers also returns global handlers', () => {
   bot.on.stateExpired('*', async function bar() {})
 
   const fooHandlers = bot.stateExpiredHandlers['foo']
-  expect(fooHandlers?.length).toEqual(2)
+  expect(fooHandlers?.map((h) => h.name)).toEqual(['foo', 'bar'])
 })
 
 test('getting global stateExpired handlers only returns global handlers once', () => {
   const bot = createBot()
 
-  bot.on.stateExpired('foo', async () => {})
-  bot.on.stateExpired('*', async () => {})
+  bot.on.stateExpired('foo', async function foo() {})
+  bot.on.stateExpired('*', async function bar() {})
 
   const commonHandlers = bot.stateExpiredHandlers['*']
-  expect(commonHandlers?.length).toEqual(1)
+  expect(commonHandlers?.map((h) => h.name)).toEqual(['bar'])
 })
 
 test('getting foo before_incoming_event hook handlers also returns global handlers', () => {
@@ -74,21 +74,21 @@ test('getting foo before_incoming_event hook handlers also returns global handle
   })
 
   const fooHandlers = bot.hookHandlers.before_incoming_event['foo']
-  expect(fooHandlers?.length).toEqual(2)
+  expect(fooHandlers?.map((h) => h.name)).toEqual(['foo', 'bar'])
 })
 
 test('getting global before_incoming_event hook handlers only returns global handlers once', () => {
   const bot = createBot()
 
-  bot.on.beforeIncomingEvent('foo', async () => {
+  bot.on.beforeIncomingEvent('foo', async function foo() {
     return undefined
   })
-  bot.on.beforeIncomingEvent('*', async () => {
+  bot.on.beforeIncomingEvent('*', async function bar() {
     return undefined
   })
 
   const commonHandlers = bot.hookHandlers.before_incoming_event['*']
-  expect(commonHandlers?.length).toEqual(1)
+  expect(commonHandlers?.map((h) => h.name)).toEqual(['bar'])
 })
 
 // TODO: add a test to ensure plugin handlers are also returned
