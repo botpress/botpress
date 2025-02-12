59aa38e827f239f415658dd608b40a038be5f906
Abraham Lopez <abraham.lopez@botpress.com>
2025-02-12 12:01:10 -0500

    feat(integrations/google-ai): add Gemini 2.0 Flash model

--- integrations/google-ai/src/schemas.ts
+++ integrations/google-ai/src/schemas.ts
@@ -1,7 +1,12 @@
 import { z } from '@botpress/sdk'
 
 export const languageModelId = z
-  .enum(['models/gemini-1.5-flash-8b-001', 'models/gemini-1.5-flash-002', 'models/gemini-1.5-pro-002'])
+  .enum([
+    'models/gemini-2.0-flash',
+    'models/gemini-1.5-flash-8b-001',
+    'models/gemini-1.5-flash-002',
+    'models/gemini-1.5-pro-002',
+  ])
   .describe('Model to use for content generation')
   .placeholder('models/gemini-1.5-flash-002')
 export type LanguageModelId = z.infer<typeof languageModelId>
