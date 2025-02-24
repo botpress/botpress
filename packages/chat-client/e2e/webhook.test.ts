fa0d3af878a9d441897b12e3d1a75c94f3c27284
François Levasseur <francois_levasseur@hotmail.com>
2025-02-24 16:42:16 -0500

    fix(chat-client): polish error throwing experience (#13655)

--- packages/chat-client/e2e/webhook.test.ts
+++ packages/chat-client/e2e/webhook.test.ts
@@ -8,5 +8,5 @@ const invalidApiUrl = config.get('API_URL') + '1234'
 test('client throws if webhook id is incorrect', async () => {
   const client = new chat.Client({ apiUrl: invalidApiUrl })
   const userPromise = client.createUser({ id: 'invalid' })
-  expect(userPromise).rejects.toThrow(chat.UnknownError)
+  expect(userPromise).rejects.toThrow(chat.ChatClientError)
 })
