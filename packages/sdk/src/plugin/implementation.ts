d590a706beb816053be9850e9af6132cce3bb641
Francois Levasseur <francois.levasseur@botpress.com>
2025-03-04 16:38:20 -0500

    update

--- packages/sdk/src/plugin/implementation.ts
+++ packages/sdk/src/plugin/implementation.ts
@@ -180,12 +180,13 @@ export class PluginImplementation<TPlugin extends BasePlugin = BasePlugin> imple
                 const specificHandlers = hooks[prop2 as string] ?? []
 
                 // for "before_incoming_event", "after_incoming_event" and other event related hooks
-                const interfaceHandlers = Object.entries(hooks)
+                const interfaceHandlers = Object.entries(hooks as Record<string, Function[]>) // TODO: fix typing here
                   .filter(([e]) => this._eventResolvesTo(e, prop2))
                   .flatMap(([, handlers]) => handlers ?? [])
 
                 const globalHandlers = hooks['*'] ?? []
                 const handlers = utils.arrays.unique([...specificHandlers, ...interfaceHandlers, ...globalHandlers])
+
                 return handlers.map((handler) =>
                   utils.functions.setName(
                     (input: any) => handler({ ...input, ...this._getTools(input.client) }),
