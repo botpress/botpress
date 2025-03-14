17675a421d96ab6bd6508fa519dcda3ee89b7a25
Lakshya <57751629+lakshya0246@users.noreply.github.com>
2025-03-14 09:44:11 -0400

    bump version

--- integrations/notion/integration.definition.ts
+++ integrations/notion/integration.definition.ts
@@ -7,7 +7,7 @@ export default new IntegrationDefinition({
   name: 'notion',
   description: 'Import files from Notion to Botpress',
   title: 'Notion',
-  version: '1.0.0',
+  version: '2.0.0',
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration: {
