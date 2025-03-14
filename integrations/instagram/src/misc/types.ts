63077b1a48ab30435cad1710df04058c542ee18c
Alireza Darbandi <56823688+AlirezaDarbandi@users.noreply.github.com>
2025-03-15 02:43:12 +0330

    Revert "fix(integrations/instagram): Fix postbacks (#13711)"
    This reverts commit a74a7e4ce6f6c77877c42264fc0f4f92a1b96698.

--- integrations/instagram/src/misc/types.ts
+++ integrations/instagram/src/misc/types.ts
@@ -1,53 +1,33 @@
-import { z } from '@botpress/sdk'
 import * as bp from '.botpress'
 
-const InstagramMessageBaseSchema = z.object({
-  sender: z.object({ id: z.string() }),
-  recipient: z.object({ id: z.string() }),
-  timestamp: z.number(),
-})
+export type InstagramPayload = {
+  object: string
+  entry: InstagramEntry[]
+}
 
-export const InstagramMessagingEntryMessageSchema = InstagramMessageBaseSchema.extend({
-  message: z.object({
-    mid: z.string(),
-    attachments: z.array(z.object({ type: z.string(), payload: z.object({ url: z.string() }) })).optional(),
-    is_echo: z.boolean().optional(),
-    quick_reply: z.object({ payload: z.string() }).optional(),
-    text: z.string().optional(),
-  }),
-})
+export type InstagramEntry = {
+  id: string
+  time: number
+  messaging: InstagramMessage[]
+}
 
-export const InstagramMessagingEntryPostbackSchema = InstagramMessageBaseSchema.extend({
-  postback: z.object({
-    mid: z.string(),
-    title: z.string(),
-    payload: z.string(),
-  }),
-})
-
-const InstagramMessagingEntryOtherSchema = InstagramMessageBaseSchema
-
-const InstagramMessagingEntrySchema = z.union([
-  InstagramMessagingEntryMessageSchema,
-  InstagramMessagingEntryPostbackSchema,
-  InstagramMessagingEntryOtherSchema,
-])
-
-const InstagramEntrySchema = z.object({
-  id: z.string(),
-  time: z.number(),
-  messaging: z.array(InstagramMessagingEntrySchema),
-})
-
-export const InstagramPayloadSchema = z.object({
-  object: z.string(),
-  entry: z.array(InstagramEntrySchema),
-})
-
-export type InstagramPayload = z.infer<typeof InstagramPayloadSchema>
-export type InstagramMessagingEntry = z.infer<typeof InstagramMessagingEntrySchema>
-export type InstagramMessagingEntryPostback = z.infer<typeof InstagramMessagingEntryPostbackSchema>
-export type InstagramMessagingEntryMessage = z.infer<typeof InstagramMessagingEntryMessageSchema>
+export type InstagramMessage = {
+  sender: { id: string }
+  recipient: { id: string }
+  timestamp: number
+  message?: {
+    mid: string
+    text: string
+    is_echo: boolean
+    quick_reply?: { payload: string }
+    attachments?: { type: string; payload: { url: string } }[]
+  }
+  postback?: {
+    mid: string
+    payload: string
+    title: string
+  }
+}
 
 export type Carousel = bp.channels.channel.carousel.Carousel
 export type Card = bp.channels.channel.card.Card
