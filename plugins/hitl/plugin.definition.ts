26512f6e1d4056900a5e560ce2740a9ad1e4f5e8
Francois Levasseur <francois.levasseur@botpress.com>
2025-03-20 15:16:36 -0400

    update

--- plugins/hitl/plugin.definition.ts
+++ plugins/hitl/plugin.definition.ts
@@ -16,7 +16,7 @@ export const DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE =
 
 export default new sdk.PluginDefinition({
   name: 'hitl',
-  version: '0.4.0',
+  version: '0.4.1',
   title: 'Human In The Loop',
   description: 'Seamlessly transfer conversations to human agents',
   icon: 'icon.svg',
