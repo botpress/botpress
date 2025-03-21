botpress/botpress

commit 2e57baa4228c2598fe7780607d0344f7a6cdc155

fix(cli): ensure plugins are plublished with dependencies (#13632)

packages/cli/src/command-implementations/dev-command.ts
@@ -243,10 +243,9 @@ export class DevCommand extends ProjectCommand<DevCommandDefinition> {
     const line = this.logger.line()
     line.started(`Deploying dev integration ${chalk.bold(integrationDef.name)}...`)
 
-    let createIntegrationBody = await this.prepareCreateIntegrationBody(integrationDef)
-    createIntegrationBody = {
-      ...createIntegrationBody,
-      interfaces: await this.fetchIntegrationInterfaceInstances(integrationDef, api),
+    const createIntegrationBody = {
+      ...(await this.prepareCreateIntegrationBody(integrationDef)),
+      ...(await this.prepareIntegrationDependencies(integrationDef, api)),
       url: externalUrl,
     }
 
@@ -313,14 +312,12 @@ export class DevCommand extends ProjectCommand<DevCommandDefinition> {
     const updateLine = this.logger.line()
     updateLine.started('Deploying dev bot...')
 
-    const integrationInstances = await this.fetchBotIntegrationInstances(botDef, api)
-    const createBotBody = await apiUtils.prepareCreateBotBody(botDef)
     const updateBotBody = apiUtils.prepareUpdateBotBody(
       {
-        ...createBotBody,
+        ...(await apiUtils.prepareCreateBotBody(botDef)),
+        ...(await this.prepareBotDependencies(botDef, api)),
         id: bot.id,
         url: externalUrl,
-        integrations: integrationInstances,
       },
       bot
     )

packages/cli/src/api/client.ts
@@ -82,6 +82,14 @@ export class ApiClient {
     return this.client.updateWorkspace({ id: this.workspaceId, ...props })
   }
 
+  public async getIntegration(ref: ApiPackageRef): Promise<Integration> {
+    const integration = await this.findIntegration(ref)
+    if (!integration) {
+      throw new Error(`Integration "${formatPackageRef(ref)}" not found`)
+    }
+    return integration
+  }
+
   public async findIntegration(ref: ApiPackageRef): Promise<Integration | undefined> {
     const formatted = formatPackageRef(ref)
 
@@ -125,6 +133,14 @@ export class ApiClient {
       .getPublicIntegration(ref)
       .then((r) => r.integration)
       .catch(this._returnUndefinedOnError('ResourceNotFound'))
+  }
+
+  public async getPublicInterface(ref: ApiPackageRef): Promise<Interface> {
+    const intrface = await this.findPublicInterface(ref)
+    if (!intrface) {
+      throw new Error(`Interface "${formatPackageRef(ref)}" not found`)
+    }
+    return intrface
   }
 
   public async findPublicInterface(ref: ApiPackageRef): Promise<Interface | undefined> {

packages/cli/src/command-implementations/deploy-command.ts
@@ -87,10 +87,9 @@ export class DeployCommand extends ProjectCommand<DeployCommandDefinition> {
 
     this.logger.debug('Preparing integration request body...')
 
-    let createBody = await this.prepareCreateIntegrationBody(integrationDef)
-    createBody = {
-      ...createBody,
-      interfaces: await this.fetchIntegrationInterfaceInstances(integrationDef, api),
+    const createBody = {
+      ...(await this.prepareCreateIntegrationBody(integrationDef)),
+      ...(await this.prepareIntegrationDependencies(integrationDef, api)),
       public: this.argv.public,
     }
 
@@ -252,6 +251,7 @@ export class DeployCommand extends ProjectCommand<DeployCommandDefinition> {
 
     const createBody = {
       ...(await apiUtils.prepareCreatePluginBody(pluginDef)),
+      ...(await this.preparePluginDependencies(pluginDef, api)),
       code: {
         node: codeCJS,
         browser: codeESM,
@@ -386,14 +386,12 @@ export class DeployCommand extends ProjectCommand<DeployCommandDefinition> {
     const line = this.logger.line()
     line.started(`Deploying bot ${chalk.bold(bot.name)}...`)
 
-    const integrationInstances = await this.fetchBotIntegrationInstances(botDefinition, api)
-    const createBotBody = await apiUtils.prepareCreateBotBody(botDefinition)
     const updateBotBody = apiUtils.prepareUpdateBotBody(
       {
-        ...createBotBody,
+        ...(await apiUtils.prepareCreateBotBody(botDefinition)),
+        ...(await this.prepareBotDependencies(botDefinition, api)),
         id: bot.id,
         code,
-        integrations: integrationInstances,
       },
       bot
     )

packages/cli/src/command-implementations/project-command.ts
@@ -1,7 +1,6 @@
 import type * as client from '@botpress/client'
 import type * as sdk from '@botpress/sdk'
 import type { YargsConfig } from '@bpinternal/yargs-extra'
-import bluebird from 'bluebird'
 import chalk from 'chalk'
 import fs from 'fs'
 import _ from 'lodash'
@@ -11,7 +10,6 @@ import * as codegen from '../code-generation'
 import type * as config from '../config'
 import * as consts from '../consts'
 import * as errors from '../errors'
-import { formatPackageRef, PackageRef } from '../package-ref'
 import { validateIntegrationDefinition, validateBotDefinition } from '../sdk'
 import type { CommandArgv, CommandDefinition } from '../typings'
 import * as utils from '../utils'
@@ -23,16 +21,6 @@ export type ProjectCache = { botId: string; devId: string }
 type ConfigurableProjectPaths = { workDir: string }
 type ConstantProjectPaths = typeof consts.fromWorkDir
 type AllProjectPaths = ConfigurableProjectPaths & ConstantProjectPaths
-
-type IntegrationInstance = NonNullable<sdk.BotDefinition['integrations']>[string]
-type RemoteIntegrationInstance = utils.types.Merge<IntegrationInstance, { id: string }>
-type LocalIntegrationInstance = utils.types.Merge<IntegrationInstance, { uri?: string }>
-type BotIntegrationRequest = NonNullable<NonNullable<apiUtils.UpdateBotRequestBody['integrations']>[string]>
-
-type InterfaceExtension = NonNullable<sdk.IntegrationDefinition['interfaces']>[string]
-type RemoteInterfaceExtension = utils.types.Merge<InterfaceExtension, { id: string }>
-type LocalInterfaceExtension = utils.types.Merge<InterfaceExtension, { uri?: string }>
-type IntegrationInterfaceRequest = NonNullable<NonNullable<apiUtils.UpdateIntegrationRequestBody['interfaces']>[string]>
 
 type LintIgnoredConfig = { bpLintDisabled?: boolean }
 
@@ -67,99 +55,6 @@ export abstract class ProjectCommand<C extends ProjectCommandDefinition> extends
 
   protected get projectCache() {
     return new utils.cache.FSKeyValueCache<ProjectCache>(this.projectPaths.abs.projectCacheFile)
-  }
-
-  protected async fetchBotIntegrationInstances(
-    botDefinition: sdk.BotDefinition,
-    api: apiUtils.ApiClient
-  ): Promise<Record<string, BotIntegrationRequest>> {
-    const integrationList = _(botDefinition.integrations).values().filter(utils.guards.is.defined).value()
-
-    const { remoteInstances, localInstances } = this._splitApiAndLocalIntegrationInstances(integrationList)
-
-    const fetchedInstances: RemoteIntegrationInstance[] = await bluebird.map(
-      localInstances,
-      async (instance): Promise<RemoteIntegrationInstance> => {
-        const ref: PackageRef = { type: 'name', name: instance.name, version: instance.version }
-        const integration = await api.findIntegration(ref)
-        if (!integration) {
-          const formattedRef = formatPackageRef(ref)
-          throw new errors.BotpressCLIError(`Integration "${formattedRef}" not found`)
-        }
-        return { ...instance, id: integration.id }
-      }
-    )
-
-    return _([...fetchedInstances, ...remoteInstances])
-      .keyBy((i) => i.id)
-      .mapValues(({ enabled, configurationType, configuration }) => ({
-        enabled,
-        configurationType: configurationType ?? null,
-        configuration,
-      }))
-      .value()
-  }
-
-  protected async fetchIntegrationInterfaceInstances(
-    integrationDefinition: sdk.IntegrationDefinition,
-    api: apiUtils.ApiClient
-  ): Promise<Record<string, IntegrationInterfaceRequest>> {
-    const interfaceList = _(integrationDefinition.interfaces).values().filter(utils.guards.is.defined).value()
-
-    const { remoteInstances, localInstances } = this._splitApiAndLocalInterfaceInstances(interfaceList)
-
-    const fetchedInstances: RemoteInterfaceExtension[] = await bluebird.map(
-      localInstances,
-      async (instance): Promise<RemoteInterfaceExtension> => {
-        const ref: PackageRef = { type: 'name', name: instance.name, version: instance.version }
-        const interfaceInstance = await api.findPublicInterface(ref)
-        if (!interfaceInstance) {
-          const formattedRef = formatPackageRef(ref)
-          throw new errors.BotpressCLIError(`Interface "${formattedRef}" not found`)
-        }
-        return { ...instance, id: interfaceInstance.id }
-      }
-    )
-
-    return _([...fetchedInstances, ...remoteInstances])
-      .keyBy((i) => i.id)
-      .value()
-  }
-
-  private _splitApiAndLocalIntegrationInstances(instances: IntegrationInstance[]): {
-    remoteInstances: RemoteIntegrationInstance[]
-    localInstances: LocalIntegrationInstance[]
-  } {
-    const remoteInstances: RemoteIntegrationInstance[] = []
-    const localInstances: LocalIntegrationInstance[] = []
-    for (const instance of instances) {
-      const { id } = instance
-      if (id) {
-        remoteInstances.push({ ...instance, id })
-      } else {
-        localInstances.push(instance)
-      }
-    }
-
-    return { remoteInstances, localInstances }
-  }
-
-  private _splitApiAndLocalInterfaceInstances(instances: InterfaceExtension[]): {
-    remoteInstances: RemoteInterfaceExtension[]
-    localInstances: LocalInterfaceExtension[]
-  } {
-    const remoteInstances: RemoteInterfaceExtension[] = []
-    const localInstances: LocalInterfaceExtension[] = []
-    for (const instance of instances) {
-      const { id } = instance
-      if (id) {
-        remoteInstances.push({ ...instance, id })
-      } else {
-        localInstances.push(instance)
-      }
-    }
-
-    return { remoteInstances, localInstances }
   }
 
   protected async readProjectDefinitionFromFS(): Promise<ProjectDefinition> {
@@ -436,6 +331,62 @@ export abstract class ProjectCommand<C extends ProjectCommandDefinition> extends
           }))
         : undefined,
     }
+  }
+
+  protected async prepareBotDependencies(
+    botDef: sdk.BotDefinition,
+    api: apiUtils.ApiClient
+  ): Promise<Partial<apiUtils.UpdateBotRequestBody>> {
+    const integrations = await this._fetchDependencies(botDef.integrations ?? {}, ({ name, version }) =>
+      api.getIntegration({ type: 'name', name, version })
+    )
+    return {
+      integrations: _(integrations)
+        .keyBy((i) => i.id)
+        .value(),
+    }
+  }
+
+  protected async prepareIntegrationDependencies(
+    integrationDef: sdk.IntegrationDefinition,
+    api: apiUtils.ApiClient
+  ): Promise<Partial<apiUtils.CreateIntegrationRequestBody>> {
+    const interfaces = await this._fetchDependencies(integrationDef.interfaces ?? {}, ({ name, version }) =>
+      api.getPublicInterface({ type: 'name', name, version })
+    )
+    return { interfaces }
+  }
+
+  protected async preparePluginDependencies(
+    pluginDef: sdk.PluginDefinition,
+    api: apiUtils.ApiClient
+  ): Promise<Partial<apiUtils.CreatePluginRequestBody>> {
+    const integrations = await this._fetchDependencies(pluginDef.integrations ?? {}, ({ name, version }) =>
+      api.getIntegration({ type: 'name', name, version })
+    )
+    const interfaces = await this._fetchDependencies(pluginDef.interfaces ?? {}, ({ name, version }) =>
+      api.getPublicInterface({ type: 'name', name, version })
+    )
+    return {
+      dependencies: {
+        integrations,
+        interfaces,
+      },
+    }
+  }
+
+  private _fetchDependencies = async <T extends { id?: string; name: string; version: string }>(
+    deps: Record<string, T>,
+    fetcher: (dep: T) => Promise<{ id: string }>
+  ): Promise<Record<string, T & { id: string }>> => {
+    const isRemote = (dep: T): dep is T & { id: string } => !!dep.id
+    return utils.records.mapValuesAsync(deps, async (dep): Promise<T & { id: string }> => {
+      if (isRemote(dep)) {
+        return dep
+      }
+      const { id } = await fetcher(dep)
+      return { id, ...dep }
+    })
   }
 
   protected readProjectFile = async (

packages/cli/src/utils/record-utils.test.ts
@@ -16,3 +16,13 @@ test('zip objects should return both values only when key is defined in both obj
 
   expect(zipped).toEqual(expected)
 })
+
+test('mapValuesAsync should return a new object with the values mapped asynchronously', async () => {
+  const obj = { a: 1, b: 2, c: 3 }
+
+  const mapped = await recordUtils.mapValuesAsync(obj, async (v) => v * 2)
+
+  const expected = { a: 2, b: 4, c: 6 }
+
+  expect(mapped).toEqual(expected)
+})

packages/cli/src/utils/record-utils.ts
@@ -1,3 +1,4 @@
+import bluebird from 'bluebird'
 import _ from 'lodash'
 
 export const setNullOnMissingValues = <A, B>(
@@ -47,13 +48,14 @@ export const mapValuesAsync = async <A, B>(
   record: Record<string, A>,
   fn: (value: A, key: string) => Promise<B>
 ): Promise<Record<string, B>> => {
-  const newRecord: Record<string, B> = {}
+  const entries: [string, A][] = Object.entries(record)
 
-  for (const [key, value] of Object.entries(record)) {
-    newRecord[key] = await fn(value, key)
-  }
+  const newEntries = await bluebird.map(
+    entries,
+    async ([key, value]): Promise<[string, B]> => [key, await fn(value, key)]
+  )
 
-  return newRecord
+  return Object.fromEntries(newEntries)
 }
 
 export const mapKeys = <A>(record: Record<string, A>, fn: (value: A, key: string) => string): Record<string, A> => {

commit 1e4d273e66d9a3b8bb505048afb6ecb82f6ec8e2

fix(cli): fix dev bot deploy message
commit 2e57baa4228c2598fe7780607d0344f7a6cdc155 broke logging for
the dev command when the bot has dependencies, because it now
asks the client to fetch the dependencies and the client prints
log lines when it finds them, so it breaks the `updateLine`
single-line logger and the message never gets printed.

packages/cli/src/command-implementations/dev-command.ts
@@ -309,8 +309,7 @@ export class DevCommand extends ProjectCommand<DevCommandDefinition> {
       await this.projectCache.set('devId', bot.id)
     }
 
-    const updateLine = this.logger.line()
-    updateLine.started('Deploying dev bot...')
+    this.logger.started('Deploying dev bot...')
 
     const updateBotBody = apiUtils.prepareUpdateBotBody(
       {
@@ -325,8 +324,7 @@ export class DevCommand extends ProjectCommand<DevCommandDefinition> {
     const { bot: updatedBot } = await api.client.updateBot(updateBotBody).catch((thrown) => {
       throw errors.BotpressCLIError.wrap(thrown, 'Could not deploy dev bot')
     })
-    updateLine.success(`Dev Bot deployed with id "${updatedBot.id}" at "${externalUrl}"`)
-    updateLine.commit()
+    this.logger.success(`Dev Bot deployed with id "${updatedBot.id}" at "${externalUrl}"`)
 
     const tablesPublisher = new tables.TablesPublisher({ api, logger: this.logger, prompt: this.prompt })
     await tablesPublisher.deployTables({ botId: updatedBot.id, botDefinition: botDef })