ddd30ff69a4ee686ba7e457e5fe664eef62be106
François Levasseur <francois_levasseur@hotmail.com>
2025-03-24 13:54:31 -0400

    fix(plugins/hitl): disable hitl in studio (#13728)

--- plugins/hitl/src/index.ts
+++ plugins/hitl/src/index.ts
@@ -1,35 +1,66 @@
+import * as sdk from '@botpress/sdk'
+import { isBrowser } from 'browser-or-node'
 import * as actions from './actions'
 import * as hooks from './hooks'
 import * as bp from '.botpress'
 
 const plugin = new bp.Plugin({
   actions: {
-    startHitl: actions.startHitl,
-    stopHitl: actions.stopHitl,
+    startHitl: async (props) => {
+      if (isBrowser) {
+        throw new sdk.RuntimeError('HITL is not supported in the browser')
+      }
+      return await actions.startHitl(props)
+    },
+    stopHitl: async (props) => {
+      if (isBrowser) {
+        throw new sdk.RuntimeError('HITL is not supported in the browser')
+      }
+      return await actions.stopHitl(props)
+    },
   },
 })
 
 plugin.on.beforeIncomingMessage('*', async (props) => {
+  if (isBrowser) {
+    props.logger.warn('HITL is not supported in the browser')
+    return
+  }
   props.logger.info('Before incoming message', props.data.payload)
   return await hooks.beforeIncomingMessage.all.handleMessage(props)
 })
 
 plugin.on.beforeIncomingEvent('hitl:hitlAssigned', async (props) => {
+  if (isBrowser) {
+    props.logger.warn('HITL is not supported in the browser')
+    return
+  }
   props.logger.info('HITL assigned', props.data.payload)
   return await hooks.beforeIncomingEvent.hitlAssigned.handleEvent(props)
 })
 
 plugin.on.beforeIncomingEvent('hitl:hitlStopped', async (props) => {
+  if (isBrowser) {
+    props.logger.warn('HITL is not supported in the browser')
+    return
+  }
   props.logger.info('HITL stopped', props.data.payload)
   return await hooks.beforeIncomingEvent.hitlStopped.handleEvent(props)
 })
 
 plugin.on.beforeIncomingEvent('humanAgentAssignedTimeout', async (props) => {
+  if (isBrowser) {
+    props.logger.warn('HITL is not supported in the browser')
+    return
+  }
   props.logger.info('HITL agent assigned timeout', props.data.payload)
   return await hooks.beforeIncomingEvent.humanAgentAssignedTimeout.handleEvent(props)
 })
 
 plugin.on.beforeIncomingEvent('*', async (props) => {
+  if (isBrowser) {
+    return
+  }
   props.logger.info('Before incoming event', props.data.payload)
   return await hooks.beforeIncomingEvent.all.handleEvent(props)
 })
