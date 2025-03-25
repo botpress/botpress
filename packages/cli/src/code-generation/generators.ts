6e88a0d2df1e13d19bcc7012feecb8f5fb741c38
Pascal Breton <pascal.breton@botpress.com>
2025-03-25 16:30:27 -0400

    feat(cli): add code generation for recurring events

--- packages/cli/src/code-generation/generators.ts
+++ packages/cli/src/code-generation/generators.ts
@@ -34,9 +34,7 @@ export const jsonSchemaToTypescriptZuiSchema = async (
   let code = [
     consts.GENERATED_HEADER,
     'import { z } from "@botpress/sdk"',
-    `export const ${name} = {`,
-    ...Object.entries(allProps).map(([key, value]) => `  ${key}: ${value},`),
-    '}',
+    `export const ${name} = ${typescriptValuesToRecordString(allProps)}`,
   ].join('\n')
   code = await prettier.format(code, { parser: 'typescript' })
   return code
@@ -61,3 +59,9 @@ export function primitiveRecordToTypescriptValues(x: Record<string, Primitive>):
     .fromPairs()
     .value()
 }
+
+export const primitiveRecordToRecordString = (record: Record<string, Primitive>): string =>
+  typescriptValuesToRecordString(primitiveRecordToTypescriptValues(record))
+
+export const typescriptValuesToRecordString = (record: Record<string, string>): string =>
+  ['{', ...Object.entries(record).map(([key, value]) => `  ${key}: ${value},`), '}'].join('\n')
