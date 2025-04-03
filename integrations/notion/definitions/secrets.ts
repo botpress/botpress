3f7f7344decf02ba0a5af687adfef1a6e36d6644
Pascal B <pascal.breton@botpress.com>
2025-04-03 10:48:14 -0400

    chore(notion): add sentry (#13759)

--- integrations/notion/definitions/secrets.ts
+++ integrations/notion/definitions/secrets.ts
@@ -1,6 +1,8 @@
 import * as sdk from '@botpress/sdk'
+import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 
 export const secrets = {
+  ...sentryHelpers.COMMON_SECRET_NAMES,
   CLIENT_ID: {
     description: 'The client ID of the Botpress Notion Integration.',
   },
