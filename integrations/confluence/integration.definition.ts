940a2ac7d73be51f18e1cd59bcf31446299b7da9
michaelmass <10071388+michaelmass@users.noreply.github.com>
2025-04-11 14:06:46 -0400

    update

--- integrations/confluence/integration.definition.ts
+++ integrations/confluence/integration.definition.ts
@@ -2,10 +2,9 @@ import { IntegrationDefinition, z } from '@botpress/sdk'
 import filesReadonly from './bp_modules/files-readonly'
 
 import { actions, channels } from './definitions'
-import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
-  name: integrationName,
+  name: 'confluence',
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
