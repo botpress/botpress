f8c9134afe83d3531734fece0bc6c072657a2fd0
Pascal B <pascal.breton@botpress.com>
2025-04-15 14:49:39 -0400

    fix(zendesk): reuse hitl users (#13790)

--- plugins/hitl/plugin.definition.ts
+++ plugins/hitl/plugin.definition.ts
@@ -16,7 +16,7 @@ export const DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE =
 
 export default new sdk.PluginDefinition({
   name: 'hitl',
-  version: '0.7.1',
+  version: '0.7.2',
   title: 'Human In The Loop',
   description: 'Seamlessly transfer conversations to human agents',
   icon: 'icon.svg',
