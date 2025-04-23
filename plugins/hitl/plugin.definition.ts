502d18d0665420e82970d746591c287f6f13ab8c
Pascal B <pascal.breton@botpress.com>
2025-04-23 16:46:22 -0400

    fix(hitl): only check end command for text payloads (#13805)

--- plugins/hitl/plugin.definition.ts
+++ plugins/hitl/plugin.definition.ts
@@ -16,7 +16,7 @@ export const DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE =
 
 export default new sdk.PluginDefinition({
   name: 'hitl',
-  version: '0.7.2',
+  version: '0.7.3',
   title: 'Human In The Loop',
   description: 'Seamlessly transfer conversations to human agents',
   icon: 'icon.svg',
