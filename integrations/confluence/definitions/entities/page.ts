a0026e5da5b4e8628d52e6ca44f3246069a63541
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2025-04-25 17:30:07 -0400

    chore(confluence): update confluence version to 3.1.0 (#13808)

--- integrations/confluence/definitions/entities/page.ts
+++ integrations/confluence/definitions/entities/page.ts
@@ -5,11 +5,12 @@ const { z } = sdk
 export namespace Page {
   const _fields = {
     id: z.string().title('ID').describe('The ID of the page.'),
-    title: z.string().title('Title').describe('The title of the page.'),
+    title: z.string().title('Title').describe('The title of the page.').optional(),
     status: z
       .enum(['current', 'draft', 'archived', 'historical', 'trashed', 'any', 'deleted'])
       .title('Status')
-      .describe('The status of the page.'),
+      .describe('The status of the page.')
+      .optional(),
     spaceId: z.string().title('Space ID').describe('The ID of the space the page is in.'),
     parentId: z
       .string()
@@ -26,33 +27,48 @@ export namespace Page {
       .title('Position')
       .describe('Position of child page within the given parent page tree.')
       .nullable(),
-    authorId: z.string().title('Author ID').describe('The account ID of the user who created this page originally.'),
-    ownerId: z.string().title('Owner ID').describe('The account ID of the user who owns this page.').nullable(),
+    authorId: z
+      .string()
+      .title('Author ID')
+      .describe('The account ID of the user who created this page originally.')
+      .optional(),
+    ownerId: z
+      .string()
+      .title('Owner ID')
+      .describe('The account ID of the user who owns this page.')
+      .nullable()
+      .optional(),
     lastOwnerId: z
       .string()
       .title('Last Owner ID')
       .describe('The account ID of the user who last edited this page.')
-      .nullable(),
+      .nullable()
+      .optional(),
     createdAt: z
       .string()
       .title('Created At')
-      .describe("Date and time when the page was created. In format 'YYYY-MM-DDTHH:mm:ss.sssZ'."),
-    body: z.object({
-      atlas_doc_format: z.object({
-        value: z.string().title('Value').describe('The content of the page.'),
-        representation: z.string().title('Representation').describe('The representation of the content.'),
-      }),
-    }),
-    version: z.object({
-      createdAt: z.string().title('Created At').describe('The creation date of the version.'),
-      number: z.number().title('number').describe('Represents the version number.'),
-      authorId: z.string().title('Author ID').describe('The account ID of the user who created this version.'),
-      minorEdit: z.boolean().title('Minor Edit').describe('Indicates whether this version is a minor edit or not.'),
-    }),
+      .describe("Date and time when the page was created. In format 'YYYY-MM-DDTHH:mm:ss.sssZ'.")
+      .optional(),
+    body: z
+      .object({
+        atlas_doc_format: z.object({
+          value: z.string().title('Value').describe('The content of the page.'),
+          representation: z.string().title('Representation').describe('The representation of the content.'),
+        }),
+      })
+      .optional(),
+    version: z
+      .object({
+        createdAt: z.string().title('Created At').describe('The creation date of the version.'),
+        number: z.number().title('number').describe('Represents the version number.'),
+        authorId: z.string().title('Author ID').describe('The account ID of the user who created this version.'),
+        minorEdit: z.boolean().title('Minor Edit').describe('Indicates whether this version is a minor edit or not.'),
+      })
+      .optional(),
     _links: z.object({
-      webui: z.string().title('Web UI').describe('Web UI link of the content.'),
-      editui: z.string().title('Self').describe('Edit UI link of the content.'),
-      tinyui: z.string().title('Self').describe('Web UI link of the content.'),
+      webui: z.string().title('Web UI').describe('Web UI link of the content.').optional(),
+      editui: z.string().title('Edit UI').describe('Edit UI link of the content.').optional(),
+      tinyui: z.string().title('Web UI').describe('Web UI link of the content.').optional(),
     }),
   } as const
 
--- integrations/confluence/integration.definition.ts
+++ integrations/confluence/integration.definition.ts
@@ -1,11 +1,15 @@
 import { IntegrationDefinition, z } from '@botpress/sdk'
-import filesReadonly from './bp_modules/files-readonly'
 
-import { actions } from './definitions'
+import creatable from './bp_modules/creatable'
+import deletable from './bp_modules/deletable'
+import filesReadonly from './bp_modules/files-readonly'
+import readable from './bp_modules/readable'
+import updatable from './bp_modules/updatable'
+import { entities } from './definitions'
 
 export default new IntegrationDefinition({
   name: 'confluence',
-  version: '2.1.0',
+  version: '3.1.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   title: 'Confluence',
@@ -15,13 +19,58 @@ export default new IntegrationDefinition({
       host: z.string().describe('Host URI. Format is https://your_workspace_name.atlassian.net').title('Host'),
       user: z.string().describe('Email of the user').title('User Email'),
       apiToken: z.string().describe('API Token').title('API Token'),
+      webhookUrl: z.string().describe('The url to post the bot answers to.').title('Webhook URL'),
     }),
   },
-  actions,
-}).extend(filesReadonly, ({}) => ({
-  entities: {},
-  actions: {
-    listItemsInFolder: { name: 'filesReadonlyListItemsInFolder' },
-    transferFileToBotpress: { name: 'filesReadonlyTransferFileToBotpress' },
+  actions: {},
+  entities,
+  channels: {
+    comment: {
+      title: 'Comments',
+      description: 'Comments on pages',
+      conversation: {
+        tags: {
+          id: { title: 'Conversation ID', description: 'The ID of the conversation in confluence' },
+        },
+      },
+      messages: {
+        text: {
+          schema: z.object({
+            text: z.string(),
+          }),
+        },
+      },
+    },
   },
-}))
+  user: {
+    tags: {
+      id: { title: 'User ID', description: 'The ID of the user in confluence' },
+    },
+  },
+})
+  .extend(readable, ({ entities }) => ({
+    entities: { item: entities.page },
+    actions: { read: { name: 'getPage' } },
+  }))
+  .extend(creatable, ({ entities }) => ({
+    entities: { item: entities.page },
+    actions: { create: { name: 'createPage' } },
+    events: { created: { name: 'pageCreated' } },
+  }))
+  .extend(updatable, ({ entities }) => ({
+    entities: { item: entities.page },
+    actions: { update: { name: 'updatePage' } },
+    events: { updated: { name: 'pageUpdated' } },
+  }))
+  .extend(deletable, ({ entities }) => ({
+    entities: { item: entities.page },
+    actions: { delete: { name: 'deletePage' } },
+    events: { deleted: { name: 'pageDeleted' } },
+  }))
+  .extend(filesReadonly, ({}) => ({
+    entities: {},
+    actions: {
+      listItemsInFolder: { name: 'filesReadonlyListItemsInFolder' },
+      transferFileToBotpress: { name: 'filesReadonlyTransferFileToBotpress' },
+    },
+  }))
