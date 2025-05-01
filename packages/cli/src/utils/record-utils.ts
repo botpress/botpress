d57e539852f3b8133f85fbb1dfbcd2a6090d602a
Pascal Breton <pascal.breton@botpress.com>
2025-05-01 15:38:58 -0400

    revert changes to record-utils

--- packages/cli/src/utils/record-utils.ts
+++ packages/cli/src/utils/record-utils.ts
@@ -5,7 +5,11 @@ export const setNullOnMissingValues = <A, B>(
   record: Record<string, A> = {},
   oldRecord: Record<string, B> = {}
 ): Record<string, A | null> => {
-  const newRecord: Record<string, A | null> = structuredClone(record)
+  const newRecord: Record<string, A | null> = {}
+
+  for (const [key, value] of Object.entries(record)) {
+    newRecord[key] = value
+  }
 
   for (const value of Object.keys(oldRecord)) {
     if (!record[value]) {
