6c5e49503368fa7511bfafb921f5edc7fe644d0d
Pascal Breton <pascal.breton@botpress.com>
2025-05-05 17:06:20 -0400

    feat(hitl): allow to override configuration for individual sessions

--- plugins/hitl/plugin.definition.ts
+++ plugins/hitl/plugin.definition.ts
@@ -14,84 +14,84 @@ export const DEFAULT_USER_HITL_COMMAND_MESSAGE =
 export const DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE =
   'No human agent is available at the moment. Please try again later. I will continue assisting you for the time being.'
 
+const PLUGIN_CONFIG_SCHEMA = sdk.z.object({
+  onHitlHandoffMessage: sdk.z
+    .string()
+    .title('Escalation Started Message')
+    .describe('The message to send to the user when transferring to a human agent')
+    .optional()
+    .placeholder(DEFAULT_HITL_HANDOFF_MESSAGE),
+  onHumanAgentAssignedMessage: sdk.z
+    .string()
+    .title('Human Agent Assigned Message')
+    .describe('The message to send to the user when a human agent is assigned')
+    .optional()
+    .placeholder(DEFAULT_HUMAN_AGENT_ASSIGNED_MESSAGE),
+  onHitlStoppedMessage: sdk.z
+    .string()
+    .title('Escalation Terminated Message')
+    .describe('The message to send to the user when the HITL session stops and control is transferred back to bot')
+    .optional()
+    .placeholder(DEFAULT_HITL_STOPPED_MESSAGE),
+  onUserHitlCancelledMessage: sdk.z
+    .string()
+    .title('Escalation Aborted Message')
+    .describe('The message to send to the human agent when the user abruptly ends the HITL session')
+    .optional()
+    .placeholder(DEFAULT_USER_HITL_CANCELLED_MESSAGE),
+  onIncompatibleMsgTypeMessage: sdk.z
+    .string()
+    .title('Incompatible Message Type Warning')
+    .describe(
+      'The warning to send to the human agent when they send a message that is not supported by the hitl session'
+    )
+    .optional()
+    .placeholder(DEFAULT_INCOMPATIBLE_MSGTYPE_MESSAGE),
+  userHitlCloseCommand: sdk.z
+    .string()
+    .title('Termination Command')
+    .describe(
+      'Users may send this command to end the HITL session at any time. It is case-insensitive, so it works regardless of letter casing.'
+    )
+    .optional()
+    .placeholder(DEFAULT_USER_HITL_CLOSE_COMMAND),
+  onUserHitlCloseMessage: sdk.z
+    .string()
+    .title('Termination Command Message')
+    .describe('The message to send to the user when they end the HITL session using the termination command')
+    .optional()
+    .placeholder(DEFAULT_USER_HITL_COMMAND_MESSAGE),
+  onAgentAssignedTimeoutMessage: sdk.z
+    .string()
+    .title('Agent Assigned Timeout Message')
+    .describe('The message to send to the user when no human agent is assigned within the timeout period')
+    .optional()
+    .placeholder(DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE),
+  agentAssignedTimeoutSeconds: sdk.z
+    .number()
+    .title('Agent Assigned Timeout')
+    .describe(
+      'The time in seconds to wait before giving up and telling the user that no human agent is available. Set to 0 to disable'
+    )
+    .nonnegative()
+    .optional()
+    .placeholder('0'),
+  flowOnHitlStopped: sdk.z
+    .boolean()
+    .default(true)
+    .title('Continue Flow on Session End?')
+    .describe('Enable this to continue the flow when the HITL session ends. Otherwise, the flow waits for user input.'),
+})
+
 export default new sdk.PluginDefinition({
   name: 'hitl',
-  version: '0.8.0',
+  version: '0.9.0',
   title: 'Human In The Loop',
   description: 'Seamlessly transfer conversations to human agents',
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration: {
-    schema: sdk.z.object({
-      onHitlHandoffMessage: sdk.z
-        .string()
-        .title('Escalation Started Message')
-        .describe('The message to send to the user when transferring to a human agent')
-        .optional()
-        .placeholder(DEFAULT_HITL_HANDOFF_MESSAGE),
-      onHumanAgentAssignedMessage: sdk.z
-        .string()
-        .title('Human Agent Assigned Message')
-        .describe('The message to send to the user when a human agent is assigned')
-        .optional()
-        .placeholder(DEFAULT_HUMAN_AGENT_ASSIGNED_MESSAGE),
-      onHitlStoppedMessage: sdk.z
-        .string()
-        .title('Escalation Terminated Message')
-        .describe('The message to send to the user when the HITL session stops and control is transferred back to bot')
-        .optional()
-        .placeholder(DEFAULT_HITL_STOPPED_MESSAGE),
-      onUserHitlCancelledMessage: sdk.z
-        .string()
-        .title('Escalation Aborted Message')
-        .describe('The message to send to the human agent when the user abruptly ends the HITL session')
-        .optional()
-        .placeholder(DEFAULT_USER_HITL_CANCELLED_MESSAGE),
-      onIncompatibleMsgTypeMessage: sdk.z
-        .string()
-        .title('Incompatible Message Type Warning')
-        .describe(
-          'The warning to send to the human agent when they send a message that is not supported by the hitl session'
-        )
-        .optional()
-        .placeholder(DEFAULT_INCOMPATIBLE_MSGTYPE_MESSAGE),
-      userHitlCloseCommand: sdk.z
-        .string()
-        .title('Termination Command')
-        .describe(
-          'Users may send this command to end the HITL session at any time. It is case-insensitive, so it works regardless of letter casing.'
-        )
-        .optional()
-        .placeholder(DEFAULT_USER_HITL_CLOSE_COMMAND),
-      onUserHitlCloseMessage: sdk.z
-        .string()
-        .title('Termination Command Message')
-        .describe('The message to send to the user when they end the HITL session using the termination command')
-        .optional()
-        .placeholder(DEFAULT_USER_HITL_COMMAND_MESSAGE),
-      onAgentAssignedTimeoutMessage: sdk.z
-        .string()
-        .title('Agent Assigned Timeout Message')
-        .describe('The message to send to the user when no human agent is assigned within the timeout period')
-        .optional()
-        .placeholder(DEFAULT_AGENT_ASSIGNED_TIMEOUT_MESSAGE),
-      agentAssignedTimeoutSeconds: sdk.z
-        .number()
-        .title('Agent Assigned Timeout')
-        .describe(
-          'The time in seconds to wait before giving up and telling the user that no human agent is available. Set to 0 to disable'
-        )
-        .nonnegative()
-        .optional()
-        .placeholder('0'),
-      flowOnHitlStopped: sdk.z
-        .boolean()
-        .default(true)
-        .title('Continue Flow on Session End?')
-        .describe(
-          'Enable this to continue the flow when the HITL session ends. Otherwise, the flow waits for user input.'
-        ),
-    }),
+    schema: PLUGIN_CONFIG_SCHEMA,
   },
   actions: {
     startHitl: {
@@ -118,6 +118,10 @@ export default new sdk.PluginDefinition({
             .title('Conversation ID') // this is the upstream conversation
             .describe('ID of the conversation on which to start the HITL mode')
             .placeholder('{{ event.conversationId }}'),
+          configurationOverrides: PLUGIN_CONFIG_SCHEMA.partial()
+            .optional()
+            .title('Configuration Overrides')
+            .describe('Use this to override the global configuration for this specific HITL session'),
         }),
       },
       output: { schema: sdk.z.object({}) },
@@ -142,6 +146,10 @@ export default new sdk.PluginDefinition({
       schema: sdk.z.object({
         hitlActive: sdk.z.boolean().title('Is HITL Enabled?').describe('Whether the conversation is in HITL mode'),
       }),
+    },
+    effectiveSessionConfig: {
+      type: 'conversation',
+      schema: PLUGIN_CONFIG_SCHEMA,
     },
   },
   user: {
