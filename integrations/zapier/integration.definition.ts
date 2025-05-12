c7e1fe23d831cc9052cf27b96d919b5fdb72cefe
Francois Levasseur <francois.levasseur@botpress.com>
2025-05-12 16:52:59 -0400

    update

--- integrations/zapier/integration.definition.ts
+++ integrations/zapier/integration.definition.ts
@@ -5,7 +5,7 @@ import { TriggerSchema, EventSchema, ZapierTriggersStateName, ZapierTriggersStat
 
 export default new IntegrationDefinition({
   name: 'zapier',
-  version: '0.3.1',
+  version: '0.3.5',
   title: 'Zapier',
   description:
     "Trigger workflows from Zapier or let Zapier trigger your workflows to automate tasks and enhance your bot's capabilities.",
