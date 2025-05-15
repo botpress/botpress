c9a2b00e10cf33ed71ac9a6d4dc90470b8f44633
Pascal B <pascal.breton@botpress.com>
2025-05-15 11:14:05 -0400

    fix(file-synchronizer): ignore max size when set to 0 (#13861)

--- plugins/file-synchronizer/plugin.definition.ts
+++ plugins/file-synchronizer/plugin.definition.ts
@@ -1,70 +1,62 @@
 import * as sdk from '@botpress/sdk'
 import filesReadonly from './bp_modules/files-readonly'
 
-const FILE_FILTER_PROPS = {
-  includeFiles: sdk.z
-    .array(
-      sdk.z
-        .object({
-          pathGlobPattern: sdk.z
-            .string()
-            .describe(
-              'A glob pattern to match against the file path. Only files that match the pattern will be synchronized. Any pattern supported by picomatch is supported.'
-            ),
-          maxSizeInBytes: sdk.z
-            .number()
-            .optional()
-            .describe(
-              'Filter by maximum size (in bytes). Only files smaller than the specified size will be synchronized.'
-            ),
-          modifiedAfter: sdk.z
-            .string()
-            .datetime()
-            .optional()
-            .describe(
-              'Filter the items by modified date. Only files modified after the specified date will be synchronized.'
-            ),
-          applyOptionsToMatchedFiles: sdk.z
-            .object({
-              addToKbId: sdk.z
-                .string()
-                .optional()
-                .title('Knowledge Base ID')
-                .describe(
-                  'The ID of the knowledge base to add the file to. Note that files added to knowledge bases will count towards both the Vector DB Storage quota and the File Storage quota of the workspace.'
-                ),
-            })
-            .optional()
-            .title('Apply to Matched Files')
-            .describe('Options to apply to the matched files.'),
-        })
-        .title('Include Criteria')
-        .describe('A file must match all criteria to be synchronized.')
-    )
-    .title('Include Rules')
-    .describe('A list of rules to include files. Only files that match one or more rules will be synchronized.'),
-  excludeFiles: sdk.z
-    .array(
-      sdk.z
-        .object({
-          pathGlobPattern: sdk.z
-            .string()
-            .describe(
-              'A glob pattern to match against the file path. Files that match the pattern will be ignored, even if they match the includeFiles configuration.'
-            ),
-        })
-        .title('Exclude Criteria')
-        .describe('A file must match all exclude criteria to be ignored.')
-    )
-    .title('Exclude Rules')
-    .describe(
-      'A list of rules to exclude files. Files that match one or more rules will be ignored. This takes precedence over Include Rules.'
-    ),
-} as const
+const FILE_FILTER_PROPS = sdk.z.object({
+  includeFiles: sdk.z.array(
+    sdk.z
+      .object({
+        pathGlobPattern: sdk.z
+          .string()
+          .describe(
+            'A glob pattern to match against the file path. Only files that match the pattern will be synchronized. Any pattern supported by picomatch is supported.'
+          ),
+        maxSizeInBytes: sdk.z
+          .number()
+          .optional()
+          .describe(
+            'Filter by maximum size (in bytes). Only files smaller than the specified size will be synchronized.'
+          ),
+        modifiedAfter: sdk.z
+          .string()
+          .datetime()
+          .optional()
+          .describe(
+            'Filter the items by modified date. Only files modified after the specified date will be synchronized.'
+          ),
+        applyOptionsToMatchedFiles: sdk.z
+          .object({
+            addToKbId: sdk.z
+              .string()
+              .optional()
+              .title('Knowledge Base ID')
+              .describe(
+                'The ID of the knowledge base to add the file to. Note that files added to knowledge bases will count towards both the Vector DB Storage quota and the File Storage quota of the workspace.'
+              ),
+          })
+          .optional()
+          .title('Apply to Matched Files')
+          .describe('Options to apply to the matched files.'),
+      })
+      .title('Include Criteria')
+      .describe('A file must match all criteria to be synchronized.')
+  ),
+  excludeFiles: sdk.z.array(
+    sdk.z
+      .object({
+        pathGlobPattern: sdk.z
+          .string()
+          .describe(
+            'A glob pattern to match against the file path. Files that match the pattern will be ignored, even if they match the includeFiles configuration.'
+          ),
+      })
+      .title('Exclude Criteria')
+      .describe('A file must match all exclude criteria to be ignored.')
+  ),
+})
 
 export default new sdk.PluginDefinition({
   name: 'file-synchronizer',
-  version: '0.6.0',
+  version: '0.6.1',
   title: 'File Synchronizer',
   description: 'Synchronize files from external services to Botpress',
   icon: 'icon.svg',
@@ -77,7 +69,14 @@ export default new sdk.PluginDefinition({
         .describe(
           'Enable real-time synchronization. Whever a file is created, updated, or deleted, synchronize it to Botpress immediately. This does not work with every integration.'
         ),
-      ...FILE_FILTER_PROPS,
+      includeFiles: FILE_FILTER_PROPS.shape.includeFiles
+        .title('Include Rules')
+        .describe('A list of rules to include files. Only files that match one or more rules will be synchronized.'),
+      excludeFiles: FILE_FILTER_PROPS.shape.excludeFiles
+        .title('Exclude Rules')
+        .describe(
+          'A list of rules to exclude files. Files that match one or more rules will be ignored. This takes precedence over Include Rules.'
+        ),
     }),
   },
   actions: {
@@ -86,11 +85,11 @@ export default new sdk.PluginDefinition({
       description: 'Start synchronization of files from the external service to Botpress',
       input: {
         schema: sdk.z.object({
-          includeFiles: FILE_FILTER_PROPS.includeFiles
+          includeFiles: FILE_FILTER_PROPS.shape.includeFiles
             .title('Include Rules Override')
             .describe('If omitted, the global Include Rules will be used.')
             .optional(),
-          excludeFiles: FILE_FILTER_PROPS.excludeFiles
+          excludeFiles: FILE_FILTER_PROPS.shape.excludeFiles
             .title('Exclude Rules Override')
             .describe('If omitted, the global Exclude Rules will be used')
             .optional(),
