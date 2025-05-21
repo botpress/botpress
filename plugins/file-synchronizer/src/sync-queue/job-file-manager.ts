1376ab9c045f8d25ffa90b2683cc583fa1c21209
Pascal B <pascal.breton@botpress.com>
2025-05-21 14:41:04 -0400

    fix(file-synchronizer): use a workflow to enumerate files (#13870)

--- plugins/file-synchronizer/src/sync-queue/job-file-manager.ts
+++ plugins/file-synchronizer/src/sync-queue/job-file-manager.ts
@@ -12,9 +12,10 @@ const QUEUE_ITEM = models.FILE_WITH_PATH.extend({
 })
 
 export const getSyncQueue = async (
-  props: bp.WorkflowHandlerProps['processQueue']
+  props: bp.WorkflowHandlerProps['processQueue'] | bp.WorkflowHandlerProps['buildQueue'],
+  jobFileId?: string
 ): Promise<{ syncQueue: types.SyncQueue; key: string }> => {
-  const { jobFileContent, key } = await _retrieveJobFile(props).catch(async (thrown: unknown) => {
+  const { jobFileContent, key } = await _retrieveJobFile(props, jobFileId).catch(async (thrown: unknown) => {
     const err: Error = thrown instanceof Error ? thrown : new Error(String(thrown))
     await props.workflow.setFailed({ failureReason: `Failed to retrieve job file: ${err.message}` })
     throw new Error(`Failed to retrieve job file: ${thrown}`)
@@ -36,9 +37,12 @@ export const getSyncQueue = async (
 }
 
 const _retrieveJobFile = async (
-  props: bp.WorkflowHandlerProps['processQueue']
+  props: bp.WorkflowHandlerProps['processQueue'] | bp.WorkflowHandlerProps['buildQueue'],
+  jobFileId?: string
 ): Promise<{ jobFileContent: string; key: string }> => {
-  const { file: jobFile } = await props.client.getFile({ id: props.workflow.input.jobFileId })
+  const { file: jobFile } = await props.client.getFile({
+    id: props.workflow.input.jobFileId ?? jobFileId,
+  })
   const jobFileContent = await fetch(jobFile.url).then((res) => res.text())
 
   return { jobFileContent, key: jobFile.key }
@@ -52,7 +56,7 @@ export const updateSyncQueue = async (
 ): Promise<string> => {
   const { file: jobFile } = await props.client.uploadFile({
     key,
-    content: syncQueue.map((item) => JSON.stringify(item)).join('\n'),
+    content: syncQueue.map((item) => JSON.stringify(item)).join('\n') + '\n',
     tags,
   })
 
