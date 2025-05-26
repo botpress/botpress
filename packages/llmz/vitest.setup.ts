5a521005066149c92e0117bbf8a9eed511964cf4
Sylvain Perron <sylvain.perron@botpress.com>
2025-05-26 15:17:07 -0400

    install openai if missing

--- packages/llmz/vitest.setup.ts
+++ packages/llmz/vitest.setup.ts
@@ -1,3 +1,4 @@
+import { Client } from '@botpress/client'
 import { beforeAll } from 'vitest'
 import { init } from './src/utils.js'
 
@@ -11,4 +12,24 @@ beforeAll(async () => {
   if (!process.env.CLOUD_BOT_ID) {
     throw new Error('Missing CLOUD_BOT_ID')
   }
+
+  const client = new Client({
+    apiUrl: process.env.CLOUD_API_ENDPOINT ?? 'https://api.botpress.dev',
+    botId: process.env.CLOUD_BOT_ID,
+    token: process.env.CLOUD_PAT,
+  })
+
+  const { integration: openai } = await client.getPublicIntegration({
+    name: 'openai',
+    version: 'latest',
+  })
+
+  await client.updateBot({
+    id: process.env.CLOUD_BOT_ID,
+    integrations: {
+      [openai.id]: {
+        enabled: true,
+      },
+    },
+  })
 })
