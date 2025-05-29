b0d128da4778f2dbcc0fd55d625e78cbb7a18398
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-05-29 13:16:21 -0400

    Update integration version

--- integrations/whatsapp/integration.definition.ts
+++ integrations/whatsapp/integration.definition.ts
@@ -76,7 +76,7 @@ const defaultBotPhoneNumberId = {
 
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
-  version: '4.0.2',
+  version: '4.0.3',
   title: 'WhatsApp',
   description: 'Send and receive messages through WhatsApp.',
   icon: 'icon.svg',
