de69c3587fb2063a38995e2734bccf80e28b35aa
Sylvain Perron <1315508+slvnperron@users.noreply.github.com>
2025-06-16 13:47:21 -0400

    Update index.ts

--- packages/cli/src/code-generation/index.ts
+++ packages/cli/src/code-generation/index.ts
@@ -1,7 +1,7 @@
 export * from './typings'
 export { secretEnvVariableName } from './integration-implementation/integration-secret'
 export { generateBotImplementation } from './bot-implementation'
-export { generateIntegrationImplementation } from './integration-implementation/index.js'
+export { generateIntegrationImplementation } from './integration-implementation'
 export { generatePluginImplementation } from './plugin-implementation'
 export { generateIntegrationPackage } from './integration-package'
 export { generateInterfacePackage } from './interface-package'
