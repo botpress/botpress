2b223002171046a6adf0f6584653ceba1632bdae
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-06-19 16:14:19 -0400

    feat: Add validation request to check if provided API key is valid

--- integrations/sendgrid/src/index.ts
+++ integrations/sendgrid/src/index.ts
@@ -1,10 +1,30 @@
+import { RuntimeError } from '@botpress/sdk'
+import sgClient from '@sendgrid/client'
 import sgMail from '@sendgrid/mail'
 import actions from './actions'
+import { parseError } from './misc/utils'
 import * as bp from '.botpress'
 
 export default new bp.Integration({
-  register: async (props) => {
-    sgMail.setApiKey(props.ctx.configuration.apiKey)
+  register: async ({ ctx }) => {
+    sgClient.setApiKey(ctx.configuration.apiKey)
+    sgMail.setClient(sgClient)
+
+    await sgClient.request(
+      {
+        method: 'GET',
+        url: '/v3/scopes',
+      },
+      (error, [resp]) => {
+        if (error) {
+          throw parseError(error)
+        }
+
+        if (resp && resp.statusCode < 200 && resp.statusCode >= 300) {
+          throw new RuntimeError('An invalid API key was provided.')
+        }
+      }
+    )
   },
   unregister: async () => {},
   actions,
