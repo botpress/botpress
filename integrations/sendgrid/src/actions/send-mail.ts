de0f98ded21d43e27305b70710c6c006cffbbde2
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-06-19 14:31:15 -0400

    refactor: Inlined "mapInputToSendMailRequestBody" function logic in sendMail action to reduce abstraction

--- integrations/sendgrid/src/actions/send-mail.ts
+++ integrations/sendgrid/src/actions/send-mail.ts
@@ -1,13 +1,22 @@
-import { input } from '@botpress/sdk'
 import sgMail from '@sendgrid/mail'
-import type { sendMailInputSchema } from '../../definitions/actions'
 import { parseError } from '../misc/utils'
 import * as bp from '.botpress'
 
 export const sendMail: bp.IntegrationProps['actions']['sendMail'] = async ({ input, logger }) => {
   try {
-    const requestBody = mapInputToSendMailRequestBody(input)
-    const [response, _] = await sgMail.send(requestBody)
+    const [response, _] = await sgMail.send({
+      // Transforms input schema data into SendGrid
+      // request body (To avoid vendor lock-in)
+      personalizations: [
+        {
+          to: input.to,
+        },
+      ],
+      from: input.from,
+      replyTo: input.replyTo,
+      subject: input.subject,
+      text: input.body,
+    })
 
     return {
       status: response.statusCode,
@@ -16,24 +25,5 @@ export const sendMail: bp.IntegrationProps['actions']['sendMail'] = async ({ inp
     const error = parseError(thrown)
     logger.forBot().error('Failed to send email', error)
     throw error
-  }
-}
-
-/** Takes the sendMailInputSchema data and transforms it into a format that SendGrid will accept.
- *
- * @remark The goal of this is to have a common input schema between each of the
- *  email integrations while handling what to send to each email API internally
- *  (avoiding vendor lock-in). */
-const mapInputToSendMailRequestBody = (input: input<typeof sendMailInputSchema>) => {
-  return {
-    personalizations: [
-      {
-        to: input.to,
-      },
-    ],
-    from: input.from,
-    replyTo: input.replyTo,
-    subject: input.subject,
-    text: input.body,
   }
 }
