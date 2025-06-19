4f0e527904bc1c2efe8aecf8683df7fca3bfe650
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-06-19 17:40:29 -0400

    fix: Simplified input validation schema for sendMail action

--- integrations/sendgrid/definitions/actions.ts
+++ integrations/sendgrid/definitions/actions.ts
@@ -1,22 +1,26 @@
 import { z } from '@botpress/sdk'
 import { EMAIL_ADDRESS_DESCRIPTION, EmailAddressSchema, NonBlankString } from './common'
 
-export const EmailDataSchema = z
-  .object({
-    name: NonBlankString.optional().describe('The name of the correspondent (e.g. "John" or "John Doe")'),
-    email: EmailAddressSchema,
-  })
-  .or(EmailAddressSchema)
+/** The input binding for this doesn't show up at all in the Botpress Studio Action Node.
+ *
+ *  @remark Will uncomment back in once I fix it in
+ *   Botpress Studio, or remove it if it's not possible. */
+// export const EmailDataSchema = z
+//   .object({
+//     name: NonBlankString.optional().describe('The name of the correspondent (e.g. "John" or "John Doe")'),
+//     email: EmailAddressSchema,
+//   })
+//   .or(EmailAddressSchema)
 
 /** The common send email input schema which will be exposed to users in Botpress Studio */
 export const sendMailInputSchema = z.object({
-  to: EmailDataSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Email Recipient'),
-  from: EmailDataSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Email Sender'),
+  to: EmailAddressSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Email Recipient'),
+  from: EmailAddressSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Email Sender'),
   subject: NonBlankString.describe('The subject of the email (e.g. How to build a bot with Botpress!)').title(
     'Email Subject'
   ),
   body: NonBlankString.describe('The plain text body of the email').title('Email Body'), // TODO: Think of an example to place here
-  replyTo: EmailDataSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Reply To').optional(),
+  replyTo: EmailAddressSchema.describe(EMAIL_ADDRESS_DESCRIPTION).title('Reply To').optional(),
 })
 
 /** The response schema for SendGrid's send email endpoint for successful requests. */
--- integrations/sendgrid/integration.definition.ts
+++ integrations/sendgrid/integration.definition.ts
@@ -4,7 +4,7 @@ import { sendEmailOutputSchema, sendMailInputSchema } from './definitions/action
 export default new IntegrationDefinition({
   name: 'sendgrid',
   title: 'SendGrid',
-  version: '0.0.2',
+  version: '0.0.3',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: 'Send simple plain text emails using the SendGrid email service.',
