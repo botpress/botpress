535fad5affb77d3ca96f215185056a1805151ea2
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-06-23 11:55:17 -0400

    chore: fix name, add title & description in the integration definition

--- integrations/resend/integration.definition.ts
+++ integrations/resend/integration.definition.ts
@@ -1,8 +1,10 @@
 import { IntegrationDefinition } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
-  name: 'Resend',
+  name: 'resend',
+  title: 'Resend',
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
+  description: 'Send simple plain text emails using the Resend email service.',
 })
