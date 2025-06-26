a7a4548a05cc177173ff3f87a207ba8abae47a2f
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-06-26 13:09:51 -0400

    chore: increment integration version to minor v0.1.0

--- integrations/sendgrid/integration.definition.ts
+++ integrations/sendgrid/integration.definition.ts
@@ -4,7 +4,7 @@ import { sendEmailOutputSchema, sendMailInputSchema } from './definitions/action
 export default new IntegrationDefinition({
   name: 'sendgrid',
   title: 'SendGrid',
-  version: '0.0.1',
+  version: '0.1.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: 'Send simple rich text emails using the SendGrid email service.',
