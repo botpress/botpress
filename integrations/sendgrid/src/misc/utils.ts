0286a92ac1094072ea88b21c81ecffe8575e88a9
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-07-03 13:30:15 -0400

    Revert "feat: Add additional context error message for when api key is the issue"
    This reverts commit 2b18c9e326cf1a47165f27f5fe4d54aa861c5a5b.

--- integrations/sendgrid/src/misc/utils.ts
+++ integrations/sendgrid/src/misc/utils.ts
@@ -1,40 +1,18 @@
 import { RuntimeError } from '@botpress/sdk'
 import { ResponseError } from '@sendgrid/helpers/classes'
 import { SendGridResponseError } from './custom-types'
-import { TIntegration } from '.botpress'
 
 export const isSendGridError = (thrown: unknown): thrown is SendGridResponseError => {
   return thrown instanceof ResponseError && typeof thrown.response.body === 'object' && 'errors' in thrown.response.body
 }
 
-export const parseError = (
-  config: TIntegration['configuration'],
-  thrown: unknown,
-  sendGridErrorMessageOverride: string | null = null
-) => {
+export const parseError = (thrown: unknown, sendGridErrorMessageOverride: string | null = null) => {
   if (isSendGridError(thrown)) {
     if (sendGridErrorMessageOverride) {
       return new RuntimeError(sendGridErrorMessageOverride, thrown)
     }
 
     const errorMessage = thrown.response.body.errors[0]?.message ?? thrown.message
-
-    if (errorMessage === 'Permission denied, wrong credentials' || thrown.code === 401) {
-      const trimApiKey = config.apiKey.trim()
-      const isApiKeyEmpty = trimApiKey.length === 0
-
-      let cutoffPosition = trimApiKey.lastIndexOf('.')
-      if (cutoffPosition === -1 || cutoffPosition >= trimApiKey.length - 1) {
-        cutoffPosition = Math.max(0, trimApiKey.length / 2)
-      }
-
-      const errorMessage = isApiKeyEmpty
-        ? 'No API key was sent to the SendGrid API'
-        : `An invalid API key was sent to the SendGrid API\n${trimApiKey.slice(0, cutoffPosition)}`
-
-      return new RuntimeError(errorMessage, thrown)
-    }
-
     return new RuntimeError(
       `SendGrid API yielded an error with status code: "${thrown.code}", and message: "${errorMessage}"`,
       thrown
