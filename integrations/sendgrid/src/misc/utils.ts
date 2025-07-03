b66cc09d20bc24ceed6701aef24563b1e758309e
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-07-03 16:40:52 -0400

    refactor: abstract parse send grid error logic into own function

--- integrations/sendgrid/src/misc/utils.ts
+++ integrations/sendgrid/src/misc/utils.ts
@@ -3,6 +3,8 @@ import { ResponseError } from '@sendgrid/helpers/classes'
 import { SendGridResponseError } from './custom-types'
 import type * as bp from '.botpress'
 
+const UNAUTHORIZED = 401 as const
+
 export const isSendGridError = (thrown: unknown): thrown is SendGridResponseError => {
   return thrown instanceof ResponseError && typeof thrown.response.body === 'object' && 'errors' in thrown.response.body
 }
@@ -13,27 +15,7 @@ export const parseError = (
   sendGridErrorMessageOverride: string | null = null
 ) => {
   if (isSendGridError(thrown)) {
-    if (sendGridErrorMessageOverride) {
-      return new RuntimeError(sendGridErrorMessageOverride, thrown)
-    }
-
-    const errorMessage = thrown.response.body.errors[0]?.message ?? thrown.message
-
-    if (thrown.code === 401) {
-      const apiKey = ctx.configuration.apiKey.trim()
-      const isApiKeyEmpty = apiKey.length === 0
-
-      const errorMessage = isApiKeyEmpty
-        ? 'No API key was sent to the SendGrid API'
-        : `An invalid API key was sent to the SendGrid API\n${_maskApiKey(apiKey)}`
-
-      return new RuntimeError(errorMessage, thrown)
-    }
-
-    return new RuntimeError(
-      `SendGrid API yielded an error with status code: "${thrown.code}", and message: "${errorMessage}"`,
-      thrown
-    )
+    return _parseSendGridError(ctx, thrown, sendGridErrorMessageOverride)
   }
 
   if (thrown instanceof RuntimeError) {
@@ -41,6 +23,34 @@ export const parseError = (
   }
 
   return thrown instanceof Error ? new RuntimeError(thrown.message, thrown) : new RuntimeError(String(thrown))
+}
+
+const _parseSendGridError = (
+  ctx: bp.HandlerProps['ctx'],
+  error: SendGridResponseError,
+  sendGridErrorMessageOverride: string | null = null
+) => {
+  if (sendGridErrorMessageOverride) {
+    return new RuntimeError(sendGridErrorMessageOverride, error)
+  }
+
+  const errorMessage = error.response.body.errors[0]?.message ?? error.message
+
+  if (error.code === UNAUTHORIZED) {
+    const apiKey = ctx.configuration.apiKey.trim()
+    const isApiKeyEmpty = apiKey.length === 0
+
+    const errorMessage = isApiKeyEmpty
+      ? 'No API key was sent to the SendGrid API'
+      : `An invalid API key was sent to the SendGrid API\n${_maskApiKey(apiKey)}`
+
+    return new RuntimeError(errorMessage, error)
+  }
+
+  return new RuntimeError(
+    `SendGrid API yielded an error with status code: "${error.code}", and message: "${errorMessage}"`,
+    error
+  )
 }
 
 /** @param apiKey Expected to be trimmed of whitespace */
