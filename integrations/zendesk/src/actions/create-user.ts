ba6fafdc6a0c45afcf9c3ef9760d4ad2f0dde0fc
Francois Levasseur <francois.levasseur@botpress.com>
2025-07-07 19:36:49 -0400

    fix(integrations/zendesk): ignore picture url when too long

--- integrations/zendesk/src/actions/create-user.ts
+++ integrations/zendesk/src/actions/create-user.ts
@@ -1,7 +1,12 @@
 import { getZendeskClient } from '../client'
 import * as bp from '.botpress'
 
-export const createUser: bp.IntegrationProps['actions']['createUser'] = async ({ ctx, client: bpClient, input }) => {
+export const createUser: bp.IntegrationProps['actions']['createUser'] = async ({
+  ctx,
+  client: bpClient,
+  input,
+  logger,
+}) => {
   const zendeskClient = getZendeskClient(ctx.configuration)
 
   const { name, email, pictureUrl } = input
@@ -16,11 +21,22 @@ export const createUser: bp.IntegrationProps['actions']['createUser'] = async ({
     discriminateByTags: ['email'],
   })
 
+  const encodedPictureUrl = pictureUrl && encodeURI(pictureUrl)
+
+  let remote_photo_url = pictureUrl
+
+  const urlLength = _getEncodedLength(encodedPictureUrl)
+  if (urlLength && urlLength > 255) {
+    // Zendesk has a limit of 255 characters for the remote_photo_url
+    logger.forBot().warn(`Picture URL is too long (${urlLength} characters). It will not be set in Zendesk.`)
+    remote_photo_url = ''
+  }
+
   const zendeskUser = await zendeskClient.createOrUpdateUser({
     role: 'end-user',
     external_id: user.id,
     name,
-    remote_photo_url: pictureUrl,
+    remote_photo_url,
     email,
   })
 
@@ -37,3 +53,5 @@ export const createUser: bp.IntegrationProps['actions']['createUser'] = async ({
     userId: user.id,
   }
 }
+
+const _getEncodedLength = (url: string | undefined): number | undefined => (url ? encodeURI(url).length : undefined)
