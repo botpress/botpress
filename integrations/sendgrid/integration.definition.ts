379010b417706b060254565cd7adae02dbe79b3b
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-07-09 11:23:06 -0400

    chore: update integration version

--- integrations/sendgrid/integration.definition.ts
+++ integrations/sendgrid/integration.definition.ts
@@ -12,7 +12,7 @@ import {
 export default new IntegrationDefinition({
   name: 'sendgrid',
   title: 'SendGrid',
-  version: '0.1.3',
+  version: '0.1.4',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: 'Send markdown rich-text emails using the SendGrid email service.',
