58248df11186ab2d038d0b67f408a93836b9bac1
Ben Luks <ben@Mac.localdomain>
2025-07-16 15:30:21 -0400

    remove print statements

---
+++ integrations/gmail/src/actions/get-my-email.ts
@@ -0,0 +1,13 @@
+import { wrapAction } from '../action-wrapper'
+
+export const getMyEmail = wrapAction(
+  { actionName: 'getMyEmail', errorMessage: 'Failed to get email' },
+  async ({ googleClient }, input) => {
+    const email = await googleClient.getMyEmail()
+    console.info(`got the email: ${email}`)
+    if (!email) {
+      throw new Error('Unable to retrieve email address')
+    }
+    return { emailAddress: email }
+  }
+)
