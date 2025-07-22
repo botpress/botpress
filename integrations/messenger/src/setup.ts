ef00de09c9aa057c6809bd76badee9f69d9b8c60
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-07-22 10:21:23 -0400

    Use register instead of unregister
    Turns out register is always called when config type changes but not
    unregister

--- integrations/messenger/src/setup.ts
+++ integrations/messenger/src/setup.ts
@@ -2,16 +2,22 @@ import { getMetaClientCredentials } from './misc/auth'
 import { MetaClient } from './misc/meta-client'
 import * as bp from '.botpress'
 
-export const register: bp.IntegrationProps['register'] = async () => {}
+type RegisterProps = Parameters<bp.IntegrationProps['register']>[0]
 
-export const unregister: bp.IntegrationProps['unregister'] = async ({ ctx, logger, client }) => {
-  if (ctx.configurationType === null) {
+export const register: bp.IntegrationProps['register'] = async (props) => {
+  const { ctx, client } = props
+  if (ctx.configurationType === 'manualApp') {
     await client.configureIntegration({
       identifier: null,
     })
-
-    const metaClient = new MetaClient(logger)
-    const { pageToken, pageId } = await getMetaClientCredentials(client, ctx)
-    await metaClient.unsubscribeFromWebhooks(pageToken, pageId)
+    await _unsubscribeFromWebhooks(props)
   }
+}
+
+export const unregister: bp.IntegrationProps['unregister'] = async () => {}
+
+const _unsubscribeFromWebhooks = async ({ ctx, logger, client }: RegisterProps) => {
+  const metaClient = new MetaClient(logger)
+  const { pageToken, pageId } = await getMetaClientCredentials(client, ctx)
+  await metaClient.unsubscribeFromWebhooks(pageToken, pageId)
 }
