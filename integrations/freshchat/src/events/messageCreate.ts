c3a9d692372fb6819396c47e954b5d414a5d22e6
David Vitor Antonio <david.vitora@gmail.com>
2025-07-31 14:59:10 -0300

    add receive files

--- integrations/freshchat/src/events/messageCreate.ts
+++ integrations/freshchat/src/events/messageCreate.ts
@@ -1,6 +1,7 @@
 import { MessageCreateFreshchatEvent } from '../definitions/freshchat-events'
 import { updateAgentUser } from '../util'
 import * as bp from '.botpress'
+import { Message } from '@botpress/common'
 
 export const executeMessageCreate = async ({
   freshchatEvent,
@@ -39,13 +40,64 @@ export const executeMessageCreate = async ({
   // An unassigned agent might send a message
   await updateAgentUser(user, client, ctx, logger)
 
+  const items: Extract<Message, { type: 'bloc' }>['payload']['items'] = []
   for (const messagePart of freshchatEvent.data.message.message_parts) {
-    await client.createMessage({
-      tags: {},
-      type: 'text',
-      userId: user?.id as string,
-      conversationId: conversation.id,
-      payload: { text: messagePart.text.content },
-    })
+    if ('text' in messagePart) {
+      items.push({
+        type: 'text',
+        payload: {
+          text: messagePart.text.content,
+        },
+      })
+    } else if ('file' in messagePart) {
+      const { name, url, content_type } = messagePart.file as {
+        name: string
+        url: string
+        file_size_in_bytes: number
+        content_type: string
+      }
+      if (content_type.startsWith('image/')) {
+        items.push({
+          type: 'image',
+          payload: {
+            imageUrl: url,
+          },
+        })
+      } else if (content_type.startsWith('audio/')) {
+        items.push({
+          type: 'audio',
+          payload: {
+            audioUrl: url,
+          },
+        })
+      } else if (content_type.startsWith('video/')) {
+        items.push({
+          type: 'video',
+          payload: {
+            videoUrl: url,
+          },
+        })
+      } else {
+        items.push({
+          type: 'file',
+          payload: {
+            title: name,
+            fileUrl: url,
+          },
+        })
+      }
+    } else {
+      logger.forBot().warn(`Unsupported message part type ${Object.keys(messagePart)[0]}`)
+    }
   }
+
+  await client.createMessage({
+    type: 'bloc',
+    payload: {
+      items,
+    },
+    tags: {},
+    userId: user?.id as string,
+    conversationId: conversation.id,
+  })
 }
