78b72f4ca60575945ff79939eb1f34cc031b5e8b
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-01 14:36:35 -0400

    fix: update the bloc message type to override audio message payload schema to match non-bloc audio payload schema

--- integrations/telegram/definitions/channels.ts
+++ integrations/telegram/definitions/channels.ts
@@ -7,7 +7,25 @@ const _audioMessageDefinition = {
   }),
 }
 
+const _blocSchema = z.union([
+  z.object({ type: z.literal('text'), payload: messages.defaults.text.schema }),
+  z.object({ type: z.literal('markdown'), payload: messages.markdown.schema }),
+  z.object({ type: z.literal('image'), payload: messages.defaults.image.schema }),
+  z.object({ type: z.literal('audio'), payload: _audioMessageDefinition.schema }),
+  z.object({ type: z.literal('video'), payload: messages.defaults.video.schema }),
+  z.object({ type: z.literal('file'), payload: messages.defaults.file.schema }),
+  z.object({ type: z.literal('location'), payload: messages.defaults.location.schema }),
+])
+
+const _blocMessageDefinition = {
+  ...messages.defaults.bloc,
+  schema: z.object({
+    items: z.array(_blocSchema),
+  }),
+}
+
 export const telegramMessageChannels = {
   ...messages.defaults,
   audio: _audioMessageDefinition,
+  bloc: _blocMessageDefinition,
 }
