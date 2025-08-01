2d0a431e85434e7560ccf0a6f260d38866bf8e75
Nathaniel Girard <72364963+Nathaniel-Girard@users.noreply.github.com>
2025-08-01 14:54:02 -0400

    chore(cli): updated template versions to 0.1.0 (#14101)

--- packages/cli/templates/hello-world/integration.definition.ts
+++ packages/cli/templates/hello-world/integration.definition.ts
@@ -3,7 +3,7 @@ import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
   name: integrationName,
-  version: '0.0.1',
+  version: '0.1.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   actions: {
