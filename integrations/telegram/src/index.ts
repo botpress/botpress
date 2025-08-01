fae3ee4f77761aea9780d160770a9ec68a1c5303
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-01 17:59:13 -0400

    refactor: extract message handlers

--- integrations/telegram/src/index.ts
+++ integrations/telegram/src/index.ts
@@ -1,18 +1,27 @@
-import { RuntimeError } from '@botpress/client'
 import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 import { ok } from 'assert/strict'
 
-import { Markup, Telegraf } from 'telegraf'
+import { Telegraf } from 'telegraf'
 import type { User } from 'telegraf/typings/core/types/typegram'
 
-import { stdMarkdownToTelegramHtml } from './misc/markdown-to-telegram-html'
+import {
+  handleAudioMessage,
+  handleBlocMessage,
+  handleCardMessage,
+  handleCarouselMessage,
+  handleChoiceMessage,
+  handleDropdownMessage,
+  handleFileMessage,
+  handleImageMessage,
+  handleLocationMessage,
+  handleTextMessage,
+  handleVideoMessage,
+} from './misc/message-handlers'
 import { TelegramMessage } from './misc/types'
 import {
   getUserPictureDataUri,
   getUserNameFromTelegramUser,
   getChat,
-  sendCard,
-  ackMessage,
   convertTelegramMessageToBotpressMessage,
   wrapHandler,
   getMessageId,
@@ -58,107 +67,17 @@ const integration = new bp.Integration({
   channels: {
     channel: {
       messages: {
-        text: async ({ payload, ctx, conversation, ack, logger }) => {
-          const { text } = payload
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending markdown message to Telegram chat ${chat}:`, text)
-          const { html } = stdMarkdownToTelegramHtml(text)
-          // TODO: Implement extracted data
-          const message = await client.telegram.sendMessage(chat, html, {
-            parse_mode: 'HTML',
-          })
-          await ackMessage(message, ack)
-        },
-        image: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending image message to Telegram chat ${chat}`, payload.imageUrl)
-          const message = await client.telegram.sendPhoto(chat, payload.imageUrl)
-          await ackMessage(message, ack)
-        },
-        audio: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending audio voice to Telegram chat ${chat}:`, payload.audioUrl)
-          try {
-            const message = await client.telegram.sendVoice(chat, payload.audioUrl, { caption: payload.caption })
-            await ackMessage(message, ack)
-          } catch {
-            // If the audio file is too large to be voice, Telegram should send it as an audio file, but if for some reason it doesn't, we can send it as an audio file
-            const message = await client.telegram.sendAudio(chat, payload.audioUrl, { caption: payload.caption })
-            await ackMessage(message, ack)
-          }
-        },
-        video: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending video message to Telegram chat ${chat}:`, payload.videoUrl)
-          const message = await client.telegram.sendVideo(chat, payload.videoUrl)
-          await ackMessage(message, ack)
-        },
-        file: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending file message to Telegram chat ${chat}:`, payload.fileUrl)
-          const message = await client.telegram.sendDocument(chat, payload.fileUrl)
-          await ackMessage(message, ack)
-        },
-        location: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending location message to Telegram chat ${chat}:`, {
-            latitude: payload.latitude,
-            longitude: payload.longitude,
-          })
-          const message = await client.telegram.sendLocation(chat, payload.latitude, payload.longitude)
-          await ackMessage(message, ack)
-        },
-        card: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending card message to Telegram chat ${chat}:`, payload)
-          await sendCard(payload, client, chat, ack)
-        },
-        carousel: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending carousel message to Telegram chat ${chat}:`, payload)
-          payload.items.forEach(async (item) => {
-            await sendCard(item, client, chat, ack)
-          })
-        },
-        dropdown: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          const buttons = payload.options.map((choice) => Markup.button.callback(choice.label, choice.value))
-          logger.forBot().debug(`Sending dropdown message to Telegram chat ${chat}:`, payload)
-          const message = await client.telegram.sendMessage(chat, payload.text, Markup.keyboard(buttons).oneTime())
-          await ackMessage(message, ack)
-        },
-        choice: async ({ payload, ctx, conversation, ack, logger }) => {
-          const client = new Telegraf(ctx.configuration.botToken)
-          const chat = getChat(conversation)
-          logger.forBot().debug(`Sending choice message to Telegram chat ${chat}:`, payload)
-          const buttons = payload.options.map((choice) => Markup.button.callback(choice.label, choice.value))
-          const message = await client.telegram.sendMessage(chat, payload.text, Markup.keyboard(buttons).oneTime())
-          await ackMessage(message, ack)
-        },
-        bloc: async ({ client, payload, ctx, conversation }) => {
-          if (payload.items.length > 20) {
-            throw new RuntimeError('Telegram only allows 20 messages to be sent every 60 seconds')
-          }
-
-          for (const item of payload.items) {
-            await client.createMessage({
-              userId: ctx.botUserId,
-              conversationId: conversation.id,
-              type: item.type,
-              payload: item.payload,
-              tags: {},
-            })
-          }
-        },
+        text: handleTextMessage,
+        image: handleImageMessage,
+        audio: handleAudioMessage,
+        video: handleVideoMessage,
+        file: handleFileMessage,
+        location: handleLocationMessage,
+        card: handleCardMessage,
+        carousel: handleCarouselMessage,
+        dropdown: handleDropdownMessage,
+        choice: handleChoiceMessage,
+        bloc: handleBlocMessage,
       },
     },
   },
---
+++ integrations/telegram/src/misc/message-handlers.ts
@@ -0,0 +1,143 @@
+import { RuntimeError } from '@botpress/client'
+import { Markup, Telegraf } from 'telegraf'
+import { stdMarkdownToTelegramHtml } from './markdown-to-telegram-html'
+import { ackMessage, getChat, sendCard } from './utils'
+import * as bp from '.botpress'
+
+export type MessageHandlerProps<T extends keyof bp.MessageProps['channel']> = bp.MessageProps['channel'][T]
+
+export const handleTextMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'text'>) => {
+  const { text } = payload
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending markdown message to Telegram chat ${chat}:`, text)
+  const { html } = stdMarkdownToTelegramHtml(text)
+  // TODO: Implement extracted data
+  const message = await client.telegram.sendMessage(chat, html, {
+    parse_mode: 'HTML',
+  })
+  await ackMessage(message, ack)
+}
+
+export const handleImageMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'image'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending image message to Telegram chat ${chat}`, payload.imageUrl)
+  const message = await client.telegram.sendPhoto(chat, payload.imageUrl)
+  await ackMessage(message, ack)
+}
+
+export const handleAudioMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'audio'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending audio voice to Telegram chat ${chat}:`, payload.audioUrl)
+  try {
+    const message = await client.telegram.sendVoice(chat, payload.audioUrl, { caption: payload.caption })
+    await ackMessage(message, ack)
+  } catch {
+    // If the audio file is too large to be voice, Telegram should send it as an audio file, but if for some reason it doesn't, we can send it as an audio file
+    const message = await client.telegram.sendAudio(chat, payload.audioUrl, { caption: payload.caption })
+    await ackMessage(message, ack)
+  }
+}
+
+export const handleVideoMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'video'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending video message to Telegram chat ${chat}:`, payload.videoUrl)
+  const message = await client.telegram.sendVideo(chat, payload.videoUrl)
+  await ackMessage(message, ack)
+}
+
+export const handleFileMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'file'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending file message to Telegram chat ${chat}:`, payload.fileUrl)
+  const message = await client.telegram.sendDocument(chat, payload.fileUrl)
+  await ackMessage(message, ack)
+}
+
+export const handleLocationMessage = async ({
+  payload,
+  ctx,
+  conversation,
+  ack,
+  logger,
+}: MessageHandlerProps<'location'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending location message to Telegram chat ${chat}:`, {
+    latitude: payload.latitude,
+    longitude: payload.longitude,
+  })
+  const message = await client.telegram.sendLocation(chat, payload.latitude, payload.longitude)
+  await ackMessage(message, ack)
+}
+
+export const handleCardMessage = async ({ payload, ctx, conversation, ack, logger }: MessageHandlerProps<'card'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending card message to Telegram chat ${chat}:`, payload)
+  await sendCard(payload, client, chat, ack)
+}
+
+export const handleCarouselMessage = async ({
+  payload,
+  ctx,
+  conversation,
+  ack,
+  logger,
+}: MessageHandlerProps<'carousel'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending carousel message to Telegram chat ${chat}:`, payload)
+  payload.items.forEach(async (item) => {
+    await sendCard(item, client, chat, ack)
+  })
+}
+
+export const handleDropdownMessage = async ({
+  payload,
+  ctx,
+  conversation,
+  ack,
+  logger,
+}: MessageHandlerProps<'dropdown'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  const buttons = payload.options.map((choice) => Markup.button.callback(choice.label, choice.value))
+  logger.forBot().debug(`Sending dropdown message to Telegram chat ${chat}:`, payload)
+  const message = await client.telegram.sendMessage(chat, payload.text, Markup.keyboard(buttons).oneTime())
+  await ackMessage(message, ack)
+}
+
+export const handleChoiceMessage = async ({
+  payload,
+  ctx,
+  conversation,
+  ack,
+  logger,
+}: MessageHandlerProps<'choice'>) => {
+  const client = new Telegraf(ctx.configuration.botToken)
+  const chat = getChat(conversation)
+  logger.forBot().debug(`Sending choice message to Telegram chat ${chat}:`, payload)
+  const buttons = payload.options.map((choice) => Markup.button.callback(choice.label, choice.value))
+  const message = await client.telegram.sendMessage(chat, payload.text, Markup.keyboard(buttons).oneTime())
+  await ackMessage(message, ack)
+}
+
+export const handleBlocMessage = async ({ client, payload, ctx, conversation }: MessageHandlerProps<'bloc'>) => {
+  if (payload.items.length > 20) {
+    throw new RuntimeError('Telegram only allows 20 messages to be sent every 60 seconds')
+  }
+
+  for (const item of payload.items) {
+    await client.createMessage({
+      userId: ctx.botUserId,
+      conversationId: conversation.id,
+      type: item.type,
+      payload: item.payload,
+      tags: {},
+    })
+  }
+}
