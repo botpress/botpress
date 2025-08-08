8cb50a0d456d2b1be2fff6de1ce8787bcce9b7bf
Francois Levasseur <francois.levasseur@botpress.com>
2025-08-08 16:47:48 -0400

    update

--- plugins/conversation-insights/src/index.ts
+++ plugins/conversation-insights/src/index.ts
@@ -4,56 +4,39 @@ const plugin = new bp.Plugin({
   actions: {},
 })
 
+// TODO: generate a type for CommonProps in the CLI / SDK
+type CommonProps =
+  | bp.HookHandlerProps['after_incoming_message']
+  | bp.HookHandlerProps['after_outgoing_message']
+  | bp.EventHandlerProps
+
 plugin.on.afterIncomingMessage('*', async (props) => {
-  const conversationTags = await props.client
-    .getConversation({ id: props.data.conversationId })
-    .then(({ conversation }) => conversation.tags)
-
-  await _onNewMessage(
-    {
-      ...props,
-      conversation: { id: props.data.conversationId, tags: { ...conversationTags } },
-      userId: props.data.userId,
-    },
-    true
-  )
-
-  return {}
+  const { conversation } = await props.client.getConversation({ id: props.data.conversationId })
+  await _onNewMessage({ ...props, conversation, isDirty: true })
+  return undefined
 })
 
 plugin.on.afterOutgoingMessage('*', async (props) => {
-  const conversation = await props.client.getConversation({ id: props.data.message.conversationId })
-  await _onNewMessage({ ...props, conversation: conversation.conversation, userId: props.data.message.userId }, false)
-
-  return {}
+  const { conversation } = await props.client.getConversation({ id: props.data.message.conversationId })
+  await _onNewMessage({ ...props, conversation, isDirty: false })
+  return undefined
 })
 
 plugin.on.event('updateTitleAndSummary', async (props) => {
-  const conversations = await props.client.listConversations({
-    tags: { isDirty: 'true' },
-  })
+  const conversations = await props.client.listConversations({ tags: { isDirty: 'true' } })
 
   for (const conversation of conversations.conversations) {
     const messages = await props.client.listMessages({ conversationId: conversation.id })
     const newMessages = messages.messages.map((message) => message.payload.text)
-
-    await _updateTitleAndSummary({ client: props.client, conversationId: conversation.id }, newMessages)
+    await _updateTitleAndSummary({ ...props, conversationId: conversation.id, messages: newMessages })
   }
 })
 
-const _onNewMessage = async (
-  props: {
-    conversation: {
-      id: bp.MessageHandlerProps['conversation']['id']
-      tags: bp.MessageHandlerProps['conversation']['tags']
-    }
-    states: bp.MessageHandlerProps['states']
-    userId: bp.MessageHandlerProps['user']['id']
-    client: bp.MessageHandlerProps['client']
-    logger: bp.MessageHandlerProps['logger']
-  },
+type OnNewMessageProps = CommonProps & {
+  conversation: bp.ClientOutputs['getConversation']['conversation']
   isDirty: boolean
-) => {
+}
+const _onNewMessage = async (props: OnNewMessageProps) => {
   const message_count = props.conversation.tags.message_count ? parseInt(props.conversation.tags.message_count) + 1 : 1
 
   const participant_count = await props.client
@@ -63,7 +46,7 @@ const _onNewMessage = async (
   const tags = {
     message_count: message_count.toString(),
     participant_count: participant_count.toString(),
-    isDirty: isDirty ? 'true' : 'false',
+    isDirty: props.isDirty ? 'true' : 'false',
   }
 
   await props.client.updateConversation({
@@ -72,14 +55,15 @@ const _onNewMessage = async (
   })
 }
 
-const _updateTitleAndSummary = async (
-  props: { client: bp.MessageHandlerProps['client']; conversationId: string },
-  _messages: string[]
-) => {
-  //TODO
+type UpdateTitleAndSummaryProps = CommonProps & {
+  conversationId: string
+  messages: string[]
+}
+const _updateTitleAndSummary = async (props: UpdateTitleAndSummaryProps) => {
   await props.client.updateConversation({
     id: props.conversationId,
     tags: {
+      // TODO: use the cognitive client / service to generate a title and summary
       title: 'The conversation title!',
       summary: 'This is normally where the conversation summary would be.',
       isDirty: 'false',
