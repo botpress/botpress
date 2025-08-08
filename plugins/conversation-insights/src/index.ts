3f75a0cfcc29b3ee2b9df878425ce0972eece5b6
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-08-08 15:27:44 -0400

    fixed some requested changes

--- plugins/conversation-insights/src/index.ts
+++ plugins/conversation-insights/src/index.ts
@@ -6,32 +6,27 @@ const plugin = new bp.Plugin({
 })
 
 plugin.on.afterIncomingMessage('*', async (props) => {
-  const conversationTags = await props.client.getConversation({ id: props.data.conversationId })
+  const conversationTags = await props.client
+    .getConversation({ id: props.data.conversationId })
+    .then(({ conversation }) => conversation.tags)
 
-  // removing tags from plugins
-  const allTagKeys = Object.keys(definition.conversation?.tags ?? {})
-  const tags = allTagKeys.reduce(
-    (acc, key) => {
-      if (conversationTags.conversation.tags[key]) acc[key] = conversationTags.conversation.tags[key]
-      return acc
+  await _onNewMessage(
+    {
+      ...props,
+      conversation: { id: props.data.conversationId, tags: { ...conversationTags } },
+      userId: props.data.userId,
     },
-    {} as Record<string, string>
+    true
   )
 
-  await _onNewMessage({
-    ...props,
-    conversation: { id: props.data.conversationId, tags: { ...tags, isDirty: 'true' } },
-    userId: props.data.userId,
-  })
-
-  return { data: props.data }
+  return {}
 })
 
 plugin.on.afterOutgoingMessage('*', async (props) => {
   const conversation = await props.client.getConversation({ id: props.data.message.conversationId })
-  await _onNewMessage({ ...props, conversation: conversation.conversation, userId: props.data.message.userId })
+  await _onNewMessage({ ...props, conversation: conversation.conversation, userId: props.data.message.userId }, false)
 
-  return { data: { message: props.data.message } }
+  return {}
 })
 
 plugin.on.event('updateTitleAndSummary', async (props) => {
@@ -40,31 +35,36 @@ plugin.on.event('updateTitleAndSummary', async (props) => {
   })
 
   for (const conversation of conversations.conversations) {
-    const messages = props.client.listMessages({ conversationId: conversation.id })
-    const newMessages = (await messages).messages.map((message) => message.payload.text)
+    const messages = await props.client.listMessages({ conversationId: conversation.id })
+    const newMessages = messages.messages.map((message) => message.payload.text)
 
     await _updateTitleAndSummary({ client: props.client, conversationId: conversation.id }, newMessages)
   }
 })
 
-const _onNewMessage = async (props: {
-  conversation: {
-    id: bp.MessageHandlerProps['conversation']['id']
-    tags: bp.MessageHandlerProps['conversation']['tags']
-  }
-  states: bp.MessageHandlerProps['states']
-  userId: bp.MessageHandlerProps['user']['id']
-  client: bp.MessageHandlerProps['client']
-  logger: bp.MessageHandlerProps['logger']
-}) => {
+const _onNewMessage = async (
+  props: {
+    conversation: {
+      id: bp.MessageHandlerProps['conversation']['id']
+      tags: bp.MessageHandlerProps['conversation']['tags']
+    }
+    states: bp.MessageHandlerProps['states']
+    userId: bp.MessageHandlerProps['user']['id']
+    client: bp.MessageHandlerProps['client']
+    logger: bp.MessageHandlerProps['logger']
+  },
+  isDirty: boolean
+) => {
   const message_count = props.conversation.tags.message_count ? parseInt(props.conversation.tags.message_count) + 1 : 1
 
-  const participant_count = (await props.client.listParticipants({ id: props.conversation.id })).participants.length
+  const participant_count = await props.client
+    .listParticipants({ id: props.conversation.id })
+    .then((participants) => participants.participants.length)
 
   const tags = {
-    ...props.conversation.tags,
     message_count: message_count.toString(),
     participant_count: participant_count.toString(),
+    isDirty: isDirty ? 'true' : 'false',
   }
 
   props.client.updateConversation({
@@ -78,7 +78,7 @@ const _updateTitleAndSummary = async (
   _messages: string[]
 ) => {
   //TODO: use a workflow that calls the cognitive service
-  props.client.updateConversation({
+  await props.client.updateConversation({
     id: props.conversationId,
     tags: {
       title: 'The conversation title!',
