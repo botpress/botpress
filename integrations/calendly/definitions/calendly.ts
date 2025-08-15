79a45984ede4d7d13ea33d182b07a8521dbb37d3
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-15 12:22:50 -0400

    fix: incorrect allowed property value in webhookDetailsSchema

--- integrations/calendly/definitions/calendly.ts
+++ integrations/calendly/definitions/calendly.ts
@@ -41,7 +41,7 @@ export const webhookDetailsSchema = z.object({
   events: z.array(z.string()),
   group: z.null(),
   organization: calendlyUri,
-  retry_started_at: z.null(),
+  retry_started_at: z.union([z.coerce.date(), z.null()]),
   scope: z.string(),
   state: z.string(),
   updated_at: z.coerce.date(),
