aecb4e69549f5654ce7f661a4617873b129febd0
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-15 14:37:44 -0400

    refactor: setup logic

--- integrations/calendly/src/setup.ts
+++ integrations/calendly/src/setup.ts
@@ -10,31 +10,33 @@ const performUnregistration = async (
 ) => {
   const { current_organization: organizationUri, uri: userUri } = userResp.resource
 
-  // This technically will break if for some
-  // reason the account has over 100 webhooks
-  const orgWebhooks = await getWebhooksList(httpClient, {
-    scope: 'organization',
-    organization: organizationUri,
-  })
+  // This will break if for some reason the
+  // calendly account has over 100 webhooks
+  const webhooksToDelete: CalendlyDefs.WebhookDetails[] = (
+    await Promise.all([
+      getWebhooksList(httpClient, {
+        scope: 'organization',
+        organization: organizationUri,
+      }),
+      getWebhooksList(httpClient, {
+        scope: 'user',
+        organization: organizationUri,
+        user: userUri,
+      }),
+    ])
+  )
+    .flatMap((resp) => resp.collection)
+    .filter((webhook) => webhook.callback_url === webhookUrl)
 
-  const userWebhooks = await getWebhooksList(httpClient, {
-    scope: 'user',
-    organization: organizationUri,
-    user: userUri,
-  })
-
-  const webhooksToDelete: CalendlyDefs.WebhookDetails[] = [
-    ...orgWebhooks.collection.filter((webhook) => webhook.callback_url === webhookUrl),
-    ...userWebhooks.collection.filter((webhook) => webhook.callback_url === webhookUrl),
-  ]
-
-  webhooksToDelete.forEach((webhook) => removeWebhook(httpClient, webhook.uri))
+  for (const webhook of webhooksToDelete) {
+    await removeWebhook(httpClient, webhook.uri)
+  }
 }
 
 export const unregister: bp.Integration['unregister'] = async ({ ctx, webhookUrl }) => {
   const httpClient = createCalendlyClient(ctx.configuration.accessToken)
   const currentUser = await getCurrentUser(httpClient)
-  performUnregistration(httpClient, currentUser, webhookUrl)
+  await performUnregistration(httpClient, currentUser, webhookUrl)
 }
 
 export const register: bp.Integration['register'] = async ({ ctx, webhookUrl }) => {
@@ -50,18 +52,19 @@ export const register: bp.Integration['register'] = async ({ ctx, webhookUrl }) 
 
   const { current_organization: organizationUri, uri: userUri } = userResp.resource
 
-  createWebhook(httpClient, {
-    webhookUrl,
-    events: ['invitee.created', 'invitee.canceled', 'invitee_no_show.created', 'invitee_no_show.deleted'],
-    organization: organizationUri,
-    user: userUri,
-    scope: 'user',
-  })
-
-  createWebhook(httpClient, {
-    webhookUrl,
-    events: ['routing_form_submission.created'],
-    organization: organizationUri,
-    scope: 'organization',
-  })
+  await Promise.all([
+    createWebhook(httpClient, {
+      webhookUrl,
+      events: ['invitee.created', 'invitee.canceled', 'invitee_no_show.created', 'invitee_no_show.deleted'],
+      organization: organizationUri,
+      user: userUri,
+      scope: 'user',
+    }),
+    createWebhook(httpClient, {
+      webhookUrl,
+      events: ['routing_form_submission.created'],
+      organization: organizationUri,
+      scope: 'organization',
+    }),
+  ])
 }
