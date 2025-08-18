7491a86fee7f08582da128d8895b742efbcb324e
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-18 15:02:31 -0400

    chore: add examples of location types in webhook event output data

--- integrations/calendly/definitions/events.ts
+++ integrations/calendly/definitions/events.ts
@@ -136,7 +136,9 @@ export const inviteeEventOutputSchema = z.object({
   eventName: nonBlankString.describe('The name of the scheduled event').title('Scheduled Event Name'),
   startTime: z.date().describe('The start time of the scheduled event').title('Start Time'),
   endTime: z.date().describe('The end time of the scheduled event').title('End Time'),
-  locationType: nonBlankString.describe('The type of location for the scheduled event').title('Location Type'),
+  locationType: nonBlankString
+    .describe('The type of location for the scheduled event')
+    .title("Location Type (e.g. 'zoom', 'google_conference', 'microsoft_teams_conference')"),
   organizerName: nonBlankString.describe('The name of the event organizer').title('Organizer Name'),
   organizerEmail: nonBlankString.email().describe('The email of the event organizer').title('Organizer Email'),
   inviteeName: nonBlankString.describe('The name of the invitee').title('Invitee Name'),
