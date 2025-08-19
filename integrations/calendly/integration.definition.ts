804f0c29c460b322fddec2eabcf8701fe9488de2
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-19 15:57:37 -0400

    Revert "chore: bump integration version"
    This reverts commit ab6589214a044aefecd05abeacf28d0d040ad1e5.

--- integrations/calendly/integration.definition.ts
+++ integrations/calendly/integration.definition.ts
@@ -5,7 +5,7 @@ import { inviteeEventOutputSchema } from 'definitions/events'
 export default new IntegrationDefinition({
   name: 'calendly',
   title: 'Calendly',
-  version: '0.0.2',
+  version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
   description: 'Schedule meetings and manage events using the Calendly scheduling platform.',
