7627edc04f212ec2fbc0407d9143dfa6ee70be72
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-08-25 13:10:00 -0400

    feat: implement scaffold for send reminder action

--- integrations/docusign/integration.definition.ts
+++ integrations/docusign/integration.definition.ts
@@ -1,4 +1,4 @@
-import { IntegrationDefinition } from '@botpress/sdk'
+import { IntegrationDefinition, z } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
   name: 'docusign',
@@ -8,4 +8,16 @@ export default new IntegrationDefinition({
   icon: 'icon.svg',
   description:
     'Automate document workflows, generate intelligent insights, enhance security measures, and improve user experience.',
+  actions: {
+    sendReminder: {
+      title: 'Send Reminder',
+      description: 'Sends a reminder to for the recipient of a document to sign it',
+      input: {
+        schema: z.object({}),
+      },
+      output: {
+        schema: z.object({}),
+      },
+    },
+  },
 })
--- integrations/docusign/src/index.ts
+++ integrations/docusign/src/index.ts
@@ -3,7 +3,11 @@ import * as bp from '.botpress'
 export default new bp.Integration({
   register: async () => {},
   unregister: async () => {},
-  actions: {},
+  actions: {
+    sendReminder: async () => {
+      return {}
+    },
+  },
   channels: {},
   handler: async () => {},
 })
