4329e93161819792896c60a9d9a4963f30bc556f
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-08-28 13:32:47 -0400

    chore(integrations/hubspot): Create empty integration

---
+++ integrations/hubspot/integration.definition.ts
@@ -0,0 +1,8 @@
+import { IntegrationDefinition } from '@botpress/sdk'
+
+export default new IntegrationDefinition({
+  name: 'hubspot',
+  version: '0.0.1',
+  readme: 'hub.md',
+  icon: 'icon.svg',
+})
