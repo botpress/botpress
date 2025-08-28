220fd9990b7807f9c1de9491f4052998dcdcbbeb
Paul <paul.chevilley@gmail.com>
2025-08-28 08:40:49 -0400

    feat(integrations): add cal.com integration (#14170)

---
+++ integrations/calcom/src/actions/generateLink.ts
@@ -0,0 +1,17 @@
+import { ActionHandlerProps } from '@botpress/sdk/dist/integration'
+import { CalcomApi } from 'src/calcom.api'
+import { TIntegration } from '.botpress'
+import { Input } from '.botpress/implementation/typings/actions/generateLink/input'
+
+export async function generateLink(
+  props: ActionHandlerProps<TIntegration, 'generateLink', Input>
+): Promise<{ url: string }> {
+  const { input, ctx } = props
+
+  const calcom = new CalcomApi(ctx.configuration.calcomApiKey, props.logger.forBot())
+  const url = await calcom.generateLink(input.email, input.eventTypeId)
+
+  return {
+    url,
+  }
+}
