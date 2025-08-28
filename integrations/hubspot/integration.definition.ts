2ab76e9f579fe4bde3518657dd0364290c62722c
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-08-28 14:44:42 -0400

    Fix BP lint errors

--- integrations/hubspot/integration.definition.ts
+++ integrations/hubspot/integration.definition.ts
@@ -2,6 +2,8 @@ import { IntegrationDefinition } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
   name: 'hubspot',
+  title: 'Hubspot',
+  description: 'Manage contacts, tickets and more from your chatbot.',
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
