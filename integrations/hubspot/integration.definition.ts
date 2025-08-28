0dfe1a46387a8d8de8b2cf0d197c6b43d7f4deda
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-08-28 17:00:13 -0400

    Add empty events to fix CI issues

--- integrations/hubspot/integration.definition.ts
+++ integrations/hubspot/integration.definition.ts
@@ -1,4 +1,4 @@
-import { IntegrationDefinition } from '@botpress/sdk'
+import { IntegrationDefinition, z } from '@botpress/sdk'
 
 export default new IntegrationDefinition({
   name: 'hubspot',
@@ -7,4 +7,16 @@ export default new IntegrationDefinition({
   version: '0.0.1',
   readme: 'hub.md',
   icon: 'icon.svg',
+  events: {
+    contactCreated: {
+      title: 'Contact Created',
+      description: 'A new contact has been created in Hubspot.',
+      schema: z.object({}),
+    },
+    contactUpdated: {
+      title: 'Contact Updated',
+      description: 'A contact has been updated in Hubspot.',
+      schema: z.object({}),
+    },
+  },
 })
