2473a5c5b4cadc309ee6f294a06ba6236b3e7517
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-09-01 09:15:36 -0400

    chore(integrations/hubspot): Move definitions in separate file and create skeleton for contact actions

--- integrations/hubspot/src/actions/index.ts
+++ integrations/hubspot/src/actions/index.ts
@@ -1,4 +1,5 @@
 import * as contactActions from './contact'
+import { createTicket } from './create-ticket'
 import * as dealActions from './deal'
 import * as leadActions from './lead'
 import * as bp from '.botpress'
@@ -7,4 +8,5 @@ export default {
   ...contactActions,
   ...dealActions,
   ...leadActions,
+  createTicket,
 } as const satisfies bp.IntegrationProps['actions']
