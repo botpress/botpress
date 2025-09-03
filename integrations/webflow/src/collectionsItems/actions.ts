910b47142e76811a1ebfb2ece8dbb0914d135435
Mathieu Faucher <mathieufaucher@Mathieus-MacBook-Pro.local>
2025-09-03 10:53:57 -0400

    working version of simpler client

--- integrations/webflow/src/collectionsItems/actions.ts
+++ integrations/webflow/src/collectionsItems/actions.ts
@@ -1,52 +1,23 @@
 import * as sdk from '@botpress/sdk'
 import axios from 'axios'
-import { WeblowClientBuilder } from 'src/client'
+
+import { WebflowClient } from 'src/client'
 import * as bp from '../../.botpress'
 import { ItemOutput } from '../types'
 
 export const listItems: bp.IntegrationProps['actions']['listItems'] = async (props) => {
-  try {
-    const builder = new WeblowClientBuilder()
-    props.logger.debug('1')
-    builder.producePath(
-      `/collections/${props.input.collectionID}/items${props.input.isLiveItems ? '/live' : ''}?offset=${props.input.pagination?.offset ?? 0}&limit=${props.input.pagination?.limit ?? 100}`
-    )
-    props.logger.debug('2')
-    builder.produceToken(props.input.apiTokenOverwrite ?? props.ctx.configuration.apiToken)
-    props.logger.debug('3')
-    const { data } = await builder.getWebflowClient().sendRequest()
-    props.logger.debug('4')
-
-    return data
-  } catch (err) {
-    if (axios.isAxiosError(err)) {
-      const serverMessage = (err.response?.data as any)?.message || err.response?.statusText || err.message
-      throw new sdk.RuntimeError(`webflow API error: ${serverMessage}`)
-    }
-    throw err
-  }
+  const client = new WebflowClient(props.ctx.configuration.apiToken)
+  return await client.listItems(
+    props.input.collectionID,
+    props.input.pagination?.offset ?? 0,
+    props.input.pagination?.limit ?? 100,
+    props.input.isLiveItems
+  )
 }
 
 export const getItem: bp.IntegrationProps['actions']['getItem'] = async (props) => {
-  const apiToken = props.input.apiTokenOverwrite ? props.input.apiTokenOverwrite : props.ctx.configuration.apiToken
-
-  try {
-    const response = await axios.get<ItemOutput>(
-      `https://api.webflow.com/v2/collections/${props.input.collectionID}/items/${props.input.itemID}${props.input.isLiveItems ? '/live' : ''}`,
-      {
-        headers: {
-          Authorization: `Bearer ${apiToken}`,
-        },
-      }
-    )
-    return { itemDetails: response.data }
-  } catch (err) {
-    if (axios.isAxiosError(err)) {
-      const serverMessage = (err.response?.data as any)?.message || err.response?.statusText || err.message
-      throw new sdk.RuntimeError(`webflow API error: ${serverMessage}`)
-    }
-    throw err
-  }
+  const client = new WebflowClient(props.ctx.configuration.apiToken)
+  return await client.getItem(props.input.collectionID, props.input.itemID, props.input.isLiveItems)
 }
 
 export const createItems: bp.IntegrationProps['actions']['createItems'] = async (props) => {
