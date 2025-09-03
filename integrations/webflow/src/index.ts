2c1c0f79d48afccefeaf8b289f00e2d7e3c293c9
François Levasseur <francois_levasseur@hotmail.com>
2025-09-03 14:32:17 -0400

    chore(integrations/webflow): improve error handling (#14189)

--- integrations/webflow/src/index.ts
+++ integrations/webflow/src/index.ts
@@ -1,3 +1,5 @@
+import * as sdk from '@botpress/sdk'
+import axios from 'axios'
 import * as bp from '../.botpress'
 import { WebflowClient } from './client'
 
@@ -7,108 +9,92 @@ export default new bp.Integration({
   actions: {
     async listCollections(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.listCollections(props.ctx.configuration.siteID)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .listCollections(props.ctx.configuration.siteID)
+        .catch(_handleError('Failed to list collections'))
     },
 
     async getCollectionDetails(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.getCollectionDetails(props.input.collectionID)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .getCollectionDetails(props.input.collectionID)
+        .catch(_handleError('Failed to get collection details'))
     },
 
     async createCollection(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.createCollection(props.ctx.configuration.siteID, props.input.collectionInfo)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .createCollection(props.ctx.configuration.siteID, props.input.collectionInfo)
+        .catch(_handleError('Failed to create collection'))
     },
 
     async deleteCollection(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.deleteCollection(props.input.collectionID)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client.deleteCollection(props.input.collectionID).catch(_handleError('Failed to delete collection'))
     },
 
     async listItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.listItems(
-          props.input.collectionID,
-          props.input.pagination?.offset ?? 0,
-          props.input.pagination?.limit ?? 100,
-          props.input.isLiveItems
-        )
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .listItems(props.input.collectionID, props.input.pagination?.offset ?? 0, props.input.pagination?.limit ?? 100)
+        .catch(_handleError('Failed to list items'))
     },
 
     async getItem(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.getItem(props.input.collectionID, props.input.itemID, props.input.isLiveItems)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .getItem(props.input.collectionID, props.input.itemID, props.input.isLiveItems)
+        .catch(_handleError('Failed to get item'))
     },
 
     async createItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.createItems(props.input.collectionID, props.input.items, props.input.isLiveItems)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .createItems(props.input.collectionID, props.input.items, props.input.isLiveItems)
+        .catch(_handleError('Failed to create items'))
     },
 
     async updateItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.updateItem(props.input.collectionID, props.input.items, props.input.isLiveItems)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .updateItem(props.input.collectionID, props.input.items, props.input.isLiveItems)
+        .catch(_handleError('Failed to update items'))
     },
 
     async deleteItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.deleteItem(props.input.collectionID, props.input.itemIDs)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .deleteItem(props.input.collectionID, props.input.itemIDs)
+        .catch(_handleError('Failed to delete items'))
     },
 
     async publishItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.publishItems(props.input.collectionID, props.input.itemIds)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .publishItems(props.input.collectionID, props.input.itemIds)
+        .catch(_handleError('Failed to publish items'))
     },
 
     async unpublishLiveItems(props) {
       const client = new WebflowClient(props.ctx.configuration.apiToken)
-      try {
-        return await client.unpublishLiveItems(props.input.collectionID, props.input.itemIds)
-      } catch (thrown) {
-        throw thrown instanceof Error ? thrown : new Error(String(thrown))
-      }
+      return await client
+        .unpublishLiveItems(props.input.collectionID, props.input.itemIds)
+        .catch(_handleError('Failed to unpublish live items'))
     },
   },
   channels: {},
   handler: async () => {},
 })
+
+const _handleError = (outterMessage: string) => (thrown: unknown) => {
+  let innerMessage: string | undefined = undefined
+  if (axios.isAxiosError(thrown)) {
+    innerMessage = thrown.response?.data?.message || thrown.message
+  } else {
+    const err = thrown instanceof Error ? thrown : new Error(String(thrown))
+    innerMessage = err.message
+  }
+
+  const fullMessage = innerMessage ? `${outterMessage}: ${innerMessage}` : outterMessage
+  throw new sdk.RuntimeError(fullMessage)
+}
