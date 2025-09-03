4e27468c56be98bace5aac3785e69adea8df461b
François Levasseur <francois_levasseur@hotmail.com>
2025-09-03 14:33:45 -0400

    chore(integrations/webflow): rm unused types

--- integrations/webflow/src/types.ts
+++
@@ -1,7 +0,0 @@
-import * as bp from '../.botpress'
-
-export type Collection = bp.actions.listCollections.output.Output['collections'][number]
-export type CollectionDetails = bp.actions.getCollectionDetails.output.Output['collectionDetails']
-export type ItemOutput = bp.actions.listItems.output.Output['items'][number]
-export type ItemInput = bp.actions.createItems.input.Input['items'][number]
-export type Pagination = bp.actions.listItems.input.Input['pagination']
