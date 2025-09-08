fbb36f637a6bed03c9c9e3dc7707113d603daa8f
François Levasseur <francois_levasseur@hotmail.com>
2025-09-08 15:36:09 -0400

    chore: refac some eslint config files

--- packages/llmz/eslint.config.mjs
+++ packages/llmz/eslint.config.mjs
@@ -3,7 +3,7 @@ import rootConfig from '../../eslint.config.mjs'
 export default [
   ...rootConfig,
   {
-    ignores: ['examples/**/*', 'src/__tests__/**/*', 'tsup.config.ts', 'vitest.*.ts'],
+    ignores: ['src/__tests__/**/*', 'tsup.config.ts', 'vitest.*.ts'],
     languageOptions: {
       parserOptions: {
         project: ['./tsconfig.json'],
