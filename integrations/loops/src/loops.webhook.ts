496d9c48bfa6e568d93e9664e1773fa728ef2c79
yxl05 <yang.xiang.64@hotmail.com>
2025-09-09 00:26:35 -0400

    ...

--- integrations/loops/src/loops.webhook.ts
+++ integrations/loops/src/loops.webhook.ts
@@ -1,14 +1,6 @@
 import { TIntegration } from '.botpress'
-import { RuntimeError } from '@botpress/sdk'
 import { WebhookHandlerProps } from '@botpress/sdk/dist/integration'
 
 export const verifyRequest = async (props: WebhookHandlerProps<TIntegration>): Promise<void> => {
-  const req = props.req
-  const eventId = req.headers['webhook-id']
-  const timeStamp = req.headers['webhook-timestamp']
-  const webhookSignature = req.headers['webhook-signature']
-
-  if (!eventId || !timeStamp || !webhookSignature) {
-    throw new RuntimeError('Webhook request is missing required headers')
-  }
+  // TODO: implement
 }
