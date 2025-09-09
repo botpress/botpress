6ecee946082395e5f11f6cc94b435210b7a9f5d5
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-09-09 17:16:22 -0400

    chore(integrations/intercom,cicd): Temporarily remove OAuth, reenable deployment and make client secret optional

--- integrations/intercom/integration.definition.ts
+++ integrations/intercom/integration.definition.ts
@@ -10,30 +10,42 @@ export default new IntegrationDefinition({
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration: {
-    identifier: {
-      linkTemplateScript: 'linkTemplate.vrl',
-      required: true,
-    },
     schema: z.object({
       adminId: z.string().min(1).describe('The admin ID of the Bot'),
+      accessToken: z.string().min(1).describe('The access token of the Intercom app'),
+      clientSecret: z
+        .string()
+        .min(1)
+        .secret()
+        .optional()
+        .describe('The client secret of the Intercom app, used for event signature validation'),
     }),
-  },
-  configurations: {
-    manual: {
-      title: 'Manual Configuration',
-      description: 'Manual configuration, use your own Intercom app (for advanced use cases only)',
-      schema: z.object({
-        adminId: z.string().min(1).describe('The admin ID of the Bot'),
-        accessToken: z.string().min(1).describe('The access token of the Intercom app'),
-        clientSecret: z
-          .string()
-          .min(1)
-          .secret()
-          .describe(
-            'The client secret of the Intercom app. Required for event signature validation, even if not authenticated by OAuth'
-          ),
-      }),
-    },
+    // TODO: Uncomment this once the Intercom app is approved
+    //   identifier: {
+    //     linkTemplateScript: 'linkTemplate.vrl',
+    //     required: true,
+    //   },
+    //   schema: z.object({
+    //     adminId: z.string().min(1).describe('The admin ID of the Bot'),
+    //   }),
+    // },
+    // configurations: {
+    //   manual: {
+    //     title: 'Manual Configuration',
+    //     description: 'Manual configuration, use your own Intercom app (for advanced use cases only)',
+    //     schema: z.object({
+    //       adminId: z.string().min(1).describe('The admin ID of the Bot'),
+    //       accessToken: z.string().min(1).describe('The access token of the Intercom app'),
+    //       clientSecret: z
+    //         .string()
+    //         .min(1)
+    //         .secret()
+    //         .describe(
+    //           'The client secret of the Intercom app. Required for event signature validation, even if not authenticated by OAuth'
+    //         ),
+    //     }),
+    //   },
+    // },
   },
   channels: {
     channel: {
