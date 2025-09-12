a927a4003f7f06f816b28f26aab87454d031941f
Xavier Hamel <xavier.hamel@botpress.com>
2025-09-12 13:27:58 -0400

    filter webhook request for handled topics

--- integrations/feature-base/src/handler.ts
+++ integrations/feature-base/src/handler.ts
@@ -12,6 +12,15 @@ const webhookRequestSchema = z.union([
   commentCreatedSchema,
 ])
 
+const webhookTopicSchema = z.object({
+  topic: z.string(),
+})
+
+const isHandeledTopic = (request: z.infer<typeof webhookTopicSchema>) => {
+  const topics: string[] = webhookRequestSchema.options.map((option) => option.shape.topic.value)
+  return topics.includes(request.topic)
+}
+
 export const handler: bp.IntegrationProps['handler'] = async (props) => {
   if (!props.req.body) {
     props.logger.error('Handler received an empty body')
@@ -26,7 +35,21 @@ export const handler: bp.IntegrationProps['handler'] = async (props) => {
     return
   }
 
+  const topicResult = webhookTopicSchema.safeParse(json)
+
+  if (!topicResult.success) {
+    props.logger.error(`Failed to validate request body: ${topicResult.error.message}`)
+    return
+  }
+
+  // We check that the request is actually a topic that can be handle by the handler. This prevent
+  // from throwing an error because we are not able to parse the payload.
+  if (!isHandeledTopic(topicResult.data)) {
+    return
+  }
+
   const { success, error, data: webhookRequestPayload } = webhookRequestSchema.safeParse(json)
+
   if (!success) {
     props.logger.error(`Failed to validate request body: ${error.message}`)
     return
