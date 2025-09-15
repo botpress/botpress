351b840b578969dd1e1bc3172f02bd4832b2b9c0
Simon Risk <simonnrisk@gmail.com>
2025-09-15 16:05:17 -0400

    chore: update tsconfig, package.json

--- integrations/klaviyo/integration.definition.ts
+++ integrations/klaviyo/integration.definition.ts
@@ -1,12 +1,11 @@
 import { IntegrationDefinition, z } from '@botpress/sdk'
 import { actions } from './definitions'
-import { integrationName } from './package.json'
 
 export default new IntegrationDefinition({
-  name: integrationName,
+  name: 'klaviyo',
   title: 'Klaviyo',
-  description: 'Connect with Klaviyo to manage customer profiles, segments, and marketing campaigns',
-  version: '0.1.18',
+  description: 'Manage customer profiles, generate leads, and curate marketing campaigns',
+  version: '0.1.19',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
