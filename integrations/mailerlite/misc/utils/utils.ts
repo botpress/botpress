d8c0721c94e5e660b2a0a0b4c69e291188e09365
sham-bolic <maxcyfong@gmail.com>
2025-09-16 14:08:46 -0400

    putting json parse in a try catch

--- integrations/mailerlite/misc/utils/utils.ts
+++ integrations/mailerlite/misc/utils/utils.ts
@@ -1,8 +1,16 @@
+import { RuntimeError } from '@botpress/client'
+
 export const getRequestPayload = <T extends { email: string; customFields?: string }>(
   input: T
 ): Record<string, any> => {
   const { email, customFields, ...rest } = input
-  const parsedCustomFields = customFields ? JSON.parse(customFields) : {}
+
+  let parsedCustomFields = {}
+  try {
+    parsedCustomFields = customFields ? JSON.parse(customFields) : {}
+  } catch (e) {
+    throw new RuntimeError('Parsing JSON failed with error ', e instanceof Error ? e : undefined)
+  }
   return {
     email,
     fields: {
