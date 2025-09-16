d94798de63d58e6e5f2faedd9833cc7aae5b2924
Sylvain Perron <sylvain.perron@botpress.com>
2025-09-16 19:12:05 -0400

    fix(client): added support for bun

--- packages/client/src/common/consts.ts
+++ packages/client/src/common/consts.ts
@@ -6,5 +6,5 @@ const _100mb = 100 * 1024 * 1024
 
 export const maxBodyLength = _100mb
 export const maxContentLength = _100mb
-export const httpAgent = isNode ? new http.Agent({ keepAlive: true }) : undefined
-export const httpsAgent = isNode ? new https.Agent({ keepAlive: true }) : undefined
+export const httpAgent = isNode && http && http.Agent ? new http.Agent({ keepAlive: true }) : undefined
+export const httpsAgent = isNode && https && https.Agent ? new https.Agent({ keepAlive: true }) : undefined
