af872f5fee3cf939382ed4960c2a09f14de9ed34
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-09-19 16:40:20 -0300

    fix: handler not used in integration index file

--- integrations/docusign/src/index.ts
+++ integrations/docusign/src/index.ts
@@ -1,4 +1,5 @@
 import actions from './actions'
+import { handler } from './handler'
 import * as bp from '.botpress'
 
 export default new bp.Integration({
@@ -6,5 +7,5 @@ export default new bp.Integration({
   unregister: async () => {},
   actions,
   channels: {},
-  handler: async () => {},
+  handler,
 })
