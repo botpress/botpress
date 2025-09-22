033916191b46d02f6247d0cf1b01ed797a268666
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-09-22 13:15:08 -0300

    feat: add more webhook events in the schema

--- integrations/docusign/src/events/schemas.ts
+++ integrations/docusign/src/events/schemas.ts
@@ -1,7 +1,14 @@
 import { z } from '@botpress/sdk'
 
 export const docusignEvelopeEventSchema = z.object({
-  event: z.union([z.literal('envelope-sent'), z.literal('envelope-completed')]),
+  event: z.union([
+    z.literal('envelope-sent'),
+    z.literal('envelope-resent'),
+    z.literal('envelope-delivered'),
+    z.literal('envelope-completed'),
+    z.literal('envelope-declined'),
+    z.literal('envelope-voided'),
+  ]),
   generatedDateTime: z.string().datetime(),
   data: z.object({
     userId: z.string(),
