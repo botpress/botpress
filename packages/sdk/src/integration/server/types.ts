8e047980de2b00a3dfc0703deeff0dd6a6cbaa92
Pascal Breton <179493770+pascal-botpress@users.noreply.github.com>
2025-10-08 10:30:11 -0400

    feat(sdk): update integration server to support aliases

--- packages/sdk/src/integration/server/types.ts
+++ packages/sdk/src/integration/server/types.ts
@@ -21,6 +21,7 @@ export type IntegrationContext<TIntegration extends BaseIntegration = BaseIntegr
   botId: string
   botUserId: string
   integrationId: string
+  integrationAlias: string
   webhookId: string
   operation: string
 } & IntegrationContextConfig<TIntegration>
