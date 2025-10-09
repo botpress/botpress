f92fcbc7b6ee040bc45e2d24b50d487b7663a5e6
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-10-09 11:56:49 -0400

    Fix bug where we tried subscribing pages in manual configuration

--- integrations/messenger/src/setup.ts
+++ integrations/messenger/src/setup.ts
@@ -1,16 +1,14 @@
-import { RuntimeError } from '@botpress/sdk'
-import { getMetaClientCredentials } from './misc/auth'
-import { createMetaClient } from './misc/meta-client'
+import { getOAuthMetaClientCredentials } from './misc/auth'
+import { createAuthenticatedMetaClient } from './misc/meta-client'
 
 import * as bp from '.botpress'
-import type { ManualConfig } from '.botpress/implementation/typings/configurations/manual'
 
 type RegisterProps = Parameters<bp.IntegrationProps['register']>[0]
 
 export const register: bp.IntegrationProps['register'] = async (props) => {
   const { ctx } = props
   if (ctx.configurationType === 'manual') {
-    await _registerManual(props, ctx.configuration)
+    await _registerManual(props)
   } else if (ctx.configurationType === 'sandbox') {
     await _registerSandbox(props)
   } else {
@@ -20,28 +18,9 @@ export const register: bp.IntegrationProps['register'] = async (props) => {
 
 export const unregister: bp.IntegrationProps['unregister'] = async () => {}
 
-const _registerManual = async (props: RegisterProps, config: ManualConfig) => {
-  const { client, logger, ctx } = props
-  logger.forBot().debug('Registering manual')
-
-  const { pageId, accessToken } = config
-
-  if (!accessToken || !pageId) {
-    throw new RuntimeError('Missing access token or page ID')
-  }
-
-  try {
-    const metaClient = await createMetaClient(ctx, client, logger)
-
-    await _clearAllIdentifiers(props)
-    await _unsubscribeFromOAuthWebhooks(props)
-    await metaClient.subscribeToWebhooks(pageId)
-
-    logger.forBot().info(`Successfully registered manual integration for page ${pageId}`)
-  } catch (thrown) {
-    const error = thrown instanceof Error ? thrown : new Error(String(thrown))
-    throw new RuntimeError('Failed to register manual integration: ' + error.message, error)
-  }
+const _registerManual = async (props: RegisterProps) => {
+  await _clearAllIdentifiers(props)
+  await _unsubscribeFromOAuthWebhooks(props)
 }
 
 const _registerSandbox = async (props: RegisterProps) => {
