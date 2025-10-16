55bf614350388e85eeafda0a5a077c72aeddd909
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-10-16 17:13:34 -0400

    formatted with prettier

--- integrations/workable/integration.definition.ts
+++ integrations/workable/integration.definition.ts
@@ -2,16 +2,24 @@ import { z, IntegrationDefinition } from '@botpress/sdk'
 import { getCandidate, getCandidates } from 'definitions/actions'
 
 export default new IntegrationDefinition({
-  name: "workable",
-  title: "Workable",
+  name: 'workable',
+  title: 'Workable',
   description: 'Integration with Workable for Botpress',
   version: '0.1.0',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
     schema: z.object({
-      apiToken: z.string().min(1, 'API token is required').describe('Your Workable API access token').title('API access token'),
-      subDomain: z.string().min(1, 'Account subdomain is required').describe('Your account subdomain is required').title('Account subdomain'),
+      apiToken: z
+        .string()
+        .min(1, 'API token is required')
+        .describe('Your Workable API access token')
+        .title('API access token'),
+      subDomain: z
+        .string()
+        .min(1, 'Account subdomain is required')
+        .describe('Your account subdomain is required')
+        .title('Account subdomain'),
     }),
   },
   actions: {
