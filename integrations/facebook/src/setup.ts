61fc9dd50aaea05d352cc91793c463a4d998b41a
Sébastien Poitras <sebastien.poitras@botpress.com>
2025-10-22 10:38:15 -0400

    fix(integrations/facebook): Make sure `pageId` is set before unsubscribing from webhooks (#14367)

--- integrations/facebook/src/setup.ts
+++ integrations/facebook/src/setup.ts
@@ -1,6 +1,5 @@
 import { getOAuthMetaClientCredentials } from './misc/auth'
 import { createAuthenticatedMetaClient } from './misc/meta-client'
-
 import * as bp from '.botpress'
 
 type RegisterProps = Parameters<bp.IntegrationProps['register']>[0]
@@ -27,7 +26,13 @@ const _unsubscribeFromOAuthWebhooks = async ({ ctx, logger, client }: RegisterPr
   }
 
   const { pageId } = credentials
+  if (!pageId) {
+    // No page ID means the OAuth flow was probably never fully completed
+    return
+  }
+
   const metaClient = await createAuthenticatedMetaClient({ configType: 'oauth', ctx, client, logger })
+
   if (await metaClient.isSubscribedToWebhooks(pageId)) {
     await metaClient.unsubscribeFromWebhooks(pageId)
   }
