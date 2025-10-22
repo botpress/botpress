4b2a99dd79b4c27475eda985e9d0317cc21b4579
François Levasseur <francois_levasseur@hotmail.com>
2025-10-22 13:18:34 -0400

    chore(integrations/workable): use import aliases (#14371)

--- integrations/workable/definitions/actions/candidates.ts
+++ integrations/workable/definitions/actions/candidates.ts
@@ -1,19 +1,19 @@
 import { ActionDefinition } from '@botpress/sdk'
 import {
-  getCandidateInputModel,
-  getCandidateOutputModel,
-  listCandidatesInputModel,
-  listCandidatesOutputModel,
+  getCandidateInputSchema,
+  getCandidateOutputSchema,
+  listCandidatesInputSchema,
+  listCandidatesOutputSchema,
 } from 'definitions/models/candidates'
 
 export const listCandidates = {
   title: 'List candidates',
   description: 'List the candidates for a job',
   input: {
-    schema: listCandidatesInputModel,
+    schema: listCandidatesInputSchema,
   },
   output: {
-    schema: listCandidatesOutputModel,
+    schema: listCandidatesOutputSchema,
   },
 } satisfies ActionDefinition
 
@@ -21,9 +21,9 @@ export const getCandidate = {
   title: 'Get candidate',
   description: 'Get a candidate by ID',
   input: {
-    schema: getCandidateInputModel,
+    schema: getCandidateInputSchema,
   },
   output: {
-    schema: getCandidateOutputModel,
+    schema: getCandidateOutputSchema,
   },
 } satisfies ActionDefinition
