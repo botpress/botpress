56c737416237817efaf16552807db43a80f0f5ee
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-10-23 17:53:35 -0400

    fixed wrong event type

--- integrations/workable/definitions/events/candidates.ts
+++ integrations/workable/definitions/events/candidates.ts
@@ -15,7 +15,7 @@ export const candidateCreatedSchema = webhookEvent
 export const candidateMovedSchema = webhookEvent
   .extend({
     data: candidateSchema,
-    eventType: z.literal(eventTypes.Enum.candidate_created).title('Event Type').describe('The type of event'),
+    eventType: z.literal(eventTypes.Enum.candidate_moved).title('Event Type').describe('The type of event'),
   })
   .title('Data')
   .describe('Event data')
