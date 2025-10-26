f73d8973b36177dc193b78cf6e45091d4ca5f2d2
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-10-26 19:55:06 -0400

    working version before removal of markdown-it

--- integrations/twilio/src/markdown-to-twilio.test.ts
+++ integrations/twilio/src/markdown-to-twilio.test.ts
@@ -2,36 +2,39 @@ import { test, expect } from 'vitest'
 import { parseMarkdown } from './markdown-to-twilio'
 
 const mdGenericTests = {
-  Title_1: { input: '# H1', expected: 'H1' },
-  Title_2: { input: '## H2', expected: 'H2' },
-  Title_3: { input: '### H3', expected: 'H3' },
-  Ordered_list: { input: '1. orderedListItem1\n2. item2', expected: '1. orderedListItem1\n2. item2' },
-  Unordered_list: { input: '- unorderedListItem1\n- item2', expected: '- unorderedListItem1\n- item2' },
-  Horizontal_rule: { input: 'horizontal\n\n---\n\nrule', expected: 'horizontal\n---\nrule' },
-  Image: { input: '![image](https://tinyurl.com/mrv4bmyk)', expected: 'https://tinyurl.com/mrv4bmyk' },
-  Table: { input: '| 1 | 2 |\n| - | - |\n| a | b |', expected: '| 1 | 2 |\n| - | - |\n| a | b |' },
-  Footnote: { input: 'footnote[^1]\n\n[^1]: the footnote', expected: 'footnote[^1]\n[^1]: the footnote' },
-  Definition: { input: 'term\n: definition', expected: 'term\n: definition' },
-  Task_list: { input: '- [x] taskListItem1\n- [ ] item2', expected: '- [x] taskListItem1\n- [ ] item2' },
-  Emoji_direct: { input: 'emoji direct 😂', expected: 'emoji direct 😂' },
-  // Highlight: { input: '==Highlight==', expected: 'Highlight' }, // chatGPT does not seem to send these
-  Subscript: { input: 'H~2~O', expected: 'H2O' },
-  Superscript: { input: 'X^2^', expected: 'X2' },
-  Link_only: { input: 'https://www.example.com', expected: 'https://www.example.com' },
+  Title_1: { input: '# H1', expected: 'H1\n' },
+  Title_2: { input: '## H2', expected: 'H2\n' },
+  Title_3: { input: '### H3', expected: 'H3\n' },
+  Ordered_list: { input: '1. orderedListItem1\n2. item2', expected: '1. orderedListItem1\n2. item2\n' },
+  Unordered_list: { input: '- unorderedListItem1\n- item2', expected: '- unorderedListItem1\n- item2\n' },
+  Horizontal_rule: { input: 'horizontal\n\n---\n\nrule', expected: 'horizontal\n---\nrule\n' },
+  Image: { input: '![image](https://tinyurl.com/mrv4bmyk)', expected: 'https://tinyurl.com/mrv4bmyk\n' },
+  Image_with_fallback: { input: '![test](https://tinyurl.com/mrv4bmyk)', expected: 'https://tinyurl.com/mrv4bmyk\n' },
+  Table: { input: '| 1 | 2 |\n| - | - |\n| a | b |', expected: '| 1 | 2 |\n| a | b |\n' },
+  Footnote: { input: 'footnote[^1]\n\n[^1]: the footnote', expected: 'footnote[1]\n[1] the footnote\n' },
+  Definition: { input: 'term\n: definition', expected: 'term\n: definition\n' },
+  Task_list: { input: '- [x] taskListItem1\n- [ ] item2', expected: '☑︎ taskListItem1\n☐ item2\n' },
+  Emoji_direct: { input: 'emoji direct 😂', expected: 'emoji direct 😂\n' },
+  Link: { input: '[title](https://www.example.com)', expected: 'https://www.example.com\n' },
+  Link_only: { input: 'https://www.example.com', expected: 'https://www.example.com\n' },
+  italic_quote_list: {
+    input: `_underscore_\n> quote\n1. item1\n2. item2`,
+    expected: 'underscore\nQuote: “quote”\n1. item1\n2. item2\n',
+  },
 }
 
 const smsSpecificTests = {
-  Blockquote: { input: '> blockquote', expected: 'blockquote' },
-  Code: { input: '`code`', expected: 'code' },
-  Code_snippet: { input: '```\n{\n\ta: null\n}\n```', expected: '{\n\ta: null\n}' },
-  Bold_with_asterisk: { input: '**bold-asterisk**', expected: 'bold-asterisk' },
-  Bold_with_underscore: { input: '__bold-underscore__', expected: 'bold-underscore' },
-  Italic_with_asterisk: { input: '*italic-asterisk*', expected: 'italic-asterisk' },
-  Italic_with_underscore: { input: '_italic-underscore_', expected: 'italic-underscore' },
-  Strikethrough: { input: '~~strikethrough~~', expected: 'strikethrough' },
-  Link: { input: '[title](https://www.example.com)', expected: '[title](https://www.example.com)' },
+  Blockquote: { input: '> blockquote', expected: 'Quote: “blockquote”\n' },
+  Code: { input: '`code`', expected: 'code\n' },
+  Code_snippet: { input: '```\n{\n\ta: null\n}\n```', expected: '{\n\ta: null\n}\n' },
+  Bold_with_asterisk: { input: '**bold-asterisk**', expected: 'bold-asterisk\n' },
+  Bold_with_underscore: { input: '__bold-underscore__', expected: 'bold-underscore\n' },
+  Italic_with_asterisk: { input: '*italic-asterisk*', expected: 'italic-asterisk\n' },
+  Italic_with_underscore: { input: '_italic-underscore_', expected: 'italic-underscore\n' },
+  Strikethrough: { input: '~~strikethrough~~', expected: 'strikethrough\n' },
 }
 const smsTests = { ...mdGenericTests, ...smsSpecificTests }
+const rcsTests = smsTests
 
 const messengerSpecificTests = {
   Blockquote: { input: '> blockquote', expected: 'blockquote' },
@@ -42,12 +45,11 @@ const messengerSpecificTests = {
   Italic_with_asterisk: { input: '*italic-asterisk*', expected: '_italic-asterisk_' },
   Italic_with_underscore: { input: '_italic-underscore_', expected: '_italic-underscore_' },
   Strikethrough: { input: '~~strikethrough~~', expected: '~strikethrough~' },
-  Link: { input: '[title](https://www.example.com)', expected: 'https://www.example.com' },
 }
 const messengerTests = { ...mdGenericTests, ...messengerSpecificTests }
 
 const whatsappSpecificTests = {
-  Blockquote: { input: '> blockquote', expected: '> blockquote' },
+  Blockquote: { input: '> blockquote', expected: 'Quote: “blockquote”' },
   Code: { input: '`code`', expected: '`code`' },
   Code_snippet: { input: '```\n{\n\ta: null\n}\n```', expected: '```{\n\ta: null\n}```' },
   Bold_with_asterisk: { input: '**bold-asterisk**', expected: '*bold-asterisk*' },
@@ -55,35 +57,40 @@ const whatsappSpecificTests = {
   Italic_with_asterisk: { input: '*italic-asterisk*', expected: '_italic-asterisk_' },
   Italic_with_underscore: { input: '_italic-underscore_', expected: '_italic-underscore_' },
   Strikethrough: { input: '~~strikethrough~~', expected: '~strikethrough~' },
-  Link: { input: '[title](https://www.example.com)', expected: 'https://www.example.com' },
 }
 const whatsappTests = { ...mdGenericTests, ...whatsappSpecificTests }
 
 test.each(Object.entries(smsTests))(
-  '[SMS, MMS, RCS] Test %s',
+  '[SMS, MMS] Test %s',
   (_testName: string, testValues: { input: string; expected: string }): void => {
     const actual = parseMarkdown(testValues.input, 'sms/mms')
-    const actualRcs = parseMarkdown(testValues.input, 'rcs')
-    expect(actual).toBe(testValues.expected)
-    expect(actualRcs).toBe(testValues.expected)
-  }
-)
-
-test.each(Object.entries(messengerTests))(
-  '[Messenger] Test %s',
-  (_testName: string, testValues: { input: string; expected: string }): void => {
-    const actual = parseMarkdown(testValues.input, 'messenger')
     expect(actual).toBe(testValues.expected)
   }
 )
 
-test.each(Object.entries(whatsappTests))(
-  '[WhatsApp] Test %s',
-  (_testName: string, testValues: { input: string; expected: string }): void => {
-    const actual = parseMarkdown(testValues.input, 'whatsapp')
-    expect(actual).toBe(testValues.expected)
-  }
-)
+// test.each(Object.entries(rcsTests))(
+//   '[RCS] Test %s',
+//   (_testName: string, testValues: { input: string; expected: string }): void => {
+//     const actualRcs = parseMarkdown(testValues.input, 'rcs')
+//     expect(actualRcs).toBe(testValues.expected)
+//   }
+// )
+
+// test.each(Object.entries(messengerTests))(
+//   '[Messenger] Test %s',
+//   (_testName: string, testValues: { input: string; expected: string }): void => {
+//     const actual = parseMarkdown(testValues.input, 'messenger')
+//     expect(actual).toBe(testValues.expected)
+//   }
+// )
+
+// test.each(Object.entries(whatsappTests))(
+//   '[WhatsApp] Test %s',
+//   (_testName: string, testValues: { input: string; expected: string }): void => {
+//     const actual = parseMarkdown(testValues.input, 'whatsapp')
+//     expect(actual).toBe(testValues.expected)
+//   }
+// )
 
 const bigInput = `# H1
 ## H2
@@ -93,21 +100,38 @@ const bigInput = `# H1
 __bold-underscore__
 _italic-underscore_
 > blockquote
-1. orderedListItem1\n2. item2
-- unorderedListItem1\n- item2
+1. orderedListItem1
+2. item2
+- unorderedListItem1
+- item2
 \`code\`
-horizontal\n\n---\n\nrule
+horizontal
+
+---
+
+rule
 [title](https://www.example.com)
 ![image](https://tinyurl.com/mrv4bmyk)
-| 1 | 2 |\n| - | - |\n| a | b |
-\`\`\`\n{\n\ta: null\n}\n\`\`\`
-footnote[^1]\n\n[^1]: the footnote
-term\n: definition
+![test](https://tinyurl.com/mrv4bmyk)
+| 1 | 2 |
+| - | - |
+| a | b |
+\`\`\`
+{
+  a: null
+}
+\`\`\`
+footnote[^1]
+
+[^1]: the footnote
+
+term
+: definition
 ~~strikethrough~~
-- [x] taskListItem1\n- [ ] item2
+- [x] taskListItem1
+- [ ] item2
 emoji direct 😂
-H~2~0
-X^2^`
+`
 
 const expectedForBigInputSMS = `H1
 H2
@@ -116,22 +140,32 @@ bold-asterisk
 italic-asterisk
 bold-underscore
 italic-underscore
-blockquote
-1. orderedListItem1\n2. item2
-- unorderedListItem1\n- item2
+Quote: “blockquote”
+1. orderedListItem1
+2. item2
+- unorderedListItem1
+- item2
 code
-horizontal\n---\nrule
-[title](https://www.example.com)
+horizontal
+---
+rule
+https://www.example.com
 https://tinyurl.com/mrv4bmyk
-| 1 | 2 |\n| - | - |\n| a | b |
-{\n\ta: null\n}
-footnote[^1]\n[^1]: the footnote
-term\n: definition
+https://tinyurl.com/mrv4bmyk
+| 1 | 2 |
+| a | b |
+{
+  a: null
+}
+footnote[1]
+term
+: definition
 strikethrough
-- [x] taskListItem1\n- [ ] item2
+☑︎ taskListItem1
+☐ item2
 emoji direct 😂
-H20
-X2`
+[1] the footnote\n`
+const expectedForBigInputRCS = expectedForBigInputSMS
 
 const expectedForBigInputMessenger = `H1
 H2
@@ -147,15 +181,14 @@ blockquote
 horizontal\n---\nrule
 https://www.example.com
 https://tinyurl.com/mrv4bmyk
+https://tinyurl.com/mrv4bmyk
 | 1 | 2 |\n| - | - |\n| a | b |
 \`\`\`\n{\n\ta: null\n}\n\`\`\`
 footnote[^1]\n[^1]: the footnote
 term\n: definition
 ~strikethrough~
 - [x] taskListItem1\n- [ ] item2
-emoji direct 😂
-H20
-X2`
+emoji direct 😂`
 
 const expectedForBigInputWhatsApp = `H1
 H2
@@ -171,29 +204,31 @@ _italic-underscore_
 horizontal\n---\nrule
 https://www.example.com
 https://tinyurl.com/mrv4bmyk
+https://tinyurl.com/mrv4bmyk
 | 1 | 2 |\n| - | - |\n| a | b |
 \`\`\`{\n\ta: null\n}\`\`\`
 footnote[^1]\n[^1]: the footnote
 term\n: definition
 ~strikethrough~
 - [x] taskListItem1\n- [ ] item2
-emoji direct 😂
-H20
-X2`
+emoji direct 😂`
 
-test('[SMS, MMS, RCS] Multi-line multi markup test', () => {
+test('[SMS, MMS] Multi-line multi markup test', () => {
   const actual = parseMarkdown(bigInput, 'sms/mms')
-  const actualRcs = parseMarkdown(bigInput, 'rcs')
   expect(actual).toBe(expectedForBigInputSMS)
-  expect(actualRcs).toBe(expectedForBigInputSMS)
 })
 
-test('[Messenger] Multi-line multi markup test', () => {
-  const actual = parseMarkdown(bigInput, 'messenger')
-  expect(actual).toBe(expectedForBigInputMessenger)
-})
+// test('[RCS] Multi-line multi markup test', () => {
+//   const actualRcs = parseMarkdown(bigInput, 'rcs')
+//   expect(actualRcs).toBe(expectedForBigInputRCS)
+// })
 
-test('[WhatsApp] Multi-line multi markup test', () => {
-  const actual = parseMarkdown(bigInput, 'whatsapp')
-  expect(actual).toBe(expectedForBigInputWhatsApp)
-})
+// test('[Messenger] Multi-line multi markup test', () => {
+//   const actual = parseMarkdown(bigInput, 'messenger')
+//   expect(actual).toBe(expectedForBigInputMessenger)
+// })
+
+// test('[WhatsApp] Multi-line multi markup test', () => {
+//   const actual = parseMarkdown(bigInput, 'whatsapp')
+//   expect(actual).toBe(expectedForBigInputWhatsApp)
+// })
--- integrations/twilio/src/markdown-to-twilio.ts
+++ integrations/twilio/src/markdown-to-twilio.ts
@@ -3,7 +3,141 @@ import MarkdownIt from 'markdown-it'
 import MarkdownItSub from 'markdown-it-sub'
 // @ts-ignore
 import MarkdownItSup from 'markdown-it-sup'
+import {
+  Blockquote,
+  Delete,
+  Emphasis,
+  FootnoteDefinition,
+  Heading,
+  List,
+  ListItem,
+  Paragraph,
+  Root,
+  Strong,
+  Table,
+  TableCell,
+} from 'mdast'
+import { remark } from 'remark'
+import remarkGfm from 'remark-gfm'
 import { TwilioChannel } from './types'
+
+const _stripAllMarkdown = (markdown: string): string => {
+  const tree = remark().use(remarkGfm).parse(markdown)
+  console.log('tree:', tree, '\n=====================')
+  const ans = _visitTree(tree)
+  return ans
+}
+
+type RootNodes =
+  | Root
+  | Heading
+  | Paragraph
+  | List
+  | ListItem
+  | Strong
+  | Emphasis
+  | Blockquote
+  | Delete
+  | Table
+  | TableCell
+  | FootnoteDefinition
+// const _visitTree = (tree: Parent): string => {
+const _visitTree = (tree: RootNodes): string => {
+  let tmp = ''
+  let footnoteTmp = ''
+  const isRoot = tree.type === 'root'
+  for (const node of tree.children) {
+    console.log('node:', node, '\n=====================')
+    switch (node.type) {
+      case 'blockquote':
+        tmp = `${tmp}Quote: “${_visitTree(node)}”\n`
+        break
+      case 'break':
+        tmp = `${tmp}\n`
+        break
+      case 'code':
+        tmp = `${tmp}${node.value}\n`
+        break
+      case 'delete':
+        tmp = `${tmp}${_visitTree(node)}`
+        break
+      case 'emphasis':
+        tmp = `${tmp}${_visitTree(node)}`
+        break
+      case 'footnoteDefinition':
+        footnoteTmp = `${footnoteTmp}[${node.identifier}] ${_visitTree(node)}\n`
+        break
+      case 'footnoteReference':
+        tmp = `${tmp}[${node.identifier}]`
+        break
+      case 'heading':
+        tmp = `${tmp}${_visitTree(node)}\n`
+        break
+      case 'image':
+        tmp = `${tmp}${node.url}`
+        break
+      case 'inlineCode':
+        tmp = `${tmp}${node.value}`
+        break
+      case 'link':
+        tmp = `${tmp}${node.url}`
+        break
+      case 'list':
+        tmp = `${tmp}${_handleList(node)}`
+        break
+      case 'paragraph':
+        tmp = `${tmp}${_visitTree(node)}${isRoot ? '\n' : ''}`
+        break
+      case 'strong':
+        tmp = `${tmp}${_visitTree(node)}`
+        break
+      case 'table':
+        tmp = `${tmp}${_handleTable(node)}`
+        break
+      case 'text':
+        tmp = `${tmp}${node.value}`
+        break
+      case 'thematicBreak':
+        tmp = `${tmp}---\n`
+        break
+      default:
+        console.error('unhandledError')
+    }
+  }
+  return `${tmp}${footnoteTmp}`
+}
+
+const _handleList = (listNode: List): string => {
+  let listTmp = ''
+  let itemCount = 0
+  for (const listItemNode of listNode.children) {
+    console.log('listItemNode:', listItemNode, '\n=====================')
+    let prefix = ''
+    if (listItemNode.checked !== undefined && listItemNode.checked !== null) {
+      prefix = listItemNode.checked ? '☑︎ ' : '☐ '
+    } else {
+      prefix = listNode.ordered ? `${++itemCount}. ` : '- '
+    }
+    listTmp = `${listTmp}${prefix}${_visitTree(listItemNode)}\n`
+  }
+  return listTmp
+}
+
+const _handleTable = (tableNode: Table): string => {
+  let tmpTable = ''
+  for (const tableRow of tableNode.children) {
+    console.log('tableRowNode:', tableRow, '\n=====================')
+    let childrenCount = 0
+    let tmpRow = '| '
+    for (const tableCell of tableRow.children) {
+      console.log('tableCellNode:', tableCell, '\n=====================')
+      tmpRow = `${tmpRow}${_visitTree(tableCell)}${childrenCount + 1 === tableRow.children.length ? ' |' : ' | '}`
+      childrenCount++
+    }
+    tmpTable = `${tmpTable}${tmpRow}\n`
+  }
+  return tmpTable
+}
 
 const md = MarkdownIt({
   html: false,
@@ -21,12 +155,12 @@ export const parseMarkdown = (markdown: string, channel: TwilioChannel): string 
     case 'whatsapp':
       return _markdownToWhatsApp(markdown)
     case 'rcs':
-      return _markdownToPlainText(markdown)
+      return _stripAllMarkdown(markdown)
     case 'sms/mms':
-      return _markdownToPlainText(markdown)
+      return _stripAllMarkdown(markdown)
     default:
       channel satisfies never
-      return _markdownToPlainText(markdown)
+      return _stripAllMarkdown(markdown)
   }
 }
 
