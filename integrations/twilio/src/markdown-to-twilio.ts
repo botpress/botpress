c740589664c2c27a794fd73e9e1a32e5d0265975
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-10-26 20:03:36 -0400

    remove markdown-it

--- integrations/twilio/src/markdown-to-twilio.ts
+++ integrations/twilio/src/markdown-to-twilio.ts
@@ -1,8 +1,3 @@
-import MarkdownIt from 'markdown-it'
-// @ts-ignore
-import MarkdownItSub from 'markdown-it-sub'
-// @ts-ignore
-import MarkdownItSup from 'markdown-it-sup'
 import {
   Blockquote,
   Delete,
@@ -139,21 +134,12 @@ const _handleTable = (tableNode: Table): string => {
   return tmpTable
 }
 
-const md = MarkdownIt({
-  html: false,
-  xhtmlOut: false,
-  breaks: false,
-})
-  .disable(['table', 'list', 'hr', 'link'])
-  .use(MarkdownItSub)
-  .use(MarkdownItSup)
-
 export const parseMarkdown = (markdown: string, channel: TwilioChannel): string => {
   switch (channel) {
     case 'messenger':
-      return _markdownToMessenger(markdown)
+      throw new Error('not implemented yet')
     case 'whatsapp':
-      return _markdownToWhatsApp(markdown)
+      throw new Error('not implemented yet')
     case 'rcs':
       return _stripAllMarkdown(markdown)
     case 'sms/mms':
@@ -162,97 +148,4 @@ export const parseMarkdown = (markdown: string, channel: TwilioChannel): string 
       channel satisfies never
       return _stripAllMarkdown(markdown)
   }
-}
-
-const _markdownToPlainText = (markdown: string): string => {
-  return _removeEmptyLinesFromText(_removeHTMLTags(_extractImagesUrl(md.render(markdown)))).trim()
-}
-
-const _markdownToMessenger = (markdown: string): string => {
-  return _removeEmptyLinesFromText(
-    _removeHTMLTags(_extractUrl(_extractImagesUrl(_changeMessengerSpecificTags(md.render(markdown)))))
-  ).trim()
-}
-
-const _markdownToWhatsApp = (markdown: string): string => {
-  return _removeEmptyLinesFromText(
-    _removeHTMLTags(_extractUrl(_extractImagesUrl(_changeWhatsAppSpecificTags(md.render(markdown)))))
-  ).trim()
-}
-
-const _removeHTMLTags = (input: string): string => {
-  return input.replace(/<[^>]*>/g, '')
-}
-
-const _extractUrl = (input: string): string => {
-  return input.replace(/\[.*?\]\((https?:\/\/[^)]+)\)/g, '$1')
-}
-
-const _extractImagesUrl = (input: string): string => {
-  return input.replace('<img src="', '').replace('" alt="image">', '')
-}
-
-const _removeEmptyLinesFromText = (input: string): string => {
-  return input.split('\n').filter(_isNonEmptyLine).join('\n')
-}
-
-const _isNonEmptyLine = (line: string): boolean => {
-  return line.trim() !== ''
-}
-
-// https://www.facebook.com/help/147348452522644
-const _changeMessengerSpecificTags = (input: string): string => {
-  const firstPass: Record<string, string> = {
-    '<pre><code>': '```\n',
-    '</code></pre>': '```',
-  }
-  const secondPass: Record<string, string> = {
-    '<strong>': '*',
-    '</strong>': '*',
-    '<em>': '_',
-    '</em>': '_',
-    '<s>': '~',
-    '</s>': '~',
-    '<code>': '`',
-    '</code>': '`',
-  }
-
-  let tmp: string
-  tmp = _replaceTagsByValue(input, firstPass)
-  tmp = _replaceTagsByValue(tmp, secondPass)
-  return tmp
-}
-
-// https://faq.whatsapp.com/539178204879377
-const _changeWhatsAppSpecificTags = (input: string): string => {
-  const firstPass: Record<string, string> = {
-    '<pre><code>': '```',
-    '\n</code></pre>': '```',
-  }
-  const secondPass: Record<string, string> = {
-    '<strong>': '*',
-    '</strong>': '*',
-    '<em>': '_',
-    '</em>': '_',
-    '<s>': '~',
-    '</s>': '~',
-    '<code>': '`',
-    '</code>': '`',
-    '<blockquote>\n<p>': '> ',
-  }
-
-  let tmp: string
-  tmp = _replaceTagsByValue(input, firstPass)
-  tmp = _replaceTagsByValue(tmp, secondPass)
-  return tmp
-}
-
-const _replaceTagsByValue = (input: string, replaceRecord: Record<string, string>): string => {
-  Object.keys(replaceRecord).forEach((replace) => {
-    if (!replaceRecord[replace]) {
-      return
-    }
-    input = input.replaceAll(replace, replaceRecord[replace])
-  })
-  return input
 }
