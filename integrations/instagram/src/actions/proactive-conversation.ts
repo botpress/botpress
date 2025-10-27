c9e07f483ea9e502702258fbc5f8eea2a4e5456f
Houssam E. Righi <houssam.righi@botpress.com>
2025-10-27 09:50:42 -0400

    getOrCreateConversationDm to support commentId

--- integrations/instagram/src/actions/proactive-conversation.ts
+++ integrations/instagram/src/actions/proactive-conversation.ts
@@ -2,19 +2,26 @@ import { RuntimeError } from '@botpress/sdk'
 import { getCredentials, InstagramClient } from 'src/misc/client'
 import * as bp from '.botpress'
 
-export const getOrCreateConversationDm: bp.IntegrationProps['actions']['getOrCreateConversationDm'] = async ({
-  client,
-  ctx,
-  input,
-  logger,
-}) => {
+export const getOrCreateConversationDm: bp.IntegrationProps['actions']['getOrCreateConversationDm'] = async (props) => {
+  const { client, ctx, input, logger } = props
   if (ctx.configurationType === 'sandbox') {
     throw new RuntimeError('Starting a conversation is not supported in sandbox mode')
+  }
+
+  const { id: userId, commentId } = input.conversation
+
+  if (commentId) {
+    let { conversation } = await client.getOrCreateConversation({
+      channel: 'channel',
+      tags: { id: userId, commentId },
+      discriminateByTags: ['id'],
+    })
+    return { conversationId: conversation.id }
   }
 
   const credentials = await getCredentials(client, ctx)
   const metaClient = new InstagramClient(logger, credentials)
   const profile = await metaClient.getUserProfile(input.conversation.id)
-  const { conversation } = await client.getOrCreateConversation({ channel: 'channel', tags: { id: profile.id } })
+  let { conversation } = await client.getOrCreateConversation({ channel: 'channel', tags: { id: profile.id } })
   return { conversationId: conversation.id }
 }
