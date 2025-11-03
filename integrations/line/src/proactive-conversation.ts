78cf3a04b1b06134827b2c4f211b32d0d9353618
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-03 16:31:28 -0500

    fixed logic in getOrCreateConversation

--- integrations/line/src/proactive-conversation.ts
+++ integrations/line/src/proactive-conversation.ts
@@ -1,17 +1,29 @@
 import { RuntimeError } from '@botpress/sdk'
+import { messagingApi as lineMessagingApi } from '@line/bot-sdk'
 import * as bp from '.botpress'
 
 const getOrCreateConversation: bp.IntegrationProps['actions']['getOrCreateConversation'] = async ({
   client,
+  ctx,
   input,
 }) => {
-  const userId = input.conversation.id
-  if (!userId) {
+  const usrId = input.conversation.id
+  const destId = input.conversation.destinationId
+  if (!usrId) {
     throw new RuntimeError('User ID is required')
   }
+  if (!destId) {
+    throw new RuntimeError('Destination ID is required')
+  }
+
+  const lineClient = new lineMessagingApi.MessagingApiClient({
+    channelAccessToken: ctx.configuration.channelAccessToken,
+  })
+  const profile = await lineClient.getProfile(usrId)
+
   const { conversation } = await client.getOrCreateConversation({
     channel: 'channel',
-    tags: { usrId: userId },
+    tags: { usrId: `${profile.userId}`, destId },
   })
 
   return {
