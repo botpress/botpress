4b009572051a65a5fd7cf25cdf99c90a16d68cfa
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-11-04 09:23:57 -0500

    renamed for better readability

--- packages/sdk/src/version-utils.ts
+++ packages/sdk/src/version-utils.ts
@@ -3,11 +3,11 @@ import { InterfacePackage, IntegrationPackage, PluginPackage } from '.'
 
 type GenericPackage = InterfacePackage | IntegrationPackage | PluginPackage
 
-export function ofMajor<T extends GenericPackage>(pkg: T): T & { version: string } {
+export function packageOfMajor<T extends GenericPackage>(pkg: T): T & { version: string } {
   return { ...pkg, version: `>=${semver.major(pkg.version)}.0.0 <${semver.major(pkg.version) + 1}.0.0` }
 }
 
-export function ofMinor<T extends GenericPackage>(pkg: T): T & { version: string } {
+export function packageOfMinor<T extends GenericPackage>(pkg: T): T & { version: string } {
   const major = semver.major(pkg.version)
   const minor = semver.minor(pkg.version)
   return {
@@ -16,6 +16,6 @@ export function ofMinor<T extends GenericPackage>(pkg: T): T & { version: string
   }
 }
 
-export function exactly<T extends GenericPackage>(pkg: T): T & { version: string } {
+export function packageOfExactVersion<T extends GenericPackage>(pkg: T): T & { version: string } {
   return { ...pkg, version: `${pkg.version}` }
 }
