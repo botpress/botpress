82c585999040047833246a76599271deac092084
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-06 09:51:13 -0500

    added tests for markdown parser

--- packages/common/src/markdown-parser/markdown-parser.test.ts
+++ packages/common/src/markdown-parser/markdown-parser.test.ts
@@ -1,8 +1,13 @@
-import { test, expect, vi } from 'vitest'
-import { parseMarkdown } from './markdown-parser'
+import { Blockquote } from 'mdast'
+import { test, expect, vi, beforeEach, beforeAll } from 'vitest'
+import { MarkdownHandlers } from './types'
+
+const getMarkDownParser = async () => {
+  return await import('./markdown-parser')
+}
 
 const parseMock = vi.fn()
-vi.mock('remark', () => ({
+vi.doMock('remark', () => ({
   remark: vi.fn(() => ({
     use: vi.fn(() => ({
       parse: parseMock,
@@ -10,14 +15,47 @@ vi.mock('remark', () => ({
   })),
 }))
 
-vi.mock('./tree-visitor', () => ({
-  visitTree: vi.fn(),
+const visitTreeMock = vi.fn()
+vi.doMock('./tree-visitor', () => ({
+  visitTree: visitTreeMock,
 }))
 
-test('Assert that the remark parse function gets called with given markdown', () => {
+beforeAll(() => {})
+
+beforeEach(() => {
+  vi.restoreAllMocks()
+})
+
+test('Assert that the remark parse function gets called with given markdown', async () => {
   const markdown = 'any string'
+  const { parseMarkdown } = await getMarkDownParser()
 
   parseMarkdown(markdown)
 
   expect(parseMock).toHaveBeenCalledWith(markdown)
+})
+
+test('Assert that visitTree gets called with correct parameters', async () => {
+  const markdown = 'any string'
+  parseMock.mockReturnValue('abc')
+  const { parseMarkdown, stripAllHandlers } = await getMarkDownParser()
+
+  parseMarkdown(markdown)
+
+  expect(visitTreeMock).toHaveBeenCalledWith('abc', stripAllHandlers, [])
+})
+
+test('Assert that visitTree gets called with custom handlers', async () => {
+  const markdown = 'any string'
+  parseMock.mockReturnValue('abc')
+  const { parseMarkdown, stripAllHandlers } = await getMarkDownParser()
+
+  const customHandlers: MarkdownHandlers = {
+    ...stripAllHandlers,
+    blockquote: (_node, _visit) => 'custom handler',
+  }
+
+  parseMarkdown(markdown, customHandlers)
+
+  expect(visitTreeMock).toHaveBeenCalledWith('abc', customHandlers, [])
 })
