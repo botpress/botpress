c2293240f7914a2c780f7cd6c66f22d3b37b5940
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-11-06 10:53:20 -0500

    Add slack

--- bots/change-lens/bot.definition.ts
+++ bots/change-lens/bot.definition.ts
@@ -1,5 +1,6 @@
 import { BotDefinition } from '@botpress/sdk'
 import * as sdk from '@botpress/sdk'
+import slack from 'bp_modules/slack'
 
 export default new BotDefinition({
   states: { metaApiVersions: { type: 'bot', schema: sdk.z.object({ graphApiVersion: sdk.z.string() }) } },
@@ -14,5 +15,17 @@ export default new BotDefinition({
       schedule: { cron: '0 * * * *' },
       payload: sdk.z.object({}),
     },
+  },
+}).addIntegration(slack, {
+  enabled: true,
+  configurationType: null, // we can't use OAuth because another bot uses it
+  configuration: {
+    typingIndicatorEmoji: false,
+    createReplyThread: {
+      enabled: false,
+      onlyOnBotMention: false,
+    },
+    botAvatarUrl: 'https://files.bpcontent.cloud/2025/06/16/20/20250616204038-BRUW6C2R.svg',
+    botName: 'Change Lens',
   },
 })
