f9f9525b4a615a7dcd9637906e45780b152ddaa0
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-06 12:14:52 -0500

    refactored markdown handler and added tests

--- packages/common/src/markdown-parser/markdown-parser.ts
+++ packages/common/src/markdown-parser/markdown-parser.ts
@@ -10,6 +10,7 @@ import {
   InlineCode,
   Link,
   List,
+  Node,
   Paragraph,
   Strong,
   Table,
@@ -17,9 +18,7 @@ import {
 } from 'mdast'
 import { remark } from 'remark'
 import remarkGfm from 'remark-gfm'
-import { handleList, handleTable } from './handlers'
-import { visitTree } from './tree-visitor'
-import { MarkdownHandlers } from './types'
+import { MarkdownHandlers, NodeHandler, RootNodes } from './types'
 
 export const stripAllHandlers: MarkdownHandlers = {
   blockquote: (node, visit) => `Quote: “${visit(node as Blockquote)}”\n`,
@@ -41,6 +40,67 @@ export const stripAllHandlers: MarkdownHandlers = {
   table: (node, _visit, parents, handlers) => handleTable(node as Table, handlers, parents),
   text: (node, _visit) => (node as Text).value,
   thematicBreak: (_node, _visit) => '---\n',
+}
+
+const FIXED_SIZE_SPACE_CHAR = '\u2002' // 'En space' yields better results for identation in WhatsApp messages
+
+export const handleList = (listNode: List, handlers: MarkdownHandlers, parents: RootNodes[]): string => {
+  parents.push(listNode)
+  const listLevel = parents.filter((parent) => parent.type === 'list').length
+  let listTmp = listLevel !== 1 ? '\n' : ''
+  let itemCount = 0
+  for (const listItemNode of listNode.children) {
+    itemCount++
+    let prefix = FIXED_SIZE_SPACE_CHAR.repeat(listLevel - 1)
+    if (listItemNode.checked !== undefined && listItemNode.checked !== null) {
+      prefix += listItemNode.checked ? '☑︎ ' : '☐ '
+    } else {
+      prefix += listNode.ordered ? `${itemCount}. ` : '- '
+    }
+    const shouldBreak = listLevel === 1 || itemCount !== listNode.children.length
+    listTmp = `${listTmp}${prefix}${visitTree(listItemNode, handlers, parents)}${shouldBreak ? '\n' : ''}`
+  }
+  parents.pop()
+  return listTmp
+}
+
+export const handleTable = (tableNode: Table, handlers: MarkdownHandlers, parents: RootNodes[]): string => {
+  parents.push(tableNode)
+  let tmpTable = ''
+  for (const tableRow of tableNode.children) {
+    let childrenCount = 0
+    let tmpRow = '| '
+    for (const tableCell of tableRow.children) {
+      tmpRow = `${tmpRow}${visitTree(tableCell, handlers, parents)}${childrenCount + 1 === tableRow.children.length ? ' |' : ' | '}`
+      childrenCount++
+    }
+    tmpTable = `${tmpTable}${tmpRow}\n`
+  }
+  parents.pop()
+  return tmpTable
+}
+
+const isNodeType = (s: string, handlers: MarkdownHandlers): s is keyof MarkdownHandlers => s in handlers
+
+export const visitTree = (tree: RootNodes, handlers: MarkdownHandlers, parents: RootNodes[]): string => {
+  let tmp = ''
+  let footnoteTmp = ''
+  parents.push(tree)
+  for (const node of tree.children) {
+    if (!isNodeType(node.type, handlers)) {
+      throw new Error(`The Markdown node type [${node.type}] is not supported`)
+    }
+
+    const handler = handlers[node.type] as NodeHandler<Node>
+
+    if (node.type === 'footnoteDefinition') {
+      footnoteTmp += handler(node, (n) => visitTree(n, handlers, parents), parents, handlers)
+      continue
+    }
+    tmp += handler(node, (n) => visitTree(n, handlers, parents), parents, handlers)
+  }
+  parents.pop()
+  return `${tmp}${footnoteTmp}`
 }
 
 export const parseMarkdown = (markdown: string, handlers: MarkdownHandlers = stripAllHandlers): string => {
