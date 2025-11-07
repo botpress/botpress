b7e007385882f7c54d2c828058f11679a7a6070c
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-11-07 14:22:52 -0500

    better expiry design

--- packages/sdk/src/plugin/state-proxy/proxy.ts
+++ packages/sdk/src/plugin/state-proxy/proxy.ts
@@ -1,4 +1,4 @@
-import { Client, RuntimeError } from '@botpress/client'
+import { Client } from '@botpress/client'
 import { BotSpecificClient, StateType } from '../../bot'
 import * as consts from '../../consts'
 import { BasePlugin, PluginRuntimeProps } from '../common'
@@ -8,16 +8,10 @@ class _StateRepo<TPayload extends object> implements StateRepo<TPayload> {
   public constructor(
     private _client: BotSpecificClient<any> | Client,
     private _stateType: StateType,
-    private _stateName: string,
-    private _expiryMs?: number
+    private _stateName: string
   ) {}
 
-  public withExpiry(expiryMs: number): _StateRepo<TPayload> {
-    return new _StateRepo<TPayload>(this._client, this._stateType, this._stateName, expiryMs)
-  }
-
   public async get(id: string): Promise<TPayload> {
-    if (this._expiryMs) throw new RuntimeError('You cannot set an expiry when getting a state')
     return await this._client
       .getState({
         type: this._stateType,
@@ -27,31 +21,30 @@ class _StateRepo<TPayload extends object> implements StateRepo<TPayload> {
       .then((r) => r.state.payload)
   }
 
-  public async set(id: string, payload: TPayload): Promise<void> {
+  public async set(id: string, payload: TPayload, options?: { expiryMs: number }): Promise<void> {
     await this._client.setState({
       type: this._stateType,
       name: this._stateName,
       id,
       payload,
-      expiry: this._expiryMs,
+      expiry: options?.expiryMs,
     })
     return
   }
 
-  public async getOrSet(id: string, payload: TPayload): Promise<TPayload> {
+  public async getOrSet(id: string, payload: TPayload, options?: { expiryMs: number }): Promise<TPayload> {
     return await this._client
       .getOrSetState({
         type: this._stateType,
         name: this._stateName,
         id,
         payload,
-        expiry: this._expiryMs,
+        expiry: options?.expiryMs,
       })
       .then((r) => r.state.payload)
   }
 
   public async delete(id: string): Promise<void> {
-    if (this._expiryMs) throw new RuntimeError('You cannot set an expiry when deleting a state')
     await this._client.setState({
       type: this._stateType,
       name: this._stateName,
@@ -62,7 +55,6 @@ class _StateRepo<TPayload extends object> implements StateRepo<TPayload> {
   }
 
   public async patch(id: string, payload: Partial<TPayload>): Promise<void> {
-    if (this._expiryMs) throw new RuntimeError('You cannot set an expiry when patching a state')
     await this._client.patchState({
       type: this._stateType,
       name: this._stateName,
@@ -87,7 +79,7 @@ export const proxyStates = <TPlugin extends BasePlugin>(
             get: (_target, stateName: string) => {
               const actualName =
                 props.alias !== undefined ? `${props.alias}${consts.PLUGIN_PREFIX_SEPARATOR}${stateName}` : stateName
-              return new _StateRepo(client, stateType, actualName, undefined)
+              return new _StateRepo(client, stateType, actualName)
             },
           }
         )
