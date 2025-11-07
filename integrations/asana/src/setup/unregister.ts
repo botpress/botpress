28909b531c576b631cda8feb5a4cc094e39157d5
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-07 11:13:09 +0700

    Fix typos in some files

--- integrations/asana/src/setup/unregister.ts
+++ integrations/asana/src/setup/unregister.ts
@@ -3,6 +3,6 @@ import type { UnregisterFunction } from '../misc/types'
 export const unregister: UnregisterFunction = async () => {
   /**
    * This is called when a bot removes the integration.
-   * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
+   * You should use this handler to instantiate resources in the external service and ensure that the configuration is valid.
    */
 }
