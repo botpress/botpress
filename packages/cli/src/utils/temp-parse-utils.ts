cd10eb942e6a599a42d286a427602f600f2246f0
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-11-07 12:31:23 -0500

    feat: add logging to the tryParseJSON function

--- packages/cli/src/utils/temp-parse-utils.ts
+++ packages/cli/src/utils/temp-parse-utils.ts
@@ -4,10 +4,12 @@ export function tryParseJSON<T extends any = any>(raw: string, debugText: string
   try {
     return JSON.parse(raw)
   } catch (thrown: unknown) {
+    const logger = new Logger()
     const error = thrown instanceof Error ? thrown : new Error(String(thrown))
-    new Logger().error(
+    logger.error(
       `Failed to read JSON file (${debugText}) -> "${error.message}"\n\n${JSON.stringify({ raw }, null, 2)}\n\n${error.stack ?? ''}`.trim()
     )
+    logger.log('======================')
     throw error
   }
 }
