77edfac329c965541da4e499c4e0652abf0eb7c1
Pascal B <179493770+pascal-botpress@users.noreply.github.com>
2025-11-12 10:59:13 -0500

    feat(sdk): add/extend plugin proxies (#14459)

--- packages/sdk/src/plugin/event-proxy/types.ts
+++ packages/sdk/src/plugin/event-proxy/types.ts
@@ -1,14 +1,19 @@
-import { ClientInputs } from '@botpress/client'
-import * as utils from '../../utils/type-utils'
-import { BasePlugin } from '../common'
+import type * as client from '@botpress/client'
+import type { AsyncCollection } from '../../utils/api-paging-utils'
+import type * as utils from '../../utils/type-utils'
+import type { BasePlugin } from '../common'
 
-export type EventSchedule = NonNullable<ClientInputs['createEvent']['schedule']>
+export type EventSchedule = NonNullable<client.ClientInputs['createEvent']['schedule']>
 export type EventSender<TPayload> = {
-  emit: (event: TPayload) => Promise<void>
-  schedule: (event: TPayload, schedule: EventSchedule) => Promise<void>
+  emit: (event: TPayload) => Promise<client.Event>
+  schedule: (event: TPayload, schedule: EventSchedule) => Promise<client.Event>
   withConversationId: (conversationId: string) => EventSender<TPayload>
   withUserId: (userId: string) => EventSender<TPayload>
   withMessageId: (messageId: string) => EventSender<TPayload>
+  list: (
+    props?: Omit<client.ClientInputs['listEvents'], 'type' | 'nextToken' | 'conversationId' | 'messageId' | 'userId'>
+  ) => AsyncCollection<client.Event>
+  getById: (props: { id: string }) => Promise<client.Event>
 }
 
 export type EventProxy<TPlugin extends BasePlugin> = utils.Normalize<{
