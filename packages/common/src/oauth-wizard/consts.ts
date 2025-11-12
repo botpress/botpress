4a0539a10f7c04581e53dced569f7e6814bb22fe
Houssam Eddine Righi <94550978+Horisofine@users.noreply.github.com>
2025-11-12 09:34:15 -0500

    feat(packages/wizard): Introduced input dialog to the wizard handler (#14486)
    Co-authored-by: Houssam E. Righi <houssam.righi@botpress.com>

--- packages/common/src/oauth-wizard/consts.ts
+++ packages/common/src/oauth-wizard/consts.ts
@@ -1,3 +1,4 @@
 export const DISABLE_INTERSTITIAL_HEADER = { 'x-bp-disable-interstitial': 'true' } as const
 export const BASE_WIZARD_PATH = '/oauth/wizard/'
 export const CHOICE_PARAM = 'wizchoice'
+export const INPUT_PARAM = 'wizinput'
