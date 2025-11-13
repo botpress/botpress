c7d32c3a15f2a312639d46f2d7e80c352614a047
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-11-13 15:21:38 -0500

    fix: lint sendData

--- integrations/make/integration.definition.ts
+++ integrations/make/integration.definition.ts
@@ -20,25 +20,28 @@ export default new IntegrationDefinition({
   channels: {},
   actions: {
     sendData: {
+      description: 'Send Data as a JSON string',
+      title: 'Send Data',
       input: {
-        schema: z
-          .object({
-            data: z.string().min(1, { message: 'Must not be empty' }).describe('JSON string of data to send'),
-          })
-          .describe('Input schema for sending data'),
+        schema: z.object({
+          data: z
+            .string()
+            .min(1, { message: 'Must not be empty' })
+            .describe('JSON string of data to send')
+            .title('Data'),
+        }),
       },
       output: {
-        schema: z
-          .object({
-            success: z.boolean().describe('True if the data was sent successfully'),
-            response: z
-              .any()
-              .describe(
-                'Data received from Make.com, will be the string `Accepted` if successful and no data is returned'
-              )
-              .nullable(),
-          })
-          .describe('Output schema after sending data, expecting any JSON structure'),
+        schema: z.object({
+          success: z.boolean().describe('True if the data was sent successfully').title('Success'),
+          response: z
+            .any()
+            .describe(
+              'Data received from Make.com, will be the string `Accepted` if successful and no data is returned'
+            )
+            .title('Reponse')
+            .nullable(),
+        }),
       },
     },
   },
