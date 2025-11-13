e8af7f3d1c33eeacc3a6235838046ee4b346b1ff
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-11-13 15:03:52 -0500

    chore(twilio): use common posthog

--- packages/common/src/posthog/events.ts
+++ packages/common/src/posthog/events.ts
@@ -1,6 +1,7 @@
 export const botpressEvents = {
   INVALID_PHONE_NUMBER: 'invalid_phone_number',
-  UNHANDLED_MESSAGE_TYPE: 'unhandled_message_type',
   UNHANDLED_ERROR: 'unhandled_error',
+  UNHANDLED_MARKDOWN: 'unhandled_markdown',
+  UNHANDLED_MESSAGE_TYPE: 'unhandled_message_type',
 } as const
 export type BotpressEvent = (typeof botpressEvents)[keyof typeof botpressEvents]
