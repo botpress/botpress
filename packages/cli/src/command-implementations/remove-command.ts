471c19d0537c5adaea66702639d3fec50dcf98c1
Nathaniel Girard <nathaniel.girard1@gmail.com>
2025-11-14 16:32:57 -0500

    refactored command

--- packages/cli/src/command-implementations/remove-command.ts
+++ packages/cli/src/command-implementations/remove-command.ts
@@ -1,5 +1,6 @@
 import * as sdk from '@botpress/sdk'
 import * as fslib from 'fs'
+import { BP_DEPENDENCIES_KEY, PKGJSON_FILE_NAME } from 'src/utils/pkgjson-utils'
 import type commandDefinitions from '../command-definitions'
 import * as consts from '../consts'
 import * as errors from '../errors'
@@ -9,19 +10,32 @@ import { ProjectCommand } from './project-command'
 export type RemoveCommandDefinition = typeof commandDefinitions.remove
 export class RemoveCommand extends ProjectCommand<RemoveCommandDefinition> {
   protected async run(): Promise<void> {
-    const ref = this.argv.alias
-    if (!ref) {
+    const alias = this.argv.alias
+    if (!alias) {
       throw new errors.BotpressCLIError('You have to provide the alias of the package to remove')
     }
 
-    const baseInstallPath = utils.path.absoluteFrom(utils.path.cwd(), this.argv.workDir)
-    //remove from package.json
-    const pkgJson = await utils.pkgJson.readPackageJson(baseInstallPath)
+    const { validatedBpDeps, workDir, pkgJson } = await this._validatePkgJson()
+
+    const correspondingPackageAlias = this._findCorrespondingPackage(alias, validatedBpDeps)
+
+    this._removeDepFromBpModules(correspondingPackageAlias, workDir, alias)
+
+    await this._removeDepFromPkgJson(correspondingPackageAlias, validatedBpDeps, pkgJson, workDir, alias)
+  }
+
+  private async _validatePkgJson(): Promise<{
+    validatedBpDeps: Record<string, string>
+    workDir: utils.path.AbsolutePath
+    pkgJson: utils.pkgJson.PackageJson
+  }> {
+    const workDir = utils.path.absoluteFrom(utils.path.cwd(), this.argv.workDir)
+    const pkgJson = await utils.pkgJson.readPackageJson(workDir)
     if (!pkgJson) {
-      throw new errors.BotpressCLIError(`No package.json found in path ${baseInstallPath}`)
+      throw new errors.BotpressCLIError(`No ${PKGJSON_FILE_NAME} found in path ${workDir}`)
     }
 
-    const { bpDependencies } = pkgJson
+    const bpDependencies = pkgJson[BP_DEPENDENCIES_KEY]
     if (!bpDependencies) {
       throw new errors.BotpressCLIError('package')
     }
@@ -29,32 +43,42 @@ export class RemoveCommand extends ProjectCommand<RemoveCommandDefinition> {
     const bpDependenciesSchema = sdk.z.record(sdk.z.string())
     const parseResult = bpDependenciesSchema.safeParse(bpDependencies)
     if (!parseResult.success) {
-      throw new errors.BotpressCLIError('Invalid bpDependencies found in package.json')
+      throw new errors.BotpressCLIError(`Invalid ${BP_DEPENDENCIES_KEY} found in ${PKGJSON_FILE_NAME}`)
     }
+    return { validatedBpDeps: parseResult.data, workDir, pkgJson }
+  }
 
-    const { data: validatedBpDeps } = parseResult
-
-    const correspondingPackageAlias = this._findCorrespondingPackage(ref, validatedBpDeps)
-
-    //remove from bp_modules
+  private _removeDepFromBpModules(correspondingPackageAlias: string, workDir: utils.path.AbsolutePath, alias: string) {
     const packageDirName = utils.casing.to.kebabCase(correspondingPackageAlias)
-    const installPath = utils.path.join(baseInstallPath, consts.installDirName, packageDirName)
+    const installPath = utils.path.join(workDir, consts.installDirName, packageDirName)
     fslib.rmSync(installPath, { force: true, recursive: true })
+    this.logger.log(`Package "${alias}" was removed from bp_modules`)
+  }
 
-    //rewrite package.json
+  private async _removeDepFromPkgJson(
+    correspondingPackageAlias: string,
+    validatedBpDeps: Record<string, string>,
+    pkgJson: utils.pkgJson.PackageJson,
+    workDir: string,
+    alias: string
+  ) {
     const { [correspondingPackageAlias]: _, ...newBpDependencies } = validatedBpDeps
-    pkgJson.bpDependencies = newBpDependencies
-
-    await utils.pkgJson.writePackageJson(baseInstallPath, pkgJson)
+    pkgJson[BP_DEPENDENCIES_KEY] = newBpDependencies
+    await utils.pkgJson.writePackageJson(workDir, pkgJson)
+    this.logger.log(`Package with alias "${alias}" was removed from ${PKGJSON_FILE_NAME}`)
   }
 
   private _findCorrespondingPackage(alias: string, bpDependencies: Record<string, string>): string {
     const correspondingPackage: string | undefined = Object.keys(bpDependencies).find((key) => key === alias)
 
     if (!correspondingPackage) {
-      throw new errors.BotpressCLIError(`No corresponding package for alias ${alias} was found`)
+      throw new errors.BotpressCLIError(
+        `No corresponding package for alias "${alias}" was found in ${BP_DEPENDENCIES_KEY}`
+      )
     }
 
     return correspondingPackage
   }
 }
+
+//TODO add e2e tests
