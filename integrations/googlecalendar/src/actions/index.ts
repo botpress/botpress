c07bdcc30dc8fd0881090922402a465fe53c7184
quu-ack <quentin.ackermann@botpress.com>
2025-11-18 15:36:32 -0500

    feat(integrations/googlecalendar): add meet links, email invitations and availability checking

--- integrations/googlecalendar/src/actions/index.ts
+++ integrations/googlecalendar/src/actions/index.ts
@@ -1,6 +1,6 @@
+import { checkAvailability } from './implementations/check-availability'
 import { createEvent } from './implementations/create-event'
 import { deleteEvent } from './implementations/delete-event'
-
 import { listEvents } from './implementations/list-events'
 import { updateEvent } from './implementations/update-event'
 import * as bp from '.botpress'
@@ -10,4 +10,5 @@ export const actions = {
   deleteEvent,
   listEvents,
   updateEvent,
+  checkAvailability,
 } as const satisfies bp.IntegrationProps['actions']
