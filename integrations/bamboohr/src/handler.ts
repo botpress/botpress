631296618b0d1a5fd87408403e2f835945e2a8bd
François Levasseur <francois_levasseur@hotmail.com>
2025-11-20 15:59:46 -0500

    chore(bamboohr): refactor handler

--- integrations/bamboohr/src/handler.ts
+++ integrations/bamboohr/src/handler.ts
@@ -1,39 +1,54 @@
-import type { z } from '@botpress/sdk'
 import { bambooHrEmployeeWebhookEvent } from 'definitions'
 import { handleOauthRequest } from './api/auth'
 import { validateBambooHrSignature } from './api/signing'
-import { parseRequestWithErrors } from './api/utils'
+import { safeParseJson } from './api/utils'
 import { handleEmployeeCreatedEvent, handleEmployeeDeletedEvent, handleEmployeeUpdatedEvent } from './events'
 import * as bp from '.botpress'
+import { BambooHRRuntimeError } from './error-handling'
 
 const _isOauthRequest = ({ req }: bp.HandlerProps) => req.path === '/oauth'
 
-export const handler = async (props: bp.HandlerProps) => {
+export const handler: bp.IntegrationProps['handler'] = async (props) => {
   const { req, logger } = props
   if (_isOauthRequest(props)) {
-    try {
-      await handleOauthRequest(props)
-    } catch (err) {
-      logger.forBot().error('Error in OAuth creation flow: ' + (err as Error).message)
-      return { status: 500, body: 'Error handling OAuth creation flow' }
-    }
+    await handleOauthRequest(props).catch((thrown) => {
+      const err = BambooHRRuntimeError.from(thrown, 'Error in OAuth creation flow')
+      logger.forBot().error(err.message)
+      return { status: 500, body: err.message }
+    })
     return { status: 200 }
   }
 
-  try {
-    await validateBambooHrSignature(props)
-  } catch (err) {
-    logger.forBot().error('Error validating HMAC signature: ' + (err as Error).message)
-    return { status: 401, body: 'Invalid HMAC signature' }
+  const { state } = await props.client.getState({
+    name: 'webhook',
+    type: 'integration',
+    id: props.ctx.integrationId,
+  })
+  const privateKey = state.payload.privateKey
+  if (!privateKey) {
+    logger.forBot().error('No private key found for webhook state.')
+    return
   }
 
-  let event: z.output<typeof bambooHrEmployeeWebhookEvent>
-  try {
-    event = await parseRequestWithErrors(req, bambooHrEmployeeWebhookEvent)
-  } catch (err) {
-    logger.forBot().error('Error parsing request body: ' + (err as Error).message)
-    return { status: 400, body: 'Invalid request body' }
+  const signingResult = await validateBambooHrSignature(props.req, privateKey)
+  if (!signingResult.success) {
+    logger.forBot().error('HMAC signature validation failed: ' + signingResult.reason)
+    return
   }
+
+  const jsonParseResult = safeParseJson(req.body ?? '')
+  if (!jsonParseResult.success) {
+    logger.forBot().error('Error parsing request body: ' + jsonParseResult.error)
+    return
+  }
+
+  const zodParseResult = bambooHrEmployeeWebhookEvent.safeParse(jsonParseResult.data)
+  if (!zodParseResult.success) {
+    logger.forBot().error('Error parsing request body: ' + zodParseResult.error.message)
+    return
+  }
+
+  const event = zodParseResult.data
 
   await Promise.all(
     event.employees.map(async (employee) => {
