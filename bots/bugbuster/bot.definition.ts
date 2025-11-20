fd16a92295a69f89927fa4de8190ca5f12653fe3
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-20 11:59:10 -0500

    added missing title and desc

--- bots/bugbuster/bot.definition.ts
+++ bots/bugbuster/bot.definition.ts
@@ -60,22 +60,25 @@ export default new sdk.BotDefinition({
     },
     issuesInStaging: {
       type: 'bot',
-      schema: sdk.z.object({
-        issues: sdk.z.array(
-          sdk.z.object({
-            id: sdk.z.string().title('IDs').describe('The IDs of the issues in staging'),
-            sinceTimestamp: sdk.z
-              .number()
-              .title('Since Timestamp')
-              .describe('The timestamp of when the issue was moved in staging'),
-            commentId: sdk.z
-              .string()
-              .optional()
-              .title('Comment ID')
-              .describe('The ID of the comment made on the issue by the bot if there is one'),
-          })
-        ),
-      }),
+      schema: sdk.z
+        .object({
+          issues: sdk.z.array(
+            sdk.z.object({
+              id: sdk.z.string().title('ID').describe('The ID of the issue'),
+              sinceTimestamp: sdk.z
+                .number()
+                .title('Since Timestamp')
+                .describe('The timestamp of when the issue was moved in staging'),
+              commentId: sdk.z
+                .string()
+                .optional()
+                .title('Comment ID')
+                .describe('The ID of the comment made on the issue by the bot if there is one'),
+            })
+          ),
+        })
+        .title('Issues')
+        .describe('The issues in staging'),
     },
   },
   workflows: {
