81380a7f4d130c0e82e177677a3b43c601777dd5
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-26 11:56:06 -0500

    added organization input field

--- integrations/github/src/definitions/actions.ts
+++ integrations/github/src/definitions/actions.ts
@@ -15,6 +15,13 @@ const findTarget = {
       query: z.string().title('Query').describe('The search query'),
       channel: z.enum(Channels).title('Channel').describe('The channel in which to execute the query'),
       repo: z.string().title('Repository').describe('The repository name'),
+      organization: z
+        .string()
+        .optional()
+        .title('Organization')
+        .describe(
+          'The organization that owns the repository. Leave empty if the repository is owned by the current user.'
+        ),
     }),
   },
   output: {
