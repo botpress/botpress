259d400ac31de1155d6b132ad1dc8c7134cfda3b
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-26 15:06:02 -0500

    added posthog package

--- integrations/github/src/index.ts
+++ integrations/github/src/index.ts
@@ -1,20 +1,25 @@
-import { sentry as sentryHelpers } from '@botpress/sdk-addons'
+import { posthogHelper } from '@botpress/common'
 import actions from './actions'
 import channels from './channels'
+import { INTEGRATION_NAME } from './const'
 import { handler } from './handler'
 import { register, unregister } from './setup'
 import * as bp from '.botpress'
 
-const integration = new bp.Integration({
-  register,
-  unregister,
-  handler,
-  actions,
-  channels,
+@posthogHelper.wrapIntegration({
+  integrationName: INTEGRATION_NAME,
+  key: bp.secrets.POSTHOG_KEY,
 })
+class GithubIntegration extends bp.Integration {
+  public constructor() {
+    super({
+      register,
+      unregister,
+      actions,
+      channels,
+      handler,
+    })
+  }
+}
 
-export default sentryHelpers.wrapIntegration(integration, {
-  dsn: bp.secrets.SENTRY_DSN,
-  environment: bp.secrets.SENTRY_ENVIRONMENT,
-  release: bp.secrets.SENTRY_RELEASE,
-})
+export default new GithubIntegration()
