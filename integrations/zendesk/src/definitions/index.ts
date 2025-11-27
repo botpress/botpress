ad06b5b5fb31b3d8778839a450fb94be176183e2
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-11-27 15:32:40 -0500

    remove all trace of manual config

--- integrations/zendesk/src/definitions/index.ts
+++ integrations/zendesk/src/definitions/index.ts
@@ -1,23 +1,5 @@
 import { z, IntegrationDefinitionProps } from '@botpress/sdk'
 
-const SHARED_CONFIGURATION = {
-  syncKnowledgeBaseWithBot: z
-    .boolean({
-      description: 'Would you like to sync Zendesk Knowledge Base into Bot Knowledge Base?',
-    })
-    .optional(),
-  knowledgeBaseId: z
-    .string({
-      description: 'ID of the Knowledge Base you wish to synchronize with your Zendesk KB',
-    })
-    .optional(),
-  ignoreNonHitlTickets: z
-    .boolean()
-    .optional()
-    .title('Ignore non-HITL tickets')
-    .describe('Ignore tickets that were not created by the startHitl action'),
-} as const
-
 export { actions } from './actions'
 export { channels } from './channels'
 
@@ -45,37 +27,28 @@ export const configuration = {
   identifier: {
     linkTemplateScript: 'linkTemplate.vrl',
   },
-  schema: z.object(SHARED_CONFIGURATION),
+  schema: z.object({
+    syncKnowledgeBaseWithBot: z
+      .boolean({
+        description: 'Would you like to sync Zendesk Knowledge Base into Bot Knowledge Base?',
+      })
+      .optional(),
+    knowledgeBaseId: z
+      .string({
+        description: 'ID of the Knowledge Base you wish to synchronize with your Zendesk KB',
+      })
+      .optional(),
+    ignoreNonHitlTickets: z
+      .boolean()
+      .optional()
+      .title('Ignore non-HITL tickets')
+      .describe('Ignore tickets that were not created by the startHitl action'),
+  }),
 } satisfies IntegrationDefinitionProps['configuration']
 
 export const identifier = {
   extractScript: 'extract.vrl',
 } as const satisfies IntegrationDefinitionProps['identifier']
-
-export const configurations = {
-  manual: {
-    title: 'Manual configuration',
-    description: 'Configure by manually supplying the refresh token and signing secret',
-    schema: z.object({
-      ...SHARED_CONFIGURATION,
-      organizationSubdomain: z
-        .string({
-          description: 'Your zendesk organization subdomain. e.g. botpress7281',
-        })
-        .min(1),
-      email: z
-        .string({
-          description: 'Your zendesk account email. e.g. john.doe@botpress.com',
-        })
-        .email(),
-      apiToken: z
-        .string({
-          description: 'Zendesk API Token',
-        })
-        .min(1),
-    }),
-  },
-} as const satisfies IntegrationDefinitionProps['configurations']
 
 export const states = {
   subscriptionInfo: {
