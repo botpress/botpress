84770a43377b72748c50d337133ef807b8f68bbf
François Levasseur <francois_levasseur@hotmail.com>
2025-11-27 16:37:15 -0500

    update

--- bots/bugbuster/src/bootstrap.ts
+++ bots/bugbuster/src/bootstrap.ts
@@ -1,15 +1,14 @@
 import { BotpressApi } from 'src/utils/botpress-utils'
-import { handleError } from 'src/utils/error-handler'
 import { LinearApi } from 'src/utils/linear-utils'
 import { IssueProcessor } from './services/issue-processor'
 import { TeamsManager } from './services/teams-manager'
-import { WorkflowHandlerProps, MessageHandlerProps, EventHandlerProps } from '.botpress'
+import * as types from './types'
 
-export type BootstrapProps = WorkflowHandlerProps['lintAll'] | MessageHandlerProps | EventHandlerProps
-export const bootstrap = async (props: BootstrapProps, conversationId?: string) => {
+export const bootstrap = async (props: types.CommonHandlerProps, conversationId?: string) => {
   const { client, logger, ctx } = props
-  const botpress = new BotpressApi(client, ctx.botId)
-  const _handleError = (context: string) => handleError({ context, logger, botpress, conversationId })
+  const botpress = await BotpressApi.create(props)
+  const _handleError = (context: string) => (thrown: unknown) =>
+    botpress.handleError({ context, conversationId }, thrown)
 
   const linear = await LinearApi.create().catch(_handleError('trying to initialize Linear API'))
   const teamsManager = new TeamsManager(linear, client, ctx.botId)
--- bots/bugbuster/src/types.ts
+++ bots/bugbuster/src/types.ts
@@ -1,5 +1,9 @@
+import * as bp from '.botpress'
+
 export type Result<T> = {
   success: boolean
   message: string
   result?: T
 }
+
+export type CommonHandlerProps = bp.WorkflowHandlerProps['lintAll'] | bp.EventHandlerProps | bp.MessageHandlerProps
