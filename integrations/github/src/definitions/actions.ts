0f99e8f47d69d9deadc6661e0bef65794c641146
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-27 11:41:39 -0500

    fixed variable name

--- integrations/github/src/definitions/actions.ts
+++ integrations/github/src/definitions/actions.ts
@@ -1,19 +1,19 @@
 import * as sdk from '@botpress/sdk'
 import { z } from '@botpress/sdk'
 
-const Channels = ['pullRequest', 'issue'] as const
+const channels = ['pullRequest', 'issue'] as const
 
 export type Target = {
   displayName: string
   tags: { [key: string]: string }
-  channel: (typeof Channels)[number]
+  channel: (typeof channels)[number]
 }
 
 const findTarget = {
   input: {
     schema: z.object({
       query: z.string().title('Query').describe('The search query'),
-      channel: z.enum(Channels).title('Channel').describe('The channel in which to execute the query'),
+      channel: z.enum(channels).title('Channel').describe('The channel in which to execute the query'),
       repo: z.string().title('Repository').describe('The repository name'),
       organization: z
         .string()
@@ -30,7 +30,7 @@ const findTarget = {
         z.object({
           displayName: z.string().title('Display Name').describe('The display name'),
           tags: z.record(z.string()).title('Tags').describe('The tags'),
-          channel: z.enum(Channels).title('Channel').describe('The channel'),
+          channel: z.enum(channels).title('Channel').describe('The channel'),
         })
       ),
     }),
