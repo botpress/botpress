c0d5afc1b567ae32c62168a98013acaac62fba11
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-11-28 15:12:41 -0500

    removed unused constant

--- bots/bugbuster/src/bootstrap.ts
+++ bots/bugbuster/src/bootstrap.ts
@@ -4,12 +4,9 @@ import { TeamsManager } from './services/teams-manager'
 import * as types from './types'
 import * as utils from './utils'
 
-export const bootstrap = async (props: types.CommonHandlerProps, conversationId?: string) => {
+export const bootstrap = async (props: types.CommonHandlerProps) => {
   const { client, logger, ctx } = props
   const botpress = utils.botpress.BotpressApi.create(props)
-
-  const _handleError = (context: string) => (thrown: unknown) =>
-    botpress.handleError({ context, conversationId }, thrown)
 
   const linear = await utils.linear.LinearApi.create()
   const teamsManager = new TeamsManager(linear, client, ctx.botId)
