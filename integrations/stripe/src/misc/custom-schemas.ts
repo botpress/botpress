b89f3da2e27cd2b03ca96300ed73eb1a94222b87
Mathieu Faucher <faucher.mathieu@gmail.com>
2025-11-27 21:48:40 -0500

    nitpicks

--- integrations/stripe/src/misc/custom-schemas.ts
+++ integrations/stripe/src/misc/custom-schemas.ts
@@ -2,65 +2,65 @@ import { z } from '@botpress/sdk'
 
 const partialCustomer = z
   .object({
-    id: z.string(),
-    email: z.string().nullable(),
-    name: z.string().nullable().optional(),
-    description: z.string().nullable(),
-    phone: z.string().nullable().optional(),
-    address: z.object({}).passthrough().nullable().optional(),
-    created: z.number(),
-    delinquent: z.boolean().nullable().optional(),
+    id: z.string().describe('The id of the customer').title('ID'),
+    email: z.string().nullable().describe('The email of the customer').title('Email'),
+    name: z.string().nullable().optional().describe('The name of the customer').title('Name'),
+    description: z.string().nullable().describe('The description of the customer').title('Description'),
+    phone: z.string().nullable().optional().describe('The phone of the customer').title('Phone'),
+    address: z.object({}).passthrough().nullable().optional().describe('The addess of the customer').title('Address'),
+    created: z.number().describe('The creation timestamp (UNIX) of the customer').title('Created'),
+    delinquent: z.boolean().nullable().optional().describe('If the customer is delinquent').title('Delinquent'),
   })
   .passthrough()
 
 export const createPaymentLinkInputSchema = z.object({
   productName: z
     .string()
-    .title('Product Name')
     .placeholder('ex: T-Shirt')
+    .title('Product Name')
     .describe('The name of the product to be sold'),
   unit_amount: z
     .number()
-    .title('Unit Price')
     .min(50, 'Amount must be at least 50 cents ($0.50 USD)')
     .optional()
     .default(50)
+    .title('Unit Price')
     .describe('The unit price in cents (minimum 50 cents for USD)'),
   currency: z
     .string()
-    .title('Currency')
     .optional()
     .default('usd')
+    .title('Currency')
     .describe('The currency in which the price will be expressed'),
   quantity: z.number().title('Quantity').optional().default(1).describe('The quantity of the product being purchased'),
   adjustableQuantity: z
     .boolean()
-    .title('Adjustable Quantity')
     .optional()
     .default(false)
+    .title('Adjustable Quantity')
     .describe('Whether or not the quantity can be adjusted'),
   adjustableQuantityMaximum: z
     .number()
-    .title('Max Quantity')
     .min(2)
     .max(999)
     .optional()
     .default(99)
+    .title('Max Quantity')
     .describe('The maximum quantity the customer can purchase, up to 999'),
   adjustableQuantityMinimum: z
     .number()
-    .title('Min Quantity')
     .min(1)
     .max(998)
     .optional()
     .default(1)
+    .title('Min Quantity')
     .describe('The minimum quantity the customer can purchase'),
 })
 
 export const createPaymentLinkOutputSchema = z
   .object({
-    id: z.string().describe('The ID of the created payment link'),
-    url: z.string().describe('The URL of the created payment link'),
+    id: z.string().describe('The ID of the created payment link').title('ID'),
+    url: z.string().describe('The URL of the created payment link').title('URL'),
   })
   .partial()
 
@@ -71,19 +71,27 @@ export const listProductPricesOutputSchema = z
     products: z
       .record(
         z.object({
-          name: z.string().describe('The name of the product'),
+          name: z.string().describe('The name of the product').title('Name'),
           prices: z
             .array(
               z.object({
-                unit_amount: z.number().nullable().describe('The unit amount for the product'),
-                currency: z.string().describe('The currency for the product'),
-                recurring: z.object({}).passthrough().nullable().optional().describe('Recurring price details'),
+                unit_amount: z.number().nullable().describe('The unit amount for the product').title('Unit Amount'),
+                currency: z.string().describe('The currency for the product').title('Currency'),
+                recurring: z
+                  .object({})
+                  .passthrough()
+                  .nullable()
+                  .optional()
+                  .describe('Recurring price details')
+                  .title('Recurring'),
               })
             )
-            .describe('A list of prices for the product'),
+            .describe('A list of prices for the product')
+            .title('Prices'),
         })
       )
-      .describe('A record of products and their prices'),
+      .describe('A record of products and their prices')
+      .title('Products'),
   })
   .partial()
 
@@ -141,8 +149,8 @@ export const createSubsLinkInputSchema = z.object({
 
 export const createSubsLinkOutputSchema = z
   .object({
-    id: z.string().describe('The ID of the created subscription link'),
-    url: z.string().describe('The URL of the created subscription link'),
+    id: z.string().describe('The ID of the created subscription link').title('ID'),
+    url: z.string().describe('The URL of the created subscription link').title('URL'),
   })
   .partial()
 
@@ -153,11 +161,12 @@ export const listPaymentLinksOutputSchema = z
     paymentLinks: z
       .array(
         z.object({
-          id: z.string().describe('The ID of the payment link'),
-          url: z.string().describe('The URL of the payment link'),
+          id: z.string().describe('The ID of the payment link').title('ID'),
+          url: z.string().describe('The URL of the payment link').title('URL'),
         })
       )
-      .describe('A list of payment links'),
+      .describe('A list of payment links')
+      .title('Payment Links'),
   })
   .partial()
 
@@ -171,7 +180,7 @@ export const findPaymentLinkInputSchema = z.object({
 
 export const findPaymentLinkOutputSchema = z
   .object({
-    id: z.string().describe('The ID of the found payment link'),
+    id: z.string().describe('The ID of the found payment link').title('ID'),
   })
   .partial()
 
@@ -185,67 +194,67 @@ export const deactivatePaymentLinkInputSchema = z.object({
 
 export const deactivatePaymentLinkOutputSchema = z
   .object({
-    id: z.string().describe('The ID of the deactivated payment link'),
-    url: z.string().describe('The URL of the deactivated payment link'),
-    active: z.boolean().describe('Whether the payment link is active'),
+    id: z.string().describe('The ID of the deactivated payment link').title('ID'),
+    url: z.string().describe('The URL of the deactivated payment link').title('URL'),
+    active: z.boolean().describe('Whether the payment link is active').title('Active'),
   })
   .partial()
 
 export const listCustomersInputSchema = z.object({
-  email: z.string().title('Email').email().max(512).optional().describe('The e-mail of the customer to search for'),
+  email: z.string().email().max(512).optional().describe('The e-mail of the customer to search for').title('Email'),
 })
 
 export const listCustomersOutputSchema = z
   .object({
-    customers: z.record(z.array(partialCustomer)).describe('A record of customers grouped by email'),
+    customers: z.record(z.array(partialCustomer)).describe('A record of customers grouped by email').title('Customers'),
   })
   .partial()
 
 export const searchCustomersInputSchema = z.object({
-  email: z.string().title('Email').max(512).optional().describe('Search by query on customer emails'),
-  name: z.string().title('Name').optional().describe('Search by query on customer name'),
-  phone: z.string().title('Phone').optional().describe('Search by query on customer phone number'),
+  email: z.string().title('Email').max(512).optional().describe('Search by query on customer emails').title('Email'),
+  name: z.string().title('Name').optional().describe('Search by query on customer name').title('Name'),
+  phone: z.string().title('Phone').optional().describe('Search by query on customer phone number').title('Phone'),
 })
 
 export const searchCustomersOutputSchema = z
   .object({
-    customers: z.array(partialCustomer).describe('A list of customers matching the search criteria'),
+    customers: z.array(partialCustomer).describe('A list of customers matching the search criteria').title('Customers'),
   })
   .partial()
 
 export const createCustomerInputSchema = z.object({
   email: z
     .string()
-    .title('Email')
     .email()
     .max(512)
+    .title('Email')
     .describe('The email of the customer')
     .placeholder('johndoe@botpress.com'),
-  name: z.string().title('Name').optional().describe('The name of the customer').placeholder('John Doe'),
-  phone: z.string().title('Phone').optional().describe('The phone number of the customer').placeholder('+1234567890'),
+  name: z.string().optional().describe('The name of the customer').placeholder('John Doe').title('Name'),
+  phone: z.string().optional().describe('The phone number of the customer').placeholder('+1234567890').title('Phone'),
   description: z
     .string()
-    .title('Description')
     .optional()
+    .title('Description')
     .describe('A description for the customer')
     .placeholder('Customer Description'),
   paymentMethodId: z
     .string()
-    .title('Payment Method ID')
     .optional()
+    .title('Payment Method ID')
     .describe('The ID of the payment method to attach to the customer')
     .placeholder('payment-method-id'),
   address: z
     .string()
-    .title('Address')
     .optional()
+    .title('Address')
     .describe('The address of the customer')
     .placeholder('123 Bot Street, Bot City, Botland, 12345'),
 })
 
 export const createCustomerOutputSchema = z
   .object({
-    customer: partialCustomer.describe('The created customer object'),
+    customer: partialCustomer.describe('The created customer object').title('Customer'),
   })
   .partial()
 
@@ -253,8 +262,12 @@ export const createOrRetrieveCustomerInputSchema = createCustomerInputSchema
 
 export const createOrRetrieveCustomerOutputSchema = z
   .object({
-    customer: partialCustomer.optional().describe('The created or retrieved customer object'),
-    customers: z.array(partialCustomer).optional().describe('A list of customers matching the criteria'),
+    customer: partialCustomer.optional().describe('The created or retrieved customer object').title('Customer'),
+    customers: z
+      .array(partialCustomer)
+      .optional()
+      .describe('A list of customers matching the criteria')
+      .title('Customers'),
   })
   .partial()
 
@@ -271,7 +284,7 @@ export const retrieveCustomerByIdOutputSchema = z
       .describe('The description of the retrieved customer')
       .title('Customer Description')
       .nullable(),
-    created: z.number().describe('The creation timestamp of the customer').title('Customer Created Timestamp'),
+    created: z.number().describe('The creation timestamp (UNIX) of the customer').title('Customer Created Timestamp'),
   })
   .passthrough()
   .partial()
@@ -284,7 +297,7 @@ const baseSchema = z.object({
 export const chargeFailedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('charge.failed'),
+      type: z.string().default('charge.failed').describe('The data type').title('Type'),
       object: z.object({}).passthrough().describe('The object of the failed charge').title('Charge Object'),
     })
     .describe('The data to send with the event')
@@ -294,7 +307,7 @@ export const chargeFailedSchema = baseSchema.extend({
 export const subscriptionCreatedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('customer.subscription.created'),
+      type: z.string().default('customer.subscription.created').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -308,7 +321,7 @@ export const subscriptionCreatedSchema = baseSchema.extend({
 export const subscriptionDeletedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('customer.subscription.deleted'),
+      type: z.string().default('customer.subscription.deleted').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -322,7 +335,7 @@ export const subscriptionDeletedSchema = baseSchema.extend({
 export const subscriptionUpdatedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('customer.subscription.updated'),
+      type: z.string().default('customer.subscription.updated').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -336,7 +349,7 @@ export const subscriptionUpdatedSchema = baseSchema.extend({
 export const invoicePaymentFailedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('invoice.payment_failed'),
+      type: z.string().default('invoice.payment_failed').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -350,7 +363,7 @@ export const invoicePaymentFailedSchema = baseSchema.extend({
 export const paymentIntentFailedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('payment_intent.payment_failed'),
+      type: z.string().default('payment_intent.payment_failed').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -364,7 +377,7 @@ export const paymentIntentFailedSchema = baseSchema.extend({
 export const subscriptionScheduleCreatedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('subscription_schedule.created'),
+      type: z.string().default('subscription_schedule.created').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
@@ -378,7 +391,7 @@ export const subscriptionScheduleCreatedSchema = baseSchema.extend({
 export const subscriptionScheduleUpdatedSchema = baseSchema.extend({
   data: z
     .object({
-      type: z.string().default('subscription_schedule.updated'),
+      type: z.string().default('subscription_schedule.updated').describe('The data type').title('Type'),
       object: z
         .object({})
         .passthrough()
