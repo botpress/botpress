ccca291307fc6ee39aad367383d3f53fa6f0da57
mikee-b <112516913+mikee-b@users.noreply.github.com>
2025-12-01 16:32:20 -0500

    feat(integrations/github): find target org (#14592)

--- integrations/github/src/definitions/actions.ts
+++ integrations/github/src/definitions/actions.ts
@@ -1,20 +1,27 @@
 import * as sdk from '@botpress/sdk'
 import { z } from '@botpress/sdk'
 
-const Channels = ['pullRequest', 'issue'] as const
+const channels = ['pullRequest', 'issue'] as const
 
 export type Target = {
   displayName: string
   tags: { [key: string]: string }
-  channel: (typeof Channels)[number]
+  channel: (typeof channels)[number]
 }
 
 const findTarget = {
   input: {
     schema: z.object({
       query: z.string().title('Query').describe('The search query'),
-      channel: z.enum(Channels).title('Channel').describe('The channel in which to execute the query'),
+      channel: z.enum(channels).title('Channel').describe('The channel in which to execute the query'),
       repo: z.string().title('Repository').describe('The repository name'),
+      organization: z
+        .string()
+        .optional()
+        .title('Organization')
+        .describe(
+          'The organization that owns the repository. Leave empty if the repository is owned by the current user.'
+        ),
     }),
   },
   output: {
@@ -23,13 +30,18 @@ const findTarget = {
         z.object({
           displayName: z.string().title('Display Name').describe('The display name'),
           tags: z.record(z.string()).title('Tags').describe('The tags'),
-          channel: z.enum(Channels).title('Channel').describe('The channel'),
+          channel: z.enum(channels).title('Channel').describe('The channel'),
         })
       ),
     }),
   },
 }
 
+const findIssuesOrPullRequests = {
+  ...findTarget,
+}
+
 export const actions = {
-  findTarget,
+  findTarget: { ...findTarget, attributes: { ...sdk.WELL_KNOWN_ATTRIBUTES.HIDDEN_IN_STUDIO } },
+  findIssuesOrPullRequests,
 } satisfies sdk.IntegrationDefinitionProps['actions']
