6b00b67522e7770ff085b34768e4eef882f20764
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-01 11:44:41 -0500

    added catchers

--- bots/bugbuster/src/handlers/time-to-lint-all.ts
+++ bots/bugbuster/src/handlers/time-to-lint-all.ts
@@ -1,11 +1,21 @@
+import * as boot from '../bootstrap'
 import * as bp from '.botpress'
 
 export const handleTimeToLintAll: bp.EventHandlers['timeToLintAll'] = async (props) => {
   const { client, ctx, workflows, logger } = props
   logger.info("'timeToLintAll' event received.")
 
-  const conversationId = await tryGetConversationId(client, ctx.botId)
-  await workflows.lintAll.startNewInstance({ input: {}, conversationId })
+  const { botpress } = boot.bootstrap(props)
+
+  const _handleError = (context: string, conversationId?: string) => (thrown: unknown) =>
+    botpress.handleError({ context, conversationId }, thrown)
+
+  const conversationId = await tryGetConversationId(client, ctx.botId).catch(
+    _handleError('trying to get Slack notification channel')
+  )
+  await workflows.lintAll
+    .startNewInstance({ input: {}, conversationId })
+    .catch(_handleError("trying to start the 'lintAll' workflow", conversationId))
 }
 
 const tryGetConversationId = async (client: bp.Client, botId: string): Promise<string | undefined> => {
