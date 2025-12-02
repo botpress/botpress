b2cb4719950d5f90d9515889249541855856fd25
David Vitor Antonio <david.vitora@gmail.com>
2025-12-02 20:38:33 -0300

    rm

--- integrations/zendesk-messaging-hitl/src/sunshine-client.ts
+++ integrations/zendesk-messaging-hitl/src/sunshine-client.ts
@@ -31,14 +31,3 @@ export type SuncoUser = {
 export type SuncoConversation = {
   id: string
 }
-
-export type SuncoClientApis = {
-  users: InstanceType<typeof SunshineConversationsClient.UsersApi>
-  conversations: InstanceType<typeof SunshineConversationsClient.ConversationsApi>
-  messages: InstanceType<typeof SunshineConversationsClient.MessagesApi>
-  webhooks: InstanceType<typeof SunshineConversationsClient.WebhooksApi>
-  integrations: InstanceType<typeof SunshineConversationsClient.IntegrationsApi>
-  switchboard: InstanceType<typeof SunshineConversationsClient.SwitchboardsApi>
-  switchboardActions: InstanceType<typeof SunshineConversationsClient.SwitchboardActionsApi>
-  switchboardIntegrations: InstanceType<typeof SunshineConversationsClient.SwitchboardIntegrationsApi>
-}
