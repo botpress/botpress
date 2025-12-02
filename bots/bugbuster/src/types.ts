61cee4b0380dbc1d139467ac03a10ec39398218d
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-02 15:14:07 -0500

    restored deleted types

--- bots/bugbuster/src/types.ts
+++ bots/bugbuster/src/types.ts
@@ -3,3 +3,14 @@ import * as bp from '.botpress'
 export type CommonHandlerProps = bp.WorkflowHandlerProps['lintAll'] | bp.EventHandlerProps | bp.MessageHandlerProps
 
 export type WatchedIssue = { id: string; sinceTimestamp: number; commentId?: string }
+
+export type LintResult =
+  | {
+      identifier: string
+      result: 'failed'
+      messages: string[]
+    }
+  | {
+      identifier: string
+      result: 'succeeded' | 'ignored'
+    }
