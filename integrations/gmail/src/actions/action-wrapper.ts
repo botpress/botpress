68aba089fa324104285adc964f17d5c4f5c47d0b
quu-ack <quentin.ackermann@botpress.com>
2025-12-02 09:39:03 -0500

    chore(integration/gmail): add general metrics

--- integrations/gmail/src/actions/action-wrapper.ts
+++ integrations/gmail/src/actions/action-wrapper.ts
@@ -1,18 +1,47 @@
-import { createActionWrapper } from '@botpress/common'
+import { createActionWrapper, posthogHelper } from '@botpress/common'
+import { INTEGRATION_NAME } from 'integration.definition'
 import { wrapWithTryCatch } from '../google-api/error-handling'
 import { GoogleClient } from '../google-api/google-client'
 import * as bp from '.botpress'
 
-export const wrapAction: typeof _wrapAction = (meta, actionImpl) =>
-  _wrapAction(meta, (props) =>
-    wrapWithTryCatch(() => {
-      props.logger
-        .forBot()
-        .debug(`Running action "${meta.actionName}" [bot id: ${props.ctx.botId}]`, { input: props.input })
+const posthogConfig = {
+  integrationName: INTEGRATION_NAME,
+  key: (bp.secrets as any).POSTHOG_KEY as string,
+}
 
-      return actionImpl(props as Parameters<typeof actionImpl>[0], props.input)
+export const wrapAction: typeof _wrapAction = (meta, actionImpl) =>
+  _wrapAction(meta, (props) => {
+    const startTime = Date.now()
+
+    props.logger
+      .forBot()
+      .debug(`Running action "${meta.actionName}" [bot id: ${props.ctx.botId}]`, { input: props.input })
+
+    return wrapWithTryCatch(() => {
+      const result = actionImpl(props as Parameters<typeof actionImpl>[0], props.input)
+
+      void Promise.resolve(result).then(() => {
+        const executionTimeMs = Date.now() - startTime
+        posthogHelper
+          .sendPosthogEvent(
+            {
+              distinctId: props.ctx.integrationId,
+              event: 'action_executed',
+              properties: {
+                actionName: meta.actionName,
+                botId: props.ctx.botId,
+                executionTimeMs,
+                success: true,
+              },
+            },
+            posthogConfig
+          )
+          .catch(() => {})
+      })
+
+      return result
     }, `Action Error: ${meta.errorMessageWhenFailed}`)()
-  )
+  })
 
 const _wrapAction = createActionWrapper<bp.IntegrationProps>()({
   toolFactories: {
--- integrations/gmail/src/setup.ts
+++ integrations/gmail/src/setup.ts
@@ -1,7 +1,16 @@
+import { posthogHelper } from '@botpress/common'
+import { INTEGRATION_NAME } from 'integration.definition'
 import { GoogleClient } from './google-api'
 import * as bp from '.botpress'
 
+const posthogConfig = {
+  integrationName: INTEGRATION_NAME,
+  key: (bp.secrets as any).POSTHOG_KEY as string,
+}
+
 export const register: bp.IntegrationProps['register'] = async ({ client, ctx }) => {
+  const startTime = Date.now()
+
   const googleClient =
     ctx.configurationType === 'customApp'
       ? await GoogleClient.createFromAuthorizationCode({
@@ -12,6 +21,38 @@ export const register: bp.IntegrationProps['register'] = async ({ client, ctx })
       : await GoogleClient.create({ client, ctx })
 
   await googleClient.watchIncomingMail()
+
+  const configurationTimeMs = Date.now() - startTime
+
+  await posthogHelper
+    .sendPosthogEvent(
+      {
+        distinctId: ctx.integrationId,
+        event: 'integration_registered',
+        properties: {
+          botId: ctx.botId,
+          configurationType: ctx.configurationType,
+          configurationTimeMs,
+        },
+      },
+      posthogConfig
+    )
+    .catch(() => {
+      // Silently fail if PostHog is unavailable
+    })
 }
 
-export const unregister: bp.IntegrationProps['unregister'] = async () => {}
+export const unregister: bp.IntegrationProps['unregister'] = async ({ ctx }) => {
+  await posthogHelper
+    .sendPosthogEvent(
+      {
+        distinctId: ctx.integrationId,
+        event: 'integration_unregistered',
+        properties: {
+          botId: ctx.botId,
+        },
+      },
+      posthogConfig
+    )
+    .catch(() => {})
+}
