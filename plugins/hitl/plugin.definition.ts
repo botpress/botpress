7198cfc0f96d2249a2c3ed04d99cdcc08dfe5626
François Levasseur <francois_levasseur@hotmail.com>
2025-12-03 12:20:07 -0500

    bump version

--- plugins/hitl/plugin.definition.ts
+++ plugins/hitl/plugin.definition.ts
@@ -102,7 +102,7 @@ const PLUGIN_CONFIG_SCHEMA = sdk.z.object({
 
 export default new sdk.PluginDefinition({
   name: 'hitl',
-  version: '1.1.1',
+  version: '1.2.0',
   title: 'Human In The Loop',
   description: 'Seamlessly transfer conversations to human agents',
   icon: 'icon.svg',
