014a3e32b2079b901edeb818bbda82c8037540ae
quu-ack <quentin.ackermann@botpress.com>
2025-12-04 11:24:22 -0500

    fix pr comments

--- integrations/gmail/src/index.ts
+++ integrations/gmail/src/index.ts
@@ -1,6 +1,6 @@
 import { posthogHelper } from '@botpress/common'
 import { sentry as sentryHelpers } from '@botpress/sdk-addons'
-import { INTEGRATION_NAME } from 'integration.definition'
+import { INTEGRATION_NAME, INTEGRATION_VERSION } from 'integration.definition'
 import { actions } from './actions'
 import { channels } from './channels'
 import { register, unregister } from './setup'
@@ -9,6 +9,7 @@ import * as bp from '.botpress'
 
 export const posthogConfig = {
   integrationName: INTEGRATION_NAME,
+  integrationVersion: INTEGRATION_VERSION,
   key: bp.secrets.POSTHOG_KEY,
 }
 @posthogHelper.wrapIntegration(posthogConfig)
