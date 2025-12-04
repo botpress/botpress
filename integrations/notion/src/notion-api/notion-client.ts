c0f6feeacdf8650cbea9c7afadbd089c1734aa85
Houssam Eddine Righi <94550978+Horisofine@users.noreply.github.com>
2025-12-04 15:55:22 -0500

    feat(integration/notion): Add `getPage` action in Notion (#14650)
    Co-authored-by: Houssam E. Righi <houssam.righi@botpress.com>

--- integrations/notion/src/notion-api/notion-client.ts
+++ integrations/notion/src/notion-api/notion-client.ts
@@ -19,7 +19,6 @@ export class NotionClient {
 
   public static async create({ ctx, client }: { client: bp.Client; ctx: bp.Context }): Promise<NotionClient> {
     const accessToken = await NotionClient._getAccessToken({ ctx, client })
-
     return new NotionClient({
       accessToken,
     })
@@ -147,7 +146,6 @@ export class NotionClient {
   @handleErrors('Failed to retrieve page')
   public async getPage({ pageId }: { pageId: string }) {
     const page = await this._notion.pages.retrieve({ page_id: pageId })
-
     return 'parent' in page ? page : undefined
   }
 
