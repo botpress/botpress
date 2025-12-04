f1c7bbb30143e0803a749441bff68786ba22f494
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-04 10:27:02 -0500

    renamed variable

--- bots/bugbuster/src/services/recently-linted-manager.ts
+++ bots/bugbuster/src/services/recently-linted-manager.ts
@@ -6,9 +6,9 @@ export class RecentlyLintedManager {
   public constructor(private _linear: lin.LinearApi) {}
 
   public async isRecentlyLinted(issue: lin.Issue): Promise<boolean> {
-    const user = await this._linear.getMe()
+    const me = await this._linear.getMe()
     const timestamps = issue.comments.nodes
-      .filter((comment) => comment.user.id === user.id)
+      .filter((comment) => comment.user.id === me.id)
       .map((comment) => new Date(comment.createdAt).getTime())
     const now = new Date().getTime()
     for (const timestamp of timestamps) {
