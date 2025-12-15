126cca497fc8e57725a8db53eeb34e1b73a706ed
quu-ack <quentin.ackermann@botpress.com>
2025-12-15 12:15:42 -0500

    feat(integrations/gmail): add core actions and labels, threads and drafts management actions

--- integrations/gmail/definitions/secrets.ts
+++ integrations/gmail/definitions/secrets.ts
@@ -1,8 +1,10 @@
+import { posthogHelper } from '@botpress/common'
 import * as sdk from '@botpress/sdk'
 import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 
 export const secrets = {
   ...sentryHelpers.COMMON_SECRET_NAMES,
+  ...posthogHelper.COMMON_SECRET_NAMES,
   CLIENT_ID: { description: 'Gmail Client ID' },
   CLIENT_SECRET: { description: 'Gmail Client Secret' },
   TOPIC_NAME: { description: 'Google Cloud Pub/Sub topic name for Gmail messages' },
