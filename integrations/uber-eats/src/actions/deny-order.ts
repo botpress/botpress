b5d3a72ad3541ca8c087862edcced550c0dec114
wojciech.farmery@botpress.com <wojciech.farmery@Wojciechs-MBP.localdomain>
2025-12-15 15:02:39 -0500

    map deny action to request body correctly

--- integrations/uber-eats/src/actions/deny-order.ts
+++ integrations/uber-eats/src/actions/deny-order.ts
@@ -1,8 +1,15 @@
 import * as bp from '.botpress'
+import type { DenyReason } from '@/gen/api'
 import { wrapUberAction } from '@/misc/action-wrapper'
 
 export const denyOrder: bp.IntegrationProps['actions']['denyOrder'] = wrapUberAction(async ({ uber, input }) => {
+  const reason: DenyReason = {
+    type: input.reason.type,
+    info: input.reason.info,
+    client_error_code: input.reason.clientErrorCode,
+  }
+
   return await uber.denyOrder(input.orderId, {
-    deny_reason: input.reason,
+    deny_reason: reason,
   })
 }, 'Failed to deny Uber Eats order')
