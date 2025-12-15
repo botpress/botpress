55e71f3c0b98a14916f93911184289ce1e01eadd
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-15 16:31:44 -0500

    gmail now using new posthogHelper api

--- integrations/gmail/src/index.ts
+++ integrations/gmail/src/index.ts
@@ -1,5 +1,4 @@
 import { posthogHelper } from '@botpress/common'
-import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 import { INTEGRATION_NAME, INTEGRATION_VERSION } from 'integration.definition'
 import { actions } from './actions'
 import { channels } from './channels'
@@ -7,28 +6,17 @@ import { register, unregister } from './setup'
 import { handler } from './webhook-events'
 import * as bp from '.botpress'
 
-export const posthogConfig = {
+const posthogConfig = {
   integrationName: INTEGRATION_NAME,
-  integrationVersion: INTEGRATION_VERSION,
   key: bp.secrets.POSTHOG_KEY,
+  integrationVersion: INTEGRATION_VERSION,
 }
-@posthogHelper.wrapIntegration(posthogConfig)
-class GmailIntegration extends bp.Integration {
-  public constructor() {
-    super({
-      register,
-      unregister,
-      actions,
-      channels,
-      handler,
-    })
-  }
+const integrationConfig: bp.IntegrationProps = {
+  register,
+  unregister,
+  actions,
+  channels,
+  handler,
 }
 
-const integration = new GmailIntegration()
-
-export default sentryHelpers.wrapIntegration(integration, {
-  dsn: bp.secrets.SENTRY_DSN,
-  environment: bp.secrets.SENTRY_ENVIRONMENT,
-  release: bp.secrets.SENTRY_RELEASE,
-})
+export default posthogHelper.wrapIntegration(posthogConfig, integrationConfig)
