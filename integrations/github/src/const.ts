62954fb00ac4eac246baaa832f0dfb6346ee3eac
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-17 11:06:27 -0500

    updated version

--- integrations/github/src/const.ts
+++ integrations/github/src/const.ts
@@ -1,3 +1,3 @@
 export const INTEGRATION_NAME = 'github'
-export const INTEGRATION_VERSION = '2.0.0'
+export const INTEGRATION_VERSION = '1.2.0'
 export const GITHUB_SIGNATURE_HEADER = 'x-hub-signature-256'
