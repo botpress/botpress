726011412d4dfbdf3ad03d00e15c8dec9bb88839
Houssam E. Righi <houssam.righi@botpress.com>
2025-12-18 13:30:36 -0500

    bump

--- integrations/messenger/integration.definition.ts
+++ integrations/messenger/integration.definition.ts
@@ -7,7 +7,7 @@ import typingIndicator from 'bp_modules/typing-indicator'
 import { messages } from './definitions/channels/channel/messages'
 
 export const INTEGRATION_NAME = 'messenger'
-export const INTEGRATION_VERSION = '5.0.5'
+export const INTEGRATION_VERSION = '5.1.1'
 
 const commonConfigSchema = z.object({
   downloadMedia: z
