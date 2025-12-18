b40c3a5e1609c06ffa96aa73b75748d50c6add13
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-12-18 17:36:47 -0500

    refactor: remove abstraction of configuration schema

--- integrations/trello/definitions/configuration.ts
+++ integrations/trello/definitions/configuration.ts
@@ -1,6 +1,20 @@
-import { IntegrationDefinitionProps } from '@botpress/sdk'
-import { TrelloConfigSchema } from './schemas'
+import { IntegrationDefinitionProps, z } from '@botpress/sdk'
+import { BoardSchema } from './schemas/entities'
 
 export const configuration = {
-  schema: TrelloConfigSchema,
+  schema: z.object({
+    trelloApiKey: z
+      .string()
+      .title('Trello API Key')
+      .describe('Can be obtained by creating an application on Trello')
+      .secret(),
+    trelloApiToken: z
+      .string()
+      .title('Trello API Token')
+      .describe('Can be obtained by granting access to the application on Trello')
+      .secret(),
+    trelloBoardId: BoardSchema.shape.id
+      .describe('Unique identifier of the board to watch for events on Trello')
+      .optional(),
+  }),
 } as const satisfies NonNullable<IntegrationDefinitionProps['configuration']>
