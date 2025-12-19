7cc91be83c2348cfb6fee0893eb288d0cd87b7c7
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-19 16:26:38 -0500

    upgraded version

--- integrations/slack/integration.definition.ts
+++ integrations/slack/integration.definition.ts
@@ -17,7 +17,7 @@ export default new IntegrationDefinition({
   name: 'slack',
   title: 'Slack',
   description: 'Automate interactions with your team.',
-  version: '3.1.2',
+  version: '3.1.3',
   icon: 'icon.svg',
   readme: 'hub.md',
   configuration,
