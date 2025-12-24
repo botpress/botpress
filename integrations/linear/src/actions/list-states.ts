50bc91f9e22b14b9b33d40d1054c864a8d3a7af9
Mikee Blanchet <mikee.blanchet@botpress.com>
2025-12-24 11:57:31 -0500

    added a try catch block

--- integrations/linear/src/actions/list-states.ts
+++ integrations/linear/src/actions/list-states.ts
@@ -1,3 +1,4 @@
+import { RuntimeError } from '@botpress/client'
 import { getLinearClient } from '../misc/utils'
 import * as bp from '.botpress'
 
@@ -8,12 +9,17 @@ export const listStates: bp.IntegrationProps['actions']['listStates'] = async (a
   } = args
 
   const linearClient = await getLinearClient(args, ctx.integrationId)
-  const states = await linearClient.workflowStates({ after: startCursor, first: count })
 
-  return {
-    nextCursor: states.pageInfo.endCursor,
-    states: states.nodes.map((state) => {
-      return { id: state.id, name: state.name }
-    }),
+  try {
+    const states = await linearClient.workflowStates({ after: startCursor, first: count })
+    return {
+      nextCursor: states.pageInfo.endCursor,
+      states: states.nodes.map((state) => {
+        return { id: state.id, name: state.name }
+      }),
+    }
+  } catch (thrown: unknown) {
+    const msg = thrown instanceof Error ? thrown.message : String(thrown)
+    throw new RuntimeError(`Failed to list states: ${msg}`)
   }
 }
