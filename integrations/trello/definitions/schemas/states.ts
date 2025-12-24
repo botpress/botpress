57f50e87c8a005575f68ac950aaec4ad7b919539
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-12-24 13:43:29 -0500

    refactor: reorder attributes of the trelloWebhookId schema property

--- integrations/trello/definitions/schemas/states.ts
+++ integrations/trello/definitions/schemas/states.ts
@@ -4,9 +4,9 @@ import { trelloIdSchema } from './entities'
 export const webhookStateSchema = z
   .object({
     trelloWebhookId: trelloIdSchema
-      .or(z.null())
+      .nullable()
+      .default(null)
       .title('Trello Webhook ID')
-      .describe('Unique id of the webhook that is created upon integration registration')
-      .default(null),
+      .describe('Unique id of the webhook that is created upon integration registration'),
   })
   .describe('State that stores the webhook id for the Trello integration')
