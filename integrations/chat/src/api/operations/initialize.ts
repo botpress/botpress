a665891695594dec30fd9128603c583f935c7acf
Nathaniel Girard <nathaniel.girard1@gmail.com>
2026-01-05 15:50:54 -0500

    used initializeIncomingMessage for chat init

--- integrations/chat/src/api/operations/initialize.ts
+++ integrations/chat/src/api/operations/initialize.ts
@@ -16,63 +16,32 @@ export const initialize: types.Operations['initializeConversation'] = async (pro
   const conversationId = req.query.conversationId
   let userKey = req.headers['x-user-key']
 
-  let user: User
+  let userId: string | undefined
   if (userKey) {
     const parsedKey = props.auth.parseKey(userKey)
-    const userId = parsedKey.id
-    user = await props.client.getUser({ id: userId }).then((res) => res.user)
-  } else {
-    user = await props.client.createUser({ tags: {} }).then((res) => res.user)
+    userId = parsedKey.id
+  }
+
+  const initializeResponse = await props.client.initializeIncomingMessage({ conversationId, userId })
+  const { conversation, user } = initializeResponse
+
+  if (!userKey) {
     userKey = props.auth.generateKey({ id: user.id })
-
-    if (conversationId) {
-      throw new errors.InvalidPayloadError('You cannot initialize an already existing conversation without a user.')
-    }
   }
 
-  if (!user) {
-    throw new errors.InternalError('User could not be resolved from authorization or be created')
+  let messagesPromise: Promise<Message[]> = Promise.resolve([])
+  if (userId && conversationId) {
+    messagesPromise = listMessages(props.client, conversation.id, 10).then((res) =>
+      res.map((m) => model.mapMessage(m as types.Message))
+    )
   }
 
-  let conversation: Conversation | undefined
-  let messages: Message[] = []
-  let participants: Participant[] = []
+  let participantsPromise: Promise<Participant[]> = Promise.resolve([user])
 
-  try {
-    if (conversationId) {
-      const conversationPromise = props.client.getConversation({ id: conversationId }).then((res) => res.conversation)
-      const messagesPromise = listMessages(props.client, conversationId, 10).then((res) =>
-        res.map((m) => model.mapMessage(m as types.Message))
-      )
-      const participantsPromise = listParticipants(props.client, conversationId, 5)
-
-      ;[conversation, messages, participants] = await Promise.all([
-        conversationPromise,
-        messagesPromise,
-        participantsPromise,
-      ])
-    } else {
-      conversation = await props.client
-        .createConversation({
-          channel: 'channel',
-          tags: {
-            owner: user.id,
-          },
-        })
-        .then((res) => res.conversation)
-      if (!conversation) {
-        throw new errors.InternalError('Failed to create conversation')
-      }
-
-      const { participant } = await props.client.addParticipant({ id: conversation.id, userId: user.id })
-      participants = [...participants, participant]
-    }
-  } catch (error) {
-    if (isApiError(error) && error.code === 403) {
-      throw new errors.ForbiddenError("You don't have access to this conversation")
-    }
-    throw new errors.InternalError(conversationId ? 'Failed to fetch conversation' : 'Failed to create conversation')
+  if (!conversationId) {
+    participantsPromise = listParticipants(props.client, conversation.id, 10)
   }
+  const [messages, participants] = await Promise.all([messagesPromise, participantsPromise])
 
   const ev: InitialEvent = {
     type: 'init',
