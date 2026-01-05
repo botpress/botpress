7fe04f30297b9dc6cea4252e57fd10b2022294de
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-05 12:59:24 -0500

    refactor(integrations/trello): action implementations import (#14745)

--- integrations/trello/src/index.ts
+++ integrations/trello/src/index.ts
@@ -1,39 +1,6 @@
 import { sentry as sentryHelpers } from '@botpress/sdk-addons'
 import * as bp from '../.botpress'
-import {
-  getBoardsByDisplayName,
-  getCardsByDisplayName,
-  getListsByDisplayName,
-  createCard,
-  moveCardUp,
-  moveCardDown,
-  moveCardToList,
-  addCardComment,
-  updateCard,
-  getAllBoardMembers,
-  getAllBoards,
-  getBoardById,
-  getBoardMembersByDisplayName,
-  getCardById,
-  getCardsInList,
-  getListById,
-  getListsInBoard,
-  getMemberByIdOrUsername,
-  cardList,
-  cardRead,
-  cardCreate,
-  cardUpdate,
-  cardDelete,
-  listList,
-  listRead,
-  boardList,
-  boardRead,
-  cardMemberList,
-  cardMemberRead,
-  boardMemberList,
-  boardMemberRead,
-  getAllCardMembers,
-} from './actions'
+import { actions } from './actions'
 import { channels } from './channels/publisher-dispatcher'
 import { handler } from './webhook-events'
 import { WebhookLifecycleManager } from './webhook-events/webhook-lifecycle-manager'
@@ -41,44 +8,7 @@ import { WebhookLifecycleManager } from './webhook-events/webhook-lifecycle-mana
 const integration = new bp.Integration({
   register: WebhookLifecycleManager.registerTrelloWebhookIfNotExists,
   unregister: WebhookLifecycleManager.unregisterTrelloWebhookIfExists,
-
-  actions: {
-    addCardComment,
-    createCard,
-    getAllBoardMembers,
-    getAllBoards,
-    getBoardById,
-    getBoardMembersByDisplayName,
-    getBoardsByDisplayName,
-    getCardById,
-    getCardsByDisplayName,
-    getCardsInList,
-    getListById,
-    getListsByDisplayName,
-    getListsInBoard,
-    getMemberByIdOrUsername,
-    moveCardDown,
-    moveCardToList,
-    moveCardUp,
-    updateCard,
-    getAllCardMembers,
-
-    // interfaces:
-    cardList,
-    cardRead,
-    cardCreate,
-    cardUpdate,
-    cardDelete,
-    listList,
-    listRead,
-    boardList,
-    boardRead,
-    cardMemberList,
-    cardMemberRead,
-    boardMemberList,
-    boardMemberRead,
-  },
-
+  actions,
   channels,
   handler,
 })
