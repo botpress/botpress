202db129b817562fec8d11158850615bbb82e67f
Houssam E. Righi <hrighi01@gmail.com>
2026-01-06 12:22:45 -0500

    fix(integration/notion): add TypeScript error comment

--- integrations/notion/src/actions/append-blocks-to-page.ts
+++ integrations/notion/src/actions/append-blocks-to-page.ts
@@ -1,12 +1,13 @@
 import { markdownToBlocks } from '@tryfabric/martian'
 import { wrapAction } from '../action-wrapper'
-import { BlockObjectRequest } from '@notionhq/client/build/src/api-endpoints'
+
 
 export const appendBlocksToPage = wrapAction(
   { actionName: 'appendBlocksToPage', errorMessage: 'Failed to append blocks to page' },
   async ({ notionClient }, input) => {
     const { pageId, markdownText } = input
-    const blocks = markdownToBlocks(markdownText) as BlockObjectRequest[]
+    const blocks = markdownToBlocks(markdownText)
+    // @ts-expect-error - @tryfabric/martian uses notion@1.0.4 types but it works. To find a better solution
     await notionClient.appendBlocksToPage({ pageId, blocks })
   }
 )
