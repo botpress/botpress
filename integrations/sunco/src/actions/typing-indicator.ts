8701cf8e9d3a9a6b85a4fb955d8e6d19ac6f3202
David Vitor Antonio <david.vitora@gmail.com>
2026-01-06 12:30:04 -0300

    chore(sunco): refactor file structure and remaining typings

---
+++ integrations/sunco/src/actions/typing-indicator.ts
@@ -0,0 +1,59 @@
+import { createClient } from '../sunshine-api'
+import { getConversationId } from '../util'
+import * as bp from '.botpress'
+
+type SendActivityProps = Pick<bp.AnyMessageProps, 'ctx' | 'client'> & {
+  conversationId: string
+  typingStatus?: 'start' | 'stop'
+  markAsRead?: boolean
+}
+
+async function sendActivity({ client, ctx, conversationId, typingStatus, markAsRead }: SendActivityProps) {
+  const { conversation } = await client.getConversation({ id: conversationId })
+  const suncoConversationId = getConversationId(conversation)
+  const { appId, keyId, keySecret } = ctx.configuration
+  const suncoClient = createClient(keyId, keySecret)
+  if (markAsRead) {
+    await suncoClient.activities.postActivity(appId, suncoConversationId, {
+      type: 'conversation:read',
+      author: { type: 'business' },
+    })
+  }
+  if (typingStatus) {
+    await suncoClient.activities.postActivity(appId, suncoConversationId, {
+      type: `typing:${typingStatus}`,
+      author: { type: 'business' },
+    })
+  }
+}
+
+export const startTypingIndicator: bp.IntegrationProps['actions']['startTypingIndicator'] = async ({
+  client,
+  ctx,
+  input,
+}) => {
+  const { conversationId } = input
+  await sendActivity({
+    client,
+    ctx,
+    conversationId,
+    typingStatus: 'start',
+    markAsRead: true,
+  })
+  return {}
+}
+
+export const stopTypingIndicator: bp.IntegrationProps['actions']['stopTypingIndicator'] = async ({
+  client,
+  ctx,
+  input,
+}) => {
+  const { conversationId } = input
+  await sendActivity({
+    client,
+    ctx,
+    conversationId,
+    typingStatus: 'stop',
+  })
+  return {}
+}
