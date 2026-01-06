3fcbb5d7fc3766c0246bb288af261bee27088a5f
Houssam E. Righi <hrighi01@gmail.com>
2026-01-06 12:22:54 -0500

    fix(integration/notion): update TypeScript error comment for notion types

--- integrations/notion/src/actions/append-blocks-to-page.ts
+++ integrations/notion/src/actions/append-blocks-to-page.ts
@@ -7,7 +7,7 @@ export const appendBlocksToPage = wrapAction(
   async ({ notionClient }, input) => {
     const { pageId, markdownText } = input
     const blocks = markdownToBlocks(markdownText)
-    // @ts-expect-error - @tryfabric/martian uses notion@1.0.4 types but it works. To find a better solution
+    // @ts-expect-error - @tryfabric/martian uses notion@1.0.4 types which needs to be updated to notion@5.6.0 but it works. To find a better solution
     await notionClient.appendBlocksToPage({ pageId, blocks })
   }
 )
