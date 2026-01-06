fd09ab3e8e084585a1b6866b1e1de27016a60b5b
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-06 15:22:26 -0500

    refactor: update the description on the webhook state schemas

--- integrations/trello/definitions/states.ts
+++ integrations/trello/definitions/states.ts
@@ -5,14 +5,12 @@ const _webhookIdStateSchema = trelloIdSchema
   .nullable()
   .default(null)
   .title('Trello Webhook ID')
-  .describe('Unique id of the webhook that is created upon integration registration')
+  .describe('Unique id of the webhook that is created by Trello upon integration registration')
 export type WebhookIdState = z.infer<typeof _webhookIdStateSchema>
 
 export const states = {
   webhook: {
     type: 'integration',
-    schema: z
-      .object({ trelloWebhookId: _webhookIdStateSchema })
-      .describe('State that stores the webhook id for the Trello integration'),
+    schema: z.object({ trelloWebhookId: _webhookIdStateSchema }),
   },
 } as const satisfies NonNullable<IntegrationDefinitionProps['states']>
