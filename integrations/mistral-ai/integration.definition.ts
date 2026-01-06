b2664e795feec83a4570c460340035131ce49075
Léo Mindlin <leo.mindlin@botpress.com>
2026-01-06 15:10:33 -0500

    setup integration, add logo

---
+++ integrations/mistral-ai/integration.definition.ts
@@ -0,0 +1,9 @@
+import { IntegrationDefinition } from '@botpress/sdk'
+import { name } from './package.json'
+
+export default new IntegrationDefinition({
+  name,
+  version: '0.1.0',
+  readme: 'hub.md',
+  icon: 'icon.svg',
+})
