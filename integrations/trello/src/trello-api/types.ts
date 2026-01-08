e6ef07ff4472ee26c1b743f74c7385a6921604e1
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-08 13:03:18 -0500

    feat: implement extended types for create & update card request payloads

---
+++ integrations/trello/src/trello-api/types.ts
@@ -0,0 +1,12 @@
+import type { Card } from 'definitions/schemas'
+import { Merge } from 'src/types'
+
+type CardPayloadOverrides = {
+  pos?: number | 'top' | 'bottom'
+}
+
+export type CreateCardPayload = Merge<
+  Pick<Card, 'name' | 'description' | 'listId'> & Omit<Partial<Card>, 'id' | 'isClosed'>,
+  CardPayloadOverrides
+>
+export type UpdateCardPayload = Merge<Pick<Card, 'id'> & Partial<Card>, CardPayloadOverrides>
