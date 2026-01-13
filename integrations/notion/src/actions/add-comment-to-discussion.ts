d448e21527e54b91fea60d032722660da6bb09e8
Houssam Eddine Righi <94550978+Horisofine@users.noreply.github.com>
2026-01-13 13:32:21 -0500

    chore(integration/notion): Void return removal (#14792)

--- integrations/notion/src/actions/add-comment-to-discussion.ts
+++ integrations/notion/src/actions/add-comment-to-discussion.ts
@@ -3,6 +3,6 @@ import { wrapAction } from '../action-wrapper'
 export const addCommentToDiscussion = wrapAction(
   { actionName: 'addCommentToDiscussion', errorMessage: 'Failed to add comment to discussion' },
   async ({ notionClient }, { commentBody, discussionId }) => {
-    await notionClient.addCommentToDiscussion({ commentBody, discussionId })
+    return await notionClient.addCommentToDiscussion({ commentBody, discussionId })
   }
 )
--- integrations/notion/src/actions/add-comment-to-page.ts
+++ integrations/notion/src/actions/add-comment-to-page.ts
@@ -3,6 +3,6 @@ import { wrapAction } from '../action-wrapper'
 export const addCommentToPage = wrapAction(
   { actionName: 'addCommentToPage', errorMessage: 'Failed to add comment to page' },
   async ({ notionClient }, { commentBody, pageId }) => {
-    await notionClient.addCommentToPage({ commentBody, pageId })
+    return await notionClient.addCommentToPage({ commentBody, pageId })
   }
 )
