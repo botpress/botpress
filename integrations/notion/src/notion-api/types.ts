d69d41750de997bc7940c51c2a2c21208c605eae
Houssam Eddine Righi <94550978+Horisofine@users.noreply.github.com>
2026-01-15 10:36:38 -0500

    feat(integration/notion)!: Major Notion update -> 3.0.0 (#14727)
    Co-authored-by: Houssam E. Righi <houssam.righi@botpress.com>
    Co-authored-by: François Levasseur <francois_levasseur@hotmail.com>

--- integrations/notion/src/notion-api/types.ts
+++ integrations/notion/src/notion-api/types.ts
@@ -1,7 +1,15 @@
 import type * as notionhq from '@notionhq/client'
-import type { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+import type { PageObjectResponse, RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
 
 export type NotionPagePropertyTypes = Valueof<PageObjectResponse['properties']>['type']
+
+export type BlockContent = {
+  blockId: string
+  parentId: string | undefined
+  type: string
+  hasChildren: boolean
+  richText: RichTextItemResponse[]
+}
 
 export type NotionTopLevelItem = Extract<
   Awaited<ReturnType<notionhq.Client['search']>>['results'][number],
@@ -13,14 +21,14 @@ export type NotionPageChild = Extract<
   { parent: any; type: 'child_page' | 'child_database' }
 >
 
-export type NotionDatabaseChild = Extract<
-  Awaited<ReturnType<notionhq.Client['databases']['query']>>['results'][number],
+export type NotionDataSourceChild = Extract<
+  Awaited<ReturnType<notionhq.Client['dataSources']['query']>>['results'][number],
   { parent: any }
 >
 
-export type NotionItem = NotionTopLevelItem | NotionPageChild | NotionDatabaseChild
+export type NotionItem = NotionTopLevelItem | NotionPageChild | NotionDataSourceChild
 export type NotionPage = Extract<NotionItem, { object: 'page' }>
-export type NotionDatabase = Extract<NotionItem, { object: 'database' }>
+export type NotionDataSource = Extract<NotionItem, { object: 'data_source' }>
 export type NotionChildPage = Extract<NotionItem, { object: 'block'; type: 'child_page' }>
 export type NotionChildDatabase = Extract<NotionItem, { object: 'block'; type: 'child_database' }>
 
