d5b0c55d0bd089c21bd0f4b28e591ba0593aa31e
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-15 20:33:31 -0500

    fix!: trim the schema of the checklistItemUpdatedEventSchema & fix linter errors

--- integrations/trello/definitions/events/checklist-events.ts
+++ integrations/trello/definitions/events/checklist-events.ts
@@ -1,6 +1,6 @@
 import { z } from '@botpress/sdk'
 import { boardSchema, cardSchema, trelloIdSchema } from '../schemas'
-import { botpressEventDataSchema, pickIdAndName, TrelloEventType } from './common'
+import { botpressEventDataSchema, pickIdAndName } from './common'
 
 // Action that is triggered when a checklist is added to a card
 export const checklistAddedToCardEventSchema = botpressEventDataSchema.extend({
@@ -48,41 +48,38 @@ export const checklistItemCreatedEventSchema = botpressEventDataSchema.extend({
 
 // Action that is triggered when an item is updated in a checklist
 export const checklistItemUpdatedEventSchema = botpressEventDataSchema.extend({
-  type: z.literal(TrelloEventType.CHECKLIST_ITEM_UPDATED).describe('Type of the action'),
-  data: z.object({
-    board: pickIdAndName(boardSchema).optional().title('Board').describe('Board where the card was updated'),
-    card: pickIdAndName(cardSchema).title('Card').describe('Card that was updated'),
-    checklist: z
-      .object({
-        id: trelloIdSchema.title('Checklist ID').describe('Unique identifier of the checklist'),
-        name: z.string().title('Checklist Name').describe('Name of the checklist'),
-      })
-      .title('Checklist')
-      .describe('Checklist where the item was updated'),
-    checkItem: z
-      .object({
-        id: trelloIdSchema.title('Item ID').describe('Unique identifier of the checklist item'),
-        name: z.string().title('Item Name').describe('Name of the checklist item'),
-        state: z
-          .union([z.literal('complete'), z.literal('incomplete')])
-          .title('Item Status')
-          .describe('The completion status of the checklist item'),
-        due: z.string().datetime().optional().title('Due Date').describe('Due date of the checklist item'),
-      })
-      .title('Checklist Item')
-      .describe('Checklist item that was updated'),
-    old: z
-      .object({
-        name: z.string().title('Old Item Name').describe('Old name of the checklist item'),
-        state: z
-          .union([z.literal('complete'), z.literal('incomplete')])
-          .title('Old Item Status')
-          .describe('The old completion status of the checklist item'),
-        due: z.string().datetime().optional().title('Old Due Date').describe('Old due date of the checklist item'),
-      })
-      .title('Old Checklist Item')
-      .describe('The previous data of the checklist item'),
-  }),
+  board: pickIdAndName(boardSchema).optional().title('Board').describe('Board where the card was updated'),
+  card: pickIdAndName(cardSchema).title('Card').describe('Card that was updated'),
+  checklist: z
+    .object({
+      id: trelloIdSchema.title('Checklist ID').describe('Unique identifier of the checklist'),
+      name: z.string().title('Checklist Name').describe('Name of the checklist'),
+    })
+    .title('Checklist')
+    .describe('Checklist where the item was updated'),
+  checkItem: z
+    .object({
+      id: trelloIdSchema.title('Item ID').describe('Unique identifier of the checklist item'),
+      name: z.string().title('Item Name').describe('Name of the checklist item'),
+      state: z
+        .union([z.literal('complete'), z.literal('incomplete')])
+        .title('Item Status')
+        .describe('The completion status of the checklist item'),
+      due: z.string().datetime().optional().title('Due Date').describe('Due date of the checklist item'),
+    })
+    .title('Checklist Item')
+    .describe('Checklist item that was updated'),
+  old: z
+    .object({
+      name: z.string().title('Old Item Name').describe('Old name of the checklist item'),
+      state: z
+        .union([z.literal('complete'), z.literal('incomplete')])
+        .title('Old Item Status')
+        .describe('The old completion status of the checklist item'),
+      due: z.string().datetime().optional().title('Old Due Date').describe('Old due date of the checklist item'),
+    })
+    .title('Old Checklist Item')
+    .describe('The previous data of the checklist item'),
 })
 
 // Action that is triggered when a checklist item's status is changed from "incomplete" to "complete" or vice versa
