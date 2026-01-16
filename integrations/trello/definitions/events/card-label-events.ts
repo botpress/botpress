fbb0784d81619cf5a8955478354ad87e204027d6
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-15 19:33:58 -0500

    feat: add validated schemas for the label events' data property

--- integrations/trello/definitions/events/card-label-events.ts
+++ integrations/trello/definitions/events/card-label-events.ts
@@ -2,30 +2,22 @@ import { z } from '@botpress/sdk'
 import { boardSchema, cardSchema, trelloIdSchema } from '../schemas'
 import { botpressEventDataSchema, pickIdAndName } from './common'
 
+export const labelSchema = z.object({
+  id: trelloIdSchema.title('Label ID').describe('Unique identifier of the label'),
+  name: z.string().title('Label Name').describe('Name of the label'),
+  color: z.string().title('Label Color').describe('Color of the label'),
+})
+
 // Action that is triggered when a label is added to a card
 export const labelAddedToCardEventSchema = botpressEventDataSchema.extend({
   board: pickIdAndName(boardSchema).optional().title('Board').describe('Board where the card was modified'),
   card: pickIdAndName(cardSchema).title('Card').describe('Card that was modified'),
-  label: z
-    .object({
-      id: trelloIdSchema.title('Label ID').describe('Unique identifier of the label'),
-      name: z.string().title('Label Name').describe('Name of the label'),
-      color: z.string().title('Label Color').describe('Color of the label'),
-    })
-    .title('Label')
-    .describe('Label that was added to the card'),
+  label: labelSchema.title('Label').describe('Label that was added to the card'),
 })
 
 // Action that is triggered when a label is removed from a card
 export const labelRemovedFromCardEventSchema = botpressEventDataSchema.extend({
   board: pickIdAndName(boardSchema).optional().title('Board').describe('Board where the card was modified'),
   card: pickIdAndName(cardSchema).title('Card').describe('Card that was modified'),
-  label: z
-    .object({
-      id: trelloIdSchema.title('Label ID').describe('Unique identifier of the label'),
-      name: z.string().title('Label Name').describe('Name of the label'),
-      color: z.string().title('Label Color').describe('Color of the label'),
-    })
-    .title('Label')
-    .describe('Label that was removed from the card'),
+  label: labelSchema.title('Label').describe('Label that was removed from the card'),
 })
--- integrations/trello/src/webhook-events/schemas/card-label-event-schemas.ts
+++ integrations/trello/src/webhook-events/schemas/card-label-event-schemas.ts
@@ -1,15 +1,26 @@
 import { z } from '@botpress/sdk'
 import { TrelloEventType } from 'definitions/events'
+import { labelSchema } from 'definitions/events/card-label-events'
+import { pickIdAndName } from 'definitions/events/common'
+import { boardSchema, cardSchema } from 'definitions/schemas'
 import { trelloEventActionSchema } from './common'
 
 export const cardLabelAddedEventActionSchema = trelloEventActionSchema.extend({
   type: z.literal(TrelloEventType.LABEL_ADDED_TO_CARD),
-  data: z.object({}),
+  data: z.object({
+    board: pickIdAndName(boardSchema),
+    card: pickIdAndName(cardSchema),
+    label: labelSchema,
+  }),
 })
 export type CardLabelAddedEventAction = z.infer<typeof cardLabelAddedEventActionSchema>
 
 export const cardLabelRemovedEventActionSchema = trelloEventActionSchema.extend({
   type: z.literal(TrelloEventType.LABEL_REMOVED_FROM_CARD),
-  data: z.object({}),
+  data: z.object({
+    board: pickIdAndName(boardSchema),
+    card: pickIdAndName(cardSchema),
+    label: labelSchema,
+  }),
 })
 export type CardLabelRemovedEventAction = z.infer<typeof cardLabelRemovedEventActionSchema>
