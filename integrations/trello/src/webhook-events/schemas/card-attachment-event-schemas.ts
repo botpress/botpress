ef53c4530a05704deeff20b4c300170e6cd0ddd3
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2026-01-15 19:33:48 -0500

    feat: add validated schemas for the attachment events' data property

--- integrations/trello/src/webhook-events/schemas/card-attachment-event-schemas.ts
+++ integrations/trello/src/webhook-events/schemas/card-attachment-event-schemas.ts
@@ -1,15 +1,35 @@
 import { z } from '@botpress/sdk'
 import { TrelloEventType } from 'definitions/events'
+import { pickIdAndName } from 'definitions/events/common'
+import { boardSchema, cardSchema, listSchema, trelloIdSchema } from 'definitions/schemas'
 import { trelloEventActionSchema } from './common'
 
 export const cardAttachmentAddedEventActionSchema = trelloEventActionSchema.extend({
   type: z.literal(TrelloEventType.ATTACHMENT_ADDED_TO_CARD),
-  data: z.object({}),
+  data: z.object({
+    board: pickIdAndName(boardSchema),
+    list: pickIdAndName(listSchema),
+    card: pickIdAndName(cardSchema),
+    attachment: z.object({
+      id: trelloIdSchema,
+      name: z.string(),
+      url: z.string().url(),
+      previewUrl: z.string().url().optional(),
+      previewUrl2x: z.string().url().optional(),
+    }),
+  }),
 })
 export type CardAttachmentAddedEventAction = z.infer<typeof cardAttachmentAddedEventActionSchema>
 
 export const cardAttachmentRemovedEventActionSchema = trelloEventActionSchema.extend({
   type: z.literal(TrelloEventType.ATTACHMENT_REMOVED_FROM_CARD),
-  data: z.object({}),
+  data: z.object({
+    board: pickIdAndName(boardSchema),
+    card: pickIdAndName(cardSchema),
+    attachment: z.object({
+      id: trelloIdSchema,
+      name: z.string(),
+    }),
+  }),
 })
 export type CardAttachmentRemovedEventAction = z.infer<typeof cardAttachmentRemovedEventActionSchema>
