622da105a9898f628282daaaa2a3552cd5bb391e
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-01-16 14:31:33 -0500

    removed hardcoded values

--- integrations/linkedin/src/linkedin-api/linkedin-oauth-client.ts
+++ integrations/linkedin/src/linkedin-api/linkedin-oauth-client.ts
@@ -4,8 +4,7 @@ import * as bp from '.botpress'
 
 const LINKEDIN_TOKEN_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
 const LINKEDIN_USERINFO_URL = 'https://api.linkedin.com/v2/userinfo'
-const OAUTH_REDIRECT_URI = 'https://webhook.botpress.cloud/oauth'
-const BOTPRESS_LINKEDIN_CLIENT_ID = '7831bmd1a7lfnj'
+const OAUTH_REDIRECT_URI = `${process.env.BP_WEBHOOK_URL}/oauth`
 
 type OAuthCredentialsPayload = bp.states.oauthCredentials.OauthCredentials['payload']
 
@@ -385,7 +384,7 @@ export class LinkedInOAuthClient {
 
   private static _getBotpressClientCredentials(): ClientCredentials {
     return {
-      clientId: BOTPRESS_LINKEDIN_CLIENT_ID,
+      clientId: bp.secrets.CLIENT_ID,
       clientSecret: bp.secrets.CLIENT_SECRET,
     }
   }
