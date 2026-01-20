65a89e70c9d289ec2faeb707149ddeac81d5143f
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-01-20 12:44:45 -0500

    added posthog

--- integrations/linkedin/integration.definition.ts
+++ integrations/linkedin/integration.definition.ts
@@ -1,8 +1,8 @@
 import { IntegrationDefinition } from '@botpress/sdk'
 import { configuration, configurations, identifier, states, secrets, actions } from './definitions'
 
-const INTEGRATION_NAME = 'linkedin'
-const INTEGRATION_VERSION = '0.1.0'
+export const INTEGRATION_NAME = 'linkedin'
+export const INTEGRATION_VERSION = '0.1.0'
 
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
