57ed9f009b63d970f86b30ea6d3cd8076bd21ff8
quu-ack <quentin.ackermann@botpress.com>
2026-01-21 15:54:49 +0100

    feat(common/posthog): add posthog event for register/unregister

--- integrations/gsheets/src/setup.ts
+++ integrations/gsheets/src/setup.ts
@@ -1,50 +1,12 @@
-import { posthogHelper } from '@botpress/common'
-import { posthogConfig } from 'src'
 import { GoogleClient } from './google-api/google-client'
 import * as bp from '.botpress'
 
 export const register: bp.IntegrationProps['register'] = async ({ logger, ctx, client }) => {
-  const startTime = Date.now()
-
   logger.forBot().info('Registering Google Sheets integration')
 
   const gsheetsClient = await GoogleClient.create({ ctx, client })
   const summary = await gsheetsClient.getSpreadsheetSummary()
   logger.forBot().info(`Successfully connected to Google Sheets: ${summary}`)
-
-  const configurationTimeMs = Date.now() - startTime
-
-  await posthogHelper
-    .sendPosthogEvent(
-      {
-        distinctId: ctx.integrationId,
-        event: 'integration_registered',
-        properties: {
-          botId: ctx.botId,
-          configurationType: ctx.configurationType,
-          configurationTimeMs,
-        },
-      },
-      posthogConfig
-    )
-    .catch(() => {
-      // Silently fail if PostHog is unavailable
-    })
 }
 
-export const unregister: bp.IntegrationProps['unregister'] = async ({ ctx }) => {
-  await posthogHelper
-    .sendPosthogEvent(
-      {
-        distinctId: ctx.integrationId,
-        event: 'integration_unregistered',
-        properties: {
-          botId: ctx.botId,
-        },
-      },
-      posthogConfig
-    )
-    .catch(() => {
-      // Silently fail if PostHog is unavailable
-    })
-}
+export const unregister: bp.IntegrationProps['unregister'] = async () => {}
