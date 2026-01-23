4651195bdffd719dcfd219a8558c144cad614646
AAAbohan <bohan.wang@botpress.com>
2026-01-23 10:33:06 -0500

    refactor: remove unused code

--- integrations/wechat-entree/definitions/channels.ts
+++ integrations/wechat-entree/definitions/channels.ts
@@ -9,22 +9,6 @@ const _textMessageDefinition = {
   }),
 }
 
-// ============== FOR FUTURE USE ==============
-// const _imageMessageDefinition = {
-//   ...messages.defaults.image,
-//   schema: messages.defaults.image.schema.extend({
-//     caption: z.string().optional().describe('The caption/description of the image'),
-//   }),
-// }
-
-// const _audioMessageDefinition = {
-//   ...messages.defaults.audio,
-//   schema: messages.defaults.audio.schema.extend({
-//     caption: z.string().optional().describe('The caption/transcription of the audio message'),
-//   }),
-// }
-// =============================================
-
 const _imageMessageDefinition = {
   ...messages.defaults.image,
 }
