2c353ac525def34eef1485d56d773a5f72729278
yueranlu <yueran.lu@botpress.com>
2026-01-26 15:32:28 -0500

    Update integration.definition.ts

--- integrations/kommo/integration.definition.ts
+++ integrations/kommo/integration.definition.ts
@@ -5,7 +5,7 @@ export default new IntegrationDefinition({
   name: 'kommo',
   title: 'Kommo',
   description: 'Manage leads and contacts in your Kommo CRM directly from your chatbot.',
-  version: '0.1.0',
+  version: '1.0.0',
   readme: 'hub.md',
   icon: 'icon.svg',
 
