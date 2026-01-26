55d3457ba735b7ba9e68d8daad2b4b2065203c6a
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-01-26 17:41:39 -0500

    added wizard

--- integrations/sunco/src/wizard.ts
+++ integrations/sunco/src/wizard.ts
@@ -4,15 +4,21 @@ import * as bp from '.botpress'
 
 type WizardHandler = oauthWizard.WizardStepHandler<bp.HandlerProps>
 
+const BOT_LIST_PATH = '/admin/ai/ai-agents/ai-agents/marketplace-bots'
+
 export const handler = async (props: bp.HandlerProps) => {
   const wizard = new oauthWizard.OAuthWizardBuilder(props)
     .addStep({
       id: 'start',
-      handler: _startHandler,
+      handler: _start,
     })
     .addStep({
       id: 'get-subdomain',
       handler: _getSubdomain,
+    })
+    .addStep({
+      id: 'add-to-channels',
+      handler: _addToChannels,
     })
     .addStep({
       id: 'end',
@@ -23,23 +29,60 @@ export const handler = async (props: bp.HandlerProps) => {
   return await wizard.handleRequest()
 }
 
-const _startHandler: WizardHandler = (props) => {
-  const { responses } = props
-  return responses.displayInput({
-    pageTitle: 'Get Zendesk Subdomain',
-    htmlOrMarkdownPageContents: "To continue, you need to enter your Zendesk's subdomain",
-    input: { label: 'e.g. https://{subdomain}.zendesk.com', type: 'text' },
-    nextStepId: 'validate-subdomain',
+const _start: WizardHandler = async (props) => {
+  const { client, responses, ctx, inputValue } = props
+
+  let subdomain = inputValue
+  if (!subdomain) {
+    const {
+      state: { payload },
+    } = await client.getOrSetState({
+      name: 'credentials',
+      type: 'integration',
+      id: ctx.integrationId,
+      payload: {},
+    })
+    if (!payload.subdomain) {
+      return responses.redirectToStep('get-subdomain')
+    }
+    subdomain = payload.subdomain
+  }
+
+  const url = `https://${subdomain}.zendesk.com${BOT_LIST_PATH}`
+  const htmlUrl = `<a href="${url}" target="_blank" rel="noopener noreferrer">
+  here
+</a>`
+  return responses.displayButtons({
+    pageTitle: 'Open Admin Center',
+    htmlOrMarkdownPageContents: `Please click ${htmlUrl} to open your Admin Center on the \`AI => AI agents => AI agents => Marketplace bots\` page. (Click on \`Change subdomain\` if your subdomain is not \`${subdomain}\`.)`,
+    buttons: [
+      { action: 'navigate', label: 'Change subdomain', navigateToStep: 'get-subdomain', buttonType: 'secondary' },
+      { action: 'navigate', label: 'Next step', navigateToStep: 'add-to-channels', buttonType: 'primary' },
+    ],
   })
 }
 
 const _getSubdomain: WizardHandler = async (props) => {
   const { responses } = props
   return responses.displayInput({
-    pageTitle: 'Get Zendesk Subdomain',
-    htmlOrMarkdownPageContents: "To continue, you need to enter your Zendesk's subdomain",
+    pageTitle: 'Get SunCo Subdomain',
+    htmlOrMarkdownPageContents: "To continue, you need to enter your SunCo's subdomain",
     input: { label: 'e.g. https://{subdomain}.zendesk.com', type: 'text' },
-    nextStepId: 'validate-subdomain',
+    nextStepId: 'start',
+  })
+}
+
+const _addToChannels: WizardHandler = async (props) => {
+  const { responses } = props
+
+  return responses.displayButtons({
+    pageTitle: 'Add The Bot To Channels',
+    htmlOrMarkdownPageContents:
+      'Click on the Botpress bot. Then in the `Basics` section, select every channel for which you want Botpress to be the default responder. Click on the `Save` button.',
+    buttons: [
+      { action: 'navigate', label: 'Previous step', navigateToStep: 'start', buttonType: 'secondary' },
+      { action: 'navigate', label: 'Finish', navigateToStep: 'end', buttonType: 'primary' },
+    ],
   })
 }
 
@@ -47,47 +90,4 @@ const _endHandler: WizardHandler = ({ responses }) => {
   return responses.endWizard({
     success: true,
   })
-}
-
-const sha256 = async (str: string) => {
-  const data = new TextEncoder().encode(str)
-  const hash = await webcrypto.subtle.digest('SHA-256', data)
-  return Buffer.from(hash).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
-}
-
-const _getOAuthRedirectUri = (ctx?: bp.Context) => oauthWizard.getWizardStepUrl('oauth-callback', ctx).toString()
-
-// client.patchState is not working correctly
-const _patchCredentialsState = async (
-  client: bp.Client,
-  ctx: bp.Context,
-  newState: Partial<typeof bp.states.credentials>
-) => {
-  const currentState = await _getCredentialsState(client, ctx)
-
-  await client.setState({
-    type: 'integration',
-    name: 'credentials',
-    id: ctx.integrationId,
-    payload: {
-      ...currentState,
-      ...newState,
-    },
-  })
-}
-
-const _getCredentialsState = async (client: bp.Client, ctx: bp.Context) => {
-  try {
-    return (
-      (
-        await client.getState({
-          type: 'integration',
-          name: 'credentials',
-          id: ctx.integrationId,
-        })
-      )?.state?.payload || {}
-    )
-  } catch {
-    return {}
-  }
 }
