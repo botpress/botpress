e11fa5d3cac70e520dc7143ace6e46ace57627fb
Philippe Lamy <philippe.lamy@loto-quebec.com>
2026-01-28 10:48:46 -0500

    chore(integration): Removed errors from register and unregister functions

--- integrations/planhat/src/index.ts
+++ integrations/planhat/src/index.ts
@@ -7,14 +7,12 @@ export default new bp.Integration({
      * This is called when an integration configuration is saved.
      * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
      */
-    throw new sdk.RuntimeError('Invalid configuration') // replace this with your own validation logic
   },
   unregister: async () => {
     /**
      * This is called when a bot removes the integration.
      * You should use this handler to instanciate ressources in the external service and ensure that the configuration is valid.
      */
-    throw new sdk.RuntimeError('Invalid configuration') // replace this with your own validation logic
   },
   actions: {
     helloWorld: async (props) => {
