b934b20f2dd0e280f753147b7e03c6897e281c36
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-01-28 12:15:06 -0500

    implemented oauth flow

--- integrations/sunco/src/setup/unregister.ts
+++ integrations/sunco/src/setup/unregister.ts
@@ -1,3 +1,16 @@
-import * as bp from '../../.botpress'
+import { deleteApp } from 'src/api/webhooks'
+import { getStoredCredentials } from 'src/get-stored-credentials'
+import * as bp from '.botpress'
 
-export const unregister: bp.IntegrationProps['unregister'] = async ({}) => {}
+export const unregister: bp.IntegrationProps['unregister'] = async (props) => {
+  const { client, ctx, logger } = props
+  logger.forBot().info('Starting Sunshine Conversations integration unregistration...')
+
+  const credentials = await getStoredCredentials(client, ctx)
+  if (credentials.configType === 'manual') {
+    logger.forBot().info('Configuration type is manual, nothing to unregister.')
+    return
+  }
+
+  await deleteApp({ credentials, logger })
+}
