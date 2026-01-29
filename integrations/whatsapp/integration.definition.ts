1f1b25858878177d96d3db084c0b02f6adc30a8d
David Vitor Antonio <david.vitora@gmail.com>
2026-01-29 11:12:21 -0300

    feat(whatsapp): add error logs for message delivery failed (#14885)

--- integrations/whatsapp/integration.definition.ts
+++ integrations/whatsapp/integration.definition.ts
@@ -93,7 +93,7 @@ const defaultBotPhoneNumberId = {
 }
 
 export const INTEGRATION_NAME = 'whatsapp'
-export const INTEGRATION_VERSION = '4.5.20'
+export const INTEGRATION_VERSION = '4.6.0'
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
   version: INTEGRATION_VERSION,
