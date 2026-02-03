41bc904a0b8b84b7c5258bbe44075077308cbbad
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-02-03 10:36:33 -0500

    upgraded version

--- integrations/sunco/integration.definition.ts
+++ integrations/sunco/integration.definition.ts
@@ -7,7 +7,7 @@ import { events } from './definitions'
 
 export default new IntegrationDefinition({
   name: 'sunco',
-  version: '1.5.0',
+  version: '1.6.0',
   title: 'Sunshine Conversations',
   description: 'Give your bot access to a powerful omnichannel messaging platform.',
   icon: 'icon.svg',
