a8f56ff0f53961c5ac47b97d8797c660599019dc
Mikee Blanchet <mikee.blanchet@botpress.com>
2026-02-03 11:34:12 -0500

    identifier is not required anymore

--- integrations/sunco/integration.definition.ts
+++ integrations/sunco/integration.definition.ts
@@ -40,7 +40,7 @@ export default new IntegrationDefinition({
   configuration: {
     identifier: {
       linkTemplateScript: 'linkTemplate.vrl',
-      required: true,
+      required: false,
     },
     schema: z.object({}),
   },
