9ab406b85ab1b4436d91db640ffdd852c2bcdb1d
François Levasseur <francois_levasseur@hotmail.com>
2026-02-16 15:13:58 -0500

    chore(zui): rm internal usage of ZodFirstPartyTypeKind (#14950)

--- packages/zui/src/z/types/native.test.ts
+++ packages/zui/src/z/types/native.test.ts
@@ -1,140 +1,123 @@
 import { test } from 'vitest'
-import { ZodFirstPartySchemaTypes, ZodFirstPartyTypeKind } from './native'
 import * as z from '../index'
 import { util } from './utils'
 
 test('first party switch', () => {
-  const myType = z.string() as z.ZodNativeSchema
+  const myType = z.ZodString.create() as z.ZodNativeSchema
 
   switch (myType.typeName) {
-    case z.ZodFirstPartyTypeKind.ZodString:
+    case 'ZodString':
       myType satisfies z.ZodString
       break
-    case z.ZodFirstPartyTypeKind.ZodNumber:
+    case 'ZodNumber':
       myType satisfies z.ZodNumber
       break
-    case z.ZodFirstPartyTypeKind.ZodNaN:
+    case 'ZodNaN':
       myType satisfies z.ZodNaN
       break
-    case z.ZodFirstPartyTypeKind.ZodBigInt:
+    case 'ZodBigInt':
       myType satisfies z.ZodBigInt
       break
-    case z.ZodFirstPartyTypeKind.ZodBoolean:
+    case 'ZodBoolean':
       myType satisfies z.ZodBoolean
       break
-    case z.ZodFirstPartyTypeKind.ZodDate:
+    case 'ZodDate':
       myType satisfies z.ZodDate
       break
-    case z.ZodFirstPartyTypeKind.ZodUndefined:
+    case 'ZodUndefined':
       myType satisfies z.ZodUndefined
       break
-    case z.ZodFirstPartyTypeKind.ZodNull:
+    case 'ZodNull':
       myType satisfies z.ZodNull
       break
-    case z.ZodFirstPartyTypeKind.ZodAny:
+    case 'ZodAny':
       myType satisfies z.ZodAny
       break
-    case z.ZodFirstPartyTypeKind.ZodUnknown:
+    case 'ZodUnknown':
       myType satisfies z.ZodUnknown
       break
-    case z.ZodFirstPartyTypeKind.ZodNever:
+    case 'ZodNever':
       myType satisfies z.ZodNever
       break
-    case z.ZodFirstPartyTypeKind.ZodVoid:
+    case 'ZodVoid':
       myType satisfies z.ZodVoid
       break
-    case z.ZodFirstPartyTypeKind.ZodArray:
+    case 'ZodArray':
       myType satisfies z.ZodArray
       break
-    case z.ZodFirstPartyTypeKind.ZodObject:
+    case 'ZodObject':
       myType satisfies z.ZodObject
       break
-    case z.ZodFirstPartyTypeKind.ZodUnion:
+    case 'ZodUnion':
       myType satisfies z.ZodUnion
       break
-    case z.ZodFirstPartyTypeKind.ZodDiscriminatedUnion:
+    case 'ZodDiscriminatedUnion':
       myType satisfies z.ZodDiscriminatedUnion
       break
-    case z.ZodFirstPartyTypeKind.ZodIntersection:
+    case 'ZodIntersection':
       myType satisfies z.ZodIntersection
       break
-    case z.ZodFirstPartyTypeKind.ZodTuple:
+    case 'ZodTuple':
       myType satisfies z.ZodTuple
       break
-    case z.ZodFirstPartyTypeKind.ZodRecord:
+    case 'ZodRecord':
       myType satisfies z.ZodRecord
       break
-    case z.ZodFirstPartyTypeKind.ZodRef:
+    case 'ZodRef':
       myType satisfies z.ZodRef
       break
-    case z.ZodFirstPartyTypeKind.ZodMap:
+    case 'ZodMap':
       myType satisfies z.ZodMap
       break
-    case z.ZodFirstPartyTypeKind.ZodSet:
+    case 'ZodSet':
       myType satisfies z.ZodSet
       break
-    case z.ZodFirstPartyTypeKind.ZodFunction:
+    case 'ZodFunction':
       myType satisfies z.ZodFunction
       break
-    case z.ZodFirstPartyTypeKind.ZodLazy:
+    case 'ZodLazy':
       myType satisfies z.ZodLazy
       break
-    case z.ZodFirstPartyTypeKind.ZodLiteral:
+    case 'ZodLiteral':
       myType satisfies z.ZodLiteral
       break
-    case z.ZodFirstPartyTypeKind.ZodEnum:
+    case 'ZodEnum':
       myType satisfies z.ZodEnum
       break
-    case z.ZodFirstPartyTypeKind.ZodEffects:
+    case 'ZodEffects':
       myType satisfies z.ZodEffects
       break
-    case z.ZodFirstPartyTypeKind.ZodNativeEnum:
+    case 'ZodNativeEnum':
       myType satisfies z.ZodNativeEnum
       break
-    case z.ZodFirstPartyTypeKind.ZodOptional:
+    case 'ZodOptional':
       myType satisfies z.ZodOptional
       break
-    case z.ZodFirstPartyTypeKind.ZodNullable:
+    case 'ZodNullable':
       myType satisfies z.ZodNullable
       break
-    case z.ZodFirstPartyTypeKind.ZodDefault:
+    case 'ZodDefault':
       myType satisfies z.ZodDefault
       break
-    case z.ZodFirstPartyTypeKind.ZodCatch:
+    case 'ZodCatch':
       myType satisfies z.ZodCatch
       break
-    case z.ZodFirstPartyTypeKind.ZodPromise:
+    case 'ZodPromise':
       myType satisfies z.ZodPromise
       break
-    case z.ZodFirstPartyTypeKind.ZodBranded:
+    case 'ZodBranded':
       myType satisfies z.ZodBranded
       break
-    case z.ZodFirstPartyTypeKind.ZodPipeline:
+    case 'ZodPipeline':
       myType satisfies z.ZodPipeline
       break
-    case z.ZodFirstPartyTypeKind.ZodSymbol:
+    case 'ZodSymbol':
       myType satisfies z.ZodSymbol
       break
-    case z.ZodFirstPartyTypeKind.ZodReadonly:
+    case 'ZodReadonly':
       myType satisfies z.ZodReadonly
       break
     default:
       util.assertNever(myType)
   }
-})
-
-test('Identify missing [ZodFirstPartySchemaTypes]', () => {
-  type ZodFirstPartySchemaForType<T extends ZodFirstPartyTypeKind> = ZodFirstPartySchemaTypes extends infer Schema
-    ? Schema extends { _def: { typeName: T } }
-      ? Schema
-      : never
-    : never
-  type ZodMappedTypes = {
-    [key in ZodFirstPartyTypeKind]: ZodFirstPartySchemaForType<key>
-  }
-  type ZodFirstPartySchemaTypesMissingFromUnion = keyof {
-    [key in keyof ZodMappedTypes as ZodMappedTypes[key] extends { _def: never } ? key : never]: unknown
-  }
-
-  util.assertEqual<ZodFirstPartySchemaTypesMissingFromUnion, never>(true)
 })
