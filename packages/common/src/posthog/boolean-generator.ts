dce8d2d494cc2e270c170dfb3b0f9744316fcf16
Mathieu Faucher <faucher.mathieu@gmail.com>
2026-02-23 17:11:01 -0500

    chore: resolve nitpick

--- packages/common/src/posthog/boolean-generator.ts
+++ packages/common/src/posthog/boolean-generator.ts
@@ -1,6 +1,6 @@
 export const useBooleanGenerator = (truthyPercentage: number): (() => boolean) => {
   if (truthyPercentage <= 0 || truthyPercentage > 100 || Number.isNaN(truthyPercentage)) {
-    throw new Error('Percentage must be an integer between 1 and 100')
+    throw new Error('Percentage must be a number between 1 and 100')
   }
 
   if (truthyPercentage === 100) {
