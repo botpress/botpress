f3640bbcf4d398fc01cb55b091b16296a7d249ab
François Levasseur <francois_levasseur@hotmail.com>
2026-02-25 18:54:23 -0500

    update

--- packages/zui/src/z/guards.ts
+++ packages/zui/src/z/guards.ts
@@ -6,6 +6,6 @@ import { ZodBaseTypeImpl } from './types'
 export const isZuiError = (thrown: unknown): thrown is IZodError =>
   thrown instanceof ZodError || (thrown instanceof Error && '__type__' in thrown && thrown.__type__ === 'ZuiError')
 
-export const isZuiType = (value: unknown): value is ZodNativeType =>
-  value instanceof ZodBaseTypeImpl ||
-  (typeof value === 'object' && value !== null && '__type__' in value && value.__type__ === 'ZuiType')
+export const isZuiType = (value: unknown): value is ZodNativeType => value instanceof ZodBaseTypeImpl
+// TODO: also accept objects with a __type__ property set to 'ZuiType' to allow for cross-realm compatibility
+// || (typeof value === 'object' && value !== null && '__type__' in value && value.__type__ === 'ZuiType')
