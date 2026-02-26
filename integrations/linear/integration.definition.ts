ed8b5e5fe71e16a08eb68616f3832feb71ffca29
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-02-26 12:20:20 -0500

    bump major

--- integrations/linear/integration.definition.ts
+++ integrations/linear/integration.definition.ts
@@ -7,7 +7,7 @@ import listable from './bp_modules/listable'
 import { actions, channels, events, configuration, configurations, user, states, entities } from './definitions'
 
 export const INTEGRATION_NAME = 'linear'
-export const INTEGRATION_VERSION = '1.3.1'
+export const INTEGRATION_VERSION = '1.4.0'
 
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
