84d127f05f5e2668025f3ff61a2e5dc95c89f8b6
quu-ack <quentin.ackermann@botpress.com>
2026-03-10 12:42:09 -0400

    fix queue

--- plugins/knowledge-connector/src/hooks/workflow-started/process-queue.ts
+++ plugins/knowledge-connector/src/hooks/workflow-started/process-queue.ts
@@ -1,5 +1,50 @@
+import * as SyncQueue from '../../sync-queue'
+import { createIntegrationTransferHandler } from '../../utils/create-integration-transfer-handler'
 import * as bp from '.botpress'
 
 export const handleEvent: bp.WorkflowHandlers['processQueue'] = async (props) => {
+  const { syncQueue, key } = await SyncQueue.jobFileManager.getSyncQueue(props)
+  const logger = props.logger.withWorkflowId(props.workflow.id)
+
+  if (
+    !props.workflow.tags.integrationInstanceAlias ||
+    !props.workflow.tags.syncJobId ||
+    !props.workflow.tags.integrationDefinitionName
+  ) {
+    throw new Error(
+      'Missing required workflow tags: integrationInstanceAlias, integrationDefinitionName, and syncJobId'
+    )
+  }
+
   await props.workflow.acknowledgeStartOfProcessing()
+
+  try {
+    const processResult = await SyncQueue.queueProcessor.processQueue({
+      logger,
+      syncQueue,
+      fileRepository: props.client,
+      integration: createIntegrationTransferHandler({
+        integrationName: props.workflow.tags.integrationDefinitionName,
+        integrationAlias: props.workflow.tags.integrationInstanceAlias,
+        client: props.client,
+        transferFileToBotpressAlias: props.workflow.input.transferFileToBotpressAlias,
+        shouldIndex: true,
+      }),
+      updateSyncQueue: (params) => SyncQueue.jobFileManager.updateSyncQueue(props, key, params.syncQueue),
+    })
+
+    if (processResult.finished === 'batch') {
+      logger.info('Batch sync success. Continuing to next batch...')
+      const timeIn5Minutes = new Date(Date.now() + 300_000).toISOString()
+      await props.workflow.update({ timeoutAt: timeIn5Minutes })
+      return
+    }
+
+    logger.info('Sync completed successfully')
+    await props.workflow.setCompleted()
+  } catch (error) {
+    logger.error('Error processing queue:', error)
+    await props.workflow.setFailed({ failureReason: 'Error processing queue' })
+    return
+  }
 }
