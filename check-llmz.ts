5b9558c262674bb0c00014b3ad2dfe7c4aa05dfd
François Levasseur <francois_levasseur@hotmail.com>
2026-03-14 16:36:21 -0400

    fl_llmz_troubleshoot

---
+++ check-llmz.ts
@@ -0,0 +1,31 @@
+import { Client } from '@botpress/client'
+import * as z from '@bpinternal/zui'
+import * as llmz from 'llmz'
+
+const resultExit = new llmz.Exit({
+  name: 'result',
+  description: 'Exit with the final result',
+  schema: z.number().describe('The final result as a number'),
+})
+
+const main = async () => {
+  const client = new Client({
+    workspaceId: '$workspaceId',
+    token: '$token',
+    botId: '$botId',
+  })
+
+  const output = await llmz.execute({
+    client,
+    instructions: 'What is 2 times 2 ?',
+    exits: [resultExit],
+  })
+
+  if (!output.is(resultExit)) {
+    throw new Error('Unexpected exit')
+  }
+
+  console.info(output.output)
+}
+
+void main()
