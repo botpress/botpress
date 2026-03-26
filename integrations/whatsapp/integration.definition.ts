9eead3222434363529f13442a55c8870b53bb591
Mathieu Faucher <faucher.mathieu@gmail.com>
2026-03-26 09:23:04 -0400

    feat(whatsapp): typing & template variables

--- integrations/whatsapp/integration.definition.ts
+++ integrations/whatsapp/integration.definition.ts
@@ -99,7 +99,7 @@ const defaultBotPhoneNumberId = {
 }
 
 export const INTEGRATION_NAME = 'whatsapp'
-export const INTEGRATION_VERSION = '4.10.0'
+export const INTEGRATION_VERSION = '4.11.0'
 export default new IntegrationDefinition({
   name: INTEGRATION_NAME,
   version: INTEGRATION_VERSION,
