093c8e3a33e2aec30b2e472c07ee8177b095013d
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-04-23 16:20:51 -0400

    fix bots

--- bots/clog/src/index.ts
+++ bots/clog/src/index.ts
@@ -1,78 +1,90 @@
-// import axios from 'axios'
-// import * as cheerio from 'cheerio'
+import * as cheerio from 'cheerio'
 import * as bp from '.botpress'
 
-// const SLACK_CHANNEL_TO_PING = 'alert-squid'
+const SLACK_CHANNEL_TO_PING = 'alert-squid'
 
 const bot = new bp.Bot({
   actions: {},
 })
 
-// const _handleApiChange = async (
-//   message: string,
-//   newGraphApiVersion: string | undefined,
-//   props: bp.EventHandlerProps
-// ): Promise<void> => {
-//   const { client, logger } = props
-//   logger.info(message)
-//   const response = await props.client.callAction({
-//     type: 'slack:startChannelConversation',
-//     input: {
-//       channelName: SLACK_CHANNEL_TO_PING,
-//     },
-//   })
-//   await props.client.createMessage({
-//     type: 'text',
-//     conversationId: response.output.conversationId,
-//     tags: {},
-//     userId: props.ctx.botId,
-//     payload: {
-//       text: message,
-//     },
-//   })
-//   await client.setState({
-//     name: 'metaApiVersions',
-//     type: 'bot',
-//     id: props.ctx.botId,
-//     payload: { currentGraphApiVersion: newGraphApiVersion },
-//   })
-// }
+const _resolveSlackChannelId = async (client: bp.Client, channelName: string): Promise<string | undefined> => {
+  const { output } = await client.callAction({
+    type: 'slack:findTarget',
+    input: { query: channelName, channel: 'channel' },
+  })
+  const exact = output.targets.find((t) => t.displayName === channelName)
+  return (exact ?? output.targets[0])?.tags.id
+}
 
-// // Checks if starts with v, has a number of at least one digit, a dot and a single digit (e.g. v00.0)
-// const versionRegexp: RegExp = /v\d+.\d/i
-// const isVersionString = (s: string): boolean => versionRegexp.test(s)
+const _handleApiChange = async (
+  message: string,
+  newGraphApiVersion: string | undefined,
+  props: bp.EventHandlerProps
+): Promise<void> => {
+  const { client, logger } = props
+  logger.info(message)
+  const channelId = await _resolveSlackChannelId(client, SLACK_CHANNEL_TO_PING)
+  if (!channelId) {
+    logger.error(`Could not find Slack channel '${SLACK_CHANNEL_TO_PING}'`)
+    return
+  }
+  const response = await client.callAction({
+    type: 'slack:getOrCreateChannelConversation',
+    input: { conversation: { channelId } },
+  })
+  await client.createMessage({
+    type: 'text',
+    conversationId: response.output.conversationId,
+    tags: {},
+    userId: props.ctx.botId,
+    payload: {
+      text: message,
+    },
+  })
+  await client.setState({
+    name: 'metaApiVersions',
+    type: 'bot',
+    id: props.ctx.botId,
+    payload: { currentGraphApiVersion: newGraphApiVersion },
+  })
+}
 
-// bot.on.event('timeToCheckApi', async (props) => {
-//   const { client, ctx, logger } = props
-//   const { state } = await client.getOrSetState({
-//     name: 'metaApiVersions',
-//     type: 'bot',
-//     id: ctx.botId,
-//     payload: { currentGraphApiVersion: undefined },
-//   })
-//   const currentGraphApiVersion = state.payload.currentGraphApiVersion
+// Checks if starts with v, has a number of at least one digit, a dot and a single digit (e.g. v00.0)
+const versionRegexp: RegExp = /v\d+.\d/i
+const isVersionString = (s: string): boolean => versionRegexp.test(s)
 
-//   const response = await axios.get('https://developers.facebook.com/docs/graph-api/changelog/')
-//   const selector = cheerio.load(response.data)
-//   const newGraphApiVersion = selector('code').first().text().trim()
+bot.on.event('timeToCheckApi', async (props) => {
+  const { client, ctx, logger } = props
+  const { state } = await client.getOrSetState({
+    name: 'metaApiVersions',
+    type: 'bot',
+    id: ctx.botId,
+    payload: { currentGraphApiVersion: undefined },
+  })
+  const currentGraphApiVersion = state.payload.currentGraphApiVersion
 
-//   if (!isVersionString(newGraphApiVersion)) {
-//     await _handleApiChange(
-//       `I failed reading the Meta's API version, I received\n${newGraphApiVersion}`,
-//       undefined,
-//       props
-//     )
-//   } else if (currentGraphApiVersion === undefined) {
-//     await _handleApiChange(
-//       `I'll notify you when Meta's Graph API version will change.\nThe current version is ${newGraphApiVersion}`,
-//       newGraphApiVersion,
-//       props
-//     )
-//   } else if (currentGraphApiVersion !== newGraphApiVersion) {
-//     await _handleApiChange(`Meta's Graph API version changed to: ${newGraphApiVersion}`, newGraphApiVersion, props)
-//   } else {
-//     logger.info(`Meta's Graph API version is ${newGraphApiVersion}`)
-//   }
-// })
+  const response = await fetch('https://developers.facebook.com/docs/graph-api/changelog/')
+  const html = await response.text()
+  const selector = cheerio.load(html)
+  const newGraphApiVersion = selector('code').first().text().trim()
+
+  if (!isVersionString(newGraphApiVersion)) {
+    await _handleApiChange(
+      `I failed reading the Meta's API version, I received\n${newGraphApiVersion}`,
+      undefined,
+      props
+    )
+  } else if (currentGraphApiVersion === undefined) {
+    await _handleApiChange(
+      `I'll notify you when Meta's Graph API version will change.\nThe current version is ${newGraphApiVersion}`,
+      newGraphApiVersion,
+      props
+    )
+  } else if (currentGraphApiVersion !== newGraphApiVersion) {
+    await _handleApiChange(`Meta's Graph API version changed to: ${newGraphApiVersion}`, newGraphApiVersion, props)
+  } else {
+    logger.info(`Meta's Graph API version is ${newGraphApiVersion}`)
+  }
+})
 
 export default bot
