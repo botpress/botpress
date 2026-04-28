9da832056a5e6ded8e2c35a8bc30e00181e5968e
FelixGCliche <48337098+FelixGCliche@users.noreply.github.com>
2026-04-28 11:21:44 -0400

    add webhook handler

--- integrations/airtable/src/index.ts
+++ integrations/airtable/src/index.ts
@@ -1,17 +1,29 @@
+import { isOAuthWizardUrl } from '@botpress/common/src/oauth-wizard'
+import * as sdk from '@botpress/sdk'
 import actions from './actions'
 import { AirtableClient } from './airtable-api/airtable-client'
+import { oauthWizardHandler } from './oauth-wizard'
 import * as botpress from '.botpress'
 
 export default new botpress.Integration({
   register: async ({ client, ctx, logger }) => {
-    const airtableClient = await AirtableClient.createFromStates({ client, ctx, logger })
-
-    await airtableClient.testConnection()
+    try {
+      const airtableClient = await AirtableClient.createFromStates({ client, ctx, logger })
+      await airtableClient.testConnection()
+    } catch (thrown) {
+      const message = thrown instanceof Error ? thrown.message : String(thrown)
+      throw new sdk.RuntimeError(`Failed to connect to Airtable. Re-run the setup wizard. (${message})`)
+    }
 
     logger.forBot().info('Connection to Airtable successful')
   },
   unregister: async () => {},
   actions,
   channels: {},
-  handler: async () => {},
+  handler: async (props) => {
+    if (isOAuthWizardUrl(props.req.path)) {
+      return await oauthWizardHandler(props)
+    }
+    return
+  },
 })
