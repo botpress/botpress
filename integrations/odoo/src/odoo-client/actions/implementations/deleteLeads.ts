b22dd7e0f0aa69a1f6a44051750dd60aab8fd357
Makhlouf Hennine <makhlouf.hennine@Makhloufs-MBP.localdomain>
2026-05-06 19:19:48 -0400

    feat(odoo-actions): implement delete leads

---
+++ integrations/odoo/src/odoo-client/actions/implementations/deleteLeads.ts
@@ -0,0 +1,10 @@
+import { wrapAction } from '../action-wrapper'
+
+export const deleteLeads = wrapAction(
+  { actionName: 'deleteLeads', errorMessage: 'Failed to delete Odoo leads' },
+  async ({ odooClient }, input) => {
+    const success = await odooClient.deleteLeads(input)
+
+    return { success }
+  }
+)
