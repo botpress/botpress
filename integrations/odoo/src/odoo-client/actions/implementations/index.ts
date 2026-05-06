2579efc24ed489ed9a8d169f0a582cdb8c307f2f
Makhlouf Hennine <makhlouf.hennine@Makhloufs-MBP.localdomain>
2026-05-06 19:17:55 -0400

    feat(odoo-actions): register lead action handlers

--- integrations/odoo/src/odoo-client/actions/implementations/index.ts
+++ integrations/odoo/src/odoo-client/actions/implementations/index.ts
@@ -1,15 +1,27 @@
 import { createContact } from './createContact'
+import { createLead } from './createLead'
 import { deleteContacts } from './deleteContacts'
+import { deleteLeads } from './deleteLeads'
 import { getContactFields } from './getContactFields'
 import { getContacts } from './getContacts'
 import { getCurrentUser } from './getCurrentUser'
+import { getLeadFields } from './getLeadFields'
+import { getLeads } from './getLeads'
 import { searchContacts } from './searchContacts'
+import { searchLeads } from './searchLeads'
 import { updateContacts } from './updateContacts'
+import { updateLeads } from './updateLeads'
 import * as bp from '.botpress'
 
 export default {
   getCurrentUser,
   getContactFields,
+  getLeadFields,
+  searchLeads,
+  getLeads,
+  createLead,
+  updateLeads,
+  deleteLeads,
   searchContacts,
   getContacts,
   createContact,
