c323dc58e76d6f941c2ba990549153bc05c14664
Makhlouf Hennine <makhlouf.hennine@Makhloufs-MBP.localdomain>
2026-05-06 19:19:35 -0400

    feat(odoo-actions): implement update leads

---
+++ integrations/odoo/src/odoo-client/actions/implementations/updateLeads.ts
@@ -0,0 +1,10 @@
+import { wrapAction } from '../action-wrapper'
+
+export const updateLeads = wrapAction(
+  { actionName: 'updateLeads', errorMessage: 'Failed to update Odoo leads' },
+  async ({ odooClient }, input) => {
+    const success = await odooClient.updateLeads(input)
+
+    return { success }
+  }
+)
