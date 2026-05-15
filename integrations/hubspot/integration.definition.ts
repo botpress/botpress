801f0963111607b8c16fc338de69d3ead88448d9
sergeikofman444 <sergeikofman444@gmail.com>
2026-05-15 15:17:50 -0400

    fix spelling

--- integrations/hubspot/integration.definition.ts
+++ integrations/hubspot/integration.definition.ts
@@ -6,7 +6,7 @@ export default new IntegrationDefinition({
   name: 'hubspot',
   title: 'HubSpot',
   description: 'Manage contacts, tickets and more from your chatbot.',
-  version: '6.0.4',
+  version: '6.0.5',
   readme: 'hub.md',
   icon: 'icon.svg',
   configuration: {
