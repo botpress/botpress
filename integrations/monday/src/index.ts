cc20022eb499eeb8fe9cee99320d9bdd32cdb0c9
Makhlouf Hennine <makhlouf.hennine@Makhloufs-MBP.localdomain>
2026-05-19 17:16:34 -0400

    feat(monday): route OAuth wizard requests

--- integrations/monday/src/index.ts
+++ integrations/monday/src/index.ts
@@ -1,10 +1,18 @@
 import * as actions from 'src/actions'
 import * as bp from '.botpress'
+import { isOAuthWizardUrl } from '@botpress/common/src/oauth-wizard'
+import { oauthWizardHandler } from './oauth-wizard'
 
 export default new bp.Integration({
   register: async () => {},
   unregister: async () => {},
   actions,
   channels: {},
-  handler: async () => {},
+  handler: async (props) => {
+    if (isOAuthWizardUrl(props.req.path)) {
+      return await oauthWizardHandler(props)
+    }
+
+    return
+  },
 })
