c9bdfdfae3f7c93368d214cc4f5773cf05d0c279
Makhlouf Hennine <makhlouf.hennine@Makhloufs-MBP.localdomain>
2026-05-19 17:21:03 -0400

    feat(monday): use OAuth token for item creation

--- integrations/monday/src/actions/index.ts
+++ integrations/monday/src/actions/index.ts
@@ -1,16 +1,41 @@
+import { RuntimeError, isApiError } from '@botpress/sdk'
 import { MondayClient } from 'src/misc/monday-client'
-import { IntegrationProps } from '.botpress'
+import * as bp from '.botpress'
 
-type CreateItem = IntegrationProps['actions']['createItem']
+type CreateItem = bp.IntegrationProps['actions']['createItem']
 
-export const createItem: CreateItem = async ({ input, ctx }) => {
-  const client = MondayClient.create({
-    personalAccessToken: ctx.configuration.personalAccessToken,
+export const createItem: CreateItem = async ({ input, ctx, client }) => {
+  const accessToken = (await _getOAuthAccessToken({ client, ctx })) ?? ctx.configuration.personalAccessToken
+
+  if (!accessToken) {
+    throw new RuntimeError('Monday credentials are missing. Please connect your Monday account or provide a personal access token.')
+  }
+
+  const mondayClient = MondayClient.create({
+    personalAccessToken: accessToken,
   })
 
-  await client.createItem(input.boardId, {
+  await mondayClient.createItem(input.boardId, {
     name: input.itemName,
   })
 
   return {}
+}
+
+const _getOAuthAccessToken = async ({ client, ctx }: { client: bp.Client; ctx: bp.Context }) => {
+  const { state } = await client
+    .getState({
+      type: 'integration',
+      name: 'oAuthCredentials',
+      id: ctx.integrationId,
+    })
+    .catch((thrown: unknown) => {
+      if (isApiError(thrown) && thrown.code === 404) {
+        return { state: undefined }
+      }
+
+      throw thrown
+    })
+
+  return state?.payload.accessToken
 }
