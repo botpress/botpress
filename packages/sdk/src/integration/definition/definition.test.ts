2cdef06c56bf9e4adb4132821bd7a93708636cc4
DubeSebastien90 <sebastien.dube@ulaval.ca>
2026-06-03 15:25:44 -0400

    chore: added tests

--- packages/sdk/src/integration/definition/definition.test.ts
+++ packages/sdk/src/integration/definition/definition.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from 'vitest'
+import { z } from '../../zui'
 import { IntegrationDefinition } from '.'
 import { InterfaceDefinition } from '../../interface'
 import { InterfacePackage } from '../../package'
@@ -59,4 +60,81 @@ test('integration with channel extending an interface with same channel merges c
     message: { tags: { foo: {}, bar: {} } },
   }
   expect(actual).toEqual(expected)
+})
+
+const variantA = z.object({ type: z.literal('a'), foo: z.string() })
+const variantB = z.object({ type: z.literal('b'), bar: z.number() })
+
+const makeDiscriminatedUnionPkg = (schema: z.ZodDiscriminatedUnion): InterfacePackage => {
+  const intrface = new InterfaceDefinition({
+    name: 'foo',
+    version: '0.0.0',
+    events: { theEvent: { schema: () => schema } },
+  })
+  return { type: 'interface', name: 'foo', version: '0.0.0', definition: intrface }
+}
+
+test('extending with discriminated union events merges the variants', () => {
+  const integration = new IntegrationDefinition({
+    name: 'foo',
+    version: '0.0.0',
+    events: {
+      theEvent: { schema: z.discriminatedUnion('type', [variantA]) },
+    },
+  }).extend(makeDiscriminatedUnionPkg(z.discriminatedUnion('type', [variantB])), () => ({
+    entities: {},
+  }))
+
+  const merged = integration.events!.theEvent.schema as z.ZodDiscriminatedUnion
+  expect(merged._def.typeName).toBe('ZodDiscriminatedUnion')
+  expect(merged._def.discriminator).toBe('type')
+  expect(merged._def.options).toHaveLength(2)
+})
+
+test('extending with discriminated union actions merges input and output variants', () => {
+  const intrface = new InterfaceDefinition({
+    name: 'foo',
+    version: '0.0.0',
+    actions: {
+      theAction: {
+        input: { schema: () => z.discriminatedUnion('type', [variantA]) },
+        output: { schema: () => z.discriminatedUnion('type', [variantA]) },
+      },
+    },
+  })
+  const pkg: InterfacePackage = { type: 'interface', name: 'foo', version: '0.0.0', definition: intrface }
+
+  const integration = new IntegrationDefinition({
+    name: 'foo',
+    version: '0.0.0',
+    actions: {
+      theAction: {
+        input: { schema: z.discriminatedUnion('type', [variantB]) },
+        output: { schema: z.discriminatedUnion('type', [variantB]) },
+      },
+    },
+  }).extend(pkg, () => ({ entities: {} }))
+
+  const mergedInput = integration.actions!.theAction.input.schema as z.ZodDiscriminatedUnion
+  const mergedOutput = integration.actions!.theAction.output.schema as z.ZodDiscriminatedUnion
+  expect(mergedInput._def.options).toHaveLength(2)
+  expect(mergedOutput._def.options).toHaveLength(2)
+})
+
+test('extending discriminated unions with different discriminator keys throws', () => {
+  const withOtherDiscriminator = z.discriminatedUnion('kind', [
+    z.object({ kind: z.literal('x'), foo: z.string() }),
+  ])
+
+  const integration = new IntegrationDefinition({
+    name: 'foo',
+    version: '0.0.0',
+    events: {
+      theEvent: { schema: z.discriminatedUnion('type', [variantA]) },
+    },
+  })
+
+  expect(() =>
+    integration.extend(makeDiscriminatedUnionPkg(withOtherDiscriminator), () => ({ entities: {} }))
+  ).toThrow("Cannot merge discriminated unions with different discriminator keys: 'type' and 'kind'")
 })
