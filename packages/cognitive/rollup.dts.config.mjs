a6723522e8bb2e317fb01e9d6d791bfa3e2b4bc5
Francois Levasseur <francois_levasseur@hotmail.com>
2026-06-05 13:56:58 -0400

    chore(cognitive): replace tsup by rollup dts plugin

---
+++ packages/cognitive/rollup.dts.config.mjs
@@ -0,0 +1,13 @@
+import dts from 'rollup-plugin-dts'
+
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './dist/index.d.ts',
+  },
+  plugins: [
+    dts({
+      tsconfig: './tsconfig.build.json',
+    }),
+  ],
+}
