64cb03ce7809c6dc85e64a6b1f724c75c1210b50
Francois Levasseur <francois_levasseur@hotmail.com>
2026-06-05 14:05:09 -0400

    chore(client): fix client type bundle

--- packages/client/rollup.dts.config.mjs
+++ packages/client/rollup.dts.config.mjs
@@ -2,14 +2,12 @@ import dts from 'rollup-plugin-dts'
 
 export default {
   input: './src/index.ts',
-  external: [/node_modules/],
   output: {
     file: './dist/index.d.ts',
   },
   plugins: [
     dts({
       tsconfig: './tsconfig.build.json',
-      respectExternal: true,
     }),
   ],
 }
