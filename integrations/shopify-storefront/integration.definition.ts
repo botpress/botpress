1ec89c3d5e6cb5a3864527793874b8f896845a92
sergeikofman444 <sergeikofman444@gmail.com>
2026-06-09 13:46:18 -0400

    bump version

--- integrations/shopify-storefront/integration.definition.ts
+++ integrations/shopify-storefront/integration.definition.ts
@@ -3,7 +3,7 @@ import { actions, states, configuration, secrets } from './definitions'
 
 export default new IntegrationDefinition({
   name: 'shopify-storefront',
-  version: '0.1.2',
+  version: '0.1.3',
   title: 'Shopify Storefront',
   description:
     'Connect your Shopify store via the Storefront API to power buyer-facing product browsing, collections, and cart/checkout flows via OAuth 2.0.',
