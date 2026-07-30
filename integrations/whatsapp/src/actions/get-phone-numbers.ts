fa9728bf473ed2c7fd4f511c656e60edc246bef2
Xavier Hamel <xavier.hamel@botpress.com>
2026-07-30 11:21:21 -0400

    fix

--- integrations/whatsapp/src/actions/get-phone-numbers.ts
+++ integrations/whatsapp/src/actions/get-phone-numbers.ts
@@ -1,7 +1,11 @@
 import { getAccessToken, getWabaId, MetaOauthClient } from '../auth'
+import { logForBotAndThrow } from '../misc/util'
 import * as bp from '.botpress'
 
 export const getPhoneNumbers: bp.IntegrationProps['actions']['getPhoneNumbers'] = async ({ client, ctx, logger }) => {
+  if (ctx.configurationType === 'sandbox') {
+    logForBotAndThrow('Getting phone numbers is not supported in sandbox mode', logger)
+  }
   const accessToken = await getAccessToken(client, ctx)
   const wabaId = await getWabaId(client, ctx)
 
