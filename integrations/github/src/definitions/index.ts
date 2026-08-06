a6410a430bb8d07db973c8891857d1d92992d8e4
DubeSebastien90 <sebastien.dube@ulaval.ca>
2026-08-06 13:51:30 -0400

    update

--- integrations/github/src/definitions/index.ts
+++ integrations/github/src/definitions/index.ts
@@ -40,7 +40,7 @@ export const configurations = {
         .secret()
         .title('GitHub App Private Key')
         .describe('The raw contents of the RSA private key. Can be downloaded from the GitHub App settings.')
-        .placeholder('-----BEGIN RSA PRIVATE KEY-----\n\n...\n\n-----END RSA PRIVATE KE-----'),
+        .placeholder('-----BEGIN RSA PRIVATE KEY-----\n\n...\n\n-----END RSA PRIVATE KEY-----'),
       githubAppInstallationId: z
         .number()
         .positive()
