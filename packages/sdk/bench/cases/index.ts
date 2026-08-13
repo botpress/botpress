f41bdf78be1cb903db098989684be54640104ac3
DubeSebastien90 <sebastien.dube@ulaval.ca>
2026-08-13 14:00:36 -0400

    chore: removed some tests

--- packages/sdk/bench/cases/index.ts
+++ packages/sdk/bench/cases/index.ts
@@ -1,14 +1,7 @@
 import * as types from '../types.js'
-import botAddIntegrationChain10 from './bot-add-integration-chain-10.js'
+import botHandlersScale30 from './bot-handlers-scale-30.js'
 import control from './control.js'
-import extendChain10 from './extend-chain-10.js'
 import manyActions30 from './many-actions-30.js'
 import realHitlPlugin from './real-hitl-plugin.js'
 
-export const cases = [
-  control,
-  manyActions30,
-  botAddIntegrationChain10,
-  extendChain10,
-  realHitlPlugin,
-] satisfies types.BenchmarkCase[]
+export const cases = [control, manyActions30, realHitlPlugin, botHandlersScale30] satisfies types.BenchmarkCase[]
