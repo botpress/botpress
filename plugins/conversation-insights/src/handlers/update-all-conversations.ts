1d21366ae8f7fedde437ef46ee8eab2c5a340a1e
Mathieu Faucher <faucher.mathieu@gmail.com>
2026-08-18 16:15:39 -0400

    fix(conversation-insight): fail the workflow after first fail

--- plugins/conversation-insights/src/handlers/update-all-conversations.ts
+++ plugins/conversation-insights/src/handlers/update-all-conversations.ts
@@ -1,14 +1,25 @@
 import { updateAllConversations } from '../updateAllConversations'
 import * as bp from '.botpress'
 
+const _updateAllConversationsOrFail = async (props: bp.WorkflowHandlerProps['updateAllConversations']) => {
+  try {
+    await updateAllConversations(props)
+  } catch (thrown) {
+    const error = thrown instanceof Error ? thrown : new Error(String(thrown))
+    await props.workflow.setFailed({
+      failureReason: `Failed to update conversation insights after the AI call retries were exhausted: ${error.message}`,
+    })
+  }
+}
+
 export const handleStartUpdateAllConversations: bp.WorkflowHandlers['updateAllConversations'] = async (props) => {
   props.logger.info('Starting updateAllConversations workflow')
-  await updateAllConversations(props)
+  await _updateAllConversationsOrFail(props)
 
   return undefined
 }
 export const handleContinueUpdateAllConversations: bp.WorkflowHandlers['updateAllConversations'] = async (props) => {
-  await updateAllConversations(props)
+  await _updateAllConversationsOrFail(props)
 
   return undefined
 }
